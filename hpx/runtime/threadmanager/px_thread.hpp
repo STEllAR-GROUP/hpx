0a6704ab5f0ccbd961db42fad908b2ebf5bc9cf1
Anshul Tandon <atandon@cct.lsu.edu>
2008-06-23 16:24:20 +0000

    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@668 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/threadmanager/px_thread.hpp
+++ hpx/runtime/threadmanager/px_thread.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2008-2009 Chirag Dekate, Hartmut Kaiser
+//  Copyright (c) 2008-2009 Chirag Dekate, Hartmut Kaiser, Anshul Tandon
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
