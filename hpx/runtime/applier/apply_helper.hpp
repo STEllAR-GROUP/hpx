e03aa94c77e49b9364ff858a99a16bb4df6a14f4
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2008-06-26 13:53:34 +0000

    Adding missing file...
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@689 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/runtime/applier/apply_helper.hpp
@@ -0,0 +1,125 @@
+//  Copyright (c) 2007-2008 Hartmut Kaiser
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_APPLIER_APPLY_HELPER_JUN_25_2008_0917PM)
+#define HPX_APPLIER_APPLY_HELPER_JUN_25_2008_0917PM
+
+#include <boost/mpl/bool.hpp>
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/include/naming.hpp>
+
+namespace hpx { namespace applier { namespace detail
+{
+    ///////////////////////////////////////////////////////////////////////
+    template <
+        typename Action,
+        typename DirectExecute = typename Action::direct_execution
+    >
+    struct apply_helper0;
+
+    template <typename Action>
+    struct apply_helper0<Action, boost::mpl::false_>
+    {
+        static void 
+        call (Action* act, threadmanager::threadmanager& tm, applier& appl, 
+            naming::address::address_type lva)
+        {
+            tm.register_work(act->get_thread_function(appl, lva));
+        }
+
+        static void 
+        call (threadmanager::threadmanager& tm, applier& appl, 
+            naming::address::address_type lva)
+        {
+            tm.register_work(Action::construct_thread_function(appl, lva));
+        }
+
+        static void 
+        call (components::continuation_type& c, 
+            threadmanager::threadmanager& tm, applier& appl, 
+            naming::address::address_type lva)
+        {
+            tm.register_work(Action::construct_thread_function(c, appl, lva));
+        }
+    };
+
+    template <typename Action>
+    struct apply_helper0<Action, boost::mpl::true_>
+    {
+        static void 
+        call (Action* act, threadmanager::threadmanager&, applier& appl, 
+            naming::address::address_type lva)
+        {
+            BOOST_ASSERT(false);    // shouldn't be called at all
+        }
+
+        // If local and to be directly executed, just call the function
+        static void
+        call (threadmanager::threadmanager&, applier& appl, 
+            naming::address::address_type addr)
+        {
+            Action::execute_function(appl, addr);
+        }
+
+        static void 
+        call (components::continuation_type& c, threadmanager::threadmanager&, 
+            applier& appl, naming::address::address_type addr)
+        {
+            Action::execute_function(appl, addr);
+            // FIXME: handle continuations
+        }
+    };
+
+    ///////////////////////////////////////////////////////////////////////
+    template <
+        typename Action, typename Arg0, 
+        typename DirectExecute = typename Action::direct_execution
+    >
+    struct apply_helper1;
+
+    template <typename Action, typename Arg0>
+    struct apply_helper1<Action, Arg0, boost::mpl::false_>
+    {
+        static void 
+        call (threadmanager::threadmanager& tm, applier& appl, 
+            naming::address::address_type addr, Arg0 const& arg0)
+        {
+            tm.register_work(Action::construct_thread_function(appl, addr, arg0));
+        }
+
+        static void 
+        call (components::continuation_type& c, 
+            threadmanager::threadmanager& tm, applier& appl, 
+            naming::address::address_type addr, Arg0 const& arg0)
+        {
+            tm.register_work(Action::construct_thread_function(c, appl, addr, arg0));
+        }
+    };
+
+    template <typename Action, typename Arg0>
+    struct apply_helper1<Action, Arg0, boost::mpl::true_>
+    {
+        // If local and to be directly executed, just call the function
+        static void
+        call (threadmanager::threadmanager&, applier& appl, 
+            naming::address::address_type addr, Arg0 const& arg0)
+        {
+            Action::execute_function(appl, addr, arg0);
+        }
+
+        static void 
+        call (components::continuation_type& c, threadmanager::threadmanager&, 
+            applier& appl, naming::address::address_type addr, 
+            Arg0 const& arg0)
+        {
+            Action::execute_function(appl, addr, arg0);
+            // FIXME: handle continuations
+        }
+    };
+
+}}}
+
+#endif
