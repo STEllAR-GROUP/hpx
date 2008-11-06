e6ddea5fa5b9d5591cf88d03bfaa686c1dd3a74a
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2008-11-06 16:59:12 +0000

    Fixed compilation problem
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@916 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/components/server/simple_component_base.hpp
+++ hpx/runtime/components/server/simple_component_base.hpp
@@ -113,7 +113,7 @@ namespace hpx { namespace components
 
         /// \brief  The function \a create is used for allocation and 
         ///         initialization of instances of the derived components.
-        static simple_component_base* 
+        static simple_component_base<Component>* 
         create(applier::applier& appl, std::size_t count)
         {
             // simple components can be created individually only
