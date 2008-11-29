ca2da798d1c15893dd9c4ebce21557f1fd8550ba
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2008-11-29 02:46:40 +0000

    More fixes
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@990 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/runtime/threads/threadmanager.cpp
+++ src/runtime/threads/threadmanager.cpp
@@ -3,10 +3,10 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/runtime/threads/threadmanager.hpp>      // must be first header!
+#include <hpx/runtime/threads/thread_affinity.hpp>      // must be first header!
+#include <hpx/runtime/threads/threadmanager.hpp>
 #include <hpx/runtime/threads/thread.hpp>
 #include <hpx/runtime/threads/thread_helpers.hpp>
-#include <hpx/runtime/threads/thread_affinity.hpp>
 #include <hpx/util/unlock_lock.hpp>
 #include <hpx/util/logging.hpp>
 #include <hpx/util/block_profiler.hpp>
