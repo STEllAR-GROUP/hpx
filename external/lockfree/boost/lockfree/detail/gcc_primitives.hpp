4ff6efe6836f8029fc9c435edc95c9964d342d55
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2008-12-12 22:17:43 +0000

    setting ax to zero speeds up the cpuid command.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@1044 5079c8e4-9419-0410-93bb-9e5ee278c886

--- external/lockfree/boost/lockfree/detail/gcc_primitives.hpp
+++ external/lockfree/boost/lockfree/detail/gcc_primitives.hpp
@@ -60,24 +60,26 @@ namespace boost { namespace lockfree
         boost::uint32_t _lo, _hi;
         __asm__ __volatile__ (
               "movl %%ebx,%%esi\n"
+              "xor eax,eax\n"
               "cpuid\n"
               "rdtsc\n"
               "movl %%esi,%%ebx\n"
             : "=a" (_lo), "=d" (_hi)
             :
-            : "%esi", "%ecx"
+            : "%esi", "%eax", "%ecx"
         );
         return ((boost::uint64_t)_hi << 32) | _lo;
 #elif defined(__x86_64__)
         boost::uint32_t _lo, _hi;
         __asm__ __volatile__ (
               "mov %%rbx,%%rsi\n"
+              "xor rax,rax\n"
               "cpuid\n"
               "rdtsc\n"
               "mov %%rsi,%%rbx\n"
             : "=a" (_lo), "=d" (_hi)
             :
-            : "%rsi", "%rcx"
+            : "%rsi", "%rax", "%rcx"
         );
         return ((boost::uint64_t)_hi << 32) | _lo;
 #endif
