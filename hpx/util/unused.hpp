86e697592e0a9f444c96391951788143599035e8
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2009-02-04 01:57:24 +0000

    One more missing file
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@1120 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/util/unused.hpp
@@ -0,0 +1,22 @@
+/*=============================================================================
+    Copyright (c) 2007-2009 Hartmut Kaiser
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+#if !defined(HPX_UNUSED_FEB_01_2009_1217PM)
+#define HPX_UNUSED_FEB_01_2009_1217PM
+
+#include <boost/fusion/include/unused.hpp>
+
+namespace hpx { namespace util
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // since boost::fusion supports exactly what we need, unused is simply
+    // imported from the fusion namespace
+    typedef boost::fusion::unused_type unused_type;
+    using boost::fusion::unused;
+
+}}
+
+#endif
