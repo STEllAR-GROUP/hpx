35dc43cc8e73a99ce12efc90253998fdf99ad78e
Dylan Stark <dstark@cct.lsu.edu>
2009-03-25 17:46:40 +0000

    Adds 'ssca2' application.
    This is mostly the setup for building the application with HPX. Eventually this application will provide an HPX implementation of the SSCA#2 Graph Analysis benchmark suite (defined as part of the HPCS program).
    
    
    
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@1333 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ applications/graphs/ssca2/rmat.cpp
@@ -0,0 +1,167 @@
+//  Copyright (c) 2007-2009 Dylan Stark
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <iostream>
+
+#include <hpx/hpx.hpp>
+#include <hpx/components/graph/graph.hpp>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+
+using namespace hpx;
+using namespace std;
+namespace po = boost::program_options;
+
+///////////////////////////////////////////////////////////////////////////////
+threads::thread_state hpx_main()
+{
+    // Create a graph.
+    std::vector<naming::id_type> localities;
+    naming::id_type locale;
+    applier::applier& appl = applier::get_applier();
+    if (appl.get_remote_prefixes(localities)) {
+        locale = localities[0];
+    }
+    else {
+        locale = appl.get_runtime_support_gid();
+    }
+    using hpx::components::graph;
+    graph G (graph::create(locale));
+
+    // Print info message
+    std::cout << "R-MAT Scalable Graph Generator\n";
+
+    // Free the graph component
+    G.free();
+    
+    // Shut down runtime services
+    components::stubs::runtime_support::shutdown_all();
+    
+    return threads::terminated;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool parse_commandline(int argc, char *argv[], po::variables_map& vm)
+{
+    try {
+        po::options_description desc_cmdline ("Usage: hpx_runtime [options]");
+        desc_cmdline.add_options()
+            ("help,h", "print out program usage (this message)")
+            ("run_agas_server,r", "run AGAS server as part of this runtime instance")
+            ("worker,w", "run this instance in worker (non-console) mode")
+            ("agas,a", po::value<std::string>(), 
+                "the IP address the AGAS server is running on (default taken "
+                "from hpx.ini), expected format: 192.168.1.1:7912")
+            ("hpx,x", po::value<std::string>(), 
+                "the IP address the HPX parcelport is listening on (default "
+                "is localhost:7910), expected format: 192.168.1.1:7913")
+            ("threads,t", po::value<int>(), 
+                "the number of operating system threads to spawn for this"
+                "HPX locality")
+        ;
+
+        po::store(po::command_line_parser(argc, argv)
+            .options(desc_cmdline).run(), vm);
+        po::notify(vm);
+
+        // print help screen
+        if (vm.count("help")) {
+            std::cout << desc_cmdline;
+            return false;
+        }
+    }
+    catch (std::exception const& e) {
+        std::cerr << "accumulator_client: exception caught: " << e.what() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+inline void 
+split_ip_address(std::string const& v, std::string& addr, boost::uint16_t& port)
+{
+    std::string::size_type p = v.find_first_of(":");
+    try {
+        if (p != std::string::npos) {
+            addr = v.substr(0, p);
+            port = boost::lexical_cast<boost::uint16_t>(v.substr(p+1));
+        }
+        else {
+            addr = v;
+        }
+    }
+    catch (boost::bad_lexical_cast const& /*e*/) {
+        std::cerr << "accumulator_client: illegal port number given: " << v.substr(p+1) << std::endl;
+        std::cerr << "                    using default value instead: " << port << std::endl;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// helper class for AGAS server initialization
+class agas_server_helper
+{
+public:
+    agas_server_helper(std::string host, boost::uint16_t port)
+      : agas_pool_(), agas_(agas_pool_, host, port)
+    {
+        agas_.run(false);
+    }
+
+private:
+    hpx::util::io_service_pool agas_pool_; 
+    hpx::naming::resolver_server agas_;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    try {
+        // analyze the command line
+        po::variables_map vm;
+        if (!parse_commandline(argc, argv, vm))
+            return -1;
+
+        // Check command line arguments.
+        std::string hpx_host("localhost"), agas_host;
+        boost::uint16_t hpx_port = HPX_PORT, agas_port = 0;
+        int num_threads = 1;
+        hpx::runtime::mode mode = hpx::runtime::console;    // default is console mode
+
+        // extract IP address/port arguments
+        if (vm.count("agas")) 
+            split_ip_address(vm["agas"].as<std::string>(), agas_host, agas_port);
+
+        if (vm.count("hpx")) 
+            split_ip_address(vm["hpx"].as<std::string>(), hpx_host, hpx_port);
+
+        if (vm.count("threads"))
+            num_threads = vm["threads"].as<int>();
+
+        if (vm.count("worker"))
+            mode = hpx::runtime::worker;
+
+        // initialize and run the AGAS service, if appropriate
+        std::auto_ptr<agas_server_helper> agas_server;
+        if (vm.count("run_agas_server"))  // run the AGAS server instance here
+            agas_server.reset(new agas_server_helper(agas_host, agas_port));
+
+        // initialize and start the HPX runtime
+        hpx::runtime rt(hpx_host, hpx_port, agas_host, agas_port, mode);
+        rt.run(hpx_main, num_threads);
+    }
+    catch (std::exception& e) {
+        std::cerr << "std::exception caught: " << e.what() << "\n";
+        return -1;
+    }
+    catch (...) {
+        std::cerr << "unexpected exception caught\n";
+        return -2;
+    }
+    return 0;
+}
+
