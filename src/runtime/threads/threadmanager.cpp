54fc5f61bf407c6aa30905a231cb2030eb40ddb9
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2009-06-23 00:25:44 +0000

    Removed non-existing #include
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@1525 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/runtime/threads/threadmanager.cpp
+++ src/runtime/threads/threadmanager.cpp
@@ -3,8 +3,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/hpx_fwd.hpp>
 #include <hpx/runtime/threads/thread_affinity.hpp>      // must be first header!
-#include <hpx/runtime/threads/threadmanager_impl.hpp>
+#include <hpx/runtime/threads/threadmanager.hpp>
 #include <hpx/runtime/threads/thread.hpp>
 #include <hpx/runtime/threads/thread_helpers.hpp>
 #include <hpx/util/unlock_lock.hpp>
