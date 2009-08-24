ce86a9c7e766402b1aceb22ce9d5313401c8fee2
Dylan Stark <dstark@cct.lsu.edu>
2009-08-24 21:02:54 +0000

    Removes some leftovers from copy/paste.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@1726 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/components/distributed_map/local_map.cpp
+++ hpx/components/distributed_map/local_map.cpp
@@ -15,15 +15,3 @@
 
 #include <hpx/components/distributed_map/server/local_map.hpp>
 #include "local_map.hpp"
-
-///////////////////////////////////////////////////////////////////////////////
-// additional action definitions required for returning List
-
-/*
-typedef hpx::components::server::ssca2::props_map_type props_map_type;
-
-HPX_REGISTER_ACTION_EX(
-    hpx::lcos::base_lco_with_value<props_map_type>::set_result_action,
-    set_result_action_vector_props_type);
-HPX_DEFINE_GET_COMPONENT_TYPE(hpx::lcos::base_lco_with_value<props_map_type>);
-*/
