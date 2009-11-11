850d2518f7b61683406ac05bceb8fe435165da6d
Matthew Anderson <manderson@cct.lsu.edu>
2009-11-11 20:25:19 +0000

    Missed a few
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@1917 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/had_amr/amr/stubs/amr_mesh_tapered.hpp
+++ examples/had_amr/amr/stubs/amr_mesh_tapered.hpp
@@ -27,33 +27,32 @@ namespace hpx { namespace components { namespace amr { namespace stubs
         ///////////////////////////////////////////////////////////////////////
         static lcos::future_value<std::vector<naming::id_type> > 
         init_execute_async(naming::id_type const& gid, 
-            components::component_type function_type, std::size_t numvalues, 
-            std::size_t numsteps, components::component_type logging_type,
-            Parameter const& par)
+            components::component_type function_type, 
+            components::component_type logging_type,
+            std::size_t level,double x,Parameter const& par)
         {
             // Create an eager_future, execute the required action,
             // we simply return the initialized future_value, the caller needs
             // to call get() on the return value to obtain the result
             typedef amr::server::amr_mesh_tapered::init_execute_action action_type;
             return lcos::eager_future<action_type>(gid, function_type,
-                numvalues, numsteps, logging_type,par);
+                logging_type,level,x, par);
         }
 
         static std::vector<naming::id_type> init_execute(naming::id_type const& gid, 
-            components::component_type function_type, std::size_t numvalues, 
-            std::size_t numsteps, components::component_type logging_type,
-            Parameter const& par)
+            components::component_type function_type,
+            components::component_type logging_type,
+            std::size_t level, double x,Parameter const& par)
         {
-            return init_execute_async(gid, function_type, numvalues, numsteps,
-                logging_type,par).get();
+            return init_execute_async(gid, function_type, logging_type,level,x,par).get();
         }
 
         ///////////////////////////////////////////////////////////////////////
         static lcos::future_value<std::vector<naming::id_type> > 
         execute_async(naming::id_type const& gid, 
             std::vector<naming::id_type> const& initial_data,
-            components::component_type function_type, std::size_t numvalues, 
-            std::size_t numsteps, components::component_type logging_type,
+            components::component_type function_type, 
+            components::component_type logging_type,
             Parameter const& par)
         {
             // Create an eager_future, execute the required action,
@@ -61,17 +60,17 @@ namespace hpx { namespace components { namespace amr { namespace stubs
             // to call get() on the return value to obtain the result
             typedef amr::server::amr_mesh_tapered::execute_action action_type;
             return lcos::eager_future<action_type>(gid, initial_data, 
-                function_type, numvalues, numsteps, logging_type, par);
+                function_type, logging_type, par);
         }
 
         static std::vector<naming::id_type> execute(naming::id_type const& gid, 
             std::vector<naming::id_type> const& initial_data,
-            components::component_type function_type, std::size_t numvalues, 
-            std::size_t numsteps, components::component_type logging_type,
+            components::component_type function_type, 
+            components::component_type logging_type,
             Parameter const& par)
         {
-            return execute_async(gid, initial_data, function_type, numvalues, 
-                numsteps, logging_type,par).get();
+            return execute_async(gid, initial_data, function_type,
+                logging_type,par).get();
         }
     };
 
