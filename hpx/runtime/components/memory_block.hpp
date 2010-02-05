cb45ea139a9cd127b247609fd4fe4fd868d2cc74
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2010-02-05 17:33:48 +0000

    Added one more helper function
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@2079 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/components/memory_block.hpp
+++ hpx/runtime/components/memory_block.hpp
@@ -205,6 +205,38 @@ namespace hpx { namespace components
             results.push_back((*lit).get());
     }
 
+    template <typename T>
+    inline access_memory_block<T>
+    get_memory_block_async(std::vector<access_memory_block<T> >& results,
+        std::vector<naming::id_type> const& gids, 
+        naming::id_type const& result)
+    {
+        typedef std::vector<lcos::future_value<memory_block_data> > 
+            lazy_results_type;
+        lazy_results_type lazy_results;
+
+        // first invoke all remote operations
+        typedef typename std::vector<naming::id_type>::const_iterator
+            const_iterator_type;
+
+        const_iterator_type end = gids.end();
+        for (const_iterator_type it = gids.begin(); it != end ++it)
+            lazy_results.push_back(stubs::memory_block::get_async(*it));
+
+        //  invoke the remote operation for the result gid as well
+        lcos::future_value<memory_block_data> lazy_result =
+            stubs::memory_block::get_async(result);
+
+        // then wait for all results to get back to us
+        typedef typename lazy_results_type::iterator iterator_type;
+        iterator_type lend = lazy_results.end();
+        for (iterator_type lit = lazy_results.begin(); lit != lend; ++lit)
+            results.push_back((*it).get());
+
+        // now return the resolved result
+        return lazy_result.get();
+    }
+
 #define BOOST_PP_ITERATION_PARAMS_1                                           \
     (3, (2, HPX_WAIT_ARGUMENT_LIMIT,                                          \
     "hpx/runtime/components/memory_block.hpp"))                               \
