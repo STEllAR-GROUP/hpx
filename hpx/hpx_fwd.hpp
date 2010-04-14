4eacab6fa148969ff91c36756c58c7ac23926e17
Alex Nagelberg <nalex@cct.lsu.edu>
2010-04-14 18:46:02 +0000

    fixed compilation issue (cstdint -> cstdlib)
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@2328 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/hpx_fwd.hpp
+++ hpx/hpx_fwd.hpp
@@ -6,7 +6,7 @@
 #if !defined(HPX_HPX_FWD_MAR_24_2008_1119AM)
 #define HPX_HPX_FWD_MAR_24_2008_1119AM
 
-#include <cstdint>
+#include <cstdlib>
 
 #include <boost/config.hpp>
 #if defined(BOOST_WINDOWS)
