0e1489faf5dcf3446eb4553b7cb9edd57d5b9160
Dylan Stark <dstark@cct.lsu.edu>
2010-07-01 18:13:49 +0000

    Moves HPX runtime setup and options processing to separate "hpx_init.hpp" header; updates fibonacci2 to use new hpx_init facility.
    The purpose of this is to reduce the burden on the applications programmer by encapsulating general runtime setup that will be the same across all (or most) application. This includes runtime options handling and helper functions such as "split_ip_address". This also has the added benefit of standardizing the set of runtime options (e.g., setting number of OS threads and selecting scheduling policy) across all application codes.
    
    
    
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@2530 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/fibonacci2.cpp
+++ examples/quickstart/fibonacci2.cpp
@@ -14,12 +14,13 @@
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/export.hpp>
 
+#include <runtime/hpx_init.hpp>
+
 using namespace hpx;
 namespace po = boost::program_options;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Helpers
-
 typedef hpx::naming::gid_type gid_type;
 
 inline gid_type find_here(void)
@@ -68,7 +69,8 @@ private:
 //     int result = fib(41);
 //     double elapsed = t.elapsed();
 // 
-//     std::cout << "elapsed: " << elapsed << ", result: " << result << std::endl;
+//     std::cout << "elapsed: " << elapsed 
+//               << ", result: " << result << std::endl;
 // }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -78,9 +80,10 @@ typedef
     actions::plain_result_action3<int, gid_type, int, int, fib> 
 fibonacci2_action;
 
-typedef lcos::eager_future<fibonacci2_action> fibonacci_future;
-
 HPX_REGISTER_PLAIN_ACTION(fibonacci2_action);
+
+///////////////////////////////////////////////////////////////////////////////
+typedef lcos::eager_future<fibonacci2_action> fibonacci_future;
 
 ///////////////////////////////////////////////////////////////////////////////
 inline void do_busy_work(double delay_coeff)
@@ -127,7 +130,7 @@ int hpx_main(po::variables_map &vm)
     if (vm.count("busywait"))
         delay_coeff = vm["busywait"].as<int>();
 
-    // try to get arguments from application configuration
+    // Try to get arguments from application configuration
     runtime& rt = get_runtime();
     argument = boost::lexical_cast<int>(
         rt.get_config().get_entry("application.fibonacci2.argument", argument));
@@ -164,203 +167,22 @@ int hpx_main(po::variables_map &vm)
     return 0;
 }
 
-///////////////////////////////////////////////////////////////////////////////
-bool parse_commandline(int argc, char *argv[], po::variables_map& vm)
-{
-    try {
-        po::options_description desc_cmdline ("Usage: fibonacci2 [options]");
-        desc_cmdline.add_options()
-            ("help,h", "print out program usage (this message)")
-            ("run_agas_server,r", "run AGAS server as part of this runtime instance")
-            ("worker,w", "run this instance in worker (non-console) mode")
-            ("config", po::value<std::string>(), 
-                "load the specified file as an application configuration file")
-            ("agas,a", po::value<std::string>(), 
-                "the IP address the AGAS server is running on (default taken "
-                "from hpx.ini), expected format: 192.168.1.1:7912")
-            ("hpx,x", po::value<std::string>(), 
-                "the IP address the HPX parcelport is listening on (default "
-                "is localhost:7910), expected format: 192.168.1.1:7913")
-            ("localities,l", po::value<int>(), 
-                "the number of localities to wait for at application startup"
-                "(default is 1)")
-            ("threads,t", po::value<int>(), 
-                "the number of operating system threads to spawn for this"
-                "HPX locality")
-            ("queueing,q", po::value<std::string>(),
-                "the queue scheduling policy to use, options are 'global' "
-                " and 'local' (default is 'global')")
-            ("value,v", po::value<int>(), 
-                "the number to be used as the argument to fib (default is 10)")
-            ("csv,s", "generate statistics of the run in comma separated format")
-            ("busywait,b", po::value<int>(),
-                "add this amount of busy wait workload to each of the iterations"
-                " [in steps of 1µs], i.e. -b1000 == 1ms")
-        ;
-
-        po::store(po::command_line_parser(argc, argv)
-            .options(desc_cmdline).run(), vm);
-        po::notify(vm);
-
-        // print help screen
-        if (vm.count("help")) {
-            std::cout << desc_cmdline;
-            return false;
-        }
-    }
-    catch (std::exception const& e) {
-        std::cerr << "fibonacci2: exception caught: " << e.what() << std::endl;
-        return false;
-    }
-    return true;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-inline void 
-split_ip_address(std::string const& v, std::string& addr, boost::uint16_t& port)
-{
-    std::string::size_type p = v.find_first_of(":");
-    try {
-        if (p != std::string::npos) {
-            addr = v.substr(0, p);
-            port = boost::lexical_cast<boost::uint16_t>(v.substr(p+1));
-        }
-        else {
-            addr = v;
-        }
-    }
-    catch (boost::bad_lexical_cast const& /*e*/) {
-        std::cerr << "fibonacci2: illegal port number given: " << v.substr(p+1) << std::endl;
-        std::cerr << "           using default value instead: " << port << std::endl;
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// helper class for AGAS server initialization
-class agas_server_helper
-{
-public:
-    agas_server_helper(std::string host, boost::uint16_t port)
-      : agas_pool_(), agas_(agas_pool_, host, port)
-    {
-        agas_.run(false);
-    }
-    ~agas_server_helper()
-    {
-        agas_.stop();
-    }
-
-private:
-    hpx::util::io_service_pool agas_pool_; 
-    hpx::naming::resolver_server agas_;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-// this is the runtime type we use in this application
-typedef hpx::threads::policies::global_queue_scheduler global_queue_policy;
-typedef hpx::threads::policies::local_queue_scheduler local_queue_policy;
-
-///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-    try {
-        // analyze the command line
-        po::variables_map vm;
-        if (!parse_commandline(argc, argv, vm))
-            return -1;
+  // Configure application-specific options
+  po::options_description 
+      desc_commandline("Usage: fibonacci2 [hpx_options] [options]");
+  desc_commandline.add_options()
+      ("value,v", po::value<int>(), 
+       "the number to be used as the argument to fib (default is 10)")
+      ("csv,s", "generate statistics of the run in comma separated format")
+      ("busywait,b", po::value<int>(),
+       "add this amount of busy wait workload to each of the iterations"
+       " [in steps of 1µs], i.e. -b1000 == 1ms")
+      ;
 
-        // Check command line arguments.
-        std::string hpx_host("localhost"), agas_host;
-        boost::uint16_t hpx_port = HPX_PORT, agas_port = 0;
-        int num_threads = 1;
-        std::string queueing = "global";
-        int num_localities = 1;
-        hpx::runtime::mode mode = hpx::runtime::console;    // default is console mode
-
-        // extract IP address/port arguments
-        if (vm.count("agas")) 
-            split_ip_address(vm["agas"].as<std::string>(), agas_host, agas_port);
-
-        if (vm.count("hpx")) 
-            split_ip_address(vm["hpx"].as<std::string>(), hpx_host, hpx_port);
-
-        if (vm.count("localities"))
-            num_localities = vm["localities"].as<int>();
-
-        if (vm.count("threads"))
-            num_threads = vm["threads"].as<int>();
-
-        if (vm.count("queueing"))
-            queueing = vm["queueing"].as<std::string>();
-
-        if (vm.count("worker")) {
-            mode = hpx::runtime::worker;
-            if (vm.count("config")) {
-                std::cerr << "fibonacci2: --config option ignored, used for console "
-                             "instance only\n";
-            }
-        }
-
-        // initialize and run the AGAS service, if appropriate
-        std::auto_ptr<agas_server_helper> agas_server;
-        if (vm.count("run_agas_server"))  // run the AGAS server instance here
-            agas_server.reset(new agas_server_helper(agas_host, agas_port));
-
-        // initialize and start the HPX runtime
-        if (queueing == "global")
-        {
-            typedef hpx::runtime_impl<global_queue_policy> runtime_type;
-
-            // Build and configure this runtime instance
-            runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode);
-            if (mode != hpx::runtime::worker && vm.count("config"))
-            {
-                std::string config(vm["config"].as<std::string>());
-                rt.get_config().load_application_configuration(config.c_str());
-            }
-               
-            // Run this runtime instance
-            if (mode != hpx::runtime::worker) {
-                rt.run(boost::bind(hpx_main, vm), num_threads, num_localities);
-            }
-            else
-            {
-                rt.run(num_threads, num_localities);
-            }
-        }
-        else if (queueing == "local")
-        {
-            typedef hpx::runtime_impl<local_queue_policy> runtime_type;
-            local_queue_policy::init_parameter_type init(num_threads, 1000);
-
-            // Build and configure this runtime instance
-            runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode, init);
-            if (mode != hpx::runtime::worker && vm.count("config"))
-            {
-                std::string config(vm["config"].as<std::string>());
-                rt.get_config().load_application_configuration(config.c_str());
-            }
-               
-            // Run this runtime instance
-            if (mode != hpx::runtime::worker) {
-                rt.run(boost::bind(hpx_main, vm), num_threads, num_localities);
-            }
-            else
-            {
-                rt.run(num_threads, num_localities);
-            }
-        }
-        else
-            BOOST_ASSERT(false);
-    }
-    catch (std::exception& e) {
-        std::cerr << "fibonacci2: std::exception caught: " << e.what() << "\n";
-        return -1;
-    }
-    catch (...) {
-        std::cerr << "fibonacci2: unexpected exception caught\n";
-        return -2;
-    }
-    return 0;
+  // Initialize and run HPX
+  int retcode = hpx_init(desc_commandline, argc, argv); 
+  return retcode;
 }
 
---
+++ runtime/hpx_init.hpp
@@ -0,0 +1,217 @@
+//  Copyright (c) 2010-2011 Phillip LeBlanc, Dylan Stark
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(RUNTIME_HPX_INIT_JUL_1_2010_1118AM)
+#define RUNTIME_HPX_INIT_JUL_1_2010_1118AM
+
+#include <iostream>
+
+#include <hpx/hpx.hpp>
+
+#include <boost/program_options.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+using namespace hpx;
+namespace po = boost::program_options;
+
+int hpx_main(po::variables_map &vm);
+
+bool parse_commandline(po::options_description& app_options, int argc, char *argv[], po::variables_map& vm)
+{
+    try {
+        po::options_description hpx_options("HPX Options");
+        hpx_options.add_options()
+            ("help,h", "print out program usage (this message)")
+            ("run_agas_server,r", "run AGAS server as part of this runtime instance")
+            ("worker,w", "run this instance in worker (non-console) mode")
+            ("config", po::value<std::string>(), 
+                "load the specified file as a configuration file")
+            ("agas,a", po::value<std::string>(), 
+                "the IP address the AGAS server is running on (default taken "
+                "from hpx.ini), expected format: 192.168.1.1:7912")
+            ("hpx,x", po::value<std::string>(), 
+                "the IP address the HPX parcelport is listening on (default "
+                "is localhost:7910), expected format: 192.168.1.1:7913")
+            ("localities,l", po::value<int>(), 
+                "the number of localities to wait for at application startup"
+                "(default is 1)")
+            ("threads,t", po::value<int>(), 
+                "the number of operating system threads to spawn for this"
+                "HPX locality")
+            ("queueing,q", po::value<std::string>(),
+                "the queue scheduling policy to use, options are 'global' "
+                " and 'local' (default is 'global')")
+        ;
+
+        po::options_description desc_cmdline;
+        desc_cmdline.add(app_options).add(hpx_options);
+
+        po::store(po::command_line_parser(argc, argv)
+            .options(desc_cmdline).run(), vm);
+        po::notify(vm);
+
+        // print help screen
+        if (vm.count("help")) {
+            std::cout << desc_cmdline;
+            return false;
+        }
+    }
+    catch (std::exception const& e) {
+        std::cerr << "hpx_init: exception caught: " << e.what() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+inline void 
+split_ip_address(std::string const& v, std::string& addr, boost::uint16_t& port)
+{
+    std::string::size_type p = v.find_first_of(":");
+    try {
+        if (p != std::string::npos) {
+            addr = v.substr(0, p);
+            port = boost::lexical_cast<boost::uint16_t>(v.substr(p+1));
+        }
+        else {
+            addr = v;
+        }
+    }
+    catch (boost::bad_lexical_cast const& /*e*/) {
+        std::cerr << "hpx_init: illegal port number given: " << v.substr(p+1) << std::endl;
+        std::cerr << "           using default value instead: " << port << std::endl;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// helper class for AGAS server initialization
+class agas_server_helper
+{
+public:
+    agas_server_helper(std::string host, boost::uint16_t port)
+      : agas_pool_(), agas_(agas_pool_, host, port)
+    {
+        agas_.run(false);
+    }
+    ~agas_server_helper()
+    {
+        agas_.stop();
+    }
+
+private:
+    hpx::util::io_service_pool agas_pool_; 
+    hpx::naming::resolver_server agas_;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// this is the runtime type we use in this application
+typedef hpx::threads::policies::global_queue_scheduler global_queue_policy;
+typedef hpx::threads::policies::local_queue_scheduler local_queue_policy;
+
+///////////////////////////////////////////////////////////////////////////////
+int hpx_init(po::options_description& desc_cmdline, int argc, char* argv[])
+{
+  try
+  {
+    // analyze the command line
+    po::variables_map vm;
+    if (!parse_commandline(desc_cmdline, argc, argv, vm))
+      return -1;
+
+    // Check command line arguments.
+    std::string hpx_host("localhost"), agas_host;
+    boost::uint16_t hpx_port = HPX_PORT, agas_port = 0;
+    int num_threads = 1;
+    std::string queueing = "global";
+    int num_localities = 1;
+    hpx::runtime::mode mode = hpx::runtime::console; // default is console mode
+
+    // extract IP address/port arguments
+    if (vm.count("agas")) 
+      split_ip_address(vm["agas"].as<std::string>(), agas_host, agas_port);
+
+    if (vm.count("hpx")) 
+      split_ip_address(vm["hpx"].as<std::string>(), hpx_host, hpx_port);
+
+    if (vm.count("localities"))
+      num_localities = vm["localities"].as<int>();
+
+    if (vm.count("threads"))
+      num_threads = vm["threads"].as<int>();
+
+    if (vm.count("queueing"))
+      queueing = vm["queueing"].as<std::string>();
+
+    if (vm.count("worker")) {
+      mode = hpx::runtime::worker;
+      if (vm.count("config")) {
+        std::cerr << "hpx_init: --config option ignored, used for console "
+          "instance only\n";
+      }
+    }
+
+    // initialize and run the AGAS service, if appropriate
+    std::auto_ptr<agas_server_helper> agas_server;
+    if (vm.count("run_agas_server"))  // run the AGAS server instance here
+      agas_server.reset(new agas_server_helper(agas_host, agas_port));
+
+    // initialize and start the HPX runtime
+    if (queueing == "global")
+    {
+      typedef hpx::runtime_impl<global_queue_policy> runtime_type;
+
+      // Build and configure this runtime instance
+      runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode);
+      if (mode != hpx::runtime::worker && vm.count("config"))
+      {
+        std::string config(vm["config"].as<std::string>());
+        rt.get_config().load_application_configuration(config.c_str());
+      }
+
+      // Run this runtime instance
+      if (mode != hpx::runtime::worker) {
+        rt.run(boost::bind(hpx_main, vm), num_threads, num_localities);
+      }
+      else
+      {
+        rt.run(num_threads, num_localities);
+      }
+    }
+    else if (queueing == "local")
+    {
+      typedef hpx::runtime_impl<local_queue_policy> runtime_type;
+      local_queue_policy::init_parameter_type init(num_threads, 1000);
+
+      // Build and configure this runtime instance
+      runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode, init);
+      if (mode != hpx::runtime::worker && vm.count("config"))
+      {
+        std::string config(vm["config"].as<std::string>());
+        rt.get_config().load_application_configuration(config.c_str());
+      }
+
+      // Run this runtime instance
+      if (mode != hpx::runtime::worker) {
+        rt.run(boost::bind(hpx_main, vm), num_threads, num_localities);
+      }
+      else
+      {
+        rt.run(num_threads, num_localities);
+      }
+    }
+    else
+      BOOST_ASSERT(false);
+  }
+  catch (std::exception& e) {
+    std::cerr << "hpx_init: std::exception caught: " << e.what() << "\n";
+    return -1;
+  }
+  catch (...) {
+    std::cerr << "hpx_init: unexpected exception caught\n";
+    return -2;
+  }
+}
+
+#endif
