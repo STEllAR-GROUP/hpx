c1b5d507f80c33168221286021ea8e3a1c4cda3e
Matthew Anderson <manderson@cct.lsu.edu>
2010-07-02 18:52:13 +0000

    Adding quad precision debug tool
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@2533 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/had_amr/visualization/debugtool/debug.cpp
+++ examples/had_amr/visualization/debugtool/debug.cpp
@@ -6,9 +6,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <sstream>
+#include <string>
+#include "mpreal.h"
 
-int floatcmp(double a,double b) {
-  double epsilon = 1.e-3;
+
+
+int floatcmp(mpfr::mpreal a,mpfr::mpreal b) {
+  mpfr::mpreal epsilon = 1.e-17;
   if ( a < b + epsilon && a > b - epsilon ) return 1;
   else return 0;
 }
@@ -19,9 +24,12 @@ int main(int argc, char *argv[]) {
   int size;
   int shape[3];
   if ( argc < 3 ) {
-    printf(" Usage: debug <logcode1.dat> <logcode2.dat>\n");
+    printf(" Usage: debug <unigrid.dat> <amr.dat>\n");
     exit(0);
   }
+
+  mpfr::mpreal::set_default_prec(128);
+
   char basename[80];
   char basename2[80];
   char cnames[80];
@@ -30,7 +38,7 @@ int main(int argc, char *argv[]) {
   sprintf(basename2,argv[2]);
 
   FILE *fdata;
-  char j1[64], j2[64], j3[64], j4[64];
+  char j1[164], j2[164], j3[164], j4[164];
 
   /* Ensure that the file exists */
   fdata = fopen(basename,"r");
@@ -59,17 +67,17 @@ int main(int argc, char *argv[]) {
   //malloc some memory
   int *level;
   int *level2;
-  double *timesteps,*x,*value;
-  double *timesteps2,*x2,*value2;
+  mpfr::mpreal *timesteps,*x,*value;
+  mpfr::mpreal *timesteps2,*x2,*value2;
   level = (int *) malloc(sizeof(int)*size);
-  timesteps = (double *) malloc(sizeof(double)*size);
-  x = (double *) malloc(sizeof(double)*size);
-  value = (double *) malloc(sizeof(double)*size);
+  timesteps = (mpfr::mpreal *) malloc(sizeof(mpfr::mpreal)*size);
+  x = (mpfr::mpreal *) malloc(sizeof(mpfr::mpreal)*size);
+  value = (mpfr::mpreal *) malloc(sizeof(mpfr::mpreal)*size);
 
   level2 = (int *) malloc(sizeof(int)*size2);
-  timesteps2 = (double *) malloc(sizeof(double)*size2);
-  x2 = (double *) malloc(sizeof(double)*size2);
-  value2 = (double *) malloc(sizeof(double)*size2);
+  timesteps2 = (mpfr::mpreal *) malloc(sizeof(mpfr::mpreal)*size2);
+  x2 = (mpfr::mpreal *) malloc(sizeof(mpfr::mpreal)*size2);
+  value2 = (mpfr::mpreal *) malloc(sizeof(mpfr::mpreal)*size2);
 
   fdata = fopen(basename,"r");
   i = 0;
@@ -79,9 +87,12 @@ int main(int argc, char *argv[]) {
       level[i] = atoi(j1);
       if (level[i] > maxlevel) maxlevel = level[i];
 
-      timesteps[i] = atof(j2);
-      x[i] = atof(j3);
-      value[i] = atof(j4);
+      std::string s = str(j2);
+      timesteps[i] = s.c_str();
+      std::string s = str(j3);
+      x[i] = s.c_str();
+      std::string s = str(j4);
+      value[i] = s.c_str();
       i++;
     }
   } else {
@@ -98,9 +109,12 @@ int main(int argc, char *argv[]) {
       level2[j] = atoi(j1);
       if (level2[j] > maxlevel2) maxlevel2 = level2[j];
 
-      timesteps2[j] = atof(j2);
-      x2[j] = atof(j3);
-      value2[j] = atof(j4);
+      std::string s = str(j2);
+      timesteps2[j] = s.c_str();
+      std::string s = str(j3);
+      x2[j] = s.c_str();
+      std::string s = str(j4);
+      value2[j] = s.c_str();
       j++;
     }
   } else {
@@ -112,7 +126,7 @@ int main(int argc, char *argv[]) {
   // Compare what's in list 1 with list 2 -- find any coincident entries; compare on x
   int count = 0;
   int k,l;
-  double tmpx,tmpx2;
+  mpfr::mpreal tmpx,tmpx2;
   for (k=0;k<i;k++) {
     tmpx = x[k];
     for (l=0;l<j;l++) {
@@ -120,14 +134,14 @@ int main(int argc, char *argv[]) {
       if ( floatcmp(tmpx,tmpx2) == 1 ) {
         // compare the timestep
         if ( floatcmp(timesteps[k],timesteps2[l]) == 1 ) {
-          // found a coincident
-          printf(" x: %g timestep: %g x2: %g timestep: %g\n",x[k],timesteps[k],x2[l],timesteps2[l]);
-          count++;
+          // compare the value
+          if ( floatcmp(value[k],value2[l]) != 1 ) {
+            std::cout << " x " << x[k] << " time " << timesteps[k] << " unigrid " << value[k] << " amr " << value2[l] << std::endl;
+          }
         }
       }
     }
   }
-  printf("\n\n Coincidences: %d\n",count);
 
   return 0;
 }
