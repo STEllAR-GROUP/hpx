093bcf90cf52deba5affb47b523c8cabb9d05309
Dylan Stark <dstark@cct.lsu.edu>
2010-07-23 22:07:41 +0000

    Updates original Fibonacci example to use hpx_init().
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@2596 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/fibonacci.cpp
+++ examples/quickstart/fibonacci.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 
 #include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+
 #include <hpx/runtime/actions/plain_action.hpp>
 #include <hpx/runtime/components/plain_component_factory.hpp>
 
@@ -82,8 +84,19 @@ int fib (naming::gid_type that_prefix, int n, int delay_coeff)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-int hpx_main(int argument, int delay_coeff, int& result, double& elapsed)
+int hpx_main(po::variables_map &vm)
 {
+    int argument = 10;
+    int delay_coeff = 0;
+    int result = 0;
+    double elapsed = 0.0;
+
+    // Process application-specific command-line options
+    if (vm.count("value"))
+        argument = vm["value"].as<int>();
+    if (vm.count("busywait"))
+        delay_coeff = vm["busywait"].as<int>();
+
     // try to get arguments from application configuration
     runtime& rt = get_runtime();
     argument = boost::lexical_cast<int>(
@@ -115,6 +128,17 @@ int hpx_main(int argument, int delay_coeff, int& result, double& elapsed)
         elapsed = t.elapsed();
     }
 
+    if (vm.count("csv"))
+    {
+      // write results as csv
+      std::cout << argument << "," 
+        << elapsed << "," << result << "," << std::endl;
+    }
+    else {
+      // write results the old fashioned way
+      std::cout << "elapsed: " << elapsed << ", result: " << result << std::endl;
+    }
+
     // initiate shutdown of the runtime systems on all localities
     components::stubs::runtime_support::shutdown_all();
 
@@ -122,241 +146,21 @@ int hpx_main(int argument, int delay_coeff, int& result, double& elapsed)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-bool parse_commandline(int argc, char *argv[], po::variables_map& vm)
-{
-    try {
-        po::options_description desc_cmdline ("Usage: fibonacci [options]");
-        desc_cmdline.add_options()
-            ("help,h", "print out program usage (this message)")
-            ("run_agas_server,r", "run AGAS server as part of this runtime instance")
-            ("worker,w", "run this instance in worker (non-console) mode")
-            ("config", po::value<std::string>(), 
-                "load the specified file as an application configuration file")
-            ("agas,a", po::value<std::string>(), 
-                "the IP address the AGAS server is running on (default taken "
-                "from hpx.ini), expected format: 192.168.1.1:7912")
-            ("hpx,x", po::value<std::string>(), 
-                "the IP address the HPX parcelport is listening on (default "
-                "is localhost:7910), expected format: 192.168.1.1:7913")
-            ("localities,l", po::value<int>(), 
-                "the number of localities to wait for at application startup"
-                "(default is 1)")
-            ("threads,t", po::value<int>(), 
-                "the number of operating system threads to spawn for this"
-                "HPX locality")
-            ("queueing,q", po::value<std::string>(),
-                "the queue scheduling policy to use, options are 'global' "
-                " and 'local' (default is 'global')")
-            ("value,v", po::value<int>(), 
-                "the number to be used as the argument to fib (default is 10)")
-            ("csv,s", "generate statistics of the run in comma separated format")
-            ("busywait,b", po::value<int>(),
-                "add this amount of busy wait workload to each of the iterations"
-                " [in steps of 1µs], i.e. -b1000 == 1ms")
-        ;
-
-        po::store(po::command_line_parser(argc, argv)
-            .options(desc_cmdline).run(), vm);
-        po::notify(vm);
-
-        // print help screen
-        if (vm.count("help")) {
-            std::cout << desc_cmdline;
-            return false;
-        }
-    }
-    catch (std::exception const& e) {
-        std::cerr << "fibonacci: exception caught: " << e.what() << std::endl;
-        return false;
-    }
-    return true;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-inline void 
-split_ip_address(std::string const& v, std::string& addr, boost::uint16_t& port)
-{
-    std::string::size_type p = v.find_first_of(":");
-    try {
-        if (p != std::string::npos) {
-            addr = v.substr(0, p);
-            port = boost::lexical_cast<boost::uint16_t>(v.substr(p+1));
-        }
-        else {
-            addr = v;
-        }
-    }
-    catch (boost::bad_lexical_cast const& /*e*/) {
-        std::cerr << "fibonacci: illegal port number given: " << v.substr(p+1) << std::endl;
-        std::cerr << "           using default value instead: " << port << std::endl;
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// helper class for AGAS server initialization
-class agas_server_helper
-{
-public:
-    agas_server_helper(std::string host, boost::uint16_t port)
-      : agas_pool_(), agas_(agas_pool_, host, port)
-    {
-        agas_.run(false);
-    }
-    ~agas_server_helper()
-    {
-        agas_.stop();
-    }
-
-private:
-    hpx::util::io_service_pool agas_pool_; 
-    hpx::naming::resolver_server agas_;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-// this is the runtime type we use in this application
-typedef hpx::threads::policies::global_queue_scheduler global_queue_policy;
-typedef hpx::threads::policies::local_queue_scheduler local_queue_policy;
-
-///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-    try {
-        // analyze the command line
-        po::variables_map vm;
-        if (!parse_commandline(argc, argv, vm))
-            return -1;
+    // Configure application-specific options
+    po::options_description 
+        desc_commandline ("usage: fibonacci [options]");
+    desc_commandline.add_options()
+        ("value,v", po::value<int>(), 
+         "the number to be used as the argument to fib (default is 10)")
+        ("csv,s", "generate statistics of the run in comma separated format")
+        ("busywait,b", po::value<int>(),
+         "add this amount of busy wait workload to each of the iterations"
+         " [in steps of 1µs], i.e. -b1000 == 1ms")
+        ;
 
-        // Check command line arguments.
-        std::string hpx_host("localhost"), agas_host;
-        boost::uint16_t hpx_port = HPX_PORT, agas_port = 0;
-        int num_threads = 1;
-        std::string queueing = "global";
-        int num_localities = 1;
-        int argument = 10;
-        hpx::runtime::mode mode = hpx::runtime::console;    // default is console mode
-        int delay_coeff = 0;
-
-        // extract IP address/port arguments
-        if (vm.count("agas")) 
-            split_ip_address(vm["agas"].as<std::string>(), agas_host, agas_port);
-
-        if (vm.count("hpx")) 
-            split_ip_address(vm["hpx"].as<std::string>(), hpx_host, hpx_port);
-
-        if (vm.count("localities"))
-            num_localities = vm["localities"].as<int>();
-
-        if (vm.count("threads"))
-            num_threads = vm["threads"].as<int>();
-
-        if (vm.count("queueing"))
-            queueing = vm["queueing"].as<std::string>();
-
-        if (vm.count("value"))
-            argument = vm["value"].as<int>();
-
-        if (vm.count("worker")) {
-            mode = hpx::runtime::worker;
-            if (vm.count("config")) {
-                std::cerr << "fibonacci: --config option ignored, used for console "
-                             "instance only\n";
-            }
-        }
-
-        if (vm.count("busywait"))
-            delay_coeff = vm["busywait"].as<int>();
-
-        // initialize and run the AGAS service, if appropriate
-        std::auto_ptr<agas_server_helper> agas_server;
-        if (vm.count("run_agas_server"))  // run the AGAS server instance here
-            agas_server.reset(new agas_server_helper(agas_host, agas_port));
-
-        int result = 0;
-        double elapsed =0;
-
-        // initialize and start the HPX runtime
-        if (queueing == "global")
-        {
-            typedef hpx::runtime_impl<global_queue_policy> runtime_type;
-
-            runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode);
-            if (mode == hpx::runtime::worker) {
-                rt.run(num_threads, num_localities);
-            }
-            else {
-                // if we've got a configuration file (as console) we read it in,
-                // otherwise this information will be automatically pulled from 
-                // the console
-                if (vm.count("config")) {
-                    std::string config(vm["config"].as<std::string>());
-                    rt.get_config().load_application_configuration(config.c_str());
-                }
-    
-                rt.run(boost::bind(hpx_main, argument, delay_coeff, 
-                    boost::ref(result), boost::ref(elapsed)), num_threads, 
-                    num_localities);
-    
-                if (vm.count("csv")) {
-                    // write results as csv
-                    std::cout << num_threads << "," << argument << "," 
-                              << elapsed << "," << result << "," << count_invocations 
-                               << "," << elapsed/count_invocations << std::endl;
-                }
-                else {
-                    // write results the old fashioned way
-                    std::cout << "elapsed: " << elapsed << ", result: " << result << std::endl;
-                    std::cout << "Number of invocations of fib(): " << count_invocations 
-                              << std::endl;
-                }
-            }
-        }
-        else if (queueing == "local")
-        {
-            typedef hpx::runtime_impl<local_queue_policy> runtime_type;
-            local_queue_policy::init_parameter_type init(num_threads, 1000);
-
-            runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode, init);
-            if (mode == hpx::runtime::worker) {
-                rt.run(num_threads, num_localities);
-            }
-            else {
-                // if we've got a configuration file (as console) we read it in,
-                // otherwise this information will be automatically pulled from 
-                // the console
-                if (vm.count("config")) {
-                    std::string config(vm["config"].as<std::string>());
-                    rt.get_config().load_application_configuration(config.c_str());
-                }
-    
-                rt.run(boost::bind(hpx_main, argument, delay_coeff, 
-                    boost::ref(result), boost::ref(elapsed)), num_threads, 
-                    num_localities);
-    
-                if (vm.count("csv")) {
-                    // write results as csv
-                    std::cout << num_threads << "," << argument << "," 
-                              << elapsed << "," << result << "," << count_invocations 
-                               << "," << elapsed/count_invocations << std::endl;
-                }
-                else {
-                    // write results the old fashioned way
-                    std::cout << "elapsed: " << elapsed << ", result: " << result << std::endl;
-                    std::cout << "Number of invocations of fib(): " << count_invocations 
-                              << std::endl;
-                }
-            }
-        }
-        else
-            BOOST_ASSERT(false);
-    }
-    catch (std::exception& e) {
-        std::cerr << "fibonacci: std::exception caught: " << e.what() << "\n";
-        return -1;
-    }
-    catch (...) {
-        std::cerr << "fibonacci: unexpected exception caught\n";
-        return -2;
-    }
-    return 0;
+    // Initialize and run HPX
+    int retcode = hpx_init(desc_commandline, argc, argv);
+    return retcode;
 }
-
