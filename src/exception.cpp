f9d7c754a174824be727b3b91464b825e80a8337
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2010-08-09 18:31:44 +0000

    Adding missing file
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@2675 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ src/exception.cpp
@@ -0,0 +1,84 @@
+//  Copyright (c) 2007-2010 Hartmut Kaiser
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/exception.hpp>
+#include <hpx/runtime/threads/thread_helpers.hpp>
+#include <stdexcept>
+
+namespace hpx { namespace detail
+{
+    template <typename Exception>
+    HPX_EXPORT void throw_exception(Exception const& e, char const* func, 
+        std::string const& file, int line)
+    {
+        threads::thread_self* self = threads::get_self_ptr();
+        if (NULL != self) {
+            threads::thread_id_type id = self->get_thread_id();
+            throw boost::enable_current_exception(
+                boost::enable_error_info(e) 
+                    << boost::throw_function(func) 
+                    << throw_thread_name(threads::get_thread_description(id))
+                    << throw_file(file) << throw_line(line));
+        }
+        else {
+            throw boost::enable_current_exception(
+                boost::enable_error_info(e) 
+                    << boost::throw_function(func) 
+                    << throw_file(file) << throw_line(line));
+        }
+    }
+
+    template HPX_EXPORT void throw_exception(hpx::exception const&, 
+        char const*, std::string const&, int);
+
+    ///////////////////////////////////////////////////////////////////////////
+    template <typename Exception>
+    HPX_EXPORT void throw_exception(Exception const& e, std::string const& func, 
+        std::string const& file, int line)
+    {
+        threads::thread_self* self = threads::get_self_ptr();
+        if (NULL != self) {
+            threads::thread_id_type id = self->get_thread_id();
+            throw boost::enable_current_exception(
+                boost::enable_error_info(e) 
+                    << throw_function(func) 
+                    << throw_thread_name(threads::get_thread_description(id))
+                    << throw_file(file) << throw_line(line));
+        }
+        else {
+            throw boost::enable_current_exception(
+                boost::enable_error_info(e) 
+                    << throw_function(func) 
+                    << throw_file(file) << throw_line(line));
+        }
+    }
+
+    template HPX_EXPORT void throw_exception<hpx::exception>(hpx::exception const&, 
+        std::string const&, std::string const&, int);
+
+    template HPX_EXPORT void throw_exception(boost::system::system_error const&, 
+        std::string const&, std::string const&, int);
+
+    template HPX_EXPORT void throw_exception(std::exception const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::bad_exception const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::bad_typeid const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::bad_cast const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::bad_alloc const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::logic_error const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::runtime_error const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::out_of_range const&, 
+        std::string const&, std::string const&, int);
+    template HPX_EXPORT void throw_exception(std::invalid_argument const&, 
+        std::string const&, std::string const&, int);
+}}
+
