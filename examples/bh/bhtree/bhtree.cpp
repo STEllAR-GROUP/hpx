05f23593ff89bd3379483b562a7940946676dec9
Chirag Dekate <cdekate@cct.lsu.edu>
2011-01-20 20:42:01 +0000

    new name ... treenode component now builds with the right name
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3040 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/bh/bhtree/bhtree.cpp
@@ -0,0 +1,26 @@
+#include <hpx/hpx.hpp>
+#include <hpx/runtime/components/component_factory.hpp>
+
+#include <hpx/util/portable_binary_iarchive.hpp>
+#include <hpx/util/portable_binary_oarchive.hpp>
+
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/export.hpp>
+
+#include "server/bhtree.hpp"
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Add factory registration functionality
+HPX_REGISTER_COMPONENT_MODULE();
+HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(
+    hpx::components::managed_component<hpx::components::server::IntrTreeNode>, 
+    IntrTreeNode);
+
+HPX_REGISTER_ACTION_EX(
+    hpx::components::server::IntrTreeNode::newNode_action,
+    IntrTreeNode_newNode_action);
+HPX_DEFINE_GET_COMPONENT_TYPE(hpx::components::server::IntrTreeNode);
+
+
+
