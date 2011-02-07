STEllAR-GROUP/hpx

commit 896aa0bdc296302d3cd10698fc967198ea5c61a0

Completing migration of get_component_type to new scheme. This has some minor implications for user components, I'll describe everything in a mail separately.
git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3119 5079c8e4-9419-0410-93bb-9e5ee278c886

hpx/runtime/components/component_type.hpp
@@ -84,41 +84,28 @@ namespace hpx { namespace components
 
     ///////////////////////////////////////////////////////////////////////////
     template <typename Component, typename Enable = void>
-    struct extract_component_type_from_component
+    struct component_type_database
     {
-        typedef component_type type;
+        static component_type value;
 
-        static type call()
-        {
-            return Component::get_component_type();
-        }
+        static HPX_ALWAYS_EXPORT component_type get();
+        static HPX_ALWAYS_EXPORT void set(component_type type);
     }; 
-
-    template <typename Component, typename Enable = void>
-    struct assign_component_type_to_component
-    {
-        typedef void type;
-
-        static void call(component_type type)
-        {
-            return Component::set_component_type(type);
-        }
-    }; 
- 
+    
+    template <typename Component, typename Enable>
+    component_type component_type_database<Component, Enable>::value = component_invalid;
+    
+    ///////////////////////////////////////////////////////////////////////////
     template <typename Component>
-    HPX_ALWAYS_EXPORT typename
-    extract_component_type_from_component<Component>::type
-    get_component_type()
+    inline component_type get_component_type()
     {
-        return extract_component_type_from_component<Component>::call();
+        return component_type_database<Component>::get();
     }
 
     template <typename Component>
-    HPX_ALWAYS_EXPORT typename
-    assign_component_type_to_component<Component>::type
-    set_component_type(component_type type)
+    inline void set_component_type(component_type type)
     {
-        return assign_component_type_to_component<Component>::call(type);
+        return component_type_database<Component>::set(type);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -136,8 +123,29 @@ namespace hpx { namespace components
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
-// This macro is deprecated now.
-#define HPX_DEFINE_GET_COMPONENT_TYPE(component)
+#define HPX_DEFINE_GET_COMPONENT_TYPE(component)                              \
+    namespace hpx { namespace components                                      \
+    {                                                                         \
+        template <> HPX_ALWAYS_EXPORT                                         \
+        component_type component_type_database<component>::get()              \
+            { return value; }                                                 \
+        template <> HPX_ALWAYS_EXPORT                                         \
+        void component_type_database<component>::set(component_type t)        \
+            { value = t; }                                                    \
+    }}
+    /**/
+
+#define HPX_DEFINE_GET_COMPONENT_TYPE_STATIC(component, type)                 \
+    namespace hpx { namespace components                                      \
+    {                                                                         \
+        template <> HPX_ALWAYS_EXPORT                                         \
+        component_type component_type_database<component>::get()              \
+            { return type; }                                                  \
+        template <> HPX_ALWAYS_EXPORT                                         \
+        void component_type_database<component>::set(component_type)          \
+            { BOOST_ASSERT(false); }                                          \
+    }}
+    /**/
 
 #endif
 

hpx/runtime/components/server/memory_block.hpp
@@ -98,18 +98,6 @@ namespace hpx { namespace components { namespace server { namespace detail
         /// memory block
         bool is_master() const { return 0 != wrapper_; }
 
-        // This is the component id. Every component needs to have an embedded
-        // enumerator 'value' which is used by the generic action implementation
-        // to associate this component with a given action.
-        static component_type get_component_type()
-        {
-            return component_memory_block;
-        }
-        static void set_component_type(component_type t)
-        {
-            BOOST_ASSERT(false);    // this shouldn't be ever called
-        }
-
         template <typename ManagedType>
         naming::id_type const& get_gid(ManagedType* p) const
         {
@@ -479,18 +467,6 @@ namespace hpx { namespace components { namespace server
             return get();
         }
 
-        // This is the component id. Every component needs to have an embedded
-        // enumerator 'value' which is used by the generic action implementation
-        // to associate this component with a given action.
-        static component_type get_component_type()
-        {
-            return wrapped_type::get_component_type();
-        }
-        static void set_component_type(component_type t)
-        {
-            wrapped_type::set_component_type(t);
-        }
-
     protected:
         // the memory for the wrappers is managed by a one_size_heap_list
         typedef components::detail::wrapper_heap_list<
@@ -503,7 +479,7 @@ namespace hpx { namespace components { namespace server
         {
             // ensure thread-safe initialization
             util::static_<heap_type, wrapper_heap_tag, HPX_RUNTIME_INSTANCE_LIMIT> 
-                heap(get_component_type());
+                heap(component_memory_block);
             return heap.get(get_runtime_instance_number());
         }
 

build/VS2010/had_amr.sln
@@ -32,8 +32,6 @@ Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "cache", "..\..\external\cac
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "hpx", "hpx\hpx.vcxproj", "{E6C6C6C4-6959-4B47-B92A-6323A906CC40}"
 EndProject
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "fibonacci2", "quickstart\fibonacci\fibonacci2.vcxproj", "{86037EB9-D590-4E13-AC7B-ED9384A0F172}"
-EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "fibonacci", "quickstart\fibonacci\fibonacci.vcxproj", "{48181BEF-AE01-4332-AE51-BB7417A95665}"
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "plugin", "plugin\plugin.vcxproj", "{E5531ED3-B452-45A6-8124-E91CA19DDD7B}"
@@ -152,16 +150,6 @@ Global
 		{E6C6C6C4-6959-4B47-B92A-6323A906CC40}.Release|Win32.ActiveCfg = Release|x64
 		{E6C6C6C4-6959-4B47-B92A-6323A906CC40}.Release|x64.ActiveCfg = Release|x64
 		{E6C6C6C4-6959-4B47-B92A-6323A906CC40}.Release|x64.Build.0 = Release|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Debug|Any CPU.ActiveCfg = Debug|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Debug|Mixed Platforms.ActiveCfg = Debug|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Debug|Mixed Platforms.Build.0 = Debug|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Debug|Win32.ActiveCfg = Debug|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Debug|x64.ActiveCfg = Debug|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Release|Any CPU.ActiveCfg = Release|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Release|Mixed Platforms.ActiveCfg = Release|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Release|Mixed Platforms.Build.0 = Release|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Release|Win32.ActiveCfg = Release|x64
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172}.Release|x64.ActiveCfg = Release|x64
 		{48181BEF-AE01-4332-AE51-BB7417A95665}.Debug|Any CPU.ActiveCfg = Debug|x64
 		{48181BEF-AE01-4332-AE51-BB7417A95665}.Debug|Mixed Platforms.ActiveCfg = Debug|x64
 		{48181BEF-AE01-4332-AE51-BB7417A95665}.Debug|Mixed Platforms.Build.0 = Debug|x64
@@ -312,7 +300,6 @@ Global
 	GlobalSection(NestedProjects) = preSolution
 		{D8ED4ED0-AF99-4227-BF45-8F8515F1FFB0} = {C24E2A49-8928-4C5E-81F6-CB0918294C0D}
 		{7A1A51F1-6629-41DB-94AD-E7420EE0F894} = {03B1522E-0ACA-4BE6-B935-A7EBD550C896}
-		{86037EB9-D590-4E13-AC7B-ED9384A0F172} = {03B1522E-0ACA-4BE6-B935-A7EBD550C896}
 		{48181BEF-AE01-4332-AE51-BB7417A95665} = {03B1522E-0ACA-4BE6-B935-A7EBD550C896}
 		{6DCE38AE-F683-4363-BF37-B7E1308135EE} = {03B1522E-0ACA-4BE6-B935-A7EBD550C896}
 		{5347BCF4-113A-4DF0-BEA0-E748A6B4F42A} = {03B1522E-0ACA-4BE6-B935-A7EBD550C896}

examples/accumulator/accumulator/accumulator.cpp
@@ -48,4 +48,6 @@ HPX_REGISTER_ACTION_EX(
 HPX_REGISTER_ACTION_EX(
     hpx::lcos::base_lco_with_value<unsigned long>::get_value_action,
     get_value_action_long);
-HPX_DEFINE_GET_COMPONENT_TYPE(hpx::lcos::base_lco_with_value<unsigned long>);
+HPX_DEFINE_GET_COMPONENT_TYPE_STATIC(
+    hpx::lcos::base_lco_with_value<unsigned long>,
+    hpx::components::component_base_lco_with_value);

examples/had_amr3d/amr_c/logging.cpp
@@ -64,7 +64,6 @@ namespace hpx { namespace components { namespace amr { namespace server
         }
 
         // output to file "output.dat"
-        FILE *fdata;
         std::vector<double> x,y,z,phi,d1phi,d2phi,d3phi,d4phi;
         double datatime;
         if ( logcode == 0 ) {
@@ -92,7 +91,7 @@ namespace hpx { namespace components { namespace amr { namespace server
               std::string Pi_str = convert(val.value_[i].phi[0][2]);
               std::string time_str = convert(val.timestep_*par->dx0*par->lambda);
 
-              fdata = fopen("chi.dat","a");
+              FILE* fdata = fopen("chi.dat","a");
               fprintf(fdata,"%d %s %s %s\n",val.level_,time_str.c_str(),x_str.c_str(),chi_str.c_str());
               fclose(fdata);
 

commit ace7dbd07d73ec6f638b74c54c1c86aceeadc94a

Fixed UNIX compiler breakage (gcc, intel, clang) caused by MSVC-specific code introduced in r3119. Note: -fvisibility=hidden is no longer compiling for gcc.
git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3122 5079c8e4-9419-0410-93bb-9e5ee278c886

hpx/runtime/actions/manage_object_action.hpp
@@ -123,7 +123,7 @@ namespace hpx { namespace actions
         void serialize(Archive& ar, const unsigned int) {}
     };
         
-    #if defined(BOOST_INTEL) 
+    #if defined(BOOST_INTEL) || defined(__clang__) 
     inline manage_object_action_base const& 
     manage_object_action_base::get_instance() const
     {

hpx/runtime/components/component_type.hpp
@@ -6,6 +6,7 @@
 #if !defined(HPX_COMPONENT_COMPONENT_TYPE_MAR_26_2008_1058AM)
 #define HPX_COMPONENT_COMPONENT_TYPE_MAR_26_2008_1058AM
 
+#include <boost/config.hpp>
 #include <boost/assert.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/cstdint.hpp>
@@ -88,8 +89,13 @@ namespace hpx { namespace components
     {
         static component_type value;
 
+    #if defined(BOOST_MSVC)
         static HPX_ALWAYS_EXPORT component_type get();
-        static HPX_ALWAYS_EXPORT void set(component_type type);
+        static HPX_ALWAYS_EXPORT void set(component_type);
+    #else
+        static component_type get() { return value; }
+        static void set(component_type t) { value = t; }
+    #endif 
     }; 
     
     template <typename Component, typename Enable>
@@ -123,7 +129,8 @@ namespace hpx { namespace components
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
-#define HPX_DEFINE_GET_COMPONENT_TYPE(component)                              \
+#if defined(BOOST_MSVC)
+  #define HPX_DEFINE_GET_COMPONENT_TYPE(component)                            \
     namespace hpx { namespace components                                      \
     {                                                                         \
         template <> HPX_ALWAYS_EXPORT                                         \
@@ -135,7 +142,7 @@ namespace hpx { namespace components
     }}
     /**/
 
-#define HPX_DEFINE_GET_COMPONENT_TYPE_STATIC(component, type)                 \
+  #define HPX_DEFINE_GET_COMPONENT_TYPE_STATIC(component, type)               \
     namespace hpx { namespace components                                      \
     {                                                                         \
         template <> HPX_ALWAYS_EXPORT                                         \
@@ -146,6 +153,10 @@ namespace hpx { namespace components
             { BOOST_ASSERT(false); }                                          \
     }}
     /**/
+#else
+  #define HPX_DEFINE_GET_COMPONENT_TYPE(component)
+  #define HPX_DEFINE_GET_COMPONENT_TYPE_STATIC(component, type)
+#endif
 
 #endif
 

hpx/runtime/components/server/memory_block.hpp
@@ -591,6 +591,33 @@ namespace hpx { namespace components { namespace server
         ///        a single pointer
         boost::intrusive_ptr<detail::memory_block_header> component_;
     };
-}}}
+}
+    template <>
+    struct HPX_ALWAYS_EXPORT component_type_database<server::detail::memory_block_header>
+    {
+        static component_type get()
+        {
+            return components::component_memory_block; 
+        }
 
+        static void set(component_type type)
+        {
+            BOOST_ASSERT(false);
+        }
+    }; 
+    
+    template <>
+    struct HPX_ALWAYS_EXPORT component_type_database<server::memory_block>
+    {
+        static component_type get()
+        {
+            return components::component_memory_block; 
+        }
+
+        static void set(component_type type)
+        {
+            BOOST_ASSERT(false);
+        }
+    }; 
+}}
 #endif

src/runtime/actions/action_manager.cpp
@@ -18,7 +18,7 @@ namespace hpx { namespace actions
     ///////////////////////////////////////////////////////////////////////////
     // This does not really belong here, but we put it into this spot for the 
     // lack of a better one :-P
-    #if !defined(BOOST_INTEL)
+    #if !defined(BOOST_INTEL) && !defined(__clang__)
     inline manage_object_action_base const& 
     manage_object_action_base::get_instance() const
     {