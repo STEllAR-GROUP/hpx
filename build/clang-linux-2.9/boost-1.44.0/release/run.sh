e3d056ad43b4ee0395b84dd62e0a54c6733a2e65
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-02-12 07:39:09 +0000

    Added crude directory structure and even cruder shell scripts that run cmake to generate GNU makefiles for intel-linux-12.0, gcc-4.1, gcc-4.2, gcc-4.3, gcc-4.4 and clang-linux-2.9.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3153 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ build/clang-linux-2.9/boost-1.44.0/release/run.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+#
+# Copyright (c) 2011 Bryce Lelbach
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying 
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+CFE_VER=2.9
+BOOST_VER=1.44.0
+
+cmake -DCMAKE_C_COMPILER="/usr/bin/clang-${CFE_VER}"                \
+      -DCMAKE_CXX_COMPILER="/usr/bin/clang++-${CFE_VER}"            \
+      -DCMAKE_CXX_FLAGS:STRING="-fvisibility=hidden -march=native"  \
+      -DCMAKE_BUILD_TYPE=Release                                    \
+      -DBOOST_DEBUG=ON                                              \
+      -DBOOST_ROOT="/opt/boost-${BOOST_VER}"                        \
+      -DBOOST_LIB_DIR="/opt/boost-${BOOST_VER}/clang-${CFE_VER}/lib"\
+      -DCMAKE_PREFIX="."                                            \
+      ../../../..
+
---
+++ build/gcc-4.3/boost-1.43.0/debug/run.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+#
+# Copyright (c) 2011 Bryce Lelbach
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying 
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+GCC_VER=4.3
+BOOST_VER=1.43.0
+
+cmake -DCMAKE_C_COMPILER="/usr/bin/gcc-${GCC_VER}"                  \
+      -DCMAKE_CXX_COMPILER="/usr/bin/g++-${GCC_VER}"                \
+      -DCMAKE_BUILD_TYPE=Debug                                      \
+      -DBOOST_DEBUG=ON                                              \
+      -DBOOST_ROOT="/opt/boost-${BOOST_VER}"                        \
+      -DBOOST_LIB_DIR="/opt/boost-${BOOST_VER}/gcc-${GCC_VER}/lib"  \
+      -DCMAKE_PREFIX="."                                            \
+      ../../../..
+
