d8fd8c04c65e5f386d2a5f3fa7a46751c2cd7bf6
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-01 07:57:47 +0000

    Added nifty little C++ std-compliant allocators wrapping nedmalloc and jemalloc, built with hpx::memory::allocator.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3338 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/memory/jemalloc_allocator.hpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(HPX_5E7FA4BF_4907_4757_A15F_97BDF229807C)
+#define HPX_5E7FA4BF_4907_4757_A15F_97BDF229807C
+
+#include <hpx/memory/allocator.hpp>
+#include <hpx/memory/jemalloc/jemalloc.h>
+
+namespace hpx { namespace memory
+{
+
+struct jemalloc
+{
+    typedef std::size_t size_type;    
+        
+    static void* malloc(size_type s)
+    {
+        return HPX_JEMALLOC_malloc(s); 
+    }
+
+    static void free(void* p)
+    {
+        HPX_JEMALLOC_free(p);
+    }
+};
+
+template <typename T>
+struct jemalloc_allocator: memory::allocator<memory::jemalloc, T> { };
+
+}}
+
+#endif // HPX_5E7FA4BF_4907_4757_A15F_97BDF229807C
+
---
+++ hpx/memory/nedmalloc_allocator.hpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(HPX_3C74DADC_8743_4902_993B_C3323BF2A863)
+#define HPX_3C74DADC_8743_4902_993B_C3323BF2A863
+
+#include <hpx/memory/allocator.hpp>
+#include <hpx/memory/nedmalloc/nedmalloc.h>
+
+namespace hpx { namespace memory
+{
+
+struct nedmalloc
+{
+    typedef std::size_t size_type;    
+        
+    static void* malloc(size_type s)
+    {
+        return nedalloc::nedmalloc(s); 
+    }
+
+    static void free(void* p)
+    {
+        nedalloc::nedfree(p);
+    }
+};
+
+template <typename T>
+struct nedmalloc_allocator: memory::allocator<memory::nedmalloc, T> { };
+
+}}
+
+#endif // HPX_3C74DADC_8743_4902_993B_C3323BF2A863
+
