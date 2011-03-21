b5a08fe4af405487a1c3e0cfa6d9d6e3cab93c54
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-21 15:08:00 +0000

    Made this more unit-testy.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3583 5079c8e4-9419-0410-93bb-9e5ee278c886

--- tests/unit/lcos/dataflow_variable.cpp
+++ tests/unit/lcos/dataflow_variable.cpp
@@ -1,48 +1,54 @@
 //  Copyright (c) 2010-2011 Dylan Stark
-// 
+//  Copyright (c)      2011 Bryce Lelbach
+//  
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <iostream>
-
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
-
+#include <hpx/util/lightweight_test.hpp>
 #include <hpx/runtime/actions/plain_action.hpp>
 #include <hpx/runtime/components/plain_component_factory.hpp>
 #include <hpx/runtime/naming/name.hpp>
 
-#include <boost/program_options.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/serialization/serialization.hpp>
-#include <boost/serialization/export.hpp>
+using boost::program_options::variables_map;
+using boost::program_options::options_description;
 
-using namespace hpx;
-namespace po = boost::program_options;
+using hpx::find_here;
+using hpx::get_runtime;
+using hpx::init;
+using hpx::finalize;
+
+using hpx::naming::id_type;
+
+using hpx::lcos::base_lco_with_value;
+using hpx::lcos::eager_future;
+using hpx::lcos::dataflow_variable;
+using hpx::lcos::future_value;
+
+using hpx::actions::plain_result_action1;
+using hpx::actions::continuation;
 
 ///////////////////////////////////////////////////////////////////////////////
-int print(naming::id_type const& d_id)
+int print(id_type const& d_id)
 {
-    typedef lcos::base_lco_with_value<int>::get_value_action get_action;
-
-    std::cout << "print> print d" << std::endl;
-    std::cout << lcos::eager_future<get_action>(d_id).get() << std::endl;
-
+    typedef base_lco_with_value<int>::get_value_action get_action;
+    HPX_TEST_EQ(eager_future<get_action>(d_id).get(), 42);
     return 0;
 }
 
-typedef actions::plain_result_action1<int, naming::id_type const&, print>
+typedef plain_result_action1<int, id_type const&, print>
     print_action;
 HPX_REGISTER_PLAIN_ACTION(print_action);
 
 ///////////////////////////////////////////////////////////////////////////////
-typedef hpx::lcos::dataflow_variable<int, int> dataflow_int_type;
+typedef dataflow_variable<int, int> dataflow_int_type;
 
 ///////////////////////////////////////////////////////////////////////////////
-int hpx_main(po::variables_map &vm)
+int hpx_main(variables_map& vm)
 {
-    naming::id_type here = hpx::find_here();
-    naming::id_type there = get_runtime().get_process().next();
+    id_type here = find_here();
+    id_type there = get_runtime().get_process().next();
 
     std::cout << ">>> print here, there" << std::endl;
     std::cout << here << " " << there << std::endl << std::endl;
@@ -52,15 +58,15 @@ int hpx_main(po::variables_map &vm)
         dataflow_int_type d1;
 
         std::cout << ">>> spawning { print d1 } here" << std::endl; 
-        lcos::future_value<int> 
-            local_spawn(lcos::eager_future<print_action>(here, d1.get_gid()));
+        future_value<int> 
+            local_spawn(eager_future<print_action>(here, d1.get_gid()));
 
         std::cout << ">>> spawning { print d1 } there" << std::endl; 
-        lcos::future_value<int> 
-            remote_spawn(lcos::eager_future<print_action>(there, d1.get_gid()));
+        future_value<int> 
+            remote_spawn(eager_future<print_action>(there, d1.get_gid()));
 
         std::cout << ">>> bind(d1, 42)" << std::endl;
-        actions::continuation(d1.get_gid()).trigger<int>(42);
+        continuation(d1.get_gid()).trigger<int>(42);
 
         // We have to use the eager_future here in order to make sure the
         // spawned action has completed before we let the dataflow_variable
@@ -70,22 +76,20 @@ int hpx_main(po::variables_map &vm)
     }
 
     // initiate shutdown of the runtime systems on all localities
-    hpx::finalize();
-
-    std::cout << "Test passed" << std::endl;
-
+    finalize();
     return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-    // Configure application-specific options
-    po::options_description
-        desc_commandline
-            ("usage: " BOOST_PP_STRINGIZE(HPX_APPLICATION_NAME) " [options]");
+    // Configure application-specific options.
+    options_description
+        desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
 
-    // Initialize and run HPX
-    return hpx::init(desc_commandline, argc, argv);
+    // Initialize and run HPX.
+    HPX_TEST_EQ_MSG(hpx::init(desc_commandline, argc, argv), 0,
+      "HPX main exited with non-zero status");
+    return hpx::util::report_errors();
 }
 
