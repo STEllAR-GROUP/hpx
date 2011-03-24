934d025b8fccbc0eccb6567dd2d9dd986a8fdd14
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-24 13:42:27 +0000

    Added copyrights, fixed error in cactus script.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3623 5079c8e4-9419-0410-93bb-9e5ee278c886

--- tests/performance/amr3d/hpx/hpx_amr3d_benchmark.sh
+++ tests/performance/amr3d/hpx/hpx_amr3d_benchmark.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env bash
 
-# HPX amr3d benchmark script
+################################################################################
+# Copyright (c) 2011 Bryce Lelbach
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying 
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+################################################################################
+
+###############################################################################
+# configuration 
 
 RUNS=3
 REFS="[0,1,2,3]"
@@ -9,13 +17,15 @@ GRAINS="[10,9,11,8,12]"
 
 HPX_ROOT="/home/wash/hpx/gcc-4.4.4-release"
 
+###############################################################################
+
 TIMESTAMP=`date '+%Y.%m.%d_%H.%M.%S'`
 
 LOG="benchmark.$TIMESTAMP.dat"
-TIMING="timing.$TIMESTAMP.dat"
+OUT="timing.$TIMESTAMP.dat"
 
-$HPX_ROOT/bin/hpx_optsweep.py -w 1800 -o $LOG -r $RUNS              \
-  -a 'REF',"$REFS" -a 'CPU',"$CORES" -a 'GRAIN',"$GRAINS"           \
-  /usr/bin/time -f 'CPU REF GRAIN %e %x' --output=$TIMING --append  \
+$HPX_ROOT/bin/hpx_optsweep.py -w 1800 -o $LOG -r $RUNS           \
+  -a 'REF',"$REFS" -a 'CPU',"$CORES" -a 'GRAIN',"$GRAINS"        \
+  /usr/bin/time -f 'CPU REF GRAIN %e %x' --output=$OUT --append  \
   $HPX_ROOT/bin/had_amr3d_client -r -eREF -pgGRAIN.ini -tCPU
 
