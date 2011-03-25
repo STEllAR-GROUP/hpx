811c376c0f8535718affc8d5c36a88d03438307f
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-25 21:30:53 +0000

    Allow the user to override each configuration test to either enabled or disabled by setting the CMake variable associated with the test to either ON or OFF.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3669 5079c8e4-9419-0410-93bb-9e5ee278c886

--- cmake/HPX_AddConfigTest.cmake
+++ cmake/HPX_AddConfigTest.cmake
@@ -11,53 +11,62 @@ hpx_include(Message
             Compile
             ParseArguments)
 
-macro(add_hpx_config_test name var)
-  hpx_parse_arguments(${name} "SOURCE;FLAGS;DEFINITIONS;LANGUAGE"
-                              "ESSENTIAL;FILE" ${ARGN})
+macro(add_hpx_config_test name variable)
+  hpx_parse_arguments(${name} "SOURCE;FLAGS;DEFINITIONS;LANGUAGE;DEFAULT"
+                              "FILE" ${ARGN})
 
-  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests)
+  # FIXME: Sadly, CMake doesn't support non-boolean options with the option
+  # command yet.
+  #option(${variable}
+  #  "Enable (ON), auto-detect (DETECT) or disable (OFF) ${name} (Default: ${${name}_DEFAULT}"
+  #  ${${name}_DEFAULT})
 
-  set(test_source "")
-
-  if(${name}_FILE)
-    set(test_source "${${name}_SOURCE}")
+  if("${variable}" STREQUAL "ON")
+    hpx_info("config_test.${name}" "Manually enabled.")
+    set(${variable} TRUE CACHE INTERNAL "${name} state.")
+    foreach(definition ${${name}_DEFINITIONS})
+      add_definitions(-D${definition})
+    endforeach()
+  elseif("${variable}" STREQUAL "OFF")
+    hpx_info("config_test.${name}" "Manually disabled.")
+    set(${variable} FALSE CACHE INTERNAL "${name} state.")
   else()
-    set(test_source
-        "${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/src.cpp")
-    file(WRITE "${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/src.cpp"
-         "${${name}_SOURCE}\n")
-  endif()
-
-  hpx_compile(${name} SOURCE ${test_source} LANGUAGE ${${name}_LANGUAGE}
-    OUTPUT ${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/${name} 
-    FLAGS ${${name}_FLAGS})
-
-  if("${${name}_RESULT}" STREQUAL "0")
-    set(test_result 0)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests)
   
-    execute_process(
-      COMMAND "${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/${name}"
-      RESULT_VARIABLE test_result OUTPUT_QUIET ERROR_QUIET) 
-
-    if("${test_result}" STREQUAL "0")
-      set(${var} TRUE CACHE INTERNAL "Test ${name} result.")
-      hpx_info("config_test.${name}" "Test passed.")
-      foreach(definition ${${name}_DEFINITIONS})
-        add_definitions(-D${definition})
-      endforeach()
+    set(test_source "")
+  
+    if(${name}_FILE)
+      set(test_source "${${name}_SOURCE}")
     else()
-      set(${var} FALSE CACHE INTERNAL "Test ${name} result.")
-      if(${name}_ESSENTIAL)
-        hpx_fail("config_test.${name}" "Test failed, returned ${test_result}.") 
+      set(test_source
+          "${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/src.cpp")
+      file(WRITE "${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/src.cpp"
+           "${${name}_SOURCE}\n")
+    endif()
+  
+    hpx_compile(${name} SOURCE ${test_source} LANGUAGE ${${name}_LANGUAGE}
+      OUTPUT ${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/${name} 
+      FLAGS ${${name}_FLAGS})
+  
+    if("${${name}_RESULT}" STREQUAL "0")
+      set(test_result 0)
+    
+      execute_process(
+        COMMAND "${CMAKE_BINARY_DIR}/${CMAKE_FILES_DIRECTORY}/config_tests/${name}"
+        RESULT_VARIABLE test_result OUTPUT_QUIET ERROR_QUIET) 
+  
+      if("${test_result}" STREQUAL "0")
+        set(${variable} TRUE CACHE INTERNAL "${name} state.")
+        hpx_info("config_test.${name}" "Test passed.")
+        foreach(definition ${${name}_DEFINITIONS})
+          add_definitions(-D${definition})
+        endforeach()
       else()
+        set(${variable} FALSE CACHE INTERNAL "${name} state.")
         hpx_warn("config_test.${name}" "Test failed, returned ${test_result}.") 
       endif()
-    endif()
-  else()
-    set(${var} FALSE CACHE INTERNAL "Test ${name} result.")
-    if(${name}_ESSENTIAL)
-      hpx_fail("config_test.${name}" "Test failed to compile.") 
     else()
+      set(${variable} FALSE CACHE INTERNAL "${name} state.")
       hpx_warn("config_test.${name}" "Test failed to compile.") 
     endif()
   endif()
@@ -65,34 +74,35 @@ endmacro()
 
 ###############################################################################
 macro(hpx_check_for_gnu_128bit_integers variable)
-  add_hpx_config_test("gnu_int128" ${variable} LANGUAGE CXX
+  add_hpx_config_test("gnu_int128" ${variable} LANGUAGE CXX 
     SOURCE ${hpx_SOURCE_DIR}/cmake/tests/gnu_128bit_integers.cpp
     FLAGS -I ${BOOST_INCLUDE_DIR} -I ${hpx_SOURCE_DIR} FILE ${ARGN})
 endmacro()
 
 macro(hpx_check_for_gnu_mcx16 variable)
-  add_hpx_config_test("gnu_mcx16" ${variable} LANGUAGE CXX
+  add_hpx_config_test("gnu_mcx16" ${variable} LANGUAGE CXX 
     SOURCE ${hpx_SOURCE_DIR}/cmake/tests/flag.cpp
     FLAGS -mcx16 FILE ${ARGN})
 endmacro()
 
-macro(hpx_check_for_gnu_march variable)
-  add_hpx_config_test("gnu_march" ${variable} LANGUAGE CXX
-    SOURCE ${hpx_SOURCE_DIR}/cmake/tests/flag.cpp
-    FLAGS -march=native FILE ${ARGN})
-endmacro()
-
 ###############################################################################
 macro(hpx_check_for_pthread_affinity_np variable)
-  add_hpx_config_test("pthread_affinity_np" ${variable} LANGUAGE CXX
+  add_hpx_config_test("pthread_affinity_np" ${variable} LANGUAGE CXX 
     SOURCE ${hpx_SOURCE_DIR}/cmake/tests/pthread_affinity_np.cpp
     FLAGS -pthread -I ${BOOST_INCLUDE_DIR} -I ${hpx_SOURCE_DIR} FILE ${ARGN})
 endmacro()
 
 ###############################################################################
 macro(hpx_check_for_rdtscp variable)
-  add_hpx_config_test("rdtscp" ${variable} LANGUAGE CXX
+  add_hpx_config_test("rdtscp" ${variable} LANGUAGE CXX 
     SOURCE ${hpx_SOURCE_DIR}/cmake/tests/rdtscp.cpp
     FLAGS -I ${BOOST_INCLUDE_DIR} -I ${hpx_SOURCE_DIR} FILE ${ARGN})
+endmacro()
+
+macro(hpx_check_for_compiler_auto_tune variable)
+  # TODO: add support for MSVC-esque compilers
+  add_hpx_config_test("compiler_auto_tune" ${variable} LANGUAGE CXX 
+    SOURCE ${hpx_SOURCE_DIR}/cmake/tests/flag.cpp
+    FLAGS -march=native FILE ${ARGN})
 endmacro()
 
