a20f879dc0b623391ebca8d42f5e3e66dafe7059
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-27 03:22:00 +0000

    Tabs.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3676 5079c8e4-9419-0410-93bb-9e5ee278c886

--- external/atomic/boost/atomic/detail/fallback.hpp
+++ external/atomic/boost/atomic/detail/fallback.hpp
@@ -17,56 +17,56 @@ namespace atomic {
 template<typename T>
 class fallback_atomic {
 public:
-	fallback_atomic(void) {}
-	explicit fallback_atomic(const T &t) {memcpy(&i, &t, sizeof(T));}
-	
-	void store(const T &t, memory_order order=memory_order_seq_cst) volatile
-	{
-		detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
-		memcpy((void*)&i, &t, sizeof(T));
-	}
-	T load(memory_order /*order*/=memory_order_seq_cst) volatile const
-	{
-		detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
-		T tmp;
-		memcpy(&tmp, (T*)&i, sizeof(T));
-		return tmp;
-	}
-	bool compare_exchange_strong(
-		T &expected,
-		T desired,
-		memory_order /*success_order*/,
-		memory_order /*failure_order*/) volatile
-	{
-		detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
-		if (memcmp((void*)&i, &expected, sizeof(T))==0) {
-			memcpy((void*)&i, &desired, sizeof(T));
-			return true;
-		} else {
-			memcpy(&expected, (void*)&i, sizeof(T));
-			return false;
-		}
-	}
-	bool compare_exchange_weak(
-		T &expected,
-		T desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
-	{
-		return compare_exchange_strong(expected, desired, success_order, failure_order);
-	}
-	T exchange(T replacement, memory_order /*order*/=memory_order_seq_cst) volatile
-	{
-		detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
-		T tmp;
-		memcpy(&tmp, (void*)&i, sizeof(T));
-		memcpy((void*)&i, &replacement, sizeof(T));
-		return tmp;
-	}
-	bool is_lock_free(void) const volatile {return false;}
+    fallback_atomic(void) {}
+    explicit fallback_atomic(const T &t) {memcpy(&i, &t, sizeof(T));}
+    
+    void store(const T &t, memory_order order=memory_order_seq_cst) volatile
+    {
+        detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
+        memcpy((void*)&i, &t, sizeof(T));
+    }
+    T load(memory_order /*order*/=memory_order_seq_cst) volatile const
+    {
+        detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
+        T tmp;
+        memcpy(&tmp, (T*)&i, sizeof(T));
+        return tmp;
+    }
+    bool compare_exchange_strong(
+        T &expected,
+        T desired,
+        memory_order /*success_order*/,
+        memory_order /*failure_order*/) volatile
+    {
+        detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
+        if (memcmp((void*)&i, &expected, sizeof(T))==0) {
+            memcpy((void*)&i, &desired, sizeof(T));
+            return true;
+        } else {
+            memcpy(&expected, (void*)&i, sizeof(T));
+            return false;
+        }
+    }
+    bool compare_exchange_weak(
+        T &expected,
+        T desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        return compare_exchange_strong(expected, desired, success_order, failure_order);
+    }
+    T exchange(T replacement, memory_order /*order*/=memory_order_seq_cst) volatile
+    {
+        detail::spinlock_pool<0>::scoped_lock guard(const_cast<T*>(&i));
+        T tmp;
+        memcpy(&tmp, (void*)&i, sizeof(T));
+        memcpy((void*)&i, &replacement, sizeof(T));
+        return tmp;
+    }
+    bool is_lock_free(void) const volatile {return false;}
 protected:
-	T i;
-	typedef T integral_type;
+    T i;
+    typedef T integral_type;
 };
 
 }
