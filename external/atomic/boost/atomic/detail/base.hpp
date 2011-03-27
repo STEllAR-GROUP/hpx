60d9ead22f861160675e0b0282c42f01754b81a2
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-27 03:21:31 +0000

    Remove tabs.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3674 5079c8e4-9419-0410-93bb-9e5ee278c886

--- external/atomic/boost/atomic/detail/base.hpp
+++ external/atomic/boost/atomic/detail/base.hpp
@@ -17,43 +17,43 @@ namespace atomic {
 
 static inline memory_order calculate_failure_order(memory_order order)
 {
-	switch(order) {
-		case memory_order_acq_rel: return memory_order_acquire;
-		case memory_order_release: return memory_order_relaxed;
-		default: return order;
-	}
+    switch(order) {
+        case memory_order_acq_rel: return memory_order_acquire;
+        case memory_order_release: return memory_order_relaxed;
+        default: return order;
+    }
 }
 
 template<typename T, unsigned short Size=sizeof(T)>
 class platform_atomic : public fallback_atomic<T> {
 public:
-	typedef fallback_atomic<T> super;
-	
-	explicit platform_atomic(T v) : super(v) {}
-	platform_atomic() {}
+    typedef fallback_atomic<T> super;
+    
+    explicit platform_atomic(T v) : super(v) {}
+    platform_atomic() {}
 protected:
-	typedef typename super::integral_type integral_type;
+    typedef typename super::integral_type integral_type;
 };
 
 template<typename T, unsigned short Size=sizeof(T)>
 class platform_atomic_integral : public build_atomic_from_exchange<fallback_atomic<T> > {
 public:
-	typedef build_atomic_from_exchange<fallback_atomic<T> > super;
-	
-	explicit platform_atomic_integral(T v) : super(v) {}
-	platform_atomic_integral() {}
+    typedef build_atomic_from_exchange<fallback_atomic<T> > super;
+    
+    explicit platform_atomic_integral(T v) : super(v) {}
+    platform_atomic_integral() {}
 protected:
-	typedef typename super::integral_type integral_type;
+    typedef typename super::integral_type integral_type;
 };
 
 template<typename T>
 static inline void platform_atomic_thread_fence(T order)
 {
-	/* FIXME: this does not provide
-	sequential consistency, need one global
-	variable for that... */
-	platform_atomic<int> a;
-	a.exchange(0, order);
+    /* FIXME: this does not provide
+    sequential consistency, need one global
+    variable for that... */
+    platform_atomic<int> a;
+    a.exchange(0, order);
 }
 
 template<typename T, unsigned short Size=sizeof(T), typename Int=typename is_integral_type<T>::test>
@@ -62,121 +62,121 @@ class internal_atomic;
 template<typename T, unsigned short Size>
 class internal_atomic<T, Size, void> : private detail::atomic::platform_atomic<T> {
 public:
-	typedef detail::atomic::platform_atomic<T> super;
-	
-	internal_atomic() {}
-	explicit internal_atomic(T v) : super(v) {}
-	
-	operator T(void) const volatile {return load();}
-	T operator=(T v) volatile {store(v); return v;}	
-	
-	using super::is_lock_free;
-	using super::load;
-	using super::store;
-	using super::exchange;
-	
-	bool compare_exchange_strong(
-		T &expected,
-		T desired,
-		memory_order order=memory_order_seq_cst) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
-	}
-	bool compare_exchange_weak(
-		T &expected,
-		T desired,
-		memory_order order=memory_order_seq_cst) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
-	}
-	bool compare_exchange_strong(
-		T &expected,
-		T desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, success_order, failure_order);
-	}
-	bool compare_exchange_weak(
-		T &expected,
-		T desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, success_order, failure_order);
-	}
+    typedef detail::atomic::platform_atomic<T> super;
+    
+    internal_atomic() {}
+    explicit internal_atomic(T v) : super(v) {}
+    
+    operator T(void) const volatile {return load();}
+    T operator=(T v) volatile {store(v); return v;}    
+    
+    using super::is_lock_free;
+    using super::load;
+    using super::store;
+    using super::exchange;
+    
+    bool compare_exchange_strong(
+        T &expected,
+        T desired,
+        memory_order order=memory_order_seq_cst) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
+    }
+    bool compare_exchange_weak(
+        T &expected,
+        T desired,
+        memory_order order=memory_order_seq_cst) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
+    }
+    bool compare_exchange_strong(
+        T &expected,
+        T desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, success_order, failure_order);
+    }
+    bool compare_exchange_weak(
+        T &expected,
+        T desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, success_order, failure_order);
+    }
 private:
-	internal_atomic(const internal_atomic &);
-	void operator=(const internal_atomic &);
+    internal_atomic(const internal_atomic &);
+    void operator=(const internal_atomic &);
 };
 
 template<typename T, unsigned short Size>
 class internal_atomic<T, Size, int> : private detail::atomic::platform_atomic_integral<T> {
 public:
-	typedef detail::atomic::platform_atomic_integral<T> super;
-	typedef typename super::integral_type integral_type;
-	
-	internal_atomic() {}
-	explicit internal_atomic(T v) : super(v) {}
-	
-	using super::is_lock_free;
-	using super::load;
-	using super::store;
-	using super::exchange;
-	using super::fetch_add;
-	using super::fetch_sub;
-	using super::fetch_and;
-	using super::fetch_or;
-	using super::fetch_xor;
-	
-	operator integral_type(void) const volatile {return load();}
-	integral_type operator=(integral_type v) volatile {store(v); return v;}	
-	
-	integral_type operator&=(integral_type c) volatile {return fetch_and(c)&c;}
-	integral_type operator|=(integral_type c) volatile {return fetch_or(c)|c;}
-	integral_type operator^=(integral_type c) volatile {return fetch_xor(c)^c;}
-	
-	integral_type operator+=(integral_type c) volatile {return fetch_add(c)+c;}
-	integral_type operator-=(integral_type c) volatile {return fetch_sub(c)-c;}
-	
-	integral_type operator++(void) volatile {return fetch_add(1)+1;}
-	integral_type operator++(int) volatile {return fetch_add(1);}
-	integral_type operator--(void) volatile {return fetch_sub(1)-1;}
-	integral_type operator--(int) volatile {return fetch_sub(1);}
-	
-	bool compare_exchange_strong(
-		integral_type &expected,
-		integral_type desired,
-		memory_order order=memory_order_seq_cst) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
-	}
-	bool compare_exchange_weak(
-		integral_type &expected,
-		integral_type desired,
-		memory_order order=memory_order_seq_cst) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
-	}
-	bool compare_exchange_strong(
-		integral_type &expected,
-		integral_type desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, success_order, failure_order);
-	}
-	bool compare_exchange_weak(
-		integral_type &expected,
-		integral_type desired,
-		memory_order success_order,
-		memory_order failure_order) volatile
-	{
-		return super::compare_exchange_strong(expected, desired, success_order, failure_order);
-	}
+    typedef detail::atomic::platform_atomic_integral<T> super;
+    typedef typename super::integral_type integral_type;
+    
+    internal_atomic() {}
+    explicit internal_atomic(T v) : super(v) {}
+    
+    using super::is_lock_free;
+    using super::load;
+    using super::store;
+    using super::exchange;
+    using super::fetch_add;
+    using super::fetch_sub;
+    using super::fetch_and;
+    using super::fetch_or;
+    using super::fetch_xor;
+    
+    operator integral_type(void) const volatile {return load();}
+    integral_type operator=(integral_type v) volatile {store(v); return v;}    
+    
+    integral_type operator&=(integral_type c) volatile {return fetch_and(c)&c;}
+    integral_type operator|=(integral_type c) volatile {return fetch_or(c)|c;}
+    integral_type operator^=(integral_type c) volatile {return fetch_xor(c)^c;}
+    
+    integral_type operator+=(integral_type c) volatile {return fetch_add(c)+c;}
+    integral_type operator-=(integral_type c) volatile {return fetch_sub(c)-c;}
+    
+    integral_type operator++(void) volatile {return fetch_add(1)+1;}
+    integral_type operator++(int) volatile {return fetch_add(1);}
+    integral_type operator--(void) volatile {return fetch_sub(1)-1;}
+    integral_type operator--(int) volatile {return fetch_sub(1);}
+    
+    bool compare_exchange_strong(
+        integral_type &expected,
+        integral_type desired,
+        memory_order order=memory_order_seq_cst) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
+    }
+    bool compare_exchange_weak(
+        integral_type &expected,
+        integral_type desired,
+        memory_order order=memory_order_seq_cst) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, order, calculate_failure_order(order));
+    }
+    bool compare_exchange_strong(
+        integral_type &expected,
+        integral_type desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, success_order, failure_order);
+    }
+    bool compare_exchange_weak(
+        integral_type &expected,
+        integral_type desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        return super::compare_exchange_strong(expected, desired, success_order, failure_order);
+    }
 private:
-	internal_atomic(const internal_atomic &);
-	void operator=(const internal_atomic &);
+    internal_atomic(const internal_atomic &);
+    void operator=(const internal_atomic &);
 };
 
 }
