be924e64e1570396d20be601d0cf9a0ad38aa5bf
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-27 03:29:48 +0000

    Added support for 128bit integers (GCC only; MSVC/Intel/Clang implementation needed).
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3679 5079c8e4-9419-0410-93bb-9e5ee278c886

--- external/atomic/boost/atomic/detail/integral-casts.hpp
+++ external/atomic/boost/atomic/detail/integral-casts.hpp
@@ -66,7 +66,7 @@ public:
     }
     
     operator T(void) const volatile {return load();}
-    T operator=(T v) volatile {store(v); return v;}	
+    T operator=(T v) volatile {store(v); return v;}    
     
     using super::is_lock_free;
 protected:
@@ -138,7 +138,7 @@ public:
     }
     
     operator T(void) const volatile {return load();}
-    T operator=(T v) volatile {store(v); return v;}	
+    T operator=(T v) volatile {store(v); return v;}    
     
     using super::is_lock_free;
 protected:
@@ -210,7 +210,7 @@ public:
     }
     
     operator T(void) const volatile {return load();}
-    T operator=(T v) volatile {store(v); return v;}	
+    T operator=(T v) volatile {store(v); return v;}    
     
     using super::is_lock_free;
 protected:
@@ -282,7 +282,7 @@ public:
     }
     
     operator T(void) const volatile {return load();}
-    T operator=(T v) volatile {store(v); return v;}	
+    T operator=(T v) volatile {store(v); return v;}    
     
     using super::is_lock_free;
 protected:
@@ -299,6 +299,87 @@ protected:
         return tmp;
     }
 };
+
+#if (defined(__amd64__) || defined(__x86_64__)) && \
+    defined(BOOST_ATOMIC_HAVE_SSE2) && \
+    defined(BOOST_ATOMIC_HAVE_GNU_SYNC_16) && \
+    defined(BOOST_ATOMIC_HAVE_GNU_ALIGNED_16) && \
+    defined(BOOST_ATOMIC_HAVE_GNU_128BIT_INTEGERS)
+
+#define BOOST_ATOMIC_HAVE_128BIT_SUPPORT
+
+template<typename T>
+class platform_atomic<T, 16> : private platform_atomic_integral<__uint128_t> {
+public:
+    typedef platform_atomic_integral<__uint128_t> super;
+#if defined(BOOST_ATOMIC_ENFORCE_PODNESS)
+    typedef union { T e; __uint128_t i;} conv;
+#endif
+
+    platform_atomic() {}
+    explicit platform_atomic(T t) : super(to_integral(t))
+    {
+    }
+    
+    void store(T t, memory_order order=memory_order_seq_cst) volatile
+    {
+        super::store(to_integral(t), order);
+    }
+    T load(memory_order order=memory_order_seq_cst) volatile const
+    {
+        return from_integral(super::load(order));
+    }
+    bool compare_exchange_strong(
+        T &expected,
+        T desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        __uint128_t _expected, _desired;
+        _expected=to_integral(expected);
+        _desired=to_integral(desired);
+        bool success=super::compare_exchange_strong(_expected, _desired, success_order, failure_order);
+        expected=from_integral(_expected);
+        return success;
+    }
+    bool compare_exchange_weak(
+        T &expected,
+        T desired,
+        memory_order success_order,
+        memory_order failure_order) volatile
+    {
+        __uint128_t _expected, _desired;
+        _expected=to_integral(expected);
+        _desired=to_integral(desired);
+        bool success=super::compare_exchange_weak(_expected, _desired, success_order, failure_order);
+        expected=from_integral(_expected);
+        return success;
+    }
+    
+    T exchange(T replacement, memory_order order=memory_order_seq_cst) volatile
+    {
+        return from_integral(super::exchange(to_integral(replacement), order));
+    }
+    
+    operator T(void) const volatile {return load();}
+    T operator=(T v) volatile {store(v); return v;}    
+    
+    using super::is_lock_free;
+protected:
+    static inline __uint128_t to_integral(T &t)
+    {
+        __uint128_t tmp;
+        memcpy(&tmp, &t, sizeof(t));
+        return tmp;
+    }
+    static inline T from_integral(__uint128_t t)
+    {
+        T tmp;
+        memcpy(&tmp, &t, sizeof(t));
+        return tmp;
+    }
+};
+#endif
 
 } } }
 
