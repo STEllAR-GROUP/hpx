ed0c6d1481a6aa2856e972eed666de31e8c2164b
Chirag Dekate <cdekate@cct.lsu.edu>
2011-03-30 19:44:15 +0000

    removing un necessary folders from examples
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3719 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/bhpx/clrsvn.sh
+++
@@ -1,8 +0,0 @@
-rm .svn -rf 
-rm itn/.svn -rf
-rm itn/server/.svn -rf
-rm itn/stubs/.svn -rf
-rm tlf/.svn -rf
-rm tlf/server/.svn -rf
-rm tlf/stubs/.svn -rf
-rm .kdev4/.svn -rf
--- examples/bhpx/run.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/cdekate/codes/hpx/examples/bhpx/tlf/
-./bhc -r -t 1 -i 5_file
