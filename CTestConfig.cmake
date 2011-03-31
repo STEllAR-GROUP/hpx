2c00d9986294fbca0757294573c83df0cd14271c
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-03-31 13:44:23 +0000

    Fix CTestConfig.cmake.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3725 5079c8e4-9419-0410-93bb-9e5ee278c886

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,5 +7,6 @@ set(CTEST_PROJECT_NAME "hpx")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "ithaca.cct.lsu.edu")
 set(CTEST_DROP_LOCATION "/submit.php?project=hpx")
+set(CTEST_UPDATE_TYPE "svn")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
