70b0c54f248ceaf5d680ce45d598f349e8b257dd
Chirag Dekate <cdekate@cct.lsu.edu>
2011-03-31 18:55:45 +0000

    added new timer
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3734 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/nbody/nbody_c/stencil.cpp
+++ examples/nbody/nbody_c/stencil.cpp
@@ -174,7 +174,7 @@ namespace hpx { namespace components { namespace nbody
               for(int d = 0; d < ci_num_par; ++d)
               {
                   global_idx[d] = (compute_index * par->granularity) + d;
-//                   std::cout << "stencil::eval:: compute index "<< compute_index << " global_idx " << global_idx[d] << std::endl;
+                   std::cout << "stencil::eval:: compute index "<< compute_index << " global_idx " << global_idx[d] << std::endl;
               }
               
               int i_num_par = 0;
--- examples/nbody/nbody_client.cpp
+++ examples/nbody/nbody_client.cpp
@@ -649,6 +649,13 @@ void TreeLeaf::moveParticles( components::nbody::Parameter & par)
         v[i] = v_half[i] + vel_dt_half [i];  
 }
 
+double chroner(timeval &t1, timeval &t2)
+{
+	double elapsedTime;
+	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+	return elapsedTime + 0.5;
+}
 
 struct less {
   template<class T>
@@ -762,6 +769,7 @@ int hpx_main(std::size_t numvals, std::size_t numsteps,bool do_logging,
         infile.close();  
         double fullForceTime=0.0;
         std::vector<naming::id_type> result_data;
+	timeval t1, t2;
         for (par->iter = 0; par->iter < par->num_iterations; ++par->iter)
         {
 
@@ -938,13 +946,13 @@ int hpx_main(std::size_t numvals, std::size_t numsteps,bool do_logging,
 
 ///////blockcomment  
 //           if (par->iter == 0)
-                hpx::util::high_resolution_timer forcetime;
-
+//                hpx::util::high_resolution_timer forcetime;
+        	gettimeofday(&t1, NULL);
                 result_data = 
                 unigrid_mesh.init_execute(function_type, numvals, 
                 numsteps, do_logging ? logging_type : components::component_invalid,par);
-                
-               iterForceTime = forcetime.elapsed();
+        	gettimeofday(&t2, NULL);        
+               iterForceTime = chroner(t1,t2); 
                fullForceTime += iterForceTime;
                std::cout << "Per Iteration ForceCalc() Time " << iterForceTime << " [s]" << std::endl;
 
