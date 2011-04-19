47ae35d472f9046b99ab5efb06dda2c63aa88184
Matthew Anderson <manderson@cct.lsu.edu>
2011-04-19 14:56:37 +0000

    Changing defaults
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3852 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/had_amr3d/amr_client.cpp
+++ examples/had_amr3d/amr_client.cpp
@@ -250,7 +250,7 @@ int main(int argc, char* argv[])
 
         std::size_t numvals;
 
-        std::size_t numsteps = 12;
+        std::size_t numsteps = 3;
         if (vm.count("numsteps"))
             numsteps = vm["numsteps"].as<std::size_t>();
         
