4d407c4ed7055cfa5b363e588ce5ceab503b18f7
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-04-20 15:15:22 +0000

    Switch accumulator to hpx_init.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3870 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/accumulator/simple_accumulator_client.cpp
+++ examples/accumulator/simple_accumulator_client.cpp
@@ -1,41 +1,30 @@
 //  Copyright (c) 2007-2010 Hartmut Kaiser, Richard D Guidry Jr
+//  Copyright (c)      2011 Bryce Lelbach
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <cstring>
-#include <iostream>
-
 #include <hpx/hpx.hpp>
-
-#include <boost/lexical_cast.hpp>
-#include <boost/program_options.hpp>
+#include <hpx/hpx_init.hpp>
 
 #include "simple_accumulator/simple_accumulator.hpp"
 
-namespace po = boost::program_options;
-
-using namespace hpx;
-using namespace std;
-
 ///////////////////////////////////////////////////////////////////////////////
-int hpx_main()
+int hpx_main(boost::program_options::variables_map&)
 {
     // get list of all known localities
-    std::vector<naming::id_type> prefixes;
-    applier::applier& appl = applier::get_applier();
-    naming::id_type prefix;
-    if (appl.get_remote_prefixes(prefixes)) {
+    std::vector<hpx::naming::id_type> prefixes;
+    hpx::applier::applier& appl = hpx::applier::get_applier();
+    hpx::naming::id_type prefix;
+
+    if (appl.get_remote_prefixes(prefixes))
         // create accumulator on any of the remote localities
         prefix = prefixes[0];
-    }
-    else {
+    else
         // create an accumulator locally
         prefix = appl.get_runtime_support_gid();
-    }
 
-    using hpx::components::simple_accumulator;
-    simple_accumulator accu;
+    hpx::components::simple_accumulator accu;
     accu.create(prefix);
 
     // print some message
@@ -47,166 +36,54 @@ int hpx_main()
     std::cin >> cmd;
     while (std::cin.good())
     {
-        if(cmd == "init") {
+        if (cmd == "init")
             accu.init();
-        }
-        else if (cmd == "add") {
+
+        else if (cmd == "add")
+        {
             std::string arg;
             std::cin >> arg;
             accu.add(boost::lexical_cast<double>(arg));
         }
-        else if (cmd == "print") {
+
+        else if (cmd == "print")
             accu.print();
-        }
-        else if (cmd == "query") {
+
+        else if (cmd == "query")
             std::cout << accu.query() << std::endl;
-        }
-        else if (cmd == "help") {
+
+        else if (cmd == "help")
             std::cout << "commands: init, add [amount], print, query, help, quit" 
                       << std::endl;
-        }
-        else if (cmd == "quit") {
+
+        else if (cmd == "quit")
             break;
-        }
-        else {
-            std::cout << "Invalid command." << std::endl;
-            std::cout << "commands: init, add [amount], print, help, quit" 
+
+        else
+            std::cout << "Invalid command.\n" 
+                         "commands: init, add [amount], print, help, quit" 
                       << std::endl;
-        }
+
         std::cin >> cmd;
     }
 
     // free the accumulator component
-    accu.free();     // this invalidates the remote reference
+    accu.free(); // this invalidates the remote reference
 
     // initiate shutdown of the runtime systems on all localities
-    components::stubs::runtime_support::shutdown_all();
+    hpx::finalize();
 
     return 0;
 }
-
-///////////////////////////////////////////////////////////////////////////////
-bool parse_commandline(int argc, char *argv[], po::variables_map& vm)
-{
-    try {
-        po::options_description desc_cmdline ("Usage: hpx_runtime [options]");
-        desc_cmdline.add_options()
-            ("help,h", "print out program usage (this message)")
-            ("run_agas_server,r", "run AGAS server as part of this runtime instance")
-            ("worker,w", "run this instance in worker (non-console) mode")
-            ("agas,a", po::value<std::string>(), 
-                "the IP address the AGAS server is running on (default taken "
-                "from hpx.ini), expected format: 192.168.1.1:7912")
-            ("hpx,x", po::value<std::string>(), 
-                "the IP address the HPX parcelport is listening on (default "
-                "is localhost:7910), expected format: 192.168.1.1:7913")
-            ("threads,t", po::value<int>(), 
-                "the number of operating system threads to spawn for this"
-                "HPX locality")
-        ;
-
-        po::store(po::command_line_parser(argc, argv)
-            .options(desc_cmdline).run(), vm);
-        po::notify(vm);
-
-        // print help screen
-        if (vm.count("help")) {
-            std::cout << desc_cmdline;
-            return false;
-        }
-    }
-    catch (std::exception const& e) {
-        std::cerr << "simple_accumulator_client: exception caught: " << e.what() << std::endl;
-        return false;
-    }
-    return true;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-inline void 
-split_ip_address(std::string const& v, std::string& addr, boost::uint16_t& port)
-{
-    std::string::size_type p = v.find_first_of(":");
-    try {
-        if (p != std::string::npos) {
-            addr = v.substr(0, p);
-            port = boost::lexical_cast<boost::uint16_t>(v.substr(p+1));
-        }
-        else {
-            addr = v;
-        }
-    }
-    catch (boost::bad_lexical_cast const& /*e*/) {
-        std::cerr << "simple_accumulator_client: illegal port number given: " << v.substr(p+1) << std::endl;
-        std::cerr << "                            using default value instead: " << port << std::endl;
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// helper class for AGAS server initialization
-class agas_server_helper
-{
-public:
-    agas_server_helper(std::string host, boost::uint16_t port)
-      : agas_pool_(), agas_(agas_pool_, host, port)
-    {
-        agas_.run(false);
-    }
-
-private:
-    hpx::util::io_service_pool agas_pool_; 
-    hpx::naming::resolver_server agas_;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-// this is the runtime type we use in this application
-typedef hpx::runtime_impl<hpx::threads::policies::global_queue_scheduler> runtime_type;
 
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-    try {
-        // analyze the command line
-        po::variables_map vm;
-        if (!parse_commandline(argc, argv, vm))
-            return -1;
+    // Configure application-specific options
+    boost::program_options::options_description
+       desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
 
-        // Check command line arguments.
-        std::string hpx_host("localhost"), agas_host;
-        boost::uint16_t hpx_port = HPX_PORT, agas_port = 0;
-        int num_threads = 1;
-        hpx::runtime::mode mode = hpx::runtime::console;    // default is console mode
-
-        // extract IP address/port arguments
-        if (vm.count("agas")) 
-            split_ip_address(vm["agas"].as<std::string>(), agas_host, agas_port);
-
-        if (vm.count("hpx")) 
-            split_ip_address(vm["hpx"].as<std::string>(), hpx_host, hpx_port);
-
-        if (vm.count("threads"))
-            num_threads = vm["threads"].as<int>();
-
-        if (vm.count("worker"))
-            mode = hpx::runtime::worker;
-
-        // initialize and run the AGAS service, if appropriate
-        std::auto_ptr<agas_server_helper> agas_server;
-        if (vm.count("run_agas_server"))  // run the AGAS server instance here
-            agas_server.reset(new agas_server_helper(agas_host, agas_port));
-
-        // initialize and start the HPX runtime
-        runtime_type rt(hpx_host, hpx_port, agas_host, agas_port, mode);
-        rt.run(hpx_main, num_threads);
-    }
-    catch (std::exception& e) {
-        std::cerr << "std::exception caught: " << e.what() << "\n";
-        return -1;
-    }
-    catch (...) {
-        std::cerr << "unexpected exception caught\n";
-        return -2;
-    }
-    return 0;
+    // Initialize and run HPX
+    return hpx::init(desc_commandline, argc, argv);
 }
 
