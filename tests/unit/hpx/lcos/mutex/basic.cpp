2a5f19d30248c31c423c1addd09e3937ca678e73
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-04-25 01:47:47 +0000

    Start fixing mutex tests.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@3943 5079c8e4-9419-0410-93bb-9e5ee278c886

--- tests/unit/hpx/lcos/mutex/basic.cpp
+++ tests/unit/hpx/lcos/mutex/basic.cpp
@@ -1,26 +1,22 @@
-//  Copyright (c) 2007-2011 Hartmut Kaiser
-//  Copyright (c)      2011 Bryce Lelbach 
-// 
+//  Copyright (C) 2001-2003 William E. Kempf
+//  Copyright (C) 2007-2011 Hartmut Kaiser
+//  Copyright (C)      2011 Bryce Lelbach
+//
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#include <boost/atomic.hpp>
 
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/util/lightweight_test.hpp>
-#include <hpx/lcos/barrier.hpp>
+#include <hpx/lcos/mutex.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
 using boost::program_options::value;
 
-using hpx::naming::id_type;
+using hpx::applier::register_work_nullary;
 
-using hpx::applier::get_applier;
-using hpx::applier::register_work;
-
-using hpx::lcos::barrier;
+using hpx::lcos::mutex;
 
 using hpx::init;
 using hpx::finalize;
@@ -28,13 +24,32 @@ using hpx::finalize;
 using hpx::util::report_errors;
 
 ///////////////////////////////////////////////////////////////////////////////
-void barrier_test(id_type const& id, boost::atomic<std::size_t>& c,
-                  std::size_t pxthreads)
+template <typename M>
+struct test_lock
 {
-    ++c;
-    // wait for all threads to enter the barrier
-    hpx::lcos::stubs::barrier::wait(id);
-}
+    typedef M mutex_type;
+    typedef typename M::scoped_lock lock_type;
+
+    void operator()()
+    {
+        mutex_type mutex;
+
+        // Test the lock's constructors.
+        {
+            lock_type lock(mutex, boost::defer_lock);
+            HPX_TEST(!lock);
+        }
+
+        lock_type lock(mutex);
+        HPX_TEST(lock ? true : false);
+
+        // Test the lock and unlock methods.
+        lock.unlock();
+        HPX_TEST(!lock);
+        lock.lock();
+        HPX_TEST(lock ? true : false);
+    }
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(variables_map& vm)
@@ -44,25 +59,9 @@ int hpx_main(variables_map& vm)
     if (vm.count("threads"))
         num_threads = vm["threads"].as<std::size_t>();
 
-    std::size_t pxthreads = num_threads * 2;
-
-    if (vm.count("pxthreads"))
-        pxthreads = vm["pxthreads"].as<std::size_t>();
-
-    id_type prefix = get_applier().get_runtime_support_gid();
-
-    // create a barrier waiting on 'count' threads
-    barrier b;
-    b.create_one(prefix, pxthreads + 1);
-
-    boost::atomic<std::size_t> c(0);
-
-    for (std::size_t j = 0; j < pxthreads; ++j)
-        register_work(boost::bind 
-            (&barrier_test, b.get_gid(), boost::ref(c), pxthreads));
-
-    b.wait(); // wait for all threads to enter the barrier
-    HPX_TEST_EQ(pxthreads, c.load());
+    test_lock<mutex> t;
+    for (std::size_t i = 0; i < num_threads; ++i)
+        register_work_nullary(t, "do_test_mutex");
 
     // initiate shutdown of the runtime system
     finalize();
@@ -75,11 +74,6 @@ int main(int argc, char* argv[])
     // Configure application-specific options
     options_description
        desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
-
-    desc_commandline.add_options()
-        ("pxthreads,T", value<std::size_t>(), 
-            "the number of PX threads to invoke (default: OS threads * 2)")
-        ;
 
     // Initialize and run HPX
     HPX_TEST_EQ_MSG(init(desc_commandline, argc, argv), 0,
