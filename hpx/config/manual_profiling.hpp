c51a665579f8b58228c4e09a21006bff37e99b43
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-05-02 19:04:17 +0000

    Fix copyright.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4043 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/config/manual_profiling.hpp
+++ hpx/config/manual_profiling.hpp
@@ -1,25 +1,24 @@
 ////////////////////////////////////////////////////////////////////////////////
 //  Copyright (c) 2011 Bryce Lelbach
-//  Copyright (C) 2007, 2008 Tim Blechmann
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
-#if !defined(HPX_8877B5FB_1967_43B9_AF98_1A01F162B725)
-#define HPX_8877B5FB_1967_43B9_AF98_1A01F162B725
+#if !defined(SHEOL_8877B5FB_1967_43B9_AF98_1A01F162B725)
+#define SHEOL_8877B5FB_1967_43B9_AF98_1A01F162B725
 
 #if defined(__GNUC__)
-  #define HPX_SUPER_PURE  __attribute__((const)) 
-  #define HPX_PURE        __attribute__((pure)) 
-  #define HPX_HOT         __attribute__((hot)) 
-  #define HPX_COLD        __attribute__((cold)) 
+  #define SHEOL_SUPER_PURE  __attribute__((const)) 
+  #define SHEOL_PURE        __attribute__((pure)) 
+  #define SHEOL_HOT         __attribute__((hot)) 
+  #define SHEOL_COLD        __attribute__((cold)) 
 #else
-  #define HPX_SUPER_PURE
-  #define HPX_PURE
-  #define HPX_HOT
-  #define HPX_COLD
+  #define SHEOL_SUPER_PURE
+  #define SHEOL_PURE
+  #define SHEOL_HOT
+  #define SHEOL_COLD
 #endif
 
-#endif // HPX_8877B5FB_1967_43B9_AF98_1A01F162B725
+#endif // SHEOL_8877B5FB_1967_43B9_AF98_1A01F162B725
 
