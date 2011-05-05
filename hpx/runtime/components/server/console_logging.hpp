81de841b103bceb78c1a1aeb60b38ca7a627660d
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-05-05 13:36:00 +0000

    Use a special console logging action.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4116 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/components/server/console_logging.hpp
+++ hpx/runtime/components/server/console_logging.hpp
@@ -30,76 +30,73 @@ namespace hpx { namespace components { namespace server
     void console_logging(logging_destination dest, int level, 
         std::string const& msg);
 
-    typedef actions::plain_direct_action3<
-        logging_destination, int, std::string const&, console_logging
-    > console_logging_action;
+//    typedef actions::plain_direct_action3<
+//        logging_destination, int, std::string const&, console_logging
+//    > console_logging_action;
 
-//     ///////////////////////////////////////////////////////////////////////////
-//     class console_logging_action
-//       : public actions::plain_action3<logging_destination, int, 
-//             std::string const&, console_logging, threads::thread_priority_low, 
-//             console_logging_action>
-//     {
-//     private:
-//         typedef actions::plain_action3<logging_destination, int, 
-//             std::string const&, console_logging, threads::thread_priority_low, 
-//             console_logging_action> 
-//         base_type;
-// 
-//     public:
-//         console_logging_action(
-//                 threads::thread_priority priority = threads::thread_priority_low)
-//           : base_type(priority)
-//         {}
-// 
-//         // construct an action from its arguments
-//         console_logging_action(logging_destination dest, int level, 
-//                 std::string const& msg) 
-//           : base_type(dest, level, msg) 
-//         {}
-// 
-//         console_logging_action(threads::thread_priority priority,
-//                 logging_destination dest, int level, std::string const& msg) 
-//           : base_type(priority, dest, level, msg) 
-//         {}
-// 
-//         /// serialization support
-//         static void register_base()
-//         {
-//             using namespace boost::serialization;
-//             void_cast_register<console_logging_action, base_type>();
-//             base_type::register_base();
-//         }
-// 
-//     public:
-//         /// The \a thread_function will be registered as the thread
-//         /// function of a thread. It encapsulates the execution of the 
-//         /// original function (given by \a func).
-//         template <typename Arg1, typename Arg2, typename Arg3>
-//         static threads::thread_state_enum thread_function(
-//             Arg1 const& dest, Arg2 const& level, Arg3 const& msg)
-// //            logging_destination dest, int level, std::string const& msg)
-//         {
-//             try {
-//                 // call the function, ignoring the return value
-//                 console_logging(dest, level, msg);
-//             }
-//             catch (hpx::exception const& /*e*/) {
-//                 /***/;      // no logging!
-//             }
-//             return threads::terminated;
-//         }
-// 
-//     private:
-//         // serialization support
-//         friend class boost::serialization::access;
-// 
-//         template<class Archive>
-//         void serialize(Archive& ar, const unsigned int /*version*/)
-//         {
-//             ar & boost::serialization::base_object<base_type>(*this);
-//         }
-//     };
+     ///////////////////////////////////////////////////////////////////////////
+     class console_logging_action
+       : public actions::plain_direct_action3<logging_destination, int, 
+             std::string const&, console_logging, 
+             console_logging_action>
+     {
+     private:
+         typedef actions::plain_direct_action3<logging_destination, int, 
+             std::string const&, console_logging, 
+             console_logging_action> 
+         base_type;
+ 
+     public:
+         console_logging_action(
+                 threads::thread_priority priority = threads::thread_priority_low)
+           : base_type()
+         {}
+ 
+         // construct an action from its arguments
+         console_logging_action(logging_destination dest, int level, 
+                 std::string const& msg) 
+           : base_type(dest, level, msg) 
+         {}
+ 
+         console_logging_action(threads::thread_priority priority,
+                 logging_destination dest, int level, std::string const& msg) 
+           : base_type(priority, dest, level, msg) 
+         {}
+ 
+         /// serialization support
+         static void register_base()
+         {
+             using namespace boost::serialization;
+             void_cast_register<console_logging_action, base_type>();
+             base_type::register_base();
+         }
+ 
+     public:
+        template <typename Arg1, typename Arg2, typename Arg3>
+        static util::unused_type
+        execute_function(naming::address::address_type lva, 
+            Arg1 const& dest, Arg2 const& level, Arg3 const& msg)
+        {
+             try {
+                 // call the function, ignoring the return value
+                 console_logging(dest, level, msg);
+             }
+             catch (hpx::exception const& /*e*/) {
+                 /***/;      // no logging!
+             }
+            return util::unused;
+        }
+ 
+     private:
+         // serialization support
+         friend class boost::serialization::access;
+ 
+         template<class Archive>
+         void serialize(Archive& ar, const unsigned int /*version*/)
+         {
+             ar & boost::serialization::base_object<base_type>(*this);
+         }
+     };
 }}}
 
 #endif
