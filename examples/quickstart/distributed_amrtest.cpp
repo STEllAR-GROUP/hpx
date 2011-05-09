b6016bd36b602fb3a50f13c76d3d7cebf91e215a
Matthew Anderson <manderson@cct.lsu.edu>
2011-05-09 19:29:18 +0000

    updated test
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4150 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/distributed_amrtest.cpp
+++ examples/quickstart/distributed_amrtest.cpp
@@ -27,7 +27,7 @@
 //       - The "-x localhost:5006" specifies that HPX should include "localhost" in the runtime and use
 //         port 5006.
 // 
-// If you'd like to get some validation that getnumber_action is being run on two different threads, you 
+// If you'd like to get some validation that get_initialdata_action is being run on two different threads, you 
 // can ask HPX to output a logfile via an environment variable. If you are using bash type
 // 
 //    export HPX_LOGLEVEL=6
@@ -39,7 +39,7 @@
 // for more high level information.
 // 
 // After you execute the application HPX creates a log file of the form hpx.PID.log. You can search this file
-// for the string getnumber_action and see that the thread IDs differ.
+// for the string get_initialdata_action and see that the thread IDs differ.
 
 //C++ include to permit output to the screen
 #include <iostream>
@@ -84,17 +84,18 @@ int main(int argc, char* argv[])
 */
 
     /*
-    getnumber() is the routine in which the "work" for this example is done. In this case the 
+    get_initialdata() is the routine in which the "work" for this example is done. In this case the 
     work is trivial -- the integer 6 is returned from each invocation -- but in a real code
     there would be a calculation in here representing some piece of capability in the underlying
     application. In fact, there would likely be (many) more than one such routine for each of the
     units of work that need to be done. We need the declaration here because of the code that 
     follows it.
     */
-naming::id_type getnumber(int hello);
+naming::id_type get_initialdata();
+naming::id_type compute(naming::id_type in1,naming::id_type in2);
 
     /*
-    This typedef takes the work routine in this example, getnumber(), and uses it to create a 
+    This typedef takes the work routine in this example, get_initialdata(), and uses it to create a 
     new type. HPX will use this declaration in the next step when it registers the routine
     with the runtime.
     
@@ -107,16 +108,21 @@ naming::id_type getnumber(int hello);
     hpx/runtime/actions/plain_action.hpp and related files for the form of these variants.
     */
 typedef 
-    actions::plain_result_action1<naming::id_type, int, getnumber> 
-getnumber_action;
+    actions::plain_result_action0<naming::id_type, get_initialdata> 
+get_initialdata_action;
+
+typedef 
+    actions::plain_result_action2<naming::id_type,naming::id_type,naming::id_type, compute> 
+compute_action;
 
     /*
-    This step registers the action just delcared, getnumber_action, based on the routine
-    getnumber() with the HPX runtime. Because this call is made by all localities running 
+    This step registers the action just delcared, get_initialdata_action, based on the routine
+    get_initialdata() with the HPX runtime. Because this call is made by all localities running 
     this example code, then when a block of code queries the runtime to see who is available
-    to provide computations on behalf of getnumber_action all localities will report back.
+    to provide computations on behalf of get_initialdata_action all localities will report back.
     */
-HPX_REGISTER_PLAIN_ACTION(getnumber_action);
+HPX_REGISTER_PLAIN_ACTION(get_initialdata_action);
+HPX_REGISTER_PLAIN_ACTION(compute_action);
 
 /* 
     hpx_main must exist with this name in any hpx program. In this example it is called
@@ -158,7 +164,7 @@ int hpx_main(po::variables_map &vm)
     /* 
         In this block of code we are querying all localities that the current runtime knows
         about to see which of them have registered to provide the action we need, ie. the 
-        getnumber_action. This list represents the universe of computational resources that we 
+        get_initialdata_action. This list represents the universe of computational resources that we 
         can schedule tasks for completion on within this runtime.
     */
 
@@ -170,7 +176,7 @@ int hpx_main(po::variables_map &vm)
         naming::id_type this_prefix = appl.get_runtime_support_gid();
     
         /* 
-            This code checks whether the action we want, in this case getnumber_action, is
+            This code checks whether the action we want, in this case get_initialdata_action, is
             registered in other localities. If it is registered in at least one other locality
             then we store the global identifier of the first of those in the list into the variable
             that_prefix, which we'll use later.
@@ -179,18 +185,18 @@ int hpx_main(po::variables_map &vm)
         /* This declaration creates a component type variable that we use to search the registry of
            actions in the code below. */
         components::component_type type = 
-            components::get_component_type<components::server::plain_function<getnumber_action> >();
+            components::get_component_type<components::server::plain_function<get_initialdata_action> >();
         // Declaration used to store the first gid (if any) of the remote prefixes
         naming::id_type that_prefix;
     
         // Search the remote localities (ie., localities that aren't this locality) for those that
-        // have registered to provide the getnumber_action service.
+        // have registered to provide the get_initialdata_action service.
         if (appl.get_remote_prefixes(prefixes, type)) {
             // If there is at least one such remote locality, store the first gid in the list
             that_prefix = prefixes[0];
         }
         else {
-            // There are no remote localities that provide the getnumber_action we want, so set the 
+            // There are no remote localities that provide the get_initialdata_action we want, so set the 
             // remote prefix variable to the gid of our local locality (executes the function locally only)
             that_prefix = this_prefix;
         }
@@ -201,7 +207,7 @@ int hpx_main(po::variables_map &vm)
         util::high_resolution_timer t;
 
         /*
-            Delcare two Local Control Objects (lcos) as futures that perform the getnumber_action we
+            Delcare two Local Control Objects (lcos) as futures that perform the get_initialdata_action we
             registered earlier. Since we have not declared a thread scheduler in this application we
             have to tell the runtime where to run the requested action. Having to tell the runtime 
             where to run the request is obviously not the best use of the ParalleX model. You would
@@ -210,11 +216,26 @@ int hpx_main(po::variables_map &vm)
             example, however, we are telling the LCO where to run the action. Note that if our action 
             required parameters, they would appear in the list after "that_prefix".
         */
-        lcos::eager_future<getnumber_action> n1(that_prefix,1);
-        lcos::eager_future<getnumber_action> n2(this_prefix,2);
+        lcos::eager_future<get_initialdata_action> n1(that_prefix);
+        lcos::eager_future<get_initialdata_action> n2(this_prefix);
 
-        //insert busy work
-        
+        naming::id_type id1 = n1.get();
+        naming::id_type id2 = n2.get();
+
+        // compute
+        lcos::eager_future<compute_action> n3(that_prefix,id1,id2);
+        lcos::eager_future<compute_action> n4(this_prefix,id1,id2);
+
+        naming::id_type id3 = n3.get();
+        naming::id_type id4 = n4.get();
+
+        // compute
+        lcos::eager_future<compute_action> n5(that_prefix,id3,id4);
+        lcos::eager_future<compute_action> n6(this_prefix,id3,id4);
+
+        naming::id_type id5 = n5.get();
+        naming::id_type id6 = n6.get();
+
         /*   Calling the "get" method on a future causes the application to return the value that was
             previouly computed (if the runtime was able to overlap some of the computation) or to 
             block until the requested value is available. Calling "get" tells the runtime that 
@@ -223,8 +244,8 @@ int hpx_main(po::variables_map &vm)
         */
 
         // Access memory
-        components::access_memory_block<data> val1( components::stubs::memory_block::get(n1.get()) );
-        components::access_memory_block<data> val2( components::stubs::memory_block::get(n2.get()) );
+        components::access_memory_block<data> val1( components::stubs::memory_block::get(n5.get()) );
+        components::access_memory_block<data> val2( components::stubs::memory_block::get(n6.get()) );
         std::cout << " Result " << val1->val_ << " " << val2->val_ << std::endl;
         std::cout << " Vector Result " << val1->x_[0] << " " << val2->x_[0] << std::endl;
         std::cout << " Proceed Result " << val1->proceed_ << " " << val2->proceed_ << std::endl;
@@ -251,10 +272,8 @@ HPX_REGISTER_MANAGE_OBJECT_ACTION(
     hpx::actions::manage_object_action<data>, manage_object_action_data)
 
 // The routine that does the "work" for this example.
-naming::id_type getnumber (int hello)
+naming::id_type get_initialdata ()
 {  
-
-    std::cout << " TEST HELLO " << hello << std::endl;
 
     naming::id_type here = applier::get_applier().get_runtime_support_gid();
     naming::id_type result = components::stubs::memory_block::create(
@@ -280,5 +299,35 @@ naming::id_type getnumber (int hello)
     }
 
     return result;
+}
+
+// the "work" 
+naming::id_type compute (naming::id_type id1, naming::id_type id2)
+{  
+
+    components::access_memory_block<data> val1(
+                components::stubs::memory_block::checkout(id1));
+
+    components::access_memory_block<data> val2(
+                components::stubs::memory_block::checkout(id2));
+
+    naming::id_type here = applier::get_applier().get_runtime_support_gid();
+    int locality = get_prefix_from_id( here );
+
+    if ( locality == 1 ) {
+      val1->val_ += 1.0 + val1->val_ + val2->val_;
+      val1->x_[0] = 2 + val1->x_[0] + val2->x_[0];
+      val1->x_[1] = 3 + val1->x_[1] + val2->x_[1];
+      val1->x_[2] = 4 + val1->x_[2] + val2->x_[2];
+      val1->proceed_ = true;
+      return id1;
+    } else {
+      val2->val_ -= 3.0 + val1->val_ + val2->val_;
+      val2->x_[0] = 20 + val1->x_[0] + val2->x_[0];
+      val2->x_[1] = 30 + val1->x_[1] + val2->x_[1];
+      val2->x_[2] = 40 + val1->x_[2] + val2->x_[2];
+      val2->proceed_ = false;
+      return id2;
+    }
 }
 
