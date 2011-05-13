720183bcd4e536b0ad576eb1249bd3b54b1de056
Matthew Anderson <manderson@cct.lsu.edu>
2011-05-13 22:03:53 +0000

    update
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4189 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/marduk/hadcc/test.cpp
+++ examples/marduk/hadcc/test.cpp
@@ -29,6 +29,10 @@ struct Globals
   double maxz0;
   double h;
   int numprocs;
+  int clusterstyle;
+  double minefficiency;
+  int mindim;
+  int refine_factor;
   std::vector<double> refine_level;
   std::vector<int> gr_sibling;
   std::vector<double> gr_t;
@@ -38,6 +42,9 @@ struct Globals
   std::vector<double> gr_maxx;
   std::vector<double> gr_maxy;
   std::vector<double> gr_maxz;
+  std::vector<int> gr_nx;
+  std::vector<int> gr_ny;
+  std::vector<int> gr_nz;
   std::vector<int> gr_proc;
   std::vector<double> gr_h;
   std::vector<int> gr_alive;
@@ -89,8 +96,18 @@ int level_return_start(int level,Globals &par);
 int grid_find_bounds(int gi,double &minx,double &maxx,
                             double &miny,double &maxy,
                             double &minz,double &maxz,Globals &par);
+int compute_error(std::vector<double> &error,int nx0, int ny0, int nz0,
+                                double minx0,double miny0,double minz0,double h);
+int level_mkall_dead(int level,Globals &par);
+int level_combine(std::vector<double> &error, std::vector<double> &localerror,
+                  int mini,int minj,int mink,
+                  int nxl,int nyl,int nzl,
+                  int nx,int ny,int nz);
 
 const int maxlevels = 25;
+const int ALIVE = 1;
+const int DEAD = 0;
+const int PENDING = -1;
 
 int main(int argc,char* argv[]) {
 
@@ -108,6 +125,10 @@ int main(int argc,char* argv[]) {
   int shadow = 0;
   double minx0,maxx0,miny0,maxy0,minz0,maxz0;
   double ethreshold = 1.e-4;
+  int clusterstyle = 0;
+  double minefficiency = 0.9;
+  int mindim = 6;
+  int refine_factor = 2;
   minx0 = -4.0;
   maxx0 =  4.0;
   miny0 = -4.0;
@@ -158,7 +179,18 @@ int main(int argc,char* argv[]) {
   if ( GetInt(list, "shadow", &shadow) == 0) {
     std::cerr << " Parameter shadow not found, using default " << std::endl;
   } 
-
+  if ( GetInt(list, "clusterstyle", &clusterstyle) == 0) {
+    std::cerr << " Parameter clusterstyle not found, using default " << std::endl;
+  } 
+  if ( GetInt(list, "mindim", &mindim) == 0) {
+    std::cerr << " Parameter mindim not found, using default " << std::endl;
+  } 
+  if ( GetInt(list, "refine_factor", &refine_factor) == 0) {
+    std::cerr << " Parameter refine_factor not found, using default " << std::endl;
+  } 
+  if ( GetDouble(list, "minefficiency", &minefficiency) == 0) {
+    std::cerr << " Parameter minefficiency not found, using default " << std::endl;
+  }
   if ( GetDouble(list, "maxx0", &maxx0) == 0) {
     std::cerr << " Parameter maxx0 not found, using default " << std::endl;
   }
@@ -209,7 +241,11 @@ int main(int argc,char* argv[]) {
   std::cout << " maxz0        : " <<  maxz0 << std::endl;
   std::cout << " minz0        : " <<  minz0 << std::endl;
   std::cout << " ghostwidth   : " <<  ghostwidth << std::endl;
-  std::cout << " bound_width   : " <<  bound_width << std::endl;
+  std::cout << " bound_width  : " <<  bound_width << std::endl;
+  std::cout << " clusterstyle : " <<  clusterstyle << std::endl;
+  std::cout << " minefficiency: " <<  minefficiency<< std::endl;
+  std::cout << " mindim       : " <<  mindim << std::endl;
+  std::cout << " refine_factor: " <<  refine_factor << std::endl;
   for (int i=0;i<allowedl;i++) {
     char tmpname[80];
     std::cout << " refine_level_" << i << " : " << par.refine_level[i] << std::endl;
@@ -230,6 +266,10 @@ int main(int argc,char* argv[]) {
   par.minz0 = minz0;
   par.numprocs = numprocs;
   par.shadow = shadow;
+  par.clusterstyle = clusterstyle;
+  par.mindim = mindim;
+  par.minefficiency = minefficiency;
+  par.refine_factor = refine_factor;
 
   // derived parameters
   double hx = (maxx0 - minx0)/(nx0-1);
@@ -246,14 +286,20 @@ int main(int argc,char* argv[]) {
   }
   par.h = h;
 
+  // memory allocation
+  int maxgids = 1000;
+  par.levelp.resize(par.allowedl);
+  par.levelp[0] = 0; 
+
   int rc = level_refine(-1,par);
+
+  rc = level_refine(0,par);
 
   return 0;
 }
 
 int level_refine(int level,Globals & par) 
 {
-
   int rc;
   int numprocs = par.numprocs;
   int ghostwidth = par.ghostwidth;
@@ -269,6 +315,18 @@ int level_refine(int level,Globals & par)
   double maxy0 = par.maxy0;
   double maxz0 = par.maxz0;
   double h = par.h;
+  int clusterstyle = par.clusterstyle;
+  double minefficiency = par.minefficiency;
+  int mindim = par.mindim;
+  int refine_factor = par.refine_factor;
+
+  // local vars
+  std::vector<int> b_minx,b_maxx,b_miny,b_maxy,b_minz,b_maxz;
+  std::vector<double> tmp_mini,tmp_maxi,tmp_minj,tmp_maxj,tmp_mink,tmp_maxk;
+  int numbox;
+
+  // hard coded parameters -- eventually to be made into full parameters
+  int maxbboxsize = 1000;
 
   if ( level == par.allowedl ) { 
     return 0;
@@ -302,76 +360,188 @@ int level_refine(int level,Globals & par)
     time = par.gr_t[gi];
   }
 
-  std::vector<double> error,flag; 
-  error.resize(nx0*ny0*nz0);
-  flag.resize(nx0*ny0*nz0);
+  int nxl   =  (int) ((maxx-minx)/hl+0.5);
+  int nyl   =  (int) ((maxy-miny)/hl+0.5);
+  int nzl   =  (int) ((maxz-minz)/hl+0.5);
+  nxl++;
+  nyl++;
+  nzl++;
 
-  // initialize some positive error
-  for (int k=0;k<nz0;k++) {
-    double z = minz0 + k*h;
-  for (int j=0;j<ny0;j++) {
-    double y = miny0 + j*h;
-  for (int i=0;i<nx0;i++) {
-    error[i+nx0*(j+ny0*k)] = 0.0;
-    double x = minx0 + i*h + 2.25;
-    double r = sqrt(x*x+y*y+z*z);
-    if ( pow(r-1.0,2) <= 0.5*0.5 && r > 0 ) {
-      error[i+nx0*(j+ny0*k)] = pow((r-1.0)*(r-1.0)-0.5*0.5,3)*8.0*(1.0-r)/pow(0.5,8)/r;
-      if ( error[i+nx0*(j+ny0*k)] < 0.0 ) error[i+nx0*(j+ny0*k)] = 0.0;
-    } 
+  //std::cout << " TEST nls: " << nxl << " " << nyl << " " << nzl << std::endl;
+  //std::cout << " TEST min/max: " << minx << " " << maxx << std::endl;
+  //std::cout << "               " << miny << " " << maxy << std::endl;
+  //std::cout << "               " << minz << " " << maxz << std::endl;
 
-    x = minx0 + i*h - 2.25;
-    r = sqrt(x*x+y*y+z*z);
-    if ( pow(r-1.0,2) <= 0.5*0.5 && r > 0 ) {
-      error[i+nx0*(j+ny0*k)] = pow((r-1.0)*(r-1.0)-0.5*0.5,3)*8.0*(1.0-r)/pow(0.5,8)/r;
-      if ( error[i+nx0*(j+ny0*k)] < 0.0 ) error[i+nx0*(j+ny0*k)] = 0.0;
+  std::vector<double> error,localerror,flag; 
+  error.resize(nxl*nyl*nzl);
+  flag.resize(nxl*nyl*nzl);
+
+  if ( level == -1 ) {
+    b_minx.resize(maxbboxsize);
+    b_maxx.resize(maxbboxsize);
+    b_miny.resize(maxbboxsize);
+    b_maxy.resize(maxbboxsize);
+    b_minz.resize(maxbboxsize);
+    b_maxz.resize(maxbboxsize);
+
+    tmp_mini.resize(maxbboxsize);
+    tmp_maxi.resize(maxbboxsize);
+    tmp_minj.resize(maxbboxsize);
+    tmp_maxj.resize(maxbboxsize);
+    tmp_mink.resize(maxbboxsize);
+    tmp_maxk.resize(maxbboxsize);
+
+    int numbox = 1;
+    b_minx[0] = 1;
+    b_maxx[0] = nxl;
+    b_miny[0] = 1;
+    b_maxy[0] = nyl;
+    b_minz[0] = 1;
+    b_maxz[0] = nzl;
+    FNAME(level_clusterdd)(&*tmp_mini.begin(),&*tmp_maxi.begin(),
+                           &*tmp_minj.begin(),&*tmp_maxj.begin(),
+                           &*tmp_mink.begin(),&*tmp_maxk.begin(),
+                           &*b_minx.begin(),&*b_maxx.begin(),
+                           &*b_miny.begin(),&*b_maxy.begin(),
+                           &*b_minz.begin(),&*b_maxz.begin(),
+                           &numbox,&numprocs,&maxbboxsize,
+                           &ghostwidth,&refine_factor,&mindim,
+                           &bound_width);
+
+    std::cout << " numbox post DD " << numbox << std::endl;
+    for (int i=0;i<numbox;i++) {
+      if (i == numbox-1 ) {
+        par.gr_sibling.push_back(-1);
+      } else {
+        par.gr_sibling.push_back(i+1);
+      }
+
+      int nx = b_maxx[i] - b_minx[i]+1;
+      int ny = b_maxy[i] - b_miny[i]+1;
+      int nz = b_maxz[i] - b_minz[i]+1;
+
+      double lminx = minx + (b_minx[i]-1)*hl;
+      double lminy = miny + (b_miny[i]-1)*hl;
+      double lminz = minz + (b_minz[i]-1)*hl;
+      double lmaxx = minx + (b_maxx[i]-1)*hl;
+      double lmaxy = miny + (b_maxy[i]-1)*hl;
+      double lmaxz = minz + (b_maxz[i]-1)*hl;
+
+      par.gr_t.push_back(time);
+      par.gr_minx.push_back(lminx);
+      par.gr_miny.push_back(lminy);
+      par.gr_minz.push_back(lminz);
+      par.gr_maxx.push_back(lmaxx);
+      par.gr_maxy.push_back(lmaxy);
+      par.gr_maxz.push_back(lmaxz);
+      par.gr_proc.push_back(i);
+      par.gr_h.push_back(hl);
+      par.gr_alive.push_back(0);
+      par.gr_nx.push_back(nx);
+      par.gr_ny.push_back(ny);
+      par.gr_nz.push_back(nz);
+    //  std::cout << " bbox: " << b_minx[i] << " " << b_maxx[i] << std::endl;
+    //  std::cout << "       " << b_miny[i] << " " << b_maxy[i] << std::endl;
+    //  std::cout << "       " << b_minz[i] << " " << b_maxz[i] << std::endl;
+
+    //  std::cout << " minx " << lminx << " maxx " << lmaxx << std::endl;
+    //  std::cout << " miny " << lminy << " maxy " << lmaxy << std::endl;
+    //  std::cout << " minz " << lminz << " maxz " << lmaxz << std::endl;
+    //  std::cout << " nx " << nx << " test " << lminx + (nx-1)*hl << std::endl;
+    //  std::cout << " ny " << nx << "      " << lminy + (ny-1)*hl << std::endl;
+    //  std::cout << " nz " << nx << "      " << lminz + (nz-1)*hl << std::endl;
     }
+   
+    return 0;
+  } else {
+    // loop over all grids in level and get its error data
+    gi = level_return_start(level,par);
 
-  } } }
+    while ( grid_return_existence(gi,par) ) {
+      int nx = par.gr_nx[gi];
+      int ny = par.gr_ny[gi];
+      int nz = par.gr_nz[gi];
 
-  double scalar = par.refine_level[0];
+      localerror.resize(nx*ny*nz);
+
+      double lminx = par.gr_minx[gi];
+      double lminy = par.gr_miny[gi];
+      double lminz = par.gr_minz[gi];
+      double lmaxx = par.gr_maxx[gi];
+      double lmaxy = par.gr_maxy[gi];
+      double lmaxz = par.gr_maxz[gi];
+
+      rc = compute_error(localerror,nx,ny,nz,
+                         lminx,lminy,lminz,par.gr_h[gi]);
+
+      int mini = (int) ((lminx - minx)/hl+0.5);
+      int minj = (int) ((lminy - miny)/hl+0.5);
+      int mink = (int) ((lminz - minz)/hl+0.5);
+
+      // sanity check
+      if ( floatcmp(lminx-(minx + mini*hl),0.0) == 0 ||
+           floatcmp(lminy-(miny + minj*hl),0.0) == 0 ||
+           floatcmp(lminz-(minz + mink*hl),0.0) == 0 ||
+           floatcmp(lmaxx-(minz + (mini+nx-1)*hl),0.0) == 0 ||
+           floatcmp(lmaxy-(minz + (minj+ny-1)*hl),0.0) == 0 ||
+           floatcmp(lmaxz-(minz + (mink+nz-1)*hl),0.0) == 0 ) {
+        std::cerr << " Index problem " << std::endl;
+        std::cerr << " lminx " << lminx << " " << minx + mini*hl << std::endl;
+        std::cerr << " lminy " << lminy << " " << miny + minj*hl << std::endl;
+        std::cerr << " lminz " << lminz << " " << minz + mink*hl << std::endl;
+        std::cerr << " lmaxx " << lminx << " " << minx + (mini+nx-1)*hl << std::endl;
+        std::cerr << " lmaxy " << lminy << " " << miny + (minj+ny-1)*hl << std::endl;
+        std::cerr << " lmaxz " << lminz << " " << minz + (mink+nz-1)*hl << std::endl;
+        exit(0);
+      }
+
+      rc = level_combine(error,localerror,
+                         mini,minj,mink,
+                         nxl,nyl,nzl,
+                         nx,ny,nz);
+
+      gi = par.gr_sibling[gi];
+    }
+  }
+
+  rc = level_mkall_dead(level+1,par);
+
+  double scalar = par.refine_level[level];
   FNAME(load_scal_mult3d)(&*error.begin(),&*error.begin(),&scalar,&nx0,&ny0,&nz0);
   FNAME(level_makeflag)(&*flag.begin(),&*error.begin(),&level,
-                                                 &minx0,&miny0,&minz0,&h,
-                                                 &nx0,&ny0,&nz0,&ethreshold);
+                                                 &minx,&miny,&minz,&h,
+                                                 &nxl,&nyl,&nzl,&ethreshold);
   {
     int shape[3];
     std::vector<double> coord;
-    coord.resize(nx0+ny0+nz0);
-    for (int i=0;i<nx0;i++) {
-      coord[i] = minx0 + i*h; 
+    coord.resize(nxl+nyl+nzl);
+    for (int i=0;i<nxl;i++) {
+      coord[i] = minx + i*h; 
     }
-    for (int i=0;i<ny0;i++) {
-      coord[i+nx0] = miny0 + i*h; 
+    for (int i=0;i<nyl;i++) {
+      coord[i+nxl] = miny + i*h; 
     }
-    for (int i=0;i<nz0;i++) {
-      coord[i+nx0+ny0] = minz0 + i*h; 
+    for (int i=0;i<nzl;i++) {
+      coord[i+nxl+nyl] = minz + i*h; 
     }
     
-   shape[0] = nx0;
-   shape[1] = ny0;
-   shape[2] = nz0;
-   gft_out_full("flag",0.0,shape,"x|y|z", 3,&*coord.begin(),&*flag.begin());
+    shape[0] = nxl;
+    shape[1] = nyl;
+    shape[2] = nzl;
+    gft_out_full("flag",0.0,shape,"x|y|z", 3,&*coord.begin(),&*flag.begin());
   }
 
   // level_cluster
-  int numbox = 1;
-  int clusterstyle = 0;
-  double minefficiency = .9;
-  int mindim = 6;
-  int refine_factor = 2;
   std::vector<double> sigi,sigj,sigk;
   std::vector<double> asigi,asigj,asigk;
   std::vector<double> lapi,lapj,lapk;
   std::vector<double> alapi,alapj,alapk;
-  std::vector<int> b_minx,b_maxx,b_miny,b_maxy,b_minz,b_maxz;
 
    sigi.resize(nx0); sigj.resize(ny0); sigk.resize(nz0);
   asigi.resize(nx0);asigj.resize(ny0);asigk.resize(nz0);
    lapi.resize(nx0); lapj.resize(ny0); lapk.resize(nz0);
   alapi.resize(nx0);alapj.resize(ny0);alapk.resize(nz0);
  
-  int maxbboxsize = 1000;
   b_minx.resize(maxbboxsize);
   b_maxx.resize(maxbboxsize);
   b_miny.resize(maxbboxsize);
@@ -403,7 +573,6 @@ int level_refine(int level,Globals & par)
   }
 
   // Take the set of subgrids to create and then output the same set but domain decomposed
-  std::vector<double> tmp_mini,tmp_maxi,tmp_minj,tmp_maxj,tmp_mink,tmp_maxk;
   tmp_mini.resize(maxbboxsize);
   tmp_maxi.resize(maxbboxsize);
   tmp_minj.resize(maxbboxsize);
@@ -481,7 +650,7 @@ int level_find_bounds(int level, double &minx, double &maxx,
 
 int grid_return_existence(int gridnum,Globals &par)
 {
-  if ( (gridnum > 0 ) && (gridnum < par.gr_minx.size()) ) {
+  if ( (gridnum >= 0 ) && (gridnum < par.gr_minx.size()) ) {
     return 1;
   } else {
     return 0;
@@ -512,4 +681,65 @@ int level_return_start(int level,Globals &par)
   }
 }
 
+int level_mkall_dead(int level,Globals &par)
+{
+  int rc;
+  // Find the beginning of level
+  int gi = level_return_start(level,par);
+
+  while ( grid_return_existence(gi,par) ) {
+    par.gr_alive[gi] = DEAD;
+    gi = par.gr_sibling[gi];
+  }
+
+  return 0;
+}
+
+int compute_error(std::vector<double> &error,int nx0, int ny0, int nz0,
+                                double minx0,double miny0,double minz0,double h)
+{
+    // initialize some positive error
+    for (int k=0;k<nz0;k++) {
+      double z = minz0 + k*h;
+    for (int j=0;j<ny0;j++) {
+      double y = miny0 + j*h;
+    for (int i=0;i<nx0;i++) {
+      error[i+nx0*(j+ny0*k)] = 0.0;
+      double x = minx0 + i*h + 2.25;
+      double r = sqrt(x*x+y*y+z*z);
+      if ( pow(r-1.0,2) <= 0.5*0.5 && r > 0 ) {
+        error[i+nx0*(j+ny0*k)] = pow((r-1.0)*(r-1.0)-0.5*0.5,3)*8.0*(1.0-r)/pow(0.5,8)/r;
+        if ( error[i+nx0*(j+ny0*k)] < 0.0 ) error[i+nx0*(j+ny0*k)] = 0.0;
+      } 
+
+      x = minx0 + i*h - 2.25;
+      r = sqrt(x*x+y*y+z*z);
+      if ( pow(r-1.0,2) <= 0.5*0.5 && r > 0 ) {
+        error[i+nx0*(j+ny0*k)] = pow((r-1.0)*(r-1.0)-0.5*0.5,3)*8.0*(1.0-r)/pow(0.5,8)/r;
+        if ( error[i+nx0*(j+ny0*k)] < 0.0 ) error[i+nx0*(j+ny0*k)] = 0.0;
+      }
+
+    } } }
+}
+
+int level_combine(std::vector<double> &error, std::vector<double> &localerror,
+                  int mini,int minj,int mink,
+                  int nxl,int nyl,int nzl,
+                  int nx,int ny,int nz)
+{
+  int il,jl,kl;
+  // combine local grid error into global grid error
+  for (int k=0;k<nx;k++) {
+    kl = k + mink;    
+  for (int j=0;j<ny;j++) {
+    jl = j + minj;    
+  for (int i=0;i<nx;i++) {
+    il = i + mini;    
+    if ( error[il + nxl*(jl + nyl*kl)] < localerror[i+nx*(j+ny*k)] ) {
+      error[il + nxl*(jl + nyl*kl)] = localerror[i+nx*(j+ny*k)];
+    }
+  } } }
+  
+  return 0;
+}
 
