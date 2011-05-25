f16412abb5b996841916389273bb68d745fb4962
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-05-25 15:57:41 +0000

    [2/10, actions] Explicitly add includes that are implicit with AGAS v1.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4252 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/actions/manage_object_action.hpp
+++ hpx/runtime/actions/manage_object_action.hpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2007-2011 Hartmut Kaiser
+//  Copyright (c)      2011 Bryce Lelbach
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,6 +12,8 @@
 #include <boost/serialization/serialization.hpp>
 
 #include <hpx/config/warnings_prefix.hpp>
+#include <hpx/util/portable_binary_oarchive.hpp>
+#include <hpx/util/portable_binary_iarchive.hpp>
 #include <hpx/runtime/actions/action_support.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
