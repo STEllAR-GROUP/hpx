1fbf17e8f1f8a4208549562f153cc8020383ce6d
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-05-25 15:57:11 +0000

    [0/10, util] Move unique_ids::get_id into a source file, add an overload for get_id that accepts a pointer to a naming::resolver_client.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4250 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/util/generate_unique_ids.hpp
+++ hpx/util/generate_unique_ids.hpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2007-2011 Hartmut Kaiser
+//  Copyright (c)      2011 Bryce Lelbach
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,8 +11,8 @@
 #include <boost/assert.hpp>
 #include <boost/cstdint.hpp>
 
+#include <hpx/hpx_fwd.hpp>
 #include <hpx/config.hpp>
-#include <hpx/runtime/naming/resolver_client.hpp>
 #include <hpx/util/spinlock_pool.hpp>
 #include <hpx/util/unlock_lock.hpp>
 
@@ -35,32 +36,10 @@ namespace hpx { namespace util
 
         /// Generate next unique component id
         naming::gid_type get_id(naming::locality const& here,
-            naming::resolver_client const& resolver, std::size_t count = 1)
-        {
-            // create a new id
-            mutex_type::scoped_lock l(this);
-
-            // ensure next_id doesn't overflow
-            if (lower_ + count > upper_) 
-            {
-                naming::gid_type lower;
-                naming::gid_type upper;
-
-                {
-                    unlock_the_lock<mutex_type::scoped_lock> ul(l);
-                    resolver.get_id_range(here, 
-                        (std::max)(std::size_t(range_delta), count), 
-                        lower, upper);
-                }
-
-                lower_ = lower;
-                upper_ = upper;
-            }
-
-            naming::gid_type result = lower_;
-            lower_ += count;
-            return result;
-        }
+            naming::resolver_client& resolver, std::size_t count = 1);
+        
+        naming::gid_type get_id(naming::locality const& here,
+            naming::resolver_client* resolver, std::size_t count = 1);
 
     private:
         /// The range of available ids for components
