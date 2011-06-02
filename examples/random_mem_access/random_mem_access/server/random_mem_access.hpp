574c5e4a73daad5eecc2ac253bb781895e976721
Matthew Anderson <manderson@cct.lsu.edu>
2011-06-02 20:08:36 +0000

    atomic random array element access
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4344 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/random_mem_access/random_mem_access/server/random_mem_access.hpp
@@ -0,0 +1,118 @@
+//  Copyright (c) 2011 Matt Anderson
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_COMPONENTS_SERVER_SIMPLE_ACCUMULATOR_JUL_18_2008_1154AM)
+#define HPX_COMPONENTS_SERVER_SIMPLE_ACCUMULATOR_JUL_18_2008_1154AM
+
+#include <iostream>
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/applier/applier.hpp>
+#include <hpx/runtime/threads/thread.hpp>
+#include <hpx/runtime/actions/component_action.hpp>
+#include <hpx/runtime/components/component_type.hpp>
+#include <hpx/runtime/components/server/simple_component_base.hpp>
+#include <boost/thread/locks.hpp>
+
+namespace hpx { namespace components { namespace server 
+{
+    ///////////////////////////////////////////////////////////////////////////
+    /// \class random_mem_access random_mem_access.hpp hpx/components/random_mem_access.hpp
+    ///
+    /// The random_mem_access is a small example components demonstrating 
+    /// the main principles of writing your own components. It exposes 4 
+    /// different actions: init, add, query, and print, showing how to used and 
+    /// implement functionality in a way conformant with the HPX runtime system. 
+    /// The random_mem_access is a very simple example of an HPX component. 
+    ///
+    /// Note that the implementation of the random_mem_access does not require 
+    /// special data members or virtual functions. All specifics are embedded 
+    /// in the random_mem_access_base class the random_mem_access is derived 
+    /// from.
+    ///
+    class random_mem_access 
+      : public simple_component_base<random_mem_access>
+    {
+    public:
+        // parcel action code: the action to be performed on the destination 
+        // object (the accumulator)
+        enum actions
+        {
+            random_mem_access_init = 0,
+            random_mem_access_add = 1,
+            random_mem_access_query_value = 2,
+            random_mem_access_print = 3
+        };
+
+        // constructor: initialize random_mem_access value
+        random_mem_access()
+          : arg_(0)
+        {}
+
+        ///////////////////////////////////////////////////////////////////////
+        // exposed functionality of this component
+
+        /// Initialize the accumulator
+        void init(int i) 
+        {
+            hpx::lcos::mutex::scoped_lock l(mtx_);
+            arg_ = i;
+            arg_init_ = i;
+        }
+
+        /// Add the given number to the accumulator
+        void add() 
+        {
+            hpx::lcos::mutex::scoped_lock l(mtx_);
+            arg_ += 1;
+        }
+
+        /// Return the current value to the caller
+        int query() 
+        {
+            hpx::lcos::mutex::scoped_lock l(mtx_);
+            return arg_;
+        }
+
+        /// Print the current value of the accumulator
+        void print() 
+        {
+            hpx::lcos::mutex::scoped_lock l(mtx_);
+            std::cout << " I started as " << arg_init_ << " and I finished as " << arg_ << std::flush << std::endl;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        // Each of the exposed functions needs to be encapsulated into an action
+        // type, allowing to generate all required boilerplate code for threads,
+        // serialization, etc.
+        typedef hpx::actions::action1<
+            random_mem_access, random_mem_access_init,int, 
+            &random_mem_access::init
+        > init_action;
+
+        typedef hpx::actions::action0<
+            random_mem_access, random_mem_access_add, 
+            &random_mem_access::add
+        > add_action;
+
+        typedef hpx::actions::result_action0<
+            random_mem_access, int, random_mem_access_query_value, 
+            &random_mem_access::query
+        > query_action;
+
+        typedef hpx::actions::action0<
+            random_mem_access, random_mem_access_print, 
+            &random_mem_access::print
+        > print_action;
+
+    private:
+        int arg_;
+        int arg_init_;
+        hpx::lcos::mutex mtx_;
+    };
+
+}}}
+
+#endif
---
+++ examples/random_mem_access/random_mem_access_client.cpp
@@ -0,0 +1,76 @@
+//  Copyright (c)      2011 Matt Anderson
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+#include <time.h>
+
+#include "random_mem_access/random_mem_access.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+int hpx_main(boost::program_options::variables_map&)
+{
+    // get list of all known localities
+    std::vector<hpx::naming::id_type> prefixes;
+    hpx::applier::applier& appl = hpx::applier::get_applier();
+    hpx::naming::id_type prefix;
+
+    if (appl.get_remote_prefixes(prefixes))
+        // create random_mem_access on any of the remote localities
+        prefix = prefixes[0];
+    else
+        // create an accumulator locally
+        prefix = appl.get_runtime_support_gid();
+
+    std::vector< hpx::components::random_mem_access > accu;
+
+    int array_size = 6;
+    accu.resize(array_size);
+
+    for (int i=0;i<array_size;i++) {
+      accu[i].create(prefix);
+    }
+
+    // initialize the array
+    for (int i=0;i<array_size;i++) {
+      accu[i].init(i); 
+    }
+
+    srand( time(NULL) );
+
+    int N = 10;
+    std::vector<hpx::lcos::future_value<void> > barrier;
+    for (int i=0;i<N;i++) {
+      int rn = rand() % array_size;
+      std::cout << " Random element access: " << rn << std::endl;
+      barrier.push_back(accu[rn].add_async());
+    }
+
+    hpx::components::wait(barrier);
+
+    std::vector<hpx::lcos::future_value<void> > barrier2;
+    for (int i=0;i<array_size;i++) {
+      barrier2.push_back(accu[i].print_async()); 
+    }
+
+    hpx::components::wait(barrier2);
+
+    // initiate shutdown of the runtime systems on all localities
+    hpx::finalize();
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    // Configure application-specific options
+    boost::program_options::options_description
+       desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
+
+    // Initialize and run HPX
+    return hpx::init(desc_commandline, argc, argv);
+}
+
