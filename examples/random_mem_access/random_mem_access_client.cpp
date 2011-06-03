a54b712bb8034df15b8be08a452532b899bd3346
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-06-03 18:32:20 +0000

    Make sure objects are really out of scope before tearing down the application
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4369 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/random_mem_access/random_mem_access_client.cpp
+++ examples/random_mem_access/random_mem_access_client.cpp
@@ -12,50 +12,52 @@
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(boost::program_options::variables_map&)
 {
-    // get list of all known localities
-    std::vector<hpx::naming::id_type> prefixes;
-    hpx::applier::applier& appl = hpx::applier::get_applier();
-    hpx::naming::id_type prefix;
+    {
+        // get list of all known localities
+        std::vector<hpx::naming::id_type> prefixes;
+        hpx::applier::applier& appl = hpx::applier::get_applier();
+        hpx::naming::id_type prefix;
 
-    if (appl.get_remote_prefixes(prefixes))
-        // create random_mem_access on any of the remote localities
-        prefix = prefixes[0];
-    else
-        // create an accumulator locally
-        prefix = appl.get_runtime_support_gid();
+        if (appl.get_remote_prefixes(prefixes))
+            // create random_mem_access on any of the remote localities
+            prefix = prefixes[0];
+        else
+            // create an accumulator locally
+            prefix = appl.get_runtime_support_gid();
 
-    std::vector< hpx::components::random_mem_access > accu;
+        std::vector< hpx::components::random_mem_access > accu;
 
-    int array_size = 6;
-    accu.resize(array_size);
+        int array_size = 6;
+        accu.resize(array_size);
 
-    for (int i=0;i<array_size;i++) {
-      accu[i].create(prefix);
+        for (int i=0;i<array_size;i++) {
+          accu[i].create(prefix);
+        }
+
+        // initialize the array
+        for (int i=0;i<array_size;i++) {
+          accu[i].init(i); 
+        }
+
+        srand( time(NULL) );
+
+        int N = 10;
+        std::vector<hpx::lcos::future_value<void> > barrier;
+        for (int i=0;i<N;i++) {
+          int rn = rand() % array_size;
+          std::cout << " Random element access: " << rn << std::endl;
+          barrier.push_back(accu[rn].add_async());
+        }
+
+        hpx::components::wait(barrier);
+
+        std::vector<hpx::lcos::future_value<void> > barrier2;
+        for (int i=0;i<array_size;i++) {
+          barrier2.push_back(accu[i].print_async()); 
+        }
+
+        hpx::components::wait(barrier2);
     }
-
-    // initialize the array
-    for (int i=0;i<array_size;i++) {
-      accu[i].init(i); 
-    }
-
-    srand( time(NULL) );
-
-    int N = 10;
-    std::vector<hpx::lcos::future_value<void> > barrier;
-    for (int i=0;i<N;i++) {
-      int rn = rand() % array_size;
-      std::cout << " Random element access: " << rn << std::endl;
-      barrier.push_back(accu[rn].add_async());
-    }
-
-    hpx::components::wait(barrier);
-
-    std::vector<hpx::lcos::future_value<void> > barrier2;
-    for (int i=0;i<array_size;i++) {
-      barrier2.push_back(accu[i].print_async()); 
-    }
-
-    hpx::components::wait(barrier2);
 
     // initiate shutdown of the runtime systems on all localities
     hpx::finalize();
