84e12b947f227d845ffc97601bcfe644e50c3dca
Matthew Anderson <manderson@cct.lsu.edu>
2011-06-06 15:29:05 +0000

    towards adding distributing factory
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4407 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/random_mem_access/random_mem_access_client.cpp
+++ examples/random_mem_access/random_mem_access_client.cpp
@@ -1,3 +1,4 @@
+//  Copyright (c)      2011 Hartmut Kaiser
 //  Copyright (c)      2011 Matt Anderson
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
@@ -14,25 +15,36 @@ int hpx_main(boost::program_options::variables_map&)
 {
     {
         // get list of all known localities
-        std::vector<hpx::naming::id_type> prefixes;
-        hpx::applier::applier& appl = hpx::applier::get_applier();
-        hpx::naming::id_type prefix;
+        //std::vector<hpx::naming::id_type> prefixes;
+        //hpx::applier::applier& appl = hpx::applier::get_applier();
+        //hpx::naming::id_type prefix;
 
-        if (appl.get_remote_prefixes(prefixes))
-            // create random_mem_access on any of the remote localities
-            prefix = prefixes[0];
-        else
-            // create an accumulator locally
-            prefix = appl.get_runtime_support_gid();
+        // create a distributing factory locally
+        components::distributing_factory factory;
+        factory.create(hpx::applier::get_applier().get_runtime_support_gid());
 
-        std::vector< hpx::components::random_mem_access > accu;
+        component_type function_type = get_component_type<hpx::components::random_mem_access>();
 
         int array_size = 6;
-        accu.resize(array_size);
+        result_type functions = factory.create_components(function_type, array_size);
 
-        for (int i=0;i<array_size;i++) {
-          accu[i].create(prefix);
-        }
+        //if (appl.get_remote_prefixes(prefixes))
+        //    // create random_mem_access on any of the remote localities
+        //    prefix = prefixes[0];
+        //else
+        //    // create an accumulator locally
+        //    prefix = appl.get_runtime_support_gid();
+
+        //std::vector< hpx::components::random_mem_access > accu;
+
+        //int array_size = 6;
+        //accu.resize(array_size);
+
+        //for (int i=0;i<array_size;i++) {
+        //  accu[i].create(prefix);
+        //}
+
+        init(locality_results(functions), numsteps);
 
         // initialize the array
         for (int i=0;i<array_size;i++) {
