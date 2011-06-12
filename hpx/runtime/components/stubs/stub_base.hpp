7384f6f0b4a235a6cba12fb0dadb4dbceada801f
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-06-12 05:18:19 +0000

    Put stub_base into hpx::components and make hpx::components::stubs::stub_base an alias for it for backwards compatibility.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4535 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/components/stubs/stub_base.hpp
+++ hpx/runtime/components/stubs/stub_base.hpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2007-2011 Hartmut Kaiser
+//  Copyright (c)      2011 Bryce Lelbach
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,7 +11,7 @@
 #include <hpx/runtime/components/stubs/runtime_support.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
-namespace hpx { namespace components { namespace stubs
+namespace hpx { namespace components
 {
     ///////////////////////////////////////////////////////////////////////////
     template <typename ServerComponent>
@@ -112,7 +113,6 @@ namespace hpx { namespace components { namespace stubs
                 get_component_type<ServerComponent>(), arg0);
         }
 
-        /// Create a new instance of an simple_accumulator
         template <typename Arg0>
         static naming::id_type
         create_one_sync(naming::gid_type const& gid, component_type type, Arg0 const& arg0)
@@ -168,7 +168,14 @@ namespace hpx { namespace components { namespace stubs
             gid = naming::invalid_id;
         }
     };
-}}}
+
+    ///////////////////////////////////////////////////////////////////////////
+    namespace stubs
+    {
+          // for backwards compatibility only
+          using components::stub_base;
+    }
+}}
 
 #endif
 
