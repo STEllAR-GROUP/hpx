9d7242842e0f69c34fe62009a17db5e753a27263
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-06-12 15:05:36 +0000

    Update hello world iostreams example.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4538 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/iostreams/hello_world.cpp
+++ examples/iostreams/hello_world.cpp
@@ -5,12 +5,18 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <vector>
+#include <list>
 #include <iostream>
 
 #include <boost/ref.hpp>
+#include <boost/foreach.hpp>
 
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
+#include <hpx/lcos/eager_future.hpp>
+#include <hpx/runtime/actions/plain_action.hpp>
+#include <hpx/runtime/components/plain_component_factory.hpp>
 #include <hpx/runtime/components/server/manage_component.hpp>
 #include <hpx/components/iostreams/lazy_ostream.hpp>
 
@@ -21,6 +27,15 @@ using hpx::init;
 using hpx::finalize;
 
 using hpx::naming::id_type;
+using hpx::naming::get_gid_from_prefix;
+
+using hpx::applier::get_prefix_id;
+using hpx::applier::get_applier;
+
+using hpx::actions::plain_action1;
+
+using hpx::lcos::future_value;
+using hpx::lcos::eager_future;
 
 using hpx::components::server::create_one;
 using hpx::components::managed_component;
@@ -31,15 +46,38 @@ using hpx::iostreams::server::output_stream;
 
 typedef managed_component<output_stream> ostream_type;
 
+void hello_world(id_type cout_gid)
+{
+    lazy_ostream hpx_cout(cout_gid);
+    hpx_cout << "[L" << get_prefix_id() << "]: hello world!" << endl;
+}
+
+typedef plain_action1<id_type, hello_world> hello_world_action;
+
+HPX_REGISTER_PLAIN_ACTION(hello_world_action);
+
+typedef eager_future<hello_world_action> hello_world_future;
+
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(variables_map&)
 {
     {
-        lazy_ostream hpx_cout
-            (id_type(create_one<ostream_type>(boost::ref(std::cout))
-                   , id_type::managed));
+        id_type cout_gid(create_one<ostream_type>(boost::ref(std::cout))
+                       , id_type::managed);
 
-        hpx_cout << "hello world!" << endl;
+        lazy_ostream hpx_cout(cout_gid);
+
+        std::list<future_value<void> > futures;
+        std::vector<id_type> prefixes;
+
+        get_applier().get_prefixes(prefixes);
+
+        BOOST_FOREACH(id_type const& node, prefixes)
+        { futures.push_back(hello_world_future(node, cout_gid)); }
+
+        // Wait for all IO to finish
+        BOOST_FOREACH(future_value<void> const& f, futures)
+        { f.get(); } 
     }
 
     // Initiate shutdown of the runtime system.
