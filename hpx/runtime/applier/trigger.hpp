cd35e8a1c4641b32abaeed743fd0351f48aa7344
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-06-19 05:31:25 +0000

    Add convienence functions for triggering remote LCOS.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4621 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/runtime/applier/trigger.hpp
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(HPX_6B2240CE_5CE8_43EA_BAFF_5C8F17D21AAE)
+#define HPX_6B2240CE_5CE8_43EA_BAFF_5C8F17D21AAE
+
+#include <hpx/runtime/actions/continuation.hpp>
+
+namespace hpx { namespace applier
+{
+
+template <typename Arg0>
+inline Arg0 const& trigger(
+    naming::id_type k
+  , Arg0 arg0
+) {
+    return actions::continuation(k).trigger<Arg0>(arg0);
+}
+
+inline void trigger(
+    naming::id_type const& k
+) {
+    actions::continuation(k).trigger();
+}
+
+inline void trigger_error(
+    naming::id_type const& k
+  , boost::exception_ptr const& e
+) {
+    actions::continuation(k).trigger_error(e);
+}
+
+}}
+
+#endif // HPX_6B2240CE_5CE8_43EA_BAFF_5C8F17D21AAE
+
