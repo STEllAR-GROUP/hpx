055d327fa25ab46faee5cbc1e2c0928cab937467
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-06-21 00:10:03 +0000

    Missed this from my last commit.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4651 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/util/one_size_heap_list.hpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(HPX_8177E36B_AF5F_4E41_8AD1_E5CF97329C3D)
+#define HPX_8177E36B_AF5F_4E41_8AD1_E5CF97329C3D
+
+#include <hpx/version.hpp>
+
+#if HPX_AGAS_VERSION <= 0x10
+    #include <hpx/util/one_size_heap_list_v1.hpp>
+#else
+    #include <hpx/util/one_size_heap_list_v2.hpp>
+#endif
+
+#endif // HPX_8177E36B_AF5F_4E41_8AD1_E5CF97329C3D
+
