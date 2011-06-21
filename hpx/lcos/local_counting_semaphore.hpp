ce0173210ef54db919fc7b355716bd075e7c045c
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-06-21 00:05:40 +0000

    Move local_counting_semaphore methods into a source file, add a signal_all method.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4648 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/lcos/local_counting_semaphore.hpp
+++ hpx/lcos/local_counting_semaphore.hpp
@@ -8,11 +8,7 @@
 #define HPX_LCOS_COUNTING_SEMAPHORE_OCT_16_2008_1007AM
 
 #include <hpx/hpx_fwd.hpp>
-#include <hpx/runtime/threads/thread.hpp>
-#include <hpx/lcos/mutex.hpp>
 #include <hpx/util/spinlock_pool.hpp>
-#include <hpx/util/unlock_lock.hpp>
-#include <hpx/util/stringstream.hpp>
 
 #include <boost/cstdint.hpp>
 #include <boost/assert.hpp>
@@ -39,11 +35,13 @@ namespace hpx { namespace lcos
     /// well: one thread waiting for several other threads to touch (signal) 
     /// the semaphore, or several threads waiting for one other thread to touch 
     /// this semaphore.
-    class local_counting_semaphore
+    class HPX_EXPORT local_counting_semaphore
     {
     private:
         struct tag {};
         typedef hpx::util::spinlock_pool<tag> mutex_type;
+
+        typedef mutex_type::scoped_lock scoped_lock;
 
         // define data structures needed for intrusive slist container used for
         // the queues
@@ -86,42 +84,7 @@ namespace hpx { namespace lcos
           : value_(value)
         {}
 
-        ~local_counting_semaphore()
-        {
-            if (!queue_.empty()) {
-                LERR_(fatal)
-                    << "lcos::local_counting_semaphore::~local_counting_semaphore:"
-                       " queue is not empty, aborting threads";
-
-                mutex_type::scoped_lock l(this);
-                while (!queue_.empty()) {
-                    threads::thread_id_type id = queue_.front().id_;
-                    queue_.front().id_ = 0;
-                    queue_.front().aborted_waiting_ = true;
-                    queue_.pop_front();
-
-                    // we know that the id is actually the pointer to the thread
-                    threads::thread* thrd = static_cast<threads::thread*>(id);
-                    LERR_(fatal)
-                            << "lcos::local_counting_semaphore::~local_counting_semaphore:"
-                            << " pending thread: " 
-                            << get_thread_state_name(thrd->get_state()) 
-                            << "(" << id << "): " << thrd->get_description();
-
-                    // forcefully abort thread, do not throw
-                    error_code ec;
-                    threads::set_thread_state(id, threads::pending, 
-                        threads::wait_abort, threads::thread_priority_normal, ec);
-                    if (ec) {
-                        LERR_(fatal)
-                            << "lcos::local_counting_semaphore::~local_counting_semaphore:"
-                            << " could not abort thread: "
-                            << get_thread_state_name(thrd->get_state()) 
-                            << "(" << id << "): " << thrd->get_description();
-                    }
-                }
-            }
-        }
+        ~local_counting_semaphore();
 
         /// \brief Wait for the semaphore to be signaled
         ///
@@ -129,87 +92,31 @@ namespace hpx { namespace lcos
         ///                 be decremented. At the same time this is the minimum 
         ///                 value of the lock count at which the thread is not 
         ///                 yielded.
-        void wait(boost::int64_t count = 1)
-        {
-            mutex_type::scoped_lock l(this);
-
-            while (value_ < count) {
-                // we need to get the self anew for each round as it might
-                // get executed in a different thread from the previous one
-                threads::thread_self& self = threads::get_self();
-                threads::thread_id_type id = self.get_thread_id();
-
-                threads::set_thread_lco_description(id, "lcos::local_counting_semaphore");
-
-                queue_entry e(id);
-                queue_.push_back(e);
-                queue_type::const_iterator last = queue_.last();
-
-                {
-                    util::unlock_the_lock<mutex_type::scoped_lock> ul(l);
-                    threads::thread_state_ex_enum statex = self.yield(threads::suspended);
-                    if (statex == threads::wait_abort) {
-                        hpx::util::osstream strm;
-                        strm << "thread(" << id << ", " << threads::get_thread_description(id)
-                             << ") aborted (yield returned wait_abort)";
-                        HPX_THROW_EXCEPTION(no_success, "lcos::local_counting_semaphore::wait",
-                            hpx::util::osstream_get_string(strm));
-                        return;
-                    }
-                }
-
-                if (e.id_)
-                    queue_.erase(last);     // remove entry from queue
-
-                if (e.aborted_waiting_) {
-                    HPX_THROW_EXCEPTION(no_success, "lcos::local_counting_semaphore::wait",
-                        "aborted wait on local_counting_semaphore");
-                    return;
-                }
-            }
-
-            value_ -= count;
-        }
+        void wait(boost::int64_t count = 1);
 
         /// \brief Signal the semaphore
         ///
         /// 
         void signal(boost::int64_t count = 1)
         {
-            mutex_type::scoped_lock l(this);
+            scoped_lock l(this);
+            signal_locked(count, l);
+        }
 
-            value_ += count;
-            if (value_ >= 0) {
-                // release all threads, they will figure out between themselves
-                // which one gets released from wait above
-#if BOOST_VERSION < 103600
-                // slist::swap has a bug in Boost 1.35.0
-                while (!queue_.empty()) {
-                    threads::thread_id_type id = queue_.front().id_;
-                    queue_.front().id_ = 0;
-                    queue_.pop_front();
-                    threads::set_thread_lco_description(id);
-                    threads::set_thread_state(id, threads::pending);
-                }
-#else
-                // swap the list
-                queue_type queue;
-                queue.swap(queue_);
-                l.unlock();
-
-                // release the threads
-                while (!queue.empty()) {
-                    threads::thread_id_type id = queue.front().id_;
-                    queue.front().id_ = 0;
-                    queue.pop_front();
-                    threads::set_thread_lco_description(id);
-                    threads::set_thread_state(id, threads::pending);
-                }
-#endif
-            }
+        boost::int64_t signal_all()
+        {
+            scoped_lock l(this);
+            boost::int64_t count = queue_.size();
+            signal_locked(count, l);
+            return count;
         }
 
     private:
+        void signal_locked(
+            boost::int64_t count
+          , scoped_lock& l
+        );
+
         boost::int64_t value_;
         queue_type queue_;
     };
