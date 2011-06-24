6940b53c653d5ae8b7767da78cbf2611e0386250
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-06-24 21:48:49 +0000

    Print out hello world for each shepherd thread, instead of just once per locality.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4668 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/iostreams/hello_world.cpp
+++ examples/iostreams/hello_world.cpp
@@ -11,6 +11,8 @@
 
 #include <boost/ref.hpp>
 #include <boost/foreach.hpp>
+#include <boost/format.hpp>
+#include <boost/thread.hpp>
 
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
@@ -18,10 +20,15 @@
 #include <hpx/runtime/actions/plain_action.hpp>
 #include <hpx/runtime/components/plain_component_factory.hpp>
 #include <hpx/runtime/components/server/manage_component.hpp>
+#include <hpx/util/uintptr_t.hpp>
 #include <hpx/components/iostreams/lazy_ostream.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
+
+using boost::barrier;
+
+using boost::format;
 
 using hpx::init;
 using hpx::finalize;
@@ -29,13 +36,19 @@ using hpx::finalize;
 using hpx::naming::id_type;
 using hpx::naming::get_gid_from_prefix;
 
+using hpx::get_runtime;
+
 using hpx::applier::get_prefix_id;
 using hpx::applier::get_applier;
+using hpx::applier::register_work;
 
 using hpx::actions::plain_action1;
+using hpx::actions::plain_result_action2;
 
 using hpx::lcos::future_value;
 using hpx::lcos::eager_future;
+
+using hpx::threads::threadmanager_base;
 
 using hpx::components::server::create_one;
 using hpx::components::managed_component;
@@ -46,17 +59,60 @@ using hpx::iostreams::server::output_stream;
 
 typedef managed_component<output_stream> ostream_type;
 
-void hello_world(id_type const& cout_gid)
+///////////////////////////////////////////////////////////////////////////////
+bool hello_world_worker(id_type const& cout_gid, std::size_t desired)
 {
-    lazy_ostream hpx_cout(cout_gid);
-    hpx_cout << "[L" << get_prefix_id() << "]: hello world!" << endl;
+    std::size_t current = threadmanager_base::get_thread_num();
+
+    if (current == desired)
+    {
+        lazy_ostream hpx_cout(cout_gid);
+        hpx_cout << boost::str(
+                        format("hello world from shepherd %1% on locality %2%")
+                      % desired 
+                      % get_prefix_id())
+            << endl;
+        return true;
+    }
+
+    else
+        return false;
 }
 
-typedef plain_action1<id_type const&, hello_world> hello_world_action;
+typedef plain_result_action2<
+    bool, id_type const&, std::size_t, hello_world_worker
+> hello_world_worker_action;
 
-HPX_REGISTER_PLAIN_ACTION(hello_world_action);
+HPX_REGISTER_PLAIN_ACTION(hello_world_worker_action);
 
-typedef eager_future<hello_world_action> hello_world_future;
+typedef eager_future<hello_world_worker_action> hello_world_worker_future;
+
+///////////////////////////////////////////////////////////////////////////////
+void hello_world_foreman(id_type const& cout_gid)
+{
+    const std::size_t shepherds
+        = get_runtime().get_process().get_num_os_threads();
+
+    const id_type prefix = get_applier().get_runtime_support_gid();
+
+    for (std::size_t i = 0; i < shepherds; ++i)
+    {
+        while (true)
+        {
+            hello_world_worker_future f(prefix, cout_gid, i);
+            
+            if (f.get())
+                break;
+        }
+    }
+}
+
+typedef plain_action1<id_type const&, hello_world_foreman>
+    hello_world_foreman_action;
+
+HPX_REGISTER_PLAIN_ACTION(hello_world_foreman_action);
+
+typedef eager_future<hello_world_foreman_action> hello_world_foreman_future;
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(variables_map&)
@@ -73,7 +129,7 @@ int hpx_main(variables_map&)
         get_applier().get_prefixes(prefixes);
 
         BOOST_FOREACH(id_type const& node, prefixes)
-        { futures.push_back(hello_world_future(node, cout_gid)); }
+        { futures.push_back(hello_world_foreman_future(node, cout_gid)); }
 
         // Wait for all IO to finish
         BOOST_FOREACH(future_value<void> const& f, futures)
