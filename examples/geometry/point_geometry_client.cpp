4e5cdd9472d765ecc77895404c3ac6077d469e5c
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-07-07 16:54:11 +0000

    Added polygon
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4799 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/geometry/point_geometry_client.cpp
+++ examples/geometry/point_geometry_client.cpp
@@ -7,6 +7,12 @@
 #include <hpx/hpx_init.hpp>
 
 #include "point_geometry/point.hpp"
+#include <boost/geometry/geometries/polygon.hpp>
+
+namespace hpx { namespace geometry
+{
+    typedef boost::geometry::model::polygon<hpx::geometry::point> polygon_2d; 
+}}
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(boost::program_options::variables_map &vm)
@@ -14,15 +20,28 @@ int hpx_main(boost::program_options::variables_map &vm)
     {
         namespace bg = boost::geometry;
 
-        hpx::geometry::point pt1(hpx::find_here(), 0, 0);
-        hpx::geometry::point pt2(hpx::find_here(), 0, 0);
+        hpx::geometry::point pt1(hpx::find_here(), 1, 1);
+        hpx::geometry::point pt2(hpx::find_here(), 2, 2);
+        hpx::geometry::point pt3(hpx::find_here(), 1, 2);
 
-        bg::assign_values(pt1, 1, 1);
-        bg::assign_values(pt2, 2, 2);
+//         bg::assign_values(pt1, 1, 1);
+//         bg::assign_values(pt2, 2, 2);
 
         double d = bg::distance(pt1, pt2);
 
         std::cout << "Distance: " << d << std::endl;
+
+        hpx::geometry::polygon_2d p;
+        p.outer().push_back(pt1);
+        p.outer().push_back(pt2);
+        p.outer().push_back(pt3);
+        p.outer().push_back(pt1);
+        bg::correct(p);
+
+        hpx::geometry::point pt4(hpx::find_here(), 1.5, 1.25);
+        bool inside = bg::within(pt4, p);
+
+        std::cout << "Point is " << (inside ? "inside" : "outside") << std::endl;
     }
 
     hpx::finalize();
