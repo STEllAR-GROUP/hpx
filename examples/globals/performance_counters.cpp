77acf66389ffa997b96a11c4197489603eaebab0
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-07-13 21:01:09 +0000

    Simplified example of performance counter installation that doesn't use static_'s
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4876 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/globals/performance_counters.cpp
+++ examples/globals/performance_counters.cpp
@@ -5,17 +5,20 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/bind.hpp>
+
 #include <hpx/hpx_init.hpp>
-#include <hpx/include/iostreams.hpp>
+#include <hpx/runtime.hpp>
 #include <hpx/performance_counters/manage_counter_type.hpp>
 #include <hpx/performance_counters/manage_counter.hpp>
-#include <hpx/util/static.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
 
-using hpx::cout;
-using hpx::endl;
+using boost::phoenix::bind;
+
+using hpx::get_runtime;
 
 using hpx::init;
 using hpx::finalize;
@@ -24,34 +27,30 @@ using hpx::performance_counters::manage_counter_type;
 using hpx::performance_counters::manage_counter;
 using hpx::performance_counters::counter_raw;
 
-using hpx::util::static_;
-
-struct perf_counter_foo_type {};
-struct perf_counter_foo_bar {};
-
 ///////////////////////////////////////////////////////////////////////////////
 boost::int64_t bar() { return 42; }
 
 ///////////////////////////////////////////////////////////////////////////////
-void startup_()
+void shutdown_(manage_counter_type* type_, manage_counter* instance_)
 {
-    static_<manage_counter_type*, perf_counter_foo_type>
-      type_(new manage_counter_type);
-    static_<manage_counter*, perf_counter_foo_bar>
-      instance_(new manage_counter);
-
-    type_.get()->install("/foo/buzz", counter_raw);  
-    instance_.get()->install("/foo(bar)/buzz", bar); 
+    BOOST_ASSERT(type_); 
+    BOOST_ASSERT(instance_); 
+    delete type_;
+    delete instance_;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-void shutdown_()
+void startup_()
 {
-    static_<manage_counter_type*, perf_counter_foo_type> type_;
-    static_<manage_counter*, perf_counter_foo_bar> instance_;
+    manage_counter_type* type_ = new manage_counter_type;
+    manage_counter* instance_ = new manage_counter;
 
-    delete type_.get();
-    delete instance_.get();
+    // Install the counters.
+    type_->install("/foo/buzz", counter_raw);  
+    instance_->install("/foo(bar)/buzz", bar); 
+
+    // Register the shutdown function which will free these counters.
+    get_runtime().add_shutdown_function(bind(&shutdown_, type_, instance_));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -68,8 +67,8 @@ int main(int argc, char* argv[])
     // Configure application-specific options
     options_description
        desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
-
+    
     // Initialize and run HPX
-    return init(desc_commandline, argc, argv, startup_, shutdown_);
+    return init(desc_commandline, argc, argv, startup_);
 }
 
