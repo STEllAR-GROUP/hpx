4d08332c85352478f2c53ba15b16f4f4dbd11793
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-07-14 20:12:01 +0000

    Update example as per Hartmut's suggestions.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4880 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/globals/performance_counters.cpp
+++ examples/globals/performance_counters.cpp
@@ -5,8 +5,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <boost/phoenix/core.hpp>
-#include <boost/phoenix/bind.hpp>
+#include <boost/bind.hpp>
 
 #include <hpx/hpx_init.hpp>
 #include <hpx/runtime.hpp>
@@ -16,7 +15,7 @@
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
 
-using boost::phoenix::bind;
+using boost::bind;
 
 using hpx::get_runtime;
 
@@ -31,25 +30,28 @@ using hpx::performance_counters::counter_raw;
 boost::int64_t bar() { return 42; }
 
 ///////////////////////////////////////////////////////////////////////////////
-void shutdown_(manage_counter_type* type_, manage_counter* instance_)
-{
+void shutdown_(
+    boost::shared_ptr<manage_counter_type> const& type_
+  , boost::shared_ptr<manage_counter> const& instance_
+) {
     BOOST_ASSERT(type_); 
     BOOST_ASSERT(instance_); 
-    delete type_;
-    delete instance_;
+    type_->uninstall();
+    instance_->uninstall();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void startup_()
 {
-    manage_counter_type* type_ = new manage_counter_type;
-    manage_counter* instance_ = new manage_counter;
+    boost::shared_ptr<manage_counter_type> type_(new manage_counter_type);
+    boost::shared_ptr<manage_counter> instance_(new manage_counter);
 
-    // Install the counters.
+    // Install a counter type and a counter instance.
     type_->install("/foo/buzz", counter_raw);  
     instance_->install("/foo(bar)/buzz", bar); 
 
-    // Register the shutdown function which will free these counters.
+    // Register the shutdown function which will clean up the counter type and
+    // instance.
     get_runtime().add_shutdown_function(bind(&shutdown_, type_, instance_));
 }
 
