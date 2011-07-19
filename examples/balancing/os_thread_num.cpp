3a3dbbf819bcffd8170d786b2b0f63da6f8f0e8c
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-07-19 15:02:03 +0000

    Update os_thread_num example.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4912 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/balancing/os_thread_num.cpp
+++ examples/balancing/os_thread_num.cpp
@@ -7,6 +7,7 @@
 #include <hpx/hpx_init.hpp>
 #include <hpx/lcos/local_barrier.hpp>
 
+#include <cmath>
 #include <vector>
 #include <map>
 
@@ -26,6 +27,8 @@ using boost::program_options::value;
 using hpx::lcos::local_barrier;
 
 using hpx::threads::threadmanager_base;
+using hpx::threads::pending;
+using hpx::threads::thread_priority_normal;
 
 using hpx::applier::register_work;
 
@@ -42,56 +45,81 @@ void get_os_thread_num(local_barrier& barr, fifo<std::size_t>& shepherds)
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(variables_map& vm)
 {
-    typedef std::map<std::size_t, std::size_t>
-        result_map;
-
-    typedef std::multimap<std::size_t, std::size_t, std::greater<std::size_t> >
-        sorter;
-
-    std::size_t pxthreads = 0;
-
-    if (vm.count("pxthreads"))
-        pxthreads = vm["pxthreads"].as<std::size_t>();
-    
-    std::size_t iterations = 0;
-
-    if (vm.count("iterations"))
-        iterations = vm["iterations"].as<std::size_t>();
-
-    result_map results;
-
-    for (std::size_t i = 0; i < iterations; ++i)
     {
-        // Have the fifo preallocate the nodes.
-        fifo<std::size_t> shepherds(pxthreads); 
-
-        local_barrier barr(pxthreads + 1);
-
-        for (std::size_t j = 0; j < pxthreads; ++j)
-            register_work(boost::bind 
-                (&get_os_thread_num, boost::ref(barr), boost::ref(shepherds)));
-
-        barr.wait(); // wait for all PX threads to enter the barrier
-
-        std::size_t shepherd = 0;
-
-        while (shepherds.dequeue(&shepherd))
-            ++results[shepherd];
-    }
-
-    sorter sort;
-
-    BOOST_FOREACH(result_map::value_type const& result, results)
-    { sort.insert(sorter::value_type(result.second, result.first)); }  
+        typedef std::map<std::size_t, std::size_t>
+            result_map;
     
-    BOOST_FOREACH(sorter::value_type const& result, sort) {
-        std::cout << std::setfill('0') << std::setw(4)
-                  << result.second << " -> "
-                  << result.first << "\n";
+        typedef std::multimap<
+            std::size_t, std::size_t, std::greater<std::size_t>
+        > sorter;
+    
+        std::size_t pxthreads = 0;
+    
+        if (vm.count("pxthreads"))
+            pxthreads = vm["pxthreads"].as<std::size_t>();
+        
+        std::size_t iterations = 0;
+    
+        if (vm.count("iterations"))
+            iterations = vm["iterations"].as<std::size_t>();
+    
+        result_map results;
+    
+        for (std::size_t i = 0; i < iterations; ++i)
+        {
+            // Have the fifo preallocate the nodes.
+            fifo<std::size_t> shepherds(pxthreads); 
+    
+            local_barrier barr(pxthreads + 1);
+   
+            const std::size_t one_percent =
+                std::floor(double(pxthreads) / 100.0);
+
+            for ( std::size_t percent_count = 0, percent = 0, j = 0
+                ; j < pxthreads
+                ; ++percent_count, ++j)
+            {
+                if (percent_count == one_percent && percent < 99)
+                {
+                    ++percent;
+                    percent_count = 0;
+                    std::cout << percent << "% of pxthreads created\n";  
+                }
+
+                register_work(boost::bind(&get_os_thread_num
+                                        , boost::ref(barr)
+                                        , boost::ref(shepherds))
+                  , "get_os_thread_num"
+                  , pending
+                  , thread_priority_normal
+                  , 0);
+            }  
+
+            std::cout << "100% of pxthreads created\n";  
+  
+            barr.wait(); // wait for all PX threads to enter the barrier
+    
+            std::size_t shepherd = 0;
+    
+            while (shepherds.dequeue(&shepherd))
+                ++results[shepherd];
+        }
+    
+        sorter sort;
+    
+        BOOST_FOREACH(result_map::value_type const& result, results)
+        { sort.insert(sorter::value_type(result.second, result.first)); }  
+        
+        BOOST_FOREACH(sorter::value_type const& result, sort) {
+            std::cout << std::setfill('0') << std::setw(4)
+                      << result.second << " -> "
+                      << result.first << "\n";
+        }
+
     }
 
     // initiate shutdown of the runtime system
-    finalize(0.5);
+    finalize();
     return 0;
 }
 
