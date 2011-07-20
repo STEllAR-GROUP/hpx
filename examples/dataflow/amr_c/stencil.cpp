9371e0a8c899ae273a7e181547b1c6b56f38f6cd
Matthew Anderson <manderson@cct.lsu.edu>
2011-07-20 16:45:20 +0000

    simple dataflow example
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4941 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/dataflow/amr_c/stencil.cpp
@@ -0,0 +1,120 @@
+//  Copyright (c) 2007-2011 Hartmut Kaiser
+//  Copyright (c)      2011 Matthew Anderson
+//  Copyright (c)      2011 Bryce Lelbach
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx.hpp>
+#include <hpx/lcos/future_wait.hpp>
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/export.hpp>
+
+#include <boost/foreach.hpp>
+
+#include <math.h>
+
+#include "stencil.hpp"
+#include "logging.hpp"
+#include "stencil_data.hpp"
+#include "stencil_functions.hpp"
+#include "stencil_data_locking.hpp"
+#include "../mesh/unigrid_mesh.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace components { namespace amr 
+{
+    ///////////////////////////////////////////////////////////////////////////
+    stencil::stencil()
+      : numsteps_(0)
+    {
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Implement actual functionality of this stencil
+    // Compute the result value for the current time step
+    int stencil::eval(naming::id_type const& result, 
+        std::vector<naming::id_type> const& gids, std::size_t row, std::size_t column,
+        double cycle_time, parameter const& par)
+    {
+        // make sure all the gids are looking valid
+        if (result == naming::invalid_id)
+        {
+            HPX_THROW_EXCEPTION(bad_parameter,
+                "stencil::eval", "result gid is invalid");
+            return -1;
+        }
+
+
+        // this should occur only after result has been delivered already
+        BOOST_FOREACH(naming::id_type gid, gids)
+        {
+            if (gid == naming::invalid_id)
+                return -1;
+        }
+
+        // get all input and result memory_block_data instances
+        std::vector<access_memory_block<stencil_data> > val;
+        access_memory_block<stencil_data> resultval = 
+            get_memory_block_async(val, gids, result);
+
+        // lock all user defined data elements, will be unlocked at function exit
+        scoped_values_lock<lcos::mutex> l(resultval, val); 
+
+        resultval->max_index_ = val[1]->max_index_;
+        resultval->index_ = val[1]->index_;
+        resultval->value_ = val[1]->value_;
+        resultval->timestep_ = val[1]->timestep_ + 1.0;
+
+        std::cout << " row " << row << " column " << column << std::endl;
+        if ( resultval->timestep_ >= par->nt0-1 ) {
+          return 0;
+        }
+        return 1;
+    }
+
+    hpx::actions::manage_object_action<stencil_data> const manage_stencil_data =
+        hpx::actions::manage_object_action<stencil_data>();
+
+}}}
+
+HPX_REGISTER_MANAGE_OBJECT_ACTION(
+    hpx::actions::manage_object_action<hpx::components::amr::stencil_data>,
+    dataflow_manage_object_action_stencil_data)
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace components { namespace amr 
+{
+    ///////////////////////////////////////////////////////////////////////////
+    naming::id_type stencil::alloc_data(int item, int maxitems, int row,
+                           std::vector<naming::id_type> const& interp_src_data,
+                           double time,
+                           parameter const& par)
+    {
+        naming::id_type here = applier::get_applier().get_runtime_support_gid();
+        naming::id_type result = components::stubs::memory_block::create(
+            here, sizeof(stencil_data), manage_stencil_data);
+
+        if (-1 != item) {
+            // provide initial data for the given data value 
+            access_memory_block<stencil_data> val(
+                components::stubs::memory_block::checkout(result));
+
+            // call provided (external) function
+            generate_initial_data(val.get_ptr(), item, maxitems, row, *par.p);
+
+            if (log_ && par->loglevel > 1)         // send initial value to logging instance
+                stubs::logging::logentry(log_, val.get(), row,item, par);
+        }
+        return result;
+    }
+
+    void stencil::init(std::size_t numsteps, naming::id_type const& logging)
+    {
+        numsteps_ = numsteps;
+        log_ = logging;
+    }
+
+}}}
+
