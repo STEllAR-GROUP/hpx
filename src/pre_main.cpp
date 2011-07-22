92b730363e737c5d7742d67eafd0729f22c68e94
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-07-22 18:55:34 +0000

    Add handling for runtimes in probe mode to pre_main.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@4997 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/pre_main.cpp
+++ src/pre_main.cpp
@@ -17,107 +17,131 @@
 namespace hpx
 {
 
-void pre_main()
+void pre_main(runtime_mode mode)
 {
-    naming::resolver_client& agas_client = get_runtime().get_agas_client();
-
-    // Load components, so that we can use the barrier LCO.
-    components::stubs::runtime_support::load_components
-        (find_here());
-
-    lcos::barrier second_stage, third_stage;
-
-    // {{{ Second and third stage barrier creation.
-    if (agas_client.is_bootstrap())
+    if (runtime_mode_probe == mode)
     {
-        second_stage.create_one(agas_client.local_prefix(),
-            get_runtime().get_config().get_num_localities());
+        naming::resolver_client& agas_client = get_runtime().get_agas_client();
 
-        third_stage.create_one(agas_client.local_prefix(),
-            get_runtime().get_config().get_num_localities());
+        components::stubs::runtime_support::load_components
+            (find_here());
 
-        naming::gid_type second = second_stage.get_gid().get_gid();
-        naming::strip_credit_from_gid(second);
-
-        naming::gid_type third = third_stage.get_gid().get_gid();
-        naming::strip_credit_from_gid(third);
-
-        agas_client.registerid("/barrier(agas#0)/second_stage", second); 
-        agas_client.registerid("/barrier(agas#0)/third_stage", third); 
-    }
-
-    else // Hosted. 
-    {
         const std::size_t allocate_size =
             get_runtime().get_config().get_agas_allocate_response_pool_size();
         const std::size_t bind_size =
             get_runtime().get_config().get_agas_bind_response_pool_size();
-
+    
         // Unblock the AGAS router by adding the initial pool size to the
-        // future pool semaphores. This ensures that no AGAS requests are 
-        // sent after first-stage AGAS bootstrap and before second-stage
-        // bootstrap.
+        // future pool semaphores. 
         agas_client.hosted->allocate_response_sema_.signal(allocate_size);
         agas_client.hosted->bind_response_sema_.signal(bind_size);
 
-        naming::gid_type second, third; 
-
-        // {{{ Find the global address of the second stage barrier.
-        for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
-        {
-            if (agas_client.queryid("/barrier(agas#0)/second_stage", second))
-                break; 
-
-            boost::this_thread::sleep(boost::get_system_time() + 
-                boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
-        }
-
-        if (HPX_UNLIKELY(second == naming::invalid_gid))
-        {
-            HPX_THROW_EXCEPTION(network_error, 
-                "pre_main", "couldn't find second stage boot barrier");
-        }
-        // }}}
-
-        // {{{ Find the global address of the third stage barrier.
-        for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
-        {
-            if (agas_client.queryid("/barrier(agas#0)/third_stage", third))
-                break; 
-
-            boost::this_thread::sleep(boost::get_system_time() + 
-                boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
-        }
-
-        if (HPX_UNLIKELY(third == naming::invalid_gid))
-        {
-            HPX_THROW_EXCEPTION(network_error, 
-                "pre_main", "couldn't find third stage boot barrier");
-        }
-        // }}}
-
-        naming::id_type second_id(second, naming::id_type::unmanaged)
-                      , third_id(third, naming::id_type::unmanaged);
-
-        // Initialize the barrier clients
-        second_stage = lcos::barrier(second_id);
-        third_stage = lcos::barrier(third_id);
+        components::stubs::runtime_support::call_startup_functions
+            (find_here());
     }
-    // }}}
 
-    // Second stage bootstrap synchronizes component loading across all
-    // localities, ensuring that the component namespace tables are fully
-    // populated before user code is executed.
-    second_stage.wait();
+    else
+    {
+        naming::resolver_client& agas_client = get_runtime().get_agas_client();
+    
+        // Load components, so that we can use the barrier LCO.
+        components::stubs::runtime_support::load_components
+            (find_here());
+    
+        lcos::barrier second_stage, third_stage;
+    
+        // {{{ Second and third stage barrier creation.
+        if (agas_client.is_bootstrap())
+        {
+            second_stage.create_one(agas_client.local_prefix(),
+                get_runtime().get_config().get_num_localities());
+    
+            third_stage.create_one(agas_client.local_prefix(),
+                get_runtime().get_config().get_num_localities());
+    
+            naming::gid_type second = second_stage.get_gid().get_gid();
+            naming::strip_credit_from_gid(second);
+    
+            naming::gid_type third = third_stage.get_gid().get_gid();
+            naming::strip_credit_from_gid(third);
+    
+            agas_client.registerid("/barrier(agas#0)/second_stage", second); 
+            agas_client.registerid("/barrier(agas#0)/third_stage", third); 
+        }
+    
+        else // Hosted. 
+        {
+            const std::size_t allocate_size =
+                get_runtime().get_config().get_agas_allocate_response_pool_size();
+            const std::size_t bind_size =
+                get_runtime().get_config().get_agas_bind_response_pool_size();
+    
+            // Unblock the AGAS router by adding the initial pool size to the
+            // future pool semaphores. This ensures that no AGAS requests are 
+            // sent after first-stage AGAS bootstrap and before second-stage
+            // bootstrap.
+            agas_client.hosted->allocate_response_sema_.signal(allocate_size);
+            agas_client.hosted->bind_response_sema_.signal(bind_size);
 
-    // Third stage bootstrap synchronizes startup functions across all
-    // localities. This is done after component loading to guarantee that
-    // all user code, including startup functions, are only run after the
-    // component tables are populated.
-    components::stubs::runtime_support::call_startup_functions
-        (find_here());
-
-    third_stage.wait();
+            naming::gid_type second, third; 
+    
+            // {{{ Find the global address of the second stage barrier.
+            for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
+            {
+                if (agas_client.queryid("/barrier(agas#0)/second_stage", second))
+                    break; 
+    
+                boost::this_thread::sleep(boost::get_system_time() + 
+                    boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
+            }
+    
+            if (HPX_UNLIKELY(second == naming::invalid_gid))
+            {
+                HPX_THROW_EXCEPTION(network_error, 
+                    "pre_main", "couldn't find second stage boot barrier");
+            }
+            // }}}
+    
+            // {{{ Find the global address of the third stage barrier.
+            for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
+            {
+                if (agas_client.queryid("/barrier(agas#0)/third_stage", third))
+                    break; 
+    
+                boost::this_thread::sleep(boost::get_system_time() + 
+                    boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
+            }
+    
+            if (HPX_UNLIKELY(third == naming::invalid_gid))
+            {
+                HPX_THROW_EXCEPTION(network_error, 
+                    "pre_main", "couldn't find third stage boot barrier");
+            }
+            // }}}
+    
+            naming::id_type second_id(second, naming::id_type::unmanaged)
+                          , third_id(third, naming::id_type::unmanaged);
+    
+            // Initialize the barrier clients
+            second_stage = lcos::barrier(second_id);
+            third_stage = lcos::barrier(third_id);
+        }
+        // }}}
+    
+        // Second stage bootstrap synchronizes component loading across all
+        // localities, ensuring that the component namespace tables are fully
+        // populated before user code is executed.
+        second_stage.wait();
+    
+        // Third stage bootstrap synchronizes startup functions across all
+        // localities. This is done after component loading to guarantee that
+        // all user code, including startup functions, are only run after the
+        // component tables are populated.
+        components::stubs::runtime_support::call_startup_functions
+            (find_here());
+    
+        third_stage.wait();
+    }
 }
 
 }
