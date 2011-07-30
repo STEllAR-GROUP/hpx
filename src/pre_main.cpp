94a99205eaf37f2eecdc1ebfa4e9a3d8b00a5596
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-07-30 03:43:31 +0000

    Refactored pre_main
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5135 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/pre_main.cpp
+++ src/pre_main.cpp
@@ -17,20 +17,65 @@
 namespace hpx
 {
 
+///////////////////////////////////////////////////////////////////////////////
+// Create a new barrier and register its gid with the given symbolic name.
+inline lcos::barrier 
+create_barrier(naming::resolver_client& agas_client, 
+    std::size_t num_localities, char const* symname)
+{
+    lcos::barrier barrier;
+    barrier.create_one(agas_client.local_prefix(), num_localities);
+
+    naming::gid_type gid = barrier.get_gid().get_gid();
+    naming::strip_credit_from_gid(gid);
+
+    agas_client.registerid(symname, gid); 
+    return barrier;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Find a registered barrier object from its symbolic name, return invalid_gid
+// if failed after N retries.
+inline lcos::barrier 
+find_barrier(naming::resolver_client& agas_client, char const* symname)
+{
+    naming::gid_type barrier_id;
+    for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
+    {
+        if (agas_client.queryid(symname, barrier_id))
+            break; 
+
+        boost::this_thread::sleep(boost::get_system_time() + 
+            boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
+    }
+    if (HPX_UNLIKELY(!barrier_id))
+    {
+        HPX_THROW_EXCEPTION(network_error, "pre_main (find_barrier)", 
+            std::string("couldn't find stage boot barrier ") + symname);
+    }
+    return lcos::barrier(naming::id_type(barrier_id, naming::id_type::unmanaged));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Symbolic names of global boot barrier objects
+const char* second_barrier = "/barrier(agas#0)/second_stage";
+const char* third_barrier = "/barrier(agas#0)/third_stage";
+
+///////////////////////////////////////////////////////////////////////////////
 void pre_main(runtime_mode mode)
 {
+    naming::resolver_client& agas_client = get_runtime().get_agas_client();
+    util::runtime_configuration const& cfg = get_runtime().get_config();
+
     if (runtime_mode_probe == mode)
     {
-        naming::resolver_client& agas_client = get_runtime().get_agas_client();
-
+        // Load components, so that we can use the barrier LCO.
         components::stubs::runtime_support::load_components
             (find_here());
 
-        const std::size_t allocate_size
-            = get_runtime().get_config().get_agas_allocate_response_pool_size();
-        const std::size_t bind_size
-            = get_runtime().get_config().get_agas_bind_response_pool_size();
-    
+        const std::size_t allocate_size = cfg.get_agas_allocate_response_pool_size();
+        const std::size_t bind_size = cfg.get_agas_bind_response_pool_size();
+
         // Unblock the AGAS router by adding the initial pool size to the
         // future pool semaphores. 
         agas_client.hosted->allocate_response_sema_.signal(allocate_size);
@@ -42,40 +87,25 @@ void pre_main(runtime_mode mode)
 
     else
     {
-        naming::resolver_client& agas_client = get_runtime().get_agas_client();
-    
         // Load components, so that we can use the barrier LCO.
         components::stubs::runtime_support::load_components
             (find_here());
-    
+
         lcos::barrier second_stage, third_stage;
-    
+
         // {{{ Second and third stage barrier creation.
         if (agas_client.is_bootstrap())
         {
-            second_stage.create_one(agas_client.local_prefix(),
-                get_runtime().get_config().get_num_localities());
-    
-            third_stage.create_one(agas_client.local_prefix(),
-                get_runtime().get_config().get_num_localities());
-    
-            naming::gid_type second = second_stage.get_gid().get_gid();
-            naming::strip_credit_from_gid(second);
-    
-            naming::gid_type third = third_stage.get_gid().get_gid();
-            naming::strip_credit_from_gid(third);
-    
-            agas_client.registerid("/barrier(agas#0)/second_stage", second); 
-            agas_client.registerid("/barrier(agas#0)/third_stage", third); 
+            std::size_t const num_localities = cfg.get_num_localities();
+            second_stage = create_barrier(agas_client, num_localities, second_barrier);
+            third_stage = create_barrier(agas_client, num_localities, third_barrier);
         }
-    
+
         else // Hosted. 
         {
-            const std::size_t allocate_size =
-                get_runtime().get_config().get_agas_allocate_response_pool_size();
-            const std::size_t bind_size =
-                get_runtime().get_config().get_agas_bind_response_pool_size();
-    
+            const std::size_t allocate_size = cfg.get_agas_allocate_response_pool_size();
+            const std::size_t bind_size = cfg.get_agas_bind_response_pool_size();
+
             // Unblock the AGAS router by adding the initial pool size to the
             // future pool semaphores. This ensures that no AGAS requests are 
             // sent after first-stage AGAS bootstrap and before second-stage
@@ -83,63 +113,24 @@ void pre_main(runtime_mode mode)
             agas_client.hosted->allocate_response_sema_.signal(allocate_size);
             agas_client.hosted->bind_response_sema_.signal(bind_size);
 
-            naming::gid_type second, third; 
-    
-            // {{{ Find the global address of the second stage barrier.
-            for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
-            {
-                if (agas_client.queryid("/barrier(agas#0)/second_stage", second))
-                    break; 
-    
-                boost::this_thread::sleep(boost::get_system_time() + 
-                    boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
-            }
-    
-            if (HPX_UNLIKELY(second == naming::invalid_gid))
-            {
-                HPX_THROW_EXCEPTION(network_error, 
-                    "pre_main", "couldn't find second stage boot barrier");
-            }
-            // }}}
-    
-            // {{{ Find the global address of the third stage barrier.
-            for (int i = 0; i < HPX_MAX_NETWORK_RETRIES; ++i)
-            {
-                if (agas_client.queryid("/barrier(agas#0)/third_stage", third))
-                    break; 
-    
-                boost::this_thread::sleep(boost::get_system_time() + 
-                    boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
-            }
-    
-            if (HPX_UNLIKELY(third == naming::invalid_gid))
-            {
-                HPX_THROW_EXCEPTION(network_error, 
-                    "pre_main", "couldn't find third stage boot barrier");
-            }
-            // }}}
-    
-            naming::id_type second_id(second, naming::id_type::unmanaged)
-                          , third_id(third, naming::id_type::unmanaged);
-    
-            // Initialize the barrier clients
-            second_stage = lcos::barrier(second_id);
-            third_stage = lcos::barrier(third_id);
+            // Initialize the barrier clients (find them in AGAS)
+            second_stage = find_barrier(agas_client, second_barrier);
+            third_stage = find_barrier(agas_client, third_barrier);
         }
         // }}}
-    
+
         // Second stage bootstrap synchronizes component loading across all
         // localities, ensuring that the component namespace tables are fully
         // populated before user code is executed.
         second_stage.wait();
-    
+
+        components::stubs::runtime_support::call_startup_functions
+            (find_here());
+
         // Third stage bootstrap synchronizes startup functions across all
         // localities. This is done after component loading to guarantee that
         // all user code, including startup functions, are only run after the
         // component tables are populated.
-        components::stubs::runtime_support::call_startup_functions
-            (find_here());
-    
         third_stage.wait();
     }
 }
