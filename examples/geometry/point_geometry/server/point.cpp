3b6009874251b0d29813a63d1f4cde3b2019561f
Matthew Anderson <manderson@cct.lsu.edu>
2011-08-01 22:52:20 +0000

    new
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5161 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/geometry/point_geometry/server/point.cpp
@@ -0,0 +1,30 @@
+//  Copyright (c) 2007-2011 Hartmut Kaiser
+//  Copyright (c) 2007-2011 Matthew Anderson
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/applier/applier.hpp>
+#include <hpx/runtime/threads/thread.hpp>
+#include <hpx/runtime/components/component_type.hpp>
+#include <hpx/runtime/components/server/managed_component_base.hpp>
+#include <hpx/runtime/actions/component_action.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometries/adapted/boost_tuple.hpp>
+
+#include "../serialize_geometry.hpp"
+#include "./point.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace geometry { namespace server 
+{
+        /// search for contact
+        bool point::search(std::vector<hpx::naming::id_type> const& search_objects) const
+        {
+            return false;
+        }
+}}}
+
