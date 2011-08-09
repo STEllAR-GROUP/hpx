ff43f339a2d06506a91a7b5e162c4916d60ce045
Daniel Kogler <dkogler@cct.lsu.edu>
2011-08-09 20:12:39 +0000

    just putting my name on the code
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5238 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/barnes_hut/barnes_hut/server/bhnode.cpp
+++ examples/barnes_hut/barnes_hut/server/bhnode.cpp
@@ -1,3 +1,9 @@
+//////////////////////////////////////////////////////////////////////////////// 
+//  Copyright (C) 2011 Daniel Kogler                                                
+//                                                                               
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        
+////////////////////////////////////////////////////////////////////////////////
 using hpx::applier::get_applier;
 namespace hpx { namespace components { namespace server
 {
--- examples/barnes_hut/barnes_hut/server/bhnode.hpp
+++ examples/barnes_hut/barnes_hut/server/bhnode.hpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (C) 2011 Dan Kogler
+//  Copyright (C) 2011 Daniel Kogler
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
