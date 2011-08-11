d38edaccc7e8e585c5251550ecc361c9864306cd
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-08-11 01:19:16 +0000

    Same windows compilation fix as before
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5249 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/throttle/throttle_client.cpp
+++ examples/throttle/throttle_client.cpp
@@ -4,11 +4,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/format.hpp>
-
 #include <hpx/hpx_init.hpp>
 
 #include "throttle/throttle.hpp"
+
+#include <boost/format.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
