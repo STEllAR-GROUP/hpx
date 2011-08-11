96fec99b20ef35662bda882e01227a76ebf4ae98
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-08-11 01:11:45 +0000

    Fixing WIndows compilation problems (hpx headers must always be the first in a file *sigh*)
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5248 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/throttle/spin.cpp
+++ examples/throttle/spin.cpp
@@ -3,11 +3,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/foreach.hpp>
-#include <boost/format.hpp>
-
 #include <hpx/hpx_init.hpp>
 #include <hpx/runtime/naming/resolver_client.hpp>
+
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
