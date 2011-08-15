9fd2622178b9f1dbaf41935c930183c766b30b23
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-08-15 21:42:10 +0000

    Demonstrates #61.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5302 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/error_handling3.cpp
+++ examples/quickstart/error_handling3.cpp
@@ -8,11 +8,14 @@
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int.hpp>
 
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/exception.hpp>
-#include <hpx/lcos/eager_future.hpp>
+#include <hpx/runtime/applier/apply.hpp>
 #include <hpx/runtime/actions/plain_action.hpp>
+#include <hpx/runtime/threads/thread_helpers.hpp>
 #include <hpx/runtime/components/plain_component_factory.hpp>
 
 using boost::program_options::variables_map;
@@ -20,19 +23,32 @@ using boost::program_options::options_description;
 
 using hpx::init;
 using hpx::finalize;
+
 using hpx::no_success;
 
+using hpx::applier::apply;
 using hpx::applier::get_applier;
+using hpx::applier::get_prefix_id;
 
 using hpx::actions::plain_action0;
 
-using hpx::lcos::eager_future;
+using hpx::naming::id_type;
 
-using hpx::naming::gid_type;
+using hpx::threads::pending;
+using hpx::threads::suspended;
+using hpx::threads::thread_state_ex_enum;
+using hpx::threads::wait_timeout;
+using hpx::threads::get_self_id;
+using hpx::threads::get_self;
+using hpx::threads::set_thread_state;
+
+using boost::posix_time::milliseconds;
 
 ///////////////////////////////////////////////////////////////////////////////
 void raise_exception()
 {
+    std::cout << (boost::format("raising exception on %d\n") % get_prefix_id())
+              << std::flush;
     HPX_THROW_EXCEPTION(no_success, "raise_exception", "unhandled exception"); 
 }
 
@@ -40,22 +56,32 @@ typedef plain_action0<raise_exception> raise_exception_action;
 
 HPX_REGISTER_PLAIN_ACTION(raise_exception_action);
 
-typedef eager_future<raise_exception_action> raise_exception_future;
-
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(variables_map& vm)
 {
     {
-        std::vector<gid_type> localities;
-        get_applier().get_agas_client().get_prefixes(localities);
-         
+        std::vector<id_type> localities;
+        get_applier().get_remote_prefixes(localities);
+
+        if (localities.empty())
+            raise_exception();
+ 
         boost::mt19937 rng(std::time(NULL));
         boost::uniform_int<std::size_t>
             locality_range(0, localities.size() - 1);
 
-        raise_exception_future f(localities[locality_range(rng)]);
+        apply<raise_exception_action>(localities[locality_range(rng)]);
 
-        f.get();
+        thread_state_ex_enum statex = wait_timeout;
+
+        while (statex == wait_timeout)
+        {
+            // Schedule a wakeup in 500 milliseconds.
+            set_thread_state(get_self_id(), milliseconds(500), pending);
+
+            // Suspend this pxthread.
+            statex = get_self().yield(suspended);
+        }
     }
 
     finalize();
