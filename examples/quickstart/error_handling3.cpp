9e8a61e32ede0282d3017953fde424524359f194
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-08-16 16:31:31 +0000

    Allowing example to compile on Windows
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5312 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/error_handling3.cpp
+++ examples/quickstart/error_handling3.cpp
@@ -3,13 +3,6 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <ctime>
-
-#include <boost/random/mersenne_twister.hpp>
-#include <boost/random/uniform_int.hpp>
-
-#include <boost/date_time/posix_time/posix_time.hpp>
-
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/exception.hpp>
@@ -17,6 +10,12 @@
 #include <hpx/runtime/actions/plain_action.hpp>
 #include <hpx/runtime/threads/thread_helpers.hpp>
 #include <hpx/runtime/components/plain_component_factory.hpp>
+
+#include <ctime>
+
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
