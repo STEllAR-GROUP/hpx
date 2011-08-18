501d2fe2da07ec918b64943cd8ba28d0ecfc7cf0
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-08-18 13:37:34 +0000

    Add another error handling example.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5360 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/quickstart/error_handling4.cpp
@@ -0,0 +1,30 @@
+//  Copyright (c) 2011 Bryce Lelbach 
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/exception.hpp>
+
+using boost::program_options::variables_map;
+
+using hpx::no_success;
+using hpx::detail::diagnostic_information;
+
+///////////////////////////////////////////////////////////////////////////////
+int hpx_main(variables_map&)
+{
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    try {
+        HPX_THROW_EXCEPTION(no_success, "main", "unhandled exception"); 
+    }
+    catch (boost::exception const& be) {
+        std::cout << diagnostic_information(be) << std::endl;
+    }
+}
+
