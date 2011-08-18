5c014f7152a0975387e9f86326ff8cba1a04cd2f
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-08-18 03:10:02 +0000

    0.6.0 release candidate 2.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5356 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/version.hpp
+++ hpx/version.hpp
@@ -4,8 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_VERSION_JUN_26_2008_0831PM)
-#define HPX_VERSION_JUN_26_2008_0831PM
+#if !defined(HPX_VERSION_AUG_17_2011_1006PM)
+#define HPX_VERSION_AUG_17_2011_1006PM
 
 #include <string>
 
