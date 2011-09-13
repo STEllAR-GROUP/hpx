47a61da47d353fbe3f10bb9059bd5900c7c0ce79
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-09-13 15:48:29 +0000

    Make hello_world C++03 compliant.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5492 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/hello_world.cpp
+++ examples/quickstart/hello_world.cpp
@@ -5,16 +5,6 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <hpx/hpx_init.hpp>
-#include <hpx/lcos/eager_future.hpp>
-#include <hpx/lcos/future_wait.hpp>
-#include <hpx/lcos/async_future_wait.hpp>
-#include <hpx/runtime/actions/continuation_impl.hpp>
-#include <hpx/runtime/actions/plain_action.hpp>
-#include <hpx/runtime/components/plain_component_factory.hpp>
-#include <hpx/include/iostreams.hpp>
-
-
 #include <vector>
 #include <list>
 #include <set>
@@ -23,6 +13,12 @@
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
 
+#include <hpx/hpx_init.hpp>
+#include <hpx/lcos/eager_future.hpp>
+#include <hpx/runtime/actions/plain_action.hpp>
+#include <hpx/runtime/components/plain_component_factory.hpp>
+#include <hpx/include/iostreams.hpp>
+
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
 
@@ -30,20 +26,21 @@ using boost::format;
 
 using hpx::init;
 using hpx::finalize;
-using hpx::get_num_os_threads;
-using hpx::find_here;
-using hpx::find_all_localities;
 
 using hpx::naming::id_type;
+using hpx::naming::get_gid_from_prefix;
+
+using hpx::get_runtime;
 
 using hpx::applier::get_prefix_id;
+using hpx::applier::get_applier;
+using hpx::applier::register_work;
 
 using hpx::actions::plain_action0;
 using hpx::actions::plain_result_action1;
 
 using hpx::lcos::future_value;
 using hpx::lcos::eager_future;
-using hpx::lcos::wait;
 
 using hpx::threads::threadmanager_base;
 
@@ -64,11 +61,10 @@ std::size_t hello_world_worker(std::size_t desired)
         return desired;
     }
 
-    return std::size_t(-1);
+    else
+        return std::size_t(-1);
 }
 
-// Define the boilerplate code necessary for the function 'hello_world_worker'
-// to be invoked as an HPX action (by a HPX future)
 typedef plain_result_action1<std::size_t, std::size_t, hello_world_worker>
     hello_world_worker_action;
 
@@ -79,29 +75,32 @@ typedef eager_future<hello_world_worker_action> hello_world_worker_future;
 ///////////////////////////////////////////////////////////////////////////////
 void hello_world_foreman()
 {
-    std::size_t const shepherds = get_num_os_threads();
-    id_type const prefix = find_here();
+    const std::size_t shepherds
+        = get_runtime().get_config().get_num_shepherds();
+
+    const id_type prefix = get_applier().get_runtime_support_gid();
 
     std::set<std::size_t> attendence;
+
     for (std::size_t shepherd = 0; shepherd < shepherds; ++shepherd)
         attendence.insert(shepherd);
 
     while (!attendence.empty())
     {
-        std::vector<future_value<std::size_t> > futures;
-        BOOST_FOREACH(std::size_t shepherd, attendence)
-        {
-            futures.push_back(hello_world_worker_future(prefix, shepherd)); 
-        }
+        std::list<future_value<std::size_t> > futures;
 
-        // wait for all of the futures to return their values, we re-spawn the
-        // future until the action gets executed on the right OS-thread
-        wait(futures, [&](int, std::size_t t) { attendence.erase(t); });
+        BOOST_FOREACH(std::size_t shepherd, attendence)
+        { futures.push_back(hello_world_worker_future(prefix, shepherd)); }
+
+        BOOST_FOREACH(future_value<std::size_t> const& f, futures)
+        {
+            std::size_t r = f.get();
+            if (r != std::size_t(-1))
+                attendence.erase(r);
+        } 
     }
 }
 
-// Define the boilerplate code necessary for the function 'hello_world_foreman'
-// to be invoked as an HPX action
 typedef plain_action0<hello_world_foreman> hello_world_foreman_action;
 
 HPX_REGISTER_PLAIN_ACTION(hello_world_foreman_action);
@@ -112,15 +111,17 @@ typedef eager_future<hello_world_foreman_action> hello_world_foreman_future;
 int hpx_main(variables_map&)
 {
     {
-        std::vector<id_type> prefixes = find_all_localities();
+        std::list<future_value<void> > futures;
+        std::vector<id_type> prefixes;
 
-        std::vector<future_value<void> > futures;
+        get_applier().get_prefixes(prefixes);
+
         BOOST_FOREACH(id_type const& node, prefixes)
-        { 
-            futures.push_back(hello_world_foreman_future(node)); 
-        }
+        { futures.push_back(hello_world_foreman_future(node)); }
 
-        wait(futures);    // Wait for all IO to finish
+        // Wait for all IO to finish
+        BOOST_FOREACH(future_value<void> const& f, futures)
+        { f.get(); } 
     }
 
     // Initiate shutdown of the runtime system.
@@ -134,7 +135,7 @@ int main(int argc, char* argv[])
 {
     // Configure application-specific options.
     options_description
-       desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
+       desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
 
     // Initialize and run HPX.
     return init(desc_commandline, argc, argv);
