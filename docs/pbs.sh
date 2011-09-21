64edfde4b8df0d560773b770b69a28ef47fded39
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-09-21 12:58:59 +0000

    Fixes to the example script.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5520 5079c8e4-9419-0410-93bb-9e5ee278c886

--- docs/pbs.sh
+++ docs/pbs.sh
@@ -4,9 +4,11 @@
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+#PBS -l nodes=N:ppn=M
 
-APP_PATH    =
-APP_OPTIONS =
+APP_PATH=
+APP_OPTIONS=
 
-pbsdsh -u $APP_PATH --nodes=`echo $PBS_NODEFILE` $APP_OPTIONS
+pbsdsh -u $APP_PATH --nodes=`cat $PBS_NODEFILE` $APP_OPTIONS
 
