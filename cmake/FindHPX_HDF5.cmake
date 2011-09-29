9f5169da0edf57fd469148d5e55990a02f23cf5b
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-09-29 01:06:23 +0000

    Build system fixes.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5682 5079c8e4-9419-0410-93bb-9e5ee278c886

--- cmake/FindHPX_HDF5.cmake
+++ cmake/FindHPX_HDF5.cmake
@@ -9,9 +9,27 @@ if(NOT HPX_FINDPACKAGE_LOADED)
   include(HPX_FindPackage)
 endif()
 
+if(HDF5_USE_SYSTEM)
+  set(HDF5_CPP_USE_SYSTEM ON)
+endif()
+
+if($ENV{HDF5_ROOT})
+  set(ENV{HDF5_CPP_ROOT} $ENV{HDF5_ROOT})
+endif()
+
+if(HDF5_ROOT)
+  set(HDF5_CPP_ROOT ${HDF5_ROOT})
+endif()
+
 hpx_find_package(HDF5
-  LIBRARIES hdf5_cpp libhdf5_cpp
+  LIBRARIES hdf5 libhdf5
   LIBRARY_PATHS lib64 lib
   HEADERS H5pubconf.h
+  HEADER_PATHS include)
+
+hpx_find_package(HDF5_CPP
+  LIBRARIES hdf5_cpp libhdf5_cpp
+  LIBRARY_PATHS lib64 lib
+  HEADERS H5Cpp.h
   HEADER_PATHS include)
 
