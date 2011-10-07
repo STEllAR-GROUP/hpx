bea38e5d4e9f1cafb9186dfa5243733d396b2f2e
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-07 19:42:05 +0000

    Update balancing example.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5832 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/balancing/os_thread_num.cpp
+++ examples/balancing/os_thread_num.cpp
@@ -5,15 +5,10 @@
 
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
+#include <hpx/include/iostreams.hpp>
 #include <hpx/lcos/local_barrier.hpp>
 
-#include <cmath>
-#include <vector>
 #include <map>
-
-#include <ios>
-#include <iomanip>
-#include <iostream>
 
 #include <boost/foreach.hpp>
 #include <boost/lockfree/fifo.hpp>
@@ -35,87 +30,96 @@ using hpx::applier::register_work;
 using hpx::init;
 using hpx::finalize;
 
+using hpx::cout;
+using hpx::flush;
+
 ///////////////////////////////////////////////////////////////////////////////
-void get_os_thread_num(local_barrier& barr, fifo<std::size_t>& shepherds)
+// we use globals here to prevent the delay from being optimized away
+double global_scratch = 0;
+boost::uint64_t num_iterations = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+double delay()
 {
-    shepherds.enqueue(threadmanager_base::get_thread_num());
+    double d = 0.;
+    for (boost::uint64_t i = 0; i < num_iterations; ++i)
+        d += 1 / (2. * i + 1);
+    return d;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void get_os_thread_num(local_barrier& barr, fifo<std::size_t>& os_threads)
+{
+    global_scratch = delay();
+    os_threads.enqueue(threadmanager_base::get_thread_num());
     barr.wait();    
 }
+
+
+///////////////////////////////////////////////////////////////////////////////
+typedef std::map<std::size_t, std::size_t>
+    result_map;
+    
+typedef std::multimap<std::size_t, std::size_t, std::greater<std::size_t> >
+    sorter;
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(variables_map& vm)
 {
     {
-        typedef std::map<std::size_t, std::size_t>
-            result_map;
+        num_iterations = vm["delay-iterations"].as<boost::uint64_t>();
     
-        typedef std::multimap<
-            std::size_t, std::size_t, std::greater<std::size_t>
-        > sorter;
+        const bool csv = vm.count("csv");
     
-        std::size_t pxthreads = 0;
-    
-        if (vm.count("pxthreads"))
-            pxthreads = vm["pxthreads"].as<std::size_t>();
+        const std::size_t pxthreads = vm["pxthreads"].as<std::size_t>();
         
-        std::size_t iterations = 0;
-    
-        if (vm.count("iterations"))
-            iterations = vm["iterations"].as<std::size_t>();
-    
         result_map results;
     
-        for (std::size_t i = 0; i < iterations; ++i)
         {
             // Have the fifo preallocate the nodes.
-            fifo<std::size_t> shepherds(pxthreads); 
+            fifo<std::size_t> os_threads(pxthreads); 
     
             local_barrier barr(pxthreads + 1);
-   
-            const std::size_t one_percent =
-                std::floor(double(pxthreads) / 100.0);
 
-            for ( std::size_t percent_count = 0, percent = 0, j = 0
-                ; j < pxthreads
-                ; ++percent_count, ++j)
+            for (std::size_t j = 0; j < pxthreads; ++j)
             {
-                if (percent_count == one_percent && percent < 99)
-                {
-                    ++percent;
-                    percent_count = 0;
-                    std::cout << percent << "% of pxthreads created\n";  
-                }
-
                 register_work(boost::bind(&get_os_thread_num
                                         , boost::ref(barr)
-                                        , boost::ref(shepherds))
+                                        , boost::ref(os_threads))
                   , "get_os_thread_num"
                   , pending
                   , thread_priority_normal
                   , 0);
             }  
 
-            std::cout << "100% of pxthreads created\n";  
-  
             barr.wait(); // wait for all PX threads to enter the barrier
     
             std::size_t shepherd = 0;
     
-            while (shepherds.dequeue(&shepherd))
+            while (os_threads.dequeue(&shepherd))
                 ++results[shepherd];
         }
     
         sorter sort;
     
         BOOST_FOREACH(result_map::value_type const& result, results)
-        { sort.insert(sorter::value_type(result.second, result.first)); }  
+        {
+            sort.insert(sorter::value_type(result.second, result.first));
+        } 
         
-        BOOST_FOREACH(sorter::value_type const& result, sort) {
-            std::cout << "  " << std::setfill('0') << std::setw(4)
-                      << result.second << ","
-                      << result.first << "\n";
+        BOOST_FOREACH(sorter::value_type const& result, sort)
+        {
+            if (csv)
+                cout << ( boost::format("%1%,%2%\n")
+                        % result.second
+                        % result.first)
+                     << flush;
+            else
+                cout << ( boost::format("OS-thread %1% ran %2% PX-threads\n")
+                        % result.second
+                        % result.first)
+                     << flush;
         }
-
     }
 
     // initiate shutdown of the runtime system
@@ -128,16 +132,22 @@ int main(int argc, char* argv[])
 {
     // Configure application-specific options
     options_description
-       desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+       cmdline("Usage: " HPX_APPLICATION_STRING " [options]");
 
-    desc_commandline.add_options()
-        ("pxthreads,T", value<std::size_t>()->default_value(128), 
-            "the number of PX threads to invoke")
-        ("iterations", value<std::size_t>()->default_value(1), 
-            "the number of times to repeat the test") 
+    cmdline.add_options()
+        ( "pxthreads"
+        , value<std::size_t>()->default_value(128)
+        , "number of PX-threads to invoke")
+        
+        ( "delay-iterations"
+        , value<boost::uint64_t>()->default_value(65536) 
+        , "number of iterations in the delay loop")
+
+        ( "csv"
+        , "output results as csv (format: OS-thread,PX-threads)")
         ;
 
     // Initialize and run HPX
-    return init(desc_commandline, argc, argv);
+    return init(cmdline, argc, argv);
 }
 
