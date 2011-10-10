50c7ab9d79ca2c1d26ab408da6d116a865345d52
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-10 00:40:52 +0000

    Core: __attribute__((deprecated(x))) is supported by GCC 4.5+
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5903 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/config.hpp
+++ hpx/config.hpp
@@ -272,6 +272,8 @@
 #  define HPX_DEPRECATED_MSG "This function is deprecated and will be removed in the future."
 #  if defined(BOOST_MSVC)
 #    define HPX_DEPRECATED(x) __declspec(deprecated(x))
+#  elif (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))
+#    define HPX_DEPRECATED(x) __attribute__((__deprecated__(x)))
 #  elif (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4))
 #    define HPX_DEPRECATED(x) __attribute__((__deprecated__))
 #  endif
