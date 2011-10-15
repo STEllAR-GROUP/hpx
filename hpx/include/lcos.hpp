7b88ab522ec7146af473454d65850df3300b3b0b
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-15 07:46:48 +0000

    LCOs: Update LCOs master include header.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5997 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/include/lcos.hpp
+++ hpx/include/lcos.hpp
@@ -1,26 +1,24 @@
 //  Copyright (c) 2007-2011 Hartmut Kaiser
 //  Copyright (c) 2011      Bryce Lelbach
-// 
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(HPX_LCOS_JUN_27_2008_0820PM)
 #define HPX_LCOS_JUN_27_2008_0820PM
 
-#include <hpx/lcos/local_barrier.hpp>
-#include <hpx/lcos/barrier.hpp>
-
 #include <hpx/lcos/eager_future.hpp>
 #include <hpx/lcos/lazy_future.hpp>
 #include <hpx/lcos/async.hpp>
 
+#include <hpx/lcos/local_barrier.hpp>
 #include <hpx/lcos/local_counting_semaphore.hpp>
-
-#include <hpx/lcos/mutex.hpp>
+#include <hpx/lcos/local_mutex.hpp>
 #include <hpx/lcos/local_shared_mutex.hpp>
-#include <hpx/lcos/recursive_mutex.hpp>
+#include <hpx/lcos/local_recursive_mutex.hpp>
 
 #include <hpx/lcos/queue.hpp>
+#include <hpx/lcos/barrier.hpp>
 
 #include <hpx/lcos/async.hpp>
 #include <hpx/lcos/future_wait.hpp>
