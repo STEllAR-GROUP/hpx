01766aeea7faefb02a09128f2b28448703b433ba
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-15 07:47:07 +0000

    LCOs: Add convenience header for including pxthread synchronization primitives.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@5998 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/include/local_lcos.hpp
@@ -0,0 +1,16 @@
+//  Copyright (c) 2011 Bryce Adelstein-Lelbach and Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_%%UUID%%)
+#define HPX_%%UUID%%
+
+#include <hpx/lcos/local_barrier.hpp>
+#include <hpx/lcos/local_counting_semaphore.hpp>
+#include <hpx/lcos/local_mutex.hpp>
+#include <hpx/lcos/local_shared_mutex.hpp>
+#include <hpx/lcos/local_recursive_mutex.hpp>
+
+#endif
+
