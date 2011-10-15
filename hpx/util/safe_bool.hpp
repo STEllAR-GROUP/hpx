f7521447c4f12f3d735165bfa0845b3dcd06583c
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-15 17:31:33 +0000

    Util: Make safe_bool's operator() const.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6001 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/util/safe_bool.hpp
+++ hpx/util/safe_bool.hpp
@@ -2,7 +2,7 @@
     Copyright (c) 2003 Joel de Guzman
     Copyright (c) 2003-2011 Hartmut Kaiser
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
 #if !defined(HPX_UTIL_MAR_26_0200PM)
@@ -11,11 +11,14 @@
 namespace hpx { namespace util
 {
     template<typename Tag>
-    class safe_bool 
+    class safe_bool
     {
     public:
         typedef void (safe_bool::*result_type)();
-        result_type operator()(bool b) { return b ? &safe_bool::true_ : 0; }
+        result_type operator()(bool b) const
+        {
+            return b ? &safe_bool::true_ : 0;
+        }
 
     private:
         void true_() {}
