bdde342fc7de4d68dd34e17854a6ff58ef9c275a
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-16 15:44:51 +0000

    Core: Fix broken header.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6012 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/include/local_lcos.hpp
+++ hpx/include/local_lcos.hpp
@@ -3,8 +3,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_%%UUID%%)
-#define HPX_%%UUID%%
+#if !defined(HPX_6EB418B5_DC41_45A3_ADF4_C45A068F73D4)
+#define HPX_6EB418B5_DC41_45A3_ADF4_C45A068F73D4
 
 #include <hpx/lcos/local_barrier.hpp>
 #include <hpx/lcos/local_counting_semaphore.hpp>
