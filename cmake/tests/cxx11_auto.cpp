b72ac920e50abbcc3853c5f8af1cfd280809b333
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-19 12:45:07 +0000

    CMake: Add C++11 auto check.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6034 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ cmake/tests/cxx11_auto.cpp
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Adelstein-Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#include <algorithm>
+
+#include <boost/array.hpp>
+
+struct accumulator
+{
+  private:
+    unsigned& total_;
+  
+  public:
+    accumulator(
+        unsigned& total
+        )
+      : total_(total)
+    {}
+
+    void operator()(
+        unsigned x
+        )
+    {
+        total_ += x;
+    }
+};
+
+int main()
+{
+    boost::array<unsigned, 10> fib { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+    unsigned total = 0;
+
+    auto f = accumulator(total);
+
+    std::for_each(fib.begin(), fib.end(), f);
+
+    return !(88 == total);
+}
+
