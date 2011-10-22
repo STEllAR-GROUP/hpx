6273d3f053e5a86634653ec53b8b2fd0264a7ca6
Matthew Anderson <manderson@cct.lsu.edu>
2011-10-22 01:06:36 +0000

    fix
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6051 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/bfs/bfs/point.cpp
+++ examples/bfs/bfs/point.cpp
@@ -24,14 +24,14 @@ typedef hpx::components::managed_component<
     hpx::geometry::server::point
 > point_geometry_type;
 
-HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(point_geometry_type, point_geometry);
+HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(point_geometry_type, bfs_geometry);
 HPX_DEFINE_GET_COMPONENT_TYPE(point_geometry_type::wrapped_type);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Serialization support for the point_geometry actions
 HPX_REGISTER_ACTION_EX(
     point_geometry_type::wrapped_type::init_action,
-    point_geometry_init_action);
+    bfs_geometry_init_action);
 //HPX_REGISTER_ACTION_EX(
 //    point_geometry_type::wrapped_type::traverse_action,
 //    point_geometry_traverse_action);
