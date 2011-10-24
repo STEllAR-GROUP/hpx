c76f535bdca45f0643d07403b69a6575256481c3
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-24 03:58:52 +0000

    CMake: Make tests stricter and more comprehensive.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6076 5079c8e4-9419-0410-93bb-9e5ee278c886

--- cmake/tests/cxx11_std_function.cpp
+++ cmake/tests/cxx11_std_function.cpp
@@ -1,13 +1,28 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2011 Bryce Lelbach
+//  Copyright (c) 2011 Bryce Adelstein-Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/bind.hpp>
+#include <boost/function.hpp>
 
 #include <functional>
+
+namespace boost
+{
+    using ::_1;
+    using ::_2;
+    using ::_3;
+}
+
+namespace std
+{
+    using ::std::placeholders::_1;
+    using ::std::placeholders::_2;
+    using ::std::placeholders::_3;
+}
 
 struct functor
 {
@@ -27,6 +42,11 @@ int free_function(int i, int j)
     return i + j;
 }
 
+int take_reference(functor& f, int i, int j)
+{
+    return f(i, j);
+}
+
 int main()
 { 
     functor f;
@@ -34,10 +54,18 @@ int main()
     std::function<int(int, int)>
         f0(f)
       , f1(free_function)
-      , f2(boost::bind(&functor::operator(), &f, _1, _2));
+      , f2(boost::bind(&functor::operator(), &f, boost::_1, boost::_2))
+      , f3(std::bind(&functor::operator(), &f, std::_1, std::_2));
+
+    std::function<int(functor&, int, int)>
+        f4(boost::bind(&take_reference, boost::_1, boost::_2, boost::_3))
+      , f5(std::bind(&take_reference, std::_1, std::_2, std::_3));
 
     return !(   (f0(9, 8) == 17)
-             && (f1(15, 2) == 17)
-             && (f2(10, 7) == 17));
+            && (f1(15, 2) == 17)
+            && (f2(15, 2) == 17)
+            && (f3(3, 14) == 17)
+            && (f4(f, 6, 11) == 17)
+            && (f5(f, 1, 16) == 17));
 }
 
