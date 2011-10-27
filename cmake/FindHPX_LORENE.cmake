c776f2b6cdae0b2429384e1a15fe975d08100dbc
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-10-27 18:27:07 +0000

    CMake: Don't re-add macro definition with each CMake rerun to prevent unnecessary recompiles.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6129 5079c8e4-9419-0410-93bb-9e5ee278c886

--- cmake/FindHPX_LORENE.cmake
+++ cmake/FindHPX_LORENE.cmake
@@ -16,29 +16,20 @@ if(LORENE_ROOT)
   set(LORENE_F77_ROOT $ENV{HOME_LORENE})
 endif()
 
-#hpx_find_package(LORENE
-#  LIBRARIES lorene_export lorene lorenef77 liblorene_export liblorene liblorenef77
-#  LIBRARY_PATHS lib Lib
-#  HEADERS bin_bhns_extr.h
-#  HEADER_PATHS include include/C++/Include C++/Include)
-
 hpx_find_package(LORENE
   LIBRARIES lorene liblorene
-  LIBRARY_PATHS lib Lib
+  LIBRARY_PATHS lib64 lib Lib
   HEADERS bin_bhns_extr.h
   HEADER_PATHS include include/C++/Include C++/Include)
 
 hpx_find_package(LORENE_F77
   LIBRARIES lorenef77 liblorenef77
-  LIBRARY_PATHS lib Lib
+  LIBRARY_PATHS lib64 lib Lib
   HEADERS unites.h
   HEADER_PATHS include include/C++/Include C++/Include)
 
-#if(LORENE_FOUND AND NOT HPX_SET_LORENE_MACRO)
-#  set(HPX_SET_LORENE_MACRO ON CACHE BOOL "Added the Lorene detection macro" FORCE)
-#  add_definitions(-DLORENE_FOUND)
-#endif()
-if(LORENE_FOUND)
+if(LORENE_FOUND AND NOT HPX_SET_LORENE_MACRO)
+  set(HPX_SET_LORENE_MACRO ON CACHE BOOL "Added the Lorene detection macro" FORCE)
   add_definitions(-DLORENE_FOUND)
 endif()
 
