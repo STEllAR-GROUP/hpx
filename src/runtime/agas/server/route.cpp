78713ee05403b5534f6ec8018b70766d8f17884d
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-11-02 01:01:00 +0000

    AGAS: Fix error code handling in route, call through resolve instead of copying over the resolve code, make sure that the GVA is fully resolved before forwarding the parcel (fixes bug with routing parcels targetting managed components).
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6169 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/runtime/agas/server/route.cpp
+++ src/runtime/agas/server/route.cpp
@@ -5,117 +5,49 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
-#if !defined(HPX_5Z23KM0QLFNHPFQT9S9Q)
-#define HPX_5Z23KM0QLFNHPFQT9S9Q
-
-
 #include <hpx/runtime/actions/continuation_impl.hpp>
 #include <hpx/runtime/agas/server/primary_namespace.hpp>
-#include <hpx/runtime/naming/resolver_client.hpp>
 
-namespace hpx { namespace agas
-{
-
-namespace server
+namespace hpx { namespace agas { namespace server
 {
 
 bool primary_namespace::route(
     parcelset::parcel const& p
   , error_code& ec
-  )
+    )
 {
-    // protection of parcel's content?
-    bool resolved;
-    naming::address addr;
+    // TODO: protection of parcel's content?
     naming::gid_type id = p.get_destination();
     naming::strip_credit_from_gid(id);
-    mutex_type::scoped_lock l(mutex_);
-    
-    gva_table_type::const_iterator it = gvas_.lower_bound(id)
-                                  , begin = gvas_.begin()
-                                  , end = gvas_.end();
 
-    if (it != end)
-    {
-        if(it->first  == id)
-        {
-            if (&ec != &throws)
-                ec = make_success_code();
-            const gva g = it->second;
-            {
-                addr.locality_ = g.endpoint;
-                addr.type_ = g.type;
-                addr.address_ = g.lva();
-            }
-        resolved = true;
-            
-        }
-        else if (it != begin)
-        {
-            --it;
-            if((it->first + it->second.count) > id)
-            {
-                if(HPX_UNLIKELY(id.get_msb() != it->first.get_msb()))
-                {
-                    HPX_THROWS_IF(ec, internal_server_error
-                      , "primary_namespace::resolve_gid" 
-                      , "MSBs of lower and upper range bound do not match");
-                    //return response();
-                }
-                if (&ec != &throws)
-                    ec = make_success_code();
+    request req(primary_ns_resolve_gid, id);
 
-                const gva g = it->second.resolve(id, it->first);
-                {
-                    addr.locality_ = g.endpoint;
-                    addr.type_ = g.type;
-                    addr.address_ = g.lva();
-                }
-            resolved = true;
-            }
+    response rep = service(req, ec);
 
-        }
+    if (ec || success != rep.get_status())
+        return false;
 
-    }
-    else if (HPX_LIKELY(!gvas_.empty()))
-    {
-        --it;
+    // Resolve the gva to the real resolved address (which is just a gva
+    // with as fully resolved LVA and an offset of zero).
+    gva const g = rep.get_gva().resolve(p.get_destination()
+                                      , rep.get_base_gid());
 
-        if((it->first + it->second.count) > id)
-        {
-            if (HPX_UNLIKELY(id.get_msb() != it->first.get_msb()))
-            {
-                HPX_THROWS_IF(ec, internal_server_error
-                  , "primary_namespace::resolve_gid" 
-                  , "MSBs of lower and upper range bound do not match");
-                //return response();
-            }
+    naming::address addr;
 
-            if (&ec != &throws)
-                ec = make_success_code();
-
-                const gva g = it->second.resolve(id, it->first);
-                {
-                    addr.locality_ = g.endpoint;
-                    addr.type_ = g.type;
-                    addr.address_ = g.lva();
-                }
-                resolved = true;
-        }
-    }
+    addr.locality_ = g.endpoint;
+    addr.type_ = g.type;
+    addr.address_ = g.lva();
 
     parcelset::parcel p_temp = p;
     p_temp.set_destination(id);
     p_temp.set_destination_addr(addr);
-    //assign the parcel to parcelhandler
-    hpx::applier::get_applier().get_parcel_handler().put_parcel(p_temp);
-    //TO DO: update sender's cache for the destination id rosolution
 
-    if(resolved)
-        return true;
-    return false;
+    // Assign the parcel to parcelhandler.
+    get_runtime().get_parcel_handler().put_parcel(p_temp);
+
+    // TODO: update sender's cache for the destination id rosolution
+    return true;
 }
 
 }}}
 
-#endif // HPX_5Z23KM0QLFNHPFQT9S9Q
