6959bb059e583f134ce0d6c72b00f0df4c059fe2
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-11-06 05:02:53 +0000

    Actions: Circumvent the circular dependencies in continuation, negating the need for continuation_impl.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6225 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/actions/continuation.hpp
+++ hpx/runtime/actions/continuation.hpp
@@ -15,6 +15,7 @@
 
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/exception.hpp>
+#include <hpx/util/logging.hpp>
 #include <hpx/runtime/naming/name.hpp>
 
 #include <hpx/config/warnings_prefix.hpp>
@@ -24,7 +25,17 @@
 #define HPX_CONTINUATION_VERSION 0x10
 
 ///////////////////////////////////////////////////////////////////////////////
-namespace hpx { namespace actions
+namespace hpx { namespace applier {
+
+    template <typename Action, typename Arg0>
+    inline bool
+    apply(naming::id_type const& gid, BOOST_FWD_REF(Arg0) arg0);
+
+    template <typename Action>
+    inline bool apply(naming::id_type const& gid);
+}
+
+namespace actions
 {
     // parcel continuations are simply lists of global ids of LCO's to call
     // set_event on
@@ -41,11 +52,17 @@ namespace hpx { namespace actions
         ///
         void trigger();
 
-        ///
         template <typename Arg0>
-        void trigger(Arg0 const& arg0);
-        template <typename Arg0>
-        void trigger(BOOST_FWD_REF(Arg0) arg0);
+        void trigger(BOOST_FWD_REF(Arg0) arg0)
+        {
+            typedef typename
+                lcos::template base_lco_with_value<Arg0>::set_result_action
+            action_type;
+    
+            LLCO_(info) << "continuation::trigger(" << gid_ << ")";
+    
+            applier::apply<action_type>(gid_, boost::forward<Arg0>(arg0));
+        }
 
         ///
         void trigger_error(boost::exception_ptr const& e);
--- hpx/runtime/actions/continuation_impl.hpp
+++ hpx/runtime/actions/continuation_impl.hpp
@@ -6,45 +6,6 @@
 #if !defined(HPX_RUNTIME_ACTIONS_CONTINUATION_IMPL_JUN_20_2008_0851PM)
 #define HPX_RUNTIME_ACTIONS_CONTINUATION_IMPL_JUN_20_2008_0851PM
 
-#include <vector>
-
-#include <hpx/hpx_fwd.hpp>
-#include <hpx/runtime/naming/name.hpp>
 #include <hpx/runtime/actions/continuation.hpp>
-#include <hpx/runtime/actions/component_action.hpp>
-#include <hpx/runtime/applier/apply.hpp>
-#include <hpx/lcos/base_lco.hpp>
-
-///////////////////////////////////////////////////////////////////////////////
-namespace hpx { namespace actions
-{
-    ///////////////////////////////////////////////////////////////////////////
-    template <typename Arg0>
-    inline void
-    continuation::trigger(Arg0 const& arg0)
-    {
-        typedef typename
-            lcos::template base_lco_with_value<Arg0>::set_result_action
-        action_type;
-
-        LLCO_(info) << "promise::set_with_value(" << gid_ << ")";
-
-        applier::apply<action_type>(gid_, arg0);
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    template <typename Arg0>
-    inline void
-    continuation::trigger(BOOST_FWD_REF(Arg0) arg0)
-    {
-        typedef typename
-            lcos::template base_lco_with_value<Arg0>::set_result_action
-        action_type;
-
-        LLCO_(info) << "promise::set_with_value(" << gid_ << ")";
-
-        applier::apply<action_type>(gid_, boost::forward<Arg0>(arg0));
-    }
-}}
 
 #endif
