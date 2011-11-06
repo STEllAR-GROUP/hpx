6159e2f2f7fc9aa4a8e1dcb882a4bc8bd2daaf5c
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-11-06 05:06:27 +0000

    Actions: Add a warning message about this header being deprecated.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6226 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/actions/continuation_impl.hpp
+++ hpx/runtime/actions/continuation_impl.hpp
@@ -6,6 +6,12 @@
 #if !defined(HPX_RUNTIME_ACTIONS_CONTINUATION_IMPL_JUN_20_2008_0851PM)
 #define HPX_RUNTIME_ACTIONS_CONTINUATION_IMPL_JUN_20_2008_0851PM
 
+#if defined(BOOST_WINDOWS) 
+#  pragma message ("Warning: This header is deprecated. Please use hpx/runtime/actions/continuation.hpp instead.")
+#else
+#  warning "This header is deprecated. Please use hpx/runtime/actions/continuation.hpp instead."
+#endif
+
 #include <hpx/runtime/actions/continuation.hpp>
 
 #endif
