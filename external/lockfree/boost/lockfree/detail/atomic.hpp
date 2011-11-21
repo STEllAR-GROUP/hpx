dbb79a13c9a0df8c033c5e4d6124236b8ba50ebe
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-11-21 18:40:10 +0000

    Disabled std:;atomics in lockfree for now
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6377 5079c8e4-9419-0410-93bb-9e5ee278c886

--- external/lockfree/boost/lockfree/detail/atomic.hpp
+++ external/lockfree/boost/lockfree/detail/atomic.hpp
@@ -1,12 +1,14 @@
 #ifndef BOOST_LOCKFREE_DETAIL_ATOMIC_HPP
 #define BOOST_LOCKFREE_DETAIL_ATOMIC_HPP
 
+#if !defined(BOOST_NO_0X_HDR_ATOMIC)
 #ifdef __GNUC__
 # if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 6) || !defined(__GXX_EXPERIMENTAL_CXX0X__)
 #  define BOOST_NO_0X_HDR_ATOMIC
 # endif
 #else
 # define BOOST_NO_0X_HDR_ATOMIC
+#endif
 #endif
 
 #ifdef BOOST_NO_0X_HDR_ATOMIC
