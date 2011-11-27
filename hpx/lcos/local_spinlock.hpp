793c2bad094cb38bbf710683e03619d71fefa485
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-11-27 23:36:20 +0000

    LCOs: Revert changes to util::spinlock, add lcos::local_spinlock.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6426 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ hpx/lcos/local_spinlock.hpp
@@ -0,0 +1,127 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//  Copyright (c) 2011 Hartmut Kaiser
+//  Copyright (c) 2008 Peter Dimov
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(HPX_B3A83B49_92E0_4150_A551_488F9F5E1113)
+#define HPX_B3A83B49_92E0_4150_A551_488F9F5E1113
+
+#include <boost/noncopyable.hpp>
+#include <boost/thread/locks.hpp>
+
+#include <boost/smart_ptr/detail/yield_k.hpp>
+
+#if defined(BOOST_WINDOWS)
+#  include <boost/smart_ptr/detail/spinlock_w32.hpp>
+#else
+#  include <boost/smart_ptr/detail/spinlock_sync.hpp>
+#endif
+
+#include <hpx/util/itt_notify.hpp>
+
+namespace hpx { namespace lcos
+{
+
+/// boost::mutex-compatible spinlock class
+struct local_spinlock : boost::noncopyable
+{
+  private:
+    boost::uint64_t v_;
+
+    ///////////////////////////////////////////////////////////////////////////
+    static void yield(std::size_t k)
+    {
+#if defined(BOOST_SMT_PAUSE)
+        if(k < 16)
+        {
+            BOOST_SMT_PAUSE
+        }
+#endif
+        else if(k < 32)
+        {
+            threads::suspend();
+        }
+        else
+        {
+            threads::suspend(boost::posix_time::microseconds(100));
+        }
+    }
+
+
+  public:
+    local_spinlock() : v_(0)
+    {
+        HPX_ITT_SYNC_CREATE(this, "lcos::local_spinlock", "");
+    }
+
+    ~local_spinlock()
+    {
+        HPX_ITT_SYNC_DESTROY(this);
+    }
+
+    void lock()
+    {
+        HPX_ITT_SYNC_PREPARE(this);
+
+        for (std::size_t k = 0; !try_lock(); ++k)
+        {
+            local_spinlock::yield(k); 
+        } 
+
+        HPX_ITT_SYNC_ACQUIRED(this);
+    }
+
+    bool try_lock()
+    {
+        HPX_ITT_SYNC_PREPARE(this);
+
+#if defined(BOOST_WINDOWS)
+        boost::uint64_t r = BOOST_INTERLOCKED_EXCHANGE(&v_, 1);
+        BOOST_COMPILER_FENCE
+#else
+        boost::uint64_t r = __sync_lock_test_and_set(&v_, 1);
+#endif
+
+        if (r == 0) {
+            HPX_ITT_SYNC_ACQUIRED(this);
+            return true;
+        }
+
+        HPX_ITT_SYNC_CANCEL(this);
+        return false;
+#endif
+    }
+
+    void unlock()
+    {
+        HPX_ITT_SYNC_RELEASING(this);
+
+#if defined(BOOST_WINDOWS)
+        BOOST_COMPILER_FENCE
+        *const_cast<boost::uint64_t volatile*>(&v_) = 0;
+#else
+        __sync_lock_release(&v_);
+#endif
+
+        HPX_ITT_SYNC_RELEASED(this);
+    }
+
+    typedef boost::uint64_t* native_handle_type;
+
+    native_handle_type native_handle()
+    {
+        return &m;
+    }
+
+    typedef boost::unique_lock<local_spinlock> scoped_lock;
+    typedef boost::detail::try_lock_wrapper<local_spinlock> scoped_try_lock;
+};
+
+}}
+
+#endif // HPX_B3A83B49_92E0_4150_A551_488F9F5E1113
+
--- hpx/util/spinlock.hpp
+++ hpx/util/spinlock.hpp
@@ -1,7 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
 //  Copyright (c) 2011 Bryce Lelbach
-//  Copyright (c) 2011 Hartmut Kaiser
-//  Copyright (c) 2008 Peter Dimov
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,112 +8,27 @@
 #if !defined(HPX_DF595582_FEBC_4EE0_A606_A1EEB171D770)
 #define HPX_DF595582_FEBC_4EE0_A606_A1EEB171D770
 
-#include <hpx/hpx_fwd.hpp>
-#include <hpx/runtime/threads/thread_helpers.hpp>
-#include <hpx/util/itt_notify.hpp>
-
 #include <boost/noncopyable.hpp>
 #include <boost/thread/locks.hpp>
 #include <boost/smart_ptr/detail/spinlock.hpp>
 
-///////////////////////////////////////////////////////////////////////////////
-namespace hpx { namespace util { namespace detail 
-{
-#if defined(BOOST_WINDOWS)
-    ///////////////////////////////////////////////////////////////////////////
-    inline void yield(unsigned k)
-    {
-        if(k < 4)
-        {
-        }
-#if defined( BOOST_SMT_PAUSE )
-        else if(k < 16)
-        {
-            BOOST_SMT_PAUSE
-        }
-#endif
-        else if(k < 32)
-        {
-            threads::suspend();
-        }
-        else
-        {
-            threads::suspend(boost::posix_time::microseconds(100));
-        }
-    }
+#include <hpx/util/itt_notify.hpp>
 
-    ///////////////////////////////////////////////////////////////////////////
-    class spinlock
-    {
-    public:
-        long v_;
-
-    public:
-        bool try_lock()
-        {
-            long r = BOOST_INTERLOCKED_EXCHANGE(&v_, 1);
-
-            BOOST_COMPILER_FENCE
-
-            return r == 0;
-        }
-
-        void lock()
-        {
-            for (unsigned k = 0; !try_lock(); ++k)
-            {
-                yield(k);
-            }
-        }
-
-        void unlock()
-        {
-            BOOST_COMPILER_FENCE
-            *const_cast<long volatile*>(&v_) = 0;
-        }
-
-    public:
-        class scoped_lock
-        {
-        private:
-            spinlock & sp_;
-
-            scoped_lock( scoped_lock const & );
-            scoped_lock & operator=( scoped_lock const & );
-
-        public:
-            explicit scoped_lock( spinlock & sp ): sp_( sp )
-            {
-                sp.lock();
-            }
-
-            ~scoped_lock()
-            {
-                sp_.unlock();
-            }
-        };
-    };
-#else
-    using boost::detail::spinlock;
-#endif
-}}}
-
-///////////////////////////////////////////////////////////////////////////////
 namespace hpx { namespace util
 {
 
-// boost::mutex-compatible spinlock class
+/// boost::mutex-compatible spinlock class
 struct spinlock : boost::noncopyable
 {
   private:
-    hpx::util::detail::spinlock m;
+    boost::detail::spinlock m;
 
   public:
     spinlock()
     {
         HPX_ITT_SYNC_CREATE(this, "util::spinlock", "");
 
-        hpx::util::detail::spinlock l = BOOST_DETAIL_SPINLOCK_INIT;
+        boost::detail::spinlock l = BOOST_DETAIL_SPINLOCK_INIT;
         m = l;
     }
 
@@ -149,7 +62,7 @@ struct spinlock : boost::noncopyable
         HPX_ITT_SYNC_RELEASED(this);
     }
 
-    typedef hpx::util::detail::spinlock* native_handle_type;
+    typedef boost::detail::spinlock* native_handle_type;
 
     native_handle_type native_handle()
     {
