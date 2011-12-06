ee94e9d3f267e7db3229a9861d0169ef4135c540
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2011-12-06 17:31:36 +0000

    Cleaning up
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6509 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/runtime/threads/policies/thread_queue.hpp
+++ hpx/runtime/threads/policies/thread_queue.hpp
@@ -364,53 +364,34 @@ namespace hpx { namespace threads { namespace policies
             return invalid_thread_id;     // thread has not been created yet
         }
 
-        void move_work_items_from(
-            thread_queue<Global> *src
-          , boost::int64_t count
-          , std::size_t num_thread
-        )
+        void move_work_items_from(thread_queue<Global> *src, 
+            boost::int64_t count, std::size_t num_thread)
         {
             boost::int64_t actual_count = 0;
-            for(;;)//boost::int64_t i = 0; i < src->work_items_count_; ++i)
+            threads::thread* trd;
+            while (dequeue(src->work_items_, trd, num_thread))
             {
-                threads::thread * trd;
-                if(dequeue(src->work_items_, trd, num_thread))
-                {
-                    --src->work_items_count_;
-                    enqueue(work_items_, trd, num_thread);
-                    ++work_items_count_;
-                    ++actual_count;
-                    if(actual_count == count)
-                    {
-                        break;
-                    }
-                }
-                else
+                --src->work_items_count_;
+                enqueue(work_items_, trd, num_thread);
+                ++work_items_count_;
+                ++actual_count;
+                if (actual_count == count)
                     break;
             }
         }
 
-        void move_task_items_from(
-            thread_queue<Global> *src
-          , boost::int64_t count
-        )
+        void move_task_items_from(thread_queue<Global> *src, 
+            boost::int64_t count)
         {
             boost::int64_t actual_count = 0;
-            for(;;)//boost::int64_t i = 0; i < src->new_tasks_count_; ++i)
+            task_description* td;
+            while (src->new_tasks_.dequeue(td))
             {
-                task_description * td;
-                if(src->new_tasks_.dequeue(td))
-                {
-                    --src->new_tasks_count_;
-                    new_tasks_.enqueue(td);
-                    ++new_tasks_count_;
-                    ++actual_count;
-                    if(actual_count == count)
-                    {
-                        break;
-                    }
-                }
-                else
+                --src->new_tasks_count_;
+                new_tasks_.enqueue(td);
+                ++new_tasks_count_;
+                ++actual_count;
+                if (actual_count == count)
                     break;
             }
         }
