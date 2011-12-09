10af68ac5d5de922adec9bd29ec560312e1be3c8
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-12-09 22:18:00 +0000

    Backport r6537.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/hpx/release@6543 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/accumulator/managed_accumulator_client.cpp
+++ examples/accumulator/managed_accumulator_client.cpp
@@ -9,9 +9,18 @@
 #include "accumulators/managed_accumulator.hpp"
 
 #include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
 char const* const help
     = "commands: reset, add [amount], query, help, quit";
+
+void ignore_whitespace(std::istream& is)
+{
+    while (' ' == is.peek() || '\t' == is.peek())
+        is.get();
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(boost::program_options::variables_map&)
@@ -24,37 +33,53 @@ int hpx_main(boost::program_options::variables_map&)
         // Print out the available commands.
         std::cout << help << std::endl << "> ";
 
-        std::string cmd;
-        std::cin >> cmd;
+        std::cin >> std::noskipws;
+
+        std::string line;
 
         // Enter the interpreter loop.
-        while (std::cin.good())
+        while (std::getline(std::cin, line).good())
         {
-            if (cmd == "reset")
-                accu.reset_sync();
+            std::vector<std::string> cmd;
 
-            else if (cmd == "add")
+            boost::algorithm::trim(line);
+
+            boost::algorithm::split(cmd, line,
+                boost::algorithm::is_any_of(" \t\n"),
+                boost::algorithm::token_compress_on);
+
+            if (!cmd.empty() && !cmd[0].empty())
             {
-                std::string arg;
-                std::cin >> arg;
-                accu.add_sync(boost::lexical_cast<boost::uint64_t>(arg));
+                if (cmd[0] == "reset")
+                    accu.reset_sync();
+
+                else if (cmd[0] == "add")
+                {
+                    if (cmd.size() != 2)
+                        std::cout << "error: invalid command '" 
+                                  << line << "'" << std::endl
+                                  << help << std::endl;
+                    else
+                        accu.add_sync(boost::lexical_cast<boost::uint64_t>
+                            (cmd[1]));
+                }
+
+                else if (cmd[0] == "query")
+                    std::cout << accu.query_sync() << std::endl;
+
+                else if (cmd[0] == "help")
+                    std::cout << help << std::endl;
+
+                else if (cmd[0] == "quit")
+                    break;
+
+                else 
+                    std::cout << "error: invalid command '" 
+                              << line << "'" << std::endl
+                              << help << std::endl;
             }
 
-            else if (cmd == "query")
-                std::cout << accu.query_sync() << std::endl;
-
-            else if (cmd == "help")
-                std::cout << help << std::endl;
-
-            else if (cmd == "quit")
-                break;
-
-            else
-                std::cout << "error: invalid command" << std::endl
-                          << help << std::endl;
-
             std::cout << "> ";
-            std::cin >> cmd;
         }
     }
 
