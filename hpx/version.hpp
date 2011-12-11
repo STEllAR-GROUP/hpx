70246cc94ffc12e0c5b408573e44397cd2b690f3
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-12-11 02:43:59 +0000

    Release: Force version.hpp substitution bump.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/tags/hpx/0.7.0@6561 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/version.hpp
+++ hpx/version.hpp
@@ -4,8 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_VERSION_AUG_18_2011_0854PM)
-#define HPX_VERSION_AUG_18_2011_0854PM
+#if !defined(HPX_VERSION_DEC_09_2011_2043PM)
+#define HPX_VERSION_DEC_09_2011_2043PM
 
 #include <string>
 
