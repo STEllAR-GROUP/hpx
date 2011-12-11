b2d2cdbc7614db4a8027d7608674bb3b971e4928
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2011-12-11 02:47:04 +0000

    One more last minute fix.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/tags/hpx/0.7.0@6562 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/version.hpp
+++ hpx/version.hpp
@@ -5,7 +5,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(HPX_VERSION_DEC_09_2011_2043PM)
-#define HPX_VERSION_DEC_09_2011_2043PM
+#define HPX_VERSION_DEC_09_2011_2043PM 
 
 #include <string>
 
