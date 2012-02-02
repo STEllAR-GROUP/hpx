9a2d65b089f42b7a9668f59ffa56529b8a324bbd
Matthew Anderson <manderson@cct.lsu.edu>
2012-02-02 22:25:34 +0000

    adding validation
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6919 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/bfs/graph500_client.cpp
+++ examples/bfs/graph500_client.cpp
@@ -20,35 +20,16 @@ static int compare_doubles(const void* a, const void* b) {
   return (aa < bb) ? -1 : (aa == bb) ? 0 : 1;
 }
 
+// this routine mirrors the matlab validation routine
+int validate(std::vector<std::size_t> const& parent,
+             std::vector<std::size_t> const& levels,
+             std::vector<std::size_t> const& parentindex,
+             std::vector<std::size_t> const& nodelist,
+             std::vector<std::size_t> const& neighborlist,
+             std::size_t searchkey,std::size_t &num_edges);
+
 void get_statistics(std::vector<double> const& x, double &minimum, double &mean, double &stdev, double &firstquartile,
-                                                  double &median, double &thirdquartile, double &maximum)
-{
-  // Compute mean
-  double temp = 0.0;
-  std::size_t n = x.size();
-  for (std::size_t i=0;i<n;i++) temp += x[i];
-  temp /= n;
-  mean = temp;
-
-  // Compute std dev
-  temp = 0.0;
-  for (std::size_t i=0;i<n;i++) temp += (x[i] - mean)*(x[i]-mean);
-  temp /= n-1;
-  stdev = sqrt(temp);
-
-  // Sort x
-  std::vector<double> xx;    
-  xx.resize(n);
-  for (std::size_t i=0;i<n;i++) {
-    xx[i] = x[i];
-  }
-  qsort(&*xx.begin(),n,sizeof(double),compare_doubles);
-  minimum = xx[0];
-  firstquartile = (xx[(n - 1) / 4] + xx[n / 4]) * .5;
-  median = (xx[(n - 1) / 2] + xx[n / 2]) * .5;
-  thirdquartile = (xx[n - 1 - (n - 1) / 4] + xx[n - 1 - n / 4]) * .5; 
-  maximum = xx[n - 1];
-};
+                                                  double &median, double &thirdquartile, double &maximum);
 
 void make_random_numbers(
        /* in */ int64_t nvalues    /* Number of values to generate */,
