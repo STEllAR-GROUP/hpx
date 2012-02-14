27a3a8083788256eac6388442e7ae1a27d016089
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2012-02-14 02:08:34 +0000

    Adding proper handling of parcel priority
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7021 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/runtime/parcelset/parcelport_connection.cpp
+++ src/runtime/parcelset/parcelport_connection.cpp
@@ -20,12 +20,15 @@ namespace hpx { namespace parcelset
     ///////////////////////////////////////////////////////////////////////////
     void parcelport_connection::set_parcel(std::vector<parcel> const& pv)
     {
-        typedef util::container_device<std::vector<char> > io_device_type;
+        // we choose the highest priority of all parcels for this message
+        threads::thread_priority priority = threads::thread_priority_default;
 
         // guard against serialization errors
         try {
             // create a special io stream on top of out_buffer_
             out_buffer_.clear();
+
+            typedef util::container_device<std::vector<char> > io_device_type;
             boost::iostreams::stream<io_device_type> io(out_buffer_);
 
             // Serialize the data
@@ -37,24 +40,24 @@ namespace hpx { namespace parcelset
 
             std::size_t count = pv.size();
             archive << count;
+
             BOOST_FOREACH(parcel const & p, pv)
             {
+                priority = (std::max)(p.get_thread_priority(), priority);
                 archive << p;
             }
         }
         catch (std::exception const& e) {
             hpx::util::osstream strm;
             // TODO: add proper error report
-            strm << "parcelport: parcel serialization failed: " << e.what()
-                 << " parcel(";// << p << ")";
+            strm << "parcelport: parcel serialization failed: " << e.what();
             HPX_THROW_EXCEPTION(no_success,
                 "parcelport_connection::set_parcel",
                 hpx::util::osstream_get_string(strm));
             return;
         }
 
-        // TODO: better priority
-        out_priority_ = boost::integer::ulittle8_t(pv[0].get_thread_priority());
+        out_priority_ = boost::integer::ulittle8_t(priority);
         out_size_ = out_buffer_.size();
     }
 }}
