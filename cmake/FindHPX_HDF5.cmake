a8e0579c3632074858c46c27831e0baa2b1b87ae
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-02-17 23:02:38 +0000

    CMake: Fix fortran/c++ mixed compilation.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7051 5079c8e4-9419-0410-93bb-9e5ee278c886

--- cmake/FindHPX_HDF5.cmake
+++ cmake/FindHPX_HDF5.cmake
@@ -11,14 +11,17 @@ endif()
 
 if(HDF5_USE_SYSTEM)
   set(HDF5_CPP_USE_SYSTEM ON)
+  set(HDF5_FORTRAN_USE_SYSTEM ON)
 endif()
 
 if($ENV{HDF5_ROOT})
   set(ENV{HDF5_CPP_ROOT} $ENV{HDF5_ROOT})
+  set(ENV{HDF5_FORTRAN_ROOT} $ENV{HDF5_ROOT})
 endif()
 
 if(HDF5_ROOT)
   set(HDF5_CPP_ROOT ${HDF5_ROOT})
+  set(HDF5_FORTRAN_ROOT ${HDF5_ROOT})
 endif()
 
 hpx_find_package(HDF5
@@ -32,4 +35,10 @@ hpx_find_package(HDF5_CPP
   LIBRARY_PATHS lib64 lib
   HEADERS H5Cpp.h
   HEADER_PATHS include include/cpp)
+
+hpx_find_package(HDF5_FORTRAN
+  LIBRARIES hdf5_fortran libhdf5_fortran hdf5_fortrandll
+  LIBRARY_PATHS lib64 lib
+  HEADERS hdf5.mod
+  HEADER_PATHS include)
 
