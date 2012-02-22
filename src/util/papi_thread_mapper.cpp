6add4cf874d0769c609e10d772ce14dd9f77af03
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2012-02-22 23:15:43 +0000

    Making things compile on Windows
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7097 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/util/papi_thread_mapper.cpp
+++ src/util/papi_thread_mapper.cpp
@@ -3,9 +3,10 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/format.hpp>
-
+#include <hpx/hpx_fwd.hpp>
 #include <hpx/util/papi_thread_mapper.hpp>
+
+#include <boost/format.hpp>
 
 #if defined(HPX_HAVE_PAPI)
 
