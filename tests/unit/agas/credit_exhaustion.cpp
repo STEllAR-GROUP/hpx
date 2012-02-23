4a4830bbfb4126cccf373553de3bb05c12443004
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-02-23 01:31:36 +0000

    AGAS: Check for object destruction in the credit exhaustion test.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7103 5079c8e4-9419-0410-93bb-9e5ee278c886

--- tests/unit/agas/credit_exhaustion.cpp
+++ tests/unit/agas/credit_exhaustion.cpp
@@ -10,10 +10,11 @@
 #include <hpx/include/plain_actions.hpp>
 #include <hpx/lcos/async.hpp>
 
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/assign/std/vector.hpp>
+
 #include <tests/unit/agas/components/simple_refcnt_checker.hpp>
 #include <tests/unit/agas/components/managed_refcnt_checker.hpp>
-
-#include <boost/assign/std/vector.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
@@ -22,6 +23,8 @@ using boost::program_options::value;
 using hpx::init;
 using hpx::finalize;
 using hpx::find_here;
+
+using boost::posix_time::milliseconds;
 
 using hpx::naming::id_type;
 using hpx::naming::get_management_type_name;
@@ -32,12 +35,14 @@ using hpx::components::get_component_type;
 
 using hpx::applier::get_applier;
 
+using hpx::agas::garbage_collect;
+
 using hpx::actions::plain_action3;
 
 using hpx::lcos::async;
 
-using hpx::test::simple_object;
-using hpx::test::managed_object;
+using hpx::test::simple_refcnt_monitor;
+using hpx::test::managed_refcnt_monitor;
 
 using hpx::util::report_errors;
 
@@ -52,18 +57,18 @@ void split(
     );
 
 typedef plain_action3<
-    // arguments
+    // Arguments.
     id_type const&
   , id_type const&
   , boost::uint16_t
-    // function
+    // Function.
   , split
 > split_action;
 
 HPX_REGISTER_PLAIN_ACTION(split_action);
 
 ///////////////////////////////////////////////////////////////////////////////
-// helper functions
+// Helper functions.
 inline boost::uint32_t get_credit(id_type const& id)
 {
     return get_credit_from_gid(id.get_gid());
@@ -76,7 +81,10 @@ void split(
   , boost::uint16_t old_credit
     )
 {
-    std::cout << "[" << find_here() << "/" << target << "]: " << old_credit << ", " << get_credit(target) << "\n";
+    cout << "[" << find_here() << "/" << target << "]: "
+         << old_credit << ", " << get_credit(target) << ", "
+         << get_management_type_name(target.get_management_type())
+         << "\n" << flush;
 
     // If we have more credits than the sender, then we're done.
     if (old_credit < get_credit(target))
@@ -90,6 +98,8 @@ void split(
 
     // Recursively call split on the sender locality.
     async<split_action>(from, here, target, get_credit(target)).get();
+
+    cout << "  after split: " << get_credit(target) << "\n" << flush;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -100,29 +110,41 @@ void hpx_test_main(
     variables_map& vm
     )
 {
+    boost::uint64_t const delay = vm["delay"].as<boost::uint64_t>();
+
+    typedef typename Client::server_type server_type;
+
+    component_type ctype = get_component_type<server_type>();
+    std::vector<id_type> remote_localities = hpx::find_remote_localities(ctype);
+
+    if (remote_localities.empty())
+        throw std::logic_error("this test cannot be run on one locality");
+
+    id_type const here = find_here();
+
+    Client monitor(here);
+
     {
-        typedef typename Client::server_type server_type;
+        id_type id = monitor.detach();
 
-        component_type ctype = get_component_type<server_type>();
-        std::vector<id_type> remote_localities = hpx::find_remote_localities(ctype);
-
-        if (remote_localities.empty())
-            throw std::logic_error("this test cannot be run on one locality");
-
-        id_type const here = find_here();
-
-        Client object(here);
-
-        cout << "id: " << object.get_gid() << " "
-             << get_management_type_name
-                    (object.get_gid().get_management_type()) << "\n"
+        cout << "id: " << id << " "
+             << get_management_type_name(id.get_management_type()) << "\n"
              << flush;
 
         async<split_action>(remote_localities[0]
                           , here
-                          , object.get_gid()
-                          , get_credit(object.get_gid())).get();
+                          , id
+                          , get_credit(id)).get();
+
+        cout << "after split: " << get_credit(id) << "\n" << flush;
     }
+
+    // Flush pending reference counting operations.
+    garbage_collect(remote_localities[0]);
+    garbage_collect();
+
+    // The component should be out of scope now.
+    HPX_TEST_EQ(true, monitor.ready(milliseconds(delay)));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -135,13 +157,13 @@ int hpx_main(
              << "simple component test\n"
              << std::string(80, '#') << "\n" << flush;
 
-        hpx_test_main<simple_object>(vm);
+        hpx_test_main<simple_refcnt_monitor>(vm);
 
         cout << std::string(80, '#') << "\n"
              << "managed component test\n"
              << std::string(80, '#') << "\n" << flush;
 
-        hpx_test_main<managed_object>(vm);
+        hpx_test_main<managed_refcnt_monitor>(vm);
     }
 
     finalize();
