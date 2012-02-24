538d6b504501835355f5d1d7a8b031655a4c6c33
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-02-24 20:14:44 +0000

    CMake: Add CMake Find_* scripts for the Quickbook toolchain.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7139 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ cmake/FindHPX_BoostBook.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2011 Bryce Lelbach
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+if(NOT HPX_FINDPATH_LOADED)
+  include(HPX_FindPATH)
+endif()
+
+if(BOOST_USE_SYSTEM)
+  set(BOOSTBOOK_DTD_USE_SYSTEM ON)
+  set(BOOSTBOOK_XSL_USE_SYSTEM ON)
+endif()
+
+if($ENV{BOOST_ROOT})
+  set(BOOSTBOOK_DTD_ROOT $ENV{BOOST_ROOT})
+  set(BOOSTBOOK_XSL_ROOT $ENV{BOOST_ROOT})
+endif()
+
+if(BOOST_ROOT)
+  set(BOOSTBOOK_DTD_ROOT ${BOOST_ROOT})
+  set(BOOSTBOOK_XSL_ROOT ${BOOST_ROOT})
+endif()
+
+hpx_find_path(BOOSTBOOK_DTD
+  FILES boostbook.dtd
+  FILE_PATHS share/boostbook/dtd dist/share/boostbook/dtd)
+
+hpx_find_path(BOOSTBOOK_XSL
+  FILES docbook.xsl
+  FILE_PATHS share/boostbook/xsl dist/share/boostbook/xsl)
+
