5b76af844dd20c5d93ac96dfb57201eb3992d98b
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-02-24 04:10:25 +0000

    Revert incorrect move, update pbs.sh
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7127 5079c8e4-9419-0410-93bb-9e5ee278c886

--- docs/pbs.sh
+++ docs/pbs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright (c) 2009-2011 Bryce Lelbach
+# Copyright (c) 2011-2012 Bryce Adelstein-Lelbach
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,5 +10,5 @@
 APP_PATH=
 APP_OPTIONS=
 
-pbsdsh -u $APP_PATH --nodes=`cat $PBS_NODEFILE` $APP_OPTIONS
+pbsdsh -u $APP_PATH --hpx:nodes=`cat $PBS_NODEFILE` $APP_OPTIONS
 
