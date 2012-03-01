6094be1f90654dd8a5dc3481a85de2e99f28036a
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2012-03-01 14:06:59 +0000

    Replaced the example in gettingstarted.qbk with real code from one of the examples
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7245 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/simplest_hello_world.cpp
+++ examples/quickstart/simplest_hello_world.cpp
@@ -9,27 +9,23 @@
 ///////////////////////////////////////////////////////////////////////////////
 // The purpose of this example is to execute a HPX-thread printing
 // "Hello world" once. That's all.
+//[simplest_hello_world_getting_started
 int hpx_main(boost::program_options::variables_map&)
 {
-    {
-        // Say Hello to the World!
-        hpx::cout << "Hello World!\n" << hpx::flush;
-    }
+    // Say Hello to the World!
+    hpx::cout << "Hello World!\n" << hpx::flush;
 
-    // Initiate shutdown of the runtime system
-    hpx::finalize();
-
+    hpx::finalize();                // Initiate shutdown of the runtime system
     return 0;
 }
 
-///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
     // Configure application-specific options.
     boost::program_options::options_description desc_commandline(
         "usage: simplest_hello_world [options]");
 
-    // Initialize and run HPX.
-    return hpx::init(desc_commandline, argc, argv);
+    return hpx::init(desc_commandline, argc, argv); // Initialize and run HPX.
 }
+//]
 
