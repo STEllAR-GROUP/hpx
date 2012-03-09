722c5bdce0cfbddfa9316617fed5eb642eed8299
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-03-09 22:08:10 +0000

    Examples: Use fully qualified names in hello_world, elaborate on some of the comments.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7360 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/hello_world.cpp
+++ examples/quickstart/hello_world.cpp
@@ -20,29 +20,6 @@
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
 
-using boost::program_options::variables_map;
-using boost::program_options::options_description;
-
-using boost::format;
-
-using hpx::init;
-using hpx::finalize;
-using hpx::get_os_thread_count;
-using hpx::find_here;
-using hpx::find_all_localities;
-
-using hpx::naming::id_type;
-
-using hpx::actions::plain_action0;
-using hpx::actions::plain_result_action1;
-
-using hpx::lcos::future;
-using hpx::lcos::async;
-using hpx::lcos::wait;
-
-using hpx::cout;
-using hpx::flush;
-
 ///////////////////////////////////////////////////////////////////////////////
 // The purpose of this example is to execute a PX-thread printing "Hello world"
 // once on each OS-thread on each of the connected localities.
@@ -58,27 +35,37 @@ using hpx::flush;
 //[hello_world_worker
 std::size_t hello_world_worker(std::size_t desired)
 {
-    std::size_t current = hpx::get_worker_thread_num(); //Returns OS thread 
-                                                        // number on worker
+    // Returns the OS-thread number of the worker that is running this
+    // PX-thread.
+    std::size_t current = hpx::get_worker_thread_num();
 
     if (current == desired)
     {
-        // Yes! The PX-thread is run by the designated OS-thread.
-        cout << ( format("hello world from OS-thread %1% on locality %2%\n")
-                  % desired
-                  % hpx::get_locality_id())
-            << flush;
+        // The PX-thread has been run on the desired OS-thread.
+
+        char const* msg = "hello world from OS-thread %1% on locality %2%\n";
+
+        hpx::cout << (boost::format(msg) % desired % hpx::get_locality_id())
+                  << hpx::flush;
+
         return desired;
     }
 
-    // this PX-thread is run by the wrong OS-thread, make the foreman retry
+    // This PX-thread has been run by the wrong OS-thread, make the foreman
+    // try again by rescheduling it.
     return std::size_t(-1);
 }
 
 // Define the boilerplate code necessary for the function 'hello_world_worker'
-// to be invoked as an HPX action (by a HPX future)
-typedef plain_result_action1<std::size_t, std::size_t, hello_world_worker>
-    hello_world_worker_action;
+// to be invoked as an HPX action (by a HPX future).
+typedef hpx::actions::plain_result_action1<
+    // Return type.
+    std::size_t
+    // Arguments. 
+  , std::size_t
+    // Wrapped function. 
+  , hello_world_worker
+> hello_world_worker_action;
 
 HPX_REGISTER_PLAIN_ACTION(hello_world_worker_action);
 //]
@@ -87,42 +74,59 @@ HPX_REGISTER_PLAIN_ACTION(hello_world_worker_action);
 //[hello_world_foreman
 void hello_world_foreman()
 {
-    std::size_t const os_threads = get_os_thread_count(); //Get the number of OS
-                                                          // threads
-    id_type const prefix = find_here(); //Determine on which locality function
-                                        // is being executed
+    // Get the number of worker OS-threads in use by this locality.
+    std::size_t const os_threads = hpx::get_os_thread_count();
 
+    // Find the global name of the current locality.
+    hpx::naming::id_type const here = hpx::find_here(); 
+
+    // Populate a set with the OS-thread numbers of all OS-threads on this
+    // locality. When the hello world message has been printed on a particular
+    // OS-thread, we will remove it from the set.
     std::set<std::size_t> attendance;
     for (std::size_t os_thread = 0; os_thread < os_threads; ++os_thread)
-        attendance.insert(os_thread); //Keeping track of the number of threads
-                                      // who have reported back
+        attendance.insert(os_thread);
 
-    // Retry until all PX-threads got executed by their designated OS-thread.
+    // As long as there are still elements in the set, we must keep scheduling
+    // PX-threads. Because HPX features work-stealing task schedulers, we have
+    // no way of enforcing which worker OS-thread will actually execute
+    // each PX-thread.
     while (!attendance.empty())
     {
-        std::vector<future<std::size_t> > futures;
+        // Each iteration, we create a task for each element in the set of
+        // OS-threads that have not said "Hello world". Each of these tasks
+        // is encapsulated in a future.
+        std::vector<hpx::lcos::future<std::size_t> > futures;
         futures.reserve(attendance.size());
-        BOOST_FOREACH(std::size_t os_thread, attendance)
+
+        BOOST_FOREACH(std::size_t worker, attendance)
         {
-            // Schedule a PX-thread encapsulating the print action on a
-            // particular OS-thread.
-            futures.push_back(async<hello_world_worker_action>(prefix, os_thread));
+            // Asynchronously start a new task. The task is encapsulated in a
+            // future, which we can query to determine if the task has
+            // completed.
+            typedef hello_world_worker_action action_type;
+            futures.push_back(hpx::lcos::async<action_type>(here, worker));
         }
 
-        // wait for all of the futures to return their values, we re-spawn the
-        // future until the action gets executed on the right OS-thread
-        wait(futures, [&](std::size_t, std::size_t t)
+        // Wait for all of the futures to finish. The callback version of the
+        // hpx::lcos::wait function takes two arguments: a vector of futures,
+        // and a binary callback.  The callback takes two arguments; the first
+        // is the index of the future in the vector, and the second is the
+        // return value of the future. hpx::lcos::wait doesn't return until
+        // all the futures in the vector have returned.
+        hpx::lcos::wait(futures, [&](std::size_t, std::size_t t)
             { if (std::size_t(-1) != t) attendance.erase(t); });
-        // it is important to note that wait returns two values which can be
-        // passed to a function that takes two variables
     }
 }
 //]
 
 // Define the boilerplate code necessary for the function 'hello_world_foreman'
-// to be invoked as an HPX action
+// to be invoked as an HPX action.
 //[hello_world_action_wrapper
-typedef plain_action0<hello_world_foreman> hello_world_foreman_action;
+typedef hpx::actions::plain_action0<
+    // Wrapped function. 
+    hello_world_foreman
+> hello_world_foreman_action;
 
 HPX_REGISTER_PLAIN_ACTION(hello_world_foreman_action);
 //]
@@ -130,28 +134,34 @@ HPX_REGISTER_PLAIN_ACTION(hello_world_foreman_action);
 ///////////////////////////////////////////////////////////////////////////////
 //[hello_world_hpx_main
 //`Here is hpx_main:
-int hpx_main(variables_map&)
+int hpx_main(boost::program_options::variables_map& vm)
 {
     {
-        std::vector<id_type> prefixes = find_all_localities();/*<Returns the number
-                                                                 of localities>*/
+        // Get a list of all available localities.
+        std::vector<hpx::naming::id_type> localities
+            = hpx::find_all_localities();
 
-        std::vector<future<void> > futures;/*<A future is a chunk of work which
-                                               is executed after being assigned>*/
-        futures.reserve(prefixes.size());
-        BOOST_FOREACH(id_type const& node, prefixes)
+        // Reserve storage space for futures, one for each locality. 
+        std::vector<hpx::lcos::future<void> > futures;
+        futures.reserve(localities.size());
+
+        BOOST_FOREACH(hpx::naming::id_type const& node, localities)
         {
-            futures.push_back(async<hello_world_foreman_action>(node)); /*<
-            asyncronous call [^hello_world_foreman_action]>*/
+            // Asynchronously start a new task. The task is encapsulated in a
+            // future, which we can query to determine if the task has
+            // completed.
+            typedef hello_world_foreman_action action_type;
+            futures.push_back(hpx::lcos::async<action_type>(node));
         }
 
-        wait(futures);    /* Wait for all IO to finish */ /*<
-        [^wait()] will cause the OS thread to wait for all the promises stored
-           in futures to be executed before continuing the program>*/
+        // The non-callback version of hpx::lcos::wait takes a single parameter,
+        // a future of vectors to wait on. hpx::lcos::wait only returns when
+        // all of the futures have finished. 
+        hpx::lcos::wait(futures);
     }
 
     // Initiate shutdown of the runtime system.
-    finalize();
+    hpx::finalize();
 
     return 0;
 }
@@ -162,20 +172,20 @@ int hpx_main(variables_map&)
 int main(int argc, char* argv[])
 {
     // Configure application-specific options.
-    options_description
+    boost::program_options::options_description
        desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
 
     // Initialize and run HPX.
-    return init(desc_commandline, argc, argv);
+    return hpx::init(desc_commandline, argc, argv);
 }
 /*`
   In HPX main is used to initialize the runtime system and pass the command 
-  line arguments to the program.  If you wish to add command line options to 
+  line arguments to the program. If you wish to add command line options to 
   your program you would add them here using the instance of the Boost 
-  class [^options_description], and invocting the public member function
+  class [^options_description], and invoking the public member function
   [^.add_options()] (see __boost_doc__ or the [link hpx.examples.fibonacci
-  Fibonacci Example] for more details).  [^init()] calls 
-  hpx_main after setting up HPX, which is where the mechanics of our program 
-  is written.
+  Fibonacci Example] for more details). [^hpx::init()] calls [^hpx_main] after
+  setting up HPX, which is where the mechanics of our program is written.
  */
 //]
+
