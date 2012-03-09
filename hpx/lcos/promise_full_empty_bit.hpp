45bcaabb62d9eb1f80a20f0224cfa4c57f0242ba
Thomas Heller <heller@cct.lsu.edu>
2012-03-09 18:34:22 +0000

    setting is_obtained_ flag to false after resetting promise to make agas work again
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7351 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/lcos/promise_full_empty_bit.hpp
+++ hpx/lcos/promise_full_empty_bit.hpp
@@ -333,6 +333,7 @@ namespace hpx { namespace lcos
         void reset()
         {
             (*impl_)->reset();
+            future_obtained_ = false;
         }
 
         /// \brief Return the global id of this \a future instance
