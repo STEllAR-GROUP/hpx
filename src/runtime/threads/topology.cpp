f8e9ddbb9024252355c286f3bf9be0627f7fe2f3
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2012-03-09 18:04:51 +0000

    Making things compile on windows
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7350 5079c8e4-9419-0410-93bb-9e5ee278c886

--- src/runtime/threads/topology.cpp
+++ src/runtime/threads/topology.cpp
@@ -6,6 +6,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <hpx/hpx_fwd.hpp>
 #include <hpx/runtime/threads/topology.hpp>
 #include <hpx/runtime.hpp>
 
