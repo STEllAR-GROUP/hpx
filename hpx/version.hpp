85b9e127d708d77cc7b940d81eb08678d9c678f8
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-03-20 03:09:59 +0000

    Bump version to 1.0.0
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7521 5079c8e4-9419-0410-93bb-9e5ee278c886

--- hpx/version.hpp
+++ hpx/version.hpp
@@ -23,10 +23,10 @@
 //  HPX_VERSION_FULL & 0x00FF00 is the minor version
 //  HPX_VERSION_FULL & 0xFF0000 is the major version
 //
-#define HPX_VERSION_FULL         0x000800
+#define HPX_VERSION_FULL         0x001000
 
-#define HPX_VERSION_MAJOR        0
-#define HPX_VERSION_MINOR        8
+#define HPX_VERSION_MAJOR        1
+#define HPX_VERSION_MINOR        0
 #define HPX_VERSION_SUBMINOR     0
 
 #define HPX_VERSION_DATE         20110818
