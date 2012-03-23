978068107872da5d27a62d14f7bc3a2375f562cd
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-03-23 01:07:40 +0000

    Backport accumulator changes from 0.7.0.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7561 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/accumulator/simple_accumulator/stubs/simple_accumulator.hpp
@@ -0,0 +1,74 @@
+//  Copyright (c) 2007-2010 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_COMPONENTS_STUBS_SIMPLE_ACCUMULATOR_JUL_18_2008_1125AM)
+#define HPX_COMPONENTS_STUBS_SIMPLE_ACCUMULATOR_JUL_18_2008_1125AM
+
+#include <hpx/runtime/naming/name.hpp>
+#include <hpx/runtime/applier/applier.hpp>
+#include <hpx/runtime/components/stubs/runtime_support.hpp>
+#include <hpx/runtime/components/stubs/stub_base.hpp>
+#include <hpx/lcos/async.hpp>
+
+#include <examples/accumulator/simple_accumulator/server/simple_accumulator.hpp>
+
+namespace hpx { namespace components { namespace stubs
+{
+    ///////////////////////////////////////////////////////////////////////////
+    /// The \a stubs#simple_accumulator class is the client side representation
+    /// of all \a server#simple_accumulator components
+    struct simple_accumulator : stub_base<server::simple_accumulator>
+    {
+        /// Query the current value of the server#simple_accumulator instance
+        /// with the given \a gid. This is a non-blocking call. The caller
+        /// needs to call \a future#get on the return value of
+        /// this function to obtain the result as returned by the simple_accumulator.
+        static lcos::future<double> query_async(naming::id_type gid)
+        {
+            // Create a future, execute the required action,
+            // we simply return the initialized future, the caller needs
+            // to call get() on the return value to obtain the result
+            typedef server::simple_accumulator::query_action action_type;
+            return lcos::async<action_type>(gid);
+        }
+
+        /// Query the current value of the server#simple_accumulator instance
+        /// with the given \a gid. Block for the current simple_accumulator value to
+        /// be returned.
+        static double query(naming::id_type gid)
+        {
+            // The following get yields control while the action above
+            // is executed and the result is returned to the future
+            return query_async(gid).get();
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        // exposed functionality of this component
+
+        /// Initialize the simple_accumulator value of the server#simple_accumulator instance
+        /// with the given \a gid
+        static void init(naming::id_type gid)
+        {
+            applier::apply<server::simple_accumulator::init_action>(gid);
+        }
+
+        /// Add the given number to the server#simple_accumulator instance
+        /// with the given \a gid
+        static void add (naming::id_type gid, double arg)
+        {
+            applier::apply<server::simple_accumulator::add_action>(gid, arg);
+        }
+
+        /// Print the current value of the server#simple_accumulator instance
+        /// with the given \a gid
+        static void print(naming::id_type gid)
+        {
+            applier::apply<server::simple_accumulator::print_action>(gid);
+        }
+    };
+
+}}}
+
+#endif
---
+++ examples/accumulator/simple_accumulator_client.cpp
@@ -0,0 +1,102 @@
+//  Copyright (c) 2007-2011 Hartmut Kaiser, Richard D Guidry Jr.
+//  Copyright (c)      2011 Bryce Adelstein-Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_init.hpp>
+
+#include "accumulators/simple_accumulator.hpp"
+
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
+char const* const help
+    = "commands: reset, add [amount], query, help, quit";
+
+void ignore_whitespace(std::istream& is)
+{
+    while (' ' == is.peek() || '\t' == is.peek())
+        is.get();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int hpx_main(boost::program_options::variables_map&)
+{
+    {
+        // Create an accumulator component on this locality.
+        examples::simple_accumulator accu;
+        accu.create(hpx::find_here());
+
+        // Print out the available commands.
+        std::cout << help << std::endl << "> ";
+
+        std::cin >> std::noskipws;
+
+        std::string line;
+
+        // Enter the interpreter loop.
+        while (std::getline(std::cin, line).good())
+        {
+            std::vector<std::string> cmd;
+
+            boost::algorithm::trim(line);
+
+            boost::algorithm::split(cmd, line,
+                boost::algorithm::is_any_of(" \t\n"),
+                boost::algorithm::token_compress_on);
+
+            if (!cmd.empty() && !cmd[0].empty())
+            {
+                if (cmd[0] == "reset")
+                    accu.reset_sync();
+
+                else if (cmd[0] == "add")
+                {
+                    if (cmd.size() != 2)
+                        std::cout << "error: invalid command '" 
+                                  << line << "'" << std::endl
+                                  << help << std::endl;
+                    else
+                        accu.add_sync(boost::lexical_cast<boost::uint64_t>
+                            (cmd[1]));
+                }
+
+                else if (cmd[0] == "query")
+                    std::cout << accu.query_sync() << std::endl;
+
+                else if (cmd[0] == "help")
+                    std::cout << help << std::endl;
+
+                else if (cmd[0] == "quit")
+                    break;
+
+                else 
+                    std::cout << "error: invalid command '" 
+                              << line << "'" << std::endl
+                              << help << std::endl;
+            }
+
+            std::cout << "> ";
+        }
+    }
+
+    // Initiate shutdown of the runtime systems on all localities.
+    hpx::finalize();
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    // Configure application-specific options.
+    boost::program_options::options_description
+       desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+
+    // Initialize and run HPX.
+    return hpx::init(desc_commandline, argc, argv);
+}
+
