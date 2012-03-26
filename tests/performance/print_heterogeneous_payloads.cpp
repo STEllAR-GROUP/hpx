8e2224d264cffdb974bbec9694bf66dc30277d32
Vinay Amatya <vamatya@cct.lsu.edu>
2012-03-26 20:11:04 +0000

    update non-standard -Hartmut
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@7594 5079c8e4-9419-0410-93bb-9e5ee278c886

--- tests/performance/print_heterogeneous_payloads.cpp
+++ tests/performance/print_heterogeneous_payloads.cpp
@@ -142,7 +142,7 @@ int app_main(
         throw std::logic_error("incorrect number of tasks generated");
 
     boost::uint64_t const payloads_sum =
-        std::accumulate(payloads.begin(), payloads.end(), 0LLU);
+        std::accumulate(payloads.begin(), payloads.end(), 0ULL);
     if (payloads_sum != total_delay)
         throw std::logic_error("incorrect total delay generated");
 
