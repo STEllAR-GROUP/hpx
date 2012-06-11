c4a82e539a7fe86f0866385264e873232f8bae4f
Matthew Anderson <manderson@cct.lsu.edu>
2012-06-11 16:42:47 +0000

    an experimental example to accomplish adaptive dataflow
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@8205 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/adaptive_dataflow/ad/point_module.cpp
@@ -0,0 +1,48 @@
+//  Copyright (c) 2007-2011 Matthew Anderson
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/components/server/managed_component_base.hpp>
+#include <hpx/runtime/components/component_factory.hpp>
+
+#include <hpx/util/portable_binary_iarchive.hpp>
+#include <hpx/util/portable_binary_oarchive.hpp>
+
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/export.hpp>
+
+#include "server/point.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+// Add factory registration functionality
+HPX_REGISTER_COMPONENT_MODULE();
+
+///////////////////////////////////////////////////////////////////////////////
+typedef hpx::components::managed_component<
+    ad::server::point
+> ad_point_type;
+
+HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(ad_point_type, ad_point);
+
+///////////////////////////////////////////////////////////////////////////////
+HPX_REGISTER_ACTION_EX(
+    ad_point_type::wrapped_type::init_action,
+    ad_point_init_action);
+
+HPX_REGISTER_ACTION_EX(
+    ad_point_type::wrapped_type::compute_action,
+    ad_point_compute_action);
+
+HPX_REGISTER_ACTION_EX(
+    ad_point_type::wrapped_type::get_item_action,
+    ad_point_get_item_action);
+
+HPX_REGISTER_ACTION_EX(
+    ad_point_type::wrapped_type::calcrhs_action,
+    ad_point_calcrhs_action);
+
+HPX_REGISTER_ACTION_EX(
+    ad_point_type::wrapped_type::remove_item_action,
+    ad_point_remove_item_action);
