fffa000fa625906e9346156ae438d488f1291388
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-06-12 01:22:51 +0000

    Purge non-release examples from the release tree. Please direct objections to me via email.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/hpx/release@8212 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/fractal_visualizer/shader_utils.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef _CREATE_SHADER_H
-#define _CREATE_SHADER_H
-#include <GL/glew.h>
-char* file_read(const char* filename);
-void print_log(GLuint object);
-GLuint create_shader(const char* filename, GLenum type);
-#endif
