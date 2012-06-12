75a3757365cb7ba3c527ff625bf1615ac33e54d1
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-06-12 02:45:05 +0000

    Dan, I don't think you were alive in 1011 :D
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@8213 5079c8e4-9419-0410-93bb-9e5ee278c886

--- tests/performance/statistical/general_pa_declarations.cpp
+++ tests/performance/statistical/general_pa_declarations.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c)      1011 Daniel Kogler
+//  Copyright (c) 2011 Daniel Kogler
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
