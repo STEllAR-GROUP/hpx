a188b1e6eb15a8c2835e523f6a1e5543ef247831
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-06-13 03:51:44 +0000

    Merge EasyBMP fixes from trunk.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/hpx/release@8227 5079c8e4-9419-0410-93bb-9e5ee278c886

--- external/EasyBMP/EasyBMP.cpp
+++ external/EasyBMP/EasyBMP.cpp
@@ -991,7 +991,6 @@ bool BMP::ReadFromFile( const char* FileName )
    // read the three bit masks
 
    ebmpWORD TempMaskWORD;
-   ebmpWORD ZeroWORD;
   
    SafeFread( (char*) &RedMask , 2 , 1 , fp );
    if( IsBigEndian() )
