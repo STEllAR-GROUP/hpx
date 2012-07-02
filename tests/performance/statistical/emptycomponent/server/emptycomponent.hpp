aa60a4ce69663682b76fba880a19de2663015d90
Daniel Kogler <dkogler@cct.lsu.edu>
2012-07-02 17:29:45 +0000

    "All the files are definitely included now, so everything should build properly"
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@8282 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ tests/performance/statistical/emptycomponent/server/emptycomponent.hpp
@@ -0,0 +1,27 @@
+//  Copyright (c)      2012 Daniel Kogler
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/applier/applier.hpp>
+#include <hpx/runtime/actions/component_action.hpp> 
+#include <hpx/runtime/components/component_type.hpp>
+#include <hpx/runtime/components/server/simple_component_base.hpp>
+
+#include "../../statstd.hpp"
+
+namespace hpx { namespace components { namespace server
+{
+
+class HPX_COMPONENT_EXPORT emptycomponent : 
+    public simple_component_base<emptycomponent>
+{
+public:
+    emptycomponent(){}
+};
+
+}}}
+
+
