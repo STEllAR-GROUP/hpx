1b24593772c473a88a67092cc96c4e7bebdbf67e
Andrew Kemp <akemp@cct.lsu.edu>
2012-07-02 01:46:50 +0000

    Fixed code formatting issues
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@8276 5079c8e4-9419-0410-93bb-9e5ee278c886

--- examples/quickstart/fractals_oclm.cpp
+++ examples/quickstart/fractals_oclm.cpp
@@ -21,106 +21,98 @@
 int main()
 {
     {
-        BMP SetImage;
         float raws[MEM_SIZE*MEM_SIZE];
 
-            cl_platform_id platform_id = NULL;
-            cl_device_id device_id = NULL;
-            cl_context context = NULL;
-            cl_command_queue command_queue = NULL;
+        cl_platform_id platform_id = NULL;
+        cl_device_id device_id = NULL;
+        cl_context context = NULL;
+        cl_command_queue command_queue = NULL;
 
-            cl_mem rawMemobj = NULL;
+        cl_mem rawMemobj = NULL;
 
-            cl_program program = NULL;
-            cl_kernel kernel = NULL;
-            cl_uint ret_num_devices;
-            cl_uint ret_num_platforms;
-            cl_int ret;
-    
-            //ebmpBYTE green[MEM_SIZE * MEM_SIZE];
+        cl_program program = NULL;
+        cl_kernel kernel = NULL;
+        cl_uint ret_num_devices;
+        cl_uint ret_num_platforms;
+        cl_int ret;
 
-            FILE *fp;
-            const char fileName[] = "fractalLimit.cl";
-            size_t source_size;
-            char *source_str = new char[MAX_SOURCE_SIZE];
-            cl_int i;
-            if (!readKernel(fileName, source_size, source_str, MAX_SOURCE_SIZE))
+        FILE *fp;
+        const char fileName[] = "fractalLimit.cl";
+        size_t source_size;
+        char *source_str = new char[MAX_SOURCE_SIZE];
+        cl_int i;
+        if (!readKernel(fileName, source_size, source_str, MAX_SOURCE_SIZE))
+        {
+            fprintf(stderr, "Failed to load kernel.\n");
+            system("Pause");
+            exit(1);
+        }
+
+        ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+        ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+
+        context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+
+        command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+
+        rawMemobj = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(raws), NULL, &ret);
+
+        program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
+
+        ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+
+        kernel = clCreateKernel(program, "set", &ret);
+
+
+        ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&rawMemobj);
+
+	    size_t global_work_size[3] = {MEM_SIZE, MEM_SIZE, 1};
+
+        ret = clEnqueueNDRangeKernel(command_queue, kernel, 3, NULL, global_work_size, NULL, 0, NULL, NULL);
+
+	    if (ret)
+	    {
+		    std::cout << ret << std::endl;
+	    }
+
+        ret = clEnqueueReadBuffer(command_queue, rawMemobj, CL_TRUE, 0,  sizeof(raws), raws, 0, NULL, NULL);
+
+	
+        ret = clFlush(command_queue);
+        ret = clFinish(command_queue);
+        ret = clReleaseKernel(kernel);
+        ret = clReleaseProgram(program);
+        ret = clReleaseMemObject(rawMemobj);
+        ret = clReleaseCommandQueue(command_queue);
+        ret = clReleaseContext(context);
+	
+	    if (ret)
+	    {
+		    std::cout << ret << std::endl;
+	    }
+
+        free(source_str);
+	    int size = MEM_SIZE;
+	    {
+            BMP SetImage;
+	        SetImage.SetBitDepth(24);
+	        SetImage.SetSize(size*2,size);
+	        RGBApixel pix;
+	        for (int i = 0; i < size; i++)
             {
-                fprintf(stderr, "Failed to load kernel.\n");
-                system("Pause");
-                exit(1);
+		        for (int j = 0; j < size; j++)
+		        {
+                    for (int k = 0; k < 2; k++)
+                    {
+			            pix.Blue = (int)raws[i * size + j];
+			            pix.Red = (int)raws[i * size + j];
+			            pix.Green = (int)raws[i * size + j];
+                        SetImage.SetPixel(i * 2 + k,j,pix);
+                    }
+		        }
             }
-
-            ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-            ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
-
-            context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
-
-            command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-
-            rawMemobj = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(raws), NULL, &ret);
-            //redMemobj = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(red), NULL, &ret);
-            //greenMemobj = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(red), NULL, &ret);
-
-            program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
-
-            ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-
-            kernel = clCreateKernel(program, "set", &ret);
-
-
-            ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&rawMemobj);
-            //ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&redMemobj);
-            //ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&greenMemobj);
-
-	        size_t global_work_size[3] = {MEM_SIZE, MEM_SIZE, 1};
-
-            ret = clEnqueueNDRangeKernel(command_queue, kernel, 3, NULL, global_work_size, NULL, 0, NULL, NULL);
-
-	        if (ret)
-	        {
-		        std::cout << ret << std::endl;
-	        }
-
-            ret = clEnqueueReadBuffer(command_queue, rawMemobj, CL_TRUE, 0,  sizeof(raws), raws, 0, NULL, NULL);
-            //ret = clEnqueueReadBuffer(command_queue, redMemobj, CL_TRUE, 0,  sizeof(red), red, 0, NULL, NULL);
-            //ret = clEnqueueReadBuffer(command_queue, greenMemobj, CL_TRUE, 0, sizeof(green), green, 0, NULL, NULL);
-
-	
-            ret = clFlush(command_queue);
-            ret = clFinish(command_queue);
-            ret = clReleaseKernel(kernel);
-            ret = clReleaseProgram(program);
-            ret = clReleaseMemObject(rawMemobj);
-            //ret = clReleaseMemObject(redMemobj);
-            //ret = clReleaseMemObject(greenMemobj);
-            ret = clReleaseCommandQueue(command_queue);
-            ret = clReleaseContext(context);
-	
-	        if (ret)
-	        {
-		        std::cout << ret << std::endl;
-	        }
-
-            free(source_str);
-	        int size = MEM_SIZE;
-	        {
-	            SetImage.SetBitDepth(24);
-	            SetImage.SetSize(size*2,size);
-	            RGBApixel pix;
-	            for (int i = 0; i < size; i++)
-		            for (int j = 0; j < size; j++)
-		            {
-                        for (int k = 0; k < 2; k++)
-                        {
-			                pix.Blue = (int)raws[i * size + j];
-			                pix.Red = (int)raws[i * size + j];
-			                pix.Green = (int)raws[i * size + j];
-                            SetImage.SetPixel(i * 2 + k,j,pix);
-                        }
-		            }
-	            SetImage.WriteToFile("out.bmp");
-	            }
+	        SetImage.WriteToFile("out.bmp");
+	    }
     }
     system("Pause");
     return 0;
