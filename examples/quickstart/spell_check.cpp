0a47f9f66faad322509d8257588118fe4e562cc4
Andrew Kemp <akemp@cct.lsu.edu>
2012-07-03 00:05:04 +0000

    Adding the spell checking program.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@8287 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/quickstart/spell_check.cpp
@@ -0,0 +1,141 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+// Naive SMP version implemented with futures.
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/actions.hpp>
+#include <hpx/include/util.hpp>
+#include <hpx/include/lcos.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <boost/algorithm/string.hpp>
+
+
+std::vector<std::string> words;
+
+std::string search(int start, int end, std::string &word);
+
+HPX_PLAIN_ACTION(search, search_action);
+
+std::string search(int start, int end, std::string &word)
+{
+    //highest value is 'z' at 122
+    //lowest value is 'a' at 97
+    //start is where our word check is located.
+    //end is where the end if this thread's search is.
+    std::string check = words[(start+end)/2];
+    //first we check if there is no definitive match
+    if (start == end && word != check)
+        return word + " was not found in this dictionary, " + check + " was the closest match.\n";
+    int size;
+    //if our value is lower than start, we disregard it.
+    if (word.length() >= check.length())
+        size = check.length();
+    else
+        size = word.length();
+    for (int i = 0; i < size; i++)
+    {
+        char check_char = check[i];
+        char word_char = word[i];
+        if (check_char != word_char)
+        {
+            if (word_char > check_char)
+                return search((start+end+1)/2, end, word);
+            else
+                return search(start, (start+end-1)/2, word);
+        }
+    }
+    if (check.length() == word.length())
+        return word + " was found in this dictionary.\n";
+    else
+        return search(start, (start+end-1)/2, word);
+}
+int hpx_main()
+{
+    
+    {
+        using namespace std;
+        ifstream fin;
+        string path = __FILE__;
+        string wordlist_path;
+        string remove = "spell_check.cpp";
+        for (int i = 0; i < path.length() - remove.length(); i++)
+        {
+            wordlist_path.push_back(path[i]);
+            if (path[i] == '\\')
+            {
+                wordlist_path.push_back(path[i]);
+            }
+        }
+        //list of American English words in alphabetical order. Provided by Kevin at http://wordlist.sourceforge.net/
+        wordlist_path = wordlist_path + "5desk.txt";
+        fin.open(wordlist_path);
+        int wordcount = 0;
+        cout << "Reading dictionary file to memory...\n";
+        hpx::util::high_resolution_timer t;
+        if(fin.is_open())
+        {
+            string temp;
+            while (fin.good())
+            {
+                getline(fin, temp);
+                for (int i = 0; i < temp.length(); i++)
+                temp[i] = tolower(temp[i]);
+                words.push_back(temp);
+                wordcount++;
+            }
+            cout << wordcount << " words loaded in " << t.elapsed() << "s.\n";
+        }
+        else
+        {
+            cout << "Error: Unable to open file.\n";
+            return 1;
+        }
+        fin.close();
+        char* word = new char[1024];
+        cout << "Enter the words you would like to spellcheck and then press \"Enter\".\n";
+        cin.getline(word,1024, '\n');
+        std::vector<std::string> strs;
+        boost::split(strs, word, boost::is_any_of("\n\t "));
+        t.restart();
+        {
+            using hpx::lcos::future;
+            using hpx::async;
+            using hpx::wait_all;
+            vector<search_action> sAct;//[sizeX * sizeY];
+            vector<future<string>> wordRun;
+            wordRun.reserve(strs.size());
+            for each(string single in strs)
+            {
+                int start = 0;
+                hpx::naming::id_type const locality_id = hpx::find_here();
+                search_action temp;
+                wordRun.push_back(async(temp, locality_id, start, wordcount, single));
+                sAct.push_back(temp);
+                //cout << search(0, wordcount, single) << endl;
+            }
+            wait_all(wordRun);
+            cout << "Search completed in " << t.elapsed() << "s.\n";
+            for (int i = 0; i < strs.size(); i++)
+            {
+                cout << wordRun[i].get();
+            }
+        }
+    }
+    return hpx::finalize(); // Handles HPX shutdown
+}
+int main()
+{
+    int code = hpx::init();
+    std::cout << std::endl;
+    system("Pause");
+    return code;
+}
