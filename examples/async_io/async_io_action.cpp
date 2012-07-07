3d9f8efdf3b57c179a676ac1651e149dc6b6078a
Hartmut Kaiser <hkaiser@cct.lsu.edu>
2012-07-07 19:50:39 +0000

    Adding another async IO example demonstrating possibly remote operation using an HPX action
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@8364 5079c8e4-9419-0410-93bb-9e5ee278c886

---
+++ examples/async_io/async_io_action.cpp
@@ -0,0 +1,94 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// The purpose of this example is to demonstrate how it is possible to
+// generically schedule an asynchronous IO task onto one of the IO-threads
+// in HPX (which are OS-threads), how to synchronize the result of this IO
+// task with a waiting HPX thread, and how to wrap all of this into a HPX
+// action.
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/actions.hpp>
+#include <hpx/include/iostreams.hpp>
+
+namespace detail
+{
+    // this function will be executed by a dedicated OS thread
+    void do_async_io(std::string const& string_to_write,
+        boost::shared_ptr<hpx::lcos::local::promise<int> > p)
+    {
+        // This IO operation will possibly block the IO thread in the
+        // kernel.
+        std::cout << "OS-thread: " << string_to_write << std::endl;
+
+        p->set_value(0);    // notify the waiting HPX thread and return a value
+    }
+
+    // This function will be executed by an HPX thread
+    hpx::lcos::future<int> async_io_worker(std::string const& string_to_write)
+    {
+        boost::shared_ptr<hpx::lcos::local::promise<int> > p =
+            boost::make_shared<hpx::lcos::local::promise<int> >();
+
+        // Get a reference to one of the IO specific HPX io_service objects ...
+        hpx::util::io_service_pool* pool =
+            hpx::get_runtime().get_thread_pool("io_pool");
+
+        // ... and schedule the handler to run on one of its OS-threads.
+        pool->get_io_service().post(
+            hpx::util::bind(&do_async_io, string_to_write, p));
+
+        return p->get_future();
+    }
+}
+
+// This function will be called whenever the action async_io_action is
+// invoked. This allows to remotely execute the async_io.
+int async_io(std::string const& string_to_write)
+{
+    hpx::lcos::future<int> f = detail::async_io_worker(string_to_write);
+    return f.get();    // simply wait for the IO to finish
+}
+
+// this defines the type async_io_action
+HPX_PLAIN_ACTION(async_io, async_io_action);
+
+int hpx_main()
+{
+    {
+        // Determine on what locality to run the IO operation. If this
+        // example is run on one locality, we perform the IO operation
+        // locally, otherwise we choose one of the remote localities to
+        // invoke the async_io_action.
+        hpx::naming::id_type id = hpx::find_here();    // default: local
+
+        std::vector<hpx::naming::id_type> localities =
+            hpx::find_remote_localities();
+        if (!localities.empty())
+            id = localities[0];         // choose the first remote locality
+
+        // Create an action instance.
+        async_io_action io;
+
+        // Initiate an asynchronous (possibly remote) IO operation and
+        // wait for it to complete without blocking any of the HPX
+        // thread-manager threads. This will suspend the current HPX
+        // thread until the IO operation is finished. Other work could
+        // be executed in the mean time.
+        int result = io(id, "Write this string to std::cout");
+
+        // Print the returned result.
+        hpx::cout << "HPX-thread: The asynchronous IO operation returned: "
+                  << result << "\n" << hpx::flush;
+    }
+
+    return hpx::finalize(); // Initiate shutdown of the runtime system.
+}
+
+int main(int argc, char* argv[])
+{
+    return hpx::init(argc, argv); // Initialize and run HPX.
+}
+
