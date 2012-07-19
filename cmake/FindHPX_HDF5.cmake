2f14d1d4c37399a1806b06559d1bb0dace13372a
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-07-19 16:25:39 -0500

    Allowing CMake to find Fortran modules on Windows

--- cmake/FindHPX_HDF5.cmake
+++ cmake/FindHPX_HDF5.cmake
@@ -15,13 +15,21 @@ if(HDF5_USE_SYSTEM)
 endif()
 
 if($ENV{HDF5_ROOT})
-  set(HDF5_CPP_ROOT $ENV{HDF5_ROOT})
-  set(HDF5_FORTRAN_ROOT $ENV{HDF5_ROOT})
+  if(NOT HDF5_CPP_ROOT)
+    set(HDF5_CPP_ROOT $ENV{HDF5_ROOT})
+  endif()
+  if(NOT HDF5_FORTRAN_ROOT)
+    set(HDF5_FORTRAN_ROOT $ENV{HDF5_ROOT})
+  endif()
 endif()
 
 if(HDF5_ROOT)
-  set(HDF5_CPP_ROOT ${HDF5_ROOT})
-  set(HDF5_FORTRAN_ROOT ${HDF5_ROOT})
+  if(NOT HDF5_CPP_ROOT)
+    set(HDF5_CPP_ROOT ${HDF5_ROOT})
+  endif()
+  if(NOT HDF5_FORTRAN_ROOT)
+    set(HDF5_FORTRAN_ROOT ${HDF5_ROOT})
+  endif()
 endif()
 
 # find main HDF5 library
@@ -53,13 +61,15 @@ hpx_find_package(HDF5_CPP
 # find HDF5 Fortran library
 if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
   set(hdf5_fortran_lib hdf5_fortranddll hdf5_fortrandll)
+  set(hdf5_fortran_mod include/fortran/Debug include/fortran/Release)
 else()
   set(hdf5_fortran_lib hdf5_fortrandll)
+  set(hdf5_fortran_mod include/fortran/Release include/fortran/Debug)
 endif()
 
 hpx_find_package(HDF5_FORTRAN
   LIBRARIES hdf5_fortran libhdf5_fortran ${hdf5_fortran_lib}
   LIBRARY_PATHS lib64 lib
   HEADERS hdf5.mod
-  HEADER_PATHS include include/fortran)
+  HEADER_PATHS include include/fortran ${hdf5_fortran_mod})
 
