74d9b7b0489567a534f87db750c7d34e94c00729
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-07-24 19:01:54 -0500

    Updating some license dates

--- examples/adaptive1d/dataflow/stubs/dynamic_stencil_value.hpp
+++ examples/adaptive1d/dataflow/stubs/dynamic_stencil_value.hpp
@@ -1,5 +1,5 @@
 //  Copyright (c) 2009-2011 Matthew Anderson
-//  Copyright (c) 2007-2010 Hartmut Kaiser
+//  Copyright (c) 2007-2012 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
--- examples/adaptive1d/dataflow/stubs/functional_component.hpp
+++ examples/adaptive1d/dataflow/stubs/functional_component.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2007-2010 Hartmut Kaiser
+//  Copyright (c) 2007-2012 Hartmut Kaiser
 //  Copyright (c) 2009-2011 Matthew Anderson
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
