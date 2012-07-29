25958e1ad76b46b424c2ea977ddc5679051870d4
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-07-29 10:00:12 -0500

    Add licenses

--- cmake/preprocess/do.cmd
+++ cmake/preprocess/do.cmd
@@ -1,1 +1,6 @@
+rem Copyright (c) 2007-2012 Hartmut Kaiser
+rem
+rem Distributed under the Boost Software License, Version 1.0. (See accompanying
+rem file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 wave -o- -DHPX_LIMIT=5 preprocess_hpx.cpp --license=preprocess_license.hpp
