cbdc800dd8d76ce64eaa4f02f01602e91f841628
vamatya <vinaya.amatya@gmail.com>
2012-07-30 18:58:22 -0500

    adding files

---
+++ examples/performance_counters/mem_counter.cpp
@@ -0,0 +1,32 @@
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/performance_counters.hpp>
+#include <boost/atomic.hpp>
+#include "mem_counter.hpp"
+
+typedef hpx::performance_counters::server::proc_statm proc_statm_type;
+
+namespace read_mem = hpx::performance_counters::server;
+
+void register_counter_type()
+{
+    namespace pc = hpx::performance_counters;
+    pc::install_counter_type(
+        "/read_memory/vm",
+        &read_mem::read_psm_vm,
+        "returns the proc_statm file for the pid value sent"
+    );
+}
+
+int hpx_main()
+{
+    {
+    }
+    return hpx::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    hpx::register_startup_function(&register_counter_type);
+    return hpx::init(argc, argv);
+}
