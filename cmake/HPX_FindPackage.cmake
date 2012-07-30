675f25bbc6339dbf033b0de5da6db21f62259b80
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-07-30 21:38:13 +0000

    CMake: Revert the changes to HPX_FindPackage in commit 9c9b768d5787076b088e9038702d6599d2a6f804, b719a0cf0e10c189ca1b8fd41b7cc1f16080c3a5, 1edddcc2ab93b7b0dd445cd6d51d5d7aa5eea0a0, and fix other issues with HPX_FindPackage.

--- cmake/HPX_FindPackage.cmake
+++ cmake/HPX_FindPackage.cmake
@@ -5,95 +5,98 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# TODO: Rename to hpx_find_library
-
 set(HPX_FINDPACKAGE_LOADED TRUE)
 
 if(NOT HPX_UTILS_LOADED)
   include(HPX_Utils)
 endif()
 
-# TODO: Name is misleading.
-macro(hpx_get_version name)
-  if(${name}_DISABLE)
-    hpx_info("get_version.${name}" "Library search disabled by user.")
-    unset(${name}_VERSION_SEARCHED CACHE)
-    unset(${name}_ROOT)
-    set(${name}_FOUND OFF CACHE BOOL "Found ${name}.")
-  else()
-    if(NOT ${name}_VERSION_SEARCHED)
-      hpx_debug("get_version.${name}" "Searching for version of package ${name}.")
+macro(hpx_find_headers name)
+  if(NOT ${name}_HEADER_SEARCHED)
+    hpx_debug("find_headers.${name}" "Finding headers for package ${name}.")
 
-      hpx_parse_arguments(${name}
-        "HEADERS;HEADER_PATHS;LIBRARIES;LIBRARY_PATHS" "ESSENTIAL" ${ARGN})
+    hpx_parse_arguments(${name}
+      "HEADERS;HEADER_PATHS;LIBRARIES;LIBRARY_PATHS" "ESSENTIAL" ${ARGN})
 
-      ##########################################################################
-      # Check if ${name}_ROOT is defined and use that path first if
-      # ${name}_USE_SYSTEM is not defined.
-      if(NOT ${name}_USE_SYSTEM)
-        if(NOT ${name}_ROOT AND NOT $ENV{${name}_ROOT} STREQUAL "")
-          set(${name}_ROOT $ENV{${name}_ROOT})
-        endif()
+    ##########################################################################
+    # Check if ${name}_ROOT is defined and use that path first if
+    # ${name}_USE_SYSTEM is not defined.
+    if(NOT ${name}_USE_SYSTEM)
+      if(NOT ${name}_ROOT AND NOT $ENV{${name}_ROOT} STREQUAL "")
+        set(${name}_ROOT $ENV{${name}_ROOT})
       endif()
-
-      set(rooted_header_paths)
-      foreach(path ${${name}_HEADER_PATHS})
-        list(APPEND rooted_header_paths ${${name}_ROOT}/${path})
-      endforeach()
-
-      if(${name}_ROOT)
-        find_path(${name}_INCLUDE_DIR
-          NAMES ${${name}_HEADERS}
-          PATHS ${rooted_header_paths}
-          NO_DEFAULT_PATH)
-
-        if(NOT ${name}_INCLUDE_DIR)
-          hpx_warn("get_version.${name}" "Header not found in ${${name}_ROOT}, trying system path.")
-          unset(${name}_ROOT)
-        else()
-          hpx_info("get_version.${name}" "Header found in ${${name}_ROOT}.")
-        endif()
-      endif()
-
-      # if not found, retry using system path
-      if(NOT ${name}_ROOT)
-        find_path(${name}_INCLUDE_DIR NAMES ${${name}_HEADERS}
-                                      PATH_SUFFIXES ${${name}_HEADER_PATHS})
-
-        if(NOT ${name}_INCLUDE_DIR)
-          if(${name}_ESSENTIAL)
-            hpx_error("get_version.${name}" "Header not found in system path.")
-          else()
-            hpx_warn("get_version.${name}" "Header not found in system path.")
-          endif()
-          unset(${name}_VERSION_SEARCHED CACHE)
-          unset(${name}_ROOT)
-        else()
-          set(${name}_VERSION_SEARCHED ON CACHE INTERNAL "Searched for ${name} library version.")
-          hpx_info("get_version.${name}" "Header found in system path.")
-        endif()
-      endif()
-
     endif()
+
+    set(rooted_header_paths)
+    foreach(path ${${name}_HEADER_PATHS})
+      list(APPEND rooted_header_paths ${${name}_ROOT}/${path})
+    endforeach()
+
+    if(${name}_ROOT)
+      find_path(${name}_INCLUDE_DIR
+        NAMES ${${name}_HEADERS}
+        PATHS ${rooted_header_paths}
+        NO_DEFAULT_PATH)
+
+      if(NOT ${name}_INCLUDE_DIR)
+        hpx_warn("find_headers.${name}" "Header not found in ${${name}_ROOT}, trying system path.")
+      else()
+        set(${name}_HEADER_SEARCHED ON CACHE INTERNAL "Searched for ${name} headers.")
+        hpx_info("find_headers.${name}" "Header found in ${${name}_ROOT}.")
+      endif()
+    endif()
+
+    # if not found, retry using system path
+    if(NOT ${name}_ROOT)
+      find_path(${name}_INCLUDE_DIR NAMES ${${name}_HEADERS}
+                                    PATH_SUFFIXES ${${name}_HEADER_PATHS})
+
+      if(NOT ${name}_INCLUDE_DIR)
+        if(${name}_ESSENTIAL)
+          hpx_error("find_headers.${name}" "Header not found in system path.")
+        else()
+          hpx_warn("find_headers.${name}" "Header not found in system path.")
+        endif()
+      else()
+        set(${name}_HEADER_SEARCHED ON CACHE INTERNAL "Searched for ${name} headers.")
+        hpx_info("find_headers.${name}" "Header found in system path.")
+      endif()
+    endif()
+
   endif()
 endmacro()
 
 macro(hpx_find_package name)
-  set(${name}_FOUND OFF CACHE BOOL "Found ${name}.")
-  set(${name}_LIBRARY ${name}_LIBRARY-NOTFOUND CACHE FILEPATH "${name} shared library.")
-  set(${name}_LIBRARY_DIR ${name}_LIBRARY_DIR-NOTFOUND CACHE PATH "${name} library directory.")
-  set(${name}_INCLUDE_DIR ${name}_INCLUDE_DIR-NOTFOUND CACHE PATH "${name} include directory.")
-
   if(${name}_DISABLE)
     hpx_info("find_package.${name}" "Library search disabled by user.")
-    unset(${name}_VERSION_SEARCHED CACHE)
-    unset(${name}_ROOT)
+
+    if(NOT ${name}_ROOT)
+      set(${name}_ROOT ${name}_ROOT-NOTFOUND)
+    endif()
+
+    if(NOT ${name}_LIBRARY)
+      set(${name}_LIBRARY ${name}_LIBRARY-NOTFOUND)
+    endif()
+
+    if(NOT ${name}_LIBRARY_DIR)
+      set(${name}_LIBRARY_DIR ${name}_LIBRARY_DIR-NOTFOUND)
+    endif()
+
+    if(NOT ${name}_INCLUDE_DIR)
+      set(${name}_INCLUDE_DIR ${name}_INCLUDE_DIR-NOTFOUND)
+    endif()
+
+    set(${name}_FOUND OFF CACHE BOOL "Found ${name}.")
+    set(${name}_ROOT ${${name}_ROOT} CACHE INTERNAL "${name} root directory.")
+    set(${name}_LIBRARY ${${name}_LIBRARY} CACHE INTERNAL "${name} shared library.")
+    set(${name}_LIBRARY_DIR ${${name}_LIBRARY_DIR} CACHE INTERNAL "${name} library directory.")
+    set(${name}_INCLUDE_DIR ${${name}_INCLUDE_DIR} CACHE INTERNAL "${name} include directory.")
   else()
     if(NOT ${name}_SEARCHED)
       hpx_info("find_package.${name}" "Searching for package ${name}.")
 
-      if(NOT ${name}_VERSION_SEARCHED)
-        hpx_get_version(${name} ${ARGN})
+      if(NOT ${name}_HEADERS_SEARCHED)
+        hpx_find_headers(${name} ${ARGN})
       endif()
 
       set(rooted_paths)
@@ -111,7 +114,6 @@ macro(hpx_find_package name)
 
         if(NOT ${name}_LIBRARY)
           hpx_warn("find_package.${name}" "Library not found in ${${name}_ROOT}, trying system path.")
-          unset(${name}_ROOT)
         else()
           set(${name}_SEARCHED ON CACHE INTERNAL "Searched for ${name} library.")
           hpx_info("find_package.${name}" "Library found in ${${name}_ROOT}.")
@@ -131,8 +133,6 @@ macro(hpx_find_package name)
           else()
             hpx_warn("find_package.${name}" "Library not found in system path.")
           endif()
-          unset(${name}_SEARCHED CACHE)
-          unset(${name}_ROOT)
         else()
           set(${name}_SEARCHED ON CACHE INTERNAL "Searched for ${name} library.")
           hpx_info("find_package.${name}" "Library found in system path.")
@@ -147,8 +147,34 @@ macro(hpx_find_package name)
       if(${name}_FOUND)
         get_filename_component(${name}_ROOT ${${name}_INCLUDE_DIR} PATH)
         get_filename_component(${name}_LIBRARY_DIR ${${name}_LIBRARY} PATH)
-      endif()
+        set(${name}_FOUND ${${name}_FOUND} CACHE BOOL "Found ${name}.")
+        set(${name}_ROOT ${${name}_ROOT} CACHE INTERNAL "${name} root directory.")
+        set(${name}_LIBRARY ${${name}_LIBRARY} CACHE INTERNAL "${name} shared library.")
+        set(${name}_LIBRARY_DIR ${${name}_LIBRARY_DIR} CACHE INTERNAL "${name} library directory.")
+        set(${name}_INCLUDE_DIR ${${name}_INCLUDE_DIR} CACHE INTERNAL "${name} include directory.")
+      else()
+        if(NOT ${name}_ROOT)
+          set(${name}_ROOT ${name}_ROOT-NOTFOUND)
+        endif()
 
+        if(NOT ${name}_LIBRARY)
+          set(${name}_LIBRARY ${name}_LIBRARY-NOTFOUND)
+        endif()
+
+        if(NOT ${name}_LIBRARY_DIR)
+          set(${name}_LIBRARY_DIR ${name}_LIBRARY_DIR-NOTFOUND)
+        endif()
+
+        if(NOT ${name}_INCLUDE_DIR)
+          set(${name}_INCLUDE_DIR ${name}_INCLUDE_DIR-NOTFOUND)
+        endif()
+
+        set(${name}_FOUND OFF CACHE BOOL "Found ${name}.")
+        set(${name}_ROOT ${${name}_ROOT} CACHE INTERNAL "${name} root directory.")
+        set(${name}_LIBRARY ${${name}_LIBRARY} CACHE INTERNAL "${name} shared library.")
+        set(${name}_LIBRARY_DIR ${${name}_LIBRARY_DIR} CACHE INTERNAL "${name} library directory.")
+        set(${name}_INCLUDE_DIR ${${name}_INCLUDE_DIR} CACHE INTERNAL "${name} include directory.")
+      endif()
     endif()
   endif()
 endmacro()
