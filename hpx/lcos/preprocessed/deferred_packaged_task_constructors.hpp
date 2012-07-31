d536d10dc53bc9b1d849701aa4de14edf5e8b72d
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-07-31 10:55:50 -0500

    Yet another preprocessing problem fixed

--- hpx/lcos/preprocessed/deferred_packaged_task_constructors.hpp
+++ hpx/lcos/preprocessed/deferred_packaged_task_constructors.hpp
@@ -3,21 +3,21 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_LCOS_PREPROCESSED_DEFERRED_PACKAGED_TASK_CONSTRUCTORS_DIRECT_HPP)
-#define HPX_LCOS_PREPROCESSED_DEFERRED_PACKAGED_TASK_CONSTRUCTORS_DIRECT_HPP
+#if !defined(HPX_LCOS_PREPROCESSED_DEFERRED_PACKAGED_TASK_CONSTRUCTORS_HPP)
+#define HPX_LCOS_PREPROCESSED_DEFERRED_PACKAGED_TASK_CONSTRUCTORS_HPP
 
 #if HPX_ACTION_ARGUMENT_LIMIT <= 5
-#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_direct_5.hpp>
+#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_5.hpp>
 #elif HPX_ACTION_ARGUMENT_LIMIT <= 10
-#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_direct_10.hpp>
+#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_10.hpp>
 #elif HPX_ACTION_ARGUMENT_LIMIT <= 20
-#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_direct_20.hpp>
+#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_20.hpp>
 #elif HPX_ACTION_ARGUMENT_LIMIT <= 30
-#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_direct_30.hpp>
+#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_30.hpp>
 #elif HPX_ACTION_ARGUMENT_LIMIT <= 40
-#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_direct_40.hpp>
+#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_40.hpp>
 #elif HPX_ACTION_ARGUMENT_LIMIT <= 50
-#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_direct_50.hpp>
+#include <hpx/lcos/preprocessed/deferred_packaged_task_constructors_50.hpp>
 #else
 #error "HPX_ACTION_ARGUMENT_LIMIT out of bounds for preprocessed headers"
 #endif
