3d14a2f1bfd3cb46c871d8867c90142b55bb0b26
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-08-04 14:33:02 -0500

    Working on #438

--- src/hpx_main_argc_argv.cpp
+++ src/hpx_main_argc_argv.cpp
@@ -5,24 +5,15 @@
 
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/hpx_init.hpp>
-#include <hpx/util/get_commandline_args.hpp>
-
-#include <boost/scoped_array.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Forwarding of hpx_main, if necessary. This has to be in a separate
 // translation unit to ensure the linker can pick or ignore this function,
 // depending on whether the main executable defines this symbol or not.
-int hpx_main()
+int hpx_main(int argc, char* argv[])
 {
-    // Reconstruct the command line arguments from system settings.
-    std::vector<std::string> args = hpx::util::get_commandline_args();
-
-    // Copy all arguments which are not hpx related to a temporary array
-    boost::scoped_array<char*> argv(new char*[args.size()]);
-    for(std::size_t i = 0; i < args.size(); ++i)
-        argv[i] = const_cast<char*>(args[i].data());
-
-    // Invoke hpx_main
-    return hpx_main(static_cast<int>(args.size()), argv.get());
+    // Invoke hpx::user_main
+    int result = hpx::user_main(argc, argv);
+    hpx::finalize();
+    return result;
 }
