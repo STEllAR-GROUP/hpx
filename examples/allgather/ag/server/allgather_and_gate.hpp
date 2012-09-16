8f63bc9252165650411c7f68dedab8d7200b86a4
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-09-15 21:38:55 -0500

    Fixing all_gather example (accounting for recent changes in and_gate).

--- examples/allgather/ag/server/allgather_and_gate.hpp
+++ examples/allgather/ag/server/allgather_and_gate.hpp
@@ -49,12 +49,12 @@ namespace ag { namespace server
     private:
         mutable mutex_type mtx_;
 
-        std::size_t rank_;                // our rank
+        std::size_t rank_;                              // our rank
 
         std::vector<double> n_;                         // all-gathered values
         std::vector<hpx::naming::id_type> components_;  // components for allgather
 
-        hpx::lcos::local::and_gate gate_; // synchronization gate
+        hpx::lcos::local::base_and_gate<> gate_;        // synchronization gate
     };
 }}
 
