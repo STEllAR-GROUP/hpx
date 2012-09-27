a5fe229f1455ade44cc0de22b0abda51cc4b5a51
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-09-27 14:44:17 -0500

    Adding prefix of executable to the component paths searched for installed HPX components. As long as external projects maintain the same directory structure as HPX itself all components should be found by default without any additional ini file.

--- src/util/find_prefix.cpp
+++ src/util/find_prefix.cpp
@@ -7,7 +7,18 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <hpx/config.hpp>
+#include <hpx/exception.hpp>
 
+#if defined(BOOST_WINDOWS)
+#  include <windows.h>
+#elif defined(__linux__)
+#  include <unistd.h>
+#  include <linux/limits.h>
+#elif __APPLE__
+#  include <mach-o/dyld.h>
+#endif
+
+#include <boost/cstdint.hpp>
 #include <boost/plugin/dll.hpp>
 #include <boost/filesystem/path.hpp>
 
@@ -35,6 +46,65 @@ namespace hpx { namespace util
             ;   // just ignore loader problems
         }
         return HPX_PREFIX;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    std::string get_executable_prefix()
+    {
+        using boost::filesystem::path;
+        path p(get_executable_filename());
+
+        return p.parent_path().parent_path().string();
+    }
+
+    std::string get_executable_filename()
+    {
+        std::string r;
+
+#if defined(BOOST_WINDOWS)
+        char exe_path[MAX_PATH + 1] = { '\0' };
+        if (!GetModuleFileName(NULL, exe_path, sizeof(exe_path)))
+        {
+            HPX_THROW_EXCEPTION(hpx::dynamic_link_failure,
+                "get_executable_filename",
+                "unable to find executable filename");
+        }
+        r = exe_path;
+
+#elif defined(__linux__)
+        char exe_path[PATH_MAX + 1];
+        ssize_t length = readlink("/proc/self/exe", exe_path, sizeof(exe_path));
+
+        if (length == -1) 
+        {
+            HPX_THROW_EXCEPTION(hpx::dynamic_link_failure,
+                "get_executable_filename",
+                "unable to find executable filename, /proc may be unavailable");
+        }
+
+        exe_path[length] = '\0';
+        r = exe_path;
+
+#elif defined(__APPLE__)
+        char exe_path[MAXPATHLEN + 1];
+        boost::uint32_t buf_length
+
+        int length = _NSGetExecutablePath(exe_path, &len);
+        if (0 != _NSGetExecutablePath(exe_path, &len))
+        {
+            HPX_THROW_EXCEPTION(hpx::dynamic_link_failure,
+                "get_executable_filename",
+                "unable to find executable filename");
+        }
+
+        exe_path[length] = '\0';
+        r = exe_path;
+
+#else
+#  error Unsupported platform
+#endif
+
+        return r;
     }
 }}
 
