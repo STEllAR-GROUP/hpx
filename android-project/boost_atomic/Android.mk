3ef552cc6a47b6672edaf563146553c44708ac72
Thomas Heller <thom.heller@gmail.com>
2012-10-08 16:45:57 +0200

    Adding manually created Android Build system
    TODO: In order to fully support and fix #459 we need to have cmake generate the necessary Android.mk files

---
+++ android-project/boost_atomic/Android.mk
@@ -0,0 +1,23 @@
+
+# Copyright (c) 2012 Thomas Heller
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+MY_DIR:=$(call my-dir)
+include $(MY_DIR)/../boost_common.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE:=boost_atomic
+LOCAL_CPP_EXTENSION:=.cpp
+LOCAL_C_INCLUDES:=$(BOOST_SRC_ROOT)
+#LOCAL_EXPORT_C_INCLUDES:=$(LOCAL_C_INCLUDES)
+LOCAL_PATH:=$(BOOST_SRC_ROOT)
+LOCAL_SRC_FILES:=libs/atomic/src/lockpool.cpp
+LOCAL_CPPFLAGS:=-DBOOST_ATOMIC_DYN_LINK=1
+LOCAL_CPPFLAGS+=-DBOOST_ATOMIC_SOURCE
+NDK_TOOLCHAIN_VERSION:=4.6
+LOCAL_ARM_NEON:=true
+LOCAL_LDLIBS := -fuse-ld=gold
+LOCAL_CPP_FEATURES:=exceptions rtti
+include $(BUILD_SHARED_LIBRARY)
