0a55ea49ee20c70a92a63c0dcf94a6c9c0638ed3
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-10-08 13:33:56 -0500

    Fixes to the prtial preprocessing for new create component functionality

--- hpx/runtime/components/server/create_component_with_args.hpp
+++ hpx/runtime/components/server/create_component_with_args.hpp
@@ -8,12 +8,16 @@
 #ifndef HPX_RUNTIME_COMPONENTS_SERVER_CREATE_COMPONENT_WITH_ARGS_HPP
 #define HPX_RUNTIME_COMPONENTS_SERVER_CREATE_COMPONENT_WITH_ARGS_HPP
 
-#include <boost/utility/enable_if.hpp>
 #include <hpx/util/detail/remove_reference.hpp>
 #include <hpx/util/move.hpp>
-#include <boost/type_traits/remove_const.hpp>
-
 #include <hpx/runtime/components/server/create_component.hpp>
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits/remove_const.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/enum_params.hpp>
+#include <boost/preprocessor/iterate.hpp>
+#include <boost/preprocessor/repeat.hpp>
 
 namespace hpx { namespace components { namespace server
 {
@@ -41,6 +45,14 @@ namespace hpx { namespace components { namespace server
         BOOST_PP_CAT(a, N) = boost::move(other. BOOST_PP_CAT(a, N));          \
     /**/
 
+#if !defined(HPX_USE_PREPROCESSOR_LIMIT_EXPANSION)
+#  include <hpx/runtime/components/server/preprocessed/create_component_with_args.hpp>
+#else
+
+#if defined(__WAVE__) && defined(HPX_CREATE_PREPROCESSED_FILES)
+#  pragma wave option(preserve: 1, line: 0, output: "preprocessed/create_component_with_args_" HPX_LIMIT_STR ".hpp")
+#endif
+
 #define BOOST_PP_ITERATION_PARAMS_1                                           \
     (                                                                         \
         3                                                                     \
@@ -53,20 +65,28 @@ namespace hpx { namespace components { namespace server
     /**/
 
 #include BOOST_PP_ITERATE()
+
+#if defined(__WAVE__) && defined (HPX_CREATE_PREPROCESSED_FILES)
+#  pragma wave option(output: null)
+#endif
+
+#endif // !defined(HPX_USE_PREPROCESSOR_LIMIT_EXPANSION)
+
 }}}
 
-#undef HPX_RUNTIME_SUPPORT_COMPONENT_CTOR
 #undef HPX_RUNTIME_SUPPORT_CTOR_M0
 #undef HPX_RUNTIME_SUPPORT_CTOR_M1
 #undef HPX_RUNTIME_SUPPORT_CTOR_M2
 #undef HPX_RUNTIME_SUPPORT_CTOR_M3
+#undef HPX_RUNTIME_SUPPORT_CTOR_M4
+#undef HPX_RUNTIME_SUPPORT_CTOR_M5
 
 #endif
 
 #else
 
 #define N BOOST_PP_ITERATION()
-        
+
         template <typename Component, BOOST_PP_ENUM_PARAMS(N, typename A)>
         struct BOOST_PP_CAT(component_constructor_functor, N)
         {
