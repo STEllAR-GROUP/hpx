5f03683a9c59b255bd1a065b5bbecafb2f30b8ae
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-10-08 13:33:56 -0500

    Fixes to the prtial preprocessing for new create component functionality

---
+++ hpx/runtime/components/server/preprocessed/create_component_with_args.hpp
@@ -0,0 +1,36 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//  Copyright (c)      2012 Thomas Heller
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_PREPROCESSED_RUNTIME_COMPONENTS_SERVER_CREATE_COMPONENT_WITH_ARGS_HPP)
+#define HPX_PREPROCESSED_RUNTIME_COMPONENTS_SERVER_CREATE_COMPONENT_WITH_ARGS_HPP
+
+#if HPX_ACTION_ARGUMENT_LIMIT  <= 5
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_5.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 10
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_10.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 15
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_15.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 20
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_20.hpp>
+/*
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 25
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_25.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 30
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_30.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 35
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_35.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 40
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_40.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 45
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_45.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 50
+#include <hpx/runtime/components/server/preprocessed/create_component_with_args_50.hpp>
+*/
+#else
+#error "HPX_ACTION_ARGUMENT_LIMIT out of bounds for preprocessed headers"
+#endif
+
+#endif
