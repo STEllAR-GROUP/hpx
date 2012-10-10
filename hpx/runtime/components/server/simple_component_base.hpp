8fa0ed5a787fcbad40085dd38df495e5642b297a
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-10-10 12:55:38 -0500

    Components: Add component ctor support for simple components.

--- hpx/runtime/components/server/simple_component_base.hpp
+++ hpx/runtime/components/server/simple_component_base.hpp
@@ -146,6 +146,18 @@ namespace hpx { namespace components
         typedef component_type derived_type;
         typedef detail::simple_heap_factory<component_type> heap_type;
 
+#define SIMPLE_COMPONENT_CONSTRUCT(Z, N, _)                                   \
+        template <BOOST_PP_ENUM_PARAMS(N, typename T)>                        \
+        simple_component(HPX_ENUM_FWD_ARGS(N, T, t))                          \
+          : Component(HPX_ENUM_FORWARD_ARGS(N, T, t))                         \
+        {}                                                                    \
+    /**/
+
+        BOOST_PP_REPEAT_FROM_TO(1, HPX_COMPONENT_CREATE_ARGUMENT_LIMIT,
+            SIMPLE_COMPONENT_CONSTRUCT, _)
+
+#undef SIMPLE_COMPONENT_CONSTRUCT
+
         /// \brief  The function \a create is used for allocation and
         ///         initialization of instances of the derived components.
 #if defined(NDEBUG) && defined(BOOST_DISABLE_ASSERTS)
