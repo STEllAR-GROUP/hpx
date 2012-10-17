d3ac3bc568e33092cbc98ab9807d9427ebff1ef9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-10-17 07:24:34 -0500

    Cleaning up residual effects of recent change.

--- hpx/runtime/parcelset/policies/global_parcelhandler_queue.hpp
+++ hpx/runtime/parcelset/policies/global_parcelhandler_queue.hpp
@@ -73,7 +73,7 @@ namespace hpx { namespace parcelset { namespace policies
             return true;
         }
 
-        bool get_parcel(parcel& p)
+        bool get_parcel(parcel& p, error_code& ec)
         {
             // Remove the first parcel from queue.
             mutex_type::scoped_lock l(mtx_);
@@ -84,7 +84,9 @@ namespace hpx { namespace parcelset { namespace policies
                 errors_.pop();
 
                 // now rethrow the topmost exception
-                boost::rethrow_exception(e);
+                // now rethrow the topmost exception
+                HPX_RETHROWS_IF(ec, serialization_error, 
+                    "parcelset::get_parcel", boost::to_string(e));
             }
             else if (!parcels_.empty()) {
                 parcel_map_type::iterator front = parcels_.begin();
@@ -96,16 +98,28 @@ namespace hpx { namespace parcelset { namespace policies
             return false;
         }
 
-        bool get_parcel(parcel& p, naming::gid_type const& parcel_id)
+        bool get_parcel(parcel& p, naming::gid_type const& parcel_id,
+            error_code& ec)
         {
             // Remove the requested parcel from queue.
             mutex_type::scoped_lock l(mtx_);
 
-            parcel_map_type::iterator it = parcels_.find(parcel_id);
-            if (it != parcels_.end()) {
-                p = (*it).second;
-                parcels_.erase(it);
-                return true;
+            if (!errors_.empty()) {
+                // handle pending exceptions first
+                boost::exception_ptr e = errors_.front();
+                errors_.pop();
+
+                // now rethrow the topmost exception
+                HPX_RETHROWS_IF(ec, serialization_error, 
+                    "parcelset::get_parcel", boost::to_string(e));
+            }
+            else {
+                parcel_map_type::iterator it = parcels_.find(parcel_id);
+                if (it != parcels_.end()) {
+                    p = (*it).second;
+                    parcels_.erase(it);
+                    return true;
+                }
             }
 
             return false;
