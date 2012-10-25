8902cc39ec4860dca18e8ce5a420524d56f10f68
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-10-25 14:46:48 -0500

    Modifying pingpong example to show how to pass large vectors in a shared_ptr.

--- examples/quickstart/pingpong.cpp
+++ examples/quickstart/pingpong.cpp
@@ -13,24 +13,50 @@
 
 #include <vector>
 
+#include <boost/serialization/shared_ptr.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
 const std::size_t vsize_default = 1024*1024;
 const std::size_t numiter_default = 5;
 
-void onRecv(hpx::naming::id_type to, std::vector<double> const & in, std::size_t counter);
-HPX_PLAIN_ACTION(onRecv, onRecv_action);
+///////////////////////////////////////////////////////////////////////////////
+void on_recv(hpx::naming::id_type to, std::vector<double> const & in, 
+    std::size_t counter);
+HPX_PLAIN_ACTION(on_recv, on_recv_action);
 
-void onRecv(hpx::naming::id_type to, std::vector<double> const & in, std::size_t counter)
+void on_recv(hpx::naming::id_type to, std::vector<double> const & in, 
+    std::size_t counter)
 {
     // received vector in
     if (--counter == 0) return;
 
-    // send it to remote locality (to), and wait till it is received
-    std::vector<double> mydata(in);
+    // send it to remote locality (to), and wait until it is received
+    std::vector<double> data(in);
 
-    onRecv_action act;
-    act(to, hpx::find_here(), boost::move(mydata), counter);
+    on_recv_action act;
+    act(to, hpx::find_here(), boost::move(data), counter);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+void on_recv_ind(hpx::naming::id_type to, 
+    boost::shared_ptr<std::vector<double> > in, std::size_t counter);
+HPX_PLAIN_ACTION(on_recv_ind, on_recv_ind_action);
+
+void on_recv_ind(hpx::naming::id_type to, 
+    boost::shared_ptr<std::vector<double> > in, std::size_t counter)
+{
+    // received vector in
+    if (--counter == 0) return;
+
+    // send it to remote locality (to), and wait until it is received
+    boost::shared_ptr<std::vector<double> > data(
+        boost::make_shared<std::vector<double> >(*in));
+
+    on_recv_ind_action act;
+    act(to, hpx::find_here(), data, counter);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 int hpx_main(boost::program_options::variables_map &b_arg)
 {
     std::size_t const vsize = b_arg["vsize"].as<std::size_t>();
@@ -39,20 +65,43 @@ int hpx_main(boost::program_options::variables_map &b_arg)
     std::vector<hpx::naming::id_type> localities = hpx::find_all_localities();
     hpx::naming::id_type to = localities.back(); // send to last element
 
-    std::vector<double> mydata(vsize, double(3.11));
+    // test sending messages back and forth using a larger vector as one of 
+    // the arguments
+    {
+        std::vector<double> data(vsize, double(3.11));
 
-    hpx::util::high_resolution_timer timer1;
+        hpx::util::high_resolution_timer timer1;
 
-    if (numiter != 0) {
-        onRecv_action act;
-        act(to, hpx::find_here(), mydata, numiter);
+        if (numiter != 0) {
+            on_recv_action act;
+            act(to, hpx::find_here(), data, numiter);
+        }
+
+        double time = timer1.elapsed();
+
+        std::cout << "[hpx_pingpong]:total_time(secs)=" << time << ":vsize="
+                  << vsize << ":localities=" << localities.size()
+                  << ":numiter=" << numiter << std::endl;
     }
 
-    double time = timer1.elapsed();
+    // do the same but with a wrapped vector
+    {
+        boost::shared_ptr<std::vector<double> > data(
+            boost::make_shared<std::vector<double> >(vsize, double(3.11)));
 
-    std::cout << "[hpx_pingpong]:total_time(secs)=" << time << ":vsize="
-              << vsize << ":localities=" << localities.size()
-              << ":numiter=" << numiter << std::endl;
+        hpx::util::high_resolution_timer timer1;
+
+        if (numiter != 0) {
+            on_recv_ind_action act;
+            act(to, hpx::find_here(), data, numiter);
+        }
+
+        double time = timer1.elapsed();
+
+        std::cout << "[hpx_pingpong]:total_time_ind(secs)=" << time << ":vsize="
+                  << vsize << ":localities=" << localities.size()
+                  << ":numiter=" << numiter << std::endl;
+    }
 
     hpx::finalize();
     return 0;
