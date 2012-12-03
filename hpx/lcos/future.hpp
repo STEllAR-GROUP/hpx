8a72eb7c0e82afe55807a04a7d59ad0a6180b27b
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-12-03 12:04:25 -0600

    LCOS: Add channel.

--- hpx/lcos/future.hpp
+++ hpx/lcos/future.hpp
@@ -34,6 +34,8 @@ namespace hpx { namespace lcos
         {
             template <typename ContResult> struct continuation_base;
         }
+
+        template <typename T> struct channel;
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -61,6 +63,10 @@ namespace hpx { namespace lcos
         friend class local::packaged_continuation;
         template <typename ContResult>
         friend struct local::detail::continuation_base;
+
+        template <typename T>
+        friend struct local::channel;
+
         template <typename Result_, typename RemoteResult_>
         friend class promise;
         friend struct detail::future_data<Result>;
@@ -256,10 +262,14 @@ namespace hpx { namespace lcos
         friend class local::promise<void>;
         friend class local::packaged_task<void()>;
         friend class local::futures_factory<void()>;
+
         template <typename ContResult, typename Result_>
         friend class local::packaged_continuation;
         template <typename ContResult>
         friend struct local::detail::continuation_base;
+
+        template <typename T>
+        friend struct local::channel;
 
         friend class promise<void, util::unused_type>;
         friend struct detail::future_data<void>;
