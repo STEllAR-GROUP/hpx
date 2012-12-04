77fca6fd650b30329a5796b273f3319b8e540521
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2012-12-04 13:24:41 -0600

    Fixing library names in hello_world example build script

--- examples/hello_world_component/hello.sh
+++ examples/hello_world_component/hello.sh
@@ -12,7 +12,7 @@ c++ -o libhello_world.so hello_world_component.cpp `pkg-config --cflags --libs h
 
 # Create the directory where we want to install the library
 mkdir -p ~/my_hpx_libs
-mv libhpx_component_hello_world.so ~/my_hpx_libs
+mv libhello_worldd.so ~/my_hpx_libs
 
 # If we don't have a .hpx.ini yet, create one and tell it about
 # our private hpx library directory
@@ -32,10 +32,11 @@ path = ${HOME}/my_hpx_libs
 EOF
 
 # Compile the client
-c++ -o hello_world_client hello_world_client.cpp `pkg-config --cflags --libs hpx_application` -lhpx_component_iostreams -lhpx_component_hello_world -L ~/my_hpx_libs
+c++ -o hello_world_client hello_world_client.cpp `pkg-config --cflags --libs hpx_application` -liostreams -lhello_world -L ~/my_hpx_libs
 
 # Prepare the environment so that we can run the command
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/my_hpx_libs"
 
 # Run the client, first add our directory to the LD_LIBRARY_PATH
-./hello_world_client
+./hello_world_client 
+
