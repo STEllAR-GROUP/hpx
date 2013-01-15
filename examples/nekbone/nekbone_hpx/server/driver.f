3864ff6dee2cbd8925ad07fb001642ff28dba288
Matt Anderson <anderson.matthew.w@gmail.com>
2013-01-15 17:03:45 -0500

    cg

--- examples/nekbone/nekbone_hpx/server/driver.f
+++ examples/nekbone/nekbone_hpx/server/driver.f
@@ -427,6 +427,96 @@ c     Weights
 ccccccccccccccccccccccccccccccccccc
       real f(n),c(n)
         end subroutine set_f 
+        subroutine cg(hpx_bti,x,f,g,c,r,w,p,z,n,niter,
+c SIZE
+     &     nelx,nely,nelz,nelt,nelg,
+     &     nx1,ny1,nz1,ndim,nfield,nid,
+c DXYZ
+     &     dxm1,dxtm1,
+c INPUT
+     &     xc,yc,zc,bc,ccurve,cbc,
+c MASS
+     &     bm1,binvm1,volvm1,
+c PARALLEL
+     &     node,pid,np,nullpid,node0
+     &     nelgf,lglel,gllel,gllnid,nelgv,nelgt,
+     &     nvtot,ifgprnt,wdsize,isize,lsize,csize,
+     &     ifdblas,cr_h,gsh,gsh_fld,xxth,
+c WZ
+     &     zgm1,wxm1,wym1,wzm1,w3m1)
+      use, intrinsic :: iso_c_binding, only : c_ptr
+      TYPE(C_PTR), INTENT(IN), VALUE :: hpx_bti
+ccccccccccccccccccccccccccccccccccc
+c SIZE
+c     These numbers come from example1 
+      parameter (ldim=3)
+      parameter (lx1=10,ly1=lx1,lz1=lx1)
+      parameter (lxd=10,lyd=lxd,lzd=1)
+
+      parameter (lp = 2)
+      parameter (lelt=1)
+
+      parameter (lelg=lelt*lp)
+      parameter (lelx=lelg,lely=1,lelz=1)
+
+      parameter (ldimt=1,ldimt1=ldimt+1)
+
+c      common /dimn/ nelx,nely,nelz,nelt,nelg
+c     $            , nx1,ny1,nz1,ndim,nfield,nid
+      integer nelx,nely,nelz,nelt,nelg
+     &            , nx1,ny1,nz1,ndim,nfield,nid
+ccccccccccccccccccccccccccccccccccc
+ccccccccccccccccccccccccccccccccccc
+c DXYZ
+      real dxm1(lx1,lx1),  dxtm1(lx1,lx1)
+ccccccccccccccccccccccccccccccccccc
+c INPUT
+      real xc(8,lelt),yc(8,lelt),zc(8,lelt),
+     &       bc(5,6,lelt,0:ldimt1)
+      character*1     ccurve(12,lelt)
+      character*3     cbc(6,lelt,0:ldimt1)
+ccccccccccccccccccccccccccccccccccc
+c MASS
+      real bm1   (lx1,ly1,lz1,lelt)
+     &      ,binvm1(lx1,ly1,lz1,lelt)
+     &      ,volvm1
+ccccccccccccccccccccccccccccccccccc
+c PARALLEL
+      integer node,pid,np,nullpid,node0
+c     Maximum number of elements (limited to 2**31/12, at least
+c      for now)
+      parameter(nelgt_max = 178956970)
+      integer nelgf(0:ldimt1)
+     &              ,lglel(lelt)
+     &              ,gllel(lelg)
+     &              ,gllnid(lelg)
+     &              ,nelgv,nelgt
+
+      integer*8      nvtot
+
+      logical ifgprnt
+      integer wdsize,isize,lsize,csize
+      logical ifdblas
+C
+C     crystal-router, gather-scatter, and xxt handles (xxt=csr grid
+C     solve)
+C
+      integer cr_h, gsh, gsh_fld(0:ldimt1), xxth(ldimt1)
+ccccccccccccccccccccccccccccccccccc
+c WZ
+c     Gauss-Labotto and Gauss points
+      real zgm1(lx1,3)
+
+c     Weights
+      real wxm1(lx1), wym1(ly1), wzm1(lz1), w3m1(lx1,ly1,lz1)
+ccccccccccccccccccccccccccccccccccc
+      parameter (lxyz=lx1*ly1*lz1)
+      real ur(lxyz),us(lxyz),ut(lxyz),wk(lxyz)
+
+      real x(n),f(n),r(n),w(n),p(n),z(n),g(1),c(n)
+
+      character*1 ans
+        end subroutine cg
       end interface ! }}}
 
       integer hpx_mype,hpx_numberpe
@@ -603,6 +693,25 @@ c WZ
 
         call set_f(
      &     hpx_bti,f,c,n,
+c SIZE
+     &     nelx,nely,nelz,nelt,nelg,
+     &     nx1,ny1,nz1,ndim,nfield,nid,
+c DXYZ
+     &     dxm1,dxtm1,
+c INPUT
+     &     xc,yc,zc,bc,ccurve,cbc,
+c MASS
+     &     bm1,binvm1,volvm1,
+c PARALLEL
+     &     node,pid,np,nullpid,node0
+     &     nelgf,lglel,gllel,gllnid,nelgv,nelgt, 
+     &     nvtot,ifgprnt,wdsize,isize,lsize,csize,
+     &     ifdblas,cr_h,gsh,gsh_fld,xxth,
+c WZ
+     &     zgm1,wxm1,wym1,wzm1,w3m1)
+
+        call cg(
+     &     hpx_bti,x,f,g,c,r,w,p,z,n,niter,
 c SIZE
      &     nelx,nely,nelz,nelt,nelg,
      &     nx1,ny1,nz1,ndim,nfield,nid,
