9993e3890864c951eb72ec1674c283c71a6695e4
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2013-01-16 15:44:38 -0600

    Prepare release.

--- examples/fractal_visualizer/mesh.v.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec3 coord3d;
-attribute vec2 texcoord;
-varying vec2 f_texcoord;
-uniform mat4 mvp;
-
-void main(void) {
-  gl_Position = mvp * vec4(coord3d, 1.0);
-  f_texcoord = texcoord;
-}
