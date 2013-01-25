5359573a1caa39528baec04dd1a9503f3d1f8e41
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-01-25 11:33:22 -0600

    Adding new files

---
+++ hpx/lcos/async_continue_fwd.hpp
@@ -0,0 +1,85 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+///////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_PP_IS_ITERATING
+
+#if !defined(HPX_LCOS_ASYNC_CONTINUE_FWD_JAN_25_2013_0828AM)
+#define HPX_LCOS_ASYNC_CONTINUE_FWD_JAN_25_2013_0828AM
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/traits.hpp>
+#include <hpx/util/move.hpp>
+
+#include <boost/preprocessor/repeat.hpp>
+#include <boost/preprocessor/iterate.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
+
+#if !defined(HPX_USE_PREPROCESSOR_LIMIT_EXPANSION)
+#  include <hpx/lcos/preprocessed/async_fwd.hpp>
+#else
+
+#if defined(__WAVE__) && defined(HPX_CREATE_PREPROCESSED_FILES)
+#  pragma wave option(preserve: 1, line: 0, output: "preprocessed/async_continue_fwd_" HPX_LIMIT_STR ".hpp")
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx
+{
+    ///////////////////////////////////////////////////////////////////////////
+    template <typename Component, typename Result, typename Arguments,
+        typename Derived, typename F>
+    typename boost::enable_if<
+        boost::mpl::bool_<boost::fusion::result_of::size<Arguments>::value == 0>
+      , lcos::future<typename traits::promise_local_result<Result>::type
+    >::type
+    async_continue(
+        hpx::actions::action<
+            Component, Result, Arguments, Derived
+        > /*act*/, naming::id_type const& gid, BOOST_FWD_REF(F) f);
+}
+
+#define BOOST_PP_ITERATION_PARAMS_1                                           \
+    (3, (1, HPX_ACTION_ARGUMENT_LIMIT,                                        \
+    "hpx/lcos/async_continue_fwd.hpp"))                                       \
+    /**/
+
+#include BOOST_PP_ITERATE()
+
+#if defined(__WAVE__) && defined (HPX_CREATE_PREPROCESSED_FILES)
+#  pragma wave option(output: null)
+#endif
+
+#endif // !defined(HPX_USE_PREPROCESSOR_LIMIT_EXPANSION)
+
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+//  Preprocessor vertical repetition code
+///////////////////////////////////////////////////////////////////////////////
+#else // defined(BOOST_PP_IS_ITERATING)
+
+#define N BOOST_PP_ITERATION()
+
+namespace hpx
+{
+    ///////////////////////////////////////////////////////////////////////////
+    template <typename Component, typename Result, typename Arguments,
+        typename Derived, BOOST_PP_ENUM_PARAMS(N, typename Arg), typename F>
+    typename boost::enable_if<
+        boost::mpl::bool_<boost::fusion::result_of::size<Arguments>::value == N>
+      , lcos::future<typename traits::promise_local_result<Result>::type
+    >::type
+    async (
+        hpx::actions::action<
+            Component, Result, Arguments, Derived
+        > /*act*/, naming::id_type const& gid, HPX_ENUM_FWD_ARGS(N, Arg, arg),
+        BOOST_FWD_REF(F) f);
+}
+
+#undef N
+
+#endif
