bfe0454125b67522cde12f6eb899a8c5b4978252
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-01-28 16:50:48 -0600

    Adding apply_continue

--- hpx/apply.hpp
+++ hpx/apply.hpp
@@ -12,6 +12,7 @@
 #include <hpx/async.hpp>
 #include <hpx/runtime/threads/thread_helpers.hpp>
 #include <hpx/runtime/applier/apply.hpp>
+#include <hpx/runtime/applier/apply_continue.hpp>
 #include <hpx/util/move.hpp>
 #include <hpx/util/bind.hpp>
 #include <hpx/util/bind_action.hpp>
---
+++ hpx/runtime/applier/preprocessed/apply_continue.hpp
@@ -0,0 +1,36 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//  Copyright (c)      2012 Thomas Heller
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_PREPROCESSED_RUNTIME_APPLIER_APPLY_CONTINUE_HPP)
+#define HPX_PREPROCESSED_RUNTIME_APPLIER_APPLY_CONTINUE_HPP
+
+#if HPX_ACTION_ARGUMENT_LIMIT  <= 5
+#include <hpx/runtime/applier/preprocessed/apply_continue_5.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 10
+#include <hpx/runtime/applier/preprocessed/apply_continue_10.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 15
+#include <hpx/runtime/applier/preprocessed/apply_continue_15.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 20
+#include <hpx/runtime/applier/preprocessed/apply_continue_20.hpp>
+/*
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 25
+#include <hpx/runtime/applier/preprocessed/apply_continue_25.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 30
+#include <hpx/runtime/applier/preprocessed/apply_continue_30.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 35
+#include <hpx/runtime/applier/preprocessed/apply_continue_35.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 40
+#include <hpx/runtime/applier/preprocessed/apply_continue_40.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 45
+#include <hpx/runtime/applier/preprocessed/apply_continue_45.hpp>
+#elif HPX_ACTION_ARGUMENT_LIMIT <= 50
+#include <hpx/runtime/applier/preprocessed/apply_continue_50.hpp>
+*/
+#else
+#error "HPX_ACTION_ARGUMENT_LIMIT out of bounds for preprocessed headers"
+#endif
+
+#endif
