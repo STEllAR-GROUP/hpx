da4a3ef394d1f9a56668fa72db2661fb9047a9bc
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-02-05 12:30:28 -0600

    Interpreting more bind options, not quite there yet

--- hpx/runtime/threads/topology.hpp
+++ hpx/runtime/threads/topology.hpp
@@ -51,7 +51,7 @@ namespace hpx { namespace threads
         ///        processing unit available to the service threads in the
         ///        application.
         ///
-        /// \param used_processing_units [in] This is the mask of processing 
+        /// \param used_processing_units [in] This is the mask of processing
         ///                   units which are not available for service threads.
         /// \param ec         [in,out] this represents the error status on exit,
         ///                   if this is pre-initialized to \a hpx#throws
@@ -106,7 +106,7 @@ namespace hpx { namespace threads
         ///                   if this is pre-initialized to \a hpx#throws
         ///                   the function will throw on error instead.
         ///
-        /// \note  Use this function on systems where the affinity must be 
+        /// \note  Use this function on systems where the affinity must be
         ///        set from outside the thread itself.
         virtual void set_thread_affinity_mask(boost::thread& t,
             mask_type mask, error_code& ec = throws) const = 0;
@@ -119,13 +119,13 @@ namespace hpx { namespace threads
         ///                   if this is pre-initialized to \a hpx#throws
         ///                   the function will throw on error instead.
         ///
-        /// \note  Use this function on systems where the affinity must be 
+        /// \note  Use this function on systems where the affinity must be
         ///        set from inside the thread itself.
         virtual void set_thread_affinity_mask(mask_type mask,
             error_code& ec = throws) const = 0;
 
         /// \brief Return a bit mask where each set bit corresponds to a
-        ///        processing unit co-located with the memory the given 
+        ///        processing unit co-located with the memory the given
         ///        address is currently allocated on.
         ///
         /// \param ec         [in,out] this represents the error status on exit,
@@ -177,7 +177,7 @@ namespace hpx { namespace threads
         struct spec_type
         {
             enum type { unknown, thread, socket, numanode, core, pu };
-            static char const* const type_name(type t);
+            HPX_API_EXPORT static char const* const type_name(type t);
 
             spec_type(type t = unknown, mask_type min = ~0x0ul, mask_type max = ~0x0ul)
               : type_(t), index_min_(min), index_max_(max)
@@ -199,11 +199,11 @@ namespace hpx { namespace threads
         typedef std::pair<spec_type, mapping_type> full_mapping_type;
         typedef std::vector<full_mapping_type> mappings_type;
 
-        HPX_API_EXPORT void parse_mappings(std::string const& spec, 
+        HPX_API_EXPORT void parse_mappings(std::string const& spec,
             mappings_type& mappings, error_code& ec = throws);
     }
 
-    HPX_API_EXPORT void parse_affinity_options(std::string const& spec, 
+    HPX_API_EXPORT void parse_affinity_options(std::string const& spec,
         std::vector<mask_type>& affinities, error_code& ec = throws);
 #endif
 
--- tests/unit/util/parse_affinity_options.cpp
+++ tests/unit/util/parse_affinity_options.cpp
@@ -9,121 +9,210 @@
 #include <hpx/include/threads.hpp>
 #include <hpx/util/lightweight_test.hpp>
 
+#include <boost/assign/std/vector.hpp>
+
+#include <iostream>
+#include <algorithm>
+
+// The affinity masks this test is verifying the results against are specific 
+// to a particular machine. If you enable this option you might see a lot of 
+// test failures, which is expected.
+// #define VERIFY_AFFINITY_MASKS
+
 ///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace threads { namespace detail
+{
+    std::ostream& operator<<(std::ostream& os, spec_type const& data)
+    {
+        os << spec_type::type_name(data.type_)
+           << "," << data.index_min_ << "," << data.index_max_;
+        return os;
+    }
+}}}
+
 namespace test
 {
     using hpx::threads::detail::spec_type;
 
-    struct data_good
+    struct data_good_thread
     {
-        std::string option_;
         spec_type thread;
         spec_type socket;
         spec_type core;
         spec_type pu;
     };
 
+    struct data_good
+    {
+        std::string option_;
+        data_good_thread t[2];
+        hpx::threads::mask_type masks[2];
+    };
+
     data_good data[] =
     {
-        { "thread:1=socket:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::socket, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::unknown, 0, 0)
+        {   "thread:0=socket:0;thread:1=socket:0",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 0),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }, {
+                    spec_type(spec_type::thread, 1, 0),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }
+            },
+            { 0xfff, 0xfff }
         },
-        { "thread:0-1=socket:0",
-          spec_type(spec_type::thread, 0, 1),
-          spec_type(spec_type::socket, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::unknown, 0, 0)
+        {   "thread:0-1=socket:0",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 1),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }
+            },
+            { 0xfff, 0xfff }
         },
-        { "thread:1=numanode:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::numanode, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::unknown, 0, 0)
+        {   "thread:0=numanode:0;thread:1=numanode:0",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 0),
+                    spec_type(spec_type::numanode, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }, {
+                    spec_type(spec_type::thread, 1, 0),
+                    spec_type(spec_type::numanode, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }
+            },
+            { 0xfff, 0xfff }
         },
-        { "thread:0-1=numanode:0",
-          spec_type(spec_type::thread, 0, 1),
-          spec_type(spec_type::numanode, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::unknown, 0, 0)
+        {   "thread:0-1=numanode:0",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 1),
+                    spec_type(spec_type::numanode, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }
+            },
+            { 0xfff, 0xfff }
         },
-        { "thread:1=socket:0.core:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::socket, 0, 0),
-          spec_type(spec_type::core, 0, 0),
-          spec_type(spec_type::unknown, 0, 0)
+        {   "thread:0=socket:0.core:0;thread:1=socket:0.core:1",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 0),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::core, 0, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }, {
+                    spec_type(spec_type::thread, 1, 0),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::core, 1, 0),
+                    spec_type(spec_type::unknown, 0, 0)
+                }
+            },
+            { 0x003, 0x00c }
         },
-        { "thread:0-1=socket:0.core:0-1",
-          spec_type(spec_type::thread, 0, 1),
-          spec_type(spec_type::socket, 0, 0),
-          spec_type(spec_type::core, 0, 1),
-          spec_type(spec_type::unknown, 0, 0)
+        {   "thread:0-1=socket:0.core:0-1",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 1),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::core, 0, 1),
+                    spec_type(spec_type::unknown, 0, 0)
+                }
+            },
+            { 0x003, 0x00c }
         },
-        { "thread:1=numanode:0.core:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::numanode, 0, 0),
-          spec_type(spec_type::core, 0, 0),
-          spec_type(spec_type::unknown, 0, 0)
-        },
-        { "thread:1=socket:0.core:0.pu:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::socket, 0, 0),
-          spec_type(spec_type::core, 0, 0),
-          spec_type(spec_type::pu, 0, 0)
-        },
-        { "thread:1=numanode:0.core:0.pu:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::numanode, 0, 0),
-          spec_type(spec_type::core, 0, 0),
-          spec_type(spec_type::pu, 0, 0)
-        },
-        { "thread:1=socket:0.pu:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::socket, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::pu, 0, 0)
-        },
-        { "thread:1=numanode:0.pu:0",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::numanode, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::pu, 0, 0)
+        {   "thread:0-1=socket:0.core:all.pu:0",
+            {
+                {
+                    spec_type(spec_type::thread, 0, 1),
+                    spec_type(spec_type::socket, 0, 0),
+                    spec_type(spec_type::core, ~0x0ul, 0),
+                    spec_type(spec_type::pu, 0, 0)
+                }
+            },
+            { 0x001, 0x008 }
         },
 
-        { "thread:1=core:3",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::core, 3, 0),
-          spec_type(spec_type::unknown, 0, 0)
-        },
-        { "thread:1=core:0.pu:2",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::core, 0, 0),
-          spec_type(spec_type::pu, 2, 0)
-        },
-
-        { "thread:1=pu:2",
-          spec_type(spec_type::thread, 1, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::pu, 2, 0)
-        },
-
-        { "thread:2=core:all.pu:1",
-          spec_type(spec_type::thread, 2, 0),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::core, ~0x0ul, 0),
-          spec_type(spec_type::pu, 1, 0)
-        },
-        { "t:0-3=c:0-3.p:1",
-          spec_type(spec_type::thread, 0, 3),
-          spec_type(spec_type::unknown, 0, 0),
-          spec_type(spec_type::core, 0, 3),
-          spec_type(spec_type::pu, 1, 0)
-        },
+//         { "thread:0-1=socket:0.core:0-1",
+//           spec_type(spec_type::thread, 0, 1),
+//           spec_type(spec_type::socket, 0, 0),
+//           spec_type(spec_type::core, 0, 1),
+//           spec_type(spec_type::unknown, 0, 0)
+//         },
+//         { "thread:1=numanode:0.core:0",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::numanode, 0, 0),
+//           spec_type(spec_type::core, 0, 0),
+//           spec_type(spec_type::unknown, 0, 0)
+//         },
+//         { "thread:1=socket:0.core:0.pu:0",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::socket, 0, 0),
+//           spec_type(spec_type::core, 0, 0),
+//           spec_type(spec_type::pu, 0, 0)
+//         },
+//         { "thread:1=numanode:0.core:0.pu:0",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::numanode, 0, 0),
+//           spec_type(spec_type::core, 0, 0),
+//           spec_type(spec_type::pu, 0, 0)
+//         },
+//         { "thread:1=socket:0.pu:0",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::socket, 0, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::pu, 0, 0)
+//         },
+//         { "thread:1=numanode:0.pu:0",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::numanode, 0, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::pu, 0, 0)
+//         },
+//
+//         { "thread:1=core:3",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::core, 3, 0),
+//           spec_type(spec_type::unknown, 0, 0)
+//         },
+//         { "thread:1=core:0.pu:2",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::core, 0, 0),
+//           spec_type(spec_type::pu, 2, 0)
+//         },
+//
+//         { "thread:1=pu:2",
+//           spec_type(spec_type::thread, 1, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::pu, 2, 0)
+//         },
+//
+//         { "thread:2=core:all.pu:1",
+//           spec_type(spec_type::thread, 2, 0),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::core, ~0x0ul, 0),
+//           spec_type(spec_type::pu, 1, 0)
+//         },
+//         { "t:0-3=c:0-3.p:1",
+//           spec_type(spec_type::thread, 0, 3),
+//           spec_type(spec_type::unknown, 0, 0),
+//           spec_type(spec_type::core, 0, 3),
+//           spec_type(spec_type::pu, 1, 0)
+//         },
         { "" }
     };
 
@@ -135,16 +224,35 @@ namespace test
             hpx::error_code ec;
             hpx::threads::detail::parse_mappings(t->option_, mappings, ec);
             HPX_TEST(!ec);
-            HPX_TEST(mappings.size() == 1);
-            if (mappings.size() == 1) {
-                HPX_TEST(t->thread == mappings[0].first);
-                HPX_TEST(mappings[0].second.size() == 3);
-                if (mappings[0].second.size() == 3) {
-                    HPX_TEST(t->socket == mappings[0].second[0]);
-                    HPX_TEST(t->core == mappings[0].second[1]);
-                    HPX_TEST(t->pu == mappings[0].second[2]);
+
+            int i = 0;
+            BOOST_FOREACH(hpx::threads::detail::full_mapping_type const& m, mappings)
+            {
+                HPX_TEST_EQ(t->t[i].thread, m.first);
+                HPX_TEST_EQ(m.second.size(), 3);
+                if (m.second.size() == 3) {
+                    HPX_TEST_EQ(t->t[i].socket, m.second[0]);
+                    HPX_TEST_EQ(t->t[i].core, m.second[1]);
+                    HPX_TEST_EQ(t->t[i].pu, m.second[2]);
                 }
+                ++i;
             }
+
+#if defined(VERIFY_AFFINITY_MASKS)
+            std::vector<hpx::threads::mask_type> affinities;
+            affinities.resize(hpx::get_os_thread_count(), 0);
+            hpx::threads::parse_affinity_options(t->option_, affinities, ec);
+            HPX_TEST(!ec);
+            HPX_TEST_EQ(affinities.size(), 2);
+            HPX_TEST_EQ(std::count(affinities.begin(), affinities.end(), 0), 0);
+
+            i = 0;
+            BOOST_FOREACH(hpx::threads::mask_type m, affinities)
+            {
+                HPX_TEST_EQ(t->masks[i], m);
+                ++i;
+            }
+#endif
         }
     }
 
@@ -206,8 +314,13 @@ int hpx_main()
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
+    // We force this test to use 2 threads by default.
+    using namespace boost::assign;
+    std::vector<std::string> cfg;
+    cfg += "hpx.os_threads=2";
+
     // Initialize and run HPX
-    HPX_TEST(0 == hpx::init(argc, argv));
+    HPX_TEST(0 == hpx::init(argc, argv, cfg));
     return hpx::util::report_errors();
 }
 
