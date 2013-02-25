d8e64f72f268a8ccb1ab4d3e8ae0ff7b2ba7e271
Jeroen Habraken <jhabraken@cct.lsu.edu>
2013-02-25 13:37:02 -0600

    Memory counter for Mac OS X

---
+++ src/components/memory/mem_counter_macosx.cpp
@@ -0,0 +1,62 @@
+// Copyright (c) 2013 Jeroen Habraken
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_fwd.hpp>
+
+#if defined(__APPLE__)
+
+#include <hpx/exception.hpp>
+#include <boost/format.hpp>
+#include <mach/mach.h>
+#include <mach/task.h>
+
+namespace hpx { namespace performance_counters { namespace memory
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // returns virtual memory value
+    boost::uint64_t read_psm_vm()
+    {
+        struct task_basic_info t_info;
+        mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+        if (task_info(mach_task_self(),
+                TASK_BASIC_INFO,
+                reinterpret_cast<task_info_t>(&t_info),
+                &t_info_count) != KERN_SUCCESS)
+        {
+            HPX_THROW_EXCEPTION(kernel_error,
+                "hpx::performance_counters::memory::read_psm_vm",
+                "task_info failed");
+
+            return boost::uint64_t(-1);
+        }
+
+        return t_info.virtual_size;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // returns resident memory value
+    boost::uint64_t read_psm_resident()
+    {
+        struct task_basic_info t_info;
+        mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+        if (task_info(mach_task_self(),
+                TASK_BASIC_INFO,
+                reinterpret_cast<task_info_t>(&t_info),
+                &t_info_count) != KERN_SUCCESS)
+        {
+            HPX_THROW_EXCEPTION(kernel_error,
+                "hpx::performance_counters::memory::read_psm_vm",
+                "task_info failed");
+
+            return boost::uint64_t(-1);
+        }
+
+        return t_info.resident_size;
+    }
+}}}
+
+#endif
