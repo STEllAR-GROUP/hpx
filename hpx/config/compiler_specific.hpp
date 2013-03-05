68cc17ae98c8dd9a0469a734d4e5ca3306f8e442
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-03-05 13:04:14 -0600

    Trying to fix gcc 4.4 action compilation

--- hpx/config/compiler_specific.hpp
+++ hpx/config/compiler_specific.hpp
@@ -6,11 +6,19 @@
 #if !defined(HPX_COMPILER_SPECIFIC_201204261048)
 #define HPX_COMPILER_SPECIFIC_201204261048
 
-// macros to facilitate handling of compiler-specific issues
-#define HPX_GCC_VERSION (__GNUC__*10000 + __GNUC_MINOR__*100 + __GNUC__PATCHLEVEL__)
+#if defined(__GNUC__)
 
-#if HPX_GCC_VERSION >= 40600
-#define HPX_GCC_DIAGNOSTIC_PRAGMA_CONTEXTS 1
+// macros to facilitate handling of compiler-specific issues
+#  define HPX_GCC_VERSION (__GNUC__*10000 + __GNUC_MINOR__*100 + __GNUC__PATCHLEVEL__)
+
+#  if HPX_GCC_VERSION >= 40600
+#    define HPX_GCC_DIAGNOSTIC_PRAGMA_CONTEXTS 1
+#  endif
+
+#else
+
+#  undef HPX_GCC_VERSION
+
 #endif
 
 #endif
