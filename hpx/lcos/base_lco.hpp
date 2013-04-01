a1ffaed4a459e17ec3a38941eaaed9e5eab9f362
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-04-01 16:03:51 -0500

    Recreating message_handler branch

--- hpx/lcos/base_lco.hpp
+++ hpx/lcos/base_lco.hpp
@@ -140,11 +140,11 @@ HPX_REGISTER_ACTION_DECLARATION(
     hpx::lcos::base_lco::disconnect_action, base_disconnect_action)
 
 ///////////////////////////////////////////////////////////////////////////////
-// HPX_ACTION_USES_MESSAGE_COALESCING_NOTHROW(
-//     hpx::lcos::base_lco::set_event_action, "lco_set_value_action",
-//     std::size_t(-1), std::size_t(-1))
-// HPX_ACTION_USES_MESSAGE_COALESCING_NOTHROW(
-//     hpx::lcos::base_lco::set_exception_action, "lco_set_value_action",
-//     std::size_t(-1), std::size_t(-1))
+HPX_ACTION_USES_MESSAGE_COALESCING_NOTHROW(
+    hpx::lcos::base_lco::set_event_action, "lco_set_value_action",
+    std::size_t(-1), std::size_t(-1))
+HPX_ACTION_USES_MESSAGE_COALESCING_NOTHROW(
+    hpx::lcos::base_lco::set_exception_action, "lco_set_value_action",
+    std::size_t(-1), std::size_t(-1))
 
 #endif
