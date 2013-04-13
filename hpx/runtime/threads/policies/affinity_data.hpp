7443bdd22b346d9cf1752fc694df43761c666a08
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-04-13 14:11:56 -0500

    Making gcc happy

--- hpx/runtime/threads/policies/affinity_data.hpp
+++ hpx/runtime/threads/policies/affinity_data.hpp
@@ -31,7 +31,7 @@ namespace hpx { namespace threads { namespace policies { namespace detail
         mask_cref_type get_pu_mask(topology const& topology,
             std::size_t num_thread, bool numa_sensitive) const;
 
-        std::size_t affinity_data::get_pu_num(std::size_t num_thread) const
+        std::size_t get_pu_num(std::size_t num_thread) const
         {
             BOOST_ASSERT(num_thread < pu_nums_.size());
             return pu_nums_[num_thread];
