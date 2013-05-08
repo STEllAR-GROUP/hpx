8563d65d824b162a3921d0d170301e96cf73c90f
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-05-08 09:43:24 -0500

    Tabs --> Spaces

--- tests/performance/hpx_homogeneous_timed_task_spawn.cpp
+++ tests/performance/hpx_homogeneous_timed_task_spawn.cpp
@@ -92,8 +92,8 @@ int hpx_main(
             throw std::invalid_argument("count of 0 tasks specified\n");
 
         // Reset performance counters (if specified on command line)
-		reset_active_counters();
-        
+        reset_active_counters();
+
         // Start the clock.
         high_resolution_timer t;
 
@@ -107,11 +107,11 @@ int hpx_main(
             suspend();
         } while (get_thread_count(hpx::threads::thread_priority_normal) > 1);
 
-		// Stop the clock
-		double time_elapsed=t.elapsed();
-		
-		// Evaluate Performance Counters
-		evaluate_active_counters();
+        // Stop the clock
+        double time_elapsed = t.elapsed();
+
+        // Evaluate Performance Counters
+        evaluate_active_counters();
 
         print_results(get_os_thread_count(), time_elapsed);
     }
