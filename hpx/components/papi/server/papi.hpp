7cc62c621f9f94797fdb858627fa947b5c2f22e6
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-05-10 12:00:33 -0500

    Applying patch provided by Kevin Hook, also tabs --> spaces

--- hpx/components/papi/server/papi.hpp
+++ hpx/components/papi/server/papi.hpp
@@ -175,10 +175,10 @@ namespace hpx { namespace performance_counters { namespace papi { namespace serv
         virtual bool start();
         // stop the counter
         virtual bool stop();
-	// various reset flavors
-	virtual void reset();
-	virtual void reset_counter_value() {reset();}
-	
+    // various reset flavors
+    virtual void reset();
+    virtual void reset_counter_value() {reset();}
+    
         // get the current value of this performance counter
         hpx::performance_counters::counter_value get_counter_value(bool reset=false);
 
@@ -231,22 +231,22 @@ namespace hpx { namespace performance_counters { namespace papi { namespace serv
 
         // stop low level counter
         bool stop_counter()
-	{
-	    if (status_ == PAPI_COUNTER_ACTIVE)
-	    {
-		if (!counters_->remove_event(this)) return false;
-	    }
-	    return true;
-	}
-	// reset counter
-	void reset_counter()
-	{
-	    // if active, clear the previous contents of low level counter
-	    if (status_ == PAPI_COUNTER_ACTIVE)
-		counters_->read_value(this, true);
+    {
+        if (status_ == PAPI_COUNTER_ACTIVE)
+        {
+        if (!counters_->remove_event(this)) return false;
+        }
+        return true;
+    }
+    // reset counter
+    void reset_counter()
+    {
+        // if active, clear the previous contents of low level counter
+        if (status_ == PAPI_COUNTER_ACTIVE)
+        counters_->read_value(this, true);
 
-	    value_ = 0;
-	}
+        value_ = 0;
+    }
     };
 
 }}}}
