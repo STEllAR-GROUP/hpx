299e8fcaaf0b344c348f6612f0db19d5b30594f9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-05-16 06:34:55 -0500

    Moving easybmp library

--- examples/quickstart/fractals.cpp
+++ examples/quickstart/fractals.cpp
@@ -9,15 +9,15 @@
 #include <hpx/include/actions.hpp>
 #include <hpx/include/util.hpp>
 #include <hpx/include/lcos.hpp>
+#include <hpx/include/iostream.hpp>
 
-#include <iostream>
 #include <vector>
 
 #include <boost/cstdint.hpp>
 #include <boost/format.hpp>
 
-#include <external/EasyBMP/EasyBMP.h>
-#include <external/EasyBMP/EasyBMP.cpp>
+#include <EasyBMP/EasyBMP.h>
+#include <EasyBMP/EasyBMP.cpp>
 
 const int sizeY = 256;
 const int sizeX = sizeY;
@@ -31,19 +31,17 @@ HPX_PLAIN_ACTION(fractals, fractals_action);
 ///////////////////////////////////////////////////////////////////////////////
 int fractals(float x0, float y0, int max_iteration)
 {
-    
-            float x = 0, y = 0;
-            int iteration = 0;
-              while ( x*x + y*y < 2*2  &&  iteration < max_iteration )
-              {
-                float xtemp = x*x - y*y + x0;
-                y = 2*x*y + y0;
-                    
-                x = xtemp;
+    float x = 0, y = 0;
+    int iteration = 0;
+    while (x*x + y*y < 2*2 && iteration < max_iteration)
+    {
+        float xtemp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtemp;
 
-                iteration++;
-              }
-    return iteration;   // wait for the Futures to return their values
+        ++iteration;
+    }
+    return iteration;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -53,58 +51,77 @@ int hpx_main()
     SetImage.SetBitDepth(24);
     SetImage.SetSize(sizeX * 2,sizeY);
     hpx::util::high_resolution_timer t;
+
     {
-        int max_iteration = 255;
         using hpx::lcos::future;
         using hpx::async;
         using namespace std;
         using hpx::wait_all;
-    vector<fractals_action> fAct;//[sizeX * sizeY];
-    vector<future<int>> iteration;
-    iteration.reserve(sizeX*sizeY);
-    std::cout << "Initial setup completed in " << t.elapsed() << "s. Initializing and running futures...\n";
-    t.restart();
-    for (int i = 0; i < sizeX; i++)
-        for (int j = 0; j < sizeY; j++)
+
+        int max_iteration = 255;
+
+        vector<fractals_action> fAct;   //[sizeX * sizeY];
+        vector<future<int>> iteration;
+
+        iteration.reserve(sizeX*sizeY);
+        hpx::cout << "Initial setup completed in " 
+                  << t.elapsed() 
+                  << "s. Initializing and running futures...\n";
+        t.restart();
+
+        for (int i = 0; i < sizeX; i++)
         {
-            hpx::naming::id_type const locality_id = hpx::find_here();
-            float x0 = (float)i * 3.5 / (float)sizeX - 2.5;
-            float y0 = (float)j * 2.0 / (float)sizeY - 1.0;
-            //int it = iteration.get();
-            fractals_action temp;
-            iteration.push_back(async(temp, locality_id, x0, y0, max_iteration));
-            fAct.push_back(temp);
-        }
-    wait_all(iteration);
-    //int total = 0;
-    std::cout << sizeX*sizeY << " calculations run in " << t.elapsed() << "s. Transferring from futures to general memory...\n";
-    t.restart();
-    for (int i = 0; i < sizeX; i++)
-        for (int j = 0; j < sizeY; j++)
-        {
-            int it = iteration[i*sizeX + j].get();
-            //total += it;
-            for (int k = 0; k < 2; k++)
+            for (int j = 0; j < sizeY; j++)
             {
-            RGBApixel pix;
-            pix.Blue = (it*255)/max_iteration;
-            pix.Red = (it*255)/max_iteration;
-            pix.Green = (it*255)/max_iteration;
-            SetImage.SetPixel(i * 2 + k,j,pix);
+                hpx::id_type const locality_id = hpx::find_here();
+
+                float x0 = (float)i * 3.5 / (float)sizeX - 2.5;
+                float y0 = (float)j * 2.0 / (float)sizeY - 1.0;
+
+                fractals_action temp;
+                iteration.push_back(async(temp, locality_id, x0, y0, max_iteration));
+                fAct.push_back(temp);
+            }
+        }
+        wait_all(iteration);
+
+        hpx::cout << sizeX*sizeY << " calculations run in " 
+                  << t.elapsed() 
+                  << "s. Transferring from futures to general memory...\n";
+        t.restart();
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                int it = iteration[i*sizeX + j].get();
+                //total += it;
+                for (int k = 0; k < 2; k++)
+                {
+                RGBApixel pix;
+                pix.Blue = (it*255)/max_iteration;
+                pix.Red = (it*255)/max_iteration;
+                pix.Green = (it*255)/max_iteration;
+                SetImage.SetPixel(i * 2 + k,j,pix);
+                }
             }
         }
     }
-    std::cout << "Transfer process completed in " << t.elapsed() << "s. Writing to hard disk...\n";
+
+    hpx::cout << "Transfer process completed in " 
+              << t.elapsed() << "s. Writing to hard disk...\n";
     t.restart();
+
     SetImage.WriteToFile("out.bmp");
     
-    std::cout << "Fractal image written to file \"out.bmp\" from memory in " << t.elapsed() << "s.\nInitializing shutdown process.\n";
+    hpx::cout << "Fractal image written to file \"out.bmp\" from memory in " 
+              << t.elapsed() << "s.\nInitializing shutdown process.\n";
 
     return hpx::finalize(); // Handles HPX shutdown
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    return hpx::init();
+    return hpx::init(argc, argv);
 }
 
