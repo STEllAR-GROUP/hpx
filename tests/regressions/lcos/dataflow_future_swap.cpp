f6477f15e82e4eaa297dc4af16f598327b27a148
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-05-29 19:34:06 -0500

    Adding test case for #774: using local dataflow without explicit namespace

--- tests/regressions/lcos/dataflow_future_swap.cpp
+++ tests/regressions/lcos/dataflow_future_swap.cpp
@@ -3,6 +3,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// This test case demonstrates the issue described in #775: runtime error with 
+// local dataflow (copying futures?).
+
 #include <iostream>
 
 #include <hpx/hpx.hpp>
