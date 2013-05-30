011761d178f6994c70769f5e5761c1814a1fba76
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-05-29 19:42:18 -0500

    Adding test case for #773: local dataflow with unwrap: functor operators need to be const

--- tests/regressions/lcos/dataflow_const_functor.cpp
+++ tests/regressions/lcos/dataflow_const_functor.cpp
@@ -3,31 +3,26 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// This test case demonstrates the issue described in #774: using local dataflow
-// without explicit namespace.
-
-#include <iostream>
+// This test case demonstrates the issue described in #773: local dataflow with 
+// unwrap: functor operators need to be const.
 
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_main.hpp>
 #include <hpx/lcos/local/dataflow.hpp>
 #include <hpx/util/unwrap.hpp>
 
-// the following line causes compile errors
-using hpx::lcos::local::dataflow;
-
 typedef hpx::lcos::future< double > future_type;
 
 template< typename Value >
-struct mul
+struct mul 
 {
     const Value a;
 
-    mul( const Value alpha )
+    mul( const Value alpha ) 
       : a( alpha )
     {}
 
-    double operator() ( double x1 , double x2 ) const // this has to be const?!
+    double operator() ( double x1 , double x2 ) //const // this has to be const?!
     {
         return x1*x2*a;
     }
@@ -37,12 +32,9 @@ int main()
 {
     auto functor = hpx::util::unwrap(mul<double>( 0.5 ));
     future_type f1 = hpx::make_ready_future( 1.0 );
+    future_type f2 = hpx::lcos::local::dataflow( functor , f1 , f1 );
 
-    // compile error even when using full namespace
-    future_type f2 = dataflow( functor , f1 , f1 );
-    future_type f3 = hpx::lcos::local::dataflow( hpx::util::unwrap(mul<double>( 2.0 )) , f1 , f1 );
-
-    hpx::wait_all(f1, f2, f3);
+    hpx::wait_all(f1, f2);
 
     return 0;
 }
