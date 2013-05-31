a0e70816c84e388d2c1fefcb7627a88f14aa1431
Thomas Heller <thom.heller@gmail.com>
2013-05-31 12:23:02 +0000

    Fixing #773: local dataflow with unwrap: functor operators need to be const

--- hpx/util/unwrap.hpp
+++ hpx/util/unwrap.hpp
@@ -83,23 +83,6 @@ namespace hpx { namespace util {
 
 #define     HPX_UTIL_UNWRAP_IMPL_OPERATOR(Z, N, D)                              \
             template <typename This, BOOST_PP_ENUM_PARAMS(N, typename A)>       \
-            struct result<This const(BOOST_PP_ENUM_PARAMS(N, A))>               \
-            {                                                                   \
-                typedef typename                                                \
-                    boost::result_of<                                           \
-                        F const(BOOST_PP_ENUM(N, HPX_UTIL_UNWRAP_IMPL_RESULT_OF, _))\
-                    >::type                                                     \
-                    type;                                                       \
-            };                                                                  \
-                                                                                \
-            template <BOOST_PP_ENUM_PARAMS(N, typename A)>                      \
-            typename result<unwrap_impl const(BOOST_PP_ENUM_PARAMS(N, A))>::type\
-            operator()(HPX_ENUM_FWD_ARGS(N, A, a)) const                        \
-            {                                                                   \
-                return f_(BOOST_PP_ENUM(N, HPX_UTIL_UNWRAP_IMPL_INVOKE, _));    \
-            }                                                                   \
-                                                                                \
-            template <typename This, BOOST_PP_ENUM_PARAMS(N, typename A)>       \
             struct result<This(BOOST_PP_ENUM_PARAMS(N, A))>                     \
             {                                                                   \
                 typedef typename                                                \
