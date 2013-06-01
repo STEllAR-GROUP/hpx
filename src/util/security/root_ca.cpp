30137ed83b5c53fed15a2d48d63f0544e5e8a6f5
Jeroen Habraken <jhabraken@cct.lsu.edu>
2013-06-01 17:17:44 +0000

    Fix HPX to build without HPX_HAVE_SECURITY

--- src/util/security/root_ca.cpp
+++ src/util/security/root_ca.cpp
@@ -1,7 +1,10 @@
 //  Copyright (c) 2013 Hartmut Kaiser
+//  Copyright (c) 2013 Jeroen Habraken
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if defined(HPX_HAVE_SECURITY)
 
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/util/security/root_ca.hpp>
@@ -76,3 +79,4 @@ namespace hpx { namespace util { namespace security
     }
 }}}
 
+#endif
--- src/util/security/sub_ca.cpp
+++ src/util/security/sub_ca.cpp
@@ -1,7 +1,10 @@
 //  Copyright (c) 2013 Hartmut Kaiser
+//  Copyright (c) 2013 Jeroen Habraken
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if defined(HPX_HAVE_SECURITY)
 
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/util/plugin.hpp>
@@ -93,3 +96,4 @@ namespace hpx { namespace util { namespace security
     }
 }}}
 
+#endif
