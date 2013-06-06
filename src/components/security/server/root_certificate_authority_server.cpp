d83668833a5947009114b3627d5ad7b49d640d7f
Jeroen Habraken <jhabraken@cct.lsu.edu>
2013-06-06 18:18:18 +0000

    Throw on invalid certificate signing request

--- src/components/security/server/root_certificate_authority_server.cpp
+++ src/components/security/server/root_certificate_authority_server.cpp
@@ -4,6 +4,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/components/security/server/root_certificate_authority.hpp>
+#include <hpx/exception.hpp>
 
 namespace hpx { namespace components { namespace security { namespace server
 {
@@ -42,17 +43,24 @@ namespace hpx { namespace components { namespace security { namespace server
     root_certificate_authority::sign_certificate_signing_request(
         signed_type<certificate_signing_request> const & signed_csr) const
     {
+        certificate_signing_request const & csr = signed_csr.get_type();
+        if (csr.get_subject_public_key().verify(signed_csr) == false)
+        {
+            HPX_THROW_EXCEPTION(
+                hpx::security_error
+              , "root_certificate_authority::sign_certificate_signing_request"
+              , boost::str(boost::format(
+                    "the certificate signing request signature is invalid: %1%") %
+                    signed_csr)
+            )
+        }
+
+        // TODO, capability checks
+
         signed_type<certificate> signed_certificate;
 
-        certificate_signing_request const & csr = signed_csr.get_type();
-
-        if (csr.get_subject_public_key().verify(signed_csr))
-        {
-            // TODO, capability checks
-
-            signed_certificate = key_pair_.sign(certificate(
-                get_gid().get_gid(), csr));
-        }
+        signed_certificate = key_pair_.sign(certificate(
+            get_gid().get_gid(), csr));
 
         return signed_certificate;
     }
--- src/components/security/server/subordinate_certificate_authority_server.cpp
+++ src/components/security/server/subordinate_certificate_authority_server.cpp
@@ -62,17 +62,24 @@ namespace hpx { namespace components { namespace security { namespace server
     subordinate_certificate_authority::sign_certificate_signing_request(
         signed_type<certificate_signing_request> const & signed_csr) const
     {
+        certificate_signing_request const & csr = signed_csr.get_type();
+        if (csr.get_subject_public_key().verify(signed_csr) == false)
+        {
+            HPX_THROW_EXCEPTION(
+                hpx::security_error
+              , "root_certificate_authority::sign_certificate_signing_request"
+              , boost::str(boost::format(
+                    "the certificate signing request signature is invalid: %1%") %
+                    signed_csr)
+            )
+        }
+
+        // TODO, capability checks
+
         signed_type<certificate> signed_certificate;
 
-        certificate_signing_request const & csr = signed_csr.get_type();
-
-        if (csr.get_subject_public_key().verify(signed_csr))
-        {
-            // TODO, capability checks
-
-            signed_certificate = key_pair_.sign(certificate(
-                get_gid().get_gid(), csr));
-        }
+        signed_certificate = key_pair_.sign(certificate(
+            get_gid().get_gid(), csr));
 
         return signed_certificate;
     }
