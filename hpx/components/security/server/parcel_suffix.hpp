d73fd3a34333a81b636fcc5def69880837f622ba
Jeroen Habraken <jhabraken@cct.lsu.edu>
2013-06-07 20:29:17 +0000

    Initial verify function

--- hpx/components/security/server/parcel_suffix.hpp
+++ hpx/components/security/server/parcel_suffix.hpp
@@ -7,6 +7,7 @@
 #define HPX_COMPONENTS_SECURITY_SERVER_PARCEL_SUFFIX_HPP
 
 #include <boost/cstdint.hpp>
+#include <boost/io/ios_state.hpp>
 #include <hpx/hpx_fwd.hpp>
 
 #include "hash.hpp"
@@ -23,6 +24,37 @@ namespace hpx { namespace components { namespace security { namespace server
         parcel_suffix(naming::gid_type parcel_id, hash & hash)
           : parcel_id_(parcel_id), hash_(hash.final())
         {
+        }
+
+        naming::gid_type const & get_parcel_id() const
+        {
+            return parcel_id_;
+        }
+
+        traits::hash<>::final_type const & get_hash() const
+        {
+            return hash_;
+        }
+
+        friend std::ostream & operator<<(std::ostream & os,
+                                         parcel_suffix const & parcel_suffix)
+        {
+            boost::io::ios_flags_saver ifs(os);
+
+            os << "<parcel_suffix "
+               << parcel_suffix.parcel_id_
+               << " \"";
+
+            for (std::size_t i = 0; i < traits::hash<>::final_type::static_size; ++i)
+            {
+                os << std::hex
+                   << std::nouppercase
+                   << std::setfill('0')
+                   << std::setw(2)
+                   << static_cast<unsigned int>(parcel_suffix.hash_[i]);
+            }
+
+            return os << "\">";
         }
 
     private:
--- tests/unit/security/certificate_authority.cpp
+++ tests/unit/security/certificate_authority.cpp
@@ -3,16 +3,22 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <boost/make_shared.hpp>
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/components/security/server/certificate_store.hpp>
+#include <hpx/components/security/server/hash.hpp>
+#include <hpx/components/security/server/parcel_suffix.hpp>
 #include <hpx/components/security/server/public_key.hpp>
 #include <hpx/components/security/server/signed_type.hpp>
+#include <hpx/components/security/server/verify.hpp>
+#include <hpx/runtime/parcelset/parcel.hpp>
 #include <hpx/util/lightweight_test.hpp>
 #include <hpx/util/security/root_certificate_authority.hpp>
 #include <hpx/util/security/subordinate_certificate_authority.hpp>
 
 #include <iostream>
+#include <vector>
 
 int hpx_main(boost::program_options::variables_map &)
 {
@@ -28,7 +34,7 @@ int hpx_main(boost::program_options::variables_map &)
 
         std::cout << root_certificate << std::endl;
 
-        server::certificate_store store(root_certificate);
+        server::certificate_store certificate_store(root_certificate);
 
         server::public_key const & root_public_key =
             root_certificate.get_type().get_subject_public_key();
@@ -48,19 +54,28 @@ int hpx_main(boost::program_options::variables_map &)
 
         std::cout << subordinate_certificate << std::endl;
 
-        store.insert(subordinate_certificate);
+        certificate_store.insert(subordinate_certificate);
 
         HPX_TEST(root_public_key.verify(subordinate_certificate));
 
 
-        /* server::hash hash;
-        hash.update(
-            reinterpret_cast<unsigned char const *>("Hello, world!"), 13);
+        server::hash hash;
+        hash.update(reinterpret_cast<unsigned char const *>("Hello, world!"), 13);
 
-        server::parcel parcel(0, hash);
+        server::parcel_suffix parcel_suffix(
+            hpx::parcelset::parcel::generate_unique_id(), hash);
 
-        server::signed_type<server::parcel> signed_parcel =
-            subordinate_key_pair.sign(parcel); */
+        server::signed_type<server::parcel_suffix> signed_parcel_suffix =
+            subordinate_certificate_authority.get_key_pair().sign(
+                parcel_suffix);
+
+        boost::shared_ptr<std::vector<char> > parcel_data =
+            boost::make_shared<std::vector<char> >(
+                reinterpret_cast<char const *>(&signed_parcel_suffix),
+                reinterpret_cast<char const *>(&signed_parcel_suffix) + sizeof signed_parcel_suffix);
+
+        HPX_TEST(server::verify(certificate_store,
+                                parcel_data));
     }
 
     return hpx::finalize();
