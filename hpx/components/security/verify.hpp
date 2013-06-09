6d32c8daad7acbde17351941a9b5b38a53d506bd
Jeroen Habraken <jhabraken@cct.lsu.edu>
2013-06-09 21:02:06 +0000

    Added at_locality to the certificate_store, working on verify

--- hpx/components/security/verify.hpp
+++ hpx/components/security/verify.hpp
@@ -12,6 +12,7 @@
 
 #include <vector>
 
+#include "certificate.hpp"
 #include "certificate_store.hpp"
 #include "parcel_suffix.hpp"
 #include "signed_type.hpp"
@@ -34,27 +35,37 @@ namespace hpx { namespace components { namespace security
             return false;
         }
 
-//        server::hash hash;
-//        hash.update(
-//            reinterpret_cast<unsigned char const*>(&parcel_data.front()),
-//            parcel_data.size() - sizeof(signed_parcel_suffix));
-//        hash.final();
-
         signed_type<parcel_suffix> const * parcel_suffix_ptr =
             reinterpret_cast<signed_type<parcel_suffix> const *>(
                 &(parcel_data.back()) - signed_type<parcel_suffix>::size() + 1);
 
         parcel_id = parcel_suffix_ptr->get_type().get_parcel_id();
 
-        hpx::naming::gid_type subject(
-            hpx::naming::replace_locality_id(
-                HPX_SUBORDINATE_CERTIFICATE_AUTHORITY_MSB
-              , get_locality_id_from_gid(parcel_id))
-          , HPX_SUBORDINATE_CERTIFICATE_AUTHORITY_LSB);
+        signed_type<certificate> subject_certificate =
+            certificate_store.at_locality(parcel_id, ec);
 
-        // std::cout << subject << "\n";
+        if (ec)
+            return false;
 
-        // TODO
+        if (subject_certificate.get_type().get_subject_public_key().verify(
+                *parcel_suffix_ptr) == false)
+        {
+            HPX_THROWS_IF(
+                ec
+              , hpx::security_error
+              , "verify"
+              , boost::str(boost::format(
+                    "The parcel_suffix signature is invalid: %1%") %
+                    *parcel_suffix_ptr)
+            );
+            return false;
+        }
+
+//        server::hash hash;
+//        hash.update(
+//            reinterpret_cast<unsigned char const*>(&parcel_data.front()),
+//            parcel_data.size() - sizeof(signed_parcel_suffix));
+//        hash.final();
 
         if (&ec != &throws)
             ec = make_success_code();
