719e7cb3d9bfa40b8cf5900ab4ff2fbfee290a2b
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-06-15 11:28:14 -0500

    Fixing inconsistencies

--- hpx/runtime/agas/addressing_service.hpp
+++ hpx/runtime/agas/addressing_service.hpp
@@ -309,7 +309,7 @@ struct HPX_EXPORT addressing_service : boost::noncopyable
 
     naming::gid_type const& get_local_locality(error_code& ec = throws) const
     {
-        if (!locality_) {
+        if (locality_ == naming::invalid_gid) {
             HPX_THROWS_IF(ec, invalid_status,
                 "addressing_service::get_local_locality",
                 "local locality has not been initialized (yet)");
@@ -344,6 +344,7 @@ struct HPX_EXPORT addressing_service : boost::noncopyable
     bool resolve_locally_known_addresses(
         naming::gid_type const& id
       , naming::address& addr
+      , error_code& ec
       );
 
     /// \brief Register performance counter types exposing properties from the
