08896a65a6d6024db99eca82d4c81224a97d9610
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-07-14 18:06:50 -0500

    Attempt to fix compilation error of one of the tests

--- hpx/util/coroutine/coroutine.hpp
+++ hpx/util/coroutine/coroutine.hpp
@@ -43,6 +43,7 @@
 
 #include <hpx/util/coroutine/detail/arg_max.hpp>
 #include <hpx/util/coroutine/detail/coroutine_impl.hpp>
+#include <hpx/util/coroutine/detail/coroutine_impl_impl.hpp>
 #include <hpx/util/coroutine/detail/is_callable.hpp>
 #include <hpx/util/coroutine/detail/argument_packer.hpp>
 #include <hpx/util/coroutine/detail/argument_unpacker.hpp>
