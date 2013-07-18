62ee0bf241f6aded8b699c95aa0fc63888329070
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-07-17 20:37:58 -0500

    Revert some of the recent changes to fix segfault in release mode. However, this will break executors.

--- hpx/util/coroutine/detail/coroutine_impl.hpp
+++ hpx/util/coroutine/detail/coroutine_impl.hpp
@@ -179,10 +179,10 @@ namespace hpx { namespace util { namespace coroutines { namespace detail
     static hpx::util::thread_specific_ptr<self_type*, tls_tag> self_;
 
   public:
-    static void set_self(self_type* self);
-    static self_type* get_self();
-    static void init_self();
-    static void reset_self();
+    HPX_COROUTINE_EXPORT static void set_self(self_type* self);
+    HPX_COROUTINE_EXPORT static self_type* get_self();
+    HPX_COROUTINE_EXPORT static void init_self();
+    HPX_COROUTINE_EXPORT static void reset_self();
 
 #if defined(HPX_GENERIC_COROUTINES)
   protected:
@@ -292,10 +292,8 @@ namespace hpx { namespace util { namespace coroutines { namespace detail
 
           typedef typename coroutine_type::self self_type;
           {
-              self_type* old_self = super_type::get_self();
-              self_type self(this, old_self);
-              reset_self_on_exit on_exit(&self, old_self);
-
+              self_type self(this);
+              reset_self_on_exit on_exit(&self);
               this->m_result_last = m_fun(*this->args());
 
               // if this thread returned 'terminated' we need to reset the functor
@@ -345,17 +343,14 @@ namespace hpx { namespace util { namespace coroutines { namespace detail
     {
         typedef typename coroutine_type::self self_type;
 
-        reset_self_on_exit(self_type* val, self_type* old_val = NULL)
-          : old_self(old_val)
+        reset_self_on_exit(self_type* val)
         {
             super_type::set_self(val);
         }
         ~reset_self_on_exit()
         {
-            super_type::set_self(old_self);
+            super_type::set_self(NULL);
         }
-
-        self_type* old_self;
     };
 
   public:
@@ -382,9 +377,8 @@ namespace hpx { namespace util { namespace coroutines { namespace detail
       typedef typename coroutine_type::result_slot_type result_slot_type;
 
       {
-          self_type* old_self = super_type::get_self();
-          self_type self(this, old_self);
-          reset_self_on_exit on_exit(&self, old_self);
+          self_type self(this);
+          reset_self_on_exit on_exit(&self);
           detail::unpack(m_fun, *this->args(),
               detail::trait_tag<typename coroutine_type::arg_slot_traits>());
       }
@@ -405,10 +399,8 @@ namespace hpx { namespace util { namespace coroutines { namespace detail
       typedef typename coroutine_type::result_slot_type result_slot_type;
 
       {
-          self_type* old_self = super_type::get_self();
-          self_type self(this, old_self);
-          reset_self_on_exit on_exit(&self, old_self);
-
+          self_type self(this);
+          reset_self_on_exit on_exit(&self);
           this->m_result_last = boost::in_place(result_slot_type(
                   detail::unpack(m_fun, *this->args(), detail::trait_tag<traits>())
               ));
@@ -574,8 +566,6 @@ namespace hpx { namespace util { namespace coroutines { namespace detail
       p->reset();
   }
 }}}}
-
-#include <hpx/util/coroutine/detail/coroutine_impl_impl.hpp>
 
 #if defined(_MSC_VER)
 #pragma warning(pop)
