dbaf3c1c90ebe4b62fc7c8582ac050d48741451e
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-08-17 13:33:28 -0500

    Fixing Windows compilation problem

--- tests/performance/sizeof.cpp
+++ tests/performance/sizeof.cpp
@@ -3,11 +3,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/format.hpp>
-#include <boost/preprocessor/stringize.hpp>
-
 #include <hpx/hpx_init.hpp>
 #include <hpx/include/iostreams.hpp>
+
+#include <boost/format.hpp>
+#include <boost/preprocessor/stringize.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
