af13b4505924fe5f29696d5ae4b20bf3fa270a4d
K-ballo <k@fusionfenix.com>
2013-08-29 15:07:51 -0500

    Fallback to std::is_bind_expression
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- hpx/traits/is_bind_expression.hpp
+++ hpx/traits/is_bind_expression.hpp
@@ -6,13 +6,23 @@
 #if !defined(HPX_TRAITS_IS_BIND_EXPRESSION_AUG_28_2013_0603PM)
 #define HPX_TRAITS_IS_BIND_EXPRESSION_AUG_28_2013_0603PM
 
+#include <boost/config.hpp>
+
 #include <boost/mpl/bool.hpp>
+
+#ifndef BOOST_NO_CXX11_HDR_FUNCTIONAL
+#   include <functional>
+#endif
 
 namespace hpx { namespace traits
 {
     template <typename T>
     struct is_bind_expression
+#   ifndef BOOST_NO_CXX11_HDR_FUNCTIONAL
+      : std::is_bind_expression<T>
+#   else
       : boost::mpl::false_
+#   endif
     {};
 }}
 
