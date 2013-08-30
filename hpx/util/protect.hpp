311a78a7d7e67eed623fb8a542fdeb6c05430a3e
K-ballo <k@fusionfenix.com>
2013-08-29 20:42:32 -0500

    Simpler protected_bind
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- hpx/util/protect.hpp
+++ hpx/util/protect.hpp
@@ -1,6 +1,7 @@
 //  Copyright (c) 2002 Peter Dimov and Multi Media Ltd.
 //  Copyright (c) 2009 Steven Watanabe
 //  Copyright (c) 2011-2013 Hartmut Kaiser
+//  Copyright (c) 2013 Agustin Berge
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
@@ -11,248 +12,69 @@
 
 #include <boost/config.hpp>
 #include <hpx/traits/is_bind_expression.hpp>
-#include <hpx/traits/is_callable.hpp>
 #include <hpx/util/move.hpp>
-#include <hpx/util/detail/remove_reference.hpp>
+#include <hpx/util/decay.hpp>
 
-#include <boost/mpl/identity.hpp>
 #include <boost/utility/enable_if.hpp>
 #include <boost/utility/result_of.hpp>
 
 namespace hpx { namespace util { namespace detail
 {
-    // Handle the nullary operator()() separately
-    template <typename F, typename Enable = void>
-    class nullary_protected_bind
-    {
-    public:
-        nullary_protected_bind(nullary_protected_bind const& other)
-          : f_(other.f_)
-        {
-        }
-
-        // move constructor
-        nullary_protected_bind(BOOST_RV_REF(nullary_protected_bind) other)
-          : f_(boost::move(other.f_))
-        {
-        }
-
-        explicit nullary_protected_bind(F const & f)
-          : f_(f)
-        {}
-
-        explicit nullary_protected_bind(BOOST_RV_REF(F) f)
-          : f_(boost::move(f))
-        {}
-
-        nullary_protected_bind& operator=(BOOST_COPY_ASSIGN_REF(nullary_protected_bind) rhs)
-        {
-            if (this != &rhs)
-                f_ = rhs.f_;
-            return *this;
-        }
-
-        nullary_protected_bind& operator=(BOOST_RV_REF(nullary_protected_bind) rhs)
-        {
-            if (this != &rhs)
-               f_ = boost::move(rhs.f_);
-            return *this;
-        }
-        
-        template <typename>
-        struct result
-        {};
-
-        BOOST_FORCEINLINE
-        void operator()() const
-        {
-            f_();
-        }
-
-    protected:
-        F f_;
-
-    private:
-        BOOST_COPYABLE_AND_MOVABLE(nullary_protected_bind)
-    };
-
-    template <typename F>
-    class nullary_protected_bind<
-        F, typename boost::enable_if<traits::is_callable<F()>>::type>
-    {
-    public:
-        nullary_protected_bind(nullary_protected_bind const& other)
-          : f_(other.f_)
-        {
-        }
-
-        // move constructor
-        nullary_protected_bind(BOOST_RV_REF(nullary_protected_bind) other)
-          : f_(boost::move(other.f_))
-        {
-        }
-
-        explicit nullary_protected_bind(F const & f)
-          : f_(f)
-        {}
-
-        explicit nullary_protected_bind(BOOST_RV_REF(F) f)
-          : f_(boost::move(f))
-        {}
-
-        nullary_protected_bind& operator=(BOOST_COPY_ASSIGN_REF(nullary_protected_bind) rhs)
-        {
-            if (this != &rhs)
-                f_ = rhs.f_;
-            return *this;
-        }
-
-        nullary_protected_bind& operator=(BOOST_RV_REF(nullary_protected_bind) rhs)
-        {
-            if (this != &rhs)
-               f_ = boost::move(rhs.f_);
-            return *this;
-        }
-
-        template <typename>
-        struct result;
-
-        template <typename This>
-        struct result<This()>
-        {
-            typedef typename boost::result_of<F()>::type type;
-        };
-
-        BOOST_FORCEINLINE
-        typename boost::result_of<F()>::type operator()()
-        {
-            return f_();
-        }
-
-        BOOST_FORCEINLINE
-        typename boost::result_of<F const()>::type operator()() const
-        {
-            return f_();
-        }
-
-    protected:
-        F f_;
-
-    private:
-        BOOST_COPYABLE_AND_MOVABLE(nullary_protected_bind)
-    };
-
     ///////////////////////////////////////////////////////////////////////////
     template <typename F>
-    class protected_bind : public nullary_protected_bind<F>
+    class protected_bind : public F
     {
     public:
         // copy constructor
         protected_bind(protected_bind const& other)
-          : nullary_protected_bind<F>(other)
-        {
-        }
+          : F(other)
+        {}
 
         // move constructor
         protected_bind(BOOST_RV_REF(protected_bind) other)
-          : nullary_protected_bind<F>(boost::move(other))
-        {
-        }
+          : F(boost::move(other))
+        {}
 
         explicit protected_bind(F const & f)
-          : nullary_protected_bind<F>(f)
+          : F(f)
         {}
 
         explicit protected_bind(BOOST_RV_REF(F) f)
-          : nullary_protected_bind<F>(boost::move(f))
+          : F(boost::move(f))
         {}
 
         protected_bind& operator=(BOOST_COPY_ASSIGN_REF(protected_bind) rhs)
         {
-            nullary_protected_bind<F>::operator=(rhs);
+            F::operator=(rhs);
             return *this;
         }
 
         protected_bind& operator=(BOOST_RV_REF(protected_bind) rhs)
         {
-            nullary_protected_bind<F>::operator=(boost::move(rhs));
+            F::operator=(boost::move(rhs));
             return *this;
         }
-
-        template <typename S>
-        struct result
-#       if defined(BOOST_MSVC)
-          : nullary_protected_bind<F>::result<S>
-#       else
-          : nullary_protected_bind<F>::template result<S>
-#       endif
-        {};
-
-        using nullary_protected_bind<F>::operator();
-
-#define HPX_UTIL_PROTECT_OPERATOR(Z, N, D)                                      \
-        template <typename This, BOOST_PP_ENUM_PARAMS(N, typename A)>           \
-        struct result<This(BOOST_PP_ENUM_PARAMS(N, A))>                         \
-        {                                                                       \
-            typedef                                                             \
-                typename boost::result_of<F(BOOST_PP_ENUM_PARAMS(N, A))>::type  \
-                type;                                                           \
-        };                                                                      \
-                                                                                \
-        template <BOOST_PP_ENUM_PARAMS(N, typename A)>                          \
-        BOOST_FORCEINLINE                                                       \
-        typename boost::result_of<F(BOOST_PP_ENUM_PARAMS(N, A))>::type          \
-        operator()(HPX_ENUM_FWD_ARGS(N, A, a))                                  \
-        {                                                                       \
-            return this->f_(HPX_ENUM_FORWARD_ARGS(N, A, a));                    \
-        }                                                                       \
-                                                                                \
-        template <typename This, BOOST_PP_ENUM_PARAMS(N, typename A)>           \
-        struct result<This const (BOOST_PP_ENUM_PARAMS(N, A))>                  \
-        {                                                                       \
-            typedef                                                             \
-                typename boost::result_of<                                      \
-                    F const (BOOST_PP_ENUM_PARAMS(N, A))                        \
-                >::type                                                         \
-                type;                                                           \
-        };                                                                      \
-                                                                                \
-        template <BOOST_PP_ENUM_PARAMS(N, typename A)>                          \
-        BOOST_FORCEINLINE                                                       \
-        typename boost::result_of<F const (BOOST_PP_ENUM_PARAMS(N, A))>::type   \
-        operator()(HPX_ENUM_FWD_ARGS(N, A, a)) const                            \
-        {                                                                       \
-            return this->f_(HPX_ENUM_FORWARD_ARGS(N, A, a));                    \
-        }                                                                       \
-    /**/
-
-        BOOST_PP_REPEAT_FROM_TO(1, HPX_FUNCTION_ARGUMENT_LIMIT,
-                HPX_UTIL_PROTECT_OPERATOR, _)
-
-    private:
-        BOOST_COPYABLE_AND_MOVABLE(protected_bind)
     };
 }}} // namespace hpx::util::detail
 
 namespace hpx { namespace util
 {
-    template <typename F>
+    template <typename T>
     typename boost::enable_if<
-        traits::is_bind_expression<typename detail::remove_reference<F>::type>
-      , detail::protected_bind<typename detail::remove_reference<F>::type>
+        traits::is_bind_expression<typename util::decay<T>::type>
+      , detail::protected_bind<typename util::decay<T>::type>
     >::type
-    protect(BOOST_FWD_REF(F) f)
+    protect(BOOST_FWD_REF(T) f)
     {
         return detail::protected_bind<
-            typename detail::remove_reference<F>::type
-        >(boost::forward<F>(f));
+            typename util::decay<T>::type
+        >(boost::forward<T>(f));
     }
     
-    // handle everything that is not a class separately as those
-    // don't have any typename T::result_type defined
+    // leave everything that is not a bind expression as is
     template <typename T>
     typename boost::disable_if<
-        traits::is_bind_expression<typename detail::remove_reference<T>::type>
+        traits::is_bind_expression<typename util::decay<T>::type>
       , BOOST_FWD_REF(T)
     >::type
     protect(BOOST_FWD_REF(T) v)
