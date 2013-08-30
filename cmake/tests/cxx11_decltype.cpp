410a37448b06316e291034f915dc2601a9aaa75a
K-ballo <k@fusionfenix.com>
2013-08-30 11:12:19 -0500

    Refactored decltype test to not use lambdas/trailing-return-types
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- cmake/tests/cxx11_decltype.cpp
+++ cmake/tests/cxx11_decltype.cpp
@@ -1,12 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
 //  Copyright (c) 2012 Thomas Heller
+//  Copyright (c) 2013 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
 int foo()
-{}
+{
+    return 0;
+}
+
+decltype((42)) foo2()
+{
+    return 42;
+}
 
 template <typename F>
 void bar(F f)
@@ -17,5 +25,5 @@ void bar(F f)
 int main()
 {
     bar(&foo);
-    bar([]() -> decltype(42) {return 42;});
+    bar(&foo2);
 }
