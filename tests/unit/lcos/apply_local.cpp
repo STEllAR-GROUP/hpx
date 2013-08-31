be56114ced566f7a22e1a6478f4c2879dcc62b13
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-08-31 10:06:55 -0500

    Fixing test for gcc 4.4

--- tests/unit/lcos/apply_local.cpp
+++ tests/unit/lcos/apply_local.cpp
@@ -47,7 +47,7 @@ struct increment_type
     }
 };
 
-#ifndef BOOST_NO_CXX11_LAMBDAS
+#if !defined(BOOST_NO_CXX11_LAMBDAS) || !defined( BOOST_NO_CXX11_AUTO_DECLARATIONS)
 auto increment_lambda = [](boost::int32_t i){ accumulator += i; };
 #endif /*BOOST_NO_CXX11_LAMBDAS*/
 
@@ -92,20 +92,20 @@ int hpx_main()
         hpx::apply(hpx::util::bind(obj, 1));
         hpx::apply(hpx::util::bind(obj, _1), 1);
     }
-    
-#   ifndef BOOST_NO_CXX11_LAMBDAS
+
+#   if !defined(BOOST_NO_CXX11_LAMBDAS) || !defined( BOOST_NO_CXX11_AUTO_DECLARATIONS)
     {
         using hpx::util::placeholders::_1;
         using hpx::util::placeholders::_2;
-        
+
 // Lambdas are not assignable...
 //
-//        hpx::apply(increment_lambda, 1);      
+//        hpx::apply(increment_lambda, 1);
 //
 //        hpx::apply(hpx::util::bind(increment_lambda, 1));
 //        hpx::apply(hpx::util::bind(increment_lambda, _1), 1);
     }
-#   endif /*BOOST_NO_CXX11_LAMBDAS*/
+#   endif /*!defined(BOOST_NO_CXX11_LAMBDAS) || !defined( BOOST_NO_CXX11_AUTO_DECLARATIONS)*/
 
     return hpx::finalize();
 }
@@ -117,7 +117,7 @@ int main(int argc, char* argv[])
     // Initialize and run HPX
     HPX_TEST_EQ_MSG(hpx::init(argc, argv), 0,
         "HPX main exited with non-zero status");
-    
+
 #   ifndef BOOST_NO_CXX11_LAMBDAS
     HPX_TEST_EQ(accumulator.load(), 12);
 #   else /*BOOST_NO_CXX11_LAMBDAS*/
