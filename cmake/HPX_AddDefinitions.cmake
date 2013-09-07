00a7b6d994ca3fa6117eaf45c6db1d9dbbedb447
Thomas Heller <thom.heller@gmail.com>
2013-09-07 15:09:24 -0500

    Next try at fixing preprocessor defines

--- cmake/HPX_AddDefinitions.cmake
+++ cmake/HPX_AddDefinitions.cmake
@@ -10,15 +10,8 @@ include(HPX_Include)
 hpx_include(Message
             ParseArguments)
 
-set(HPX_DEFINITIONS CACHE INTERNAL "" FORCE)
-
 macro(hpx_add_definitions definition)
-  hpx_parse_arguments(ADD "" "NOPREPROCESS" ${ARGN})
-
   hpx_debug("add_definitions" "${definition}")
-  if(NOT ADD_NOPREPROCESS) 
-    set(HPX_PREPROCESS_DEFINITIONS ${HPX_PREPROCESS_DEFINITIONS} ${definition})
-  endif()
   set(HPX_DEFINITIONS ${HPX_DEFINITIONS} ${definition})
   add_definitions(${definition})
 endmacro()
