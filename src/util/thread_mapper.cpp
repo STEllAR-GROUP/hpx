1db61e18760b4540b10f598ef9811bde12563586
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-09-16 08:45:57 -0500

    Fix compilation warning using MSVC

--- src/util/thread_mapper.cpp
+++ src/util/thread_mapper.cpp
@@ -2,11 +2,12 @@
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#include <cstring>
 
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/util/thread_mapper.hpp>
 #include <hpx/exception.hpp>
+
+#include <cstring>
 
 #include <boost/format.hpp>
 
