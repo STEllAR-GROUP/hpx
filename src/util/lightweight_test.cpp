9d976b4461cac738498c850504b082ccbe8fbff8
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-09-25 10:01:32 -0500

    Fixed #875: global_fixture is defined in a header file

---
+++ src/util/lightweight_test.cpp
@@ -0,0 +1,13 @@
+//  Copyright (c) 2013 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+namespace hpx { namespace util { namespace detail
+{
+    fixture global_fixture = fixture(std::cerr);
+}}}
+
