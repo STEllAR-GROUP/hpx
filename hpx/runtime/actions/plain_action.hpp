8dbec2df99c86bff84cd3889fcbce3112380298d
K-ballo <k@fusionfenix.com>
2013-09-30 12:03:33 -0500

    Added missing include

--- hpx/runtime/actions/plain_action.hpp
+++ hpx/runtime/actions/plain_action.hpp
@@ -20,6 +20,7 @@
 #include <hpx/runtime/actions/continuation.hpp>
 #include <hpx/runtime/actions/action_support.hpp>
 #include <hpx/runtime/components/console_error_sink.hpp>
+#include <hpx/runtime/components/plain_component_factory.hpp>
 #include <hpx/runtime/components/server/plain_function.hpp>
 #include <hpx/util/unused.hpp>
 #include <hpx/util/void_cast.hpp>
