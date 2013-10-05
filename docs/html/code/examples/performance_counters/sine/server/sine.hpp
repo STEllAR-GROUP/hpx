beaf3ec78333a816f65304a8e590986c97616a61
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-10-05 12:43:15 -0500

    Manually adding sourcefiles

---
+++ docs/html/code/examples/performance_counters/sine/server/sine.hpp
@@ -0,0 +1,52 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(PERFORMANCE_COUNTERS_SINE_SEP_20_2011_0112PM)
+#define PERFORMANCE_COUNTERS_SINE_SEP_20_2011_0112PM
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/util/interval_timer.hpp>
+#include <hpx/lcos/local/spinlock.hpp>
+#include <hpx/performance_counters/base_performance_counter.hpp>
+
+namespace performance_counters { namespace sine { namespace server
+{
+    ///////////////////////////////////////////////////////////////////////////
+    //[sine_counter_definition
+    class sine_counter
+      : public hpx::performance_counters::base_performance_counter<sine_counter>
+    //]
+    {
+    public:
+        sine_counter() : current_value_(0) {}
+        sine_counter(hpx::performance_counters::counter_info const& info);
+
+        /// This function will be called in order to query the current value of
+        /// this performance counter
+        hpx::performance_counters::counter_value get_counter_value(bool reset);
+
+        /// The functions below will be called to start and stop collecting
+        /// counter values from this counter.
+        bool start();
+        bool stop();
+
+        /// finalize() will be called just before the instance gets destructed
+        void finalize();
+
+    protected:
+        bool evaluate();
+
+    private:
+        typedef hpx::lcos::local::spinlock mutex_type;
+
+        mutable mutex_type mtx_;
+        double current_value_;
+        boost::uint64_t evaluated_at_;
+
+        hpx::util::interval_timer timer_;
+    };
+}}}
+
+#endif
---
+++ docs/html/code/hpx/performance_counters/base_performance_counter.hpp
@@ -0,0 +1,60 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_PERFORMANCE_COUNTERS_BASE_PERFORMANCE_COUNTER_JAN_18_2013_1036AM)
+#define HPX_PERFORMANCE_COUNTERS_BASE_PERFORMANCE_COUNTER_JAN_18_2013_1036AM
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/components/component_type.hpp>
+#include <hpx/runtime/components/server/managed_component_base.hpp>
+#include <hpx/runtime/actions/component_action.hpp>
+#include <hpx/performance_counters/counters.hpp>
+#include <hpx/performance_counters/server/base_performance_counter.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+//[performance_counter_base_class
+namespace hpx { namespace performance_counters
+{
+    template <typename Derived>
+    class base_performance_counter;
+}}
+//]
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace performance_counters
+{
+    template <typename Derived>
+    class base_performance_counter
+      : public hpx::performance_counters::server::base_performance_counter,
+        public hpx::components::managed_component_base<Derived>
+    {
+    private:
+        typedef hpx::components::managed_component_base<Derived> base_type;
+
+    public:
+        typedef Derived type_holder;
+        typedef hpx::performance_counters::server::base_performance_counter
+            base_type_holder;
+
+        base_performance_counter()
+        {}
+
+        base_performance_counter(hpx::performance_counters::counter_info const& info)
+          : base_type_holder(info)
+        {}
+
+        // Disambiguate finalize() which is implemented in both base classes
+        void finalize()
+        {
+            base_type_holder::finalize();
+            base_type::finalize();
+        }
+
+        using base_type::get_component_type;
+        using base_type::set_component_type;
+    };
+}}
+
+#endif
