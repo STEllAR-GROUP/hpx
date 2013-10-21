d03d96cece39b80b3df48238b1ab87defcec8e24
K-ballo <k@fusionfenix.com>
2013-10-21 17:53:11 -0500

    Fixed rvalue-refs members in tuples for GCC4.4

--- hpx/util/tuple.hpp
+++ hpx/util/tuple.hpp
@@ -131,18 +131,15 @@ namespace hpx { namespace util
               : _value(other._value)
             {}
         };
-
+        
+#       if !defined(HPX_GCC_VERSION) || HPX_GCC_VERSION >= 40600
         template <typename T>
         struct tuple_member<BOOST_RV_REF(T)>
         {
             BOOST_COPYABLE_AND_MOVABLE(tuple_member);
 
         public: // exposition-only
-#           if !defined(HPX_GCC_VERSION) || HPX_GCC_VERSION >= 40600
             BOOST_RV_REF(T) _value;
-#           else
-            T _value;
-#           endif
 
         public:
             // 20.4.2.1, tuple construction
@@ -154,6 +151,26 @@ namespace hpx { namespace util
               : _value(boost::forward<T>(other._value))
             {}
         };
+#       else
+        template <typename T>
+        struct tuple_member<BOOST_RV_REF(T)>
+        {
+            BOOST_COPYABLE_AND_MOVABLE(tuple_member);
+
+        public: // exposition-only
+            T _value;
+
+        public:
+            // 20.4.2.1, tuple construction
+            BOOST_CONSTEXPR explicit tuple_member(BOOST_RV_REF(T) value)
+              : _value(boost::forward<T>(value))
+            {}
+
+            BOOST_CONSTEXPR tuple_member(tuple_member const& other)
+              : _value(other._value)
+            {}
+        };
+#       endif
 
         ///////////////////////////////////////////////////////////////////////
         template <typename TTuple, typename UTuple, typename Enable = void>
