f2530faa5c6b7e9372303ae16a89563a69824d29
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-11-07 07:20:22 -0600

    Adding copyrights/licenses

--- tests/regressions/lcos/after_588.cpp
+++ tests/regressions/lcos/after_588.cpp
@@ -1,3 +1,7 @@
+//  Copyright (c) 2013 Thomas Heller
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/hpx.hpp>
 #include <hpx/runtime/components/component_factory.hpp>
