f40c5c962850f7033a2f20d296a898f2280ca22f
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-11-08 14:28:54 -0600

    Fixed #1002: If MPI parcelport is not available, running HPX under mpirun should fail

--- hpx/util/mpi_environment.hpp
+++ hpx/util/mpi_environment.hpp
@@ -12,12 +12,14 @@
 #include <hpx/hpx_fwd.hpp>
 #include <cstdlib>
 
-namespace hpx { namespace util {
+namespace hpx { namespace util
+{
     struct command_line_handling;
 
     struct HPX_EXPORT mpi_environment
     {
-        static int init(int *argc, char ***argv, command_line_handling& cfg);
+        static std::size_t init(int *argc, char ***argv, command_line_handling& cfg,
+            std::size_t node);
         static void finalize();
 
         static bool enabled();
@@ -31,6 +33,19 @@ namespace hpx { namespace util {
     private:
         static bool enabled_;
         static int provided_threading_flag_;
+    };
+}}
+
+#else
+
+namespace hpx { namespace util
+{
+    struct command_line_handling;
+
+    struct HPX_EXPORT mpi_environment
+    {
+        static std::size_t init(int *argc, char ***argv, command_line_handling& cfg,
+            std::size_t node);
     };
 }}
 
