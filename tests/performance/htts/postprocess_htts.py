dbf40d76e9ef7e696eefbfab4b75e3e1e3c84baf
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2013-12-21 09:11:18 -0600

    Add parameter sweep and postprocessing scripts

---
+++ tests/performance/htts/postprocess_htts.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python
+
+from math import sqrt
+from sys import exit
+
+from optparse import OptionParser
+
+from numpy import std, mean
+
+# 0: OS-threads - Independent Variable
+# 1: Tasks - Independent Variable
+# 2: Delay [micro-seconds] - Independent Variable
+# 3: Total Walltime [seconds]
+# 4+: Counters
+
+OS_THREADS = 0
+TASKS = 1
+DELAY = 2
+
+LAST_IVAR = DELAY
+
+# Returns the index of the independent variable that we use to differentiate
+# datasets (dataset == line on the graph).
+def dataset_key(row):
+    return int(row[DELAY])
+
+# Returns a list of all the independent variables. 
+def ivars(row):
+    return tuple(int(row[x]) for x in (DELAY, TASKS, OS_THREADS))
+
+# Returns a list of all the dependent variables. 
+def dvars(row):
+    return row[(LAST_IVAR + 1):]
+
+op = OptionParser(usage="%prog [file]")
+args = op.parse_args()[1]
+
+if len(args) != 1:
+    op.print_help()
+    exit(1)
+
+f = open(args[0], 'r')
+
+master = {}
+legend = []
+
+try:
+    while True:
+        line = f.next()
+
+        # Look for legend 
+        if line[0] == '#':
+            if line[1] == '#':
+                row = line.split(':')
+                legend.append(row[1].strip())
+            continue   
+
+        # Look for blank lines
+        if line == "\n":
+            continue
+
+        row = line.split()
+
+        if not dataset_key(row) in master:
+            master[dataset_key(row)] = {}
+
+        if not ivars(row) in master[dataset_key(row)]:
+            master[dataset_key(row)][ivars(row)] = []
+
+        master[dataset_key(row)][ivars(row)].append(dvars(row))  
+
+except StopIteration:
+    pass
+
+sample_size = None
+number_of_dvars = None
+
+for (key, dataset) in sorted(master.iteritems()):
+    for (ivs, dvs) in sorted(dataset.iteritems()):
+        if sample_size is None:
+            sample_size = len(dvs)
+        else:
+            assert sample_size is len(dvs) 
+
+        for dv in dvs:
+            if number_of_dvars is None:
+                number_of_dvars = len(dv)
+            else:
+                assert number_of_dvars is len(dv)
+
+for i in range(0, LAST_IVAR + 1):
+    print '## %i: %s' % (i, legend[i]) 
+for i in range(0, (len(legend) - (LAST_IVAR + 1)) * 2, 2):
+    i0 = (LAST_IVAR + 1) + i
+    i1 = (LAST_IVAR + 1) + (i / 2)
+    print '## %i: %s - Average of %i Samples' % (i0, legend[i1], sample_size)
+    print '## %i: %s - Standard Deviation' % (i0 + 1, legend[i1])
+
+for (key, dataset) in sorted(master.iteritems()):
+    # iv is a list, dvs is a list of lists.
+    for (iv, dvs) in sorted(dataset.iteritems()):
+        for e in iv:
+            print e,
+
+        for i in range(0, number_of_dvars):
+            values = []
+            for j in range(0, sample_size):
+                values.append(float(dvs[j][i]))
+
+            print mean(values), std(values),
+
+        print
+ 
+    print
+    print
+
---
+++ tests/performance/htts/run_htts.sh
@@ -0,0 +1,104 @@
+#! /bin/bash
+
+# Independent variables are:
+#
+# * Cores
+# * Tasks
+# * Delay
+#
+# Usually we put cores on the X-axis, tasks is fixed, and we split the different
+# delays into different data sets. 
+
+usage()
+{
+    echo "Usage: $0 -b /path/to/benchmark -t max_cores -T 'x y ...' -d 'x y ...' [-s samples] [-f /path/to/options/file]"
+    echo
+    echo "This script runs a parameter sweep for the HTTS benchmark" 
+    echo
+    echo "Options:"
+    echo "  -b    Path to the benchmark."
+    echo "  -t    Maximum number of cores to use." 
+    echo "  -T    Space-separated list of task counts to use."
+    echo "  -d    Space-separated list of delays to use."
+    echo "  -s    Number of samples to collect for each combination of parameters."
+    echo "  -f    Path to a file containing options to pass to the benchmark."
+}
+
+BENCHMARK=""
+
+MAX_CORES=""
+
+DELAY=""
+
+TASKS=""
+
+SAMPLES=1
+
+OPTIONS_FILE=""
+
+###############################################################################
+# Argument parsing
+while getopts "b:t:T:d:s:f:h" OPTION; do case $OPTION in
+    h)
+        usage
+        exit 0
+        ;;
+    b)
+        BENCHMARK=$OPTARG
+        ;;
+    t)
+        MAX_CORES=$OPTARG
+        ;;
+    T)
+        TASKS="$OPTARG"
+        ;;
+    d)
+        DELAY="$OPTARG"
+        ;;
+    s)
+        SAMPLES=$OPTARG
+        ;;
+    f)
+        OPTIONS_FILE=$OPTARG
+        ;;
+    ?)
+        usage
+        exit 1
+        ;;
+esac; done
+
+if [ "$BENCHMARK" == "" ] || \
+   [ "$MAX_CORES" == "" ] || \
+   [ "$DELAY"     == "" ] || \
+   [ "$TASKS"     == "" ] 
+then
+    usage
+    exit 1
+fi
+
+J=0
+for delay in $DELAY; do
+    for tasks in $TASKS; do
+        for cores in $(bash -c "echo {1..$MAX_CORES}"); do
+            for samples in $(bash -c "echo {1..$SAMPLES}"); do
+                header="--no-header"
+                if [ $J == 0 ]; then
+                    header=""
+                fi
+    
+                options=""
+                if [ "$OPTIONS_FILE" ]; then
+                    options=$(cat $OPTIONS_FILE)
+                fi
+    
+                $BENCHMARK $header --tasks=$tasks --delay=$delay -t$cores $options 
+    
+                J=$((${J}+1))
+            done
+        done
+    done
+
+    echo; echo
+done
+ 
+
