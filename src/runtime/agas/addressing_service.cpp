b8e685da467a5df5009f5339b4bfc9707e084c13
K-ballo <k@fusionfenix.com>
2014-01-03 23:56:52 -0600

    Workaround MSVC issue

--- src/runtime/agas/addressing_service.cpp
+++ src/runtime/agas/addressing_service.cpp
@@ -1910,7 +1910,7 @@ lcos::unique_future<bool> addressing_service::register_name_async(
         );
     }
 
-    return f;
+    return boost::move(f);
 } // }}}
 
 ///////////////////////////////////////////////////////////////////////////////
