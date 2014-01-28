9c7078417a7bb6ad2f2f9ebf5d2c3dbe385b40a8
Thomas Heller <thomas.heller@cs.fau.de>
2014-01-28 08:46:22 +0100

    Adding missing copyright

--- hpx/runtime/parcelset/policies/ipc/sender.hpp
+++ hpx/runtime/parcelset/policies/ipc/sender.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//  Copyright (c)      2014 Thomas Heller
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef HPX_PARCELSET_POLICIES_IPC_SENDER_HPP
 #define HPX_PARCELSET_POLICIES_IPC_SENDER_HPP
