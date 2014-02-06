ae01e53c63baf3005a1614b42840c21a21e98e54
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2014-02-06 17:43:13 -0600

    Add updated delay_baseline overhead - now using the timed worker.

--- tests/performance/delay_baseline.cpp
+++ tests/performance/delay_baseline.cpp
@@ -3,7 +3,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include "worker.hpp"
+#include "worker_timed.hpp"
 
 #include <hpx/util/high_resolution_timer.hpp>
 
@@ -13,11 +13,6 @@
 #include <boost/cstdint.hpp>
 #include <boost/format.hpp>
 #include <boost/program_options.hpp>
-#include <boost/accumulators/accumulators.hpp>
-#include <boost/accumulators/statistics/stats.hpp>
-#include <boost/accumulators/statistics/median.hpp>
-#include <boost/accumulators/statistics/mean.hpp>
-#include <boost/accumulators/statistics/sum.hpp>
 
 using boost::program_options::variables_map;
 using boost::program_options::options_description;
@@ -26,40 +21,29 @@ using boost::program_options::store;
 using boost::program_options::command_line_parser;
 using boost::program_options::notify;
 
-using boost::accumulators::accumulator_set;
-using boost::accumulators::stats;
-using boost::accumulators::median;
-using boost::accumulators::mean;
-using boost::accumulators::sum;
-
-typedef boost::accumulators::tag::median median_tag;
-typedef boost::accumulators::tag::mean mean_tag;
-typedef boost::accumulators::tag::sum sum_tag;
-
 using hpx::util::high_resolution_timer;
 
 ///////////////////////////////////////////////////////////////////////////////
-boost::uint64_t tasks = 64;
-boost::uint64_t delay = 0;
+boost::uint64_t tasks = 500000;
+boost::uint64_t delay = 5;
 bool header = true;
 
 ///////////////////////////////////////////////////////////////////////////////
 void print_results(
     variables_map& vm
-  , double median_
-  , double mean_
   , double sum_
+  , double mean_
     )
 {
     if (header)
-        std::cout << "Tasks,Delay (iterations),Total Walltime (seconds),"
-                     "Median Walltime (seconds),Average Walltime (seconds),\n";
+        std::cout << "Tasks,Specified Task Length (microseconds),"
+                     "Walltime (seconds),Average Task Length (microseconds)\n";
 
     std::string const tasks_str = boost::str(boost::format("%lu,") % tasks);
     std::string const delay_str = boost::str(boost::format("%lu,") % delay);
 
-    std::cout << ( boost::format("%-21s %-21s %10.12s, %10.12s, %10.12s\n")
-                 % tasks_str % delay_str % sum_ % median_ % mean_);
+    std::cout << ( boost::format("%-21s %-21s %10.12g %10.12g\n")
+                 % tasks_str % delay_str % sum_ % mean_);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -67,27 +51,26 @@ int app_main(
     variables_map& vm
     )
 {
+    if (vm.count("no-header"))
+        header = false;
+
     if (0 == tasks)
-        throw std::invalid_argument("error: count of 0 tasks specified\n");
+        throw std::invalid_argument("count of 0 tasks specified\n");
 
-    accumulator_set<double, stats<median_tag, mean_tag, sum_tag> > results;
-
-    volatile double d = 0.0;
+    // Start the clock.
+    high_resolution_timer t;
 
     for (boost::uint64_t i = 0; i < tasks; ++i)
     {
-        // Start the clock.
-        high_resolution_timer t;
-
-        worker(delay, &d);
-
-        results(t.elapsed());
+        worker_timed(delay);
     }
 
-    // Print out the results.
-    print_results(vm, median(results), mean(results), sum(results));
+    double elapsed = t.elapsed();
 
-    return static_cast<int>(d);
+    // Print out the results.
+    print_results(vm, elapsed, (elapsed * 1e6) / double(tasks));
+
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -107,15 +90,12 @@ int main(
         , "print out program usage (this message)")
 
         ( "tasks"
-        , value<boost::uint64_t>(&tasks)->default_value(64)
+        , value<boost::uint64_t>(&tasks)->default_value(100000)
         , "number of tasks to invoke")
 
         ( "delay"
-        , value<boost::uint64_t>(&delay)->default_value(0)
-        , "number of iterations in the delay loop")
-
-        ( "no-header"
-        , "do not print out the csv header row")
+        , value<boost::uint64_t>(&delay)->default_value(5)
+        , "duration of delay in microseconds")
         ;
 
     store(command_line_parser(argc, argv).options(cmdline).run(), vm);
@@ -128,9 +108,6 @@ int main(
         std::cout << cmdline;
         return 0;
     }
-
-    if (vm.count("no-header"))
-        header = false;
 
     return app_main(vm);
 }
