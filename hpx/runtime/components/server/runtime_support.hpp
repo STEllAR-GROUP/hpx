de59281bfc5cf6ad5661fbb9b0080c0f588e4d09
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-02-08 18:15:05 -0600

    Removing superfluous comma

--- hpx/runtime/components/server/runtime_support.hpp
+++ hpx/runtime/components/server/runtime_support.hpp
@@ -615,7 +615,7 @@ namespace hpx { namespace components { namespace server
     struct migrate_component_here_action
       : ::hpx::actions::result_action2<
             runtime_support, naming::gid_type
-          , boost::shared_ptr<Component> const&, naming::id_type,
+          , boost::shared_ptr<Component> const&, naming::id_type
           , &runtime_support::migrate_component_to_here<Component>
           , migrate_component_here_action<Component> >
     {};
