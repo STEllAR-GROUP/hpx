6d63903e2d10a632e07aa9b2194c5b685c3d4869
Thomas Heller <thom.heller@gmail.com>
2014-02-19 12:47:57 +0100

    Fixing lockfree_fifo test

--- tests/unit/threads/lockfree_fifo.cpp
+++ tests/unit/threads/lockfree_fifo.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (C) 2011 Bryce Lelbach 
+//  Copyright (C) 2011 Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -64,16 +64,17 @@ int main(int argc, char** argv)
 
     options_description
         desc_cmdline("Usage: " HPX_APPLICATION_STRING " [options]");
-    
+
     desc_cmdline.add_options()
         ("help,h", "print out program usage (this message)")
-        ("threads,t", value<boost::uint64_t>(&threads)->default_value(2), 
-         "the number of worker threads inserting objects into the fifo") 
-        ("items,i", value<boost::uint64_t>(&items)->default_value(500000), 
-         "the number of items to create per queue") 
+        ("threads,t", value<boost::uint64_t>(&threads)->default_value(2),
+         "the number of worker threads inserting objects into the fifo")
+        ("items,i", value<boost::uint64_t>(&items)->default_value(500000),
+         "the number of items to create per queue")
     ;
 
-    store(command_line_parser(argc, argv).options(desc_cmdline).run(), vm);
+    store(
+        command_line_parser(argc, argv).options(desc_cmdline).allow_unregistered().run(), vm);
 
     notify(vm);
 
@@ -86,19 +87,19 @@ int main(int argc, char** argv)
 
     if (vm.count("threads"))
         threads = vm["threads"].as<boost::uint64_t>();
-  
+
     stolen.resize(threads);
- 
+
     for (boost::uint64_t i = 0; i < threads; ++i)
     {
-        queues.push_back(new boost::lockfree::queue<boost::uint64_t>);
+        queues.push_back(new boost::lockfree::queue<boost::uint64_t>(items));
 
         for (boost::uint64_t j = 0; j < items; ++j)
             (*queues[i]).push(j);
 
         BOOST_TEST(!(*queues[i]).empty());
     }
- 
+
     {
         boost::thread_group tg;
 
@@ -112,7 +113,7 @@ int main(int argc, char** argv)
         BOOST_TEST(stolen[i] == 0);
 
     for (boost::uint64_t i = 0; i < threads; ++i)
-        delete queues[i]; 
+        delete queues[i];
 
     return boost::report_errors();
 }
