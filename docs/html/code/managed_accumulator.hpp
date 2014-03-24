bbdacc685267164a714c7f7523318351b1b8b15c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-03-24 15:06:13 -0500

    Updating docs

--- docs/html/code/managed_accumulator.hpp
+++ docs/html/code/managed_accumulator.hpp
@@ -4,100 +4,114 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_AF0DB32C_432B_4E92_9040_66177E0693CC)
-#define HPX_AF0DB32C_432B_4E92_9040_66177E0693CC
+#if !defined(HPX_B808C8CA_810E_4583_9EA2_528553C8B511)
+#define HPX_B808C8CA_810E_4583_9EA2_528553C8B511
 
-#include <hpx/runtime/components/stubs/stub_base.hpp>
-#include <hpx/runtime/applier/apply.hpp>
-#include <hpx/include/async.hpp>
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/components/server/managed_component_base.hpp>
+#include <hpx/runtime/components/server/locking_hook.hpp>
+#include <hpx/runtime/actions/component_action.hpp>
 
-#include "../server/managed_accumulator.hpp"
-
-namespace examples { namespace stubs
+///////////////////////////////////////////////////////////////////////////////
+namespace examples { namespace server
 {
     ///////////////////////////////////////////////////////////////////////////
-    //[managed_accumulator_stubs_inherit
-    struct managed_accumulator
-      : hpx::components::stub_base<server::managed_accumulator>
+    /// This class is a very simple example of an HPX component. An HPX
+    /// component is a class that:
+    ///
+    ///     * Inherits from a component base class (either
+    ///       \a hpx::components::managed_component_base or
+    ///       \a hpx::components::simple_component_base).
+    ///     * Exposes methods that can be called asynchronously and/or remotely.
+    ///       These constructs are known as HPX actions.
+    ///
+    /// By deriving this component from \a locking_hook the runtime system 
+    /// ensures that all action invocations are serialized. That means that 
+    /// the system ensures that no two actions are invoked at the same time on
+    /// a given component instance. This makes the component thread safe and no
+    /// additional locking has to be implemented by the user.
+    ///
+    /// Components are first-class objects in HPX. This means that they are
+    /// globally addressable; all components have a unique GID.
+    ///
+    /// This example demonstrates how to write a managed component. Managed
+    /// components are allocated in bulk by HPX. When a component needs to be
+    /// created in large quantities, managed components should be used. Because
+    /// managed components are allocated in bulk, the creation of a new managed
+    /// component usually does not require AGAS requests.
+    ///
+    /// This component exposes 3 different actions: reset, add and query.
+    //[managed_accumulator_server_inherit
+    class managed_accumulator
+      : public hpx::components::locking_hook<
+            hpx::components::managed_component_base<managed_accumulator> 
+        >
     //]
     {
-        typedef server::managed_accumulator::argument_type argument_type;
+    public:
+        typedef boost::int64_t argument_type;
 
-        ///////////////////////////////////////////////////////////////////////
-        /// Reset the accumulator's value to 0.
-        ///
-        /// \note This function has fire-and-forget semantics. It will not wait
-        ///       for the action to be executed. Instead, it will return
-        ///       immediately after the action has has been dispatched.
-        //[managed_accumulator_stubs_reset_non_blocking
-        static void reset_non_blocking(hpx::naming::id_type const& gid)
-        {
-            typedef server::managed_accumulator::reset_action action_type;
-            hpx::apply<action_type>(gid);
-        }
+        //[managed_accumulator_server_ctor
+        managed_accumulator() : value_(0) {}
         //]
 
-        /// Reset the accumulator's value to 0.
-        ///
-        /// \note This function is fully synchronous.
-        static void reset_sync(hpx::naming::id_type const& gid)
-        {
-            typedef server::managed_accumulator::reset_action action_type;
-            hpx::async<action_type>(gid).get();
-        }
-
         ///////////////////////////////////////////////////////////////////////
-        /// Add \p arg to the accumulator's value.
-        ///
-        /// \note This function has fire-and-forget semantics. It will not wait
-        ///       for the action to be executed. Instead, it will return
-        ///       immediately after the action has has been dispatched.
-        static void
-        add_non_blocking(hpx::naming::id_type const& gid, boost::uint64_t arg)
+        // Exposed functionality of this component.
+
+        //[managed_accumulator_methods
+        /// Reset the value to 0.
+        void reset()
         {
-            typedef server::managed_accumulator::add_action action_type;
-            hpx::apply<action_type>(gid, arg);
+            // Atomically set value_ to 0.
+            value_= 0;
         }
 
-        /// Add \p arg to the accumulator's value.
-        ///
-        /// \note This function is fully synchronous.
-        //[managed_accumulator_stubs_add_sync
-        static void
-        add_sync(hpx::naming::id_type const& gid, argument_type arg)
+        /// Add the given number to the accumulator.
+        void add(argument_type arg)
         {
-            typedef server::managed_accumulator::add_action action_type;
-            hpx::async<action_type>(gid, arg).get();
+            // Atomically add value_ to arg, and store the result in value_.
+            value_ += arg;
+        }
+
+        /// Return the current value to the caller.
+        argument_type query() const
+        {
+            // Get the value of value_.
+            return value_;
         }
         //]
 
         ///////////////////////////////////////////////////////////////////////
-        /// Asynchronously query the current value of the accumulator.
-        ///
-        /// \returns This function returns an \a hpx::lcos::future. When the
-        ///          value of this computation is needed, the get() method of
-        ///          the future should be called. If the value is available,
-        ///          get() will return immediately; otherwise, it will block
-        ///          until the value is ready.
-        //[managed_accumulator_stubs_query_async
-        static hpx::lcos::unique_future<argument_type>
-        query_async(hpx::naming::id_type const& gid)
-        {
-            typedef server::managed_accumulator::query_action action_type;
-            return hpx::async<action_type>(gid);
-        }
+        // Each of the exposed functions needs to be encapsulated into an
+        // action type, generating all required boilerplate code for threads,
+        // serialization, etc.
+
+        //[managed_accumulator_action_types
+        HPX_DEFINE_COMPONENT_ACTION(managed_accumulator, reset);
+        HPX_DEFINE_COMPONENT_ACTION(managed_accumulator, add);
+        HPX_DEFINE_COMPONENT_CONST_ACTION(managed_accumulator, query);
         //]
 
-        /// Query the current value of the accumulator.
-        ///
-        /// \note This function is fully synchronous.
-        static argument_type query_sync(hpx::naming::id_type const& gid)
-        {
-            // The following get yields control while the action is executed.
-            return query_async(gid).get();
-        }
+    //[managed_accumulator_server_data_member
+    private:
+        argument_type value_;
+    //]
     };
 }}
+
+//[managed_accumulator_registration_declarations
+HPX_REGISTER_ACTION_DECLARATION(
+    examples::server::managed_accumulator::reset_action,
+    managed_accumulator_reset_action);
+
+HPX_REGISTER_ACTION_DECLARATION(
+    examples::server::managed_accumulator::add_action,
+    managed_accumulator_add_action);
+
+HPX_REGISTER_ACTION_DECLARATION(
+    examples::server::managed_accumulator::query_action,
+    managed_accumulator_query_action);
+//]
 
 #endif
 
