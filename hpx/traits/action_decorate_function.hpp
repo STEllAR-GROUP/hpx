0a383dcda7f807eab79addecc799a43b9a99ddcd
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-03-30 17:58:32 -0500

    Adding missing files

---
+++ hpx/traits/action_decorate_function.hpp
@@ -0,0 +1,37 @@
+//  Copyright (c) 2007-2014 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_TRAITS_ACTION_DECORATE_FUNCTION_MAR_30_2014_1054AM)
+#define HPX_TRAITS_ACTION_DECORATE_FUNCTION_MAR_30_2014_1054AM
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/naming/name.hpp>
+
+namespace hpx { namespace traits
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // Customization point for action capabilities
+    template <typename Action, typename Enable>
+    struct action_decorate_function
+    {
+        // return the required capabilities to invoke the given action
+        template <typename F>
+        static HPX_STD_FUNCTION<threads::thread_function_type>
+        call(naming::address::address_type lva, F && f)
+        {
+            // by default we forward this to the component type
+            typedef typename Action::component_type component_type;
+            return component_type::decorate_action(lva, std::forward<F>(f));
+        }
+    };
+
+    template <typename Action>
+    struct action_decorate_function<Action, typename Action::type>
+      : action_decorate_function<typename Action::type>
+    {};
+}}
+
+#endif
+
---
+++ hpx/traits/action_schedule_thread.hpp
@@ -0,0 +1,37 @@
+//  Copyright (c) 2007-2014 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_TRAITS_ACTION_SCHEDULE_THREAD_MAR_30_2014_0325PM)
+#define HPX_TRAITS_ACTION_SCHEDULE_THREAD_MAR_30_2014_0325PM
+
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/runtime/threads/thread_init_data.hpp>
+
+namespace hpx { namespace traits
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // Customization point for action capabilities
+    template <typename Action, typename Enable>
+    struct action_schedule_thread
+    {
+        // return the required capabilities to invoke the given action
+        static void call(naming::address::address_type lva,
+            threads::thread_init_data& data,
+            threads::thread_state_enum initial_state)
+        {
+            // by default we forward this to the component type
+            typedef typename Action::component_type component_type;
+            return component_type::schedule_thread(lva, data, initial_state);
+        }
+    };
+
+    template <typename Action>
+    struct action_schedule_thread<Action, typename Action::type>
+      : action_schedule_thread<typename Action::type>
+    {};
+}}
+
+#endif
+
