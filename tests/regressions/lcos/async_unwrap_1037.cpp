936aee8eee88a19a62ca9024f7f9c686ea10d726
Thomas Heller <thom.heller@gmail.com>
2014-04-01 13:07:53 +0200

    Adding testcase to demonstrate recent failures

--- tests/regressions/lcos/async_unwrap_1037.cpp
+++ tests/regressions/lcos/async_unwrap_1037.cpp
@@ -22,8 +22,58 @@ hpx::future<int> f(int i)
     return hpx::make_ready_future(i);
 }
 
+int ff()
+{
+    return 1;
+}
+
+hpx::future<int> g()
+{
+    return f(1);
+}
+
+HPX_PLAIN_ACTION(g);
+
+hpx::future<int> h()
+{
+    return hpx::async(f, 1);
+}
+
+HPX_PLAIN_ACTION(h);
+
+hpx::future<int> i()
+{
+    return hpx::async(ff);
+}
+
+HPX_PLAIN_ACTION(i);
+
 int main()
 {
     HPX_TEST_EQ(f(0).get(), 1);
+    HPX_TEST_EQ(f(1).get(), 1);
+    {
+        hpx::future<int> fut = hpx::async(f, 0);
+        HPX_TEST_EQ(fut.get(), 1);
+    }
+    {
+        hpx::future<int> fut = hpx::async(f, 1);
+        HPX_TEST_EQ(fut.get(), 1);
+    }
+    {
+        hpx::future<int> fut = hpx::async(g);
+        HPX_TEST_EQ(fut.get(), 1);
+    }
+    HPX_TEST_EQ(hpx::async(g_action(), hpx::find_here()).get(), 1);
+    {
+        hpx::future<int> fut = hpx::async(h);
+        HPX_TEST_EQ(fut.get(), 1);
+    }
+    HPX_TEST_EQ(hpx::async(h_action(), hpx::find_here()).get(), 1);
+    {
+        hpx::future<int> fut = hpx::async(i);
+        HPX_TEST_EQ(fut.get(), 1);
+    }
+    HPX_TEST_EQ(hpx::async(i_action(), hpx::find_here()).get(), 1);
     return hpx::util::report_errors();
 }
