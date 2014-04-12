dfec2e5c407479d3fa535242992c6a46456aff65
K-ballo <k@fusionfenix.com>
2014-04-12 16:29:31 -0300

    Making future<void> constructors implicit

--- hpx/lcos/future.hpp
+++ hpx/lcos/future.hpp
@@ -723,7 +723,7 @@ namespace hpx { namespace lcos
         //     constructor invocation.
         //   - other.valid() == false.
         template <typename T>
-        explicit future(future<T>&& other,
+        future(future<T>&& other,
             typename boost::enable_if<boost::is_void<R>, T>::type* = 0
         ) : base_type(other.valid() ? detail::make_void_continuation(other) : 0)
         {
@@ -943,14 +943,14 @@ namespace hpx { namespace lcos
         {
             other = future<R>();
         }
-        
+
         // Effects: constructs a future<void> object that will be ready when
         //          the given future is ready
         // Postconditions:
         //   - valid() returns the same value as other.valid() prior to the
         //     constructor invocation.
         template <typename T>
-        explicit shared_future(shared_future<T> const& other,
+        shared_future(shared_future<T> const& other,
             typename boost::enable_if<boost::is_void<R>, T>::type* = 0
         ) : base_type(other.valid() ? detail::make_void_continuation(other) : 0)
         {}
