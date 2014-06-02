92caef2a8bb2841f10ba8c357ca3219abf9f277b
Grant Mercer <Gmercer015@gmail.com>
2014-06-02 09:17:17 -0500

    copy.cpp now properly tests hpx::parallel::copy

--- hpx/stl/detail/copy.hpp
+++ hpx/stl/detail/copy.hpp
@@ -1,15 +1,10 @@
-
-#if !define(HPX_STL_DETAIL_COPY_MAY_30_2014_0317PM)
+#if !defined(HPX_STL_DETAIL_COPY_MAY_30_2014_0317PM)
 #define HPX_STL_DETAIL_COPY_MAY_30_2014_0317PM
 
 #include <hpx/hpx_fwd.hpp>
+#include <hpx/exception_list.hpp>
 #include <hpx/stl/execution_policy.hpp>
 #include <hpx/stl/detail/algorithm_result.hpp>
-#include <hpx/stl/util/partitioner.hpp>
-#include <hpx/stl/util/loop.hpp>
-#include <hpx/exception_list.hpp>
-#include <hpx/util/void_guard.hpp>
-#include <hpx/util/decay.hpp>
 #include <hpx/stl/detail/zip_iterator.hpp>
 
 #include <algorithm>
@@ -21,129 +16,119 @@
 
 namespace hpx { namespace parallel
 {
-	///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
     // copy
-	namespace detail
-	{
-		template<typename ExPolicy, typename InIter, typename OutIter
-			typename IterTag>
-		typename detail::algorithm_result<ExPolicy, OutIter>::type
-		copy_seq(ExPolicy const&, InIter first, InIter last, OutIter dest,
-			IterTag)
-		{
-			try {
-				return detail::algorithm_result<ExPolicy, OutIter>::get(
-					std::copy(first,last,dest));
-			}
-			catch(std::bad_alloc const& e) {
-				throw e;
-			}
-			catch (...) {
-				throw hpx::exception_list(boost::current_exception());
-			}
-		}
-
-		        template <int N, typename R, typename ZipIter>
-        R get_iter(ZipIter&& zipiter)
+    namespace detail
+    {
+        template<typename ExPolicy, typename InIter, typename OutIter,
+            typename IterTag>
+        typename detail::algorithm_result<ExPolicy, OutIter>::type
+        copy_seq(ExPolicy const&, InIter first, InIter last, OutIter dest,
+            IterTag)
         {
-            return boost::get<N>(*zipiter);
+            try {
+                return detail::algorithm_result<ExPolicy, OutIter>::get(
+                    std::copy(first,last,dest));
+            }
+            catch(std::bad_alloc const& e) {
+                throw e;
+            }
+            catch (...) {
+                throw hpx::exception_list(boost::current_exception());
+            }
+        }
+    
+        template<typename ExPolicy, typename InIter, typename OutIter,
+            typename IterTag>
+        typename detail::algorithm_result<ExPolicy, OutIter>::type
+        copy(ExPolicy const& policy, InIter first, InIter last, OutIter dest, 
+            IterTag category)
+        {
+            typedef boost::tuple<InIter, OutIter> iterator_tuple;
+            typedef detail::zip_iterator<iterator_tuple> zip_iterator;
+            typedef typename zip_iterator::reference reference;
+            typedef
+                typename detail::algorithm_result<ExPolicy, OutIter>::type
+            result_type;
+
+            return get_iter<1, result_type>(
+                for_each_n(policy,
+                    detail::make_zip_iterator(boost::make_tuple(first,dest)),
+                    std::distance(first,last),
+                    [](reference it) {
+                        *boost::get<1>(it) = *boost::get<0>(it);
+                    },
+                    category));
         }
 
-        template <int N, typename R, typename ZipIter>
-        R get_iter(hpx::future<ZipIter>&& zipiter)
+        template<typename ExPolicy, typename InIter, typename OutIter>
+        typename boost::enable_if<
+            is_parallel_execution_policy<ExPolicy>,
+            typename detail::algorithm_result<ExPolicy, OutIter>::type
+        >::type
+        copy(ExPolicy const& policy, InIter first, InIter last, OutIter dest,
+            std::input_iterator_tag category)
         {
-            return zipiter.then(
-                [](hpx::future<ZipIter>&& f) {
-                    typename std::iterator_traits<ZipIter>::value_type t =
-                        *f.get();
-                    return boost::get<N>(t);
-                });
+            return detail::copy_seq(policy, first, last, dest, category);
         }
-	
-		template<typename ExPolicy, typename InIter, typename OutIter,
-			typename IterTag>
-		typename detail::algorithm_result<ExPolicy, OutIter>::type
-		copy(ExPolicy const& policy, InIter first, InIter last, OutIter dest, 
-			IterTag category)
-		{
-			typedef boost::tuple<InIter, OutIter> iterator_tuple;
-			typedef detail::zip_iterator<iterator_tuple> zip_iterator;
-			typedef typename zip_iterator::reference reference;
-			typedef
-				typename detail::algorithm_result<ExPolicy, void>::type
-			result_type;
 
-			return get_iter<1, result_type>(
-				for_each_n(policy,
-					detail::make_zip_iterator(boost::make_tuple(first,dest)),
-					std::distance(first,last),
-					[](reference it) {
-						*boost::get<1>(it) = *boost::get<0>(it);
-					},
-					category));
-		}
+        template<typename InIter, typename OutIter, typename IterTag>
+        OutIter copy(sequential_execution_policy const& policy,
+            InIter first, InIter last, OutIter dest, IterTag category)
+        {
+            return detail::copy_seq(policy, first, last, dest, category);
+        }
 
-		template<typename ExPolicy, typename InIter, typename OutIter>
-		typename boost::enable_if<
-			is_parallel_execution_policy<ExPolicy>,
-			typename detail::algorithm_resutl<ExPolicy, void>::type
-		>::type
-		for_each(ExPolicy const& policy, InIter first, InIter last, OutIter dest,
-			std::input_iterator_tag category)
-		{
-			return detail::copy_seq(policy, first, last, desk, category);
-		}
+        template<typename InIter, typename OutIter, typename IterTag>
+        OutIter copy(execution_policy const& policy,
+            InIter first, InIter last, OutIter dest, IterTag category)
+        {
+            switch (detail::which(policy))
+            {
+            case detail::execution_policy_enum::sequential:
+                return detail::copy(
+                    *policy.get<sequential_execution_policy>(),
+                    first, last, dest, category);
 
-		template<typename InIter, typename OutIter, typename IterTag>
-		OutIter copy(sequential_execution_policy const& policy,
-			InIter first, InIter last, OutIter dest, IterTag category)
-		{
-			return detail::copy_seq(policy, first, last, dest, category);
-		}
+            case detail::execution_policy_enum::parallel:
+                return detail::copy(
+                    *policy.get<parallel_execution_policy>(),
+                    first, last, dest, category);
 
-		template<typename InIter, typename OutIter, typename IterTag>
-		OutIter copy(execution_policy const& policy,
-			InIter first, InIter last, OutIter dest, IterTag category)
-		{
-			switch (detail::which(policy))
-			{
-			case detail::execution_policy_enum::sequential:
-				return detail::copy(
-					*policy.get<sequential_execution_policy>(),
-					first, last, dest, category);
+            case detail::execution_policy_enum::vector:
+                return detail::copy(
+                    *policy.get<vector_execution_policy>(),
+                    first, last, dest, category);
 
-			case detail::execution_policy_enum::parallel:
-				return detail::copy(
-					*policy.get<parallel_execution_policy>(),
-					first, last, dest, category);
+            case detail::execution_policy_enum::task:
+                return detail::copy(par,
+                    first, last, dest, category);
 
-			case detail::execution::policy_enum::task:
-				return detail::copy(par,
-					first, last, desk, category);
+            default:
+                HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                    "hpx::parallel::detail::transform",
+                    "Not supported execution policy");
+                break;
+            }
+        }
+    }
 
-			default:
-				HPX_THROW_EXCEPTION(hpx::bad_parameter,
-					"hpx::parallel::detail::transform",
-					"Not supported execution policy");
-				break;
-			}
-		}
-	}
+    template<typename ExPolicy, typename InIter, typename OutIter>
+    typename boost::enable_if<
+        is_execution_policy<ExPolicy>,
+        typename detail::algorithm_result<ExPolicy, OutIter>::type
+    >::type
+    copy(ExPolicy&& policy, InIter first, InIter last, OutIter dest)
+    {
+        BOOST_STATIC_ASSERT_MSG(
+            boost::is_base_of<std::input_iterator_tag,
+                typename std::iterator_traits<InIter>::iterator_category>::value,
+            "Required at least input iterator.");
 
-	template<typename ExPolicy, typename InIter, typename OutIter>
-	typename boost::enable_if<
-		is_execution_policy<ExPolicy>,
-		typename detail::algorith_result<ExPolicy, OutIter>::type
-	>::type
-	transform(ExPolicy&& policy, InIter first, InIter last, OutIter dest)
-	{
-		BOOST_STATIC_ASSERT_MSG(
-			boost::is_base_of<std::input_iterator_tag,
-				typename std::iterator_traits<InIter>::iterator_category::value,
-			"Required at least input iterator.");
+        std::iterator_traits<InIter>::iterator_category category;
+        return detail::copy(policy, first, last, dest, category);
+    }
+    
+}}
 
-		std::iterator_traits<InIter>::iterator_category category;
-		return detail::copy(policy, first, last, dest, category);
-	}
-	
-}
+#endif
--- tests/unit/stl/copy.cpp
+++ tests/unit/stl/copy.cpp
@@ -5,21 +5,116 @@
 
 #include "test_utils.hpp"
 
+////////////////////////////////////////////////////////////////////////////
+template<typename ExPolicy, typename IteratorTag>
+void test_copy(ExPolicy const& policy, IteratorTag)
+{
+    BOOST_STATIC_ASSERT(hpx::parallel::is_execution_policy<ExPolicy>::value);
+
+    typedef std::vector<std::size_t>::iterator base_iterator;
+    typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+    std::vector<std::size_t> c(10000);
+    std::vector<std::size_t> d(c.size());
+    std::iota(boost::begin(c), boost::end(c), std::rand());
+
+    base_iterator outiter = hpx::parallel::copy(policy,
+        iterator(boost::begin(c)), iterator(boost::end(c)), boost::begin(d));
+
+    std::size_t count =0;
+    HPX_TEST(std::equal(boost::begin(c), boost::end(c), boost::begin(d),
+        [&count](std::size_t v1, std::size_t v2) {
+            HPX_TEST_EQ(v1, v2);
+            ++count;
+            return v1 == v2;
+    }));
+    HPX_TEST_EQ(count, d.size());
+}
+
+template<typename IteratorTag>
+void test_copy(hpx::parallel::task_execution_policy, IteratorTag)
+{
+    typedef std::vector<std::size_t>::iterator base_iterator;
+    typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+    std::vector<std::size_t> c(10000);
+    std::vector<std::size_t> d(c.size());
+    std::iota(boost::begin(c), boost::end(c), std::rand());
+
+    hpx::future<base_iterator> f =
+        hpx::parallel::copy(hpx::parallel::task,
+            iterator(boost::begin(c)), iterator(boost::end(c)), boost::begin(d));
+    f.wait();
+
+    std::size_t count = 0;
+    HPX_TEST(std::equal(boost::begin(c), boost::end(c), boost::begin(d),
+        [&count](std::size_t v1, std::size_t v2) {
+            HPX_TEST_EQ(v1, v2);
+            ++count;
+            return v1 == v2;
+    }));
+    HPX_TEST_EQ(count, d.size());
+}
+
+template <typename IteratorTag>
+void test_copy()
+{
+    using namespace hpx::parallel;
+
+    test_copy(seq, IteratorTag());
+    test_copy(par, IteratorTag());
+    test_copy(vec, IteratorTag());
+    test_copy(task, IteratorTag());
+
+    test_copy(execution_policy(seq), IteratorTag());
+    test_copy(execution_policy(par), IteratorTag());
+    test_copy(execution_policy(vec), IteratorTag());
+    test_copy(execution_policy(task), IteratorTag());
+}
+
+void copy_test()
+{
+    test_copy<std::random_access_iterator_tag>();
+    test_copy<std::forward_iterator_tag>();
+    test_copy<std::input_iterator_tag>();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template<typename ExPolicy, typename IteratorTag>
+void test_copy_exception(ExPolicy const& policy, IteratorTag)
+{
+    BOOST_STATIC_ASSERT(hpx::parallel::is_execution_policy<ExPolicy>::value);
+
+    typedef std::vector<std::size_t>::iterator base_iterator;
+    typedef std::test_iterator<base_iterator, IteratorTag> iterator;
+
+    std::vector<std::size_t> c(10000);
+    std::vector<std::size_t> d(c.size());
+    std::iota(boost::begin(c), boost::end(c), std::rand());
+
+    bool caught_exception = false;
+    try {
+        base_iterator outiter = hpx::parallel::copy(policy,
+            iterator(boost::begin(c)), iterator(boost::end(c)), boost::begin(d),
+
+
 int hpx_main()
 {
-	std::cout << "test file testing";
-	return hpx::finalize();
+    copy_test();
+    std::cout << "test file testing";
+    char c; std::cin >> c;
+    return hpx::finalize();
 }
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::string cfg;
-	cfg.push_back("hpx.os_threads=" +
-		boost::lexical_cast<std::string>(hpx::threads::hardware::concurrency()));
+    std::vector<std::string> cfg;
+    cfg.push_back("hpx.os_threads=" +
+        boost::lexical_cast<std::string>(hpx::threads::hardware_concurrency()));
 
-	HPX_TEST_EQ_MSG(hpx::init(argc, argv, cfg), 0,
-		"HPX main exited with non-zero status");
+    HPX_TEST_EQ_MSG(hpx::init(argc, argv, cfg), 0,
+        "HPX main exited with non-zero status");
 
-	return hpx::util::report_errors();
+    return hpx::util::report_errors();
 
 }
