5e16992350ebb4f86e0485db9a7d9c8f8fda9bc9
Patricia Grubel <pagrubel@nmsu.edu>
2014-06-03 19:37:45 -0500

    oops forgot one more place for header

--- examples/1d_stencil/print_time_results.hpp
+++ examples/1d_stencil/print_time_results.hpp
@@ -48,8 +48,8 @@ void print_time_results(
     )
 {
     if (header)
-        std::cout << "OS-threads, Execution Time (seconds),"
-                " Grid Points, Time Steps\n"
+        std::cout << "OS-threads, Execution-Time(seconds),"
+                " Grid-Points, Time-Steps\n"
              << hpx::flush;
 
     std::string const threads_str = boost::str(boost::format("%lu,") % num_os_threads);
