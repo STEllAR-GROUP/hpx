6774ed66645054761e73cda0fe495c5ef42c4c8e
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-06-04 08:59:47 -0500

    More tinkering and refactoring

--- examples/mini_ghost/mini_ghost.cpp
+++ examples/mini_ghost/mini_ghost.cpp
@@ -16,12 +16,15 @@ typedef mini_ghost::grid<double> grid_type;
 
 typedef mini_ghost::stepper<grid_type::value_type> stepper_type;
 
+// Global configuration data (after initialization this is read-only)
 mini_ghost::params<grid_type::value_type> p;
+
+// Global profiling data
 hpx::lcos::local::spinlock profiling_data_mtx;
 boost::shared_ptr<hpx::lcos::local::counting_semaphore> profiling_data_sem;
 std::vector<mini_ghost::profiling::profiling_data> profiling_data;
 
-double init_start;
+double init_start = 0;
 
 void add_profile(mini_ghost::profiling::profiling_data const & pd)
 {
@@ -30,7 +33,8 @@ void add_profile(mini_ghost::profiling::profiling_data const & pd)
         profiling_data.push_back(pd);
         profiling_data_sem->signal();
     }
-    if(p.rank == 0)
+
+    if (p.rank == 0)
     {
         profiling_data_sem->wait();
     }
@@ -44,10 +48,11 @@ int hpx_main(boost::program_options::variables_map& vm)
         hpx::util::high_resolution_timer::now() - init_start);
 
     hpx::util::high_resolution_timer timer_all;
-    hpx::id_type    here = hpx::find_here();
-    std::string     name = hpx::get_locality_name();
-    p.rank = hpx::naming::get_locality_id_from_id(here);
 
+    hpx::id_type here = hpx::find_here();
+    std::string name = hpx::get_locality_name();
+
+    p.rank = hpx::naming::get_locality_id_from_id(here);
     if(p.rank == 0)
     {
         std::cout << "mini ghost started up in "
@@ -60,25 +65,30 @@ int hpx_main(boost::program_options::variables_map& vm)
     profiling_data_sem.reset(new hpx::lcos::local::counting_semaphore(p.nranks));
     p.setup(vm);
 
-    hpx::id_type stepper_id
-        = hpx::components::new_<stepper_type>(here).get();
+    // Create the local stepper object, retrieve the local pointer to it
+    hpx::id_type stepper_id = hpx::components::new_<stepper_type>(here).get();
+    boost::shared_ptr<stepper_type> stepper(
+        hpx::get_ptr<stepper_type>(stepper_id).get());
 
-    boost::shared_ptr<stepper_type>
-        stepper(hpx::get_ptr<stepper_type>(stepper_id).get());
-
+    // Initialize stepper
     stepper->init(p).get();
     mini_ghost::barrier_wait();
 
+    // Perform the actual simulation work
     stepper->run(p.num_spikes, p.num_tsteps);
     mini_ghost::barrier_wait();
 
+    // Output various pieces of information about the run
     if (stepper->get_rank() == 0)
     {
+        // Output various pieces of information about the run
         add_profile(mini_ghost::profiling::data());
-        if(p.report_perf)
+
+        if (p.report_perf)
             mini_ghost::profiling::report(std::cout, profiling_data, p);
         else
             std::cout << "Total runtime: " << timer_all.elapsed() << "\n";
+
         std::ofstream fs("results.yaml");
         mini_ghost::profiling::report(fs, profiling_data, p);
         std::cout << "finalizing ...\n";
@@ -86,6 +96,7 @@ int hpx_main(boost::program_options::variables_map& vm)
     }
     else
     {
+        // Send performance data from this locality to root
         hpx::apply(add_profile_action(), hpx::find_root_locality(),
             mini_ghost::profiling::data());
         return 0;
@@ -100,11 +111,11 @@ int main(int argc, char* argv[])
 
     p.cmd_options(desc_commandline);
 
-    // Register startup functions for creating our global barrier
+    // Register startup functions for creating/retrieving our global barrier
     hpx::register_pre_startup_function(&mini_ghost::create_barrier);
     hpx::register_startup_function(&mini_ghost::find_barrier);
 
-    // Initialize and run HPX, this test requires to run hpx_main on all localities
+    // Initialize and run HPX such that hpx_main is executed on all localities
     std::vector<std::string> cfg;
     cfg.push_back("hpx.run_hpx_main!=1");
 
--- examples/mini_ghost/stepper.hpp
+++ examples/mini_ghost/stepper.hpp
@@ -25,9 +25,7 @@ namespace mini_ghost {
     {
         static const std::size_t max_num_neighbors = 6;
 
-        typedef
-            hpx::util::serialize_buffer<Real>
-            buffer_type;
+        typedef hpx::util::serialize_buffer<Real> buffer_type;
 
         stepper();
 
@@ -35,26 +33,36 @@ namespace mini_ghost {
 
         void run(std::size_t num_spikes, std::size_t num_tsteps);
 
-        void set_global_sum(std::size_t generation, std::size_t which, Real value, std::size_t idx, std::size_t id);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_global_sum, set_global_sum_action);
+        ///////////////////////////////////////////////////////////////////////
+        // define actions to set the boundary data
+        void set_global_sum(std::size_t generation, std::size_t which,
+            Real value, std::size_t idx, std::size_t id);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_global_sum,
+            set_global_sum_action);
 
         void set_north_zone(buffer_type buffer, std::size_t step, std::size_t var);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_north_zone, set_north_zone_action);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_north_zone,
+            set_north_zone_action);
 
         void set_south_zone(buffer_type buffer, std::size_t step, std::size_t var);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_south_zone, set_south_zone_action);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_south_zone,
+            set_south_zone_action);
 
         void set_east_zone(buffer_type buffer, std::size_t step, std::size_t var);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_east_zone, set_east_zone_action);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_east_zone,
+            set_east_zone_action);
 
         void set_west_zone(buffer_type buffer, std::size_t step, std::size_t var);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_west_zone, set_west_zone_action);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_west_zone,
+            set_west_zone_action);
 
         void set_front_zone(buffer_type buffer, std::size_t step, std::size_t var);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_front_zone, set_front_zone_action);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_front_zone,
+            set_front_zone_action);
 
         void set_back_zone(buffer_type buffer, std::size_t step, std::size_t var);
-        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_back_zone, set_back_zone_action);
+        HPX_DEFINE_COMPONENT_ACTION_TPL(stepper<Real>, set_back_zone,
+            set_back_zone_action);
 
         std::size_t get_rank() const { return rank; }
 
@@ -71,18 +79,12 @@ namespace mini_ghost {
             partition<
                 Real
               , set_global_sum_action
-              , set_south_zone_action
-              , set_north_zone_action
-              , set_west_zone_action
-              , set_east_zone_action
-              , set_front_zone_action
-              , set_back_zone_action
+              , set_south_zone_action, set_north_zone_action
+              , set_west_zone_action, set_east_zone_action
+              , set_front_zone_action, set_back_zone_action
             >
             partition_type;
         std::vector<partition_type> partitions_;
-
-        hpx::lcos::local::promise<void> init_promise_;
-        hpx::future<void> init_future_;
     };
 }
 
