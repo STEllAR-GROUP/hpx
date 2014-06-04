1c8c124073eb8425403be3ef444db6b0b032fc45
Grant Mercer <Gmercer015@gmail.com>
2014-06-04 11:00:45 -0500

    added bad_alloc and exception tests for copy_if

--- tests/unit/stl/copyif.cpp
+++ tests/unit/stl/copyif.cpp
@@ -101,16 +101,205 @@ void test_copy_if()
 	test_copy_if(execution_policy(task), IteratorTag());
 }
 
-void if_copy_test()
+void copy_if_test()
 {
 	test_copy_if<std::random_access_iterator_tag>();
 	test_copy_if<std::forward_iterator_tag>();
 	test_copy_if<std::input_iterator_tag>();
 }
 
+///////////////////////////////////////////////////////////////////////////////
+template<typename ExPolicy, typename IteratorTag>
+void test_copy_if_exception(ExPolicy const& policy, IteratorTag)
+{
+	BOOST_STATIC_ASSERT(hpx::parallel::is_execution_policy<ExPolicy>::value);
+
+	typedef std::vector<std::size_t>::iterator base_iterator;
+	typedef test::test_iterator<base_iterator,IteratorTag> iterator;
+
+	std::vector<std::size_t> c(10007);
+	std::vector<std::size_t> d(c.size());
+	std::iota(boost::begin(c), boost::end(c), std::rand());
+
+	bool caught_exception = false;
+	try {
+		base_iterator outiter = hpx::parallel::transform(policy,
+			iterator(boost::begin(c)), iterator(boost::end(c)), boost::begin(d),
+			[](std::size_t v) {
+				throw std::runtime_error("test");
+				return v;
+			});
+		HPX_TEST(false);
+	}
+	catch(hpx::exception_list const& e) {
+		caught_exception = true;
+		test::test_num_exeptions<ExPolicy, IteratorTag>::call(policy, e);
+	}
+	catch(...) {
+		HPX_TEST(false);
+	}
+
+	HPX_TEST(caught_exception);
+}
+
+template<typename IteratorTag>
+void test_copy_if_exception(hpx::parallel::task_execution_policy, IteratorTag)
+{
+	typedef std::vector<std::size_t>::iterator base_iterator;
+	typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+	std::vector<std::size_t> c(10007);
+	std::vector<std::size_t> d(c.size());
+	std::iota(boost::begin(c), boost::end(c), std::rand());
+
+	bool caught_exception = false;
+	try {
+		hpx::future<base_iterator> f = 
+			hpx::parallel::copy_if(hpx::parallel::task,
+				iterator(boost::begin(c)), iterator(boost::end(c)),
+				boost::begin(d),
+				[](std::size_t v) {
+					throw std::runtime_error("test");
+					return v;
+			});
+		f.get();
+
+		HPX_TEST(false);
+	}
+	catch(hpx::exception_list const& e) {
+		caught_exception = true;
+		test::test_num_exeptions<
+			hpx::parallel::task_execution_policy, IteratorTag
+		>::call(hpx::parallel::task, e);
+	}
+	catch(...) {
+		HPX_TEST(false);
+	}
+
+	HPX_TEST(caught_exception);
+}
+
+template<typename IteratorTag>
+void test_copy_if_exception()
+{
+	using namespace hpx::parallel;
+
+	test_copy_if_exception(seq, IteratorTag());
+	test_copy_if_exception(par, IteratorTag());
+	test_copy_if_exception(vec, IteratorTag());
+	test_copy_if_exception(task, IteratorTag());
+
+	test_copy_if_exception(execution_policy(seq), IteratorTag());
+	test_copy_if_exception(execution_policy(par), IteratorTag());
+	test_copy_if_exception(execution_policy(vec), IteratorTag());
+	test_copy_if_exception(execution_policy(task), IteratorTag());
+}
+
+void copy_if_exception_test()
+{
+	test_copy_if_exception<std::random_access_iterator_tag>();
+	test_copy_if_exception<std::forward_iterator_tag>();
+	test_copy_if_exception<std::input_iterator_tag>();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template< typename ExPolicy, typename IteratorTag>
+void test_copy_if_bad_alloc(ExPolicy const& policy, IteratorTag)
+{
+	BOOST_STATIC_ASSERT(hpx::parallel::is_execution_policy<ExPolicy>::value);
+
+	typedef std::vector<std::size_t>::iterator base_iterator;
+	typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+	std::vector<std::size_t> c(10007);
+	std::vector<std::size_t> d(c.size());
+	std::iota(boost::begin(c), boost::end(c), std::rand());
+
+	bool caught_bad_alloc = false;
+	try {
+		base_iterator outiter = hpx::parallel::copy_if(policy,
+			iterator(boost::begin(c)), iterator(boost::end(c)), boost::begin(d),
+			[](std::size_t v) {
+				throw std::bad_alloc();
+				return v;
+			});
+
+		HPX_TEST(false);
+	}
+	catch(std::bad_alloc const&) {
+		caught_bad_alloc = true;
+	}
+	catch(...) {
+		HPX_TEST(false);
+	}
+
+	HPX_TEST(caught_bad_alloc);
+}
+
+template<typename IteratorTag>
+void test_copy_if_bad_alloc(hpx::parallel::task_execution_policy, IteratorTag)
+{
+	typedef std::vector<std::size_t>::iterator base_iterator;
+	typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+	std::vector<std::size_t> c(10007);
+	std::vector<std::size_t> d(c.size());
+	std::iota(boost::begin(c), boost::end(c), std::rand());
+
+	bool caught_bad_alloc = false;
+	try {
+		hpx::future<base_iterator> f =
+			hpx::parallel::copy_if(hpx::parallel::task,
+			iterator(boost::begin(c)), iterator(boost::end(c)),
+			boost::begin(d),
+			[](std::size_t v) {
+				throw std::bad_alloc();
+				return v;
+			});
+
+		f.get();
+
+		HPX_TEST(false);
+	}
+	catch(std::bad_alloc const&) {
+		caught_bad_alloc = true;
+	}
+	catch(...) {
+		HPX_TEST(false);
+	}
+
+	HPX_TEST(caught_bad_alloc);
+}
+
+template<typename IteratorTag>
+void test_copy_if_bad_alloc()
+{
+	using namespace hpx::parallel;
+
+	test_copy_if_bad_alloc(seq, IteratorTag());
+	test_copy_if_bad_alloc(par, IteratorTag());
+	test_copy_if_bad_alloc(vec, IteratorTag());
+	test_copy_if_bad_alloc(task, IteratorTag());
+
+	test_copy_if_bad_alloc(execution_policy(seq), IteratorTag());
+	test_copy_if_bad_alloc(execution_policy(par), IteratorTag());
+	test_copy_if_bad_alloc(execution_policy(vec), IteratorTag());
+	test_copy_if_bad_alloc(execution_policy(task), IteratorTag());
+}
+
+void copy_if_bad_alloc_test()
+{
+	test_copy_if_bad_alloc<std::random_access_iterator_tag>();
+	test_copy_if_bad_alloc<std::forward_iterator_tag>();
+	test_copy_if_bad_alloc<std::input_iterator_tag>();
+}
+
+
 int hpx_main()
 {
-	if_copy_test();
+	copy_if_test();
+	copy_if_exception_test();
+	copy_if_bad_alloc_test();
 	return hpx::finalize();
 }
 
