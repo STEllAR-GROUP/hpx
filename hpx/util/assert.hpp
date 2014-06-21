d3666c164ec3561cee0a436ac41c04c5e083a885
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-06-21 17:17:05 -0500

    Fix missing closing paren

--- hpx/util/assert.hpp
+++ hpx/util/assert.hpp
@@ -26,7 +26,7 @@
 #if defined(HPX_DISABLE_ASSERTS) || defined(BOOST_DISABLE_ASSERTS) || defined(NDEBUG)
 
 #if defined(HPX_GCC_VERSION) || defined(HPX_CLANG_VERSION) || \
-    (defined(HPX_INTEL_VERSION) || !defined(BOOST_INTEL_LINUX)
+    (defined(HPX_INTEL_VERSION) || !defined(BOOST_INTEL_LINUX))
 # define HPX_ASSERT(expr) ((expr) ? (void)0 : __builtin_unreachable())
 #else
 # define HPX_ASSERT(expr) ((void)0)
@@ -61,7 +61,7 @@ namespace hpx
 #if defined(HPX_DISABLE_ASSERTS) || defined(BOOST_DISABLE_ASSERTS) || defined(NDEBUG)
 
 #if defined(HPX_GCC_VERSION) || defined(HPX_CLANG_VERSION) || \
-    (defined(HPX_INTEL_VERSION) || !defined(BOOST_INTEL_LINUX)
+    (defined(HPX_INTEL_VERSION) || !defined(BOOST_INTEL_LINUX))
 # define HPX_ASSERT_MSG(expr, msg) ((expr) ? (void)0 : __builtin_unreachable())
 #else
 # define HPX_ASSERT_MSG(expr, msg) ((void)0)
