88e2e11f64d67482c202f1fa388d318f49fe893d
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-06-21 15:34:53 -0500

    Using correct API function to determine number of threads used.

--- src/runtime/threads/executors/generic_thread_pool_executor.cpp
+++ src/runtime/threads/executors/generic_thread_pool_executor.cpp
@@ -72,7 +72,7 @@ namespace hpx { namespace threads { namespace executors { namespace detail
         case threads::detail::min_concurrency:
         case threads::detail::max_concurrency:
         case threads::detail::current_concurrency:
-            return hpx::get_num_worker_threads();
+            return hpx::get_os_thread_count();
 
         default:
             break;
