35a9ce3464168c0b3c4ad832a6d817f23bba8e6d
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-06-27 14:21:30 -0500

    Fixed silly include guard names

--- hpx/parallel/algorithm.hpp
+++ hpx/parallel/algorithm.hpp
@@ -4,8 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_PARALLEL_PARALLEL_ALGORITHM_MAY_28_2014_0522PM)
-#define HPX_PARALLEL_PARALLEL_ALGORITHM_MAY_28_2014_0522PM
+#if !defined(HPX_PARALLEL_ALGORITHM_MAY_28_2014_0522PM)
+#define HPX_PARALLEL_ALGORITHM_MAY_28_2014_0522PM
 
 #include <hpx/hpx_fwd.hpp>
 
