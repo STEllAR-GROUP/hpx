8d2f043da1f563522af14ebe4963183f2f08b170
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-06-29 17:44:57 -0500

    Simplify timing routine

--- tests/performance/local/worker_timed.hpp
+++ tests/performance/local/worker_timed.hpp
@@ -1,5 +1,5 @@
 //  Copyright (c) 2011-2014 Bryce Adelstein-Lelbach
-//  Copyright (c) 2007-2012 Hartmut Kaiser
+//  Copyright (c) 2007-2014 Hartmut Kaiser
 //  Copyright (c)      2013 Thomas Heller
 //  Copyright (c)      2013 Patricia Grubel
 //
@@ -9,26 +9,20 @@
 #ifndef HPX_TESTS_PERFORMANCE_WORKER_HPP
 #define HPX_TESTS_PERFORMANCE_WORKER_HPP
 
-//#include <hpx/config.hpp>
-#include <hpx/util/hardware/timestamp.hpp>
 #include <hpx/util/high_resolution_clock.hpp>
 #include <boost/cstdint.hpp>
 
-inline void worker_timed(
-    boost::uint64_t delay_ns
-    )
+inline void worker_timed(boost::uint64_t delay_ns)
 {
     if (delay_ns == 0)
         return;
 
-    using namespace hpx::util;
-
-    boost::uint64_t start = high_resolution_clock::now();
+    boost::uint64_t start = hpx::util::high_resolution_clock::now();
 
     while (true)
     {
         // Check if we've reached the specified delay.
-        if ((high_resolution_clock::now() - start) >= delay_ns)
+        if ((hpx::util::high_resolution_clock::now() - start) >= delay_ns)
             break;
     }
 }
