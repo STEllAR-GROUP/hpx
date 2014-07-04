7a4156df7d19d476c9368548b5e90d4721c3ecdb
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-07-03 21:00:51 -0500

    Adding parallel algorithms section to docs

--- hpx/parallel/detail/reduce.hpp
+++ hpx/parallel/detail/reduce.hpp
@@ -3,7 +3,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-/// \file
+/// \file parallel/reduce.hpp
 
 #if !defined(HPX_PARALLEL_DETAILREUCE_JUN_01_2014_0903AM)
 #define HPX_PARALLEL_DETAILREUCE_JUN_01_2014_0903AM
