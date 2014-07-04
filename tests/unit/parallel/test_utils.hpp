8bea4fefce4091f2bb4b00fdf58f01b2c1a319e9
K-ballo <k@fusionfenix.com>
2014-07-04 12:45:52 -0300

    Fixed annoying comparison between signed and unsigned warning

--- tests/unit/parallel/test_utils.hpp
+++ tests/unit/parallel/test_utils.hpp
@@ -79,7 +79,7 @@ namespace test
         static void call(hpx::parallel::sequential_execution_policy const&,
             hpx::exception_list const& e)
         {
-            HPX_TEST_EQ(e.size(), 1);
+            HPX_TEST_EQ(e.size(), 1u);
         }
     };
 
@@ -88,7 +88,7 @@ namespace test
     {
         static void call(ExPolicy const&, hpx::exception_list const& e)
         {
-            HPX_TEST_EQ(e.size(), 1);
+            HPX_TEST_EQ(e.size(), 1u);
         }
     };
 
@@ -99,7 +99,7 @@ namespace test
         static void call(hpx::parallel::sequential_execution_policy const&,
             hpx::exception_list const& e)
         {
-            HPX_TEST_EQ(e.size(), 1);
+            HPX_TEST_EQ(e.size(), 1u);
         }
     };
 
@@ -112,7 +112,7 @@ namespace test
             using namespace hpx::parallel::v1::detail;
 
             if (which(policy) == static_cast<int>(execution_policy_enum::sequential)) {
-                HPX_TEST_EQ(e.size(), 1);
+                HPX_TEST_EQ(e.size(), 1u);
             }
             else {
                 // The static partitioner uses the number of threads/cores for
@@ -129,7 +129,7 @@ namespace test
         static void call(hpx::parallel::execution_policy const&,
             hpx::exception_list const& e)
         {
-            HPX_TEST_EQ(e.size(), 1);
+            HPX_TEST_EQ(e.size(), 1u);
         }
     };
 
