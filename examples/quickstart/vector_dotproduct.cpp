ac8f869762d72b03303dbab758c176294f38bda8
Grant Mercer <Gmercer015@gmail.com>
2014-07-08 09:58:06 -0500

    changed std::cout to hpx::cout in vector_dotproduct

--- examples/quickstart/vector_dotproduct.cpp
+++ examples/quickstart/vector_dotproduct.cpp
@@ -7,6 +7,7 @@
 #include <hpx/hpx.hpp>
 #include <hpx/include/numeric.hpp>
 #include <hpx/include/algorithm.hpp>
+#include <hpx/include/iostreams.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main()
@@ -17,31 +18,28 @@ int hpx_main()
     std::fill(boost::begin(xvalues), boost::end(xvalues), 1.0);
     std::fill(boost::begin(yvalues), boost::end(yvalues), 1.0);
 
-    typedef std::vector<double>::iterator iterator_type;
-
     using hpx::util::make_zip_iterator;
     using hpx::util::tuple;
     using hpx::util::make_tuple;
     using hpx::util::get;
 
-    // zip the xvalues and yvalues together
-    typedef hpx::util::zip_iterator<iterator_type, iterator_type> zip_iterator;
-    typedef zip_iterator::reference reference;
-
     // the result of the execution will be stored in location 0 of the tuple
     double result =
-        hpx::parallel::reduce(hpx::parallel::par,
+        hpx::parallel::reduce(
+            hpx::parallel::par,
             make_zip_iterator(boost::begin(xvalues), boost::begin(yvalues)),
             make_zip_iterator(boost::end(xvalues), boost::end(yvalues)),
             0.0,
             [](double const& res, double const& g) {
-                return res+ g;
-            }, [](tuple<double,double> const& r){
-                    return get<0>(r) * get<1>(r);           
-        });
+                return res + g;
+            },
+            [](tuple<double,double> const& r){
+                    return get<0>(r) * get<1>(r);
+            }
+        );
 
     // print the result
-    std::cout << result;
+    hpx::cout << result << hpx::endl;
 
     return hpx::finalize();
 }
