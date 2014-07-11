a6b1c45c5eceb06c63ed67fa37ae1993b19f0de6
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-07-11 14:46:06 -0500

    Adding assert verifying that futures are ready when serialized

--- hpx/lcos/future.hpp
+++ hpx/lcos/future.hpp
@@ -223,8 +223,11 @@ namespace hpx { namespace lcos { namespace detail
     {
         typedef typename traits::future_traits<Future>::type value_type;
 
-        if (f.valid())
+        if(f.valid())
+        {
+            HPX_ASSERT(f.is_ready());
             f.wait();
+        }
 
         int state = future_state::invalid;
         if (f.has_value())
@@ -247,8 +250,11 @@ namespace hpx { namespace lcos { namespace detail
         boost::is_void<typename traits::future_traits<Future>::type>
     >::type serialize_future_save(Archive& ar, Future const& f)
     {
-        if (f.valid())
+        if(f.valid())
+        {
+            HPX_ASSERT(f.is_ready());
             f.wait();
+        }
 
         int state = future_state::invalid;
         if (f.has_value())
