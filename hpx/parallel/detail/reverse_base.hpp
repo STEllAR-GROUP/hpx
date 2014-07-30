0454800e739ed6e22499c8ff91d453c3002d1a5c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-07-29 19:20:03 -0500

    Adding missing license, etc.

--- hpx/parallel/detail/reverse_base.hpp
+++ hpx/parallel/detail/reverse_base.hpp
@@ -1,19 +1,36 @@
-        template <typename ReverseIter>
-        typename ReverseIter::iterator_type
-        reverse_base(ReverseIter && it)
-        {
-            return it.base();
-        }
+//  Copyright (c) 2007-2014 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-        template <typename ReverseIter>
-        hpx::future<typename ReverseIter::iterator_type>
-        reverse_base(hpx::future<ReverseIter> && it)
-        {
-            return
-                it.then(
-                    [](hpx::future<ReverseIter> && it)
-                    {
-                        return it.get().base();
-                    });
-        }
+#if !defined(HPX_PARALLEL_REVERSE_BASE_JUL_29_2014_0718PM)
+#define HPX_PARALLEL_REVERSE_BASE_JUL_29_2014_0718PM
 
+#include <hpx/hpx_fwd.hpp>
+#include <hpx/lcos/future.hpp>
+
+#include <iterator>
+
+namespace hpx { namespace parallel { HPX_INLINE_NAMESPACE(v1) { namespace detail
+{
+    template <typename ReverseIter>
+    typename ReverseIter::iterator_type
+    reverse_base(ReverseIter && it)
+    {
+        return it.base();
+    }
+
+    template <typename ReverseIter>
+    hpx::future<typename ReverseIter::iterator_type>
+    reverse_base(hpx::future<ReverseIter> && it)
+    {
+        return
+            it.then(
+                [](hpx::future<ReverseIter> && it)
+                {
+                    return it.get().base();
+                });
+    }
+}}}}
+
+#endif
