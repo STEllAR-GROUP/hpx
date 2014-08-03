687218a7b7502297a1f551c2f9fd2771ddaa2ee5
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-08-02 21:02:31 -0500

    Fixing receife_buffer to hold on to the promise until the value has been set. Added specialization for receive_buffer<void>

--- hpx/lcos/local/receive_buffer.hpp
+++ hpx/lcos/local/receive_buffer.hpp
@@ -27,11 +27,39 @@ namespace hpx { namespace lcos { namespace local
     {
     protected:
         typedef Mutex mutex_type;
+        typedef hpx::lcos::local::promise<T> buffer_promise_type;
 
-        typedef T buffer_type;
-        typedef hpx::lcos::local::promise<buffer_type> buffer_promise_type;
+        struct entry_data
+        {
+        private:
+            HPX_MOVABLE_BUT_NOT_COPYABLE(entry_data)
 
-        typedef std::map<std::size_t, buffer_promise_type> buffer_map_type;
+        public:
+            entry_data()
+              : can_be_deleted_(false)
+            {}
+
+            entry_data(entry_data && rhs)
+              : promise_(std::move(rhs.promise_)),
+                can_be_deleted_(rhs.can_be_deleted_)
+            {}
+
+            hpx::future<T> get_future()
+            {
+                return promise_.get_future();
+            }
+
+            template <typename Val>
+            void set_value(Val && val)
+            {
+                return promise_.set_value(std::forward<Val>(val));
+            }
+
+            buffer_promise_type promise_;
+            bool can_be_deleted_;
+        };
+
+        typedef std::map<std::size_t, entry_data> buffer_map_type;
         typedef typename buffer_map_type::iterator iterator;
 
     private:
@@ -45,7 +73,7 @@ namespace hpx { namespace lcos { namespace local
             iterator end = buffer_map_.end();
             for (iterator it = buffer_map_.begin(); it != end; ++it)
             {
-                if ((*it).second.valid())
+                if ((*it).second.promise_.valid())
                     (*it).second.get_future();
             }
         }
@@ -83,7 +111,16 @@ namespace hpx { namespace lcos { namespace local
             iterator it = get_buffer_entry(step);
             HPX_ASSERT(it != buffer_map_.end());
 
-            erase_on_exit t(buffer_map_, it);
+            // if the value was already set we delete the entry after
+            // retrieving the future
+            if (it->second.can_be_deleted_)
+            {
+                erase_on_exit t(buffer_map_, it);
+                return it->second.get_future();
+            }
+
+            // otherwise mark the entry as to be deleted once the value was set
+            it->second.can_be_deleted_ = true;
             return it->second.get_future();
         }
 
@@ -92,7 +129,19 @@ namespace hpx { namespace lcos { namespace local
             iterator it = get_buffer_entry(step);
             HPX_ASSERT(it != buffer_map_.end());
 
+            // set value in promise
             it->second.set_value(std::move(val));
+
+            if (!it->second.can_be_deleted_)
+            {
+                // if the future was not retrieved yet mark the entry as to be
+                // deleted after it was be retrieved
+                it->second.can_be_deleted_ = true;
+                return;
+            }
+
+            // if the future was already retrieved we can delete the entry
+            buffer_map_.erase(it);
         }
 
         iterator get_buffer_entry(std::size_t step)
@@ -101,7 +150,152 @@ namespace hpx { namespace lcos { namespace local
             if (it == buffer_map_.end())
             {
                 std::pair<iterator, bool> res =
-                    buffer_map_.insert(std::make_pair(step, buffer_promise_type()));
+                    buffer_map_.insert(std::make_pair(step, entry_data()));
+                if (!res.second)
+                {
+                    HPX_THROW_EXCEPTION(invalid_status,
+                        "base_receive_buffer::get_buffer_entry",
+                        "couldn't insert a new entry into the receive buffer");
+                }
+                return res.first;
+            }
+            return it;
+        }
+
+    private:
+        mutable mutex_type mtx_;
+        buffer_map_type buffer_map_;
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+    template <typename Mutex>
+    struct receive_buffer<void, Mutex>
+    {
+    protected:
+        typedef Mutex mutex_type;
+        typedef hpx::lcos::local::promise<void> buffer_promise_type;
+
+        struct entry_data
+        {
+        private:
+            HPX_MOVABLE_BUT_NOT_COPYABLE(entry_data)
+
+        public:
+            entry_data()
+              : can_be_deleted_(false)
+            {}
+
+            entry_data(entry_data && rhs)
+              : promise_(std::move(rhs.promise_)),
+                can_be_deleted_(rhs.can_be_deleted_)
+            {}
+
+            hpx::future<void> get_future()
+            {
+                return promise_.get_future();
+            }
+
+            void set_value()
+            {
+                return promise_.set_value();
+            }
+
+            buffer_promise_type promise_;
+            bool can_be_deleted_;
+        };
+
+        typedef std::map<std::size_t, entry_data> buffer_map_type;
+        typedef typename buffer_map_type::iterator iterator;
+
+    private:
+        HPX_MOVABLE_BUT_NOT_COPYABLE(receive_buffer)
+
+    public:
+        receive_buffer() {}
+
+        ~receive_buffer()
+        {
+            iterator end = buffer_map_.end();
+            for (iterator it = buffer_map_.begin(); it != end; ++it)
+            {
+                if ((*it).second.promise_.valid())
+                    (*it).second.get_future();
+            }
+        }
+
+        hpx::future<void> receive(std::size_t step)
+        {
+            typename mutex_type::scoped_lock l(mtx_);
+            return receive_locked(step);
+        }
+
+        void store_received(std::size_t step)
+        {
+            typename mutex_type::scoped_lock l(mtx_);
+            return store_received_locked(step);
+        }
+
+    protected:
+        struct erase_on_exit
+        {
+            erase_on_exit(buffer_map_type& buffer_map, iterator it)
+              : buffer_map_(buffer_map), it_(it)
+            {}
+
+            ~erase_on_exit()
+            {
+                buffer_map_.erase(it_);
+            }
+
+            buffer_map_type& buffer_map_;
+            iterator it_;
+        };
+
+        hpx::future<void> receive_locked(std::size_t step)
+        {
+            iterator it = get_buffer_entry(step);
+            HPX_ASSERT(it != buffer_map_.end());
+
+            // if the value was already set we delete the entry after
+            // retrieving the future
+            if (it->second.can_be_deleted_)
+            {
+                erase_on_exit t(buffer_map_, it);
+                return it->second.get_future();
+            }
+
+            // otherwise mark the entry as to be deleted once the value was set
+            it->second.can_be_deleted_ = true;
+            return it->second.get_future();
+        }
+
+        void store_received_locked(std::size_t step)
+        {
+            iterator it = get_buffer_entry(step);
+            HPX_ASSERT(it != buffer_map_.end());
+
+            // set value in promise
+            it->second.set_value();
+
+            if (!it->second.can_be_deleted_)
+            {
+                // if the future was not retrieved yet mark the entry as to be
+                // deleted after it was be retrieved
+                it->second.can_be_deleted_ = true;
+                return;
+            }
+
+            // if the future was already retrieved we can delete the entry
+            buffer_map_.erase(it);
+        }
+
+        iterator get_buffer_entry(std::size_t step)
+        {
+            iterator it = buffer_map_.find(step);
+            if (it == buffer_map_.end())
+            {
+                std::pair<iterator, bool> res =
+                    buffer_map_.insert(std::make_pair(step, entry_data()));
                 if (!res.second)
                 {
                     HPX_THROW_EXCEPTION(invalid_status,
