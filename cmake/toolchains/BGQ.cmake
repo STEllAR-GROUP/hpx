71e47ad092075834fb58144c59a9bea6bdfdac81
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-08-05 12:25:53 -0500

    Cosmetics for docs generation

--- cmake/toolchains/BGQ.cmake
+++ cmake/toolchains/BGQ.cmake
@@ -2,11 +2,12 @@
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
+#
 # This is the default toolchain file to be used with CNK on a BlueGene/Q. It sets
 # the appropriate compile flags and compiler such that HPX will compile.
 # Note that you still need to provide Boost, hwloc and other utility libraries
 # like a custom allocator yourself.
+#
 
 set(CMAKE_SYSTEM_NAME Linux)
 
