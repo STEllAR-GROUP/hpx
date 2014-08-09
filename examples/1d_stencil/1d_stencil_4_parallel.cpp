0b2f67a26b97d62bace8043b12d790561ee5fd7c
Patricia Grubel <pagrubel@nmsu.edu>
2014-08-09 13:48:05 -0500

    fixed no-header option

--- examples/1d_stencil/1d_stencil_4_parallel.cpp
+++ examples/1d_stencil/1d_stencil_4_parallel.cpp
@@ -158,6 +158,9 @@ int hpx_main(boost::program_options::variables_map& vm)
     boost::uint64_t nx = vm["nx"].as<boost::uint64_t>();   // Number of grid points.
     boost::uint64_t nt = vm["nt"].as<boost::uint64_t>();   // Number of steps.
 
+    if (vm.count("no-header))
+        header = false;
+    
     // Create the stepper object
     stepper step;
 
