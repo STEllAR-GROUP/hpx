4fdd72e10f9f9813b43db59d3d91efeb73948921
Thomas Heller <thom.heller@gmail.com>
2014-09-03 05:37:50 -0500

    Fixing doc creation

--- cmake/HPX_UpdateGitDocs.cmake
+++ cmake/HPX_UpdateGitDocs.cmake
@@ -9,6 +9,10 @@ if(NOT GIT_FOUND)
   message(FATAL_ERROR "Could not find git. git is needed to download and update the GitHub pages.")
 endif()
 
+if(NOT GIT_REPOSITORY)
+  set(GIT_REPOSITORY git@github.com:STEllAR-GROUP/hpx.git --branch gh-pages)
+endif()
+
 if(EXISTS "${CMAKE_BINARY_DIR}/gh-pages")
   execute_process(
     COMMAND "${GIT_EXECUTABLE}" pull --rebase
@@ -18,8 +22,9 @@ if(EXISTS "${CMAKE_BINARY_DIR}/gh-pages")
     message(FATAL_ERROR "Updating the GitHub pages branch failed.")
   endif()
 else()
+  message("${GIT_EXECUTABLE}")
   execute_process(
-    COMMAND "${GIT_EXECUTABLE} clone git@github.com:STEllAR-GROUP/hpx.git --branch gh-pages gh-pages"
+    COMMAND "${GIT_EXECUTABLE}" clone ${GIT_REPOSITORY} gh-pages
     RESULT_VARIABLE git_clone_result)
   if(NOT "${git_clone_result}" EQUAL "0")
     message(FATAL_ERROR "Cloning the GitHub pages branch failed. Trying to clone ${GIT_REPOSITORY}")
