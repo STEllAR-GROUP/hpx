b18334635bc82450fbbfbcaa93b232a72683dc1d
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-09-12 07:51:35 -0500

    Cleaning up hpx_prefix

--- src/hpx_init.cpp
+++ src/hpx_init.cpp
@@ -1112,7 +1112,7 @@ namespace hpx
         startup_function_type const& startup,
         shutdown_function_type const& shutdown, hpx::runtime_mode mode)
     {
-        HPX_ASSERT(util::hpx_prefix(0));
+        HPX_ASSERT(util::hpx_prefix());
         return run_or_start(f, desc_cmdline, argc, argv, ini_config,
             startup, shutdown, mode, true);
     }
@@ -1124,6 +1124,7 @@ namespace hpx
         startup_function_type const& startup,
         shutdown_function_type const& shutdown, hpx::runtime_mode mode)
     {
+        HPX_ASSERT(util::hpx_prefix());
         return 0 == run_or_start(f, desc_cmdline, argc, argv, ini_config,
             startup, shutdown, mode, false);
     }
