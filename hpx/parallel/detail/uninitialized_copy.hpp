8b6a10b5e11f32d3dbdf4b6bad23f1b9a0b872bf
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-10-06 10:24:18 -0500

    More include file renaming aftermath (cherry picked from commit 9a26d8e24781c4f4df1eb0b6afb7fc04669abafc)
    Conflicts:
    	tests/performance/local/transform_reduce_scaling.cpp

--- hpx/parallel/detail/uninitialized_copy.hpp
+++ hpx/parallel/detail/uninitialized_copy.hpp
@@ -1,7 +1,7 @@
 //  Copyright (c) 2014 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or uninitialized_copy at http://www.boost.org/LICENSE_1_0.txt)
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 /// \file parallel/detail/uninitialized_copy.hpp
 
