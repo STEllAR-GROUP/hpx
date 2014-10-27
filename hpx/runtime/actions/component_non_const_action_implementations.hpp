06567b6adef9c25c8ee0a949e2729306eb240155
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-10-27 09:56:31 -0500

    Adding checks to ensure that component actions are invoke on non-locality ids only. This addresses #1297: Unable to invoke component actions recursively

--- hpx/runtime/actions/component_non_const_action_implementations.hpp
+++ hpx/runtime/actions/component_non_const_action_implementations.hpp
@@ -74,6 +74,12 @@ namespace hpx { namespace actions
         typedef action<Component, result_type, arguments_type, Derived>
             base_type;
 
+        // Only non-localities are valid targets for a component action
+        static bool is_target_valid(naming::id_type const& id)
+        {
+            return !naming::is_locality(id);
+        }
+
     protected:
         /// The \a thread_function will be registered as the thread
         /// function of a thread. It encapsulates the execution of the
@@ -279,6 +285,12 @@ namespace hpx { namespace actions
             BOOST_PP_REPEAT(N, HPX_REMOVE_QUALIFIERS, _)> arguments_type;
         typedef action<Component, result_type, arguments_type, Derived>
             base_type;
+
+        // Only non-localities are valid targets for a component action
+        static bool is_target_valid(naming::id_type const& id)
+        {
+            return !naming::is_locality(id);
+        }
 
     protected:
         /// The \a thread_function will be registered as the thread
