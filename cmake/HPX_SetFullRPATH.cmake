4454a57fd5c2067cf727c05826b460a45f4a6e50
Thomas Heller <thom.heller@gmail.com>
2014-10-30 08:51:42 -0500

    Fixing #1247: Dynamic linking error

---
+++ cmake/HPX_SetFullRPATH.cmake
@@ -0,0 +1,14 @@
+# Copyright (c) 2011-2014 Thomas Heller
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+if (NOT DEFINED LIB)
+  set(LIB "lib")
+endif(NOT DEFINED LIB)
+
+list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/${LIB}" isSystemDir)
+if(isSystemDir EQUAL -1)
+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB}")
+endif()
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH On)
