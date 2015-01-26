429a9b91ed2bb6116ee13179c16e39c9936aa747
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-01-26 14:22:02 -0600

    Fixing an issue with executors

--- hpx/runtime/threads/detail/scheduling_loop.hpp
+++ hpx/runtime/threads/detail/scheduling_loop.hpp
@@ -405,6 +405,10 @@ namespace hpx { namespace threads { namespace detail
                 {
                     hpx::agas::garbage_collect_non_blocking();
                 }
+
+                // call back into invoking context
+                if (!cb.empty())
+                    cb();
             }
 
             if (busy_loop_count > HPX_BUSY_LOOP_COUNT_MAX)
