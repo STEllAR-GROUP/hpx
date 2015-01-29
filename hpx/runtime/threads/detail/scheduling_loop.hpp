86ad17988761ef79d21165dff388696f9d50fd65
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-01-29 12:28:28 -0600

    Re-apply the fix for executors, just correctly this time

--- hpx/runtime/threads/detail/scheduling_loop.hpp
+++ hpx/runtime/threads/detail/scheduling_loop.hpp
@@ -253,7 +253,8 @@ namespace hpx { namespace threads { namespace detail
         boost::atomic<hpx::state>& global_state, boost::int64_t& executed_threads,
         boost::int64_t& executed_thread_phases, boost::uint64_t& tfunc_time,
         boost::uint64_t& exec_time,
-        util::function_nonser<void()> const& cb = util::function_nonser<void()>())
+        util::function_nonser<void()> const& cb_outer = util::function_nonser<void()>(),
+        util::function_nonser<void()> const& cb_inner = util::function_nonser<void()>())
     {
         util::itt::stack_context ctx;        // helper for itt support
         util::itt::domain domain(get_thread_name().data());
@@ -405,6 +406,10 @@ namespace hpx { namespace threads { namespace detail
                 {
                     hpx::agas::garbage_collect_non_blocking();
                 }
+
+                // call back into invoking context
+                if (!cb_inner.empty())
+                    cb_inner();
             }
 
             if (busy_loop_count > HPX_BUSY_LOOP_COUNT_MAX)
@@ -421,8 +426,8 @@ namespace hpx { namespace threads { namespace detail
             else if (idle_loop_count > HPX_IDLE_LOOP_COUNT_MAX)
             {
                 // call back into invoking context
-                if (!cb.empty())
-                    cb();
+                if (!cb_outer.empty())
+                    cb_outer();
 
                 // clean up terminated threads
                 idle_loop_count = 0;
