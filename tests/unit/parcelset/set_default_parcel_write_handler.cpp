f27ade020be1ddbcf19aee616ce442abc0eeeb6b
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-02-25 20:42:58 -0600

    Adding test

---
+++ tests/unit/parcelset/set_default_parcel_write_handler.cpp
@@ -0,0 +1,13 @@
+//  Copyright (c) 2015 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/hpx.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+int main()
+{
+    return hpx::util::report_errors();
+}
