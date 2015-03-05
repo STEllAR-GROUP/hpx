1016488b28444d0fed61befa24320eac1faab759
Thomas Heller <thom.heller@gmail.com>
2015-03-05 07:03:34 -0600

    Fixing Intel MPI include order problems

--- plugins/parcelport/mpi/parcelport_mpi.cpp
+++ plugins/parcelport/mpi/parcelport_mpi.cpp
@@ -4,6 +4,12 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/config/defines.hpp>
+
+#if defined(HPX_PARCELPORT_MPI)
+#include <mpi.h>
+#endif
+
 #include <hpx/hpx_fwd.hpp>
 
 #include <hpx/plugins/parcelport/mpi/mpi_environment.hpp>
