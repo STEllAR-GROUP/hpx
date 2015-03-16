17b2f0c4d88a939359ee6d521694fd6ba00d72c4
John Biddiscombe <biddisco@cscs.ch>
2015-03-16 15:08:30 +0100

    Style: fix indentation

--- tests/unit/parallel/exclusive_scan.cpp
+++ tests/unit/parallel/exclusive_scan.cpp
@@ -400,117 +400,117 @@ void exclusive_scan_bad_alloc_test()
 
 // n'th value of sum of 1+2+3+...
 int check_n_triangle(int n) {
-  return n<0 ? 0 : (n)*(n+1)/2;
+    return n<0 ? 0 : (n)*(n+1)/2;
 }
 
 // n'th value of sum of x+x+x+...
 int check_n_const(int n, int x) {
-  return n<0 ? 0 : n*x;
+    return n<0 ? 0 : n*x;
 }
 
 // run scan algorithm, validate that output array hold expected answers.
 template <typename ExPolicy>
 void test_exclusive_scan_validate(ExPolicy const& p, std::vector<int> &a, std::vector<int> &b)
 {
-  using namespace hpx::parallel;
-  typedef std::vector<int>::iterator Iter;
+    using namespace hpx::parallel;
+    typedef std::vector<int>::iterator Iter;
 
-  // test 1, fill array with numbers counting from 0, then run scan algorithm
-  a.clear();
-  std::copy(boost::counting_iterator<int>(0), boost::counting_iterator<int>(ARRAY_SIZE), std::back_inserter(a));
+    // test 1, fill array with numbers counting from 0, then run scan algorithm
+    a.clear();
+    std::copy(boost::counting_iterator<int>(0), boost::counting_iterator<int>(ARRAY_SIZE), std::back_inserter(a));
 #ifdef DUMP_VALUES
-  std::cout << "\nValidating counting from 0 " << "\nInput : ";
-  std::copy(a.begin(), a.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << " ... ";
-  std::copy(a.end()-DISPLAY, a.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << "\nValidating counting from 0 " << "\nInput : ";
+    std::copy(a.begin(), a.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << " ... ";
+    std::copy(a.end()-DISPLAY, a.end(), std::ostream_iterator<int>(std::cout, ", "));
 #endif
-  b.resize(a.size());
-  hpx::parallel::exclusive_scan(p, a.begin(), a.end(), b.begin(), INITIAL_VAL,
-                                [](int bar, int baz){ return bar+baz; });
+    b.resize(a.size());
+    hpx::parallel::exclusive_scan(p, a.begin(), a.end(), b.begin(), INITIAL_VAL,
+                                  [](int bar, int baz){ return bar+baz; });
 #ifdef DUMP_VALUES
-  std::cout << "\nOutput : ";
-  std::copy(b.begin(), b.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << " ... ";
-  std::copy(b.end()-DISPLAY, b.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << "\nOutput : ";
+    std::copy(b.begin(), b.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << " ... ";
+    std::copy(b.end()-DISPLAY, b.end(), std::ostream_iterator<int>(std::cout, ", "));
 #endif
-  //
-  for (std::size_t i=0; i<b.size(); ++i) {
-    // counting from zero,
-    int value = b[i];
-    int expected_value  = INITIAL_VAL + check_n_triangle(i-1);
-    if (!HPX_TEST(value == expected_value)) break;
-  }
+    //
+    for (std::size_t i=0; i<b.size(); ++i) {
+        // counting from zero,
+        int value = b[i];
+        int expected_value  = INITIAL_VAL + check_n_triangle(i-1);
+        if (!HPX_TEST(value == expected_value)) break;
+    }
 
-  // test 2, fill array with numbers counting from 1, then run scan algorithm
-  a.clear();
-  std::copy(boost::counting_iterator<int>(1), boost::counting_iterator<int>(ARRAY_SIZE), std::back_inserter(a));
-#ifdef DUMP_VALUES
-  std::cout << "\nValidating counting from 1 " << "\nInput : ";
-  std::copy(a.begin(), a.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << " ... ";
-  std::copy(a.end()-DISPLAY, a.end(), std::ostream_iterator<int>(std::cout, ", "));
-#endif
-  b.resize(a.size());
-  hpx::parallel::exclusive_scan(p, a.begin(), a.end(), b.begin(), INITIAL_VAL,
-                                [](int bar, int baz){ return bar+baz; });
-#ifdef DUMP_VALUES
-  std::cout << "\nOutput : ";
-  std::copy(b.begin(), b.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << " ... ";
-  std::copy(b.end()-DISPLAY, b.end(), std::ostream_iterator<int>(std::cout, ", "));
-#endif
-  //
-  for (std::size_t i=0; i<b.size(); ++i) {
-    // counting from 1, use i+1
-    int value = b[i];
-    int expected_value  = INITIAL_VAL + check_n_triangle(i);
-    if (!HPX_TEST(value == expected_value)) break;
-  }
+    // test 2, fill array with numbers counting from 1, then run scan algorithm
+    a.clear();
+    std::copy(boost::counting_iterator<int>(1), boost::counting_iterator<int>(ARRAY_SIZE), std::back_inserter(a));
+  #ifdef DUMP_VALUES
+    std::cout << "\nValidating counting from 1 " << "\nInput : ";
+    std::copy(a.begin(), a.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << " ... ";
+    std::copy(a.end()-DISPLAY, a.end(), std::ostream_iterator<int>(std::cout, ", "));
+  #endif
+    b.resize(a.size());
+    hpx::parallel::exclusive_scan(p, a.begin(), a.end(), b.begin(), INITIAL_VAL,
+                                  [](int bar, int baz){ return bar+baz; });
+  #ifdef DUMP_VALUES
+    std::cout << "\nOutput : ";
+    std::copy(b.begin(), b.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << " ... ";
+    std::copy(b.end()-DISPLAY, b.end(), std::ostream_iterator<int>(std::cout, ", "));
+  #endif
+    //
+    for (std::size_t i=0; i<b.size(); ++i) {
+        // counting from 1, use i+1
+        int value = b[i];
+        int expected_value  = INITIAL_VAL + check_n_triangle(i);
+        if (!HPX_TEST(value == expected_value)) break;
+    }
 
-  // test 3, fill array with constant
-  a.clear();
-  std::fill_n(std::back_inserter(a), ARRAY_SIZE, FILL_VALUE);
-#ifdef DUMP_VALUES
-  std::cout << "\nValidating constant values " << "\nInput : ";
-  std::copy(a.begin(), a.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << " ... ";
-  std::copy(a.end()-DISPLAY, a.end(), std::ostream_iterator<int>(std::cout, ", "));
-#endif
-  b.resize(a.size());
-  hpx::parallel::exclusive_scan(p, a.begin(), a.end(), b.begin(), INITIAL_VAL,
-                                [](int bar, int baz){ return bar+baz; });
-#ifdef DUMP_VALUES
-  std::cout << "\nOutput : ";
-  std::copy(b.begin(), b.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << " ... ";
-  std::copy(b.end()-DISPLAY, b.end(), std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << std::endl;
-#endif
-  //
-  for (std::size_t i=0; i<b.size(); ++i) {
-    // counting from zero,
-    int value = b[i];
-    int expected_value  = INITIAL_VAL + check_n_const(i, FILL_VALUE);
-    if (!HPX_TEST(value == expected_value)) break;
-  }
+    // test 3, fill array with constant
+    a.clear();
+    std::fill_n(std::back_inserter(a), ARRAY_SIZE, FILL_VALUE);
+  #ifdef DUMP_VALUES
+    std::cout << "\nValidating constant values " << "\nInput : ";
+    std::copy(a.begin(), a.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << " ... ";
+    std::copy(a.end()-DISPLAY, a.end(), std::ostream_iterator<int>(std::cout, ", "));
+  #endif
+    b.resize(a.size());
+    hpx::parallel::exclusive_scan(p, a.begin(), a.end(), b.begin(), INITIAL_VAL,
+                                  [](int bar, int baz){ return bar+baz; });
+  #ifdef DUMP_VALUES
+    std::cout << "\nOutput : ";
+    std::copy(b.begin(), b.begin()+DISPLAY, std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << " ... ";
+    std::copy(b.end()-DISPLAY, b.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+  #endif
+    //
+    for (std::size_t i=0; i<b.size(); ++i) {
+        // counting from zero,
+        int value = b[i];
+        int expected_value  = INITIAL_VAL + check_n_const(i, FILL_VALUE);
+        if (!HPX_TEST(value == expected_value)) break;
+    }
 }
 
 void exclusive_scan_validate()
 {
-  std::vector<int> a, b;
-  // test scan algorithms using separate array for output
-  DEBUG_OUT("\nValidating separate arrays sequential");
-  test_exclusive_scan_validate(hpx::parallel::seq, a, b);
+    std::vector<int> a, b;
+    // test scan algorithms using separate array for output
+    DEBUG_OUT("\nValidating separate arrays sequential");
+    test_exclusive_scan_validate(hpx::parallel::seq, a, b);
 
-  DEBUG_OUT("\nValidating separate arrays parallel");
-  test_exclusive_scan_validate(hpx::parallel::par, a, b);
+    DEBUG_OUT("\nValidating separate arrays parallel");
+    test_exclusive_scan_validate(hpx::parallel::par, a, b);
 
-  // test scan algorithms using same array for input and output
-  DEBUG_OUT("\nValidating in_place arrays sequential ");
-  test_exclusive_scan_validate(hpx::parallel::seq, a, a);
+    // test scan algorithms using same array for input and output
+    DEBUG_OUT("\nValidating in_place arrays sequential ");
+    test_exclusive_scan_validate(hpx::parallel::seq, a, a);
 
-  DEBUG_OUT("\nValidating in_place arrays parallel ");
-  test_exclusive_scan_validate(hpx::parallel::par, a, a);
+    DEBUG_OUT("\nValidating in_place arrays parallel ");
+    test_exclusive_scan_validate(hpx::parallel::par, a, a);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
