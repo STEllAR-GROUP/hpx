f2fca00caaf1daf92c5cbdbb81275a109dd39f5f
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-03-24 17:19:23 -0500

    Fix copyright - sorry Martin...

--- tests/regressions/threads/threads_all_1422.cpp
+++ tests/regressions/threads/threads_all_1422.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2011-2013 Bryce Adelstein-Lelbach
+//  Copyright (c) 2015 Martin Stumpf
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
