ea27900547acfdeb8f4ab4a3ffb8e08fcd57572f
John Biddiscombe <biddisco@cscs.ch>
2015-04-11 08:35:15 +0200

    Wrapper class to takes an rdma buffer and pass it to the parcel decode function with zero copy

--- plugins/parcelport/verbs/pointer_wrapper_vector.hpp
+++ plugins/parcelport/verbs/pointer_wrapper_vector.hpp
@@ -12,7 +12,10 @@
 
 namespace hpx { namespace util { namespace detail
 {
-
+// this class looks like a vector, but can be initialized from a pointer and size,
+// it is used by the verbs parcelport to pass an rdma memory chunk with received
+// data into the decode parcel buffer routines.
+// it cannot be resized or changed once created and does not delete the memory it wraps
 template<class T>
 class pointer_wrapper_vector
 {
@@ -20,15 +23,14 @@ class pointer_wrapper_vector
     T  *m_array_;
     int m_size_;
 
-    typedef T                               value_type;
-    typedef value_type &                    reference;
-    typedef const value_type &              const_reference;
-    typedef typename std::vector<T>::iterator        iterator;
-    typedef const typename std::vector<T>::iterator  const_iterator;
-    typedef typename std::vector<T>::difference_type difference_type;
-    typedef typename std::vector<T>::size_type       size_type;
-    typedef typename std::vector<T>::allocator_type       allocator_type;
-
+    typedef T                                           value_type;
+    typedef value_type &                                reference;
+    typedef const value_type &                          const_reference;
+    typedef typename std::vector<T>::iterator           iterator;
+    typedef const typename std::vector<T>::iterator     const_iterator;
+    typedef typename std::vector<T>::difference_type    difference_type;
+    typedef typename std::vector<T>::size_type          size_type;
+    typedef typename std::vector<T>::allocator_type     allocator_type;
 
     pointer_wrapper_vector() : m_array_(0), m_size_(0) {}
     pointer_wrapper_vector(T* p, std::size_t s) : m_array_(p), m_size_(s) {}
@@ -50,7 +52,7 @@ class pointer_wrapper_vector
       return iterator(&m_array_[m_size_]);
     }
 
-    reference operator[](std::size_t index) { return m_array_[index]; }
+    reference       operator[](std::size_t index)       { return m_array_[index]; }
     const_reference operator[](std::size_t index) const { return m_array_[index]; }
 
 //    const T *operator[](std::size_t index) { return &m_array[index]; }
