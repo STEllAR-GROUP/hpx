4174af43af662311ef0b869e801e702abe37bfb6
Thomas Heller <thom.heller@gmail.com>
2015-04-27 07:31:32 -0500

    Fixing typo in component name comment

--- src/runtime/components/component_type.cpp
+++ src/runtime/components/component_type.cpp
@@ -32,7 +32,7 @@ namespace hpx { namespace components
             "component_agas_symbol_namespace",                  /* 13 */
 
 #if defined(HPX_HAVE_SODIUM)
-            "signed_certificate_promise",                       /* 14 (0x14005) */
+            "signed_certificate_promise",                       /* 14 (0xe0005) */
             "component_root_certificate_authority",             /* 15 */
             "component_subordinate_certificate_authority",      /* 16 */
 #endif
