fcf99084af6fc3827dfa6f6be837dff040c7e33c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-05-12 11:26:00 -0500

    Adding missing #include

--- hpx/parallel/util/scan_partitioner.hpp
+++ hpx/parallel/util/scan_partitioner.hpp
@@ -17,6 +17,7 @@
 #include <hpx/parallel/execution_policy.hpp>
 #include <hpx/parallel/util/detail/chunk_size.hpp>
 #include <hpx/parallel/util/detail/handle_local_exceptions.hpp>
+#include <hpx/parallel/util/detail/algorithm_result.hpp>
 #include <hpx/parallel/traits/extract_partitioner.hpp>
 
 #include <algorithm>
