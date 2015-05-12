bed1023dd8a458df1e29c8888d9cb7595bf49776
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-05-12 08:43:01 -0500

    Fixing copyright

--- tests/regressions/lcos/safely_destroy_promise_1481.cpp
+++ tests/regressions/lcos/safely_destroy_promise_1481.cpp
@@ -1,4 +1,4 @@
-//  Copyright 2015 (c) Hartmut Kaiser
+//  Copyright (c) 2015 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
