de99abd9f207bb98e76c0bc05adafbebb12011c7
K-ballo <k@fusionfenix.com>
2015-05-19 10:55:21 -0500

    Fix unwrapped issue with future ranges of void type
    The implementation tried to instantiate `tuple<void>`, which was a synonym for `tuple<>` under the faux variadic implementation but is a hard error with true variadics. Fixes #1528

---
+++ tests/regressions/util/unwrapped_1528.cpp
@@ -0,0 +1,17 @@
+//  Copyright (c) 2015 Agustin Berge
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/lcos/future.hpp>
+#include <hpx/util/unwrapped.hpp>
+
+#include <vector>
+
+void noop(){}
+
+int main()
+{
+    std::vector<hpx::future<void>> fs;
+    hpx::util::unwrapped(&noop)(fs);
+}
