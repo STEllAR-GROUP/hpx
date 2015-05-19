d99fcffdbf5962cda567207c2e8527a0964555fe
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-05-19 14:13:27 -0500

    Fixing comment

--- tests/regressions/lcos/async_callback_with_bound_callback.cpp
+++ tests/regressions/lcos/async_callback_with_bound_callback.cpp
@@ -3,9 +3,6 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// This test case demonstrates the issue described in #1485:
-// `ignore_while_locked` doesn't support all Lockable types
-
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_main.hpp>
 #include <hpx/include/async.hpp>
