218488fd1d995e8202cdaf65813042e2e0900f41
AntonBikineev <ant.bikineev@gmail.com>
2015-05-20 23:21:08 -0500

    variant serialization now uses mpl containers insteaf of variadic templates to make things portable

--- hpx/runtime/serialization/variant.hpp
+++ hpx/runtime/serialization/variant.hpp
@@ -1,3 +1,6 @@
+//  copyright (c) 2005
+//  troy d. straszheim <troy@resophonic.com>
+//  http://www.resophonic.com
 //  Copyright (c) 2015 Anton Bikineev
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -6,94 +9,115 @@
 #ifndef HPX_SERIALIZATION_VARIANT_HPP
 #define HPX_SERIALIZATION_VARIANT_HPP
 
-#include <hpx/runtime/serialization/serialize.hpp>
+#include <hpx/runtime/serialization/serialization_fwd.hpp>
+#include <hpx/exception.hpp>
+
+#include <boost/mpl/front.hpp>
+#include <boost/mpl/pop_front.hpp>
+#include <boost/mpl/eval_if.hpp>
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/mpl/empty.hpp>
 
 #include <boost/variant.hpp>
 
 namespace hpx { namespace serialization
 {
-    namespace detail
+    struct variant_save_visitor :
+        boost::static_visitor<>
     {
-        struct variant_save_visitor: boost::static_visitor<>
+        variant_save_visitor(output_archive& ar) :
+            m_ar(ar)
+        {}
+
+        template<class T>
+        void operator()(T const & value) const
         {
-            variant_save_visitor(output_archive& ar)
-              : ar(ar)
-            {}
+            m_ar << value;
+        }
+    private:
+        output_archive & m_ar;
+    };
 
-            template <class T>
-            void operator()(const T& value) const
-            {
-                ar << value;
-            }
+    template<class S>
+    struct variant_impl {
 
-        private:
-            output_archive& ar;
+        struct load_null {
+            template<class V>
+            static void invoke(
+                input_archive& /*ar*/,
+                int /*which*/,
+                V & /*v*/
+            ){}
         };
 
-        template <class... Args>
-        struct load_helper_t{};
-
-#if defined(BOOST_NO_CXX11_VARIADIC_TEMPLATES) || \
-    defined(BOOST_VARIANT_DO_NOT_USE_VARIADIC_TEMPLATES) // intr. only in 1.56
-        // overload for non-variadic version of variant library
-        template <class Variant, class... Tail>
-        BOOST_FORCEINLINE void load_helper(input_archive&, Variant&,
-                int, load_helper_t<boost::detail::variant::void_, Tail...>)
-        {
-            HPX_ASSERT(false);
-        }
-#else
-        // overload for variadic version of variant library
-        template <class Variant>
-        BOOST_FORCEINLINE void load_helper(input_archive&, Variant&,
-                int, load_helper_t<>)
-        {
-            HPX_ASSERT(false);
-        }
-#endif
-        template <class Variant, class Head, class... Tail>
-        BOOST_FORCEINLINE void load_helper(input_archive& ar, Variant& var,
-                int which, load_helper_t<Head, Tail...>)
-        {
-            if (which == 0)
-            {
-                Head value; // default ctor concept
-                ar >> value;
-                var = value;
+        struct load_impl {
+            template<class V>
+            static void invoke(
+                input_archive& ar,
+                int which,
+                V & v
+            ){
+                if(which == 0){
+                    // note: A non-intrusive implementation (such as this one)
+                    // necessary has to copy the value.  This wouldn't be necessary
+                    // with an implementation that de-serialized to the address of the
+                    // aligned storage included in the variant.
+                    typedef typename boost::mpl::front<S>::type head_type;
+                    head_type value;
+                    ar >> value;
+                    v = value;
+                    return;
+                }
+                typedef typename boost::mpl::pop_front<S>::type type;
+                variant_impl<type>::load(ar, which - 1, v);
             }
-            else
-            {
-                load_helper(ar, var, which - 1, load_helper_t<Tail...>());
-            }
-        }
-    }
+        };
 
-    template <class... Args>
-    void save(output_archive& ar, const boost::variant<Args...>& variant, unsigned)
+        template<class V>
+        static void load(
+            input_archive& ar,
+            int which,
+            V & v
+        ){
+            typedef typename boost::mpl::eval_if<boost::mpl::empty<S>,
+                boost::mpl::identity<load_null>,
+                boost::mpl::identity<load_impl>
+            >::type typex;
+            typex::invoke(ar, which, v);
+        }
+
+    };
+
+    template<BOOST_VARIANT_ENUM_PARAMS(class T)>
+    void save(output_archive& ar,
+            boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)> const & v, unsigned)
     {
-        int which = variant.which();
+        int which = v.which();
         ar << which;
-        detail::variant_save_visitor visitor(ar);
-        variant.apply_visitor(visitor);
+        variant_save_visitor visitor(ar);
+        v.apply_visitor(visitor);
     }
 
-    template <class... Args>
-    void load(input_archive& ar, boost::variant<Args...>& variant, unsigned)
+    template<BOOST_VARIANT_ENUM_PARAMS(class T)>
+    void load(input_archive& ar,
+            boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)>& v, unsigned)
     {
         int which;
+        typedef typename boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)>::types types;
         ar >> which;
-        if (static_cast<std::size_t>(which) >= sizeof...(Args))
-        {
+        if(which >=  boost::mpl::size<types>::value)
+            // this might happen if a type was removed from the list of variant types
             HPX_THROW_EXCEPTION(serialization_error
-              , "load variant"
-              , "which of loaded variant is greater then saved one");
-        }
-
-        detail::load_helper(ar, variant, which, detail::load_helper_t<Args...>());
+              , "load<Archive, Variant, version>"
+              , "type was removed from the list of variant types");
+        variant_impl<types>::load(ar, which, v);
     }
 
-    HPX_SERIALIZATION_SPLIT_FREE_TEMPLATE((template <class... Args>),
-            (boost::variant<Args...>));
-}}
+    HPX_SERIALIZATION_SPLIT_FREE_TEMPLATE((template <BOOST_VARIANT_ENUM_PARAMS(class T)>),
+            (boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)>));
 
-#endif
+} // namespace serialization
+} // namespace boost
+
+#endif //HPX_SERIALIZATION_VARIANT_HPP
