3706b4363511159e82a0d3f47d28be367bcc9408
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-05-22 16:57:32 -0500

    Finalize fly-by change by removing unneeded code

--- hpx/runtime/agas/primary_namespace.hpp
+++ hpx/runtime/agas/primary_namespace.hpp
@@ -46,23 +46,6 @@ struct primary_namespace
     {
         this->base_type::service_non_blocking(this->get_gid(), req, priority);
     }
-
-//     std::vector<response> bulk_service(
-//         std::vector<request> const& reqs
-//       , threads::thread_priority priority = threads::thread_priority_default
-//       , error_code& ec = throws
-//         )
-//     {
-//         return this->base_type::bulk_service(this->get_gid(), reqs, priority, ec);
-//     }
-
-//     void bulk_service_non_blocking(
-//         std::vector<request> const& reqs
-//       , threads::thread_priority priority = threads::thread_priority_default
-//         )
-//     {
-//         this->base_type::bulk_service_non_blocking(this->get_gid(), reqs, priority);
-//     }
 };
 
 }}
