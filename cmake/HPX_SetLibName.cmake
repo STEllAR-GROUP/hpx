becefcde51ae4f08d910981da8a8d2ce6f64b9bb
John Biddiscombe <biddisco@cscs.ch>
2015-06-04 08:46:27 +0200

    Add missing copyright notice

--- cmake/HPX_SetLibName.cmake
+++ cmake/HPX_SetLibName.cmake
@@ -1,3 +1,8 @@
+# Copyright (c) 2015      John Biddiscombe
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #-------------------------------------------------------------------------------
 # adds hpx_ prefix to give hpx_${name} to libraries and components
 #-------------------------------------------------------------------------------
