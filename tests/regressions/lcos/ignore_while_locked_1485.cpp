17120c28efb26a673a80e339ec82a5bc037614e3
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-06-23 13:20:45 -0500

    This fixes possible deadlock in the test ignore_while_locked_1485

--- tests/regressions/lcos/ignore_while_locked_1485.cpp
+++ tests/regressions/lcos/ignore_while_locked_1485.cpp
@@ -20,10 +20,23 @@ struct wait_for_flag
       : flag(false), woken(0)
     {}
 
-    void wait()
+    void wait(hpx::lcos::local::spinlock& local_mutex,
+        hpx::lcos::local::condition_variable& local_cond_var, bool& running)
     {
-        while(!flag)
+        bool first = true;
+        while (!flag)
         {
+            boost::unique_lock<hpx::lcos::local::spinlock> lk(mutex);
+
+            // signal successful initialization
+            if (first)
+            {
+                boost::lock_guard<hpx::lcos::local::spinlock> lk(local_mutex);
+                running = true;
+                first = false;
+                local_cond_var.notify_all();
+            }
+
             cond_var.wait(mutex);
         }
         ++woken;
@@ -37,8 +50,21 @@ void test_condition_with_mutex()
 {
     wait_for_flag data;
 
-    hpx::thread thread(&wait_for_flag::wait, boost::ref(data));
+    bool running = false;
+    hpx::lcos::local::spinlock local_mutex;
+    hpx::lcos::local::condition_variable local_cond_var;
 
+    hpx::thread thread(&wait_for_flag::wait, boost::ref(data),
+        boost::ref(local_mutex), boost::ref(local_cond_var), boost::ref(running));
+
+    // wait for the thread to run
+    {
+        boost::unique_lock<hpx::lcos::local::spinlock> lk(local_mutex);
+        while (!running)
+            local_cond_var.wait(lk);
+    }
+
+    // now start actual test
     data.flag.store(true);
 
     {
