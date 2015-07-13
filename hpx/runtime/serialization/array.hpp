679d3707fbbddff14d09c90bc386c8b5c2c30d2a
Andreas Schäfer <gentryx@gmx.de>
2015-07-13 16:59:16 +0200

    added copyright notes

--- hpx/runtime/serialization/array.hpp
+++ hpx/runtime/serialization/array.hpp
@@ -1,5 +1,6 @@
 //  (C) Copyright 2005 Matthias Troyer and Dave Abrahams
 //  Copyright (c) 2015 Anton Bikineev
+//  Copyright (c) 2015 Andreas Schaefer
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
--- tests/unit/serialization/serialization_array.cpp
+++ tests/unit/serialization/serialization_array.cpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2014 Thomas Heller
+//  Copyright (c) 2015 Andreas Schaefer
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
