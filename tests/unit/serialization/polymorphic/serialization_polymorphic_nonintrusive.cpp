f859daa2aa3e6f7dfd7ade8abf562515d632ca29
Andreas Schäfer <gentryx@gmx.de>
2015-08-06 08:03:08 +0200

    added test to check (currently broken) deserialization of objects with custom c-tors via smart pointers

--- tests/unit/serialization/polymorphic/serialization_polymorphic_nonintrusive.cpp
+++ tests/unit/serialization/polymorphic/serialization_polymorphic_nonintrusive.cpp
@@ -6,6 +6,7 @@
 
 #include <hpx/runtime/serialization/serialize.hpp>
 #include <hpx/runtime/serialization/base_object.hpp>
+#include <hpx/runtime/serialization/shared_ptr.hpp>
 
 #include <hpx/runtime/serialization/input_archive.hpp>
 #include <hpx/runtime/serialization/output_archive.hpp>
@@ -14,7 +15,7 @@
 
 struct A
 {
-    A() : a(8) {}
+    A(int a = 8) : a(a) {}
     virtual ~A() {}
 
     int a;
@@ -68,7 +69,73 @@ void serialize(Archive& ar, D& d, unsigned)
 
 HPX_SERIALIZATION_REGISTER_CLASS(D);
 
-int main()
+template<typename T>
+struct C
+{
+    HPX_SERIALIZATION_POLYMORPHIC_TEMPLATE_SEMIINTRUSIVE(C);
+
+    C(T c) :
+        c(c)
+    {}
+
+    T c;
+};
+
+template<typename Archive, typename T>
+void serialize(Archive& ar, C<T>& c, unsigned)
+{
+    ar & c.c;
+}
+
+template<typename T>
+C<T> *c_factory(hpx::serialization::input_archive& ar, C<T> */*unused*/)
+{
+    C<T> *c = new C<T>(999);
+    serialize(ar, *c, 0);
+    return c;
+}
+
+HPX_TRAITS_NONINTRUSIVE_POLYMORPHIC_TEMPLATE((template <class T>), C<T>)
+HPX_SERIALIZATION_REGISTER_CLASS_TEMPLATE(template<class T>, C<T>)
+HPX_SERIALIZATION_WITH_CUSTOM_CONSTRUCTOR_TEMPLATE((template<typename T>), (C<T>), c_factory);
+
+template<typename T>
+struct E : public A
+{
+public:
+    HPX_SERIALIZATION_POLYMORPHIC_TEMPLATE_SEMIINTRUSIVE(E);
+
+    E(int i, T t) :
+        A(i),
+        c(t)
+    {}
+
+    C<T> c;
+};
+
+namespace hpx { namespace serialization {
+
+    template <class Archive, class T>
+    void serialize(Archive& archive, E<T>& s, unsigned)
+    {
+        archive & hpx::serialization::base_object<A>(s);
+        archive & s.c;
+    }
+} }
+
+template<typename T>
+E<T> *e_factory(hpx::serialization::input_archive& ar, E<T> */*unused*/)
+{
+    E<T> *e = new E<T>(99, 9999);
+    serialize(ar, *e, 0);
+    return e;
+}
+
+HPX_TRAITS_NONINTRUSIVE_POLYMORPHIC_TEMPLATE((template <class T>), E<T>)
+HPX_SERIALIZATION_REGISTER_CLASS_TEMPLATE(template<class T>, E<T>)
+HPX_SERIALIZATION_WITH_CUSTOM_CONSTRUCTOR_TEMPLATE((template<typename T>), (E<T>), e_factory);
+
+void test_basic()
 {
     std::vector<char> buffer;
     hpx::serialization::output_archive oarchive(buffer);
@@ -88,6 +155,30 @@ int main()
     HPX_TEST_EQ(b2.b, d1.b);
     HPX_TEST_EQ(d.b, d1.b);
     HPX_TEST_EQ(d.d, d1.d);
+}
+
+void test_member()
+{
+    std::vector<char> buffer;
+    {
+        boost::shared_ptr<A> struct_a(new E<float>(1, 2.3f));
+        hpx::serialization::output_archive oarchive(buffer);
+        oarchive << struct_a;
+    }
+    {
+        boost::shared_ptr<A> struct_b;
+        hpx::serialization::input_archive iarchive(buffer);
+        iarchive >> struct_b;
+        HPX_TEST_EQ(struct_b->a, 1);
+        HPX_TEST_EQ(dynamic_cast<E<float>*>(&*struct_b)->c.c, 2.3f);
+    }
+}
+
+
+int main()
+{
+    test_basic();
+    test_member();
 
     return hpx::util::report_errors();
 }
