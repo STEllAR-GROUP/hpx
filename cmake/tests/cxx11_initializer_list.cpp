999c9b987b241fbc7a2fe3b5155e9418636bb1b5
K-ballo <k@fusionfenix.com>
2015-09-09 11:16:39 -0300

    Add missing include for initializer-list feature test

--- cmake/tests/cxx11_initializer_list.cpp
+++ cmake/tests/cxx11_initializer_list.cpp
@@ -6,6 +6,8 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <initializer_list>
+
 void test_aggregate_initializer_list() {
   int ail[] = {1, 2, 3};
 }
