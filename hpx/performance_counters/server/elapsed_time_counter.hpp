1531cb46620cbff8b5dbd56c87a7df0a1160cd56
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2015-09-26 17:32:54 -0500

    Making sure that uptime counter throws exception on any attempt to be reset.

--- hpx/performance_counters/server/elapsed_time_counter.hpp
+++ hpx/performance_counters/server/elapsed_time_counter.hpp
@@ -29,6 +29,7 @@ namespace hpx { namespace performance_counters { namespace server
         elapsed_time_counter(counter_info const& info);
 
         hpx::performance_counters::counter_value get_counter_value(bool reset);
+        void reset_counter_value();
 
         /// \brief finalize() will be called just before the instance gets
         ///        destructed
---
+++ tests/regressions/performance_counters/uptime_1737.cpp
@@ -0,0 +1,58 @@
+//  Copyright (c) 2015 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Demonstrating #1737: Uptime problems
+
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/performance_counters.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+int hpx_main(int argc, char ** argv)
+{
+    using hpx::performance_counters::performance_counter;
+
+    performance_counter uptime("/runtime/uptime");
+
+    // verify that attempts to reset the uptime counter fail
+    {
+        bool exception_thrown = false;
+        try {
+            uptime.reset_sync();
+            HPX_TEST(false);
+        }
+        catch (hpx::exception const&) {
+            exception_thrown = true;
+        }
+        HPX_TEST(exception_thrown);
+    }
+
+    {
+        bool exception_thrown = false;
+        try {
+            uptime.get_value_sync<double>(true);
+            HPX_TEST(false);
+        }
+        catch (hpx::exception const&) {
+            exception_thrown = true;
+        }
+        HPX_TEST(exception_thrown);
+    }
+
+    // make sure reported value is in seconds
+    double start = uptime.get_value_sync<double>();
+    hpx::this_thread::sleep_for(boost::chrono::seconds(1));
+    double end = uptime.get_value_sync<double>();
+
+    HPX_TEST(end - start >= 1.0);
+
+    return hpx::finalize();
+}
+
+int main(int argc, char **argv)
+{
+    HPX_TEST_EQ(hpx::init(argc, argv), 0);
+    return hpx::util::report_errors();
+}
