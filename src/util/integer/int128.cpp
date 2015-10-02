136d660f6ab478363bc2c9139f1d7103259982fb
Thomas Heller <thom.heller@gmail.com>
2015-10-02 07:30:31 +0200

    Fixing (u)int128.cpp missing serialization header

--- src/util/integer/int128.cpp
+++ src/util/integer/int128.cpp
@@ -7,6 +7,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/util/integer/int128.hpp>
+#include <hpx/runtime/serialization/serialize.hpp>
 
 #include <cmath>
 #include <cstring>
