c6ae89eed7667da3f3ce0fc8cce1959fa7094d7e
Thomas Heller <thom.heller@gmail.com>
2015-10-29 20:39:29 +0100

    Adding leak test as suggested by @hkaiser

--- tests/regressions/lcos/promise_1620.cpp
+++ tests/regressions/lcos/promise_1620.cpp
@@ -7,10 +7,44 @@
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_start.hpp>
 #include <hpx/include/iostreams.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+struct test
+{
+    test() { ++count; }
+    test(test const & t) { ++count; }
+    test& operator=(test const & t) { ++count; return *this; }
+    ~test() { --count; }
+
+    static boost::atomic<int> count;
+};
+
+boost::atomic<int> test::count(0);
+
+test call() { return test(); }
+HPX_PLAIN_ACTION(call);
+
+void test_leak()
+{
+    {
+        hpx::lcos::promise<test> p;
+        hpx::apply_c<call_action>(p.get_id(), hpx::find_here());
+        hpx::future<test> f = p.get_future();
+        f.get();
+    }
+
+    hpx::agas::garbage_collect();
+    hpx::this_thread::yield();
+    hpx::agas::garbage_collect();
+    hpx::this_thread::yield();
+    HPX_TEST_EQ(test::count, 0);
+}
 
 int hpx_main(int argc, char* argv[])
 {
     {
+        test_leak();
+
         hpx::id_type promise_id;
         {
             hpx::promise<int> p;
