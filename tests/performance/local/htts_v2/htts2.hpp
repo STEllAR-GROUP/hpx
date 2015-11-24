824c924122774d72d89c5f49583fb2787c321ea2
Thomas Heller <thom.heller@gmail.com>
2015-11-24 14:56:41 +0100

    include all of hpx config to have HPX_MSVC available

--- tests/performance/local/htts_v2/htts2.hpp
+++ tests/performance/local/htts_v2/htts2.hpp
@@ -6,7 +6,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/config/defines.hpp>   // avoid issues with Intel14/libstdc++4.4 nullptr
+#include <hpx/config.hpp>
 
 #include <iostream>
 #include <boost/format.hpp>
