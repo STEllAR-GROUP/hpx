17e0ac0c1c369dcd2f34d2ccbbaf583ff9c725e9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2016-02-21 14:52:59 -0600

    Detect unknown command line options for applications which use the emulated main()

--- src/hpx_user_main_argc_argv.cpp
+++ src/hpx_user_main_argc_argv.cpp
@@ -1,9 +1,10 @@
-//  Copyright (c) 2007-2013 Hartmut Kaiser
+//  Copyright (c) 2007-2016 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/hpx_init.hpp>
+#include <hpx/exception.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Forwarding of hpx_startup::user_main, if necessary. This has to be in a
@@ -12,5 +13,18 @@
 // or not.
 int hpx_startup::user_main(int argc, char** argv)
 {
+    // If we have seen unknown command line options we can throw here as we
+    // know that the user is not going to look at the arguments.
+    std::string unknown_command_line =
+        hpx::get_config_entry("hpx.unknown_cmd_line_option", "");
+
+    if (!unknown_command_line.empty())
+    {
+        hpx::detail::report_exception_and_terminate(
+            HPX_GET_EXCEPTION(hpx::bad_parameter, "hpx_startup::user_main",
+                "unknown command line option(s): " + unknown_command_line)
+        );
+    }
+
     return hpx_startup::user_main();
 }
