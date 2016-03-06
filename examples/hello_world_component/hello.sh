04cddd6212f5f55e2d3e9bab74faf0d5d1d6ab68
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2016-03-06 13:30:16 -0600

    More shuffling of parameter sequencing

--- examples/hello_world_component/hello.sh
+++ examples/hello_world_component/hello.sh
@@ -9,18 +9,18 @@
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HPX_LOCATION/lib/pkgconfig
 
 # Compile the library
-c++ -o libhpx_hello_world.so hello_world_component.cpp `pkg-config --cflags --libs hpx_component` -DHPX_COMPONENT_NAME=hello_world
+c++ -o libhpx_hello_world.so hello_world_component.cpp `pkg-config --cflags --libs hpx_component` -DHPX_COMPONENT_NAME=hello_world -lhpx_iostreams
 
 # Create the directory where we want to install the library
 mkdir -p ~/my_hpx_libs
 mv libhpx_hello_world.so ~/my_hpx_libs
 
 # Compile the client
-c++ -o hello_world_client hello_world_client.cpp `pkg-config --cflags --libs hpx_application` -lhpx_iostreams -lhpx_hello_world -L ~/my_hpx_libs
+c++ -o hello_world_client hello_world_client.cpp `pkg-config --cflags --libs hpx_application` -L ~/my_hpx_libs -lhpx_hello_world -lhpx_iostreams
 
 # Prepare the environment so that we can run the command
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/my_hpx_libs"
 
 # Run the client, first add our directory to the LD_LIBRARY_PATH
-./hello_world_client 
+./hello_world_client
 
