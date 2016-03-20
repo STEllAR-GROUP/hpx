e8c7fe471e0531fae92ae285d08c9dfc4a648fde
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2016-03-20 13:29:38 -0500

    Adding missing service executor types

--- hpx/parallel/executors/service_executors.hpp
+++ hpx/parallel/executors/service_executors.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2007-2015 Hartmut Kaiser
+//  Copyright (c) 2007-2016 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -42,6 +42,74 @@ namespace hpx { namespace parallel { HPX_INLINE_NAMESPACE(v3)
                 BOOST_SCOPED_ENUM(threads::executors::service_executor_type) t,
                 char const* name_suffix = "")
           : threads::executors::service_executor(t, name_suffix)
+        {}
+    };
+
+    /// A \a io_pool_executor exposes the predefined HPX IO thread pool
+    /// through an executor interface.
+    ///
+    /// \note All tasks executed by one of these executors will run on
+    ///       one of the OS-threads dedicated for the IO thread pool. The
+    ///       tasks will not run a HPX-threads.
+    ///
+    struct io_pool_executor : service_executor
+    {
+        /// Create a new service executor for the IO thread pool
+        io_pool_executor()
+          : service_executor(
+                threads::executors::service_executor_type::io_thread_pool)
+        {}
+    };
+
+    /// A \a io_pool_executor exposes the predefined HPX parcel thread pool
+    /// through an executor interface.
+    ///
+    /// \note All tasks executed by one of these executors will run on
+    ///       one of the OS-threads dedicated for the parcel thread pool. The
+    ///       tasks will not run a HPX-threads.
+    ///
+    struct parcel_pool_executor : service_executor
+    {
+        /// Create a new service executor for the parcel thread pool
+        ///
+        /// \param name_suffix  [in] The name suffix to use for the underlying
+        ///                     thread pool (default: "-tcp")
+        parcel_pool_executor(char const* name_suffix = "-tcp")
+          : service_executor(
+                threads::executors::service_executor_type::parcel_thread_pool,
+                name_suffix)
+        {}
+    };
+
+    /// A \a io_pool_executor exposes the predefined HPX timer thread pool
+    /// through an executor interface.
+    ///
+    /// \note All tasks executed by one of these executors will run on
+    ///       one of the OS-threads dedicated for the timer thread pool. The
+    ///       tasks will not run a HPX-threads.
+    ///
+    struct timer_pool_executor : service_executor
+    {
+        /// Create a new service executor for the timer thread pool
+        timer_pool_executor()
+          : service_executor(
+                threads::executors::service_executor_type::timer_thread_pool)
+        {}
+    };
+
+    /// A \a io_pool_executor exposes the predefined HPX main thread pool
+    /// through an executor interface.
+    ///
+    /// \note All tasks executed by one of these executors will run on
+    ///       one of the OS-threads dedicated for the main thread pool. The
+    ///       tasks will not run a HPX-threads.
+    ///
+    struct main_pool_executor : service_executor
+    {
+        /// Create a new service executor for the main thread pool
+        main_pool_executor()
+          : service_executor(
+                threads::executors::service_executor_type::main_thread)
         {}
     };
 }}}
