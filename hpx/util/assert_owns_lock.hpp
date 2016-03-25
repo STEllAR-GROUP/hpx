6ff593e943301374738bdd64de5af72712952155
Agustin K-ballo Berge <k@fusionfenix.com>
2016-03-25 10:58:32 -0300

    Simplify HPX_ASSERT_OWNS_LOCK

--- hpx/util/assert_owns_lock.hpp
+++ hpx/util/assert_owns_lock.hpp
@@ -7,57 +7,29 @@
 #define HPX_UTIL_ASSERT_OWNS_LOCK_DEC_4_2013_0130PM
 
 #include <hpx/config.hpp>
+#include <hpx/traits/has_member_xxx.hpp>
 #include <hpx/util/assert.hpp>
 
-#include <boost/thread/locks.hpp>
-
-#include <utility>
+#include <type_traits>
 
 ///////////////////////////////////////////////////////////////////////////////
 namespace hpx { namespace util { namespace detail
 {
+    HPX_HAS_MEMBER_XXX_TRAIT_DEF(owns_lock);
+
     template <typename Lock>
-    void assert_owns_lock(Lock const&, int) {}
+    void assert_owns_lock(Lock const&, int)
+    {}
 
 #if !defined(HPX_DISABLE_ASSERTS) && !defined(BOOST_DISABLE_ASSERTS) && !defined(NDEBUG)
-    template <typename Mutex>
-    void assert_owns_lock(boost::unique_lock<Mutex> const& l, long)
-    {
-        HPX_ASSERT(l.owns_lock());
-    }
 
-    template <typename Mutex>
-    void assert_owns_lock(boost::shared_lock<Mutex> const& l, long)
-    {
-        HPX_ASSERT(l.owns_lock());
-    }
-
-    template <typename Mutex>
-    void assert_owns_lock(boost::upgrade_lock<Mutex> const& l, long)
-    {
-        HPX_ASSERT(l.owns_lock());
-    }
-
-#   if defined(HPX_HAVE_CXX11_SFINAE_EXPR)
     template <typename Lock>
-    decltype(std::declval<Lock>().owns_lock())
-    assert_owns_lock(Lock const& l, long)
+    typename std::enable_if<
+        has_owns_lock<Lock>::value
+    >::type assert_owns_lock(Lock const& l, long)
     {
         HPX_ASSERT(l.owns_lock());
-        return true;
     }
-#   endif
-
-#else
-
-#   if defined(HPX_HAVE_CXX11_SFINAE_EXPR)
-    template <typename Lock>
-    decltype(std::declval<Lock>().owns_lock())
-    assert_owns_lock(Lock const&, long)
-    {
-        return true;
-    }
-#   endif
 
 #endif
 }}}
