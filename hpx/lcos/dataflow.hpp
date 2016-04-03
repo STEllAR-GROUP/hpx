98a173549deaca84bab62735b4c559a70895a7da
Agustin K-ballo Berge <k@fusionfenix.com>
2016-04-03 11:15:19 -0300

    Reimplement dataflow for tuple-like instead of fusion-sequences

--- hpx/lcos/dataflow.hpp
+++ hpx/lcos/dataflow.hpp
@@ -8,30 +8,25 @@
 
 #include <hpx/config.hpp>
 #include <hpx/apply.hpp>
+#include <hpx/lcos/future.hpp>
+#include <hpx/runtime/launch_policy.hpp>
 #include <hpx/traits/is_action.hpp>
 #include <hpx/traits/is_future.hpp>
 #include <hpx/traits/is_future_range.hpp>
 #include <hpx/traits/is_distribution_policy.hpp>
 #include <hpx/traits/is_launch_policy.hpp>
-#include <hpx/runtime/launch_policy.hpp>
-#include <hpx/util/bind.hpp>
 #include <hpx/util/deferred_call.hpp>
 #include <hpx/util/invoke_fused.hpp>
-#include <hpx/lcos/future.hpp>
-#include <hpx/lcos/dataflow.hpp>
+#include <hpx/util/thread_description.hpp>
+#include <hpx/util/tuple.hpp>
+#include <hpx/util/detail/pack.hpp>
 
-#include <boost/fusion/include/begin.hpp>
-#include <boost/fusion/include/deref.hpp>
-#include <boost/fusion/include/end.hpp>
-#include <boost/fusion/include/next.hpp>
-#include <boost/fusion/include/for_each.hpp>
+#include <boost/atomic.hpp>
 #include <boost/mpl/bool.hpp>
-#include <boost/mpl/or.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/range/functions.hpp>
 #include <boost/ref.hpp>
 #include <boost/type_traits/is_same.hpp>
-#include <boost/atomic.hpp>
 #include <boost/utility/enable_if.hpp>
 
 #include <iterator>
@@ -96,7 +91,7 @@ namespace hpx { namespace lcos { namespace detail
             !traits::is_action<F>::value
         >::type>
     {
-        typedef typename hpx::util::detail::fused_result_of<
+        typedef typename util::detail::fused_result_of<
                 F(Args &&)
             >::type type;
     };
@@ -126,15 +121,19 @@ namespace hpx { namespace lcos { namespace detail
 
         typedef hpx::lcos::future<result_type> type;
 
+        typedef typename util::detail::make_index_pack<
+                util::tuple_size<Futures>::value
+            >::type indices_type;
+
         typedef
-            typename boost::fusion::result_of::end<Futures>::type
-            end_type;
+            boost::mpl::bool_<boost::is_void<result_type>::value>
+            is_void;
 
         typedef
             typename boost::mpl::if_<
-                boost::is_void<result_type>
-              , void(dataflow_frame::*)(boost::mpl::true_)
-              , void(dataflow_frame::*)(boost::mpl::false_)
+                is_void
+              , void(dataflow_frame::*)(indices_type, boost::mpl::true_)
+              , void(dataflow_frame::*)(indices_type, boost::mpl::false_)
             >::type
             execute_function_type;
 
@@ -157,16 +156,20 @@ namespace hpx { namespace lcos { namespace detail
 
     protected:
         ///////////////////////////////////////////////////////////////////////
+        template <std::size_t ...Is>
         HPX_FORCEINLINE
-        void execute(boost::mpl::false_)
+        void execute(util::detail::pack_c<std::size_t, Is...>, boost::mpl::false_)
         {
             try {
                 result_type res =
-                    hpx::util::invoke_fused(
-                        func_, std::move(futures_));
+                    util::invoke_fused(func_, std::move(futures_));
 
                 // reset futures
-                boost::fusion::for_each(futures_, reset_dataflow_future());
+                reset_dataflow_future reset;
+                int const _sequencer[]= {
+                    ((reset(util::get<Is>(futures_))), 0)...
+                };
+                (void)_sequencer;
 
                 this->set_data(std::move(res));
             }
@@ -175,15 +178,19 @@ namespace hpx { namespace lcos { namespace detail
             }
         }
 
+        template <std::size_t ...Is>
         HPX_FORCEINLINE
-        void execute(boost::mpl::true_)
+        void execute(util::detail::pack_c<std::size_t, Is...>, boost::mpl::true_)
         {
             try {
-                hpx::util::invoke_fused(
-                    func_, std::move(futures_));
+                util::invoke_fused(func_, std::move(futures_));
 
                 // reset futures
-                boost::fusion::for_each(futures_, reset_dataflow_future());
+                reset_dataflow_future reset;
+                int const _sequencer[]= {
+                    ((reset(util::get<Is>(futures_))), 0)...
+                };
+                (void)_sequencer;
 
                 this->set_data(util::unused_type());
             }
@@ -196,12 +203,9 @@ namespace hpx { namespace lcos { namespace detail
         HPX_FORCEINLINE
         void finalize(launch policy)
         {
-            typedef
-                boost::mpl::bool_<boost::is_void<result_type>::value>
-                is_void;
             if (policy == hpx::launch::sync)
             {
-                execute(is_void());
+                execute(indices_type(), is_void());
                 return;
             }
 
@@ -209,7 +213,8 @@ namespace hpx { namespace lcos { namespace detail
             execute_function_type f = &dataflow_frame::execute;
             boost::intrusive_ptr<dataflow_frame> this_(this);
             threads::register_thread_nullary(
-                util::deferred_call(f, std::move(this_), is_void())
+                util::deferred_call(
+                    f, std::move(this_), indices_type(), is_void())
               , util::thread_description(func_)
               , threads::pending
               , true
@@ -219,13 +224,9 @@ namespace hpx { namespace lcos { namespace detail
         HPX_FORCEINLINE
         void finalize(threads::executor& sched)
         {
-            typedef
-                boost::mpl::bool_<boost::is_void<result_type>::value>
-                is_void;
-
             execute_function_type f = &dataflow_frame::execute;
             boost::intrusive_ptr<dataflow_frame> this_(this);
-            hpx::apply(sched, f, std::move(this_), is_void());
+            hpx::apply(sched, f, std::move(this_), indices_type(), is_void());
         }
 
         // handle executors through their executor_traits
@@ -233,33 +234,29 @@ namespace hpx { namespace lcos { namespace detail
         HPX_FORCEINLINE
         void finalize(Executor& exec)
         {
-            typedef
-                boost::mpl::bool_<boost::is_void<result_type>::value>
-                is_void;
-
             execute_function_type f = &dataflow_frame::execute;
             boost::intrusive_ptr<dataflow_frame> this_(this);
 
             parallel::executor_traits<Executor>::apply_execute(exec,
-                hpx::util::deferred_call(f, std::move(this_), is_void()));
+                util::deferred_call(
+                    f, std::move(this_), indices_type(), is_void()));
         }
 
         ///////////////////////////////////////////////////////////////////////
-        template <typename Iter>
+        template <std::size_t I>
         HPX_FORCEINLINE
-        void do_await(Iter &&, boost::mpl::true_)
+        void do_await(boost::mpl::true_)
         {
             done_ = true;
         }
 
         // Current element is a not a future or future range, e.g. a just plain
         // value.
-        template <typename Iter, typename IsFuture, typename IsRange>
+        template <std::size_t I, typename IsFuture, typename IsRange>
         HPX_FORCEINLINE
-        void await_next_respawn(Iter iter, IsFuture is_future,
-            IsRange is_range)
+        void await_next_respawn(IsFuture is_future, IsRange is_range)
         {
-            await_next(iter, is_future, is_range);
+            await_next<I>(is_future, is_range);
 
             // avoid finalizing more than once
             bool expected = true;
@@ -267,26 +264,18 @@ namespace hpx { namespace lcos { namespace detail
                 finalize(policy_);
         }
 
-        template <typename Iter>
+        template <std::size_t I>
         HPX_FORCEINLINE
-        void await_next(Iter iter, boost::mpl::false_, boost::mpl::false_)
+        void await_next(boost::mpl::false_, boost::mpl::false_)
         {
-            typedef
-                typename boost::fusion::result_of::next<Iter>::type
-                next_type;
-
-            do_await(
-                boost::fusion::next(iter)
-              , boost::mpl::bool_<
-                    boost::is_same<next_type, end_type>::value
-                >()
-            );
+            do_await<I + 1>(
+                boost::mpl::bool_<util::tuple_size<Futures>::value == I + 1>());
         }
 
-        template <typename TupleIter, typename Iter>
-        void await_range_respawn(TupleIter iter, Iter next, Iter end)
+        template <std::size_t I, typename Iter>
+        void await_range_respawn(Iter next, Iter end)
         {
-            await_range(iter, next, end);
+            await_range<I>(next, end);
 
             // avoid finalizing more than once
             bool expected = true;
@@ -294,12 +283,11 @@ namespace hpx { namespace lcos { namespace detail
                 finalize(policy_);
         }
 
-        template <typename TupleIter, typename Iter>
-        void await_range(TupleIter iter, Iter next, Iter end)
+        template <std::size_t I, typename Iter>
+        void await_range(Iter next, Iter end)
         {
-            void (dataflow_frame::*f)(
-                    TupleIter, Iter, Iter
-                ) = &dataflow_frame::await_range_respawn;
+            void (dataflow_frame::*f)(Iter, Iter) =
+                &dataflow_frame::await_range_respawn<I>;
 
             for (/**/; next != end; ++next)
             {
@@ -328,10 +316,9 @@ namespace hpx { namespace lcos { namespace detail
                     {
                         boost::intrusive_ptr<dataflow_frame> this_(this);
                         next_future_data->set_on_completed(
-                            hpx::util::bind(
+                            util::deferred_call(
                                 f
                               , std::move(this_)
-                              , std::move(iter)
                               , std::move(next)
                               , std::move(end)
                             )
@@ -341,51 +328,38 @@ namespace hpx { namespace lcos { namespace detail
                 }
             }
 
-            typedef
-                typename boost::fusion::result_of::next<TupleIter>::type
-                next_type;
-
-            do_await(
-                boost::fusion::next(iter)
-              , boost::mpl::bool_<
-                    boost::is_same<next_type, end_type>::value
-                >()
-            );
+            do_await<I + 1>(
+                boost::mpl::bool_<util::tuple_size<Futures>::value == I + 1>());
         }
 
         // Current element is a range (vector) of futures
-        template <typename Iter>
+        template <std::size_t I>
         HPX_FORCEINLINE
-        void await_next(Iter iter, boost::mpl::false_, boost::mpl::true_)
+        void await_next(boost::mpl::false_, boost::mpl::true_)
         {
-            await_range(
-                iter
-              , boost::begin(boost::unwrap_ref(boost::fusion::deref(iter)))
-              , boost::end(boost::unwrap_ref(boost::fusion::deref(iter)))
+            typedef
+                typename util::tuple_element<I, Futures>::type
+                future_type;
+            future_type & f_ = util::get<I>(futures_);
+
+            await_range<I>(
+                boost::begin(boost::unwrap_ref(f_))
+              , boost::end(boost::unwrap_ref(f_))
             );
         }
 
         // Current element is a simple future
-        template <typename Iter>
+        template <std::size_t I>
         HPX_FORCEINLINE
-        void await_next(Iter iter, boost::mpl::true_, boost::mpl::false_)
+        void await_next(boost::mpl::true_, boost::mpl::false_)
         {
             typedef
-                typename boost::fusion::result_of::next<Iter>::type
-                next_type;
-
-            typedef
-                typename util::decay_unwrap<
-                    typename boost::fusion::result_of::deref<Iter>::type
-                >::type
+                typename util::tuple_element<I, Futures>::type
                 future_type;
-            future_type &  f_ =
-                boost::fusion::deref(iter);
+            future_type & f_ = util::get<I>(futures_);
 
             typedef
-                typename traits::future_traits<
-                    future_type
-                >::type
+                typename traits::future_traits<future_type>::type
                 future_result_type;
 
             boost::intrusive_ptr<
@@ -401,15 +375,14 @@ namespace hpx { namespace lcos { namespace detail
                 if (!next_future_data->is_ready())
                 {
                     void (dataflow_frame::*f)(
-                            Iter, boost::mpl::true_, boost::mpl::false_
-                        ) = &dataflow_frame::await_next_respawn;
+                            boost::mpl::true_, boost::mpl::false_
+                        ) = &dataflow_frame::await_next_respawn<I>;
 
                     boost::intrusive_ptr<dataflow_frame> this_(this);
                     next_future_data->set_on_completed(
-                        hpx::util::bind(
+                        util::deferred_call(
                             f
                           , std::move(this_)
-                          , std::move(iter)
                           , boost::mpl::true_()
                           , boost::mpl::false_()
                         )
@@ -418,23 +391,17 @@ namespace hpx { namespace lcos { namespace detail
                 }
             }
 
-            do_await(
-                boost::fusion::next(iter)
-              , boost::mpl::bool_<
-                    boost::is_same<next_type, end_type>::value
-                >()
-            );
+            do_await<I + 1>(
+                boost::mpl::bool_<util::tuple_size<Futures>::value == I + 1>());
         }
 
         ///////////////////////////////////////////////////////////////////////
-        template <typename Iter>
+        template <std::size_t I>
         HPX_FORCEINLINE
-        void do_await(Iter iter, boost::mpl::false_)
+        void do_await(boost::mpl::false_)
         {
             typedef
-                typename util::decay_unwrap<
-                    typename boost::fusion::result_of::deref<Iter>::type
-                >::type
+                typename util::tuple_element<I, Futures>::type
                 future_type;
 
             typedef typename traits::is_future<
@@ -445,22 +412,14 @@ namespace hpx { namespace lcos { namespace detail
                 future_type
             >::type is_range;
 
-            await_next(std::move(iter), is_future(), is_range());
+            await_next<I>(is_future(), is_range());
         }
 
     public:
         HPX_FORCEINLINE void do_await()
         {
-            typedef
-                typename boost::fusion::result_of::begin<Futures>::type
-                begin_type;
-
-            do_await(
-                boost::fusion::begin(futures_)
-              , boost::mpl::bool_<
-                    boost::is_same<begin_type, end_type>::value
-                >()
-            );
+            do_await<0>(
+                boost::mpl::bool_<util::tuple_size<Futures>::value == 0>());
 
             // avoid finalizing more than once
             bool expected = true;
@@ -490,7 +449,7 @@ namespace hpx { namespace lcos { namespace detail
         typename dataflow_frame<
             launch
           , Derived
-          , hpx::util::tuple<
+          , util::tuple<
                 hpx::id_type
               , typename hpx::traits::acquire_future<Ts>::type...
             >
@@ -503,7 +462,7 @@ namespace hpx { namespace lcos { namespace detail
                 dataflow_frame<
                     launch
                   , Derived
-                  , hpx::util::tuple<
+                  , util::tuple<
                         hpx::id_type
                       , typename hpx::traits::acquire_future<Ts>::type...
                     >
@@ -513,7 +472,7 @@ namespace hpx { namespace lcos { namespace detail
             boost::intrusive_ptr<frame_type> p(new frame_type(
                     launch::async
                   , Derived()
-                  , hpx::util::forward_as_tuple(
+                  , util::forward_as_tuple(
                         id
                       , hpx::traits::acquire_future_disp()(
                             std::forward<Ts>(ts)
@@ -533,7 +492,7 @@ namespace hpx { namespace lcos { namespace detail
         typename dataflow_frame<
             launch
           , Derived
-          , hpx::util::tuple<
+          , util::tuple<
                 hpx::id_type
               , typename hpx::traits::acquire_future<Ts>::type...
             >
