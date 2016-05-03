5ebd6f1fa02fb51a5c4640628892a060a6905671
John Biddiscombe <biddisco@cscs.ch>
2016-05-03 08:50:40 +0200

    Switch reduce_by_key to new prefix_scan implementation

--- hpx/parallel/algorithms/reduce_by_key.hpp
+++ hpx/parallel/algorithms/reduce_by_key.hpp
@@ -10,10 +10,12 @@
 //
 #include <hpx/parallel/executors.hpp>
 //
+#include <hpx/parallel/algorithms/sort.hpp>
+#include <hpx/parallel/algorithms/inclusive_scan.hpp>
+#include <hpx/parallel/algorithms/prefix_scan.hpp>
 #include <hpx/parallel/algorithms/copy.hpp>
 #include <hpx/parallel/algorithms/for_each.hpp>
 #include <hpx/parallel/algorithms/inclusive_scan.hpp>
-#include <hpx/parallel/algorithms/sort.hpp>
 #include <hpx/parallel/util/zip_iterator.hpp>
 #include <hpx/util/transform_iterator.hpp>
 #include <hpx/util/tuple.hpp>
@@ -356,7 +358,7 @@ namespace hpx { namespace parallel { HPX_INLINE_NAMESPACE(v1)
                 //
                 typedef hpx::util::tuple<value_type, reduce_key_series_states>
                     lambda_type;
-                hpx::parallel::inclusive_scan(sync_policy, states_begin,
+                hpx::parallel::prefix_scan_inclusive(sync_policy, states_begin,
                     states_end, states_out_begin, initial,
                     // B is the current entry, A is the one passed in from 'previous'
                     [&func](zip_type_in a, zip_type_in b)->lambda_type
