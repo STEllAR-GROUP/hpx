b197e786904d99e9f8b448c4d9e44467218f2703
Thomas Heller <thomas.heller@cs.fau.de>
2016-05-12 11:47:29 +0200

    Refining cuda launcher
     - Add possibility to launch kernels from within a device
     - Flyby: improving compiler flags

--- hpx/compute/cuda/detail/launch.hpp
+++ hpx/compute/cuda/detail/launch.hpp
@@ -23,43 +23,89 @@
 
 namespace hpx { namespace compute { namespace cuda { namespace detail
 {
-    template <typename F, typename Args>
-    __global__ void launch_helper(F f, Args args)
+    template <typename Closure>
+    __global__ void launch_function(Closure closure)
     {
-        // FIXME: is it possible to move tha arguments?
-        hpx::util::invoke_fused(f, args);
+        closure();
     }
 
-    // Launch any given function F with the given parameters. This function
-    // does not involve any device synchronization.
-    template <typename F, typename DimType, typename ...Ts>
-    void launch(target const& t, DimType gridDim, DimType blockDim, F && f,
-        Ts&&... vs)
+    template <typename F, typename ...Ts>
+    struct launch_helper
     {
-        detail::scoped_active_target active(t);
-
         typedef
             hpx::util::tuple<typename util::decay<Ts>::type...>
             args_type;
         typedef typename util::decay<F>::type fun_type;
 
-        fun_type f_ = std::forward<F>(f);
-        args_type args(std::forward<Ts>(vs)...);
 
-        void (*launch_function)(fun_type, args_type)
-            = launch_helper<fun_type, args_type>;
-
-        launch_function<<<gridDim, blockDim, 0, active.stream()>>>(
-            std::move(f_), std::move(args));
-
-        cudaError_t error = cudaGetLastError();
-        if(error != cudaSuccess)
+        struct closure
         {
-            HPX_THROW_EXCEPTION(kernel_error,
-                "cuda::detail::launch()",
-                std::string("kernel launch failed: ") +
-                    cudaGetErrorString(error));
+            fun_type f_;
+            args_type args_;
+            HPX_DEVICE void operator()()
+            {
+                // FIXME: is it possible to move tha arguments?
+                hpx::util::invoke_fused(f_, args_);
+            }
+        };
+
+        typedef void (*launch_function_type)(closure);
+
+        HPX_HOST_DEVICE
+        static launch_function_type get_launch_function()
+        {
+            return launch_function<closure>;
         }
+
+        template <typename DimType>
+        HPX_HOST_DEVICE
+        static void call(target const& tgt, DimType gridDim, DimType blockDim,
+            fun_type f, args_type args)
+        {
+            // This is needed for the device code to make sure the kernel
+            // is instantiated correctly.
+            launch_function_type launcher = get_launch_function();
+            closure c{std::move(f), std::move(args)};
+
+#if !defined(__CUDA_ARCH__)
+            detail::scoped_active_target active(tgt);
+
+            launch_function<<<gridDim, blockDim, 0, active.stream()>>>(
+                std::move(c));
+
+            cudaError_t error = cudaGetLastError();
+            if(error != cudaSuccess)
+            {
+                HPX_THROW_EXCEPTION(kernel_error,
+                    "cuda::detail::launch()",
+                    std::string("kernel launch failed: ") +
+                        cudaGetErrorString(error));
+            }
+#elif __CUDA_ARCH__ >= 350
+            void *param_buffer
+                = cudaGetParameterBuffer(std::alignment_of<closure>::value, sizeof(closure));
+            std::memcpy(param_buffer, &c, sizeof(closure));
+            cudaLaunchDevice(reinterpret_cast<void*>(launcher), param_buffer,
+                dim3(gridDim), dim3(blockDim), 0, tgt.native_handle().stream_);
+#else
+
+#endif
+        }
+    };
+
+
+    // Launch any given function F with the given parameters. This function
+    // does not involve any device synchronization.
+    template <typename DimType, typename F, typename ...Ts>
+    HPX_HOST_DEVICE
+    void launch(target const& t, DimType gridDim, DimType blockDim, F && f,
+        Ts&&... vs)
+    {
+        typedef
+            hpx::util::tuple<typename util::decay<Ts>::type...>
+            args_type;
+        launch_helper<F, Ts...>::call(t, gridDim, blockDim, std::forward<F>(f),
+            util::forward_as_tuple(std::forward<Ts>(vs)...));
     }
 }}}}
 
