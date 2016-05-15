76ef5e69713c31aa00cb88efac6f979b460eed6f
Agustin K-ballo Berge <k@fusionfenix.com>
2016-05-15 11:24:21 -0300

    Fix some typos in config tests, reorder misplaced entries, rename HPX_HAVE_CXX11_IS_TRIVIALLY_COPYABLE to HPX_HAVE_CXX11_STD_IS_TRIVIALLY_COPYABLE for consistency with the rest of the std:: macros

--- hpx/traits.hpp
+++ hpx/traits.hpp
@@ -233,7 +233,7 @@ namespace hpx { namespace traits
     ///////////////////////////////////////////////////////////////////////////
     struct general_pointer_tag {};
 
-#if defined(HPX_HAVE_CXX11_IS_TRIVIALLY_COPYABLE)
+#if defined(HPX_HAVE_CXX11_STD_IS_TRIVIALLY_COPYABLE)
     struct trivially_copyable_pointer_tag : general_pointer_tag {};
 #endif
 
