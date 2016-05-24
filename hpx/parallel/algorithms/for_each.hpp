e321d617ae1711dec380d26f6d6b2a686d2ee827
zkhatami88 <z.khatami88@gmail.com>
2016-05-24 14:32:25 -0500

    for_each for prefetching

--- hpx/parallel/algorithms/for_each.hpp
+++ hpx/parallel/algorithms/for_each.hpp
@@ -71,11 +71,13 @@ namespace hpx { namespace parallel { HPX_INLINE_NAMESPACE(v1)
                         [f, proj](std::size_t /*part_index*/,
                             InIter part_begin, std::size_t part_size) mutable
                         {
+
+                            typedef typename util::loop_n_iterator_mapping<InIter>::type iterator_type;
                             // VS2015 bails out when proj or f are captured by
                             // ref
                             util::loop_n(
                                 part_begin, part_size,
-                                [=](InIter curr) mutable
+                                [=](iterator_type curr) mutable
                                 {
                                     hpx::util::invoke(
                                         f, hpx::util::invoke(proj, *curr));
