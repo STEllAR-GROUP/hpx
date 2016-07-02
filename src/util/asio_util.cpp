42a95c2b7ec71ddb914d1066199fd82d9fa7bcd8
John Biddiscombe <biddisco@cscs.ch>
2016-07-02 20:13:41 +0200

    Revert to using inet_pton and inet_ntop on linux
    Keep boost::asio functions for windows, they do not quite work as
    expected on linux - see issue 2177 for details.

--- src/util/asio_util.cpp
+++ src/util/asio_util.cpp
@@ -172,34 +172,47 @@ namespace hpx { namespace util
         return true;
     }
 
+
     ///////////////////////////////////////////////////////////////////////
     // Take an ip v4 or v6 address and "standardize" it for comparison checks
+    // note that this code doesn't work as expected if we use the boost
+    // inet_pton functions on linux. see issue #2177 for further info
     std::string cleanup_ip_address(const std::string &addr)
     {
-        unsigned char buf[sizeof(struct in6_addr)];
+        char buf[sizeof(struct in6_addr)];
         int i=0, domain[2] = {AF_INET, AF_INET6};
         char str[INET6_ADDRSTRLEN];
-        boost::system::error_code ec;
-        unsigned long scope_id = 0x0e;
 
-        for (i = 0; i < 2; ++i) {
+#if defined(HPX_WINDOWS)
+        unsigned long scope_id;
+        boost::system::error_code ec;
+#endif
+
+        for (i=0; i<2; ++i) {
+#if defined(HPX_WINDOWS)
             int s = boost::asio::detail::socket_ops::inet_pton(
-                domain[i], &addr[0], buf, &scope_id, ec);
-            if (!ec && s > 0)
-                break;
+              domain[i], &addr[0], buf, &scope_id, ec);
+            if (s>0 && !ec) break;
+#else
+            int s = inet_pton(domain[i], &addr[0], buf);
+            if (s>0) break;
+#endif
         }
-        if (i == 2) {
+        if (i==2) {
             HPX_THROW_EXCEPTION(bad_parameter, "cleanup_ip_address",
                 "Invalid IP address string");
         }
 
-        if (boost::asio::detail::socket_ops::inet_ntop(
-                domain[i], buf, str, INET6_ADDRSTRLEN, scope_id, ec) == NULL)
-        {
-            HPX_THROW_EXCEPTION(bad_parameter, "cleanup_ip_address",
-                "inet_ntop failure");
-        }
-        return std::string(str);
+#if defined(HPX_WINDOWS)
+       if (boost::asio::detail::socket_ops::inet_ntop(
+          domain[i], buf, str, INET6_ADDRSTRLEN, scope_id, ec) == 0) {
+#else
+       if (inet_ntop(domain[i], buf, str, INET6_ADDRSTRLEN) == NULL) {
+#endif
+           HPX_THROW_EXCEPTION(bad_parameter, "cleanup_ip_address",
+               "inet_ntop failure");
+       }
+       return std::string(str);
     }
 
     endpoint_iterator_type connect_begin(std::string const & address,
@@ -313,4 +326,3 @@ namespace hpx { namespace util
         return endpoint_iterator_type();
     }
 }}
-
