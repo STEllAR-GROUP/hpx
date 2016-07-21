2f833b341cb1e1d61d517d09dab50c62a3487404
Thomas Heller <thomas.heller@cs.fau.de>
2016-07-21 14:40:35 +0200

    Added inspect checks for enable_if, type_traits and mpl

--- tools/inspect/deprecated_include_check.cpp
+++ tools/inspect/deprecated_include_check.cpp
@@ -27,6 +27,7 @@ namespace boost
       { "boost/type_traits\\.hpp", "separate type-traits headers" },
       { "boost/unordered_map\\.hpp", "unordered_map" },
       { "boost/unordered_set\\.hpp", "unordered_set" },
+      { "boost/utility/enable_if\\.hpp", "type_traits" },
       { "boost/detail/atomic_count\\.hpp", "hpx/util/atomic_count.hpp" },
       { "boost/function\\.hpp", "hpx/util/function.hpp" },
       { "hpx/hpx_fwd\\.hpp", "nothing (remove unconditionally)" },
