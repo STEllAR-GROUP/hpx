c3b11c86503f3c40ee3ab2b51c7e118983427099
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2016-07-29 07:56:59 -0500

    Addressing review comments

--- tests/regressions/util/use_all_cores_2262.cpp
+++ tests/regressions/util/use_all_cores_2262.cpp
@@ -3,6 +3,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// This test verifies that 'hpx.os_threads=all' is equivalent to specifying
+// all of the avalable cores (see #2262).
+
 #include <hpx/hpx_init.hpp>
 #include <hpx/include/threads.hpp>
 #include <hpx/util/lightweight_test.hpp>
@@ -19,9 +22,16 @@ int hpx_main(int argc, char* argv[])
     return hpx::finalize();
 }
 
-int main(int argc, char* argv[])
+// Ignore all command line options to avoid any interference with the test
+// runners.
+char* argv[] =
+{
+    "use_all_cores_2262", nullptr
+};
+
+int main()
 {
     std::vector<std::string> cfg = { "hpx.os_threads=all" };
-    HPX_TEST_EQ(hpx::init(argc, argv, cfg), 0);
+    HPX_TEST_EQ(hpx::init(1, argv, cfg), 0);
     return hpx::util::report_errors();
 }
