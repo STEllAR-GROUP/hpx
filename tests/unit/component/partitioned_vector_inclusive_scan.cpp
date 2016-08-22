0ae6f1b704b24a8a6e329e28afbf9dd894e7f05e
Minh-Khanh Do <minhkhanh.do@studium.uni-erlangen.de>
2016-08-22 04:43:57 +0200

    refactoring of partitioned inclusive scan in preparation for exclusive scan

--- tests/unit/component/partitioned_vector_inclusive_scan.cpp
+++ tests/unit/component/partitioned_vector_inclusive_scan.cpp
@@ -5,14 +5,11 @@
 
 #include <hpx/hpx_main.hpp>
 #include <hpx/include/partitioned_vector.hpp>
-#include <hpx/include/parallel_for_each.hpp>
 #include <hpx/include/parallel_scan.hpp>
-#include <hpx/include/parallel_copy.hpp>
-#include <hpx/util/zip_iterator.hpp>
 
 #include <hpx/util/lightweight_test.hpp>
 
-#include <boost/range/functions.hpp>
+#include "partitioned_vector_scan.hpp"
 
 #include <type_traits>
 #include <vector>
@@ -34,120 +31,6 @@ HPX_REGISTER_PARTITIONED_VECTOR(int);
         << " " << e << " " << f << " " << g << " " << h << " " << i << " ";
 
 ///////////////////////////////////////////////////////////////////////////////
-
-struct iota :
-    public hpx::parallel::v1::detail::algorithm<iota>
-{
-    iota()
-        : iota::algorithm("iota")
-    {}
-
-    template <typename ExPolicy, typename InIter, typename T>
-    static hpx::util::unused_type
-    sequential(ExPolicy && policy, InIter first, InIter last, T && init)
-    {
-        std::iota(first, last, init);
-        return hpx::util::unused;
-    }
-
-    template <typename ExPolicy, typename InIter, typename T>
-    static hpx::util::unused_type
-    parallel(ExPolicy && policy, InIter first, InIter last, T && init)
-    {
-        return hpx::util::void_guard<result_type>(),
-            sequential(policy, first, last, init);
-    }
-};
-
-template <typename T>
-void iota_vector(hpx::partitioned_vector<T>& v, T val)
-{
-    auto first = v.begin();
-    auto last = v.end();
-
-    typedef hpx::traits::segmented_iterator_traits<decltype(first)> traits;
-    typedef typename traits::segment_iterator segment_iterator;
-    typedef typename traits::local_iterator local_iterator_type;
-
-    segment_iterator sit = traits::segment(first);
-    segment_iterator send = traits::segment(last);
-
-    T temp_val = val;
-
-    for (; sit != send; ++sit)
-    {
-        local_iterator_type beg = traits::begin(sit);
-        local_iterator_type end = traits::end(sit);
-
-        hpx::parallel::v1::detail::dispatch(traits::get_id(sit),
-                iota(), hpx::parallel::seq, std::true_type(), beg, end, temp_val
-        );
-
-        temp_val += std::distance(beg, end);
-    }
-}
-
-template <typename Value>
-struct verify :
-    public hpx::parallel::v1::detail::algorithm<verify<Value>, Value>
-{
-    verify()
-        : verify::algorithm("verify")
-    {}
-
-    template <typename ExPolicy, typename SegIter, typename InIter>
-    static Value
-    sequential(ExPolicy && policy, SegIter first, SegIter last, InIter in)
-    {
-        return std::equal(first, last, in.begin());
-    }
-
-    template <typename ExPolicy, typename SegIter, typename InIter>
-    static Value
-    parallel(ExPolicy && policy, InIter first, InIter last, InIter in)
-    {
-        return sequential(policy, first, last, in);
-    }
-};
-
-
-template<typename T>
-void verify_values(hpx::partitioned_vector<T> v1, std::vector<T> v2)
-{
-    auto first = v1.begin();
-    auto last = v1.end();
-
-    typedef hpx::traits::segmented_iterator_traits<decltype(first)> traits;
-    typedef typename traits::segment_iterator segment_iterator;
-    typedef typename traits::local_iterator local_iterator_type;
-
-    segment_iterator sit = traits::segment(first);
-    segment_iterator send = traits::segment(last);
-
-    auto beg2 = v2.begin();
-
-    std::vector<bool> results;
-
-    for (; sit != send; ++sit)
-    {
-        local_iterator_type beg = traits::begin(sit);
-        local_iterator_type end = traits::end(sit);
-
-        std::vector<T> test(std::distance(beg, end));
-        std::copy_n(beg2, test.size(), test.begin());
-
-        results.push_back(
-            hpx::parallel::v1::detail::dispatch(traits::get_id(sit),
-                verify<bool>(), hpx::parallel::seq, std::true_type(), beg, end, test
-        ));
-
-        beg2 += std::distance(beg, end);
-    }
-    bool final_result = std::all_of(results.begin(), results.end(),
-        [](bool v) { return v; });
-
-    HPX_TEST(final_result);
-}
 
 template<typename T>
 struct opt
@@ -451,6 +334,8 @@ void inclusive_scan_tests()
     inclusive_scan_tests_with_policy<T>(length, hpx::container_layout(3));
     inclusive_scan_tests_with_policy<T>(length, hpx::container_layout(3, localities));
     inclusive_scan_tests_with_policy<T>(length, hpx::container_layout(localities));
+    
+    inclusive_scan_tests_with_policy<T>(1000, hpx::container_layout(1000));
 
     inclusive_scan_tests_segmented_out_with_policy<T>(length,
         hpx::container_layout(localities), hpx::container_layout(localities));
