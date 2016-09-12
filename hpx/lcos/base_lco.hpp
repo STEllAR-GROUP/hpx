7db5869462d7eefb0795a666934d8f0d24d2d5c1
Thomas Heller <thomas.heller@cs.fau.de>
2016-09-12 13:26:03 +0200

    Revert "Disabling parcel coalescing for LCOs"
    This reverts commit 074694396d088977a534db28caa46e38a101eed7.

--- hpx/lcos/base_lco.hpp
+++ hpx/lcos/base_lco.hpp
@@ -126,4 +126,12 @@ HPX_REGISTER_ACTION_DECLARATION(
 HPX_REGISTER_ACTION_DECLARATION(
     hpx::lcos::base_lco::disconnect_action, base_disconnect_action)
 
+///////////////////////////////////////////////////////////////////////////////
+HPX_ACTION_USES_MESSAGE_COALESCING_NOTHROW_DECLARATION(
+    hpx::lcos::base_lco::set_event_action, "lco_set_value_action",
+    std::size_t(-1), std::size_t(-1))
+HPX_ACTION_USES_MESSAGE_COALESCING_NOTHROW_DECLARATION(
+    hpx::lcos::base_lco::set_exception_action, "lco_set_value_action",
+    std::size_t(-1), std::size_t(-1))
+
 #endif
