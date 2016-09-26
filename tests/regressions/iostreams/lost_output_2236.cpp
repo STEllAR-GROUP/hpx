2f58fa6711167f4bc93e77eff3b47152008318e3
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2016-09-26 08:24:18 -0500

    Fix a warning about unsigned >= 0 being always true

--- tests/regressions/iostreams/lost_output_2236.cpp
+++ tests/regressions/iostreams/lost_output_2236.cpp
@@ -305,8 +305,8 @@ namespace server {
         state();
         if (w < weight)
         {
+            HPX_ASSERT(strong_count > 0);
             strong_count--;
-            HPX_ASSERT(strong_count >= 0);
         }
         else
         {
