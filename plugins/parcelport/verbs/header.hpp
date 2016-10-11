1bfefd6a484ac1109873cb5d9d1a1170390345a0
John Biddiscombe <biddisco@cscs.ch>
2016-10-11 22:49:46 +0200

    Cleanup message header to save a little space

--- plugins/parcelport/verbs/header.hpp
+++ plugins/parcelport/verbs/header.hpp
@@ -1,6 +1,6 @@
-//  Copyright (c) 2013-2014 Hartmut Kaiser
+//  Copyright (c) 2015-2016 John Biddiscombe
 //  Copyright (c) 2013-2015 Thomas Heller
-//  Copyright (c)      2015 John Biddiscombe
+//  Copyright (c) 2013-2014 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -28,52 +28,50 @@ namespace policies { namespace verbs
     template <int SIZE>
     struct header
     {
-        typedef int  value_type;
-        typedef char flag_type;
+        typedef boost::uint32_t value_type;
+        typedef char            flag_type;
         enum data_pos
         {
-            pos_tag               = 0 * sizeof(value_type),
-            pos_size              = 1 * sizeof(value_type),
-            pos_numbytes          = 2 * sizeof(value_type),
+            pos_message_tag       = 0 * sizeof(value_type),
+            pos_message_size      = 1 * sizeof(value_type),
             pos_numchunks_first   = 3 * sizeof(value_type),
             pos_numchunks_second  = 4 * sizeof(value_type),
-            pos_chunk_flag        = 5 * sizeof(value_type),
-            pos_piggy_back_flag   = 6 * sizeof(value_type),// + (1 * sizeof(flag_type)),
-            pos_chunk_offset      = 7 * sizeof(value_type),// + (2 * sizeof(flag_type)),
-            pos_piggy_back_offset = 8 * sizeof(value_type),// + (2 * sizeof(flag_type)) + 1,
-            pos_piggy_back_key    = 9 * sizeof(value_type),// + (2 * sizeof(flag_type)) + 1,
-            pos_piggy_back_addr   =10 * sizeof(value_type),// + (2 * sizeof(flag_type)) + 1,
-            pos_data_zone         =10 * sizeof(value_type) + sizeof(const void *)
+            pos_flags             = 5 * sizeof(value_type),
+            pos_chunk_offset      = 6 * sizeof(value_type),
+            pos_piggy_back_offset = 7 * sizeof(value_type),
+            pos_piggy_back_key    = 8 * sizeof(value_type),
+            pos_piggy_back_addr   = 9 * sizeof(value_type),
+            pos_data_zone         = 9 * sizeof(value_type) + sizeof(const void *)
         };
-
-        static int const data_size_ = SIZE;
-
+        //
+        static const unsigned int data_size_      = SIZE;
+        static const unsigned int chunk_flag      = 0x01;
+        static const unsigned int piggy_back_flag = 0x02;
+        //
         template <typename Buffer>
         header(Buffer const & buffer, uint32_t tag)
         {
             boost::int64_t size = static_cast<boost::int64_t>(buffer.size_);
-            boost::int64_t numbytes = static_cast<boost::int64_t>(buffer.data_size_);
 
             HPX_ASSERT(size <= (std::numeric_limits<value_type>::max)());
-            HPX_ASSERT(numbytes <= (std::numeric_limits<value_type>::max)());
 
-            set<pos_tag>(static_cast<value_type>(tag));
-            set<pos_size>(static_cast<value_type>(size));
-            set<pos_numbytes>(static_cast<value_type>(numbytes));
+            set<pos_message_tag>(static_cast<value_type>(tag));
+            set<pos_message_size>(static_cast<value_type>(size));
             set<pos_numchunks_first>(static_cast<value_type>(buffer.num_chunks_.first));
             set<pos_numchunks_second>(static_cast<value_type>(buffer.num_chunks_.second));
+
             // find out how much space is needed for chunk information
             const std::vector<serialization::serialization_chunk>& chunks = buffer.chunks_;
             size_t chunkbytes = chunks.size() * sizeof(serialization::serialization_chunk);
             // can we send the chunk info inside the header
             if (chunkbytes <= (data_size_ - pos_data_zone)) {
-              set<pos_chunk_flag>(static_cast<value_type>(1));
+              set<pos_flags>(get<pos_flags>() | static_cast<value_type>(chunk_flag));
               set<pos_chunk_offset>(static_cast<value_type>(pos_data_zone));
               std::memcpy(&data_[pos_data_zone], chunks.data(), chunkbytes);
               LOG_DEBUG_MSG("Chunkbytes is " << hexnumber(chunkbytes));
             }
             else {
-              set<pos_chunk_flag>(static_cast<value_type>(0));
+              set<pos_flags>(get<pos_flags>() & ~chunk_flag);
               set<pos_chunk_offset>(static_cast<value_type>(0));
               chunkbytes = 0;
             }
@@ -83,10 +81,10 @@ namespace policies { namespace verbs
 
             // can we send main message chunk as well as other information
             if (buffer.data_.size() <= (data_size_ - chunkbytes - pos_data_zone)) {
-                set<pos_piggy_back_flag>(static_cast<value_type>(1));
+                set<pos_flags>(get<pos_flags>() | static_cast<value_type>(piggy_back_flag));
             }
             else {
-              set<pos_piggy_back_flag>(static_cast<value_type>(0));
+                set<pos_flags>(get<pos_flags>() & ~piggy_back_flag);
             }
         }
 
@@ -98,21 +96,12 @@ namespace policies { namespace verbs
         void reset()
         {
             std::memset(&data_[0], -1, data_size_);
-            set<pos_piggy_back_flag>(static_cast<value_type>(1));
+            set<pos_flags>(0xFF);
         }
 
         bool valid() const
         {
             return data_[0] != -1;
-        }
-
-        void assert_valid() const
-        {
-            HPX_ASSERT(tag() != -1);
-            HPX_ASSERT(size() != -1);
-            HPX_ASSERT(numbytes() != -1);
-            HPX_ASSERT(num_chunks().first != -1);
-            HPX_ASSERT(num_chunks().second != -1);
         }
 
         char *data()
@@ -122,17 +111,12 @@ namespace policies { namespace verbs
 
         value_type tag() const
         {
-            return get<pos_tag>();
+            return get<pos_message_tag>();
         }
 
         value_type size() const
         {
-            return get<pos_size>();
-        }
-
-        value_type numbytes() const
-        {
-            return get<pos_numbytes>();
+            return get<pos_message_size>();
         }
 
         std::pair<value_type, value_type> num_chunks() const
@@ -143,7 +127,7 @@ namespace policies { namespace verbs
 
         char * piggy_back()
         {
-            if(get<pos_piggy_back_flag>()!=0) {
+            if((get<pos_flags>() & piggy_back_flag) !=0) {
                 return &data_[get<pos_piggy_back_offset>()];
             }
             return 0;
@@ -151,7 +135,7 @@ namespace policies { namespace verbs
 
         char * chunk_data()
         {
-            if(get<pos_chunk_flag>()!=0) {
+            if((get<pos_flags>() & chunk_flag) !=0) {
                 return &data_[get<pos_chunk_offset>()];
             }
             return 0;
@@ -160,7 +144,7 @@ namespace policies { namespace verbs
         std::size_t header_length()
         {
             // if chunks are included in header, return pos_data_zone + chunkbytes
-            if(get<pos_chunk_flag>())
+            if((get<pos_flags>() & chunk_flag) != 0)
                 return static_cast<std::size_t>(get<pos_piggy_back_offset>());
             // otherwise, just end of normal header
             else
