d3949ad41e5a41d6ad4dd813d10f21ab2f7726c4
atrantan <atrantan@lsu.edu>
2016-12-20 17:11:58 -0600

    Remove macro redefinition

--- examples/compute/cuda/partitioned_vector.cu
+++ examples/compute/cuda/partitioned_vector.cu
@@ -3,10 +3,6 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define HPX_APPLICATION_NAME partitioned_vector_cu
-#define HPX_APPLICATION_STRING "partitioned_vector_cu"
-#define HPX_APPLICATION_EXPORTS
-
 #include <hpx/include/compute.hpp>
 #include <hpx/include/partitioned_vector.hpp>
 #include <hpx/include/parallel_for_each.hpp>
