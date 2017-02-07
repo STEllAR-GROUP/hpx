d6c335912f3663c55d26435dcf0f1086d231831e
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-02-07 09:11:21 -0600

    Remove forward declaration of schedulers types

--- hpx/runtime/threads/policies/local_priority_queue_scheduler.hpp
+++ hpx/runtime/threads/policies/local_priority_queue_scheduler.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2007-2016 Hartmut Kaiser
+//  Copyright (c) 2007-2017 Hartmut Kaiser
 //  Copyright (c) 2011      Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -9,6 +9,7 @@
 
 #include <hpx/config.hpp>
 #include <hpx/runtime/threads/policies/affinity_data.hpp>
+#include <hpx/runtime/threads/policies/lockfree_queue_backends.hpp>
 #include <hpx/runtime/threads/policies/scheduler_base.hpp>
 #include <hpx/runtime/threads/policies/thread_queue.hpp>
 #include <hpx/runtime/threads/thread_data.hpp>
@@ -20,6 +21,7 @@
 
 #include <boost/atomic.hpp>
 #include <boost/exception_ptr.hpp>
+#include <boost/thread/mutex.hpp>
 
 #include <cstddef>
 #include <cstdint>
@@ -51,12 +53,11 @@ namespace hpx { namespace threads { namespace policies
     /// High priority threads are executed by the first N OS threads before any
     /// other work is executed. Low priority threads are executed by the last
     /// OS thread whenever no other work is available.
-    template <typename Mutex
-            , typename PendingQueuing
-            , typename StagedQueuing
-            , typename TerminatedQueuing
-             >
-    class local_priority_queue_scheduler : public scheduler_base
+    template <typename Mutex = boost::mutex,
+        typename PendingQueuing = lockfree_fifo,
+        typename StagedQueuing = lockfree_fifo,
+        typename TerminatedQueuing = lockfree_lifo>
+    class HPX_EXPORT local_priority_queue_scheduler : public scheduler_base
     {
     protected:
         // The maximum number of active threads this thread manager should
--- hpx/runtime/threads_fwd.hpp
+++ hpx/runtime/threads_fwd.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2007-2014 Hartmut Kaiser
+//  Copyright (c) 2007-2017 Hartmut Kaiser
 //  Copyright (c) 2011      Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -10,90 +10,14 @@
 #include <hpx/config.hpp>
 #include <hpx/runtime/threads/thread_data_fwd.hpp>
 #include <hpx/runtime/threads/thread_enums.hpp>
-#include <hpx/util/unique_function.hpp>
-
-#include <boost/thread/mutex.hpp>
 
 namespace hpx
 {
-    /// \namespace threads
-    ///
-    /// The namespace \a thread-manager contains all the definitions required
-    /// for the scheduling, execution and general management of \a
-    /// hpx#threadmanager#thread's.
     namespace threads
     {
         namespace policies
         {
             struct scheduler_base;
-
-            struct lockfree_fifo;
-            struct lockfree_lifo;
-
-            // multi priority scheduler with work-stealing
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT local_priority_queue_scheduler;
-
-            // single priority scheduler with work-stealing
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT local_queue_scheduler;
-
-#if defined(HPX_HAVE_PERIODIC_PRIORITY_SCHEDULER)
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT periodic_priority_queue_scheduler;
-#endif
-
-#if defined(HPX_HAVE_STATIC_PRIORITY_SCHEDULER)
-            // multi priority scheduler with no work-stealing
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT static_priority_queue_scheduler;
-#endif
-
-#if defined(HPX_HAVE_STATIC_SCHEDULER)
-            // single priority scheduler with no work-stealing
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT static_queue_scheduler;
-#endif
-
-#if defined(HPX_HAVE_THROTTLE_SCHEDULER) && defined(HPX_HAVE_APEX)
-            // single priority scheduler with work-stealing and throttling
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT throttle_queue_scheduler;
-#endif
-
-#if defined(HPX_HAVE_HIERARCHY_SCHEDULER)
-            template <typename Mutex = boost::mutex
-                    , typename PendingQueuing = lockfree_fifo
-                    , typename StagedQueuing = lockfree_fifo
-                    , typename TerminatedQueuing = lockfree_lifo
-                     >
-            class HPX_EXPORT hierarchy_scheduler;
-#endif
-
             class HPX_EXPORT callback_notifier;
         }
 
