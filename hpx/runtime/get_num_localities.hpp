2e8e143937fb5f1bd558a369385ea20227c099a4
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-02-09 14:37:36 -0600

    Relaxing header dependencies to avoid circular #include problems

--- hpx/runtime/get_num_localities.hpp
+++ hpx/runtime/get_num_localities.hpp
@@ -11,7 +11,7 @@
 
 #include <hpx/config.hpp>
 #include <hpx/exception_fwd.hpp>
-#include <hpx/lcos/future.hpp>
+#include <hpx/lcos_fwd.hpp>
 #include <hpx/runtime/launch_policy.hpp>
 #include <hpx/runtime/components/component_type.hpp>
 
