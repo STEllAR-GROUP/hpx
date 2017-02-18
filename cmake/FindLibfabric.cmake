128662d4dc0d05bb4a0b71fae5af62a32548f8ff
John Biddiscombe <biddisco@cscs.ch>
2017-02-19 00:55:20 +0100

    First work on libfabric parcelport
    Memory registration and pools converted from verbs version
    and working as intended.
    
    Most other code copied over from verbs and not yet modified
    to work in new libfabric domain.

---
+++ cmake/FindLibfabric.cmake
@@ -0,0 +1,36 @@
+
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBFABRIC QUIET libfabric)
+
+find_path(LIBFABRIC_INCLUDE_DIR rdma/fabric.h
+  HINTS
+    ${LIBFABRIC_ROOT} ENV LIBFABRIC_ROOT
+    ${LIBFABRIC_DIR} ENV LIBFABRIC_DIR
+  PATH_SUFFIXES include)
+
+find_library(LIBFABRIC_LIBRARY NAMES fabric
+  HINTS
+    ${LIBFABRIC_ROOT} ENV LIBFABRIC_ROOT
+  PATH_SUFFIXES lib lib64)
+
+set(LIBFABRIC_LIBRARIES ${LIBFABRIC_LIBRARY} CACHE INTERNAL "")
+set(LIBFABRIC_INCLUDE_DIRS ${LIBFABRIC_INCLUDE_DIR} CACHE INTERNAL "")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Libfabric DEFAULT_MSG
+  LIBFABRIC_LIBRARY LIBFABRIC_INCLUDE_DIR)
+
+#foreach(v LIBFABRIC_ROOT)
+#  get_property(_type CACHE ${v} PROPERTY TYPE)
+#  if(_type)
+#    set_property(CACHE ${v} PROPERTY ADVANCED 1)
+#    if("x${_type}" STREQUAL "xUNINITIALIZED")
+#      set_property(CACHE ${v} PROPERTY TYPE PATH)
+#    endif()
+#  endif()
+#endforeach()
+
+mark_as_advanced(LIBFABRIC_ROOT LIBFABRIC_LIBRARY LIBFABRIC_INCLUDE_DIR)
---
+++ plugins/parcelport/libfabric/config-gni.h
@@ -0,0 +1,81 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* defined to 1 if configured with --enable-debug */
+#define ENABLE_DEBUG 0
+
+/* Define to 1 if clock_gettime is available. */
+#define HAVE_CLOCK_GETTIME 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Defined to 1 if Linux epoll is available */
+#define HAVE_EPOLL 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `fabric' library (-lfabric). */
+#define HAVE_LIBFABRIC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 to enable valgrind annotations */
+/* #undef INCLUDE_VALGRIND */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "fabtests"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "ofiwg@lists.openfabrics.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "fabtests"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "fabtests 1.4.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "fabtests"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.4.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.4.1"
