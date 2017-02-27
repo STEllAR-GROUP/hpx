93052261aa57d2842049d8b09411d6a3704396d8
John Biddiscombe <biddisco@cscs.ch>
2017-02-27 15:08:32 +0100

    Simplify parcelport logging macros to reduce duplication

--- plugins/parcelport/parcelport_logging.hpp
+++ plugins/parcelport/parcelport_logging.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2014-2016 John Biddiscombe
+//  Copyright (c) 2014-2017 John Biddiscombe
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -13,15 +13,35 @@
 #include <chrono>
 //
 #include <hpx/config.hpp>
-#include <hpx/runtime/threads/thread.hpp>
-//
 #include <hpx/config/parcelport_defines.hpp>
+#include <hpx/runtime/threads/thread.hpp>
 //
 #include <boost/preprocessor.hpp>
 
-//
+// ------------------------------------------------------------------
+// Set flags to help simplify the log defines
+// ------------------------------------------------------------------
+
+// full logging support, we want everything
+#if   defined(HPX_PARCELPORT_VERBS_HAVE_LOGGING)      || \
+      defined(HPX_PARCELPORT_LIBFABRIC_HAVE_LOGGING)
+#  define HPX_PARCELPORT_LOGGING_INCLUDE_FILES
+#  define HPX_PARCELPORT_LOGGING_HAVE_TRACE_LOG
+#  define HPX_PARCELPORT_LOGGING_HAVE_DEBUG_LOG
+#  define HPX_PARCELPORT_LOGGING_HAVE_TIMED_LOG
+#  define HPX_PARCELPORT_LOGGING_HAVE_DEVEL_LOG
+
+// just a subset of logging for dev mode enabled
+#elif defined(HPX_PARCELPORT_VERBS_HAVE_DEV_MODE)     || \
+      defined(HPX_PARCELPORT_LIBFABRIC_HAVE_DEV_MODE)
+#  define HPX_PARCELPORT_LOGGING_INCLUDE_FILES
+#  define HPX_PARCELPORT_LOGGING_HAVE_TIMED_LOG
+#  define HPX_PARCELPORT_LOGGING_HAVE_DEVEL_LOG
+#endif
+
+// ------------------------------------------------------------------
 // useful macros for formatting log messages
-//
+// ------------------------------------------------------------------
 #define nhex(n) "0x" << std::setfill('0') << std::setw(n) << std::noshowbase << std::hex
 #define hexpointer(p) nhex(16) << (uintptr_t)(p) << " "
 #define hexuint64(p)  nhex(16) << (uintptr_t)(p) << " "
@@ -38,6 +58,9 @@
                                << (int)(reinterpret_cast<const uint8_t*>(&p))[3] << " "
 #define sockaddress(p) ipaddress(((struct sockaddr_in*)(p))->sin_addr.s_addr)
 
+// ------------------------------------------------------------------
+// helper class for printing thread ID
+// ------------------------------------------------------------------
 namespace hpx {
 namespace parcelset {
 namespace policies {
@@ -65,14 +88,10 @@ namespace detail {
 #define THREAD_ID "" \
     << hpx::parcelset::policies::libfabric::detail::rdma_thread_print_helper()
 
-//
-// If any log options are ON then we must include the boost log files
-//
-#if defined(HPX_PARCELPORT_VERBS_HAVE_DEV_MODE)     || \
-    defined(HPX_PARCELPORT_VERBS_HAVE_LOGGING)      || \
-    defined(HPX_PARCELPORT_LIBFABRIC_HAVE_DEV_MODE) || \
-    defined(HPX_PARCELPORT_LIBFABRIC_HAVE_LOGGING)
-
+// ------------------------------------------------------------------
+// include files needed for boost::log
+// ------------------------------------------------------------------
+#ifdef HPX_PARCELPORT_LOGGING_INCLUDE_FILES
 #  include <boost/log/trivial.hpp>
 #  include <boost/log/expressions/formatter.hpp>
 #  include <boost/log/expressions/formatters.hpp>
@@ -86,13 +105,20 @@ namespace detail {
 #  include <boost/log/utility/setup/common_attributes.hpp>
 #endif
 
-//
-// Full Logging enabled
-//
-#if defined(HPX_PARCELPORT_LIBFABRIC_HAVE_LOGGING) || \
-    defined(HPX_PARCELPORT_VERBS_HAVE_LOGGING)
-
+// ------------------------------------------------------------------
+// Trace messages are enabled for full debug
+// ------------------------------------------------------------------
+#ifdef HPX_PARCELPORT_LOGGING_HAVE_TRACE_LOG
 #  define LOG_TRACE_MSG(x) BOOST_LOG_TRIVIAL(trace)   << THREAD_ID << " " << x;
+#else
+#  define LOG_TRACE_MSG(x)
+#endif
+
+// ------------------------------------------------------------------
+// if enabled : define all main logging macros
+// ------------------------------------------------------------------
+#ifdef HPX_PARCELPORT_LOGGING_HAVE_DEBUG_LOG
+
 #  define LOG_DEBUG_MSG(x) BOOST_LOG_TRIVIAL(debug)   << THREAD_ID << " " << x;
 #  define LOG_INFO_MSG(x)  BOOST_LOG_TRIVIAL(info)    << THREAD_ID << " " << x;
 #  define LOG_WARN_MSG(x)  BOOST_LOG_TRIVIAL(warning) << THREAD_ID << " " << x;
@@ -103,75 +129,44 @@ namespace detail {
 //
 #  define FUNC_START_DEBUG_MSG LOG_TRACE_MSG("*** Enter " << __func__);
 #  define FUNC_END_DEBUG_MSG   LOG_TRACE_MSG("### Exit  " << __func__);
-
+//
 #  define LOG_FORMAT_MSG(x)                                    \
     (dynamic_cast<std::ostringstream &> (                      \
         std::ostringstream().seekp(0, std::ios_base::cur) << x \
         << __FILE__ << " " << std::dec << __LINE__ )).str()
 
-#  define LOG_TIMED_INIT(name)                                                      \
-    using namespace std::chrono;                                                    \
-    static time_point<system_clock> log_timed_start_ ## name = system_clock::now(); \
-
-#  define LOG_TIMED_MSG(name, level, delay, x)             \
-    time_point<system_clock> log_timed_now_ ## name =      \
-        system_clock::now();                               \
-    duration<double> log_timed_elapsed_ ## name =          \
-      log_timed_now_ ## name - log_timed_start_ ## name;   \
-    if (log_timed_elapsed_ ## name.count()>delay) {        \
-        LOG_DEVEL_MSG(x);                                  \
-        log_timed_start_ ## name = log_timed_now_ ## name; \
-    }
-
-#  define LOG_TIMED_BLOCK(name, level, delay, x)           \
-    time_point<system_clock> log_timed_now_ ## name =      \
-        system_clock::now();                               \
-    duration<double> log_timed_elapsed_ ## name =          \
-      log_timed_now_ ## name - log_timed_start_ ## name;   \
-    if (log_timed_elapsed_ ## name.count()>delay) {        \
-        log_timed_start_ ## name = log_timed_now_ ## name; \
-        x;                                                 \
-    }
-
-//
-// Logging disabled, #define all macros (except for error), to be empty
-//
 #else
-#  define LOG_DEVEL_MSG(x)
 #  define LOG_DEBUG_MSG(x)
-#  define LOG_TRACE_MSG(x)
 #  define LOG_INFO_MSG(x)
 #  define LOG_WARN_MSG(x)
 #  define LOG_ERROR_MSG(x) std::cout << "00: <ERROR> " << THREAD_ID << " " \
     << x << " " << __FILE__ << " " << std::dec << __LINE__ << std::endl;
-#  define LOG_FORMAT_MSG(x) ""
+#  define LOG_FATAL_MSG(x) LOG_ERROR_MSG(x)
+//
 #  define LOG_EXCLUSIVE(x)
-
+//
 #  define FUNC_START_DEBUG_MSG
 #  define FUNC_END_DEBUG_MSG
+//
+#  define LOG_FORMAT_MSG(x) ""
 
-#  define LOG_TIMED_INIT(name)
-#  define LOG_TIMED_MSG(name, level, delay, x)
-#  define LOG_TIMED_BLOCK(name, level, delay, x)
 #endif
-//
-// Log DEVEL : just enable the LOG_DEVEL macro to bypass most log output
+
+// ------------------------------------------------------------------
+// dev logging: just enable the LOG_DEVEL macro to bypass most log output
 // but still show some that have been specially marked
-//
-#if defined(HPX_PARCELPORT_VERBS_HAVE_DEV_MODE) || \
-    defined(HPX_PARCELPORT_LIBFABRIC_HAVE_DEV_MODE)
-#  undef  LOG_TRACE_MSG
-#  undef  LOG_DEVEL_MSG
-#  undef  LOG_DEBUG_MSG
-#  undef  LOG_EXCLUSIVE
-#  undef  LOG_TIMED_INIT
-#  undef  LOG_TIMED_MSG
-#  undef  LOG_TIMED_BLOCK
-#  define LOG_EXCLUSIVE(x) x
-#  define LOG_TRACE_MSG(x) BOOST_LOG_TRIVIAL(trace) << "" << THREAD_ID << " " << x;
+// ------------------------------------------------------------------
+#ifdef HPX_PARCELPORT_LOGGING_HAVE_DEVEL_LOG
 #  define LOG_DEVEL_MSG(x) BOOST_LOG_TRIVIAL(debug) << "" << THREAD_ID << " " << x;
-#  define LOG_DEBUG_MSG(x) BOOST_LOG_TRIVIAL(debug) << "" << THREAD_ID << " " << x;
-#  define LOG_BOOST_MSG(x,y) BOOST_LOG_TRIVIAL(debug) << "" << THREAD_ID << " " << boost::format(x) y;
+#else
+#  define LOG_DEVEL_MSG(x)
+#endif
+
+// ------------------------------------------------------------------
+// Timed log macros : used during long loops to avoid excessive output
+// only prints the log messge every N seconds
+// ------------------------------------------------------------------
+#ifdef HPX_PARCELPORT_LOGGING_HAVE_TIMED_LOG
 
 #  define LOG_TIMED_INIT(name)                                                      \
     using namespace std::chrono;                                                    \
@@ -197,13 +192,18 @@ namespace detail {
         x;                                                 \
     }
 
+#else
+#  define LOG_TIMED_INIT(name)
+#  define LOG_TIMED_MSG(name, level, delay, x)
+#  define LOG_TIMED_BLOCK(name, level, delay, x)
 #endif
 
-//
+// ------------------------------------------------------------------
 // Utility to allow automatic printing of enum names in log messages
 //
 // example of usage
 // DEFINE_ENUM_WITH_STRING_CONVERSIONS(test_type, (test1)(test2)(test3))
+// ------------------------------------------------------------------
 
 #  define X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE(r, data, elem)  \
     case elem : return BOOST_PP_STRINGIZE(elem);
