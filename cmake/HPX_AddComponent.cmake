e3c47792e375833a865440d787243f5306a8cbb1
atrantan <atrantan@lsu.edu>
2017-03-22 14:46:06 -0500

    Manage .cu files in add_hpx_component()

--- cmake/HPX_AddComponent.cmake
+++ cmake/HPX_AddComponent.cmake
@@ -131,6 +131,17 @@ macro(add_hpx_component name)
     endif()
   endif()
 
+  # Manage files with .cu extension in case When Cuda Clang is used
+  if(HPX_WITH_CUDA_CLANG)
+    foreach(source ${${name}_SOURCES})
+      get_filename_component(extension ${source} EXT)
+      if(${extension} STREQUAL ".cu")
+        SET_SOURCE_FILES_PROPERTIES(${source} PROPERTIES
+          LANGUAGE CXX)
+      endif()
+    endforeach()
+  endif()
+
   if(HPX_WITH_CUDA AND NOT HPX_WITH_CUDA_CLANG)
     cuda_add_library(${name}_component ${${name}_lib_linktype} ${exclude_from_all}
       ${${name}_SOURCES} ${${name}_HEADERS})
@@ -167,6 +178,7 @@ macro(add_hpx_component name)
     TYPE COMPONENT
     NAME ${name}
     EXPORT
+    SOURCES ${${name}_SOURCES}
     FOLDER ${${name}_FOLDER}
     COMPILE_FLAGS ${${name}_COMPILE_FLAGS}
     LINK_FLAGS ${${name}_LINK_FLAGS}
