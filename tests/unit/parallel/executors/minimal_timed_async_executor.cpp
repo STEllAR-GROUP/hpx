b876477302b07cbff7278dd6c9183f74e8a7c532
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-04-26 10:28:06 -0500

    Add missing #include

--- tests/unit/parallel/executors/minimal_timed_async_executor.cpp
+++ tests/unit/parallel/executors/minimal_timed_async_executor.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2007-2016 Hartmut Kaiser
+//  Copyright (c) 2007-2017 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,6 +12,7 @@
 #include <boost/range/functions.hpp>
 
 #include <algorithm>
+#include <array>
 #include <cstddef>
 #include <chrono>
 #include <cstdlib>
