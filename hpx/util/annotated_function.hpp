b0b06479392c4c0c58b664b1894f18c8b1cad0fa
Thomas Heller <thomas.heller@cs.fau.de>
2017-05-04 10:22:26 +0200

    Fixing hpx::util::annotated_function
    When used with the service_executor, annotated_function might be instantiated
    from a non-HPX thread

--- hpx/util/annotated_function.hpp
+++ hpx/util/annotated_function.hpp
@@ -78,22 +78,27 @@ namespace hpx { namespace util
         HPX_NON_COPYABLE(annotate_function);
 
         explicit annotate_function(char const* name)
-          : desc_(hpx::threads::set_thread_description(
-                hpx::threads::get_self_id(), name))
+          : desc_(hpx::threads::get_self_ptr() ?
+                hpx::threads::set_thread_description(hpx::threads::get_self_id(),
+                    name) :
+                nullptr)
         {}
         template <typename F>
         explicit annotate_function(F && f)
-          : desc_(hpx::threads::set_thread_description(
-                hpx::threads::get_self_id(),
-                hpx::traits::get_function_annotation<
-                    typename std::decay<F>::type
-                >::call(f)))
+          : desc_(hpx::threads::get_self_ptr() ?
+                hpx::threads::set_thread_description(
+                    hpx::threads::get_self_id(),
+                    hpx::traits::get_function_annotation<
+                        typename std::decay<F>::type
+                    >::call(f)) :
+                nullptr)
         {}
 
         ~annotate_function()
         {
-            hpx::threads::set_thread_description(
-                hpx::threads::get_self_id(), desc_);
+            if (hpx::threads::get_self_ptr())
+                hpx::threads::set_thread_description(
+                    hpx::threads::get_self_id(), desc_);
         }
 
         hpx::util::thread_description desc_;
