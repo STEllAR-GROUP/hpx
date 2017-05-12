f9a355abd302bb0f67b9e2a5973d87f82bbe58c7
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-05-12 08:38:29 -0500

    Adding missing #include
    - flyby: fixing test, adapting to new executor API

--- tests/regressions/lcos/barrier_hang.cpp
+++ tests/regressions/lcos/barrier_hang.cpp
@@ -7,13 +7,19 @@
 #include <hpx/hpx_main.hpp>
 #include <hpx/lcos/barrier.hpp>
 #include <hpx/lcos/broadcast.hpp>
+#include <hpx/parallel/execution.hpp>
 #include <hpx/parallel/execution_policy.hpp>
+#include <hpx/util/lightweight_test.hpp>
 
 #include <cstddef>
 #include <cstdio>
 #include <iostream>
 #include <string>
 
+#include <boost/atomic.hpp>
+#include <boost/range/irange.hpp>
+
+boost::atomic<std::size_t> local_count;
 
 struct spmd_block_helper
 {
@@ -23,58 +29,52 @@ struct spmd_block_helper
     template <typename ... Ts>
     void operator()(std::size_t image_id, Ts && ... ts) const
     {
-        printf("image_id is equal to %lu and num_images is %lu\n",
-            image_id, num_images_);
-
-        std::cout << "The barrier name is "
-            << name_ + "_barrier" << std::endl;
-
-        hpx::lcos::barrier
-            barrier(name_ + "_barrier" , num_images_, image_id);
-
         // Ensure that other images reaches that point
+        hpx::lcos::barrier barrier(name_ + "_barrier" , num_images_, image_id);
         barrier.wait();
+
+        ++local_count;
     }
 };
 
-
-void bulk_test(
+std::size_t bulk_test(
     std::string name,
     std::size_t images_per_locality,
     std::size_t num_images)
 {
-
-    using hpx::parallel::execution::par;
-
     std::size_t offset = hpx::get_locality_id();
     offset *= images_per_locality;
 
-    return
-        hpx::parallel::executor_traits<
-            decltype(par)::executor_type
-            >::bulk_execute(
-                par.executor(),
-                spmd_block_helper{name,num_images},
-                boost::irange(
-                    offset, offset + images_per_locality));
+    local_count.store(0);
+
+    hpx::parallel::execution::sync_bulk_execute(
+        hpx::parallel::execution::par.executor(),
+        spmd_block_helper{name, num_images},
+        boost::irange(offset, offset + images_per_locality));
+
+    return local_count.load();
 }
 HPX_PLAIN_ACTION(bulk_test, bulk_test_action);
-
-
 
 int main()
 {
     std::string name = "test";
     std::size_t images_per_locality = 4;
-    std::size_t num_images
-        = hpx::get_num_localities(hpx::launch::sync) * images_per_locality;
+    std::size_t num_localities = hpx::get_num_localities(hpx::launch::sync);
+    std::size_t num_images = num_localities * images_per_locality;
 
     bulk_test_action act;
 
-    hpx::util::unwrapped(hpx::lcos::broadcast(
-        act, hpx::find_all_localities(), name, images_per_locality, num_images));
+    std::vector<std::size_t> result = hpx::lcos::broadcast(
+            act, hpx::find_all_localities(), name,
+            images_per_locality, num_images
+        ).get();
 
-    std::cout << "done...\n";
+    for (std::size_t s : result)
+    {
+        HPX_TEST_EQ(s, images_per_locality);
+    }
+    HPX_TEST_EQ(num_localities, result.size());
 
-    return 0;
+    return hpx::util::report_errors();
 }
