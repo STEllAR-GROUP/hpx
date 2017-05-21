87a6547c5e392c314486afe40b70fc05528379cb
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-05-21 16:53:03 -0500

    Reduce output noise from cmake create_symbolic_link macro (Windows)

--- cmake/scripts/create_symbolic_link.bat
+++ cmake/scripts/create_symbolic_link.bat
@@ -2,5 +2,5 @@
 ::
 :: Distributed under the Boost Software License, Version 1.0. (See accompanying
 :: file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-mklink /J "%1" "%2"
+::
+@mklink /J "%1" "%2" > NUL
