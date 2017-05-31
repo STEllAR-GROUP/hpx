50078d08442a9f6b4dafaefe8b045b039ffaf81c
Taeguk Kwon <xornrbboy@gmail.com>
2017-05-31 03:33:31 -0500

    Use c++11 inline namespace with considering #2653.

--- hpx/parallel/algorithms/is_heap.hpp
+++ hpx/parallel/algorithms/is_heap.hpp
@@ -16,7 +16,6 @@
 #include <hpx/util/invoke.hpp>
 
 #include <hpx/parallel/algorithms/detail/dispatch.hpp>
-#include <hpx/parallel/config/inline_namespace.hpp>
 #include <hpx/parallel/execution_policy.hpp>
 #include <hpx/parallel/executors/execution.hpp>
 #include <hpx/parallel/traits/projected.hpp>
@@ -37,7 +36,7 @@
 #include <type_traits>
 #include <utility>
 
-namespace hpx { namespace parallel { HPX_INLINE_NAMESPACE(v1)
+namespace hpx { namespace parallel { inline namespace v1
 {
     ///////////////////////////////////////////////////////////////////////////
     // is_heap
