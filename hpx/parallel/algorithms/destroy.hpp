52f143c0331c85c22782fb8738b63a5633b454ab
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-06-02 12:24:45 -0500

    Adding parallel::destroy and destroy_n

---
+++ hpx/parallel/algorithms/destroy.hpp
@@ -0,0 +1,289 @@
+//  Copyright (c) 2014-2017 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file parallel/algorithms/destroy.hpp
+
+#if !defined(HPX_PARALLEL_DETAIL_destroy_JUN_01_2017_1049AM)
+#define HPX_PARALLEL_DETAIL_destroy_JUN_01_2017_1049AM
+
+#include <hpx/config.hpp>
+#include <hpx/traits/is_iterator.hpp>
+#include <hpx/util/void_guard.hpp>
+
+#include <hpx/parallel/algorithms/detail/dispatch.hpp>
+#include <hpx/parallel/algorithms/detail/is_negative.hpp>
+#include <hpx/parallel/execution_policy.hpp>
+#include <hpx/parallel/util/detail/algorithm_result.hpp>
+#include <hpx/parallel/util/loop.hpp>
+#include <hpx/parallel/util/foreach_partitioner.hpp>
+#include <hpx/parallel/util/projection_identity.hpp>
+
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <memory>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+namespace hpx { namespace parallel { inline namespace v1
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // destroy
+    namespace detail
+    {
+        /// \cond NOINTERNAL
+
+        // provide our own implementation of std::destroy
+        // as some versions of MSVC horribly fail at compiling it for some types
+        // T
+        template <typename FwdIter>
+        void std_destroy(FwdIter first, FwdIter last)
+        {
+            typedef typename std::iterator_traits<FwdIter>::value_type
+                value_type;
+
+            for (/* */; first != last; ++first)
+            {
+                std::addressof(*first)->~value_type();
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        template <typename ExPolicy, typename FwdIter>
+        typename util::detail::algorithm_result<ExPolicy, FwdIter>::type
+        parallel_sequential_destroy_n(
+            ExPolicy && policy, FwdIter first, std::size_t count)
+        {
+            if (count == 0)
+            {
+                return util::detail::algorithm_result<ExPolicy, FwdIter>::get(
+                    std::move(first));
+            }
+
+            return util::foreach_partitioner<ExPolicy>::call(
+                std::forward<ExPolicy>(policy), first, count,
+                [](FwdIter first, std::size_t count, std::size_t)
+                {
+                    typedef typename std::iterator_traits<FwdIter>::value_type
+                        value_type;
+
+                    return util::loop_n<ExPolicy>(first, count,
+                        [](FwdIter it)
+                        {
+                            std::addressof(*it)->~value_type();
+                        });
+                },
+                util::projection_identity());
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        template <typename FwdIter>
+        struct destroy
+          : public detail::algorithm<destroy<FwdIter> >
+        {
+            destroy()
+              : destroy::algorithm("destroy")
+            {}
+
+            template <typename ExPolicy>
+            static hpx::util::unused_type
+            sequential(ExPolicy, FwdIter first, FwdIter last)
+            {
+                std_destroy(first, last);
+                return hpx::util::unused;
+            }
+
+            template <typename ExPolicy>
+            static typename util::detail::algorithm_result<ExPolicy>::type
+            parallel(ExPolicy && policy, FwdIter first, FwdIter last)
+            {
+                return util::detail::algorithm_result<ExPolicy>::get(
+                    parallel_sequential_destroy_n(
+                        std::forward<ExPolicy>(policy), first,
+                        std::distance(first, last)));
+            }
+        };
+        /// \endcond
+    }
+
+    /// Destroys objects of type typename iterator_traits<ForwardIt>::value_type
+    /// in the range [first, last).
+    ///
+    /// \note   Complexity: Performs exactly \a last - \a first operations.
+    ///
+    /// \tparam ExPolicy    The type of the execution policy to use (deduced).
+    ///                     It describes the manner in which the execution
+    ///                     of the algorithm may be parallelized and the manner
+    ///                     in which it executes the assignments.
+    /// \tparam FwdIter     The type of the source iterators used (deduced).
+    ///                     This iterator type must meet the requirements of an
+    ///                     forward iterator.
+    ///
+    /// \param policy       The execution policy to use for the scheduling of
+    ///                     the iterations.
+    /// \param first        Refers to the beginning of the sequence of elements
+    ///                     the algorithm will be applied to.
+    /// \param last         Refers to the end of the sequence of elements the
+    ///                     algorithm will be applied to.
+    ///
+    /// The operations in the parallel \a destroy
+    /// algorithm invoked with an execution policy object of type \a sequenced_policy
+    /// execute in sequential order in the calling thread.
+    ///
+    /// The operations in the parallel \a destroy
+    /// algorithm invoked with an execution policy object of type \a parallel_policy
+    /// or \a parallel_task_policy are permitted to execute in an
+    /// unordered fashion in unspecified threads, and indeterminately sequenced
+    /// within each thread.
+    ///
+    /// \returns  The \a destroy algorithm returns a
+    ///           \a hpx::future<void>, if the execution policy is of type
+    ///           \a sequenced_task_policy or
+    ///           \a parallel_task_policy and returns \a void otherwise.
+    ///
+    template <typename ExPolicy, typename FwdIter,
+    HPX_CONCEPT_REQUIRES_(
+        execution::is_execution_policy<ExPolicy>::value &&
+        hpx::traits::is_iterator<FwdIter>::value)>
+    typename util::detail::algorithm_result<ExPolicy>::type
+    destroy(ExPolicy && policy, FwdIter first, FwdIter last)
+    {
+        static_assert(
+            (hpx::traits::is_forward_iterator<FwdIter>::value),
+            "Required at least forward iterator.");
+
+        typedef std::integral_constant<bool,
+                execution::is_sequential_execution_policy<ExPolicy>::value
+            > is_seq;
+
+        return detail::destroy<FwdIter>().call(
+            std::forward<ExPolicy>(policy), is_seq(), first, last);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // destroy_n
+    namespace detail
+    {
+        /// \cond NOINTERNAL
+
+        // provide our own implementation of std::destroy
+        // as some versions of MSVC horribly fail at compiling it for some
+        // types T
+        template <typename FwdIter>
+        FwdIter std_destroy_n(FwdIter first, std::size_t count)
+        {
+            typedef typename std::iterator_traits<FwdIter>::value_type
+                value_type;
+
+            for (/* */; count != 0; (void) ++first, --count)
+            {
+                std::addressof(*first)->~value_type();
+            }
+
+            return first;
+        }
+
+        template <typename FwdIter>
+        struct destroy_n
+          : public detail::algorithm<
+                destroy_n<FwdIter>, FwdIter>
+        {
+            destroy_n()
+              : destroy_n::algorithm("destroy_n")
+            {}
+
+            template <typename ExPolicy>
+            static FwdIter sequential(ExPolicy, FwdIter first, std::size_t count)
+            {
+                return std_destroy_n(first, count);
+            }
+
+            template <typename ExPolicy>
+            static typename util::detail::algorithm_result<
+                ExPolicy, FwdIter
+            >::type
+            parallel(ExPolicy && policy, FwdIter first, std::size_t count)
+            {
+                return parallel_sequential_destroy_n(
+                    std::forward<ExPolicy>(policy), first, count);
+            }
+        };
+        /// \endcond
+    }
+
+    /// Destroys objects of type typename iterator_traits<ForwardIt>::value_type
+    /// in the range [first, first + count).
+    ///
+    /// \note   Complexity: Performs exactly \a count operations, if
+    ///         count > 0, no assignments otherwise.
+    ///
+    /// \tparam ExPolicy    The type of the execution policy to use (deduced).
+    ///                     It describes the manner in which the execution
+    ///                     of the algorithm may be parallelized and the manner
+    ///                     in which it executes the assignments.
+    /// \tparam FwdIter     The type of the source iterators used (deduced).
+    ///                     This iterator type must meet the requirements of an
+    ///                     forward iterator.
+    /// \tparam Size        The type of the argument specifying the number of
+    ///                     elements to apply this algorithm to.
+    ///
+    /// \param policy       The execution policy to use for the scheduling of
+    ///                     the iterations.
+    /// \param first        Refers to the beginning of the sequence of elements
+    ///                     the algorithm will be applied to.
+    /// \param count        Refers to the number of elements starting at
+    ///                     \a first the algorithm will be applied to.
+    ///
+    /// The operations in the parallel \a destroy_n
+    /// algorithm invoked with an execution policy object of type
+    /// \a sequenced_policy execute in sequential order in the
+    /// calling thread.
+    ///
+    /// The operations in the parallel \a destroy_n
+    /// algorithm invoked with an execution policy object of type
+    /// \a parallel_policy or
+    /// \a parallel_task_policy are permitted to execute in an
+    /// unordered fashion in unspecified threads, and indeterminately sequenced
+    /// within each thread.
+    ///
+    /// \returns  The \a destroy_n algorithm returns a
+    ///           \a hpx::future<FwdIter> if the execution policy is of type
+    ///           \a sequenced_task_policy or
+    ///           \a parallel_task_policy and
+    ///           returns \a FwdIter otherwise.
+    ///           The \a destroy_n algorithm returns the
+    ///           iterator to the element in the source range, one past
+    ///           the last element constructed.
+    ///
+    template <typename ExPolicy, typename FwdIter, typename Size,
+    HPX_CONCEPT_REQUIRES_(
+        execution::is_execution_policy<ExPolicy>::value &&
+        hpx::traits::is_iterator<FwdIter>::value)>
+    typename util::detail::algorithm_result<ExPolicy, FwdIter>::type
+    destroy_n(ExPolicy && policy, FwdIter first, Size count)
+    {
+        static_assert(
+            (hpx::traits::is_forward_iterator<FwdIter>::value),
+            "Requires at least forward iterator.");
+
+        // if count is representing a negative value, we do nothing
+        if (detail::is_negative(count))
+        {
+            return util::detail::algorithm_result<ExPolicy, FwdIter>::get(
+                std::move(first));
+        }
+
+        typedef std::integral_constant<bool,
+                execution::is_sequential_execution_policy<ExPolicy>::value
+            > is_seq;
+
+        return detail::destroy_n<FwdIter>().call(
+            std::forward<ExPolicy>(policy), is_seq(),
+            first, std::size_t(count));
+    }
+}}}
+
+#endif
---
+++ tests/unit/parallel/algorithms/destroyn.cpp
@@ -0,0 +1,494 @@
+//  Copyright (c) 2014-2017 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/hpx.hpp>
+#include <hpx/include/parallel_destroy.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+#include <boost/atomic.hpp>
+#include <boost/range/functions.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "test_utils.hpp"
+
+struct destructable
+{
+    destructable()
+      : value_(0)
+    {}
+
+    ~destructable()
+    {
+        std::memset(&value_, 0xcd, sizeof(value_));
+    }
+
+    std::uint32_t value_;
+};
+
+std::size_t const data_size = 10007;
+
+////////////////////////////////////////////////////////////////////////////
+template <typename ExPolicy, typename IteratorTag>
+void test_destroy_n(ExPolicy policy, IteratorTag)
+{
+    static_assert(
+        hpx::parallel::execution::is_execution_policy<ExPolicy>::value,
+        "hpx::parallel::execution::is_execution_policy<ExPolicy>::value");
+
+    typedef destructable* base_iterator;
+    typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+    destructable* p = (destructable*)std::malloc(
+        data_size * sizeof(destructable));
+
+    // value-initialize data in array
+    std::for_each(
+        p, p + data_size,
+        [](destructable& d)
+        {
+            ::new (static_cast<void*>(std::addressof(d))) destructable;
+        });
+
+    hpx::parallel::destroy_n(
+        std::forward<ExPolicy>(policy),
+        iterator(p), data_size);
+
+    std::size_t count = 0;
+    std::for_each(p, p + data_size,
+        [&count](destructable v1)
+        {
+            HPX_TEST_EQ(v1.value_, (std::uint32_t)0xcdcdcdcd);
+            ++count;
+        });
+    HPX_TEST_EQ(count, data_size);
+
+    std::free(p);
+}
+
+template <typename ExPolicy, typename IteratorTag>
+void test_destroy_n_async(ExPolicy policy, IteratorTag)
+{
+    typedef destructable* base_iterator;
+    typedef test::test_iterator<base_iterator, IteratorTag> iterator;
+
+    destructable* p =
+        (destructable*)std::malloc(data_size * sizeof(destructable));
+
+    // value-initialize data in array
+    std::for_each(
+        p, p + data_size,
+        [](destructable& d)
+        {
+            ::new (static_cast<void*>(std::addressof(d))) destructable;
+        });
+
+    auto f =
+        hpx::parallel::destroy_n(
+            std::forward<ExPolicy>(policy),
+            iterator(p), data_size);
+    f.wait();
+
+    std::size_t count = 0;
+    std::for_each(p, p + data_size,
+        [&count](destructable v1)
+        {
+            HPX_TEST_EQ(v1.value_, (std::uint32_t)0xcdcdcdcd);
+            ++count;
+        });
+    HPX_TEST_EQ(count, data_size);
+
+    std::free(p);
+}
+
+template <typename IteratorTag>
+void test_destroy_n()
+{
+    using namespace hpx::parallel;
+
+    test_destroy_n(execution::seq, IteratorTag());
+    test_destroy_n(execution::par, IteratorTag());
+    test_destroy_n(execution::par_unseq, IteratorTag());
+
+    test_destroy_n_async(execution::seq(execution::task), IteratorTag());
+    test_destroy_n_async(execution::par(execution::task), IteratorTag());
+
+#if defined(HPX_HAVE_GENERIC_EXECUTION_POLICY)
+    test_destroy_n(execution_policy(execution::seq), IteratorTag());
+    test_destroy_n(execution_policy(execution::par), IteratorTag());
+    test_destroy_n(execution_policy(execution::par_unseq), IteratorTag());
+
+    test_destroy_n(
+        execution_policy(execution::seq(execution::task)), IteratorTag());
+    test_destroy_n(
+        execution_policy(execution::par(execution::task)), IteratorTag());
+#endif
+}
+
+void destroy_n_test()
+{
+    test_destroy_n<std::random_access_iterator_tag>();
+    test_destroy_n<std::forward_iterator_tag>();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template<typename ExPolicy, typename IteratorTag>
+void test_destroy_n_exception(ExPolicy policy, IteratorTag)
+{
+    static_assert(
+        hpx::parallel::execution::is_execution_policy<ExPolicy>::value,
+        "hpx::parallel::execution::is_execution_policy<ExPolicy>::value");
+
+    typedef test::count_instances_v<destructable> data_type;
+    typedef data_type* base_iterator;
+    typedef test::decorated_iterator<base_iterator, IteratorTag>
+        decorated_iterator;
+
+    data_type* p = (data_type*)std::malloc(data_size * sizeof(data_type));
+
+    data_type::instance_count.store(0);
+    data_type::max_instance_count.store(0);
+
+    // value-initialize data in array
+    std::for_each(
+        p, p + data_size,
+        [](data_type& d)
+        {
+            ::new (static_cast<void*>(std::addressof(d))) data_type;
+        });
+
+    HPX_TEST_EQ(data_type::instance_count.load(), data_size);
+
+    boost::atomic<std::size_t> throw_after(std::rand() % data_size); //-V104
+    std::size_t throw_after_ = throw_after.load();
+
+    bool caught_exception = false;
+    try {
+        hpx::parallel::destroy_n(policy,
+            decorated_iterator(
+                p,
+                [&throw_after]()
+                {
+                    if (throw_after-- == 0)
+                        throw std::runtime_error("test");
+                }),
+            data_size);
+        HPX_TEST(false);
+    }
+    catch(hpx::exception_list const& e) {
+        caught_exception = true;
+        test::test_num_exceptions<ExPolicy, IteratorTag>::call(policy, e);
+    }
+    catch(...) {
+        HPX_TEST(false);
+    }
+
+    HPX_TEST(caught_exception);
+    HPX_TEST_LTE(data_type::instance_count.load(),
+        std::size_t(data_size-throw_after_));
+
+    std::free(p);
+}
+
+template <typename ExPolicy, typename IteratorTag>
+void test_destroy_n_exception_async(
+    ExPolicy policy, IteratorTag)
+{
+    typedef test::count_instances_v<destructable> data_type;
+    typedef data_type* base_iterator;
+    typedef test::decorated_iterator<base_iterator, IteratorTag>
+        decorated_iterator;
+
+    data_type* p = (data_type*)std::malloc(data_size * sizeof(data_type));
+
+    data_type::instance_count.store(0);
+    data_type::max_instance_count.store(0);
+
+    // value-initialize data in array
+    std::for_each(
+        p, p + data_size,
+        [](data_type& d)
+        {
+            ::new (static_cast<void*>(std::addressof(d))) data_type;
+        });
+
+    HPX_TEST_EQ(data_type::instance_count.load(), data_size);
+
+    boost::atomic<std::size_t> throw_after(std::rand() % data_size); //-V104
+    std::size_t throw_after_ = throw_after.load();
+
+    bool caught_exception = false;
+    bool returned_from_algorithm = false;
+    try {
+        auto f =
+            hpx::parallel::destroy_n(policy,
+                decorated_iterator(
+                    p,
+                    [&throw_after]()
+                    {
+                        if (throw_after-- == 0)
+                            throw std::runtime_error("test");
+                    }),
+                data_size);
+
+        returned_from_algorithm = true;
+        f.get();
+
+        HPX_TEST(false);
+    }
+    catch(hpx::exception_list const& e) {
+        caught_exception = true;
+        test::test_num_exceptions<ExPolicy, IteratorTag>::call(policy, e);
+    }
+    catch(...) {
+        HPX_TEST(false);
+    }
+
+    HPX_TEST(caught_exception);
+    HPX_TEST(returned_from_algorithm);
+    HPX_TEST_LTE(data_type::instance_count.load(),
+        std::size_t(data_size-throw_after_));
+
+    std::free(p);
+}
+
+template<typename IteratorTag>
+void test_destroy_n_exception()
+{
+    using namespace hpx::parallel;
+
+    // If the execution policy object is of type vector_execution_policy,
+    // std::terminate shall be called. therefore we do not test exceptions
+    // with a vector execution policy
+    test_destroy_n_exception(execution::seq, IteratorTag());
+    test_destroy_n_exception(execution::par, IteratorTag());
+
+    test_destroy_n_exception_async(execution::seq(execution::task), IteratorTag());
+    test_destroy_n_exception_async(execution::par(execution::task), IteratorTag());
+
+#if defined(HPX_HAVE_GENERIC_EXECUTION_POLICY)
+    test_destroy_n_exception(execution_policy(execution::seq), IteratorTag());
+    test_destroy_n_exception(execution_policy(execution::par), IteratorTag());
+
+    test_destroy_n_exception(execution_policy(execution::seq(execution::task)),
+        IteratorTag());
+    test_destroy_n_exception(execution_policy(execution::par(execution::task)),
+        IteratorTag());
+#endif
+}
+
+void destroy_n_exception_test()
+{
+    test_destroy_n_exception<std::random_access_iterator_tag>();
+    test_destroy_n_exception<std::forward_iterator_tag>();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template< typename ExPolicy, typename IteratorTag>
+void test_destroy_n_bad_alloc(ExPolicy policy, IteratorTag)
+{
+    static_assert(
+        hpx::parallel::execution::is_execution_policy<ExPolicy>::value,
+        "hpx::parallel::execution::is_execution_policy<ExPolicy>::value");
+
+    typedef test::count_instances_v<destructable> data_type;
+    typedef data_type* base_iterator;
+    typedef test::decorated_iterator<base_iterator, IteratorTag>
+        decorated_iterator;
+
+    data_type* p = (data_type*)std::malloc(data_size * sizeof(data_type));
+
+    data_type::instance_count.store(0);
+    data_type::max_instance_count.store(0);
+
+    // value-initialize data in array
+    std::for_each(
+        p, p + data_size,
+        [](data_type& d)
+        {
+            ::new (static_cast<void*>(std::addressof(d))) data_type;
+        });
+
+    HPX_TEST_EQ(data_type::instance_count.load(), data_size);
+
+    boost::atomic<std::size_t> throw_after(std::rand() % data_size); //-V104
+    std::size_t throw_after_ = throw_after.load();
+
+    bool caught_bad_alloc = false;
+    try {
+        hpx::parallel::destroy_n(policy,
+            decorated_iterator(
+                p,
+                [&throw_after]()
+                {
+                    if (throw_after-- == 0)
+                        throw std::bad_alloc();
+                }),
+            data_size);
+
+        HPX_TEST(false);
+    }
+    catch(std::bad_alloc const&) {
+        caught_bad_alloc = true;
+    }
+    catch(...) {
+        HPX_TEST(false);
+    }
+
+    HPX_TEST(caught_bad_alloc);
+    HPX_TEST_LTE(data_type::instance_count.load(),
+        std::size_t(data_size-throw_after_));
+
+    std::free(p);
+}
+
+template <typename ExPolicy, typename IteratorTag>
+void test_destroy_n_bad_alloc_async(
+    ExPolicy policy, IteratorTag)
+{
+    typedef test::count_instances_v<destructable> data_type;
+    typedef data_type* base_iterator;
+    typedef test::decorated_iterator<base_iterator, IteratorTag>
+        decorated_iterator;
+
+    data_type* p = (data_type*)std::malloc(data_size * sizeof(data_type));
+
+    data_type::instance_count.store(0);
+    data_type::max_instance_count.store(0);
+
+    // value-initialize data in array
+    std::for_each(
+        p, p + data_size,
+        [](data_type& d)
+        {
+            ::new (static_cast<void*>(std::addressof(d))) data_type;
+        });
+
+    HPX_TEST_EQ(data_type::instance_count.load(), data_size);
+
+    boost::atomic<std::size_t> throw_after(std::rand() % data_size); //-V104
+    std::size_t throw_after_ = throw_after.load();
+
+    bool caught_bad_alloc = false;
+    bool returned_from_algorithm = false;
+    try {
+        auto f =
+            hpx::parallel::destroy_n(policy,
+                decorated_iterator(
+                    p,
+                    [&throw_after]()
+                    {
+                        if (throw_after-- == 0)
+                            throw std::bad_alloc();
+                    }),
+                data_size);
+
+        returned_from_algorithm = true;
+        f.get();
+
+        HPX_TEST(false);
+    }
+    catch(std::bad_alloc const&) {
+        caught_bad_alloc = true;
+    }
+    catch(...) {
+        HPX_TEST(false);
+    }
+
+    HPX_TEST(caught_bad_alloc);
+    HPX_TEST(returned_from_algorithm);
+    HPX_TEST_LTE(data_type::instance_count.load(),
+        std::size_t(data_size-throw_after_));
+
+    std::free(p);
+}
+
+template<typename IteratorTag>
+void test_destroy_n_bad_alloc()
+{
+    using namespace hpx::parallel;
+
+    // If the execution policy object is of type vector_execution_policy,
+    // std::terminate shall be called. therefore we do not test exceptions
+    // with a vector execution policy
+    test_destroy_n_bad_alloc(execution::seq, IteratorTag());
+    test_destroy_n_bad_alloc(execution::par, IteratorTag());
+
+    test_destroy_n_bad_alloc_async(
+        execution::seq(execution::task),
+        IteratorTag());
+    test_destroy_n_bad_alloc_async(
+        execution::par(execution::task),
+        IteratorTag());
+
+#if defined(HPX_HAVE_GENERIC_EXECUTION_POLICY)
+    test_destroy_n_bad_alloc(
+        execution_policy(execution::seq),
+        IteratorTag());
+    test_destroy_n_bad_alloc(
+        execution_policy(execution::par),
+        IteratorTag());
+
+    test_destroy_n_bad_alloc(
+        execution_policy(execution::seq(execution::task)),
+        IteratorTag());
+    test_destroy_n_bad_alloc(
+        execution_policy(execution::par(execution::task)),
+        IteratorTag());
+#endif
+}
+
+void destroy_n_bad_alloc_test()
+{
+    test_destroy_n_bad_alloc<std::random_access_iterator_tag>();
+    test_destroy_n_bad_alloc<std::forward_iterator_tag>();
+}
+
+int hpx_main(boost::program_options::variables_map& vm)
+{
+    unsigned int seed = (unsigned int)std::time(nullptr);
+    if (vm.count("seed"))
+        seed = vm["seed"].as<unsigned int>();
+
+    std::cout << "using seed: " << seed << std::endl;
+    std::srand(seed);
+
+    destroy_n_test();
+    destroy_n_exception_test();
+    destroy_n_bad_alloc_test();
+    return hpx::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    // add command line option which controls the random number generator seed
+    using namespace boost::program_options;
+    options_description desc_commandline(
+        "Usage: " HPX_APPLICATION_STRING " [options]");
+
+    desc_commandline.add_options()
+        ("seed,s", value<unsigned int>(),
+        "the random number generator seed to use for this run")
+        ;
+
+    // By default this test should run on all available cores
+    std::vector<std::string> const cfg = {
+        "hpx.os_threads=all"
+    };
+
+    // Initialize and run HPX
+    HPX_TEST_EQ_MSG(hpx::init(desc_commandline, argc, argv, cfg), 0,
+        "HPX main exited with non-zero status");
+
+    return hpx::util::report_errors();
+}
