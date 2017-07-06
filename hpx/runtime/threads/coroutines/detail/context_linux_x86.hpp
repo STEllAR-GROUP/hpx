4b3b87c17f928778e7c0d08b1640807b7d38fe1b
ct <ct@>
2017-07-05 18:36:58 -0700

    cleaned up formatting issues

--- hpx/runtime/threads/coroutines/detail/context_linux_x86.hpp
+++ hpx/runtime/threads/coroutines/detail/context_linux_x86.hpp
@@ -160,9 +160,9 @@ namespace hpx { namespace threads { namespace coroutines
                 action.sa_flags = SA_SIGINFO|SA_ONSTACK; //SA_STACK
                 action.sa_sigaction = &sigsegv_handler;
 
-                sigaltstack(&segv_stack, NULL);
+                sigaltstack(&segv_stack, nullptr);
                 sigfillset(&action.sa_mask);
-                sigaction(SIGSEGV, &action, NULL);
+                sigaction(SIGSEGV, &action, nullptr);
             }
 
             /**
@@ -228,18 +228,27 @@ namespace hpx { namespace threads { namespace coroutines
             }
 
             static void sigsegv_handler(int signum, siginfo_t *info,
-                void *data) 
+                void *data)
             {
                 void *addr = info->si_addr;
-                std::cerr << "Stack overflow in coroutine at address "
-                    << std::internal << std::hex << std::setw(sizeof(addr)*2+2)
-                    << std::setfill('0') << static_cast<int*>(addr) << "." << std::endl;
 
-                std::cerr << std::endl << "Configure the hpx runtime to allocate a larger "
+                std::cerr << "Stack overflow in coroutine at address "
+                    << std::internal << std::hex
+                    << std::setw(sizeof(addr)*2+2)
+                    << std::setfill('0') << static_cast<int*>(addr)
+                    << "." << std::endl;
+
+                std::cerr << std::endl << "Configure the hpx runtime to "
+                    << "allocate a larger "
                     << "coroutine stack size." << std::endl
-                    << "Use the hpx.stacks.small_size, hpx.stacks.medium_size, " << std::endl
-                    << "hpx.stacks.large_size, or hpx.stacks.huge_size runtime " << std::endl
-                    << "flags to configure coroutine heap sizes." << std::endl;
+                    << "Use the hpx.stacks.small_size, "
+                    << "hpx.stacks.medium_size, "
+                    << std::endl
+                    << "hpx.stacks.large_size, "
+                    << "or hpx.stacks.huge_size runtime "
+                    << std::endl
+                    << "flags to configure coroutine heap sizes." 
+                    << std::endl << std::endl;
 
                 std::exit(EXIT_FAILURE);
             }
--- tests/unit/threads/thread_stacksize_overflow.cpp
+++ tests/unit/threads/thread_stacksize_overflow.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2012 Hartmut Kaiser
-// Copyright (C) 2017 Christopher Taylor 
+// Copyright (C) 2017 Christopher Taylor
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
