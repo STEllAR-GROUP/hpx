a22b44b17ff3d2b2ca3cffa5ce19cc856a19c632
Ajai V George <ajaivgeorgedev@gmail.com>
2017-07-07 07:20:41 +0530

    Sequential segmented find_end function object along with return type and partial test

--- hpx/parallel/segmented_algorithms/find.hpp
+++ hpx/parallel/segmented_algorithms/find.hpp
@@ -345,6 +345,129 @@ namespace hpx { namespace parallel { inline namespace v1
         find_if_not_(ExPolicy && policy, InIter first, InIter last, F && f,
             std::false_type);
 
+        template <typename FwdIter>
+        struct find_end_return
+        {
+            FwdIter seq_first;
+            int partial_position;
+        };
+
+        typedef struct find_end_return find_end_return;
+
+        /// \cond NOINTERNAL
+        template <typename Iter>
+        struct segmented_find_end : public detail::algorithm<find_end<Iter>, Iter>
+        {
+            segmented_find_end()
+              : find_end::algorithm("segmented_find_end")
+            {}
+
+            template <typename ExPolicy, typename FwdIter1, typename FwdIter2,
+                typename Pred>
+            static find_end_return
+            sequential(ExPolicy, FwdIter1 first1, FwdIter1 last1,
+                FwdIter2 first2, FwdIter2 last2, Pred && op,
+                FwdIter1 start, int g_cursor = 0)
+            {
+                int cursor = g_cursor;
+                while(last1 != first1)
+                {
+                    if(*first1 == first2[cursor])
+                    {
+                        if(cursor == 0)
+                            start = first1;
+                        cursor++;
+                        if(cursor == std::distance(first2,last2))
+                        {
+                            find_end_return ret = {start,-1};
+                            return ret;
+                        }
+                    }
+                    else if(*first1 == *first2)
+                    {
+                        start = first1;
+                        cursor = 1;
+                    }
+                    else
+                    {
+                        cursor = 0;
+                    }
+                    first1++
+                }
+                find_end_return ret = {start, cursor};
+                return ret;
+            }
+
+            // template <typename ExPolicy, typename FwdIter1, typename FwdIter2,
+            //     typename Pred>
+            // static typename util::detail::algorithm_result<
+            //     ExPolicy, find_end_return
+            // >::type
+            // parallel(ExPolicy && policy, FwdIter1 first1, FwdIter1 last1,
+            //     FwdIter2 first2, FwdIter2 last2, Pred && op)
+            // {
+            //     typedef util::detail::algorithm_result<ExPolicy, FwdIter1> result;
+            //     typedef typename std::iterator_traits<FwdIter1>::reference reference;
+            //     typedef typename std::iterator_traits<FwdIter1>::difference_type
+            //         difference_type;
+            //
+            //     difference_type diff = std::distance(first2, last2);
+            //     if (diff <= 0)
+            //         return result::get(std::move(last1));
+            //
+            //     difference_type count = std::distance(first1, last1);
+            //     if (diff > count)
+            //         return result::get(std::move(last1));
+            //
+            //     util::cancellation_token<
+            //         difference_type, std::greater<difference_type>
+            //     > tok(-1);
+            //
+            //     return util::partitioner<ExPolicy, FwdIter1, void>::
+            //         call_with_index(
+            //             std::forward<ExPolicy>(policy), first1, count-(diff-1), 1,
+            //             [=](FwdIter1 it, std::size_t part_size,
+            //                 std::size_t base_idx) mutable
+            //             {
+            //                 FwdIter1 curr = it;
+            //
+            //                 util::loop_idx_n(
+            //                     base_idx, it, part_size, tok,
+            //                     [=, &tok, &curr](reference t, std::size_t i)
+            //                     {
+            //                         ++curr;
+            //                         if (op(t, *first2))
+            //                         {
+            //                             difference_type local_count = 1;
+            //                             FwdIter2 needle = first2;
+            //                             FwdIter1 mid = curr;
+            //
+            //                             for (difference_type len = 0;
+            //                                  local_count != diff && len != count;
+            //                                  (void) ++local_count, ++len, ++mid)
+            //                             {
+            //                                 if (*mid != *++needle)
+            //                                     break;
+            //                             }
+            //
+            //                             if (local_count == diff)
+            //                                 tok.cancel(i);
+            //                         }
+            //                     });
+            //             },
+            //             [=](std::vector<hpx::future<void> > &&) mutable -> FwdIter1
+            //             {
+            //                 difference_type find_end_res = tok.get_data();
+            //                 if (find_end_res != count)
+            //                     std::advance(first1, find_end_res);
+            //                 else
+            //                     first1 = last1;
+            //
+            //                 return std::move(first1);
+            //             });
+            // }
+        };
+
         template <typename Algo, typename ExPolicy, typename FwdIter1,
             typename FwdIter2, typename Pred>
         inline typename std::enable_if<
@@ -422,136 +545,136 @@ namespace hpx { namespace parallel { inline namespace v1
             return result::get(std::move(output));
         }
 
-        template <typename Algo, typename ExPolicy, typename FwdIter1,
-            typename FwdIter2, typename Pred>
-        inline typename std::enable_if<
-            execution::is_execution_policy<ExPolicy>::value,
-            typename util::detail::algorithm_result<ExPolicy, FwdIter1>::type
-        >::type
-        segmented_find_end(Algo && algo, ExPolicy && policy, FwdIter1 first1, FwdIter1 last1,
-            FwdIter2 first2, FwdIter2 last2, Pred && op, std::false_type)
-        {
-            typedef hpx::traits::segmented_iterator_traits<FwdIter1> traits;
-            typedef typename traits::segment_iterator segment_iterator;
-            typedef typename traits::local_iterator local_iterator_type;
-            typedef util::detail::algorithm_result<ExPolicy, FwdIter1> result;
-
-            typedef std::integral_constant<bool,
-                    !hpx::traits::is_forward_iterator<FwdIter1>::value
-                > forced_seq;
-
-            segment_iterator sit = traits::segment(first1);
-            segment_iterator send = traits::segment(last1);
-
-            std::vector<future<FwdIter1> > segments;
-            segments.reserve(std::distance(sit, send));
-
-            if (sit == send)
-            {
-                // all elements are on the same partition
-                local_iterator_type beg = traits::local(first1);
-                local_iterator_type end = traits::local(last1);
-                if (beg != end)
-                {
-                    segments.push_back(
-                        hpx::make_future<FwdIter1>(
-                            dispatch_async(traits::get_id(sit), algo,
-                                policy, forced_seq(), beg, end, first2, last2, op),
-                            [send,end,last1](local_iterator_type const& out)
-                                -> FwdIter1
-                            {
-                                if(out != end)
-                                    return traits::compose(send, out);
-                                else
-                                    return last1;
-                            }));
-                }
-            }
-            else {
-                // handle the remaining part of the first partition
-                local_iterator_type beg = traits::local(first1);
-                local_iterator_type end = traits::end(sit);
-                if (beg != end)
-                {
-                    segments.push_back(
-                        hpx::make_future<FwdIter1>(
-                            dispatch_async(traits::get_id(sit), algo,
-                                policy, forced_seq(), beg, end, first2, last2, op),
-                            [sit,end,last1](local_iterator_type const& out)
-                                -> FwdIter1
-                            {
-                                if(out != end)
-                                    return traits::compose(sit, out);
-                                else
-                                    return last1;
-                            }));
-                }
-
-                // handle all of the full partitions
-                for (++sit; sit != send; ++sit)
-                {
-                    beg = traits::begin(sit);
-                    end = traits::end(sit);
-                    if (beg != end)
-                    {
-                        segments.push_back(
-                            hpx::make_future<FwdIter1>(
-                                dispatch_async(traits::get_id(sit), algo,
-                                    policy, forced_seq(), beg, end, first2, last2, op),
-                                [sit,end,last1](local_iterator_type const& out)
-                                    -> FwdIter1
-                                {
-                                    if(out != end)
-                                        return traits::compose(sit, out);
-                                    else
-                                        return last1;
-                                }));
-                    }
-                }
-
-                // handle the beginning of the last partition
-                beg = traits::begin(sit);
-                end = traits::local(last1);
-                if (beg != end)
-                {
-                    segments.push_back(
-                        hpx::make_future<FwdIter1>(
-                            dispatch_async(traits::get_id(sit), algo,
-                                policy, forced_seq(), beg, end, first2, last2, op),
-                            [sit,end,last1](local_iterator_type const& out)
-                                -> FwdIter1
-                            {
-                                if(out != end)
-                                    return traits::compose(sit, out);
-                                else
-                                    return last1;
-                            }));
-                }
-            }
-            return result::get(
-                dataflow(
-                    [=](std::vector<hpx::future<FwdIter1> > && r)
-                        ->  FwdIter1
-                    {
-                        // handle any remote exceptions, will throw on error
-                        std::list<std::exception_ptr> errors;
-                        parallel::util::detail::handle_remote_exceptions<
-                            ExPolicy
-                        >::call(r, errors);
-
-                        std::vector<FwdIter1> res =
-                            hpx::util::unwrapped(std::move(r));
-                        auto it = res.begin();
-                        while(it!=res.end())
-                        {
-                            if(*it != last1)
-                                return *it;
-                            it++;
-                        }
-                        return res.back();
-                    },
-                    std::move(segments)));
-        }
+        // template <typename Algo, typename ExPolicy, typename FwdIter1,
+        //     typename FwdIter2, typename Pred>
+        // inline typename std::enable_if<
+        //     execution::is_execution_policy<ExPolicy>::value,
+        //     typename util::detail::algorithm_result<ExPolicy, FwdIter1>::type
+        // >::type
+        // segmented_find_end(Algo && algo, ExPolicy && policy, FwdIter1 first1, FwdIter1 last1,
+        //     FwdIter2 first2, FwdIter2 last2, Pred && op, std::false_type)
+        // {
+        //     typedef hpx::traits::segmented_iterator_traits<FwdIter1> traits;
+        //     typedef typename traits::segment_iterator segment_iterator;
+        //     typedef typename traits::local_iterator local_iterator_type;
+        //     typedef util::detail::algorithm_result<ExPolicy, FwdIter1> result;
+        //
+        //     typedef std::integral_constant<bool,
+        //             !hpx::traits::is_forward_iterator<FwdIter1>::value
+        //         > forced_seq;
+        //
+        //     segment_iterator sit = traits::segment(first1);
+        //     segment_iterator send = traits::segment(last1);
+        //
+        //     std::vector<future<FwdIter1> > segments;
+        //     segments.reserve(std::distance(sit, send));
+        //
+        //     if (sit == send)
+        //     {
+        //         // all elements are on the same partition
+        //         local_iterator_type beg = traits::local(first1);
+        //         local_iterator_type end = traits::local(last1);
+        //         if (beg != end)
+        //         {
+        //             segments.push_back(
+        //                 hpx::make_future<FwdIter1>(
+        //                     dispatch_async(traits::get_id(sit), algo,
+        //                         policy, forced_seq(), beg, end, first2, last2, op),
+        //                     [send,end,last1](local_iterator_type const& out)
+        //                         -> FwdIter1
+        //                     {
+        //                         if(out != end)
+        //                             return traits::compose(send, out);
+        //                         else
+        //                             return last1;
+        //                     }));
+        //         }
+        //     }
+        //     else {
+        //         // handle the remaining part of the first partition
+        //         local_iterator_type beg = traits::local(first1);
+        //         local_iterator_type end = traits::end(sit);
+        //         if (beg != end)
+        //         {
+        //             segments.push_back(
+        //                 hpx::make_future<FwdIter1>(
+        //                     dispatch_async(traits::get_id(sit), algo,
+        //                         policy, forced_seq(), beg, end, first2, last2, op),
+        //                     [sit,end,last1](local_iterator_type const& out)
+        //                         -> FwdIter1
+        //                     {
+        //                         if(out != end)
+        //                             return traits::compose(sit, out);
+        //                         else
+        //                             return last1;
+        //                     }));
+        //         }
+        //
+        //         // handle all of the full partitions
+        //         for (++sit; sit != send; ++sit)
+        //         {
+        //             beg = traits::begin(sit);
+        //             end = traits::end(sit);
+        //             if (beg != end)
+        //             {
+        //                 segments.push_back(
+        //                     hpx::make_future<FwdIter1>(
+        //                         dispatch_async(traits::get_id(sit), algo,
+        //                             policy, forced_seq(), beg, end, first2, last2, op),
+        //                         [sit,end,last1](local_iterator_type const& out)
+        //                             -> FwdIter1
+        //                         {
+        //                             if(out != end)
+        //                                 return traits::compose(sit, out);
+        //                             else
+        //                                 return last1;
+        //                         }));
+        //             }
+        //         }
+        //
+        //         // handle the beginning of the last partition
+        //         beg = traits::begin(sit);
+        //         end = traits::local(last1);
+        //         if (beg != end)
+        //         {
+        //             segments.push_back(
+        //                 hpx::make_future<FwdIter1>(
+        //                     dispatch_async(traits::get_id(sit), algo,
+        //                         policy, forced_seq(), beg, end, first2, last2, op),
+        //                     [sit,end,last1](local_iterator_type const& out)
+        //                         -> FwdIter1
+        //                     {
+        //                         if(out != end)
+        //                             return traits::compose(sit, out);
+        //                         else
+        //                             return last1;
+        //                     }));
+        //         }
+        //     }
+        //     return result::get(
+        //         dataflow(
+        //             [=](std::vector<hpx::future<FwdIter1> > && r)
+        //                 ->  FwdIter1
+        //             {
+        //                 // handle any remote exceptions, will throw on error
+        //                 std::list<std::exception_ptr> errors;
+        //                 parallel::util::detail::handle_remote_exceptions<
+        //                     ExPolicy
+        //                 >::call(r, errors);
+        //
+        //                 std::vector<FwdIter1> res =
+        //                     hpx::util::unwrapped(std::move(r));
+        //                 auto it = res.begin();
+        //                 while(it!=res.end())
+        //                 {
+        //                     if(*it != last1)
+        //                         return *it;
+        //                     it++;
+        //                 }
+        //                 return res.back();
+        //             },
+        //             std::move(segments)));
+        // }
 
         template <typename ExPolicy, typename FwdIter1, typename FwdIter2,
             typename Pred>
---
+++ tests/unit/parallel/segmented_algorithms/partitioned_vector_find_end.cpp
@@ -0,0 +1,62 @@
+//  Copyright (c) 2017 Ajai V George
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/partitioned_vector.hpp>
+#include <hpx/include/parallel_find.hpp>
+
+#include <hpx/util/lightweight_test.hpp>
+
+#include <cstddef>
+#include <vector>
+#include <iostream>
+///////////////////////////////////////////////////////////////////////////////
+// Define the vector types to be used.
+HPX_REGISTER_PARTITIONED_VECTOR(double);
+HPX_REGISTER_PARTITIONED_VECTOR(int);
+
+///////////////////////////////////////////////////////////////////////////////
+template <typename ExPolicy, typename T>
+void test_find_end(ExPolicy && policy,
+    hpx::partitioned_vector<T> & xvalues, T val)
+{
+    auto last = hpx::parallel::find_end(policy, xvalues.begin(),
+        xvalues.end(), val);
+    HPX_TEST_EQ(*last,val);
+}
+
+template <typename ExPolicy, typename T>
+void test_find_end_async(ExPolicy && policy,
+    hpx::partitioned_vector<T> & xvalues, T val)
+{
+    auto last = hpx::parallel::find_end(policy, xvalues.begin(),
+        xvalues.end(), val).get();
+    HPX_TEST_EQ(*last,val);
+}
+
+template <typename T>
+void find_end_tests(std::vector<hpx::id_type> &localities)
+{
+    std::size_t const num = 1000;
+    hpx::partitioned_vector<T> xvalues(num, T(1),hpx::container_layout(localities));
+    hpx::parallel::inclusive_scan(hpx::parallel::execution::seq, xvalues.begin(),
+        xvalues.end(), xvalues.begin(), T(0), std::plus<T>());
+
+    test_find_end(hpx::parallel::execution::seq, xvalues, T(512));
+    test_find_end(hpx::parallel::execution::par, xvalues, T(512));
+    test_find_end_async(hpx::parallel::execution::seq(hpx::parallel::execution::task),
+        xvalues, T(512));
+    test_find_end_async(hpx::parallel::execution::par(hpx::parallel::execution::task),
+        xvalues, T(512));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    std::vector<hpx::id_type> localities = hpx::find_all_localities();
+    find_tests<int>(localities);
+    find_tests<double>(localities);
+    return 0;
+}
