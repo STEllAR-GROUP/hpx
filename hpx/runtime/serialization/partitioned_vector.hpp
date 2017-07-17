2399fe4007e31ca147b0e776fcf47b1e9ca79b36
ct-clmsn <ct-clmsn>
2017-07-16 21:52:13 -0400

    fix implementation to support friendship in partitioned_vector implementation file

--- hpx/runtime/serialization/partitioned_vector.hpp
+++ hpx/runtime/serialization/partitioned_vector.hpp
@@ -10,61 +10,75 @@
 #include <hpx/runtime/serialization/serialize.hpp>
 #include <hpx/traits/is_bitwise_serializable.hpp>
 
-#include <hpx/include/parallel_is_partitioned.hpp>
+#include <hpx/traits/segmented_iterator_traits.hpp>
 #include <hpx/include/partitioned_vector.hpp>
-
-#include <cstddef>
-#include <cstdint>
-#include <type_traits>
 
 namespace hpx { namespace serialization
 {
     namespace detail
     {
         template<typename T>
-        partitioned_vector_segmented_serializer(input_archive & ar,
-            hpx::partitioned_vector<T> & v)
+        partitioned_vector_segmented_serializer
         {
-            static_assert(
-                hpx::parallel::is_partitioned(hpx::parallel::execution::par,
-                    std::begin(v), std::end(v), [](std::size_t n) { return n > 0; });
-                "hpx::serialization::serialize requires segemented partitioned_vector");
+            static void serialize(input_archive & ar,
+                hpx::partitioned_vector<T> & v, std::false_type)
+            {
+                static_assert(
+                    hpx::parallel::is_partitioned(hpx::parallel::execution::par,
+                        std::begin(v), std::end(v), [](std::size_t n) { return n > 0; });
+                    "hpx::serialization::serialize requires segemented partitioned_vector");
+            }
 
-            std::string pvec_registered_name;
-            ar >> pvec_registered_name;
-            hpx::future<void> fconnect = v.connect_to(pvec_registered_name);
-            fconnect.wait();
-        }
+            static void serialize(input_archive & ar,
+                hpx::partitioned_vector<T> & v, std::true_type)
+            {
+                std::string pvec_registered_name;
+                ar >> pvec_registered_name;
+                hpx::future<void> fconnect = v.connect_to(pvec_registered_name);
+                fconnect.wait();
+            }
 
-        template<typename T>
-        partitioned_vector_segmented_serializer(output_archive & ar,
-            hpx::partitioned_vector<T> & v)
-        {
-            static_assert(
-                hpx::parallel::is_partitioned(hpx::parallel::execution::par,
-                    std::begin(v), std::end(v), [](std::size_t n) { return n > 0; });
-                "hpx::serialization::serialize requires segemented partitioned_vector");
+            static void serialize(output_archive & ar,
+                const hpx::partitioned_vector<T> & v, std::false_type) const
+            {
+                static_assert(
+                    hpx::parallel::is_partitioned(hpx::parallel::execution::par,
+                        std::begin(v), std::end(v), [](std::size_t n) { return n > 0; });
+                    "hpx::serialization::serialize requires segemented partitioned_vector");
 
-            static_assert(
-                v.registered_name_.size() > 0,
-                "hpx::serialization::serialize requires a registered partitioned_vector");
+                static_assert(
+                    v.registered_name_.size() > 0,
+                    "hpx::serialization::serialize requires a registered partitioned_vector");
+            }
 
-            ar << v.registered_name_;
-        }
+            static void serialize(output_archive & ar,
+                const hpx::partitioned_vector<T> & v, std::true_type) const
+            {
+                ar << v.registered_name_;
+            }
+        };
     }
 
     template <typename T>
     void serialize(input_archive & ar, hpx::partitioned_vector<T> & v,
         unsigned)
     {
-        partitioned_vector_segmented_serializer(ar, v);
+        typedef hpx::traits::is_segmented_iterator<FwdIter> is_segmented;
+
+        auto vitr = std::begin(v);
+        detail::partitioned_vector_segmented_serializer::serialize(
+            ar, v, is_segmented(vitr));
     }
 
     template <typename T>
     void serialize(output_archive & ar, const hpx::partitioned_vector<T> & v,
         unsigned)
     {
-        partitioned_vector_segemented_serializer(ar, v);
+        typedef hpx::traits::is_segmented_iterator<FwdIter> is_segmented;
+
+        auto vitr = std::begin(v);
+        detail::partitioned_vector_segmented_serializer::serialize(
+            ar, v, is_segmented(vitr));
     }
 
 }}
