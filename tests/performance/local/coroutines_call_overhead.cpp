0d9d74d1cc78aa738d66bbb04fb5d3e600675ad4
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-07-20 17:44:30 -0500

    Removing dependency on Boost.Date_Time

--- tests/performance/local/coroutines_call_overhead.cpp
+++ tests/performance/local/coroutines_call_overhead.cpp
@@ -9,12 +9,13 @@
 #include <hpx/hpx.hpp>
 
 #include <boost/format.hpp>
-#include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/random.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
 
+#include <chrono>
 #include <cstdint>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -39,12 +40,14 @@ bool header = true;
 ///////////////////////////////////////////////////////////////////////////////
 std::string format_build_date(std::string timestamp)
 {
-    boost::gregorian::date d = boost::gregorian::from_us_string(timestamp);
+    std::chrono::time_point<std::chrono::system_clock> now =
+        std::chrono::system_clock::now();
 
-    char const* fmt = "%02i-%02i-%04i";
+    std::time_t current_time = std::chrono::system_clock::to_time_t(now);
 
-    return boost::str(boost::format(fmt)
-                     % d.month().as_number() % d.day() % d.year());
+    std::string ts = std::ctime(&current_time);
+    ts.resize(ts.size()-1);     // remove trailing '\n'
+    return ts;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
