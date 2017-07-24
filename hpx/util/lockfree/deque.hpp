a08615af59ba18cd6f419b484b30476aa13945d9
Agustin K-ballo Berge <k@fusionfenix.com>
2017-07-24 10:51:56 -0300

    Add deprecated checks for Boost.Atomic

--- hpx/util/lockfree/deque.hpp
+++ hpx/util/lockfree/deque.hpp
@@ -15,6 +15,9 @@
 //  contention, all operations have a constant complexity, except for the dtor.
 ////////////////////////////////////////////////////////////////////////////////
 
+// hpxinspect:nodeprecatedinclude:boost/atomic.hpp
+// hpxinspect:nodeprecatedname:boost::atomic
+
 #if !defined(HPX_F985C12D_03E7_4E25_8CB1_018A56A265E0)
 #define HPX_F985C12D_03E7_4E25_8CB1_018A56A265E0
 
