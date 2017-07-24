ddae98dfa393c2c016efd16bffa5c67a9b5900af
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-07-24 09:56:29 -0500

    Fix use of stringize in runtime_config

--- src/util/runtime_configuration.cpp
+++ src/util/runtime_configuration.cpp
@@ -239,7 +239,7 @@ namespace hpx { namespace util
             "min_add_new_count = ${HPX_THREAD_QUEUE_MIN_ADD_NEW_COUNT:10}",
             "max_add_new_count = ${HPX_THREAD_QUEUE_MAX_ADD_NEW_COUNT:10}",
             "max_delete_count = ${HPX_THREAD_QUEUE_MAX_DELETE_COUNT:1000}",
-            "max_terminated_threads = ${HPX_THREAD_QUEUE_MAX_TERMINATED_THREADS:"
+            "max_terminated_threads = ${HPX_SCHEDULER_MAX_TERMINATED_THREADS:"
               HPX_PP_STRINGIZE(HPX_PP_EXPAND(HPX_SCHEDULER_MAX_TERMINATED_THREADS)) "}",
 
             "[hpx.commandline]",
