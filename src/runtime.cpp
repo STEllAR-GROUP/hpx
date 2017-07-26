609a99b1f1ea1c4846564a03f9135bc2aed19b85
ABresting <abhimanyurawat@yahoo.com>
2017-07-27 00:49:57 +0530

    Added runtime support for stack_overflow detection

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -1,5 +1,6 @@
 //  Copyright (c) 2007-2013 Hartmut Kaiser
 //  Copyright (c)      2011 Bryce Lelbach
+//  Copyright (c)      2017 Abhimanyu Rawat
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -108,6 +109,7 @@ namespace hpx
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include "util/stack_overflow_detection.cpp"
 
 namespace hpx
 {
@@ -166,8 +168,13 @@ namespace hpx
         sigaction(SIGFPE, &new_action, nullptr);  // Floating point exception
         sigaction(SIGILL, &new_action, nullptr);  // Illegal instruction
         sigaction(SIGPIPE, &new_action, nullptr); // Bad pipe
-        sigaction(SIGSEGV, &new_action, nullptr); // Segmentation fault
+        #if defined(__linux__ && HPX_HAVE_STACKOVERFLOW_DETECTION)
+            stack_overflow_detection();  // Call to use libsigsegv for Seg. fault
+        #else
+            sigaction(SIGSEGV, &new_action, nullptr); // Segmentation fault
+        #endif
         sigaction(SIGSYS, &new_action, nullptr);  // Bad syscall
+        
 #endif
 
         std::set_new_handler(hpx::new_handler);
