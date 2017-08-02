68bd40c68d3f36e357385e8c7f38b69969ce6970
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-08-02 08:43:33 -0500

    Fixing test for partitioned_vector serialization
    - flyby: add missing #includes to hpx/include/serialization.hpp

--- tests/unit/serialization/serialization_partitioned_vector.cpp
+++ tests/unit/serialization/serialization_partitioned_vector.cpp
@@ -3,14 +3,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/config.hpp>
+#include <hpx/hpx_main.hpp>
 
 #include <hpx/include/parallel_fill.hpp>
 #include <hpx/include/partitioned_vector.hpp>
-#include <hpx/runtime/serialization/serialize.hpp>
-#include <hpx/runtime/serialization/partitioned_vector.hpp>
-#include <hpx/runtime/serialization/input_archive.hpp>
-#include <hpx/runtime/serialization/output_archive.hpp>
+#include <hpx/include/serialization.hpp>
 
 #include <hpx/util/lightweight_test.hpp>
 
