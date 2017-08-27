bf0a8bee8db02b1460913e09dab7be7eac64ef6d
Taeguk Kwon <xornrbboy@gmail.com>
2017-08-28 00:23:56 +0900

    Improve benchmark codes of parallel::unique for various data type.

--- tests/performance/parallel_algorithms/local/benchmark_unique.cpp
+++ tests/performance/parallel_algorithms/local/benchmark_unique.cpp
@@ -44,6 +44,27 @@ struct random_fill
 };
 
 ///////////////////////////////////////////////////////////////////////////////
+struct heavy_type
+{
+    heavy_type() = default;
+    heavy_type(int rand_no)
+    {
+        vec_.reserve(vec_size_);
+        for (std::size_t i = 0u; i < vec_size_; ++i)
+            vec_.push_back(rand_no);
+    }
+
+    bool operator==(heavy_type const & t) const
+    {
+        return std::equal(std::begin(vec_), std::end(vec_),
+            std::begin(t.vec_), std::end(t.vec_));
+    }
+
+    std::vector<int> vec_;
+    static const std::size_t vec_size_{ 50 };
+};
+
+///////////////////////////////////////////////////////////////////////////////
 template <typename OrgIter, typename InIter>
 double run_unique_benchmark_std(int test_count,
     OrgIter org_first, OrgIter org_last, InIter first, InIter last)
@@ -86,16 +107,16 @@ double run_unique_benchmark_hpx(int test_count, ExPolicy policy,
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-template <typename IteratorTag>
+template <typename IteratorTag, typename DataType>
 void run_benchmark(std::size_t vector_size, int test_count,
-    std::size_t random_range, IteratorTag)
+    std::size_t random_range, IteratorTag, DataType)
 {
     std::cout << "* Preparing Benchmark..." << std::endl;
 
-    typedef typename std::vector<int>::iterator base_iterator;
+    typedef typename std::vector<DataType>::iterator base_iterator;
     typedef test::test_iterator<base_iterator, IteratorTag> iterator;
 
-    std::vector<int> v(vector_size), org_v;
+    std::vector<DataType> v(vector_size), org_v;
     iterator first = iterator(std::begin(v));
     iterator last = iterator(std::end(v));
 
@@ -144,8 +165,36 @@ void run_benchmark(std::size_t vector_size, int test_count,
     std::cout << "----------------------------------------------" << std::endl;
 }
 
+template <typename IteratorTag>
+void run_benchmark(std::size_t vector_size, int test_count,
+    std::size_t random_range,
+    IteratorTag iterator_tag, std::string const& data_type_str)
+{
+    if (data_type_str == "light")
+        run_benchmark(vector_size, test_count, random_range,
+            iterator_tag, int());
+    else // heavy
+        run_benchmark(vector_size, test_count, random_range,
+            iterator_tag, heavy_type());
+}
+
+void run_benchmark(std::size_t vector_size, int test_count,
+    std::size_t random_range,
+    std::string const& iterator_tag_str, std::string const& data_type_str)
+{
+    if (iterator_tag_str == "random")
+        run_benchmark(vector_size, test_count, random_range,
+            std::random_access_iterator_tag(), data_type_str);
+    else if (iterator_tag_str == "bidirectional")
+        run_benchmark(vector_size, test_count, random_range,
+            std::bidirectional_iterator_tag(), data_type_str);
+    else // forward
+        run_benchmark(vector_size, test_count, random_range,
+            std::forward_iterator_tag(), data_type_str);
+}
+
 ///////////////////////////////////////////////////////////////////////////////
-std::string correct_iterator_tag_str(std::string iterator_tag)
+std::string correct_iterator_tag_str(std::string const& iterator_tag)
 {
     if (iterator_tag != "random" &&
         iterator_tag != "bidirectional" &&
@@ -153,6 +202,15 @@ std::string correct_iterator_tag_str(std::string iterator_tag)
         return "random";
     else
         return iterator_tag;
+}
+
+std::string correct_data_type_str(std::string const& data_type)
+{
+    if (data_type != "light" &&
+        data_type != "heavy")
+        return "light";
+    else
+        return data_type;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -170,6 +228,8 @@ int hpx_main(boost::program_options::variables_map& vm)
     int test_count = vm["test_count"].as<int>();
     std::string iterator_tag_str = correct_iterator_tag_str(
         vm["iterator_tag"].as<std::string>());
+    std::string data_type_str = correct_data_type_str(
+        vm["data_type"].as<std::string>());
 
     std::size_t const os_threads = hpx::get_os_thread_count();
 
@@ -181,19 +241,13 @@ int hpx_main(boost::program_options::variables_map& vm)
     std::cout << "vector_size  : " << vector_size << std::endl;
     std::cout << "random_range : " << random_range << std::endl;
     std::cout << "iterator_tag : " << iterator_tag_str << std::endl;
+    std::cout << "data_type    : " << data_type_str << std::endl;
     std::cout << "test_count   : " << test_count << std::endl;
     std::cout << "os threads   : " << os_threads << std::endl;
     std::cout << "----------------------------------------------\n" << std::endl;
 
-    if (iterator_tag_str == "random")
-        run_benchmark(vector_size, test_count, random_range,
-            std::random_access_iterator_tag());
-    else if (iterator_tag_str == "bidirectional")
-        run_benchmark(vector_size, test_count, random_range,
-            std::bidirectional_iterator_tag());
-    else // forward
-        run_benchmark(vector_size, test_count, random_range,
-            std::forward_iterator_tag());
+    run_benchmark(vector_size, test_count, random_range,
+        iterator_tag_str, data_type_str);
 
     return hpx::finalize();
 }
@@ -214,6 +268,9 @@ int main(int argc, char* argv[])
         ("iterator_tag",
             boost::program_options::value<std::string>()->default_value("random"),
             "the kind of iterator tag (random/bidirectional/forward)")
+        ("data_type",
+            boost::program_options::value<std::string>()->default_value("light"),
+            "the kind of data type (light/heavy)")
         ("test_count",
             boost::program_options::value<int>()->default_value(10),
             "number of tests to be averaged (default: 10)")
