ff2e7123776c2f409853f805b7c7ab0d38d4e7ab
John Biddiscombe <biddisco@cscs.ch>
2017-10-02 11:29:20 +0200

    Fix Copyright

--- tests/regressions/multiple_init_2918.cpp
+++ tests/regressions/multiple_init_2918.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 20017 msimberg
+//  Copyright (c) 2017 Mikael Simberg
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
