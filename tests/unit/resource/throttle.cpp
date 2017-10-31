0e731098d64765e742e572f5a6074a7ba2f28b76
Mikael Simberg <simberg@cscs.ch>
2017-10-31 09:13:08 +0100

    Clean up throttle test

--- tests/unit/resource/throttle.cpp
+++ tests/unit/resource/throttle.cpp
@@ -19,126 +19,125 @@ int hpx_main(int argc, char* argv[])
 {
     std::size_t const num_threads = hpx::resource::get_num_threads("default");
 
-    HPX_ASSERT(num_threads >= 2);
-
-    // TODO: Leave in or remove? Require at least 2 threads?
-    //HPX_TEST_EQ(std::size_t(4), hpx::resource::get_num_threads("default"));
+    HPX_TEST_EQ(std::size_t(4), num_threads);
 
     hpx::threads::detail::thread_pool_base& tp =
         hpx::resource::get_thread_pool("default");
 
-    //HPX_TEST_EQ(hpx::threads::count(tp.get_used_processing_units()), std::size_t(4));
+    HPX_TEST_EQ(hpx::threads::count(tp.get_used_processing_units()), std::size_t(4));
 
-    // Check number of used resources
-    for (std::size_t thread_num = 0; thread_num < num_threads - 1; ++thread_num)
     {
-        HPX_TEST_EQ(std::size_t(num_threads - thread_num),
-                    hpx::threads::count(tp.get_used_processing_units()));
-        tp.remove_processing_unit(thread_num);
-        HPX_TEST_EQ(std::size_t(num_threads - thread_num - 1),
-            hpx::threads::count(tp.get_used_processing_units()));
-    }
-
-    for (std::size_t thread_num = 0; thread_num < num_threads - 1; ++thread_num)
-    {
-        HPX_TEST_EQ(std::size_t(thread_num + 1),
-                    hpx::threads::count(tp.get_used_processing_units()));
-        tp.add_processing_unit(thread_num, thread_num + tp.get_thread_offset());
-        HPX_TEST_EQ(std::size_t(thread_num + 2),
-            hpx::threads::count(tp.get_used_processing_units()));
-    }
-
-    // Check when removing all but one, we end up on the same thread
-    std::size_t num_thread = 0;
-    auto test_function = [&num_thread, &tp]()
-    {
-        HPX_TEST_EQ(num_thread + tp.get_thread_offset(),
-            hpx::get_worker_thread_num());
-    };
-
-    for (num_thread = 0;
-        num_thread < num_threads;
-        ++num_thread)
-    {
-        for (std::size_t num_thread_remove = 0;
-             num_thread_remove < num_threads;
-             ++num_thread_remove)
+        // Check number of used resources
+        for (std::size_t thread_num = 0; thread_num < num_threads - 1; ++thread_num)
         {
-            if (num_thread != num_thread_remove)
-            {
-                tp.remove_processing_unit(num_thread_remove);
-            }
+            tp.remove_processing_unit(thread_num);
+            HPX_TEST_EQ(std::size_t(num_threads - thread_num - 1),
+                hpx::threads::count(tp.get_used_processing_units()));
         }
 
-        hpx::async(test_function).get();
-
-        for (std::size_t num_thread_add = 0;
-             num_thread_add < num_threads;
-             ++num_thread_add)
+        for (std::size_t thread_num = 0; thread_num < num_threads - 1; ++thread_num)
         {
-            if (num_thread != num_thread_add)
-            {
-                tp.add_processing_unit(num_thread_add,
-                    num_thread_add + tp.get_thread_offset());
-            }
+            tp.add_processing_unit(thread_num, thread_num + tp.get_thread_offset());
+            HPX_TEST_EQ(std::size_t(thread_num + 2),
+                hpx::threads::count(tp.get_used_processing_units()));
         }
     }
 
-    // Check random scheduling with reducing resources.
-    std::size_t thread_num = 0;
-    bool up = true;
-    std::vector<hpx::future<void>> fs;
-    hpx::util::high_resolution_timer t;
-    while (t.elapsed() < 2)
     {
-        for (std::size_t i = 0;
-            i < hpx::resource::get_num_threads("default") * 10;
-            ++i)
+        // Check when removing all but one, we end up on the same thread
+        std::size_t thread_num = 0;
+        auto test_function = [&thread_num, &tp]()
         {
-            fs.push_back(hpx::async([](){}));
-        }
-        if (up)
+            HPX_TEST_EQ(thread_num + tp.get_thread_offset(),
+                hpx::get_worker_thread_num());
+        };
+
+        for (thread_num = 0; thread_num < num_threads;
+            ++thread_num)
         {
-            if (thread_num != hpx::resource::get_num_threads("default") - 1)
+            for (std::size_t thread_num_remove = 0;
+                thread_num_remove < num_threads;
+                ++thread_num_remove)
             {
-                tp.remove_processing_unit(thread_num);
+                if (thread_num != thread_num_remove)
+                {
+                    tp.remove_processing_unit(thread_num_remove);
+                }
             }
 
-            ++thread_num;
+            hpx::async(test_function).get();
 
-            if (thread_num == hpx::resource::get_num_threads("default"))
+            for (std::size_t thread_num_add = 0;
+                thread_num_add < num_threads;
+                ++thread_num_add)
             {
-                up = false;
+                if (thread_num != thread_num_add)
+                {
+                    tp.add_processing_unit(thread_num_add,
+                        thread_num_add + tp.get_thread_offset());
+                }
+            }
+        }
+    }
+
+    {
+        // Check random scheduling with reducing resources.
+        std::size_t thread_num = 0;
+        bool up = true;
+        std::vector<hpx::future<void>> fs;
+        hpx::util::high_resolution_timer t;
+        while (t.elapsed() < 2)
+        {
+            for (std::size_t i = 0;
+                i < hpx::resource::get_num_threads("default") * 10;
+                ++i)
+            {
+                fs.push_back(hpx::async([](){}));
+            }
+
+            if (up)
+            {
+                if (thread_num != hpx::resource::get_num_threads("default") - 1)
+                {
+                    tp.remove_processing_unit(thread_num);
+                }
+
+                ++thread_num;
+
+                if (thread_num == hpx::resource::get_num_threads("default"))
+                {
+                    up = false;
+                    --thread_num;
+                }
+            }
+            else
+            {
+                tp.add_processing_unit(
+                    thread_num - 1, thread_num + tp.get_thread_offset() - 1);
+
                 --thread_num;
+
+                if (thread_num == 0)
+                {
+                    up = true;
+                }
             }
         }
-        else
-        {
-            tp.add_processing_unit(
-                thread_num - 1, thread_num + tp.get_thread_offset() - 1);
 
-            --thread_num;
-
-            if (thread_num == 0)
-            {
-                up = true;
-            }
-        }
+        hpx::when_all(std::move(fs)).get();
     }
-    hpx::when_all(std::move(fs)).get();
 
     return hpx::finalize();
 }
 
 int main(int argc, char* argv[])
 {
-    // TODO: Re-enable fixed number of threads.
-    //std::vector<std::string> cfg = {
-    //"hpx.os_threads=4"
-    //};
+    std::vector<std::string> cfg = {
+        "hpx.os_threads=4"
+    };
 
     // set up the resource partitioner
-    //hpx::resource::partitioner rp(argc, argv, std::move(cfg));
+    hpx::resource::partitioner rp(argc, argv, std::move(cfg));
 
     // now run the test
     HPX_TEST_EQ(hpx::init(argc, argv), 0);
