c42fbf054776d85b6d65c4ed63ede65d566f5237
Mikael Simberg <mikael.simberg@iki.fi>
2017-12-12 09:54:11 +0100

    Add back test to check that suspending threads is disallowed when elasticity is disabled

---
+++ tests/unit/resource/suspend_disabled.cpp
@@ -0,0 +1,47 @@
+//  Copyright (c) 2017 Mikael Simberg
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// Simple test verifying basic resource_partitioner functionality.
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/resource_partitioner.hpp>
+#include <hpx/include/threads.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+#include <cstddef>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+int hpx_main(int argc, char* argv[])
+{
+    // Try suspending without elasticity enabled, should throw an exception
+    try
+    {
+        hpx::threads::detail::thread_pool_base& tp =
+            hpx::resource::get_thread_pool("default");
+
+        tp.suspend_processing_unit(0);
+        HPX_TEST_MSG(false, "Suspending should not be allowed with "
+                     "elasticity disabled");
+    }
+    catch (std::runtime_error const&)
+    {
+    }
+
+    return hpx::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> cfg =
+    {
+        "hpx.os_threads=4"
+    };
+
+    hpx::resource::partitioner rp(argc, argv, std::move(cfg));
+    HPX_TEST_EQ(hpx::init(argc, argv), 0);
+}
