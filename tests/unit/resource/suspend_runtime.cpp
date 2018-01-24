f0e7ff8d8e0bd2c5aadc1b97bdaf2cbdf3dd72e4
Mikael Simberg <mikael.simberg@iki.fi>
2018-01-24 09:57:06 +0100

    Clean up runtime suspension test

--- tests/unit/resource/suspend_runtime.cpp
+++ tests/unit/resource/suspend_runtime.cpp
@@ -29,50 +29,38 @@
 
 int hpx_main(int, char**)
 {
-    std::cout << "hello from hpx_main!\n";
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    // std::vector<std::string> cfg =
-    // {
-    //     "hpx.os_threads=4"
-    // };
-
-    // hpx::resource::partitioner rp(argc, argv); //, std::move(cfg));
-
-
+    std::vector<std::string> cfg =
     {
-        // hpx::start(hpx::util::function_nonser<int(int, char**)>(), argc, argv);
-        hpx::start(argc, argv);
+        "hpx.os_threads=4"
+    };
 
-        for (std::size_t i = 0; i < 10; ++i)
-        {
-            double x = 13.2;
-            hpx::async([&x]()
+
+    hpx::start(argc, argv);
+    hpx::suspend();
+
+    for (std::size_t i = 0; i < 100; ++i)
+    {
+        hpx::resume();
+
+        hpx::async([]()
+            {
+                for (std::size_t i = 0; i < 10000; ++i)
                 {
-                    for (std::size_t i = 0; i < 20000; ++i)
-                    {
-                        hpx::async([&x]()
-                            {
-                                for (std::size_t j = 0; j < 20000; ++j)
-                                {
-                                    x -= sin(17 * x - 3.2);
-                                }
-                            });
-                    }
-                });
+                    hpx::async([](){});
+                }
+            });
 
-            hpx::suspend();
-            std::cout << x << "\n";
-            hpx::compat::this_thread::sleep_for(std::chrono::seconds(4));
-            hpx::resume();
-        }
-
-        hpx::async([]() { hpx::finalize(); });
-        hpx::stop();
+        hpx::suspend();
     }
+
+    hpx::resume();
+    hpx::async([]() { hpx::finalize(); });
+    hpx::stop();
 
     return hpx::util::report_errors();
 }
