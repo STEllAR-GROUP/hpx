2053e4ca76aa7ec034553a32350cc62a3b756d6c
Mikael Simberg <mikael.simberg@iki.fi>
2018-02-05 11:55:48 +0100

    Revert "Revert "Adapting stack overhead limit for gcc 4.9""
    This reverts commit db79e4c4cda8c546bb65d77ad424955892332e6e.

--- hpx/config.hpp
+++ hpx/config.hpp
@@ -26,6 +26,7 @@
 #include <hpx/config/forceinline.hpp>
 #include <hpx/config/lambda_capture.hpp>
 #include <hpx/config/manual_profiling.hpp>
+#include <hpx/config/threads_stack.hpp>
 #include <hpx/config/version.hpp>
 
 #include <boost/version.hpp>
@@ -406,58 +407,6 @@
 #  if defined(HPX_DEBUG)
 #    define HPX_HAVE_VERIFY_LOCKS_GLOBALY
 #  endif
-#endif
-
-///////////////////////////////////////////////////////////////////////////////
-
-#if !defined(HPX_THREADS_STACK_OVERHEAD)
-#  if defined(HPX_DEBUG)
-#    if defined(HPX_GCC_VERSION)
-#      define HPX_THREADS_STACK_OVERHEAD 0x3000
-#    else
-#      define HPX_THREADS_STACK_OVERHEAD 0x2800
-#    endif
-#  else
-#    if defined(HPX_INTEL_VERSION)
-#      define HPX_THREADS_STACK_OVERHEAD 0x2800
-#    else
-#      define HPX_THREADS_STACK_OVERHEAD 0x800
-#    endif
-#  endif
-#endif
-
-#if !defined(HPX_SMALL_STACK_SIZE)
-#  if defined(__has_feature)
-#    if __has_feature(address_sanitizer)
-#      define HPX_SMALL_STACK_SIZE  0x20000       // 128kByte
-#    endif
-#  endif
-#endif
-
-#if !defined(HPX_SMALL_STACK_SIZE)
-#  if defined(HPX_WINDOWS) && !defined(HPX_HAVE_GENERIC_CONTEXT_COROUTINES)
-#    define HPX_SMALL_STACK_SIZE    0x4000        // 16kByte
-#  else
-#    if defined(HPX_DEBUG)
-#      define HPX_SMALL_STACK_SIZE  0x20000       // 128kByte
-#    else
-#      if defined(__powerpc__) || defined(__INTEL_COMPILER)
-#         define HPX_SMALL_STACK_SIZE  0x20000       // 128kByte
-#      else
-#         define HPX_SMALL_STACK_SIZE  0x10000        // 64kByte
-#      endif
-#    endif
-#  endif
-#endif
-
-#if !defined(HPX_MEDIUM_STACK_SIZE)
-#  define HPX_MEDIUM_STACK_SIZE   0x0020000       // 128kByte
-#endif
-#if !defined(HPX_LARGE_STACK_SIZE)
-#  define HPX_LARGE_STACK_SIZE    0x0200000       // 2MByte
-#endif
-#if !defined(HPX_HUGE_STACK_SIZE)
-#  define HPX_HUGE_STACK_SIZE     0x2000000       // 32MByte
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
