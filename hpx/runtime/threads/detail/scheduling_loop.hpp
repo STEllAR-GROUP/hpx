a35c997836a32d531a4ce754288e9e405fe1e25f
John Biddiscombe <biddisco@cscs.ch>
2018-02-22 23:18:55 +0100

    Fix a couple of places in scheduling loop where task priority was not being honoured

--- hpx/runtime/threads/detail/scheduling_loop.hpp
+++ hpx/runtime/threads/detail/scheduling_loop.hpp
@@ -394,7 +394,7 @@ namespace hpx { namespace threads { namespace detail
         // Create in suspended to prevent the thread from being scheduled
         // directly...
         scheduler.SchedulingPolicy::create_thread(background_init,
-            &background_thread, suspended, hpx::throws, 
+            &background_thread, suspended, hpx::throws,
             thread_schedule_hint(num_thread));
         HPX_ASSERT(background_thread);
         scheduler.SchedulingPolicy::increment_background_thread_count();
@@ -695,7 +695,8 @@ namespace hpx { namespace threads { namespace detail
                     // scheduler queue already but the state has not been reset
                     // yet
                     scheduler.SchedulingPolicy::schedule_thread(thrd,
-                        num_thread, num_thread);
+                        num_thread, num_thread,
+                        thrd->get_priority());
                 }
 
                 // Remove the mapping from thread_map_ if HPX thread is depleted
@@ -752,7 +753,9 @@ namespace hpx { namespace threads { namespace detail
                                     scheduler.SchedulingPolicy::
                                         decrement_background_thread_count();
                                     scheduler.SchedulingPolicy::schedule_thread(
-                                        background_thread.get(), num_thread, num_thread);
+                                        background_thread.get(),
+                                        num_thread, num_thread,
+                                        background_thread->get_priority());
                                     background_thread.reset();
                                     background_running.reset();
                                 }
@@ -854,7 +857,9 @@ namespace hpx { namespace threads { namespace detail
                         scheduler.SchedulingPolicy::
                             decrement_background_thread_count();
                         scheduler.SchedulingPolicy::schedule_thread(
-                            background_thread.get(), num_thread, num_thread);
+                            background_thread.get(),
+                            num_thread, num_thread,
+                            background_thread->get_priority());
                         background_thread.reset();
                         background_running.reset();
                     }
