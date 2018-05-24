b63c889a5b6fafc67fe40fdaa938ba4d8af9c120
Mikael Simberg <mikael.simberg@iki.fi>
2018-05-24 10:22:04 +0200

    Add quickstart section to documentation

---
+++ examples/quickstart/potpourri.cpp
@@ -0,0 +1,75 @@
+//  Copyright (c) 2018 Mikael Simberg
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//[quickstart_overview
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/iostreams.hpp>
+#include <hpx/include/lcos.hpp>
+#include <hpx/include/parallel_generate.hpp>
+#include <hpx/include/parallel_sort.hpp>
+
+#include <random>
+
+void final_task(hpx::future<hpx::util::tuple<hpx::future<double>, hpx::future<void>>>)
+{
+    hpx::cout << "in final_task" << hpx::endl;
+}
+
+int main(int, char**)
+{
+    // A function can be launched asynchronously. The program will not block
+    // here until the result is available.
+    hpx::future<int> f = hpx::async([]() { return 42; });
+    hpx::cout << "Just launched a task!" << hpx::endl;
+
+    // Use get to retrieve the value from the future. This will block this task
+    // until the future is ready, but the HPX runtime will schedule other tasks
+    // if there are tasks available.
+    hpx::cout << "f contains " << f.get() << hpx::endl;
+
+    // Let's launch another task.
+    hpx::future<double> g = hpx::async([]() { return 3.14; });
+
+    // Tasks can be chained using the then method. The continuation takes the
+    // future as an argument.
+    hpx::future<double> result = g.then([](hpx::future<double>&& gg)
+        {
+            // This function will be called once g is ready. gg is g moved
+            // into the continuation.
+            return gg.get() * 42.0 * 42.0;
+        });
+
+    // You can check if a future is ready with the is_ready method.
+    hpx::cout << "Result is ready? " << result.is_ready() << hpx::endl;
+
+    // You can launch other work in the meantime. Let's sort a vector.
+    std::vector<int> v(1000000);
+
+    // We fill the vector synchronously and sequentially.
+    hpx::parallel::generate(hpx::parallel::execution::seq,
+                  std::begin(v), std::end(v), &std::rand);
+
+    // We can launch the sort in parallel and asynchronously.
+    hpx::future<void> done_sorting =
+        hpx::parallel::sort(
+            hpx::parallel::execution::par( // In parallel.
+                hpx::parallel::execution::task), // Asynchronously.
+            std::begin(v),
+            std::end(v));
+
+    // We launch the final task when the vector has been sorted and result is
+    // ready using when_all.
+    auto all = hpx::when_all(result, done_sorting).then(&final_task);
+
+    // We can wait for all to be ready.
+    all.wait();
+
+    // all must be ready at this point because we waited for it to be ready.
+    hpx::cout <<
+        (all.is_ready() ? "all is ready!" : "all is not ready...") << hpx::endl;
+
+    return hpx::finalize();
+}
+//]
--- examples/quickstart/simplest_hello_world_2.cpp
+++ examples/quickstart/simplest_hello_world_2.cpp
@@ -4,20 +4,22 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 ///////////////////////////////////////////////////////////////////////////////
-// The purpose of this example is to execute a HPX-thread printing
-// "Hello World!" once. That's all.
+// The purpose of this example is to initialize the HPX runtime explicitly and
+// execute a HPX-thread printing "Hello World!" once. That's all.
 
-//[simplest_hello_world_getting_started
-// Including 'hpx/hpx_main.hpp' instead of the usual 'hpx/hpx_init.hpp' enables
-// to use the plain C-main below as the direct main HPX entry point.
-#include <hpx/hpx_main.hpp>
+//[simplest_hello_world_2_getting_started
+#include <hpx/hpx_init.hpp>
 #include <hpx/include/iostreams.hpp>
 
-int main()
+int hpx_main(int, char**)
 {
     // Say hello to the world!
     hpx::cout << "Hello World!\n" << hpx::flush;
-    return 0;
+    return hpx::finalize();
+}
+
+int main()
+{
+    return hpx::init();
 }
 //]
-
