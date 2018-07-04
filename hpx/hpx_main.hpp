9ce5accf88b090cd5d4245116de822ab750ab52f
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2018-07-04 19:00:05 +0530

    Implementation for HPX runtime from main() for Linux based systems

--- hpx/hpx_main.hpp
+++ hpx/hpx_main.hpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2007-2014 Hartmut Kaiser
+//  Copyright (c)      2018 Nikunj Gupta
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -9,6 +10,20 @@
 #include <hpx/config.hpp>
 #include <hpx/hpx_init.hpp>
 
+// We support different implementation depending upon the Operating
+// System in use.
+#if defined(__linux) || defined(__linux__) || defined(linux)
+
+namespace hpx {
+    // include_libhpx_wrap here is an override for the one present in
+    // src/hpx_wrap.cpp. The value of this variable defines if we need
+    // to change the program's entry point or not.
+    extern int include_libhpx_wrap;
+    int include_libhpx_wrap = 1;
+}
+
+#else
+
 #if defined(HPX_HAVE_STATIC_LINKING)
 #include <hpx/hpx_main_impl.hpp>
 #endif
@@ -17,5 +32,6 @@
 // as the first HPX-thread (equivalent to hpx_main()). This is implemented by
 // a macro redefining main, so we disable it by default.
 #define main hpx_startup::user_main
+#endif
 
 #endif /*HPX_HPX_MAIN_HPP*/
