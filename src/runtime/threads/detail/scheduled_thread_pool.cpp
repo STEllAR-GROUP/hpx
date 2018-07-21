cb957088495ebd0bd6d22f1766c01b8db9be9ff4
Agustin K-ballo Berge <k@fusionfenix.com>
2018-07-21 12:42:13 -0300

    Explicitly instantiate configured schedulers

--- src/runtime/threads/detail/scheduled_thread_pool.cpp
+++ src/runtime/threads/detail/scheduled_thread_pool.cpp
@@ -10,34 +10,45 @@
 /// explicit template instantiation for the thread pools of our choice
 #if defined(HPX_HAVE_LOCAL_SCHEDULER)
 #include <hpx/runtime/threads/policies/local_queue_scheduler.hpp>
+template class HPX_EXPORT hpx::threads::policies::local_queue_scheduler<>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::local_queue_scheduler<>>;
 #endif
 
 #if defined(HPX_HAVE_STATIC_SCHEDULER)
 #include <hpx/runtime/threads/policies/static_queue_scheduler.hpp>
+template class HPX_EXPORT hpx::threads::policies::static_queue_scheduler<>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::static_queue_scheduler<>>;
 #endif
 
 #if defined(HPX_HAVE_STATIC_PRIORITY_SCHEDULER)
 #include <hpx/runtime/threads/policies/static_priority_queue_scheduler.hpp>
+template class HPX_EXPORT hpx::threads::policies::static_priority_queue_scheduler<>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::static_priority_queue_scheduler<>>;
 #endif
 
 #include <hpx/runtime/threads/policies/local_priority_queue_scheduler.hpp>
+template class HPX_EXPORT hpx::threads::policies::local_priority_queue_scheduler<
+    hpx::compat::mutex, hpx::threads::policies::lockfree_fifo>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::local_priority_queue_scheduler<hpx::compat::mutex,
         hpx::threads::policies::lockfree_fifo>>;
+template class HPX_EXPORT hpx::threads::policies::local_priority_queue_scheduler<
+    hpx::compat::mutex, hpx::threads::policies::lockfree_lifo>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::local_priority_queue_scheduler<hpx::compat::mutex,
         hpx::threads::policies::lockfree_lifo>>;
 
 #if defined(HPX_HAVE_ABP_SCHEDULER)
+template class HPX_EXPORT hpx::threads::policies::local_priority_queue_scheduler<
+    hpx::compat::mutex, hpx::threads::policies::lockfree_abp_fifo>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::local_priority_queue_scheduler<hpx::compat::mutex,
         hpx::threads::policies::lockfree_abp_fifo>>;
+template class HPX_EXPORT hpx::threads::policies::local_priority_queue_scheduler<
+    hpx::compat::mutex, hpx::threads::policies::lockfree_abp_lifo>;
 template class HPX_EXPORT hpx::threads::detail::scheduled_thread_pool<
     hpx::threads::policies::local_priority_queue_scheduler<hpx::compat::mutex,
         hpx::threads::policies::lockfree_abp_lifo>>;
