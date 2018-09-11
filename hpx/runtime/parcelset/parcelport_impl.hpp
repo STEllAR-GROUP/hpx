1f3396ad75d7d0238ad9d6966a73d390aed28149
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-09-11 10:37:07 -0500

    Fixing warnings related to silent conversion of size_t --> int16_t
    - flyby: adding some override keywords

--- hpx/runtime/parcelset/parcelport_impl.hpp
+++ hpx/runtime/parcelset/parcelport_impl.hpp
@@ -145,12 +145,12 @@ namespace hpx { namespace parcelset
             }
         }
 
-        ~parcelport_impl()
+        ~parcelport_impl() override
         {
             connection_cache_.clear();
         }
 
-        bool can_bootstrap() const
+        bool can_bootstrap() const override
         {
             return
                 connection_handler_traits<
@@ -158,7 +158,7 @@ namespace hpx { namespace parcelset
                 >::send_early_parcel::value;
         }
 
-        bool run(bool blocking = true)
+        bool run(bool blocking = true) override
         {
             io_service_pool_.run(false);    // start pool
 
@@ -170,7 +170,7 @@ namespace hpx { namespace parcelset
             return success;
         }
 
-        void flush_parcels()
+        void flush_parcels() override
         {
             // We suspend our thread, which will make progress on the network
             if(threads::get_self_ptr())
@@ -192,7 +192,7 @@ namespace hpx { namespace parcelset
             }
         }
 
-        void stop(bool blocking = true)
+        void stop(bool blocking = true) override
         {
             flush_parcels();
 
@@ -208,7 +208,8 @@ namespace hpx { namespace parcelset
         }
 
     public:
-        void put_parcel(locality const & dest, parcel p, write_handler_type f)
+        void put_parcel(
+            locality const& dest, parcel p, write_handler_type f) override
         {
             HPX_ASSERT(dest.type() == type());
 
@@ -237,7 +238,7 @@ namespace hpx { namespace parcelset
         }
 
         void put_parcels(locality const& dest, std::vector<parcel> parcels,
-            std::vector<write_handler_type> handlers)
+            std::vector<write_handler_type> handlers) override
         {
             if (parcels.size() != handlers.size())
             {
@@ -282,19 +283,19 @@ namespace hpx { namespace parcelset
                 });
         }
 
-        void send_early_parcel(locality const & dest, parcel p)
+        void send_early_parcel(locality const & dest, parcel p) override
         {
             send_early_parcel_impl<ConnectionHandler>(dest, std::move(p));
         }
 
-        util::io_service_pool* get_thread_pool(char const* name)
+        util::io_service_pool* get_thread_pool(char const* name) override
         {
             if (0 == std::strcmp(name, io_service_pool_.get_name()))
                 return &io_service_pool_;
             return nullptr;
         }
 
-        bool do_background_work(std::size_t num_thread)
+        bool do_background_work(std::size_t num_thread) override
         {
             trigger_pending_work();
             return do_background_work_impl<ConnectionHandler>(num_thread);
@@ -325,7 +326,8 @@ namespace hpx { namespace parcelset
                         this, loc),
                     "remove_from_connection_cache_delayed",
                     threads::pending, true, threads::thread_priority_normal,
-                    threads::thread_schedule_hint(get_next_num_thread()),
+                    threads::thread_schedule_hint(
+                        static_cast<std::int16_t>(get_next_num_thread())),
                     threads::thread_stacksize_default,
                     ec);
                 if (!ec) return;
@@ -334,7 +336,7 @@ namespace hpx { namespace parcelset
             connection_cache_.clear(loc);
         }
 
-        void remove_from_connection_cache(locality const& loc)
+        void remove_from_connection_cache(locality const& loc) override
         {
             error_code ec(lightweight);
             threads::thread_id_type id =
@@ -344,7 +346,8 @@ namespace hpx { namespace parcelset
                         this, loc),
                     "remove_from_connection_cache",
                     threads::suspended, true, threads::thread_priority_normal,
-                    threads::thread_schedule_hint(get_next_num_thread()),
+                    threads::thread_schedule_hint(
+                        static_cast<std::int16_t>(get_next_num_thread())),
                     threads::thread_stacksize_default,
                     ec);
             if (ec) return;
@@ -355,7 +358,7 @@ namespace hpx { namespace parcelset
         }
 
         /// Return the name of this locality
-        std::string get_locality_name() const
+        std::string get_locality_name() const override
         {
             return connection_handler().get_locality_name();
         }
@@ -363,7 +366,7 @@ namespace hpx { namespace parcelset
         ////////////////////////////////////////////////////////////////////////
         // Return the given connection cache statistic
         std::int64_t get_connection_cache_statistics(
-            connection_cache_statistics_type t, bool reset)
+            connection_cache_statistics_type t, bool reset) override
         {
             switch (t) {
                 case connection_cache_insertions:
