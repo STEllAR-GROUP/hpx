c163b51a9f0d643060483851c103bc39dcdb7ca9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-09-14 12:17:19 -0500

    Letting unwrap_result hold a reference to the wrapped id instead of a copy

--- hpx/runtime/components/unwrapping_result_policy.hpp
+++ hpx/runtime/components/unwrapping_result_policy.hpp
@@ -10,10 +10,10 @@
 
 #include <hpx/config.hpp>
 #include <hpx/lcos/detail/async_implementations.hpp>
-#include <hpx/lcos/detail/sync_implementations.hpp>
 #include <hpx/lcos/detail/async_unwrap_result_implementations.hpp>
+#include <hpx/lcos/detail/sync_implementations.hpp>
+#include <hpx/runtime/applier/detail/apply_implementations_fwd.hpp>
 #include <hpx/runtime/components/client_base.hpp>
-#include <hpx/runtime/components/target_distribution_policy.hpp>
 #include <hpx/runtime/launch_policy.hpp>
 #include <hpx/runtime/naming/id_type.hpp>
 #include <hpx/runtime/naming/name.hpp>
@@ -29,22 +29,12 @@ namespace hpx { namespace components
     /// This class is a distribution policy that can be using with actions that
     /// return futures. For those actions it is possible to apply certain
     /// optimizations if the action is invoked synchronously.
-    struct unwrapping_result_policy : target_distribution_policy
+    struct unwrapping_result_policy
     {
     public:
-        /// Default-construct a new instance of a \a unwrapping_result_policy.
-        /// This policy will represent the local locality.
-        unwrapping_result_policy() = default;
-
-        /// Create a new \a unwrapping_result_policy representing the
-        /// target locality
-        ///
-        /// \param id     [in] The global address of the target object
-        ///
-        unwrapping_result_policy operator()(id_type const& id) const
-        {
-            return unwrapping_result_policy(id);
-        }
+        unwrapping_result_policy(id_type const& id)
+          : id_(id)
+        {}
 
         /// Create a new \a unwrapping_result_policy representing the
         /// target locality
@@ -72,7 +62,7 @@ namespace hpx { namespace components
         async(launch policy, Ts&&... vs) const
         {
             return hpx::detail::async_unwrap_result_impl<Action>(
-                policy, this->get_next_target(), std::forward<Ts>(vs)...);
+                policy, get_next_target(), std::forward<Ts>(vs)...);
         }
 
         template <typename Action, typename ...Ts>
@@ -80,36 +70,74 @@ namespace hpx { namespace components
         async(launch::sync_policy, Ts&&... vs) const
         {
             return hpx::detail::sync_impl<Action>(
-                launch::sync, this->get_next_target(), std::forward<Ts>(vs)...);
+                launch::sync, get_next_target(), std::forward<Ts>(vs)...);
         }
 
         template <typename Action, typename Callback, typename ...Ts>
         typename async_result<Action>::type
         async_cb(launch policy, Callback&& cb, Ts&&... vs) const
         {
-            return this->target_distribution_policy::async_cb(policy,
+            return hpx::detail::async_cb_impl<Action>(policy,
+                get_next_target(), std::forward<Callback>(cb),
+                std::forward<Ts>(vs)...);
+        }
+
+        /// \note This function is part of the invocation policy implemented by
+        ///       this class
+        ///
+        template <typename Action, typename Continuation, typename... Ts>
+        bool apply(Continuation&& c, threads::thread_priority priority,
+            Ts&&... vs) const
+        {
+            return hpx::detail::apply_impl<Action>(std::forward<Continuation>(c),
+                get_next_target(), priority, std::forward<Ts>(vs)...);
+        }
+
+        template <typename Action, typename... Ts>
+        bool apply(threads::thread_priority priority, Ts&&... vs) const
+        {
+            return hpx::detail::apply_impl<Action>(
+                get_next_target(), priority, std::forward<Ts>(vs)...);
+        }
+
+        /// \note This function is part of the invocation policy implemented by
+        ///       this class
+        ///
+        template <typename Action, typename Continuation, typename Callback,
+            typename... Ts>
+        bool apply_cb(Continuation&& c, threads::thread_priority priority,
+            Callback&& cb, Ts&&... vs) const
+        {
+            return hpx::detail::apply_cb_impl<Action>(
+                std::forward<Continuation>(c), get_next_target(), priority,
                 std::forward<Callback>(cb), std::forward<Ts>(vs)...);
+        }
+
+        template <typename Action, typename Callback, typename... Ts>
+        bool apply_cb(
+            threads::thread_priority priority, Callback&& cb, Ts&&... vs) const
+        {
+            return hpx::detail::apply_cb_impl<Action>(
+                get_next_target(), priority,
+                std::forward<Callback>(cb), std::forward<Ts>(vs)...);
+        }
+
+        hpx::id_type const& get_next_target() const
+        {
+            return id_;
         }
 
     protected:
         /// \cond NOINTERNAL
-        unwrapping_result_policy(id_type const& id)
-          : target_distribution_policy(id)
-        {}
+        hpx::id_type const& id_;   // locality to encapsulate
         /// \endcond
     };
-
-    /// A predefined instance of the unwrap_result \a distribution_policy. It
-    /// will represent the local locality and will place all items to create
-    /// here.
-    static unwrapping_result_policy const unwrap_result{};
 }}
 
 /// \cond NOINTERNAL
 namespace hpx
 {
-    using hpx::components::unwrapping_result_policy;
-    using hpx::components::unwrap_result;
+    using unwrap_result = hpx::components::unwrapping_result_policy;
 
     namespace traits
     {
