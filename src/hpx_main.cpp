1b0e74b434e68569e26dd13c433be842a7afabfc
Thomas Heller <thom.heller@gmail.com>
2018-10-18 21:16:57 +0200

    Fixing Clang7 linker issues

--- src/hpx_main.cpp
+++ src/hpx_main.cpp
@@ -17,7 +17,7 @@
 // Forwarding of hpx_main, if necessary. This has to be in a separate
 // translation unit to ensure the linker can pick or ignore this function,
 // depending on whether the main executable defines this symbol or not.
-int hpx_main()
+HPX_WEAK_SYMBOL int hpx_main()
 {
     std::string cmdline(hpx::get_config_entry("hpx.reconstructed_cmd_line", ""));
 
