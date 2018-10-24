7aa73708a7af7cc85d05e6bf1f5bbc9e91df6689
John Biddiscombe <biddisco@cscs.ch>
2018-10-24 07:56:49 +0200

    Single locality tests can be run without using hpxrun/wrapper

--- cmake/HPX_AddTest.cmake
+++ cmake/HPX_AddTest.cmake
@@ -29,105 +29,53 @@ macro(add_hpx_test category name)
     set(_exe "${${name}_EXECUTABLE}")
   endif()
 
-  set(expected "0")
-
-  if(${name}_FAILURE_EXPECTED)
-    set(expected "1")
-  endif()
-
   set(args)
 
   foreach(arg ${${name}_UNPARSED_ARGUMENTS})
     set(args ${args} "${arg}")
   endforeach()
-  set(args "-v" "--" ${args})
 
-  set(cmd "${PYTHON_EXECUTABLE}"
+  if(${name}_LOCALITIES EQUAL "1")
+    set(_cmd ${_exe} --hpx:threads ${${name}_THREADS_PER_LOCALITY})
+    add_test(
+      NAME "${category}.${name}"
+      COMMAND ${_cmd} ${args})
+    set_tests_properties("${category}.${name}" PROPERTIES WILL_FAIL ${name}_FAILURE_EXPECTED)
+  else()
+    message("Test ${name} is a multi-locality test " ${${name}_LOCALITIES})
+    if(NOT HPX_WITH_NETWORKING)
+      message(FATAL_ERROR "Adding a multi-locality test with networking disabled")
+    endif()
+    # add distributed versions of each multi-locality test
+    foreach(_pp ${HPX_STATIC_PARCELPORT_PLUGINS}) # ${${name}_PARCELPORTS})
+      message("Adding test for parcelport ${_pp}")
+      if (${_pp} STREQUAL "mpi")
+        set(_cmd
+          "${MPIEXEC_EXECUTABLE}" "${MPIEXEC_PREFLAGS}" "${MPIEXEC_NUMPROC_FLAG}" "${${name}_LOCALITIES}"
+          "${_exe}"
+          --hpx:threads ${${name}_THREADS_PER_LOCALITY}
+        )
+      else()
+        set(expected "0")
+        if(${name}_FAILURE_EXPECTED)
+          set(expected "1")
+        endif()
+        set(_cmd "${PYTHON_EXECUTABLE}"
           "${CMAKE_BINARY_DIR}/bin/hpxrun.py"
           ${_exe}
           "-e" "${expected}"
-          "-t" "${${name}_THREADS_PER_LOCALITY}")
-
-  if(HPX_WITH_NETWORKING)
-      list(APPEND cmd "-l" "${${name}_LOCALITIES}")
-  else()
-      set(${name}_LOCALITIES "1")
+          "-t" "${${name}_THREADS_PER_LOCALITY}"
+          "-l" "${${name}_LOCALITIES}"
+          "-p" "${_pp}"
+          "-v" -- ${args})
+      endif()
+      add_test(
+        NAME "${category}.distributed.${_pp}.${name}"
+        COMMAND ${_cmd}
+      )
+      set_tests_properties("${category}.distributed.${_pp}.${name}" PROPERTIES WILL_FAIL ${name}_FAILURE_EXPECTED)
+    endforeach()
   endif()
-
-  if(${name}_LOCALITIES STREQUAL "1")
-    add_test(
-      NAME "${category}.${name}"
-      COMMAND ${cmd} ${args})
-    else()
-      if(HPX_WITH_PARCELPORT_VERBS)
-        set(_add_test FALSE)
-        if(DEFINED ${name}_PARCELPORTS)
-          set(PP_FOUND -1)
-          list(FIND ${name}_PARCELPORTS "verbs" PP_FOUND)
-          if(NOT PP_FOUND EQUAL -1)
-            set(_add_test TRUE)
-          endif()
-        else()
-          set(_add_test TRUE)
-        endif()
-        if(_add_test)
-          add_test(
-            NAME "${category}.distributed.verbs.${name}"
-            COMMAND ${cmd} "-p" "verbs" ${args})
-        endif()
-      endif()
-      if(HPX_WITH_PARCELPORT_IPC)
-        set(_add_test FALSE)
-        if(DEFINED ${name}_PARCELPORTS)
-          set(PP_FOUND -1)
-          list(FIND ${name}_PARCELPORTS "ipc" PP_FOUND)
-          if(NOT PP_FOUND EQUAL -1)
-            set(_add_test TRUE)
-          endif()
-        else()
-          set(_add_test TRUE)
-        endif()
-        if(_add_test)
-          add_test(
-            NAME "${category}.distributed.ipc.${name}"
-            COMMAND ${cmd} "-p" "ipc" ${args})
-        endif()
-      endif()
-      if(HPX_WITH_PARCELPORT_MPI)
-        set(_add_test FALSE)
-        if(DEFINED ${name}_PARCELPORTS)
-          set(PP_FOUND -1)
-          list(FIND ${name}_PARCELPORTS "mpi" PP_FOUND)
-          if(NOT PP_FOUND EQUAL -1)
-            set(_add_test TRUE)
-          endif()
-        else()
-          set(_add_test TRUE)
-        endif()
-        if(_add_test)
-          add_test(
-            NAME "${category}.distributed.mpi.${name}"
-            COMMAND ${cmd} "-p" "mpi" "-r" "mpi" ${args})
-        endif()
-      endif()
-      if(HPX_WITH_PARCELPORT_TCP)
-        set(_add_test FALSE)
-        if(DEFINED ${name}_PARCELPORTS)
-          set(PP_FOUND -1)
-          list(FIND ${name}_PARCELPORTS "tcp" PP_FOUND)
-          if(NOT PP_FOUND EQUAL -1)
-            set(_add_test TRUE)
-          endif()
-        else()
-          set(_add_test TRUE)
-        endif()
-        if(_add_test)
-          add_test(
-            NAME "${category}.distributed.tcp.${name}"
-            COMMAND ${cmd} "-p" "tcp" ${args})
-        endif()
-      endif()
-    endif()
 endmacro()
 
 macro(add_hpx_unit_test category name)
