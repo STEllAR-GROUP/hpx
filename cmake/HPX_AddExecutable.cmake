8dd451a67f70adaa047566cb18673804faece229
Christoph Junghans <junghans@lanl.gov>
2018-11-20 09:13:10 -0700

    cmake: HPX_OUTPUT_PREFIX -> HPX_WITH_EXECUTABLE_PREFIX
    sed -i 's/HPX_OUTPUT_PREFIX/HPX_WITH_EXECUTABLE_PREFIX/g' `git grep --files-with-matches HPX_OUTPUT_PREFIX`

--- cmake/HPX_AddExecutable.cmake
+++ cmake/HPX_AddExecutable.cmake
@@ -149,7 +149,7 @@ function(add_hpx_executable name)
     endif()
   endif()
 
-  set_target_properties(${name}_exe PROPERTIES OUTPUT_NAME "${HPX_OUTPUT_PREFIX}${name}")
+  set_target_properties(${name}_exe PROPERTIES OUTPUT_NAME "${HPX_WITH_EXECUTABLE_PREFIX}${name}")
 
   if(exclude_from_all)
     set_target_properties(${name}_exe PROPERTIES ${exclude_from_all})
