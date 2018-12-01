dde3f12bd8255f954a6de0332f3054a0700f5846
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-12-01 10:48:20 -0600

    Make sure HPX is usable with latest released version of Vc (V1.4.1)

--- tests/unit/parallel/datapar_algorithms/transform_binary2_datapar.cpp
+++ tests/unit/parallel/datapar_algorithms/transform_binary2_datapar.cpp
@@ -32,7 +32,9 @@ void transform_binary2_test()
 {
     test_transform_binary2<std::random_access_iterator_tag>();
     test_transform_binary2<std::forward_iterator_tag>();
+#if defined(HPX_HAVE_ALGORITHM_INPUT_ITERATOR_SUPPORT)
     test_transform_binary2<std::input_iterator_tag>();
+#endif
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -54,7 +56,9 @@ void transform_binary2_exception_test()
 {
     test_transform_binary2_exception<std::random_access_iterator_tag>();
     test_transform_binary2_exception<std::forward_iterator_tag>();
+#if defined(HPX_HAVE_ALGORITHM_INPUT_ITERATOR_SUPPORT)
     test_transform_binary2_exception<std::input_iterator_tag>();
+#endif
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -76,7 +80,9 @@ void transform_binary2_bad_alloc_test()
 {
     test_transform_binary2_bad_alloc<std::random_access_iterator_tag>();
     test_transform_binary2_bad_alloc<std::forward_iterator_tag>();
+#if defined(HPX_HAVE_ALGORITHM_INPUT_ITERATOR_SUPPORT)
     test_transform_binary2_bad_alloc<std::input_iterator_tag>();
+#endif
 }
 
 ///////////////////////////////////////////////////////////////////////////////
