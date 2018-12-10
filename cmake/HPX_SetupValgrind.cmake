0aa91d896bc78c64c676980b17c894620d8c66ef
Christopher Hinz <chinz07@qubusproject.org>
2018-12-10 15:29:57 +0100

    Try to resolve inspect warnings

--- cmake/HPX_SetupValgrind.cmake
+++ cmake/HPX_SetupValgrind.cmake
@@ -1,3 +1,8 @@
+# Copyright (c) 2018 Christopher Hinz
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 if(HPX_WITH_VALGRIND)
     find_package(Valgrind)
     if(NOT VALGRIND_FOUND)
