a3475327fac6d684984f01908a2bd445ca4106a7
John Biddiscombe <biddisco@cscs.ch>
2018-12-12 00:01:42 +0100

    Remove more run_now arguments from thread creation

--- hpx/parallel/executors/thread_execution.hpp
+++ hpx/parallel/executors/thread_execution.hpp
@@ -114,7 +114,6 @@ namespace hpx { namespace threads
             util::deferred_call(std::forward<F>(f), std::forward<Ts>(ts)...),
             "hpx::parallel::execution::post",
             threads::pending,
-            true,
             exec.get_stacksize(),
             threads::thread_schedule_hint(),
             throws);
@@ -134,7 +133,6 @@ namespace hpx { namespace threads
             util::deferred_call(std::forward<F>(f), std::forward<Ts>(ts)...),
             "hpx::parallel::execution::post",
             threads::pending,
-            true,
             exec.get_stacksize(),
             std::forward<Hint>(hint),
             throws);
--- hpx/runtime/threads/detail/create_work.hpp
+++ hpx/runtime/threads/detail/create_work.hpp
@@ -93,17 +93,8 @@ namespace hpx { namespace threads { namespace detail
         if (data.priority == thread_priority_default)
             data.priority = thread_priority_normal;
 
-        if (thread_priority_high == data.priority ||
-            thread_priority_high_recursive == data.priority ||
-            thread_priority_boost == data.priority)
-        {
-            // For critical priority threads, create the thread immediately.
-            scheduler->create_thread(data, nullptr, initial_state, true, ec);
-        }
-        else {
-            // Create a task description for the new thread.
-            scheduler->create_thread(data, nullptr, initial_state, ec);
-        }
+        // Create a task description for the new thread.
+        scheduler->create_thread(data, nullptr, initial_state, ec);
     }
 }}}
 
