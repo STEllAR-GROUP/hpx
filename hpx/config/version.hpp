20443a716e5c38d408593ebbc415ac93d415f351
Mikael Simberg <mikael.simberg@iki.fi>
2019-02-01 16:44:01 +0100

    Update version to 1.2.1-rc1

--- hpx/config/version.hpp
+++ hpx/config/version.hpp
@@ -26,19 +26,19 @@
 //  HPX_VERSION_DATE   YYYYMMDD is the date of the release
 //                               (estimated release date for master branch)
 //
-#define HPX_VERSION_FULL         0x010200
+#define HPX_VERSION_FULL         0x010201
 
 #define HPX_VERSION_MAJOR        1
 #define HPX_VERSION_MINOR        2
-#define HPX_VERSION_SUBMINOR     0
+#define HPX_VERSION_SUBMINOR     1
 
-#define HPX_VERSION_DATE         20181112
+#define HPX_VERSION_DATE         20190201
 
 #if !defined(HPX_AGAS_VERSION)
     #define HPX_AGAS_VERSION 0x30
 #endif
 
-#define HPX_VERSION_TAG          ""
+#define HPX_VERSION_TAG          "-rc1"
 
 #if !defined(HPX_HAVE_GIT_COMMIT)
     #define HPX_HAVE_GIT_COMMIT  "unknown"
