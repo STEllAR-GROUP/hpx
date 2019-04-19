8eb24772d517f2312942b98bd32272cf679fcfd1
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-04-19 16:38:12 -0500

    Make the deprecation warnings issued by the old pp headers optional

---
+++ libs/preprocessor/include_compatibility/hpx/util/detail/pp/strip_parens.hpp
@@ -0,0 +1,19 @@
+//  Copyright (c) 2019 Thomas Heller
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/preprocessor/config/defines.hpp>
+#include <hpx/preprocessor/strip_parens.hpp>
+
+#if defined(HPX_PREPROCESSOR_HAVE_DEPRECATION_WARNINGS)
+#if defined(_MSC_VER)
+#pragma message(                                                               \
+    "The header hpx/util/detail/pp/cat.hpp is deprecated,                      \
+    please include hpx/preprocessor/cat.hpp instead")
+#else
+#warning                                                                       \
+    "The header hpx/util/detail/pp/cat.hpp is deprecated,                      \
+    please include hpx/preprocessor/cat.hpp instead"
+#endif
+#endif
