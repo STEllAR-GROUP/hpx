aea25f18697effe78c064c4e59c6d4554b870dd3
aurianer <aurianer@cscs.ch>
2019-06-17 13:48:08 +0200

    Add prepend to list function + change add_hpx_module
    add_hpx_module:
    - Remove source_root, header_root for modules and set them into the
    add_hpx_module_function.
    - Use prepend to get the absolute path back in the add_hpx_module
    func.

--- cmake/HPX_AddModule.cmake
+++ cmake/HPX_AddModule.cmake
@@ -5,43 +5,46 @@
 
 function(add_hpx_module name)
   # Retrieve arguments
-  set(options DEPRECATION_WARNINGS COMPATIBILITY_HEADERS)
-  set(one_value_args SOURCE_ROOT HEADER_ROOT COMPAT_HEADER_ROOT)
+  set(options DEPRECATION_WARNINGS)
+  set(one_value_args COMPATIBILITY_HEADERS)
   set(multi_value_args SOURCES HEADERS COMPAT_HEADERS DEPENDENCIES CMAKE_SUBDIRS)
   cmake_parse_arguments(${name} "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-  if(NOT ${name}_SOURCE_ROOT)
-      set(${name}_SOURCE_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/src")
-  endif()
-  hpx_debug("Add module ${name}: ${name}_SOURCE_ROOT: ${${name}_SOURCE_ROOT}")
+  include(HPX_AddDefinitions)
+  include(HPX_Message)
+  include(HPX_Option)
+  
+  hpx_info("  ${name}")
 
-  if(NOT ${name}_HEADER_ROOT)
-      set(${name}_HEADER_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/include")
+  # Compatibility header off is not specified
+  if (NOT ${name}_COMPATIBILITY_HEADERS)
+      set(${name}_COMPATIBILITY_HEADERS OFF)
   endif()
-  hpx_debug("Add module ${name}: ${name}_HEADER_ROOT: ${${name}_HEADER_ROOT}")
 
-  if(HPX_${name_upper}_WITH_COMPATIBILITY_HEADERS)
-    if(NOT ${name}_COMPAT_HEADER_ROOT)
-        set(${name}_COMPAT_HEADER_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/include_compatibility")
-    endif()
-    hpx_debug("Add module ${name}: ${name}_COMPAT_HEADER_ROOT: ${${name}_COMPAT_HEADER_ROOT}")
-  endif()
+  # Main directories of the module
+  set(SOURCE_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/src")
+  hpx_debug("Add module ${name}: SOURCE_ROOT: ${SOURCE_ROOT}")
+  set(HEADER_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/include")
+  hpx_debug("Add module ${name}: HEADER_ROOT: ${HEADER_ROOT}")
+  set(COMPAT_HEADER_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/include_compatibility")
+  hpx_debug("Add module ${name}: COMPAT_HEADER_ROOT: ${COMPAT_HEADER_ROOT}")
 
   string(TOUPPER ${name} name_upper)
 
+  # HPX options
+
   hpx_option(HPX_${name_upper}_WITH_TESTS
     BOOL
-    "Build HPX ${name} module tests. (default: ON)"
-    ON ADVANCED
+    "Build HPX ${name} module tests. (default: ${HPX_WITH_TESTS})"
+    ${HPX_WITH_TESTS} ADVANCED
     CATEGORY "Modules")
-  
+
   if(${name}_DEPRECATION_WARNINGS)
     hpx_option(HPX_${name_upper}_WITH_DEPRECATION_WARNINGS
       BOOL
       "Enable warnings for deprecated facilities. (default: ${HPX_WITH_DEPRECATION_WARNINGS})"
       ${HPX_WITH_DEPRECATION_WARNINGS} ADVANCED
       CATEGORY "Modules")
-    
     if(HPX_${name}_WITH_DEPRECATION_WARNINGS)
       hpx_add_config_define_namespace(
           DEFINE HPX_${name_upper}_HAVE_DEPRECATION_WARNINGS
@@ -56,7 +59,6 @@ function(add_hpx_module name)
       "Enable compatibility headers for old headers"
       ON ADVANCED
       CATEGORY "Modules")
-    
     if(HPX_${name_upper}_WITH_COMPATIBILITY_HEADERS)
       hpx_add_config_define_namespace(
           DEFINE HPX_${name_upper}_HAVE_COMPATIBILITY_HEADERS
@@ -64,40 +66,44 @@ function(add_hpx_module name)
     endif()
   endif()
 
-  set(sources ${${name}_SOURCES})
+  # Write full path for the sources files
+  include(HPX_CMakeUtils)
+  prepend(sources ${SOURCE_ROOT} ${${name}_SOURCES})
+  prepend(headers ${HEADER_ROOT} ${${name}_HEADERS})
+  if(HPX_${name_upper}_WITH_COMPATIBILITY_HEADERS)
+    prepend(compat_headers ${COMPAT_HEADER_ROOT} ${${name}_COMPAT_HEADERS})
+  endif()
+  # To make the global header file
   set(tmp_headers ${${name}_HEADERS})
-  set(compat_headers ${${name}_COMPAT_HEADERS})
 
   # Add a global include file that include all module headers
-  FILE(WRITE ${${name}_HEADER_ROOT}/hpx/${name}/${name}_module.hpp
+  FILE(WRITE ${HEADER_ROOT}/hpx/${name}/${name}_module.hpp
       "//  Copyright (c) 2019 The STE||AR GROUP\n"
       "//\n"
       "//  Distributed under the Boost Software License, Version 1.0. (See accompanying\n"
       "//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)\n\n"
   )
   foreach(header_file ${tmp_headers})
-    FILE(APPEND ${${name}_HEADER_ROOT}/hpx/${name}/${name}_module.hpp
+    FILE(APPEND ${HEADER_ROOT}/hpx/${name}/${name}_module.hpp
       "#include <${header_file}>\n"
     )
-    list(APPEND headers "${${name}_HEADER_ROOT}/${header_file}")
+    list(APPEND headers "${HEADER_ROOT}/${header_file}")
   endforeach(header_file)
 
   foreach(header_file ${headers})
       hpx_debug(${header_file})
   endforeach(header_file)
 
-  message(STATUS "${name}: Configuring")
-  
   add_library(hpx_${name} STATIC ${sources} ${headers} ${compat_headers})
   
   target_link_libraries(hpx_${name} ${${name}_DEPENDENCIES})
   target_include_directories(hpx_${name} PUBLIC
-    $<BUILD_INTERFACE:${${name}_HEADER_ROOT}>
+    $<BUILD_INTERFACE:${HEADER_ROOT}>
     $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}>)
   
   if(HPX_${name_upper}_WITH_COMPATIBILITY_HEADERS)
     target_include_directories(hpx_${name} PUBLIC
-      $<BUILD_INTERFACE:${${name}_COMPAT_HEADER_ROOT}>)
+      $<BUILD_INTERFACE:${COMPAT_HEADER_ROOT}>)
   endif()
   
   target_compile_definitions(hpx_${name} PRIVATE
@@ -108,19 +114,18 @@ function(add_hpx_module name)
   
   add_hpx_source_group(
     NAME hpx_{name}
-    ROOT ${${name}_HEADER_ROOT}/hpx
+    ROOT ${HEADER_ROOT}/hpx
     CLASS "Header Files"
     TARGETS ${headers})
   add_hpx_source_group(
     NAME hpx_{name}
-    ROOT ${${name}_SOURCE_ROOT}/hpx
+    ROOT ${SOURCE_ROOT}/hpx
     CLASS "Source Files"
     TARGETS ${sources})
-  
   if(HPX_${name_upper}_WITH_COMPATIBILITY_HEADERS)
     add_hpx_source_group(
       NAME hpx_${name}
-      ROOT ${${name}_COMPAT_HEADER_ROOT}/hpx
+      ROOT ${COMPAT_HEADER_ROOT}/hpx
       CLASS "Header Files"
       TARGETS ${compat_headers})
   endif()
@@ -160,6 +165,4 @@ function(add_hpx_module name)
     add_subdirectory(${dir})
   endforeach(dir)
   
-  message(STATUS "${name}: Configuring done")
-
 endfunction()
