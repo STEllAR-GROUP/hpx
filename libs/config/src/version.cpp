1cc03d3f624f535925b834954d95e4b978bc4d2e
Mikael Simberg <mikael.simberg@iki.fi>
2019-06-27 09:26:56 +0200

    Force linking of module symbols into main library

--- libs/config/src/version.cpp
+++ libs/config/src/version.cpp
@@ -6,12 +6,21 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <hpx/config/export_definitions.hpp>
+#include <hpx/config/force_linking.hpp>
 #include <hpx/config/version.hpp>
 #include <hpx/preprocessor/stringize.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 namespace hpx
 {
-    char const HPX_CHECK_VERSION[] = HPX_PP_STRINGIZE(HPX_CHECK_VERSION);
-    char const HPX_CHECK_BOOST_VERSION[] = HPX_PP_STRINGIZE(HPX_CHECK_BOOST_VERSION);
+    HPX_EXPORT char const HPX_CHECK_VERSION[] =
+        HPX_PP_STRINGIZE(HPX_CHECK_VERSION);
+    HPX_EXPORT char const HPX_CHECK_BOOST_VERSION[] =
+        HPX_PP_STRINGIZE(HPX_CHECK_BOOST_VERSION);
+
+    namespace config
+    {
+        void force_linking() {}
+    }
 }
--- libs/preprocessor/src/preprocessor.cpp
+++ libs/preprocessor/src/preprocessor.cpp
@@ -1,7 +1,12 @@
-//  Copyright (c) 2019 Hartmut Kaiser
+//  Copyright (c) 2019 STE||AR Group
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// This file was intentionally left empty. It is used as a dummy to force cmake
-// into generating a separate Visual Studio project for this module.
+#include <hpx/preprocessor/force_linking.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace preprocessor
+{
+    void force_linking() {}
+}}
