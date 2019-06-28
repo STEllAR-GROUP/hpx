d767b6a97bfe79df68e540dff2d99ea997ee70db
Mikael Simberg <mikael.simberg@iki.fi>
2019-06-28 17:39:31 +0200

    Add missing force_linking.hpp header to config module

---
+++ libs/config/include/hpx/config/force_linking.hpp
@@ -0,0 +1,14 @@
+//  Copyright (c) 2019 The STE||AR GROUP
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_CONFIG_FORCE_LINKING_HPP)
+#define HPX_CONFIG_FORCE_LINKING_HPP
+
+namespace hpx { namespace config
+{
+    void force_linking();
+}}
+
+#endif
