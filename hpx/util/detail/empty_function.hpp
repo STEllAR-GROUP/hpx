e21e6354019465b4783024aa56f1e9f8d88f46bc
Mikael Simberg <simberg@cscs.ch>
2019-08-02 10:16:12 +0200

    Use fallback get_empty_function_vtable with CUDA enabled
    Using the usual version results in:
    
    Error: Internal Compiler Error (codegen): "there was an error in verifying the lgenfe output!"
    
    with at least gcc 7/cuda 9.2 and gcc 8/cuda 10.1.

--- hpx/util/detail/empty_function.hpp
+++ hpx/util/detail/empty_function.hpp
@@ -30,7 +30,10 @@ namespace hpx { namespace util { namespace detail
     template <typename Sig, bool Copyable>
     struct function_vtable;
 
-#if defined(HPX_HAVE_CXX11_CONSTEXPR)
+// NOTE: nvcc (at least CUDA 9.2 and 10.1) fails with an internal compiler error
+// ("there was an error in verifying the lgenfe output!") with this enabled, so
+// we explicitly use the fallback.
+#if defined(HPX_HAVE_CXX11_CONSTEXPR) && !defined(HPX_HAVE_CUDA)
     template <typename Sig>
     HPX_CONSTEXPR function_vtable<Sig, true> const*
     get_empty_function_vtable() noexcept
