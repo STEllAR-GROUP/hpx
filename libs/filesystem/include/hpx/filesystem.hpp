d241dfdb9b20e33dd240b5c72ea272e1d9a64d45
Mikael Simberg <mikael.simberg@iki.fi>
2019-08-28 09:43:46 +0200

    Allow filesystem.hpp to include boost/filesystem.hpp

--- libs/filesystem/include/hpx/filesystem.hpp
+++ libs/filesystem/include/hpx/filesystem.hpp
@@ -3,6 +3,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+//  hpxinspect:nodeprecatedinclude:boost/filesystem.hpp
+
 /// \file
 /// This file provides a compatibility layer using Boost.Filesystem for the
 /// C++17 filesystem library. It is *not* intended to be a complete
