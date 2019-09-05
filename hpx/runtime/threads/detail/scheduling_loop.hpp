90ff8c4c6d0b362d7ea4a4009b0405977c08fd4e
John Biddiscombe <biddisco@cscs.ch>
2019-09-05 16:35:08 +0200

    Add missing include that causes build fails

--- hpx/runtime/threads/detail/scheduling_loop.hpp
+++ hpx/runtime/threads/detail/scheduling_loop.hpp
@@ -11,6 +11,9 @@
 #include <hpx/runtime/get_thread_name.hpp>
 #include <hpx/runtime/threads/policies/scheduler_base.hpp>
 #include <hpx/runtime/threads/thread_data.hpp>
+#if defined(HPX_HAVE_BACKGROUND_THREAD_COUNTERS) && defined(HPX_HAVE_THREAD_IDLE_RATES)
+# include <hpx/runtime/threads/scoped_background_timer.hpp>
+#endif
 #include <hpx/state.hpp>
 #include <hpx/hardware/timestamp.hpp>
 #include <hpx/concurrency/itt_notify.hpp>
