f5be070f06d82e8506d4bcf5928ca675743b8854
aurianer <aurianer@cscs.ch>
2019-09-20 12:20:54 +0200

    Clang-format format module

--- libs/format/src/format.cpp
+++ libs/format/src/format.cpp
@@ -18,8 +18,7 @@
 #include <sstream>
 #include <string>
 
-namespace hpx { namespace util { namespace detail
-{
+namespace hpx { namespace util { namespace detail {
     ///////////////////////////////////////////////////////////////////////////
     inline std::size_t format_atoi(
         boost::string_ref str, std::size_t* pos = nullptr) noexcept
@@ -38,12 +37,11 @@ namespace hpx { namespace util { namespace detail
         return r;
     }
 
-    inline boost::string_ref format_substr(
-        boost::string_ref str,
+    inline boost::string_ref format_substr(boost::string_ref str,
         std::size_t start, std::size_t end = boost::string_ref::npos) noexcept
     {
-        return start < str.size()
-            ? str.substr(start, end - start) : boost::string_ref{};
+        return start < str.size() ? str.substr(start, end - start) :
+                                    boost::string_ref{};
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -64,15 +62,15 @@ namespace hpx { namespace util { namespace detail
 
             std::size_t const id = format_atoi(arg_id);
             return format_field{id, spec};
-        } else {
+        }
+        else
+        {
             std::size_t const id = format_atoi(field);
             return format_field{id, ""};
         }
     }
 
-    void format_to(
-        std::ostream& os,
-        boost::string_ref format_str,
+    void format_to(std::ostream& os, boost::string_ref format_str,
         format_arg const* args, std::size_t count)
     {
         std::size_t index = 0;
@@ -83,11 +81,14 @@ namespace hpx { namespace util { namespace detail
                 HPX_ASSERT(!format_str.empty());
                 if (format_str[1] == format_str[0])
                 {
-                    os.write(format_str.data(), 1); // '{' or '}'
-                } else {
+                    os.write(format_str.data(), 1);    // '{' or '}'
+                }
+                else
+                {
                     HPX_ASSERT(format_str[0] != '}');
                     std::size_t const end = format_str.find('}');
-                    boost::string_ref field_str = format_substr(format_str, 1, end);
+                    boost::string_ref field_str =
+                        format_substr(format_str, 1, end);
                     format_field const field = parse_field(field_str);
                     format_str.remove_prefix(end - 1);
 
@@ -98,7 +99,9 @@ namespace hpx { namespace util { namespace detail
                     ++index;
                 }
                 format_str.remove_prefix(2);
-            } else {
+            }
+            else
+            {
                 std::size_t const next = format_str.find_first_of("{}");
                 std::size_t const count =
                     next != format_str.npos ? next : format_str.size();
@@ -110,11 +113,10 @@ namespace hpx { namespace util { namespace detail
     }
 
     std::string format(
-        boost::string_ref format_str,
-        format_arg const* args, std::size_t count)
+        boost::string_ref format_str, format_arg const* args, std::size_t count)
     {
         std::ostringstream os;
         detail::format_to(os, format_str, args, count);
         return os.str();
     }
-}}}
+}}}    // namespace hpx::util::detail
