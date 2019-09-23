049018aedff4edd68b8842c87949824b4a1d0a2d
aurianer <aurianer@cscs.ch>
2019-09-23 12:00:18 +0200

    Add the SPDX license in .py and .sh files

--- tests/performance/network/network_storage/plot_utils.py
+++ tests/performance/network/network_storage/plot_utils.py
@@ -1,7 +1,8 @@
-#  Copyright (c) 2014-2017 John Biddiscombe
+# Copyright (c) 2014-2017 John Biddiscombe
 #
-#  Distributed under the Boost Software License, Version 1.0. (See accompanying
-#  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# SPDX-License-Identifier: BSL-1.0
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 import optparse
 import math
--- tools/generate_issue_pr_list.sh
+++ tools/generate_issue_pr_list.sh
@@ -2,6 +2,7 @@
 #
 # Copyright (c) 2019 Mikael Simberg
 #
+# SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
 
