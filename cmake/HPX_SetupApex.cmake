c15bddf56612ddbaa3054a7a49e6edb131299d63
aurianer <aurianer@cscs.ch>
2019-09-26 13:15:12 +0200

    Fix inspect error on missing SPDX licenses

--- cmake/HPX_SetupApex.cmake
+++ cmake/HPX_SetupApex.cmake
@@ -10,6 +10,7 @@
 # Copyright (c) 2017      Taeguk Kwon
 # Copyright (c) 2018 Christopher Hinz
 #
+# SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
