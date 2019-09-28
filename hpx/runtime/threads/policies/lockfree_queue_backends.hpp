a146b7daf79ac34b5c4902eb6c6f8cd6c297ac3c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-09-28 09:01:00 -0500

    Clang-formatting file
    - flyby typedef -> using

--- hpx/runtime/threads/policies/lockfree_queue_backends.hpp
+++ hpx/runtime/threads/policies/lockfree_queue_backends.hpp
@@ -25,269 +25,265 @@
 #include <cstdint>
 #include <utility>
 
-namespace hpx { namespace threads { namespace policies
-{
+namespace hpx { namespace threads { namespace policies {
 
-struct lockfree_fifo;
+    struct lockfree_fifo;
 
-///////////////////////////////////////////////////////////////////////////////
-// FIFO
-template <typename T>
-struct lockfree_fifo_backend
-{
-    typedef boost::lockfree::queue<T> container_type;
-
-    typedef T value_type;
-    typedef T& reference;
-    typedef T const& const_reference;
-    typedef std::uint64_t size_type;
-
-    lockfree_fifo_backend(
-        size_type initial_size = 0
-      , size_type num_thread = size_type(-1)
-        )
-      : queue_(std::size_t(initial_size))
-    {}
-
-    bool push(const_reference val, bool /*other_end*/ = false)
-    {
-        return queue_.push(val);
-    }
-
-    bool pop(reference val, bool steal = true)
-    {
-        return queue_.pop(val);
-    }
-
-    bool empty()
-    {
-        return queue_.empty();
-    }
-
-  private:
-    container_type queue_;
-};
-
-struct lockfree_fifo
-{
+    ///////////////////////////////////////////////////////////////////////////////
+    // FIFO
     template <typename T>
-    struct apply
+    struct lockfree_fifo_backend
     {
-        typedef lockfree_fifo_backend<T> type;
+        using container_type = boost::lockfree::queue<T>;
+
+        using value_type = T;
+        using reference = T&;
+        using const_reference = T const&;
+        using size_type = std::uint64_t;
+
+        lockfree_fifo_backend(
+            size_type initial_size = 0, size_type num_thread = size_type(-1))
+          : queue_(std::size_t(initial_size))
+        {
+        }
+
+        bool push(const_reference val, bool /*other_end*/ = false)
+        {
+            return queue_.push(val);
+        }
+
+        bool pop(reference val, bool steal = true)
+        {
+            return queue_.pop(val);
+        }
+
+        bool empty()
+        {
+            return queue_.empty();
+        }
+
+    private:
+        container_type queue_;
     };
-};
 
-///////////////////////////////////////////////////////////////////////////////
-// MoodyCamel FIFO
-template <typename T>
-struct moodycamel_fifo_backend
-{
-    typedef moodycamel::ConcurrentQueue<T> container_type;
-    typedef T value_type;
-    typedef T& reference;
-    typedef T const& const_reference;
-    typedef T && rval_reference;
-    typedef std::uint64_t size_type;
-
-    moodycamel_fifo_backend(
-        size_type initial_size = 0
-      , size_type num_thread = size_type(-1)
-        )
-      : queue_(std::size_t(initial_size))
-    {}
-
-    bool push(rval_reference val, bool /*other_end*/ = false)
+    struct lockfree_fifo
     {
-        return queue_.enqueue(std::move(val));
-    }
+        template <typename T>
+        struct apply
+        {
+            using type = lockfree_fifo_backend<T>;
+        };
+    };
 
-    bool push(const_reference val, bool /*other_end*/ = false)
-    {
-        return queue_.enqueue(val);
-    }
-
-    bool pop(reference val, bool steal = true)
-    {
-        return queue_.try_dequeue(val);
-    }
-
-    bool empty()
-    {
-        return (queue_.size_approx()==0);
-    }
-
-  private:
-    container_type queue_;
-};
-
-struct concurrentqueue_fifo
-{
+    ////////////////////////////////////////////////////////////////////////////
+    // MoodyCamel FIFO
     template <typename T>
-    struct apply
+    struct moodycamel_fifo_backend
     {
-        typedef moodycamel_fifo_backend<T> type;
+        using container_type = moodycamel::ConcurrentQueue<T>;
+
+        using value_type = T;
+        using reference = T&;
+        using const_reference = T const&;
+        using rval_reference = T&&;
+        using size_type = std::uint64_t;
+
+        moodycamel_fifo_backend(
+            size_type initial_size = 0, size_type num_thread = size_type(-1))
+          : queue_(std::size_t(initial_size))
+        {
+        }
+
+        bool push(rval_reference val, bool /*other_end*/ = false)
+        {
+            return queue_.enqueue(std::move(val));
+        }
+
+        bool push(const_reference val, bool /*other_end*/ = false)
+        {
+            return queue_.enqueue(val);
+        }
+
+        bool pop(reference val, bool steal = true)
+        {
+            return queue_.try_dequeue(val);
+        }
+
+        bool empty()
+        {
+            return (queue_.size_approx() == 0);
+        }
+
+    private:
+        container_type queue_;
     };
-};
+
+    struct concurrentqueue_fifo
+    {
+        template <typename T>
+        struct apply
+        {
+            using type = moodycamel_fifo_backend<T>;
+        };
+    };
 
 // LIFO
 #if defined(HPX_HAVE_CXX11_STD_ATOMIC_128BIT)
-struct lockfree_lifo;
+    struct lockfree_lifo;
 
-template <typename T>
-struct lockfree_lifo_backend
-{
-    typedef boost::lockfree::deque<T> container_type;
-
-    typedef T value_type;
-    typedef T& reference;
-    typedef T const& const_reference;
-    typedef std::uint64_t size_type;
-
-    lockfree_lifo_backend(
-        size_type initial_size = 0
-      , size_type num_thread = size_type(-1)
-        )
-      : queue_(std::size_t(initial_size))
-    {}
-
-    bool push(const_reference val, bool other_end = false)
-    {
-        if (other_end)
-            return queue_.push_right(val);
-        return queue_.push_left(val);
-    }
-
-    bool pop(reference val, bool steal = true)
-    {
-        return queue_.pop_left(val);
-    }
-
-    bool empty()
-    {
-        return queue_.empty();
-    }
-
-  private:
-    container_type queue_;
-};
-
-struct lockfree_lifo
-{
     template <typename T>
-    struct apply
+    struct lockfree_lifo_backend
     {
-        typedef lockfree_lifo_backend<T> type;
-    };
-};
+        using container_type = boost::lockfree::deque<T>;
 
-///////////////////////////////////////////////////////////////////////////////
-// FIFO + stealing at opposite end.
-#if defined(HPX_HAVE_ABP_SCHEDULER)
-struct lockfree_abp_fifo;
-struct lockfree_abp_lifo;
+        using value_type = T;
+        using reference = T&;
+        using const_reference = T const&;
+        using size_type = std::uint64_t;
 
-template <typename T>
-struct lockfree_abp_fifo_backend
-{
-    typedef boost::lockfree::deque<T> container_type;
-    typedef T value_type;
-    typedef T& reference;
-    typedef T const& const_reference;
-    typedef std::uint64_t size_type;
+        lockfree_lifo_backend(
+            size_type initial_size = 0, size_type num_thread = size_type(-1))
+          : queue_(std::size_t(initial_size))
+        {
+        }
 
-    lockfree_abp_fifo_backend(
-        size_type initial_size = 0
-      , size_type num_thread = size_type(-1)
-        )
-      : queue_(std::size_t(initial_size))
-    {}
+        bool push(const_reference val, bool other_end = false)
+        {
+            if (other_end)
+                return queue_.push_right(val);
+            return queue_.push_left(val);
+        }
 
-    bool push(const_reference val, bool /*other_end*/ = false)
-    {
-        return queue_.push_left(val);
-    }
-
-    bool pop(reference val, bool steal = true)
-    {
-        if (steal)
+        bool pop(reference val, bool steal = true)
+        {
             return queue_.pop_left(val);
-        return queue_.pop_right(val);
-    }
+        }
 
-    bool empty()
-    {
-        return queue_.empty();
-    }
+        bool empty()
+        {
+            return queue_.empty();
+        }
 
-  private:
-    container_type queue_;
-};
-
-struct lockfree_abp_fifo
-{
-    template <typename T>
-    struct apply
-    {
-        typedef lockfree_abp_fifo_backend<T> type;
+    private:
+        container_type queue_;
     };
-};
 
-///////////////////////////////////////////////////////////////////////////////
-// LIFO + stealing at opposite end.
-// E.g. ABP (Arora, Blumofe and Plaxton) queuing
-// http://dl.acm.org/citation.cfm?id=277678
-template <typename T>
-struct lockfree_abp_lifo_backend
-{
-    typedef boost::lockfree::deque<T> container_type;
-    typedef T value_type;
-    typedef T& reference;
-    typedef T const& const_reference;
-    typedef std::uint64_t size_type;
-
-    lockfree_abp_lifo_backend(
-        size_type initial_size = 0
-      , size_type num_thread = size_type(-1)
-        )
-      : queue_(std::size_t(initial_size))
-    {}
-
-    bool push(const_reference val, bool other_end = false)
+    struct lockfree_lifo
     {
-        if (other_end)
-            return queue_.push_right(val);
-        return queue_.push_left(val);
-    }
+        template <typename T>
+        struct apply
+        {
+            using type = lockfree_lifo_backend<T>;
+        };
+    };
 
-    bool pop(reference val, bool steal = true)
+#if defined(HPX_HAVE_ABP_SCHEDULER)
+    ////////////////////////////////////////////////////////////////////////////
+    // FIFO + stealing at opposite end.
+    struct lockfree_abp_fifo;
+    struct lockfree_abp_lifo;
+
+    template <typename T>
+    struct lockfree_abp_fifo_backend
     {
-        if (steal)
+        using container_type = boost::lockfree::deque<T>;
+
+        using value_type = T;
+        using reference = T&;
+        using const_reference = T const&;
+        using size_type = std::uint64_t;
+
+        lockfree_abp_fifo_backend(
+            size_type initial_size = 0, size_type num_thread = size_type(-1))
+          : queue_(std::size_t(initial_size))
+        {
+        }
+
+        bool push(const_reference val, bool /*other_end*/ = false)
+        {
+            return queue_.push_left(val);
+        }
+
+        bool pop(reference val, bool steal = true)
+        {
+            if (steal)
+                return queue_.pop_left(val);
             return queue_.pop_right(val);
-        return queue_.pop_left(val);
-    }
+        }
 
-    bool empty()
-    {
-        return queue_.empty();
-    }
+        bool empty()
+        {
+            return queue_.empty();
+        }
 
-  private:
-    container_type queue_;
-};
-
-struct lockfree_abp_lifo
-{
-    template <typename T>
-    struct apply
-    {
-        typedef lockfree_abp_lifo_backend<T> type;
+    private:
+        container_type queue_;
     };
-};
 
-#endif // HPX_HAVE_ABP_SCHEDULER
-#endif // HPX_HAVE_CXX11_STD_ATOMIC_128BIT
+    struct lockfree_abp_fifo
+    {
+        template <typename T>
+        struct apply
+        {
+            using type = lockfree_abp_fifo_backend<T>;
+        };
+    };
 
-}}}
+    ////////////////////////////////////////////////////////////////////////////
+    // LIFO + stealing at opposite end.
+    // E.g. ABP (Arora, Blumofe and Plaxton) queuing
+    // http://dl.acm.org/citation.cfm?id=277678
+    template <typename T>
+    struct lockfree_abp_lifo_backend
+    {
+        using container_type = boost::lockfree::deque<T>;
 
-#endif // HPX_FB3518C8_4493_450E_A823_A9F8A3185B2D
+        using value_type = T;
+        using reference = T&;
+        using const_reference = T const&;
+        using size_type = std::uint64_t;
 
+        lockfree_abp_lifo_backend(
+            size_type initial_size = 0, size_type num_thread = size_type(-1))
+          : queue_(std::size_t(initial_size))
+        {
+        }
+
+        bool push(const_reference val, bool other_end = false)
+        {
+            if (other_end)
+                return queue_.push_right(val);
+            return queue_.push_left(val);
+        }
+
+        bool pop(reference val, bool steal = true)
+        {
+            if (steal)
+                return queue_.pop_right(val);
+            return queue_.pop_left(val);
+        }
+
+        bool empty()
+        {
+            return queue_.empty();
+        }
+
+    private:
+        container_type queue_;
+    };
+
+    struct lockfree_abp_lifo
+    {
+        template <typename T>
+        struct apply
+        {
+            using type = lockfree_abp_lifo_backend<T>;
+        };
+    };
+
+#endif    // HPX_HAVE_ABP_SCHEDULER
+#endif    // HPX_HAVE_CXX11_STD_ATOMIC_128BIT
+
+}}}    // namespace hpx::threads::policies
+
+#endif    // HPX_FB3518C8_4493_450E_A823_A9F8A3185B2D
