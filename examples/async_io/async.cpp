1fcb851aa9066bbfb61ac0c329fb2d518bbba0b8
Tapasweni Pathak <tapaswenipathak@gmail.com>
2019-10-05 02:34:10 +0530

    example fixes

--- examples/async_io/async.cpp
+++ examples/async_io/async.cpp
@@ -1,7 +1,11 @@
 #include <hpx/lcos/future.hpp>
+#include <hpx/async.hpp>
+#include <hpx/hpx_main.hpp>
+
 using namespace hpx;
 int main() {
   future<int> f = async([]() -> int { return 91; });
   int val = f.get();
+  return hpx::finalize();
 }
 
