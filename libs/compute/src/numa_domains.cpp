afddd930fc1ab32ca1798d7836b20b2e18741af3
Mikael Simberg <simberg@cscs.ch>
2019-10-23 13:17:12 +0200

    Format compute module

--- libs/compute/src/numa_domains.cpp
+++ libs/compute/src/numa_domains.cpp
@@ -8,9 +8,9 @@
 
 #include <hpx/compute/host/numa_domains.hpp>
 #include <hpx/compute/host/target.hpp>
+#include <hpx/resource_partitioner/detail/partitioner.hpp>
 #include <hpx/runtime.hpp>
 #include <hpx/runtime/get_os_thread_count.hpp>
-#include <hpx/resource_partitioner/detail/partitioner.hpp>
 #include <hpx/topology/cpu_mask.hpp>
 #include <hpx/topology/topology.hpp>
 
