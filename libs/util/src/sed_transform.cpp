e700b8cb006d7556a9028834a49ba4af22b7e503
Mikael Simberg <mikael.simberg@iki.fi>
2019-11-04 17:12:35 +0100

    Move sed_transform files to util module

--- libs/util/src/sed_transform.cpp
+++ libs/util/src/sed_transform.cpp
@@ -6,7 +6,6 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
-#include <hpx/errors.hpp>
 #include <hpx/util/sed_transform.hpp>
 
 #include <memory>
