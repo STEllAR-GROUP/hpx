59f1db1e2618401d1564bedaef4550b1235cbc66
Mikael Simberg <mikael.simberg@iki.fi>
2019-11-11 09:29:59 +0100

    Sort includes in batch_environment files

--- libs/batch_environments/tests/unit/parse_slurm_environment.cpp
+++ libs/batch_environments/tests/unit/parse_slurm_environment.cpp
@@ -4,8 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/testing.hpp>
 #include <hpx/batch_environments/slurm_environment.hpp>
+#include <hpx/testing.hpp>
 
 #include <array>
 #include <cstddef>
--- libs/batch_environments/tests/unit/parse_slurm_nodelist.cpp
+++ libs/batch_environments/tests/unit/parse_slurm_nodelist.cpp
@@ -4,9 +4,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/batch_environments.hpp>
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_main.hpp>
-#include <hpx/batch_environments.hpp>
 
 #include <iostream>
 #include <string>
