9a18bb5c4cd3c79c53d10539d5b0dfa74f7ae4e0
Kevin Huck <khuck@cs.uoregon.edu>
2019-11-20 13:42:06 -0800

    fixing annotated task to use the name, not the desc

--- hpx/util/annotated_function.hpp
+++ hpx/util/annotated_function.hpp
@@ -83,7 +83,7 @@ namespace hpx { namespace util
             /* update the task wrapper in APEX to use the specified name */
             threads::set_self_timer_data(
                 external_timer::update_task(threads::get_self_timer_data(),
-                desc_));
+                std::string(name)));
 #endif
         }
 
