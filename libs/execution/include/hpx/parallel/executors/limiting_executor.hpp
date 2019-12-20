fc94fcfed5643e378d19c437ab6410baa9c2aa82
Mikael Simberg <simberg@cscs.ch>
2019-12-20 12:11:19 +0100

    Format limiting_executor

--- libs/execution/include/hpx/parallel/executors/limiting_executor.hpp
+++ libs/execution/include/hpx/parallel/executors/limiting_executor.hpp
@@ -9,19 +9,19 @@
 
 #include <hpx/config.hpp>
 #include <hpx/apply.hpp>
+#include <hpx/functional/invoke.hpp>
 #include <hpx/parallel/executors/execution_fwd.hpp>
 #include <hpx/runtime/threads/executors/default_executor.hpp>
 #include <hpx/runtime/threads/executors/pool_executor.hpp>
 #include <hpx/runtime/threads/thread_executor.hpp>
 #include <hpx/runtime/threads/thread_pool_base.hpp>
-#include <hpx/functional/invoke.hpp>
 #include <hpx/util/yield_while.hpp>
 
 #include <atomic>
 #include <cstddef>
 #include <cstdint>
-#include <string>
 #include <iostream>
+#include <string>
 #include <type_traits>
 #include <utility>
 
@@ -30,8 +30,7 @@
 // --------------------------------------------------------------------
 //
 // --------------------------------------------------------------------
-namespace hpx { namespace parallel { namespace execution
-{
+namespace hpx { namespace parallel { namespace execution {
     template <typename Executor>
     struct limiting_executor
     {
@@ -44,34 +43,39 @@ namespace hpx { namespace parallel { namespace execution
         //
         // --------------------------------------------------------------------
         limiting_executor(std::size_t lower, std::size_t upper,
-                          bool block_on_destruction=true)
-            : executor_(Executor())
-            , count_(0)
-            , lower_threshold_(lower)
-            , upper_threshold_(upper)
-            , block_(block_on_destruction) {}
+            bool block_on_destruction = true)
+          : executor_(Executor())
+          , count_(0)
+          , lower_threshold_(lower)
+          , upper_threshold_(upper)
+          , block_(block_on_destruction)
+        {
+        }
 
-        limiting_executor(const Executor& ex, std::size_t lower, std::size_t upper,
-                            bool block_on_destruction=true)
-            : executor_(ex)
-            , count_(0)
-            , lower_threshold_(lower)
-            , upper_threshold_(upper)
-            , block_(block_on_destruction) {}
+        limiting_executor(const Executor& ex, std::size_t lower,
+            std::size_t upper, bool block_on_destruction = true)
+          : executor_(ex)
+          , count_(0)
+          , lower_threshold_(lower)
+          , upper_threshold_(upper)
+          , block_(block_on_destruction)
+        {
+        }
 
         ~limiting_executor()
         {
-            if (block_) {
-                set_and_wait(0,0);
+            if (block_)
+            {
+                set_and_wait(0, 0);
             }
         }
 
         void count_up()
         {
-            if (++count_ > upper_threshold_) {
-                hpx::util::yield_while([&](){
-                    return (count_ > lower_threshold_);
-                });
+            if (++count_ > upper_threshold_)
+            {
+                hpx::util::yield_while(
+                    [&]() { return (count_ > lower_threshold_); });
             }
         }
 
@@ -83,20 +87,17 @@ namespace hpx { namespace parallel { namespace execution
         // --------------------------------------------------------------------
         // post : for general apply()
         // --------------------------------------------------------------------
-        template <typename F, typename ... Ts>
-        void
-        post(F && f, Ts &&... ts)
+        template <typename F, typename... Ts>
+        void post(F&& f, Ts&&... ts)
         {
             count_up();
             auto&& args = hpx::util::make_tuple(std::forward<Ts>(ts)...);
-            parallel::execution::post(
-                executor_,
-                [this, HPX_CAPTURE_FORWARD(f), HPX_CAPTURE_FORWARD(args)]() mutable
-                {
+            parallel::execution::post(executor_,
+                [this, HPX_CAPTURE_FORWARD(f),
+                    HPX_CAPTURE_FORWARD(args)]() mutable {
                     hpx::util::invoke_fused(std::move(f), std::move(args));
                     count_down();
-                }
-            );
+                });
         }
 
         // --------------------------------------------------------------------
@@ -112,18 +113,14 @@ namespace hpx { namespace parallel { namespace execution
 
             count_up();
             auto&& args = hpx::util::make_tuple(std::forward<Ts>(ts)...);
-            lcos::local::futures_factory<result_type()> p(
-                executor_,
-                [this, HPX_CAPTURE_FORWARD(f), HPX_CAPTURE_FORWARD(args)]() mutable
-                {
+            lcos::local::futures_factory<result_type()> p(executor_,
+                [this, HPX_CAPTURE_FORWARD(f),
+                    HPX_CAPTURE_FORWARD(args)]() mutable {
                     hpx::util::invoke_fused(std::move(f), std::move(args));
                     count_down();
-                }
-            );
+                });
 
-            p.apply(
-                launch::async,
-                threads::thread_priority_default,
+            p.apply(launch::async, threads::thread_priority_default,
                 threads::thread_stacksize_default);
 
             return p.get_future();
@@ -146,18 +143,14 @@ namespace hpx { namespace parallel { namespace execution
             count_up();
 
             auto&& args = hpx::util::make_tuple(std::forward<Ts>(ts)...);
-            lcos::local::futures_factory<result_type()> p(
-                executor_,
-                [this, HPX_CAPTURE_FORWARD(f), HPX_CAPTURE_FORWARD(args)]() mutable
-                {
+            lcos::local::futures_factory<result_type()> p(executor_,
+                [this, HPX_CAPTURE_FORWARD(f),
+                    HPX_CAPTURE_FORWARD(args)]() mutable {
                     hpx::util::invoke_fused(std::move(f), std::move(args));
                     count_down();
-                }
-            );
+                });
 
-            p.apply(
-                launch::async,
-                threads::thread_priority_default,
+            p.apply(launch::async, threads::thread_priority_default,
                 threads::thread_stacksize_default);
 
             return p.get_future();
@@ -177,18 +170,17 @@ namespace hpx { namespace parallel { namespace execution
 
         void wait()
         {
-            hpx::util::yield_while([&](){
-                return (count_ > lower_threshold_);
-            });
+            hpx::util::yield_while(
+                [&]() { return (count_ > lower_threshold_); });
         }
 
     private:
         // --------------------------------------------------------------------
-        Executor                         executor_;
-        std::atomic<std::int64_t>        count_;
-        std::int64_t                     lower_threshold_;
-        std::int64_t                     upper_threshold_;
-        bool                             block_;
+        Executor executor_;
+        std::atomic<std::int64_t> count_;
+        std::int64_t lower_threshold_;
+        std::int64_t upper_threshold_;
+        bool block_;
     };
 
     template <typename Executor>
