296432d75d9fcd2b13466f5fac1a6ac14353f918
John Biddiscombe <biddisco@cscs.ch>
2020-01-14 11:44:16 +0100

    Initialize APEX using MPI if networking is off, but MPI support is enabled

--- src/runtime_impl.cpp
+++ src/runtime_impl.cpp
@@ -36,6 +36,10 @@
 #include <hpx/util/thread_mapper.hpp>
 #include <hpx/basic_execution/this_thread.hpp>
 
+#if defined(HPX_HAVE_LIB_MPI)
+#include <mpi.h>
+#endif
+
 #include <condition_variable>
 #include <cstddef>
 #include <cstdint>
@@ -406,8 +410,32 @@ namespace hpx
 
         // initialize instrumentation system
 #ifdef HPX_HAVE_APEX
+#if !defined(HPX_HAVE_NETWORKING) && defined(HPX_HAVE_LIB_MPI)
+        int rank_=0, size_=1, is_initialized_=0;
+        // Check if MPI_Init has been called previously
+        MPI_Initialized(&is_initialized_);
+        if(is_initialized_) {
+            MPI_Comm_rank(MPI_COMM_WORLD, &rank_);
+            MPI_Comm_size(MPI_COMM_WORLD, &size_);
+        }
+        else {
+            int required, provided;
+            required = MPI_THREAD_MULTIPLE;
+            MPI_Init_thread(0, nullptr, required, &provided);
+            if (provided < MPI_THREAD_FUNNELED) {
+                std::cout << "Your MPI installation doesn't allow multiple threads. Exiting.\n";
+                std::terminate();
+            }
+            MPI_Comm_rank(MPI_COMM_WORLD, &rank_);
+            MPI_Comm_size(MPI_COMM_WORLD, &size_);
+            std::cout << "Initializing mpi on rank " << rank_ << " of " << size_ << std::endl;
+        }
+        std::cout << "Initializing instrumentation with rank " << rank_ << " of " << size_ << std::endl;
+        util::external_timer::init(nullptr, rank_, size_);
+#else
         util::external_timer::init(nullptr, hpx::get_locality_id(),
                 hpx::get_initial_num_localities());
+#endif
 #endif
 
 
