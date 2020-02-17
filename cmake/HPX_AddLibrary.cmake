852d65b0a9690f6456d4c5779160702824a4a409
John Biddiscombe <biddisco@cscs.ch>
2020-02-17 11:33:26 +0100

    Clean up CUDA CMake and do not build cuda modules when not enabled
    Remove checks for cuda that are not needed if the modules
    are not built. Clean up CMake vars.

--- cmake/HPX_AddLibrary.cmake
+++ cmake/HPX_AddLibrary.cmake
@@ -212,7 +212,7 @@ function(add_hpx_library name)
     endforeach()
   endif()
 
-  if(HPX_WITH_CUDA AND NOT HPX_WITH_CUDA_CLANG)
+  if(HPX_WITH_CUDA_COMPUTE AND NOT HPX_WITH_CUDA_CLANG)
     cuda_add_library(
       ${name} ${${name}_linktype} ${exclude_from_all} ${${name}_SOURCES}
       ${${name}_HEADERS} ${${name}_AUXILIARY}
