d74ee101a382560e9e23d025c12b458a06f4b675
Mikael Simberg <mikael.simberg@iki.fi>
2020-03-10 15:13:38 +0100

    Remove boost/cstdint.hpp support

--- libs/serialization/tests/unit/serialization_builtins.cpp
+++ libs/serialization/tests/unit/serialization_builtins.cpp
@@ -4,13 +4,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// hpxinspect:nodeprecatedinclude:boost/cstdint.hpp
-
 #include <hpx/serialization/input_archive.hpp>
 #include <hpx/serialization/output_archive.hpp>
 #include <hpx/serialization/serialize.hpp>
-
-#include <boost/cstdint.hpp>
 
 #include <cstdint>
 #include <vector>
@@ -38,26 +34,6 @@ struct A
         ar& t_;
     }
 };
-
-#if defined(BOOST_HAS_INT128)
-std::ostream& operator<<(std::ostream& s, boost::int128_type i)
-{
-    std::int64_t low = i;
-    i >>= 64;
-    std::int64_t high = i;
-    s << std::hex << "high: i" << high << "; low: " << low;
-    return s;
-}
-
-std::ostream& operator<<(std::ostream& s, boost::uint128_type i)
-{
-    std::uint64_t low = i;
-    i >>= 64;
-    std::uint64_t high = i;
-    s << std::hex << "high: i" << high << "; low: " << low;
-    return s;
-}
-#endif
 
 #include <hpx/testing.hpp>
 
@@ -208,14 +184,6 @@ int main()
         (std::numeric_limits<unsigned long>::min)() + 100);
     test<unsigned long>((std::numeric_limits<unsigned long>::max)() - 100,
         (std::numeric_limits<unsigned long>::max)());
-#if defined(BOOST_HAS_INT128)
-    test<boost::int128_type>(
-        (std::numeric_limits<boost::int128_type>::max)() - 100,
-        (std::numeric_limits<boost::int128_type>::max)());
-    test<boost::uint128_type>(
-        (std::numeric_limits<boost::uint128_type>::max)() - 100,
-        (std::numeric_limits<boost::uint128_type>::max)());
-#endif
     test_fp<float>((std::numeric_limits<float>::min)(),
         (std::numeric_limits<float>::min)() + 100);
     test_fp<float>(-100, 100);
