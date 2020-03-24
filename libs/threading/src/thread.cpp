8742c5597aec5ab4e82df11fb660a827694b456f
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-03-24 16:35:07 -0500

    Fixing execution of thread exit functions
    - flyby: fixing solution FOLDER for performance data test

--- libs/threading/src/thread.cpp
+++ libs/threading/src/thread.cpp
@@ -110,6 +110,7 @@ namespace hpx {
                 "null thread id encountered");
         }
         threads::run_thread_exit_callbacks(id);
+        threads::free_thread_exit_callbacks(id);
     }
 
     threads::thread_result_type thread::thread_function_nullary(
---
+++ libs/threading_base/tests/regressions/thread_local_data.cpp
@@ -0,0 +1,41 @@
+//  Copyright (c) 2020 Hartmut Kaiser
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_main.hpp>
+
+#include <hpx/async.hpp>
+#include <hpx/testing.hpp>
+#include <hpx/threading_base.hpp>
+
+#include <atomic>
+#include <cstddef>
+
+std::atomic<bool> data_deallocated(false);
+
+struct test_data
+{
+    test_data() = default;
+
+    ~test_data()
+    {
+        data_deallocated = true;
+    }
+};
+
+void test()
+{
+    hpx::threads::thread_id_type id = hpx::threads::get_self_id();
+    test_data* p = new test_data;
+    hpx::threads::add_thread_exit_callback(id, [p]() { delete p; });
+    hpx::threads::set_thread_data(id, reinterpret_cast<std::size_t>(p));
+}
+
+int main()
+{
+    hpx::async(&test).get();
+    HPX_TEST(data_deallocated);
+    return hpx::util::report_errors();
+}
