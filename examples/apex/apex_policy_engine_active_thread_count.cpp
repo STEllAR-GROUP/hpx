d2e1397a4eb3910080df876bb2efd68a288c1eda
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-03-25 08:22:56 -0500

    Fixing APEX error

--- examples/apex/apex_policy_engine_active_thread_count.cpp
+++ examples/apex/apex_policy_engine_active_thread_count.cpp
@@ -31,8 +31,6 @@ std::uint64_t fibonacci(std::uint64_t n);
 // invocation to work.
 HPX_PLAIN_ACTION(fibonacci, fibonacci_action);
 
-hpx::naming::id_type counter_id;
-
 ///////////////////////////////////////////////////////////////////////////////
 std::uint64_t fibonacci(std::uint64_t n)
 {
@@ -57,35 +55,29 @@ std::uint64_t fibonacci(std::uint64_t n)
 
 using hpx::naming::id_type;
 using hpx::performance_counters::get_counter;
-using hpx::performance_counters::stubs::performance_counter;
+using hpx::performance_counters::performance_counter;
 using hpx::performance_counters::counter_value;
 using hpx::performance_counters::status_is_valid;
 static bool counters_initialized = false;
 static const char * counter_name = "/threadqueue{{locality#{}/total}}/length";
 
-id_type get_counter_id() {
+performance_counter get_counter()
+{
     // Resolve the GID of the performances counter using it's symbolic name.
     std::uint32_t const prefix = hpx::get_locality_id();
-    /*
-    id_type id = get_counter(hpx::util::format(
-        "/threads{{locality#{}/total}}/count/instantaneous/active",
-        prefix));
-    */
-    id_type id = get_counter(hpx::util::format(counter_name, prefix));
-    return id;
+    return performance_counter(hpx::util::format(counter_name, prefix));
 }
 
 void setup_counters() {
     try {
-        id_type id = get_counter_id();
+        performance_counter counter = get_counter();
         // We need to explicitly start all counters before we can use them. For
         // certain counters this could be a no-op, in which case start will
         // return 'false'.
         performance_counter::start(hpx::launch::sync, id);
         std::cout << "Counters initialized! " << id << std::endl;
-        counter_value value = performance_counter::get_value(hpx::launch::sync, id);
+        counter_value value = counter.get_counter_value(hpx::launch::sync);
         std::cout << "Active threads " << value.get_value<int>() << std::endl;
-        counter_id = id;
     }
     catch(hpx::exception const& e) {
         std::cerr << "apex_policy_engine_active_thread_count: caught exception: "
@@ -120,10 +112,10 @@ int hpx_main(hpx::program_options::variables_map& vm)
 bool test_function(apex_context const& context) {
     if (!counters_initialized) return false;
     try {
-        //id_type id = get_counter_id();
-        counter_value value1 =
-            performance_counter::get_value(hpx::launch::sync, counter_id);
-        if (value1.get_value<int>() % 2 == 1) {
+        performance_counter counter(get_counter());
+        counter_value value1 = counter.get_counter_value(hpx::launch::sync);
+        if (value1.get_value<int>() % 2 == 1)
+        {
           return APEX_NOERROR;
         } else {
           std::cerr << "Expecting an error message..." << std::endl;
