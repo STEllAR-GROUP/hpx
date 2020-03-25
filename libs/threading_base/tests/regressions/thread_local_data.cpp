47579e99f352b9e4653b42a35e0e040fbcf8a525
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-03-25 08:36:38 -0500

    Making test more realistic

--- libs/threading_base/tests/regressions/thread_local_data.cpp
+++ libs/threading_base/tests/regressions/thread_local_data.cpp
@@ -29,7 +29,16 @@ void test()
 {
     hpx::threads::thread_id_type id = hpx::threads::get_self_id();
     test_data* p = new test_data;
-    hpx::threads::add_thread_exit_callback(id, [p]() { delete p; });
+    hpx::threads::add_thread_exit_callback(id, [p, id]() {
+        hpx::threads::thread_id_type id1 = hpx::threads::get_self_id();
+        HPX_TEST_EQ(id1, id);
+
+        test_data* p1 =
+            reinterpret_cast<test_data*>(hpx::threads::get_thread_data(id1));
+        HPX_TEST_EQ(p1, p);
+
+        delete p;
+    });
     hpx::threads::set_thread_data(id, reinterpret_cast<std::size_t>(p));
 }
 
