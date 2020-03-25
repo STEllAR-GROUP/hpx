a054f8c5a94b40864d00645c7361dbbe7bcc27ea
Mikael Simberg <simberg@cscs.ch>
2020-03-25 12:00:59 +0100

    Suppress MPI warnings in version.cpp

--- libs/version/src/version.cpp
+++ libs/version/src/version.cpp
@@ -19,9 +19,24 @@
 #include <boost/version.hpp>
 
 #if defined(HPX_HAVE_PARCELPORT_MPI)
+#if defined(__clang__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wcast-qual"
+#elif defined (__GNUC__)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wcast-qual"
+#endif
+
 // Intel MPI does not like to be included after stdio.h. As such, we include mpi.h
 // as soon as possible.
 #include <mpi.h>
+
+#if defined(__clang__)
+#  pragma clang diagnostic pop
+#elif defined (__GNUC__)
+#  pragma GCC diagnostic pop
+#endif
+
 #endif
 
 #include <hwloc.h>
