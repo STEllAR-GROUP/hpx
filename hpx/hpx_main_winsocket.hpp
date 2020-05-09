cb27c477ddef7325595c69eefac5eb03a225757a
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-05-09 09:43:24 -0500

    Fixing #pragma once problem

--- hpx/hpx_main_winsocket.hpp
+++ hpx/hpx_main_winsocket.hpp
@@ -4,8 +4,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_MAIN_WINSOCKET_MAY_08_2020_1050AM)
-#define HPX_MAIN_WINSOCKET_MAY_08_2020_1050AM
+#pragma once
 
 #include <hpx/config.hpp>
 
@@ -16,5 +15,4 @@ namespace hpx { namespace detail
     HPX_EXPORT void init_winsocket();
 }}
 
-#endif
 #endif
