63e7b2df20da25b6f889f413d69b0febd92910f9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-05-11 09:11:10 -0500

    Flyby: fixing copy&paste problem

--- libs/basic_execution/src/agent_ref.cpp
+++ libs/basic_execution/src/agent_ref.cpp
@@ -34,7 +34,7 @@ namespace hpx { namespace basic_execution {
 #ifdef HPX_HAVE_VERIFY_LOCKS
         util::verify_no_locks();
 #endif
-        impl_->yield(desc);
+        impl_->yield_k(k, desc);
     }
 
     void agent_ref::suspend(const char* desc)
