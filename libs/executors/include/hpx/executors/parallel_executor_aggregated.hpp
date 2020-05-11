b17156b62da5d309a574e54e36e16358c6bd0e05
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-05-11 09:12:22 -0500

    Adding missing #include

--- libs/executors/include/hpx/executors/parallel_executor_aggregated.hpp
+++ libs/executors/include/hpx/executors/parallel_executor_aggregated.hpp
@@ -14,6 +14,7 @@
 #include <hpx/execution/algorithms/detail/predicates.hpp>
 #include <hpx/execution/detail/async_launch_policy_dispatch.hpp>
 #include <hpx/execution/detail/post_policy_dispatch.hpp>
+#include <hpx/execution/detail/sync_launch_policy_dispatch.hpp>
 #include <hpx/execution/executors/static_chunk_size.hpp>
 #include <hpx/execution/traits/is_executor.hpp>
 #include <hpx/functional/invoke.hpp>
