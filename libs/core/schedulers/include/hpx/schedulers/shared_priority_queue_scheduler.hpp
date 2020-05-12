e9f3221244e764b14e598a0b1597391f59e89a5c
Thomas Heller <thomas.heller@exasol.com>
2020-05-12 12:06:46 +0200

    Fixing UB in shared priority queue
    These problems were detected by ubsan:
      - Uninitialized access to boolean variables
      - Index out of bounds access for the debug output

--- libs/core/schedulers/include/hpx/schedulers/shared_priority_queue_scheduler.hpp
+++ libs/core/schedulers/include/hpx/schedulers/shared_priority_queue_scheduler.hpp
@@ -175,6 +175,7 @@ namespace hpx { namespace threads { namespace policies {
           , debug_init_(false)
           , thread_init_counter_(0)
         {
+            set_scheduler_mode(scheduler_mode::default_mode);
             HPX_ASSERT(num_workers_ != 0);
         }
 
@@ -427,8 +428,8 @@ namespace hpx { namespace threads { namespace policies {
                             , "D", debug::dec<2>(domain_num)
                             , "Q", debug::dec<3>(q_index)
                             , "this"
-                            , "D", debug::dec<2>(d_lookup_[local_num])
-                            , "Q", debug::dec<3>(local_num)
+                            , "D", debug::dec<2>(d_lookup_[thread_num])
+                            , "Q", debug::dec<3>(thread_num)
                             , "run_now OVERRIDE ", data.run_now
                             , debug::threadinfo<thread_init_data>(data));
                 // clang-format on
@@ -443,8 +444,8 @@ namespace hpx { namespace threads { namespace policies {
                             , "D", debug::dec<2>(domain_num)
                             , "Q", debug::dec<3>(q_index)
                             , "this"
-                            , "D", debug::dec<2>(d_lookup_[local_num])
-                            , "Q", debug::dec<3>(local_num)
+                            , "D", debug::dec<2>(d_lookup_[thread_num])
+                            , "Q", debug::dec<3>(thread_num)
                             , "run_now", data.run_now
                             , debug::threadinfo<thread_init_data>(data));
                 // clang-format on
