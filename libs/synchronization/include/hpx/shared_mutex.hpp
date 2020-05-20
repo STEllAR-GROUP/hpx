7b877136d80b4ce7997653be20ac502d86bffbe9
Mikael Simberg <mikael.simberg@iki.fi>
2020-05-20 11:39:38 +0200

    Add hpx/mutex.hpp header

---
+++ libs/synchronization/include/hpx/shared_mutex.hpp
@@ -0,0 +1,13 @@
+//  Copyright (c) 2020 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/synchronization/shared_mutex.hpp>
+
+namespace hpx {
+    using hpx::lcos::local::shared_mutex;
+}    // namespace hpx
