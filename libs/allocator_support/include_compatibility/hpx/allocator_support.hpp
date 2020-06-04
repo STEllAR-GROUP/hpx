48845237a20918697912544e7096b0cec4b3ec2d
Mikael Simberg <mikael.simberg@iki.fi>
2020-06-04 18:38:19 +0200

    Add compatibility headers for old module headers

---
+++ libs/allocator_support/include_compatibility/hpx/allocator_support.hpp
@@ -0,0 +1,19 @@
+//  Copyright (c) 2020 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/config.hpp>
+#include <hpx/allocator_support/config/defines.hpp>
+#include <hpx/modules/allocator_support.hpp>
+
+#if defined(HPX_ALLOCATOR_SUPPORT_HAVE_DEPRECATION_WARNINGS)
+#if defined(HPX_MSVC)
+#pragma message("The header hpx/allocator_support.hpp is deprecated, \
+    please include hpx/modules/allocator_support.hpp instead")
+#else
+#warning "The header hpx/allocator_support.hpp is deprecated, \
+    please include hpx/modules/allocator_support.hpp instead"
+#endif
+#endif
