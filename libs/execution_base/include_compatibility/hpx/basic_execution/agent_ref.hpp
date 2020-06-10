94749b4849ba11eb99a54e9c92212b037279a951
aurianer <aurianer@cscs.ch>
2020-06-10 18:34:49 +0200

    Add compatibility headers for basic_execution renaming

---
+++ libs/execution_base/include_compatibility/hpx/basic_execution/agent_ref.hpp
@@ -0,0 +1,19 @@
+//  Copyright (c) 2019 Ste||ar Group
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/config.hpp>
+#include <hpx/execution_base/config/defines.hpp>
+#include <hpx/execution_base/agent_ref.hpp>
+
+#if defined(HPX_EXECUTION_BASE_HAVE_DEPRECATION_WARNINGS)
+#if defined(HPX_MSVC)
+#pragma message("The header hpx/basic_execution/agent_ref.hpp is deprecated, \
+    please include hpx/execution_base/agent_ref.hpp instead")
+#else
+#warning "The header hpx/basic_execution/agent_ref.hpp is deprecated, \
+    please include hpx/execution_base/agent_ref.hpp instead"
+#endif
+#endif
