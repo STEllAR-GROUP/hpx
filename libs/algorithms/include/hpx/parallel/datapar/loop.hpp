3707ceb1f18fb9529490bde5cb873e98aaf9c6b5
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-07-03 00:41:53 +0200

    Fix datapar header paths

--- libs/algorithms/include/hpx/parallel/datapar/loop.hpp
+++ libs/algorithms/include/hpx/parallel/datapar/loop.hpp
@@ -10,9 +10,9 @@
 
 #if defined(HPX_HAVE_DATAPAR)
 #include <hpx/execution/algorithms/detail/predicates.hpp>
-#include <hpx/execution/parallel/traits/vector_pack_alignment_size.hpp>
-#include <hpx/execution/parallel/traits/vector_pack_load_store.hpp>
-#include <hpx/execution/parallel/traits/vector_pack_type.hpp>
+#include <hpx/execution/traits/vector_pack_alignment_size.hpp>
+#include <hpx/execution/traits/vector_pack_load_store.hpp>
+#include <hpx/execution/traits/vector_pack_type.hpp>
 #include <hpx/execution/traits/is_execution_policy.hpp>
 #include <hpx/executors/datapar/execution_policy_fwd.hpp>
 #include <hpx/parallel/datapar/iterator_helpers.hpp>
