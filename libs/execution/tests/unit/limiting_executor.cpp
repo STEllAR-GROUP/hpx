d90c2ecdd8a4257b5d191c1f71308e8837e9400b
John Biddiscombe <biddisco@cscs.ch>
2020-08-19 09:50:37 +0200

    Add new limiting executor test that checks number of in flight tasks

--- libs/execution/tests/unit/limiting_executor.cpp
+++ libs/execution/tests/unit/limiting_executor.cpp
@@ -4,143 +4,105 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/async.hpp>
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/include/parallel_executors.hpp>
 #include <hpx/modules/testing.hpp>
 #include <hpx/thread_executors/limiting_executor.hpp>
 
-#include <algorithm>
+#include <chrono>
 #include <cstdlib>
-#include <iterator>
-#include <numeric>
+#include <random>
 #include <string>
 #include <vector>
 
-///////////////////////////////////////////////////////////////////////////////
-hpx::thread::id thread_id_test(int passed_through)
-{
-    HPX_TEST_EQ(passed_through, 42);
-    return hpx::this_thread::get_id();
-}
+// this test launches many tasks continuously using a limiting executor
+// some of the tasks will suspend themselves randomly and so new tasks will be
+// spawned until the max tasks 'in flight' limit is reached. Once this happens
+// no new tasks should be created and the number 'in flight' should remain
+// below the limit. We track this using some counters and check that
+// at the end of the test, the max counter never exceeded the limit.
 
-void test2(int passed_through)
-{
-    HPX_TEST_EQ(passed_through, 42);
-}
-
-void test_sync()
-{
-    auto exec1 = hpx::parallel::execution::parallel_executor(
-        hpx::threads::thread_stacksize_small);
-    auto exec2 = hpx::parallel::execution::default_executor(
-        hpx::threads::thread_stacksize_large);
-
-    hpx::threads::executors::limiting_executor<decltype(exec1)> lexec1(
-        exec1, 100, 100);
-    hpx::threads::executors::limiting_executor<decltype(exec2)> lexec2(
-        exec2, 100, 100);
-
-    hpx::async(lexec1, &test2, 42);
-    hpx::async(lexec2, &test2, 42);
-
-    auto id1 =
-        hpx::parallel::execution::sync_execute(exec1, &thread_id_test, 42);
-    auto id2 = hpx::this_thread::get_id();
-    std::cout << "hpx::parallel::execution::sync_execute Ids " << id1 << " : "
-              << id2 << std::endl;
-
-    HPX_TEST(hpx::parallel::execution::sync_execute(
-                 exec1, &thread_id_test, 42) == hpx::this_thread::get_id());
-    HPX_TEST(hpx::parallel::execution::sync_execute(
-                 exec2, &thread_id_test, 42) == hpx::this_thread::get_id());
-
-    id1 = hpx::parallel::execution::sync_execute(lexec1, &thread_id_test, 42);
-    id2 = hpx::this_thread::get_id();
-    std::cout << "hpx::parallel::execution::sync_execute - limiting exec Ids "
-              << id1 << " : " << id2 << std::endl;
-
-    HPX_TEST(hpx::parallel::execution::sync_execute(
-                 lexec1, &thread_id_test, 42) == hpx::this_thread::get_id());
-    HPX_TEST(hpx::parallel::execution::sync_execute(
-                 lexec2, &thread_id_test, 42) == hpx::this_thread::get_id());
-}
-
-void test_async()
-{
-    auto exec1 = hpx::parallel::execution::parallel_executor(
-        hpx::threads::thread_stacksize_small);
-    auto exec2 = hpx::parallel::execution::default_executor(
-        hpx::threads::thread_stacksize_large);
-
-    hpx::threads::executors::limiting_executor<decltype(exec1)> lexec1(
-        exec1, 100, 100);
-    hpx::threads::executors::limiting_executor<decltype(exec2)> lexec2(
-        exec2, 100, 100);
-
-    HPX_TEST(hpx::parallel::execution::async_execute(exec1, &thread_id_test, 42)
-                 .get() != hpx::this_thread::get_id());
-    HPX_TEST(hpx::parallel::execution::async_execute(exec2, &thread_id_test, 42)
-                 .get() != hpx::this_thread::get_id());
-}
+// counters we will use to track tasks in flight
+using atype = std::atomic<std::int64_t>;
+static atype task_1_counter(0);
+static atype task_1_total(0);
+static atype task_1_max(0);
+static const std::int64_t max1 = 110;
 
 ///////////////////////////////////////////////////////////////////////////////
-hpx::thread::id test_f(hpx::future<void> f, int passed_through)
+//  simple task that can yield at random and increments counters
+void test_fn(atype& acnt, atype& atot, atype& amax)
 {
-    HPX_TEST(f.is_ready());    // make sure, future is ready
+    // Increment active task count
+    // set the max tasks running (not 100% atomic, but hopefully good enough)
+    if (++acnt > amax)
+        amax.store(acnt.load());
+    ++atot;
 
-    f.get();    // propagate exceptions
+    // yield some random amount of times to make the test more realistic
+    // this allows other tasks to run and tests if the limiting exec
+    // is doing its job
+    std::default_random_engine eng;
+    std::uniform_int_distribution<std::size_t> idist(10, 50);
+    std::size_t loop = idist(eng);
+    for (std::size_t i = 0; i < loop; ++i)
+    {
+        hpx::this_thread::yield();
+    }
 
-    HPX_TEST_EQ(passed_through, 42);
-    return hpx::this_thread::get_id();
+    // task is completing, decrement active task count
+    --acnt;
 }
 
-void test_then()
+void test_limit()
 {
     auto exec1 = hpx::parallel::execution::parallel_executor(
         hpx::threads::thread_stacksize_small);
-    hpx::threads::executors::limiting_executor<decltype(exec1)> lexec1(
-        exec1, 100, 100);
+    //
+    const bool block_on_exit = true;
+    std::vector<hpx::future<void>> futures;
+    {
+        hpx::parallel::execution::limiting_executor<decltype(exec1)> lexec1(
+            exec1, max1 / 2, max1, block_on_exit);
 
-    hpx::future<void> f = hpx::make_ready_future();
+        // run this loop for N seconds and launch as many tasks as we can
+        // then check that there were never more than N on a given executor
+        bool ok = true;
+        auto start = std::chrono::steady_clock::now();
+        while (ok)
+        {
+            futures.push_back(
+                hpx::async(lexec1, &test_fn, std::ref(task_1_counter),
+                    std::ref(task_1_total), std::ref(task_1_max)));
+            ok = (futures.size() < 50000) &&
+                (std::chrono::steady_clock::now() - start <
+                    std::chrono::milliseconds(500));
+        }
+        std::cout << "Reached end of launch with futures = " << futures.size()
+                  << std::endl;
+    }
+    // the executors should block until all tasks have completed
+    auto not_ready = std::count_if(
+        futures.begin(), futures.end(), [](auto& f) { return !f.is_ready(); });
+    // so all futures should be ready;
+    HPX_TEST_EQ(not_ready, 0);
 
-    HPX_TEST(
-        hpx::parallel::execution::then_execute(lexec1, &test_f, f, 42).get() ==
-        hpx::this_thread::get_id());
-}
-
-void static_check_executor()
-{
-    using namespace hpx::traits;
-    auto exec1 = hpx::parallel::execution::parallel_executor(
-        hpx::threads::thread_stacksize_small);
-    hpx::threads::executors::limiting_executor<decltype(exec1)> lexec1(
-        exec1, 100, 100);
-
-    //    static_assert(has_sync_execute_member<executor>::value,
-    //        "has_sync_execute_member<executor>::value");
-    //    static_assert(has_async_execute_member<executor>::value,
-    //        "has_async_execute_member<executor>::value");
-    //    static_assert(!has_then_execute_member<executor>::value,
-    //        "!has_then_execute_member<executor>::value");
-    //    static_assert(has_bulk_sync_execute_member<executor>::value,
-    //        "has_bulk_sync_execute_member<executor>::value");
-    //    static_assert(has_bulk_async_execute_member<executor>::value,
-    //        "has_bulk_async_execute_member<executor>::value");
-    //    static_assert(!has_bulk_then_execute_member<executor>::value,
-    //        "!has_bulk_then_execute_member<executor>::value");
-    //    static_assert(has_post_member<executor>::value,
-    //        "check has_post_member<executor>::value");
+    // the max counters should not exceed the limit set
+    // note that as the max set is not actually atomic, a race could allow the limit
+    // to be exceeded by the number of threads in the worst case.
+    std::cout << "Exec 1 had max " << task_1_max << " (allowed = " << max1
+              << ") from a total of " << task_1_total << std::endl;
+    HPX_TEST_LTE(task_1_max, max1);
+    // if the test fails by a small number, then this would fix it
+    // HPX_TEST_LTE(task_1_max, max1 + hpx::get_num_worker_threads());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 int hpx_main(int argc, char* argv[])
 {
-    static_check_executor();
-
-    test_sync();
-    test_async();
-    test_then();
+    test_limit();
 
     return hpx::finalize();
 }
@@ -148,7 +110,7 @@ int hpx_main(int argc, char* argv[])
 int main(int argc, char* argv[])
 {
     // By default this test should run on all available cores
-    std::vector<std::string> const cfg = {"hpx.os_threads=all"};
+    std::vector<std::string> const cfg = {"hpx.os_threads=cores"};
 
     // Initialize and run HPX
     HPX_TEST_EQ_MSG(
