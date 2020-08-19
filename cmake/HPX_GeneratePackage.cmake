2bab386d69849824fd860061628f0743791b8eb8
Mikael Simberg <mikael.simberg@iki.fi>
2020-08-19 15:06:25 +0200

    Add alias targets with namespace used for exporting

--- cmake/HPX_GeneratePackage.cmake
+++ cmake/HPX_GeneratePackage.cmake
@@ -41,6 +41,15 @@ export(
   FILE "${CMAKE_CURRENT_BINARY_DIR}/lib/cmake/${HPX_PACKAGE_NAME}/HPXTargets.cmake"
 )
 
+# Add aliases with the namespace for use within HPX
+foreach(export_target ${HPX_EXPORT_TARGETS})
+  add_library(HPX::${export_target} ALIAS ${export_target})
+endforeach()
+
+foreach(export_target ${HPX_EXPORT_INTERNAL_TARGETS})
+  add_library(HPXInternal::${export_target} ALIAS ${export_target})
+endforeach()
+
 # Export HPXTargets in the install directory
 install(
   EXPORT HPXTargets
