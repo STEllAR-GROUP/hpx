3b62f88304bc8b78a796dd77148332c8a7b95f30
aurianer <aurianer@cscs.ch>
2020-09-16 10:56:55 -0500

    Add jenkins config files for rostam

---
+++ .jenkins/lsu/env-hipcc.sh
@@ -0,0 +1,19 @@
+# Copyright (c) 2020 ETH Zurich
+#
+# SPDX-License-Identifier: BSL-1.0
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+module load rocm/3.7.0
+export CXX=hipcc
+export CC=clang
+# Tmp: needed until rocsolver is installed in the rocm module
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rauriane/install/rocsolver/lib
+
+export HPXRUN_RUNWRAPPER=srun
+
+configure_extra_options="-DCMAKE_BUILD_TYPE=${build_type}"
+configure_extra_options+=" -DHPX_WITH_MALLOC=system"
+configure_extra_options+=" -DHPX_WITH_DEPRECATION_WARNINGS=OFF"
+configure_extra_options+=" -DHPX_WITH_COMPILER_WARNINGS=ON"
+configure_extra_options+=" -DHPX_WITH_COMPILER_WARNINGS_AS_ERRORS=ON"
