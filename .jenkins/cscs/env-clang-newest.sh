a75e161c5f287190a426191e25328dead5260a91
Mikael Simberg <mikael.simberg@iki.fi>
2020-09-24 15:21:34 +0200

    Update env-clang-newest.sh

--- .jenkins/cscs/env-clang-newest.sh
+++ .jenkins/cscs/env-clang-newest.sh
@@ -32,7 +32,7 @@ configure_extra_options+=" -DHPX_WITH_CXX${CXX_STD}=ON"
 configure_extra_options+=" -DHPX_WITH_COMPILER_WARNINGS=ON"
 configure_extra_options+=" -DHPX_WITH_COMPILER_WARNINGS_AS_ERRORS=ON"
 configure_extra_options+=" -DHPX_WITH_SPINLOCK_DEADLOCK_DETECTION=ON"
-configure_extra_options+=" -DHPX_UNITY_BUILD=ON"
+configure_extra_options+=" -DHPX_WITH_UNITY_BUILD=ON"
 
 # enable extra counters to verify everything compiles
 configure_extra_options+=" -DHPX_WITH_BACKGROUD_THREAD_COUNTERS=ON"
