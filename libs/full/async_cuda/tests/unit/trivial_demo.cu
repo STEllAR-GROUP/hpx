a2808961b54f9f6b86ab5d0f3c60628c3b6bfbff
aurianer <aurianer@cscs.ch>
2020-10-25 11:30:32 -0500

    Add a copy kernel to bypass temporarily the rostam problem
    printf in kernel hangs on rostam but not on ault

--- libs/full/async_cuda/tests/unit/trivial_demo.cu
+++ libs/full/async_cuda/tests/unit/trivial_demo.cu
@@ -5,6 +5,10 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/preprocessor/seq/for_each.hpp>
+
+#include <hpx/config.hpp>
+
+#include <hpx/assert.hpp>
 #include <hpx/async_cuda/custom_gpu_api.hpp>
 
 #include <iostream>
@@ -16,12 +20,42 @@ __global__ void trivial_kernel(T val)
     printf("hello from gpu with value %f\n", static_cast<double>(val));
 }
 
+// Copy Kernel for hip on rostam (printf in kernel not working)
+template <typename T>
+__global__ void copy_kernel(T* in, T* out)
+{
+    *out = *in;
+}
+
 // here is a wrapper that can call the kernel from C++ outside of the .cu file
 template <typename T>
 void cuda_trivial_kernel(T t, cudaStream_t stream)
 {
+#if !defined(HPX_HAVE_HIP)
     trivial_kernel<<<1, 1, 0, stream>>>(t);
     cudaDeviceSynchronize();
+#else
+    // Silence -Wuninitialized until [[uninitialized]] available
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wuninitialized"
+#endif
+    T *d_in, *d_out;
+    T* out = (T*) malloc(sizeof(T));
+    cudaMalloc((void**) &d_in, sizeof(T));
+    cudaMalloc((void**) &d_out, sizeof(T));
+    cudaMemcpy(d_in, &t, sizeof(T), cudaMemcpyHostToDevice);
+    copy_kernel<<<1, 1, 0, stream>>>(d_in, d_out);
+    cudaDeviceSynchronize();
+    cudaMemcpy(out, d_out, sizeof(T), cudaMemcpyDeviceToHost);
+    HPX_ASSERT(*out == t);
+    cudaFree(d_in);
+    cudaFree(d_out);
+    free(out);
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+#endif
 }
 
 // -------------------------------------------------------------------------
@@ -35,6 +69,7 @@ void cuda_trivial_kernel(T t, cudaStream_t stream)
 
 #define GENERATE_SPECIALIZATIONS(_1, _2, elem)                                 \
     template __global__ void trivial_kernel<elem>(elem);                       \
+    template __global__ void copy_kernel<elem>(elem*, elem*);                  \
     template void cuda_trivial_kernel<elem>(elem, cudaStream_t);               \
     //
 
