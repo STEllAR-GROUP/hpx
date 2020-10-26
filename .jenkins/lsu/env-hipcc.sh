61ec5c4a28fbb8e6e2288519054b0b9a35098065
aurianer <aurianer@cscs.ch>
2020-10-26 09:15:29 -0500

    Adapt CI to rocm-3.8.0 with rocsolver installed

--- .jenkins/lsu/env-hipcc.sh
+++ .jenkins/lsu/env-hipcc.sh
@@ -4,11 +4,9 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-module load rocm/3.7.0
+module load rocm/3.8.0
 export CXX=hipcc
 export CC=clang
-# Tmp: needed until rocsolver is installed in the rocm module
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rauriane/install/rocsolver/lib
 
 export HPXRUN_RUNWRAPPER=srun
 
