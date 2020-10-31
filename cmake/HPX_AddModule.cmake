1cd8460f8e42822c8dce140f3f08b0403a515e62
Agustin K-ballo Berge <k@fusionfenix.com>
2020-10-31 17:40:47 -0300

    Removes generated headers left behind from a previous configuration

--- cmake/HPX_AddModule.cmake
+++ cmake/HPX_AddModule.cmake
@@ -232,6 +232,19 @@ function(add_hpx_module libname modulename)
     set(generated_headers ${generated_headers} ${global_config_file})
   endif()
 
+  # collect zombie generated headers
+  file(GLOB_RECURSE zombie_generated_headers
+       ${CMAKE_CURRENT_BINARY_DIR}/include/*.hpp
+       ${CMAKE_CURRENT_BINARY_DIR}/include_compatibility/*.hpp
+  )
+  list(REMOVE_ITEM zombie_generated_headers ${generated_headers}
+       ${compat_headers}
+  )
+  foreach(zombie_header IN LISTS zombie_generated_headers)
+    hpx_warn("Removing zombie generated header: ${zombie_header}")
+    file(REMOVE ${zombie_header})
+  endforeach()
+
   # list all specified headers
   foreach(header_file ${headers})
     hpx_debug(${header_file})
