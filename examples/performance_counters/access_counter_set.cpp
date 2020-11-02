6546d676a5f16fe759b0714ff1dc7df1c661e286
Mikael Simberg <mikael.simberg@iki.fi>
2020-11-02 16:10:10 +0100

    Attempt to fix memory leak in access_counter_set test

--- examples/performance_counters/access_counter_set.cpp
+++ examples/performance_counters/access_counter_set.cpp
@@ -10,8 +10,8 @@
 #include <hpx/config.hpp>
 #if !defined(HPX_COMPUTE_DEVICE_CODE)
 #include <hpx/hpx_init.hpp>
-#include <hpx/iostream.hpp>
 #include <hpx/include/performance_counters.hpp>
+#include <hpx/iostream.hpp>
 
 #include <hpx/modules/program_options.hpp>
 
@@ -34,39 +34,41 @@ int hpx_main(hpx::program_options::variables_map& vm)
     }
 
     // create the performance counter set from the given pattern (name)
-    using namespace hpx::performance_counters;
-    performance_counter_set set(name);
-
-    // retrieve the counter information for all attached counters
-    std::vector<counter_info> infos = set.get_counter_infos();
-
-    // retrieve the current counter values
-    std::vector<double> values = set.get_values<double>(hpx::launch::sync);
-
-    // print the values for all 'raw' (non-histogram) counters
-    for (std::size_t i = 0, j = 0; i != infos.size(); ++i)
     {
-        if (infos[i].type_ != counter_raw &&
-            infos[i].type_ != counter_monotonically_increasing &&
-            infos[i].type_ != counter_aggregating &&
-            infos[i].type_ != counter_elapsed_time &&
-            infos[i].type_ != counter_average_count &&
-            infos[i].type_ != counter_average_timer)
-        {
-            continue;
-        }
+        using namespace hpx::performance_counters;
+        performance_counter_set set(name);
 
-        if (infos[i].unit_of_measure_.empty())
+        // retrieve the counter information for all attached counters
+        std::vector<counter_info> infos = set.get_counter_infos();
+
+        // retrieve the current counter values
+        std::vector<double> values = set.get_values<double>(hpx::launch::sync);
+
+        // print the values for all 'raw' (non-histogram) counters
+        for (std::size_t i = 0, j = 0; i != infos.size(); ++i)
         {
-            hpx::cout << infos[i].fullname_ << ":" << values[j] << std::endl;
+            if (infos[i].type_ != counter_raw &&
+                infos[i].type_ != counter_monotonically_increasing &&
+                infos[i].type_ != counter_aggregating &&
+                infos[i].type_ != counter_elapsed_time &&
+                infos[i].type_ != counter_average_count &&
+                infos[i].type_ != counter_average_timer)
+            {
+                continue;
+            }
+
+            if (infos[i].unit_of_measure_.empty())
+            {
+                hpx::cout << infos[i].fullname_ << ":" << values[j]
+                          << std::endl;
+            }
+            else
+            {
+                hpx::cout << infos[i].fullname_ << ":" << values[j] << "["
+                          << infos[i].unit_of_measure_ << "]" << std::endl;
+            }
+            ++j;
         }
-        else
-        {
-            hpx::cout
-                << infos[i].fullname_ << ":" << values[j] << "["
-                << infos[i].unit_of_measure_ << "]" << std::endl;
-        }
-        ++j;
     }
 
     return hpx::finalize();
@@ -78,10 +80,9 @@ int main(int argc, char* argv[])
     hpx::program_options::options_description cmdline(
         "usage: access_counter_set [options]");
 
-    cmdline.add_options()
-        ("counter", hpx::program_options::value<std::string>(),
-         "name (pattern) representing the of the performance counter(s) to query")
-        ;
+    cmdline.add_options()("counter", hpx::program_options::value<std::string>(),
+        "name (pattern) representing the of the performance counter(s) to "
+        "query");
 
     hpx::init_params init_args;
     init_args.desc_cmdline = cmdline;
@@ -89,4 +90,5 @@ int main(int argc, char* argv[])
     // Initialize and run HPX.
     return hpx::init(argc, argv, init_args);
 }
+
 #endif
