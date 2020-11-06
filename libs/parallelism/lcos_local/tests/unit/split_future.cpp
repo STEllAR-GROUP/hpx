51887b80ff4ee2d76ee4b8abc9007578e833e336
Mikael Simberg <mikael.simberg@iki.fi>
2020-11-06 17:24:53 +0100

    Remove unnecessary dependencies in some distributed modules

--- libs/parallelism/lcos_local/tests/unit/split_future.cpp
+++ libs/parallelism/lcos_local/tests/unit/split_future.cpp
@@ -4,8 +4,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/hpx_init.hpp>
-#include <hpx/include/lcos.hpp>
+#include <hpx/init.hpp>
+#include <hpx/future.hpp>
+#include <hpx/thread.hpp>
 #include <hpx/modules/testing.hpp>
 
 #include <array>
