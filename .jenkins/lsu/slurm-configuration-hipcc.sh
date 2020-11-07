21013a9097e28bd283414aab0f1f6f902cdbadd8
aurianer <aurianer@cscs.ch>
2020-11-07 03:56:50 -0600

    Limit jenkins hipcc to the reno node

--- .jenkins/lsu/slurm-configuration-hipcc.sh
+++ .jenkins/lsu/slurm-configuration-hipcc.sh
@@ -4,5 +4,6 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-configuration_slurm_partition="fury"
+# The partition k40 contains the reno node with both Nvidia and AMD
+configuration_slurm_partition="k40"
 configuration_slurm_num_nodes="1"
