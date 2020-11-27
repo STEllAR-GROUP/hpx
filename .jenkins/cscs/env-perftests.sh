72d1effe85440cee715143dfea4acbf23cd2ee9c
aurianer <aurianer@cscs.ch>
2020-11-27 17:05:49 +0100

    Make the env script verbose

--- .jenkins/cscs/env-perftests.sh
+++ .jenkins/cscs/env-perftests.sh
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+set -eux
 
 source $SPACK_ROOT/share/spack/setup-env.sh
 
