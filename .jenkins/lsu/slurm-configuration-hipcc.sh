c032a3bd00fda6079d7b80c54e41a400d90da5f7
Mikael Simberg <mikael.simberg@iki.fi>
2021-01-04 12:01:04 +0100

    Use all AMD nodes again on rostam

--- .jenkins/lsu/slurm-configuration-hipcc.sh
+++ .jenkins/lsu/slurm-configuration-hipcc.sh
@@ -4,6 +4,5 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# The partition k40 contains the reno node with both Nvidia and AMD
-configuration_slurm_partition="k40"
+configuration_slurm_partition="fury"
 configuration_slurm_num_nodes="1"
