2df6e293f84b157ac81da0039b50992e06b30580
Mikael Simberg <mikael.simberg@iki.fi>
2021-01-20 12:35:39 +0100

    Make sync_wait work with void return types

--- libs/parallelism/execution/include/hpx/execution/algorithms/sync_wait.hpp
+++ libs/parallelism/execution/include/hpx/execution/algorithms/sync_wait.hpp
@@ -18,22 +18,28 @@
 
 #include <exception>
 #include <tuple>
+#include <type_traits>
 #include <utility>
 
 namespace hpx { namespace execution { namespace experimental {
     template <typename... Variants>
     struct sync_wait_single_result
     {
-        static_assert(sizeof...(Variants) == 0,
+        static_assert(sizeof...(Variants) == 1,
             "sync_wait expects the predecessor sender to have a single variant "
-            "in "
-            "sender_traits<>::value_types");
+            "in sender_traits<>::value_types");
     };
 
     template <typename T>
     struct sync_wait_single_result<hpx::util::pack<hpx::util::pack<T>>>
     {
         using type = T;
+    };
+
+    template <>
+    struct sync_wait_single_result<hpx::util::pack<hpx::util::pack<>>>
+    {
+        using type = void;
     };
 
     namespace detail {
@@ -78,39 +84,114 @@ namespace hpx { namespace execution { namespace experimental {
                 signal_done();
             }
         };
+
+        template <>
+        struct sync_wait_receiver<void>
+        {
+            struct state
+            {
+                hpx::lcos::local::condition_variable cv;
+                hpx::lcos::local::mutex m;
+                bool done = false;
+                bool has_exception = false;
+                std::exception_ptr ep;
+            };
+
+            state& st;
+
+            void signal_done() noexcept
+            {
+                std::unique_lock<hpx::lcos::local::mutex> l(st.m);
+                st.done = true;
+                st.cv.notify_one();
+            }
+
+            void set_error(std::exception_ptr ep) noexcept
+            {
+                st.ep = ep;
+                st.has_exception = true;
+                signal_done();
+            }
+
+            void set_done() noexcept
+            {
+                signal_done();
+            };
+
+            void set_value() noexcept
+            {
+                signal_done();
+            }
+        };
+
+        template <typename S>
+        auto sync_wait_impl(std::true_type, S&& s)
+        {
+            using state_type = typename detail::sync_wait_receiver<void>::state;
+
+            state_type st{};
+            hpx::execution::experimental::start(
+                hpx::execution::experimental::connect(
+                    std::forward<S>(s), detail::sync_wait_receiver<void>{st}));
+
+            {
+                std::unique_lock<hpx::lcos::local::mutex> l(st.m);
+                if (!st.done)
+                {
+                    st.cv.wait(l);
+                }
+            }
+
+            if (st.has_exception)
+            {
+                std::rethrow_exception(st.ep);
+            }
+        }
+
+        template <typename S>
+        auto sync_wait_impl(std::false_type, S&& s)
+        {
+            using value_types =
+                typename hpx::execution::experimental::traits::sender_traits<
+                    S>::template value_types<hpx::util::pack, hpx::util::pack>;
+            using result_type =
+                typename sync_wait_single_result<value_types>::type;
+            using state_type =
+                typename detail::sync_wait_receiver<result_type>::state;
+
+            state_type st{};
+            hpx::execution::experimental::start(
+                hpx::execution::experimental::connect(std::forward<S>(s),
+                    detail::sync_wait_receiver<result_type>{st}));
+
+            {
+                std::unique_lock<hpx::lcos::local::mutex> l(st.m);
+                if (!st.done)
+                {
+                    st.cv.wait(l);
+                }
+            }
+
+            if (st.has_exception)
+            {
+                std::rethrow_exception(st.ep);
+            }
+            else
+            {
+                return std::move(st.result.value());
+            }
+        }
     }    // namespace detail
 
-    // TODO: Should this work with void results?
     template <typename S>
-    auto sync_wait(S&& s)
+    decltype(auto) sync_wait(S&& s)
     {
         using value_types =
             typename hpx::execution::experimental::traits::sender_traits<
                 S>::template value_types<hpx::util::pack, hpx::util::pack>;
         using result_type = typename sync_wait_single_result<value_types>::type;
-        using state_type =
-            typename detail::sync_wait_receiver<result_type>::state;
 
-        state_type st{};
-        hpx::execution::experimental::start(
-            hpx::execution::experimental::connect(std::forward<S>(s),
-                detail::sync_wait_receiver<result_type>{st}));
-
-        {
-            std::unique_lock<hpx::lcos::local::mutex> l(st.m);
-            if (!st.done)
-            {
-                st.cv.wait(l);
-            }
-        }
-
-        if (st.has_exception)
-        {
-            std::rethrow_exception(st.ep);
-        }
-        else
-        {
-            return std::move(st.result.value());
-        }
+        return detail::sync_wait_impl(
+            std::is_void<result_type>{}, std::forward<S>(s));
     }
 }}}    // namespace hpx::execution::experimental
