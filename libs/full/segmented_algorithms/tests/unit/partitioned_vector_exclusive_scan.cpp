ef32643b14072366ff12d766acf7e5d79e37c8ff
Mikael Simberg <mikael.simberg@iki.fi>
2021-02-15 13:09:32 +0100

    Reduce vector sizes in partitioned_vector scan tests in debug mode

--- libs/full/segmented_algorithms/tests/unit/partitioned_vector_exclusive_scan.cpp
+++ libs/full/segmented_algorithms/tests/unit/partitioned_vector_exclusive_scan.cpp
@@ -325,7 +325,11 @@ void exclusive_scan_tests_inplace_with_policy(
 template <typename T>
 void exclusive_scan_tests(std::vector<hpx::id_type>& localities)
 {
+#if defined(HPX_DEBUG)
+    std::size_t const length = 10000;
+#else
     std::size_t const length = 1000000;
+#endif
 
     exclusive_scan_tests_with_policy<T>(length, hpx::container_layout);
     exclusive_scan_tests_with_policy<T>(length, hpx::container_layout(3));
