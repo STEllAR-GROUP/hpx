1a3907c977b81abfa6d3dc1200ea7b0664ab1253
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2021-03-05 13:49:24 -0600

    Fixing build system for ittnotify
    - fixing wrong #include path
    - flyby: make ittnotify functions constexpr and noexcept where possible

--- libs/full/actions_base/include/hpx/actions_base/basic_action.hpp
+++ libs/full/actions_base/include/hpx/actions_base/basic_action.hpp
@@ -46,7 +46,7 @@
 #include <hpx/type_support/pack.hpp>
 #include <hpx/util/get_and_reset_value.hpp>
 #if HPX_HAVE_ITTNOTIFY != 0 && !defined(HPX_HAVE_APEX)
-#include <hpx/concurrency/itt_notify.hpp>
+#include <hpx/modules/itt_notify.hpp>
 #endif
 
 #include <boost/utility/string_ref.hpp>
