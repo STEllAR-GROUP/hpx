bf161191efee847a68f148765d770891c6eb52b7
aurianer <aurianer@cscs.ch>
2021-03-05 14:42:48 +0100

    Add the jenkins testing script

--- .jenkins/cscs/perftests/setup_perftests.sh
+++ .jenkins/cscs/perftests/setup_perftests.sh
@@ -35,12 +35,18 @@ build_dir="/dev/shm/hpx/build"
 mkdir -p "${build_dir}"
 cp -r "${orig_src_dir}" "${src_dir}"
 
-envfile=.jenkins/cscs/env-${configuration_name}.sh
+# Tmp! Will be handled by the python script later
+envfile=${src_dir}/.jenkins/cscs/env-${configuration_name}.sh
 
+# Copy the perftest utility in the build dir
+mkdir -p ${build_dir}/tools/perftests_ci
+cp -r ${src_dir}/tools/perftests_ci/* ${build_dir}/tools/perftests_ci
+
+pushd ${build_dir} > /dev/null
 # FIXME: we can probably do this step as the other tests and use gridtools
 # script only for running and plotting
 # build binaries for performance tests
-${src_dir}/tools/perftests_ci/driver.py -v -l $logfile build -b release \
+./tools/perftests_ci/driver.py -v -l $logfile build -b release \
     -o build --source-dir ${src_dir} --build-dir ${build_dir} -e $envfile \
     -t tests.performance.local.future_overhead_report \
     || { echo 'Build failed'; exit 1; }
@@ -53,20 +59,25 @@ ${src_dir}/tools/perftests_ci/driver.py -v -l $logfile build -b release \
   result=$resultdir/local-priority-fifo.json
 
   # run performance tests
-  ${build_dir}/tools/perftests_ci/driver.py -v -l $logfile perftest run -o $result \
+  ./tools/perftests_ci/driver.py -v -l $logfile perftest run \
+      --scheduling-policy local-priority --run_output $result \
       --extra-opts ' --test-all --repetitions=15' \
       || { echo 'Running failed'; exit 1; }
+  # We add a space before --test-all because of the following issue
+  # https://bugs.python.org/issue9334
 
   # create directory for reports
   mkdir reports
   # find references for same configuration
   reference=${src_dir}/tools/perftests_ci/perftest/references/daint_default/local-priority-fifo.json
   # plot comparison of current result with references
-  ${build_dir}/tools/perftest_ci/driver.py -v -l $logfile perftest plot compare -i $reference $result -o reports-comparison-$configuration_name || { echo 'Plotting failed'; exit 1; }
+  ./tools/perftest_ci/driver.py -v -l $logfile perftest plot compare -i $reference $result -o reports-comparison-$configuration_name || { echo 'Plotting failed'; exit 1; }
 #done
 
 # Copy the testing directory for saving as an artifact
 cp -r ${build_dir}/Testing ${orig_src_dir}/${configuration_name}-Testing
+
+popd
 
 ## Things went wrong by default
 #ctest_exit_code=$?
