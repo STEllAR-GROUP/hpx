55dcf359f2c66aeb750c6c5061c724f26cd10a2d
Mikael Simberg <mikael.simberg@iki.fi>
2021-03-15 12:41:19 +0100

    Format transform.hpp

--- libs/parallelism/algorithms/include/hpx/parallel/algorithms/transform.hpp
+++ libs/parallelism/algorithms/include/hpx/parallel/algorithms/transform.hpp
@@ -760,12 +760,12 @@ namespace hpx { namespace traits {
     };
 
     template <typename ExPolicy, typename F, typename Proj1, typename Proj2>
-    struct get_function_address<
-        parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1, Proj2>>
+    struct get_function_address<parallel::v1::detail::
+            transform_binary_iteration<ExPolicy, F, Proj1, Proj2>>
     {
         static std::size_t call(
-            parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1, Proj2> const&
-                f) noexcept
+            parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1,
+                Proj2> const& f) noexcept
         {
             return get_function_address<typename std::decay<F>::type>::call(
                 f.f_);
@@ -773,12 +773,12 @@ namespace hpx { namespace traits {
     };
 
     template <typename ExPolicy, typename F, typename Proj1, typename Proj2>
-    struct get_function_annotation<
-        parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1, Proj2>>
+    struct get_function_annotation<parallel::v1::detail::
+            transform_binary_iteration<ExPolicy, F, Proj1, Proj2>>
     {
         static char const* call(
-            parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1, Proj2> const&
-                f) noexcept
+            parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1,
+                Proj2> const& f) noexcept
         {
             return get_function_annotation<typename std::decay<F>::type>::call(
                 f.f_);
@@ -800,12 +800,12 @@ namespace hpx { namespace traits {
     };
 
     template <typename ExPolicy, typename F, typename Proj1, typename Proj2>
-    struct get_function_annotation_itt<
-        parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1, Proj2>>
+    struct get_function_annotation_itt<parallel::v1::detail::
+            transform_binary_iteration<ExPolicy, F, Proj1, Proj2>>
     {
         static util::itt::string_handle call(
-            parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1, Proj2> const&
-                f) noexcept
+            parallel::v1::detail::transform_binary_iteration<ExPolicy, F, Proj1,
+                Proj2> const& f) noexcept
         {
             return get_function_annotation_itt<
                 typename std::decay<F>::type>::call(f.f_);
