90b0cb673dc1616c4d9f9a013d52075880a69541
Mikael Simberg <mikael.simberg@iki.fi>
2021-04-07 10:31:32 +0200

    Run hello_world_component executable after test builds
    Adds a minimal safety net to check that client projects can not only
    have a different build type for building, but that running executables
    also works.

--- examples/hello_world_component/hello_world_client.cpp
+++ examples/hello_world_component/hello_world_client.cpp
@@ -4,13 +4,20 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// Please keep the duplicate hpx/config.hpp include further down. It makes the
+// code within the [hello_world_client... block self-contained for the
+// documentation.
+#include <hpx/config.hpp>
+#if defined(HPX_WITH_DISTRIBUTED_RUNTIME)
+
 //[hello_world_client_getting_started
 #include <hpx/config.hpp>
-#if !defined(HPX_COMPUTE_DEVICE_CODE)
-#include "hello_world_component.hpp"
-#include <hpx/hpx_main.hpp>
+#if defined(HPX_COMPUTE_HOST_CODE)
+#include <hpx/wrap_main.hpp>
 
-int main(int argc, char* argv[])
+#include "hello_world_component.hpp"
+
+int main()
 {
     {
         // Create a single instance of the component on this locality.
@@ -25,3 +32,17 @@ int main(int argc, char* argv[])
 }
 #endif
 //]
+#else
+#include <hpx/thread.hpp>
+#include <hpx/wrap_main.hpp>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World from HPX-thread with id "
+              << hpx::this_thread::get_id() << std::endl;
+
+    return 0;
+}
+#endif
