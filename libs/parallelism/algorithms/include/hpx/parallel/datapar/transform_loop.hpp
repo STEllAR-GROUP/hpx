4f329f0a8488cfcc158819eaf6b4e97c7f0c63bd
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-04-08 13:55:24 +0530

    Fix naming conventions in datapar

--- libs/parallelism/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
+++ libs/parallelism/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
@@ -55,8 +55,8 @@ namespace hpx { namespace parallel { namespace util {
                 static constexpr std::size_t size =
                     traits::vector_pack_size<V>::value;
 
-                for (std::int64_t lenV = std::int64_t(count - (size + 1));
-                     lenV > 0; lenV -= size, len -= size)
+                for (std::int64_t len_v = std::int64_t(count - (size + 1));
+                     len_v > 0; len_v -= size, len -= size)
                 {
                     datapar_transform_loop_step::callv(f, first, dest);
                 }
@@ -182,8 +182,8 @@ namespace hpx { namespace parallel { namespace util {
                 static constexpr std::size_t size =
                     traits::vector_pack_size<V>::value;
 
-                for (std::int64_t lenV = std::int64_t(count - (size + 1));
-                     lenV > 0; lenV -= size, len -= size)
+                for (std::int64_t len_v = std::int64_t(count - (size + 1));
+                     len_v > 0; len_v -= size, len -= size)
                 {
                     datapar_transform_loop_step::callv(f, first1, first2, dest);
                 }
