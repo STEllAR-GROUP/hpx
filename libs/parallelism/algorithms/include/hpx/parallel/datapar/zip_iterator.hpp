18d2b770c657534624530db1d1e3343563aa204e
srinivasyadav18 <43375352+srinivasyadav18@users.noreply.github.com>
2021-04-10 21:03:30 +0530

    Fix is_data_aligned variadic template sequencer
    Co-authored-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>

--- libs/parallelism/algorithms/include/hpx/parallel/datapar/zip_iterator.hpp
+++ libs/parallelism/algorithms/include/hpx/parallel/datapar/zip_iterator.hpp
@@ -39,7 +39,7 @@ namespace hpx { namespace parallel { namespace util { namespace detail {
         {
             auto const& t = it.get_iterator_tuple();
             bool const sequencer[] = {
-                false, is_data_aligned(hpx::get<Is>(t))...};
+                true, is_data_aligned(hpx::get<Is>(t))...};
             return std::all_of(&sequencer[1], &sequencer[sizeof...(Is) + 1],
                 [](bool val) { return val; });
         }
