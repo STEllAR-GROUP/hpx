1f0a37a664065f5b01e03bd9c50f01a51536cb79
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-04-22 17:50:18 +0530

    Adapt transform_loop_n_ind

--- libs/parallelism/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
+++ libs/parallelism/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
@@ -96,6 +96,73 @@ namespace hpx { namespace parallel { namespace util {
     namespace detail {
         ///////////////////////////////////////////////////////////////////////
         template <typename Iterator>
+        struct datapar_transform_loop_n_ind
+        {
+            typedef typename std::decay<Iterator>::type iterator_type;
+
+            typedef typename traits::vector_pack_type<
+                typename std::iterator_traits<iterator_type>::value_type>::type
+                V;
+
+            template <typename InIter, typename OutIter, typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                iterators_datapar_compatible<InIter, OutIter>::value &&
+                    iterator_datapar_compatible<InIter>::value &&
+                    iterator_datapar_compatible<OutIter>::value,
+                std::pair<InIter, OutIter>>::type
+            call(InIter first, std::size_t count, OutIter dest, F&& f)
+            {
+                std::size_t len = count;
+
+                for (/* */; !is_data_aligned(first) && len != 0; --len)
+                {
+                    datapar_transform_loop_step_ind::call1(f, first, dest);
+                }
+
+                static constexpr std::size_t size =
+                    traits::vector_pack_size<V>::value;
+
+                for (std::int64_t len_v = std::int64_t(len - (size + 1));
+                     len_v > 0; len_v -= size, len -= size)
+                {
+                    datapar_transform_loop_step_ind::callv(f, first, dest);
+                }
+
+                for (/* */; len != 0; --len)
+                {
+                    datapar_transform_loop_step_ind::call1(f, first, dest);
+                }
+
+                return std::make_pair(std::move(first), std::move(dest));
+            }
+
+            template <typename InIter, typename OutIter, typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                !iterators_datapar_compatible<InIter, OutIter>::value ||
+                    !iterator_datapar_compatible<InIter>::value ||
+                    !iterator_datapar_compatible<OutIter>::value,
+                std::pair<InIter, OutIter>>::type
+            call(InIter first, std::size_t count, OutIter dest, F&& f)
+            {
+                return util::transform_loop_n_ind<hpx::execution::sequenced_policy>(
+                    first, count, dest, std::forward<F>(f));
+            }
+        };
+    }    // namespace detail
+
+    template <typename ExPolicy, typename Iter, typename OutIter, typename F>
+    HPX_HOST_DEVICE HPX_FORCEINLINE constexpr typename std::enable_if<
+        hpx::is_vectorpack_execution_policy<ExPolicy>::value,
+        std::pair<Iter, OutIter>>::type
+    transform_loop_n_ind(Iter it, std::size_t count, OutIter dest, F&& f)
+    {
+        return detail::datapar_transform_loop_n_ind<Iter>::call(
+            it, count, dest, std::forward<F>(f));
+    }
+
+    namespace detail {
+        ///////////////////////////////////////////////////////////////////////
+        template <typename Iterator>
         struct datapar_transform_loop
         {
             typedef typename std::decay<Iterator>::type iterator_type;
