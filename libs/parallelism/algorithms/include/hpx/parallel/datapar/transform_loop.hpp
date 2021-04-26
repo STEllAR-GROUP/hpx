1a20bfcc80be5cc1207d4b87916c3c70f2581de0
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-04-26 23:11:51 +0530

    Adapt datapar transform_loop_ind

--- libs/parallelism/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
+++ libs/parallelism/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
@@ -310,6 +310,75 @@ namespace hpx { namespace parallel { namespace util {
 
     namespace detail {
         ///////////////////////////////////////////////////////////////////////
+        template <typename Iterator>
+        struct datapar_transform_loop_ind
+        {
+            typedef typename std::decay<Iterator>::type iterator_type;
+            typedef typename std::iterator_traits<iterator_type>::value_type
+                value_type;
+
+            typedef typename traits::vector_pack_type<value_type>::type V;
+            typedef typename traits::vector_pack_type<value_type, 1>::type V1;
+
+            template <typename InIter, typename OutIter, typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                iterators_datapar_compatible<InIter, OutIter>::value &&
+                    iterator_datapar_compatible<InIter>::value &&
+                    iterator_datapar_compatible<OutIter>::value,
+                std::pair<InIter, OutIter>>::type
+            call(InIter first, InIter last, OutIter dest, F&& f)
+            {
+                return util::transform_loop_n_ind<
+                    hpx::execution::dataseq_policy>(first,
+                    std::distance(first, last), dest, std::forward<F>(f));
+            }
+
+            template <typename InIter, typename OutIter, typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                !iterators_datapar_compatible<InIter, OutIter>::value ||
+                    !iterator_datapar_compatible<InIter>::value ||
+                    !iterator_datapar_compatible<OutIter>::value,
+                std::pair<InIter, OutIter>>::type
+            call(InIter first, InIter last, OutIter dest, F&& f)
+            {
+                return util::transform_loop(
+                    hpx::execution::seq, first, last, dest, std::forward<F>(f));
+            }
+        };
+    }    // namespace detail
+
+    template <typename ExPolicy, typename IterB, typename IterE,
+        typename OutIter, typename F>
+    HPX_HOST_DEVICE
+        HPX_FORCEINLINE constexpr util::in_out_result<IterB, OutIter>
+        tag_invoke(hpx::parallel::util::transform_loop_ind_t,
+            hpx::execution::dataseq_policy, IterB it, IterE end, OutIter dest,
+            F&& f)
+    {
+        auto ret = detail::datapar_transform_loop_ind<IterB>::call(
+            it, end, dest, std::forward<F>(f));
+
+        return util::in_out_result<IterB, OutIter>{
+            std::move(ret.first), std::move(ret.second)};
+    }
+
+    template <typename ExPolicy, typename IterB, typename IterE,
+        typename OutIter, typename F>
+    HPX_HOST_DEVICE
+        HPX_FORCEINLINE constexpr util::in_out_result<IterB, OutIter>
+        tag_invoke(hpx::parallel::util::transform_loop_ind_t,
+            hpx::execution::dataseq_task_policy, IterB it, IterE end,
+            OutIter dest, F&& f)
+    {
+        auto ret = detail::datapar_transform_loop_ind<IterB>::call(
+            it, end, dest, std::forward<F>(f));
+
+        return util::in_out_result<IterB, OutIter>{
+            std::move(ret.first), std::move(ret.second)};
+    }
+
+    namespace detail {
+        ///////////////////////////////////////////////////////////////////////
         template <typename Iter1, typename Iter2>
         struct datapar_transform_binary_loop_n
         {
