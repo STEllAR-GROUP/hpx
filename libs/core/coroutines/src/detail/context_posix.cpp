4cf1389312779ccd4eeaff95a181e20d00fc650c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2021-05-26 09:06:25 -0500

    Making sure default_stack_size is defined whenever declared

--- libs/core/coroutines/src/detail/context_posix.cpp
+++ libs/core/coroutines/src/detail/context_posix.cpp
@@ -5,12 +5,22 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/config.hpp>
+#include <hpx/coroutines/detail/context_impl.hpp>
 
-#if !defined(HPX_HAVE_GENERIC_CONTEXT_COROUTINES) &&                           \
-    !(defined(__linux) || defined(linux) || defined(__linux__)) &&             \
-    (defined(_POSIX_VERSION) || defined(__bgq__) || defined(__powerpc__) ||    \
-        defined(__s390x__))
-#include <hpx/coroutines/detail/context_posix.hpp>
+// The preprocessor conditions below are kept in sync with those used in
+// context_impl.hpp
+
+#if defined(HPX_HAVE_GENERIC_CONTEXT_COROUTINES)
+
+// left empty on purpose
+
+#elif (defined(__linux) || defined(linux) || defined(__linux__)) &&            \
+    !defined(__bgq__) && !defined(__powerpc__) && !defined(__s390x__)
+
+// left empty on purpose
+
+#elif defined(_POSIX_VERSION) || defined(__bgq__) || defined(__powerpc__) ||   \
+    defined(__s390x__)
 
 #include <cstddef>
 
@@ -21,4 +31,12 @@ namespace hpx { namespace threads { namespace coroutines { namespace detail {
             SIGSTKSZ;
 }}}}}    // namespace hpx::threads::coroutines::detail::posix
 
-#endif
+#elif defined(HPX_HAVE_FIBER_BASED_COROUTINES)
+
+// left empty on purpose
+
+#else
+
+#error No default_context_impl available for this system
+
+#endif    // HPX_HAVE_GENERIC_CONTEXT_COROUTINES
