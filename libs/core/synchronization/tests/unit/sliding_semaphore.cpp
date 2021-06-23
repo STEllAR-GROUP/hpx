817550f436fe684d139a57e4afada5c35abc6163
Mikael Simberg <mikael.simberg@iki.fi>
2021-06-23 09:22:42 +0200

    Add missing header to sliding_semaphore test

--- libs/core/synchronization/tests/unit/sliding_semaphore.cpp
+++ libs/core/synchronization/tests/unit/sliding_semaphore.cpp
@@ -14,6 +14,7 @@
 #include <cstddef>
 #include <functional>
 #include <string>
+#include <utility>
 #include <vector>
 
 ///////////////////////////////////////////////////////////////////////////////
