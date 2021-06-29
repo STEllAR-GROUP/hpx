14ea626dac1fd68f2ee75879c48020097ce29006
Auriane R <aurianer@cscs.ch>
2021-06-29 16:57:42 +0200

    Remove unecessary export in definition, already in declaration

--- libs/core/debugging/src/print.cpp
+++ libs/core/debugging/src/print.cpp
@@ -88,7 +88,7 @@ namespace hpx { namespace debug {
     namespace detail {
 
         template <typename Int>
-        HPX_CORE_EXPORT void print_hex(std::ostream& os, Int v, int N)
+        void print_hex(std::ostream& os, Int v, int N)
         {
             os << std::right << "0x" << std::setfill('0') << std::setw(N)
                << std::noshowbase << std::hex << v;
@@ -100,7 +100,7 @@ namespace hpx { namespace debug {
             std::ostream&, unsigned long, int);
 
         template <typename Int>
-        HPX_CORE_EXPORT void print_ptr(std::ostream& os, Int v, int N)
+        void print_ptr(std::ostream& os, Int v, int N)
         {
             os << std::right << std::setw(N) << std::noshowbase << std::hex
                << v;
