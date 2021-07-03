ac3af6697b7179b76c70d25a5a62d2682f69d57c
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-07-03 10:36:26 +0530

    Fix datapar copy unit tests headers
    Adds pragma once
    Includes datapar headers

--- libs/parallelism/algorithms/tests/unit/algorithms/copyn_tests.hpp
+++ libs/parallelism/algorithms/tests/unit/algorithms/copyn_tests.hpp
@@ -5,6 +5,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#pragma once
+
 #include <hpx/local/init.hpp>
 #include <hpx/modules/testing.hpp>
 #include <hpx/parallel/algorithms/copy.hpp>
