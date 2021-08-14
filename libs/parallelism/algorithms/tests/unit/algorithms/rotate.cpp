69cc9083a0043df0322d4e740b64f13b8a71c356
chuanqiu <chuanqiuhe@gmail.com>
2021-08-13 19:21:52 -0500

    add uniform format header

--- libs/parallelism/algorithms/tests/unit/algorithms/rotate.cpp
+++ libs/parallelism/algorithms/tests/unit/algorithms/rotate.cpp
@@ -5,6 +5,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/execution/traits/is_execution_policy.hpp>
 #include <hpx/local/init.hpp>
 #include <hpx/modules/testing.hpp>
 #include <hpx/parallel/algorithms/rotate.hpp>
