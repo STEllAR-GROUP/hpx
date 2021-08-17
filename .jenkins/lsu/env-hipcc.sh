e2215d686590e469a293809118144d9115f0ea5b
Mikael Simberg <mikael.simberg@iki.fi>
2021-08-17 14:12:59 +0200

    Update Rostam HIP configuration

--- .jenkins/lsu/env-hipcc.sh
+++ .jenkins/lsu/env-hipcc.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 ETH Zurich
+# Copyright (c) 2020-2021 ETH Zurich
 #
 # SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -6,7 +6,6 @@
 
 module load rocm/4.0.1
 export CXX=hipcc
-export CC=clang
 
 export HPXRUN_RUNWRAPPER=srun
 export CXX_STD="17"
