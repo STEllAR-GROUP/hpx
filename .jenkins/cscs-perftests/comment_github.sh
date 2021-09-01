4559a0db173c467353ff6702036bcfaa41307c47
Mikael Simberg <mikael.simberg@iki.fi>
2021-09-01 09:41:16 +0200

    Add missing license to comment_github.sh script

--- .jenkins/cscs-perftests/comment_github.sh
+++ .jenkins/cscs-perftests/comment_github.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -l
+
+# Copyright (c) 2021 ETH Zurich
+#
+# SPDX-License-Identifier: BSL-1.0
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 set -eux
 
 pushd perftests-reports/reports-comparison
