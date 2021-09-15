6176934e03d38a40b58d36e3059eb3d25449c047
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2021-09-15 07:31:03 -0500

    Fixing wrong module dependencies

--- .jenkins/lsu/env-common.sh
+++ .jenkins/lsu/env-common.sh
@@ -5,3 +5,4 @@
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 configure_extra_options+=" -DCMAKE_BUILD_TYPE=${build_type}"
+configure_extra_options+=" -DHPX_WITH_CHECK_MODULE_DEPENDENCIES=ON"
