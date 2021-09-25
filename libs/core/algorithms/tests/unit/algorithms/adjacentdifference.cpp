daa178496d09b2b16007a438b21242352c3dba04
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-09-25 17:59:40 +0530

    Split existing adjacent_difference unit tests
    Splits the existing adjacent_difference into header and source files.
    Merges execption and bad alloc tests into one header and source.

--- libs/core/algorithms/tests/unit/algorithms/adjacentdifference.cpp
+++ libs/core/algorithms/tests/unit/algorithms/adjacentdifference.cpp
@@ -1,3 +1,4 @@
+//  Copyright (c) 2021 Srinivas Yadav
 //  Copyright (c) 2015 Daniel Bourgeois
 //
 //  SPDX-License-Identifier: BSL-1.0
@@ -5,59 +6,12 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/local/init.hpp>
-#include <hpx/modules/testing.hpp>
-#include <hpx/parallel/algorithms/adjacent_difference.hpp>
 
-#include <cstddef>
 #include <iostream>
-#include <iterator>
-#include <numeric>
 #include <string>
 #include <vector>
 
-#include "test_utils.hpp"
-
-////////////////////////////////////////////////////////////////////////////
-template <typename ExPolicy>
-void test_adjacent_difference(ExPolicy policy)
-{
-    static_assert(hpx::is_execution_policy<ExPolicy>::value,
-        "hpx::is_execution_policy<ExPolicy>::value");
-
-    std::vector<std::size_t> c = test::random_iota(10007);
-    std::vector<std::size_t> d(10007);
-    std::vector<std::size_t> d_ans(10007);
-
-    auto it = hpx::parallel::adjacent_difference(
-        policy, std::begin(c), std::end(c), std::begin(d));
-    std::adjacent_difference(std::begin(c), std::end(c), std::begin(d_ans));
-
-    HPX_TEST(std::equal(std::begin(d), std::end(d), std::begin(d_ans),
-        [](std::size_t lhs, std::size_t rhs) -> bool { return lhs == rhs; }));
-
-    HPX_TEST(std::end(d) == it);
-}
-
-template <typename ExPolicy>
-void test_adjacent_difference_async(ExPolicy p)
-{
-    static_assert(hpx::is_execution_policy<ExPolicy>::value,
-        "hpx::is_execution_policy<ExPolicy>::value");
-
-    std::vector<std::size_t> c = test::random_iota(10007);
-    std::vector<std::size_t> d(10007);
-    std::vector<std::size_t> d_ans(10007);
-
-    auto f_it = hpx::parallel::adjacent_difference(
-        p, std::begin(c), std::end(c), std::begin(d));
-    std::adjacent_difference(std::begin(c), std::end(c), std::begin(d_ans));
-
-    f_it.wait();
-    HPX_TEST(std::equal(std::begin(d), std::end(d), std::begin(d_ans),
-        [](std::size_t lhs, std::size_t rhs) -> bool { return lhs == rhs; }));
-
-    HPX_TEST(std::end(d) == f_it.get());
-}
+#include "adjacentdifference_tests.hpp"
 
 void adjacent_difference_test()
 {
@@ -68,6 +22,48 @@ void adjacent_difference_test()
 
     test_adjacent_difference_async(seq(task));
     test_adjacent_difference_async(par(task));
+}
+
+template <typename IteratorTag>
+void test_adjacent_difference_exception()
+{
+    using namespace hpx::execution;
+
+    // If the execution policy object is of type vector_execution_policy,
+    // std::terminate shall be called. therefore we do not test exceptions
+    // with a vector execution policy
+    test_adjacent_difference_exception(seq, IteratorTag());
+    test_adjacent_difference_exception(par, IteratorTag());
+
+    test_adjacent_difference_exception_async(seq(task), IteratorTag());
+    test_adjacent_difference_exception_async(par(task), IteratorTag());
+}
+
+void adjacent_difference_exception_test()
+{
+    test_adjacent_difference_exception<std::random_access_iterator_tag>();
+    test_adjacent_difference_exception<std::forward_iterator_tag>();
+}
+
+template <typename IteratorTag>
+void test_adjacent_difference_bad_alloc()
+{
+    using namespace hpx::execution;
+
+    // If the execution policy object is of type vector_execution_policy,
+    // std::terminate shall be called. therefore we do not test exceptions
+    // with a vector execution policy
+    test_adjacent_difference_bad_alloc(seq, IteratorTag());
+    test_adjacent_difference_bad_alloc(par, IteratorTag());
+
+    test_adjacent_difference_bad_alloc_async(seq(task), IteratorTag());
+    test_adjacent_difference_bad_alloc_async(par(task), IteratorTag());
+}
+
+void adjacent_difference_bad_alloc_test()
+{
+    test_adjacent_difference_bad_alloc<std::random_access_iterator_tag>();
+    test_adjacent_difference_bad_alloc<std::forward_iterator_tag>();
 }
 
 int hpx_main(hpx::program_options::variables_map& vm)
