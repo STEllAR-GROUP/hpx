c2731ef1b507c7ad4d9fbd4b06aa7c39d69d7d69
Auriane R <aurianer@cscs.ch>
2021-10-28 16:26:13 +0200

    Factorize perftests scripts between jenkins and local

--- .jenkins/cscs-perftests/batch.sh
+++ .jenkins/cscs-perftests/batch.sh
@@ -49,48 +49,7 @@ configure_build_errors=0
 test_errors=0
 plot_errors=0
 
-hpx_targets=("future_overhead_report_test" "stream_report_test")
-hpx_test_options=("--hpx:queuing=local-priority --hpx:threads=4 --test-all \
-    --repetitions=100 --futures=500000" \
-    "--vector_size=1048576 --hpx:threads=4 --iterations=5000 \
-    --warmup_iterations=500")
-
-# Build binaries for performance tests
-${perftests_dir}/driver.py -v -l $logfile build -b release -o build \
-    --source-dir ${src_dir} --build-dir ${build_dir} -e $envfile \
-    -t "${hpx_targets[@]}" \
-    || { echo 'Build failed'; configure_build_errors=1; exit 1; }
-
-index=0
-result_files=""
-
-# Run and compare for each targets specified
-for executable in "${hpx_targets[@]}"
-do
-  test_opts=${hpx_test_options[$index]}
-  result=${build_dir}/reports/${executable}.json
-  reference=${perftests_dir}/perftest/references/daint_default/${executable}.json
-  result_files+=(${result})
-  references_files+=(${reference})
-  logfile_tmp=log_perftests_${executable}.tmp
-
-  run_command=("./bin/${executable} ${test_opts}")
-
-  # TODO: make schedulers and other options vary
-
-  # Run performance tests
-  ${perftests_dir}/driver.py -v -l $logfile_tmp perftest run --local True \
-      --run_output $result --targets-and-opts "${run_command[@]}" \
-      || { echo 'Running failed'; test_errors=1; exit 1; }
-
-  index=$((index+1))
-done
-
-# Plot comparison of current result with references
-${perftests_dir}/driver.py -v -l $logfile perftest plot compare --references \
-    ${references_files[@]} --results ${result_files[@]} \
-    -o ${build_dir}/reports/reports-comparison \
-    || { echo 'Plotting failed: performance drop or unknown'; plot_errors=1; exit 1; }
+source ${src_dir}/.jenkins/cscs-perftests/launch_perftests.sh
 
 # Dummy ctest to upload the html report of the perftest
 set +e
