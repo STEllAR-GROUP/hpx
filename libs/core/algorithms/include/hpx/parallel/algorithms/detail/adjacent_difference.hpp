198b1de76f656aea3c22b19b3afa9887e9a306f4
Karame <karame@jeff.lan>
2021-10-29 01:02:07 -0500

    remove unnecessary deps
    update
    
    remove unnecessary deps
    
    renamed adjacent file

--- libs/core/algorithms/include/hpx/parallel/algorithms/detail/adjacent_difference.hpp
+++ libs/core/algorithms/include/hpx/parallel/algorithms/detail/adjacent_difference.hpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2021 Srinivas Yadav
+//  Copyright (c) 2021 Karame M.shokooh
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -27,13 +28,24 @@ namespace hpx { namespace parallel { inline namespace v1 { namespace detail {
             sequential_adjacent_difference_t<ExPolicy>>
     {
     private:
-        template <typename InIter, typename OutIter, typename Op>
+        template <typename InIter, typename Sent, typename OutIter, typename Op>
         friend inline OutIter tag_fallback_invoke(
-            sequential_adjacent_difference_t<ExPolicy>, InIter first,
-            InIter last, OutIter dest, Op&& op)
+            sequential_adjacent_difference_t<ExPolicy>, InIter first, Sent last,
+            OutIter dest, Op&& op)
         {
-            return std::adjacent_difference(
-                first, last, dest, std::forward<Op>(op));
+            if (first == last)
+                return dest;
+
+            using value_t = typename std::iterator_traits<InIter>::value_type;
+            value_t acc = *first;
+            *dest = acc;
+            while (++first != last)
+            {
+                value_t val = *first;
+                *++dest = op(val, std::move(acc));    // std::move since C++20
+                acc = std::move(val);
+            }
+            return ++dest;
         }
     };
 
@@ -43,9 +55,10 @@ namespace hpx { namespace parallel { inline namespace v1 { namespace detail {
         sequential_adjacent_difference =
             sequential_adjacent_difference_t<ExPolicy>{};
 #else
-    template <typename ExPolicy, typename InIter, typename OutIter, typename Op>
+    template <typename ExPolicy, typename InIter, typename Sent,
+        typename OutIter, typename Op>
     HPX_HOST_DEVICE HPX_FORCEINLINE OutIter sequential_adjacent_difference(
-        InIter first, InIter last, OutIter dest, Op&& op)
+        InIter first, Sent last, OutIter dest, Op&& op)
     {
         return sequential_adjacent_difference_t<ExPolicy>{}(
             first, last, dest, std::forward<Op>(op));
