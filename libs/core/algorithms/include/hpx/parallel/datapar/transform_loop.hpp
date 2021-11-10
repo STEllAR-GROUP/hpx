fa192b5045d892724584801058b053bb7b19b8ee
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-11-10 11:47:37 +0530

    Simplify datapar iterator helpers and added new facilites
    Simplify datapar iterator helpers.
    Adds datapar_loop_step_tok for tokenized loops for datapar.
    Adds transform binary loop for datapar.

--- libs/core/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
+++ libs/core/algorithms/include/hpx/parallel/datapar/transform_loop.hpp
@@ -515,5 +515,216 @@ namespace hpx { namespace parallel { namespace util {
         return detail::datapar_transform_binary_loop<InIter1, InIter2>::call(
             first1, last1, first2, last2, dest, std::forward<F>(f));
     }
+
+    namespace detail {
+        ///////////////////////////////////////////////////////////////////////
+        template <typename Iter1, typename Iter2>
+        struct datapar_transform_binary_loop_ind_n
+        {
+            typedef typename std::decay<Iter1>::type iterator1_type;
+            typedef typename std::iterator_traits<iterator1_type>::value_type
+                value_type;
+
+            typedef typename traits::vector_pack_type<value_type>::type V;
+
+            template <typename InIter1, typename InIter2, typename OutIter,
+                typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                iterators_datapar_compatible<InIter1, OutIter>::value &&
+                    iterators_datapar_compatible<InIter2, OutIter>::value &&
+                    iterator_datapar_compatible<InIter1>::value &&
+                    iterator_datapar_compatible<InIter2>::value &&
+                    iterator_datapar_compatible<OutIter>::value,
+                hpx::tuple<InIter1, InIter2, OutIter>>::type
+            call(InIter1 first1, std::size_t count, InIter2 first2,
+                OutIter dest, F&& f)
+            {
+                std::size_t len = count;
+
+                for (/* */;
+                     !(is_data_aligned(first1) && is_data_aligned(first2) &&
+                         is_data_aligned(dest)) &&
+                     len != 0;
+                     --len)
+                {
+                    datapar_transform_loop_step_ind::call1(
+                        f, first1, first2, dest);
+                }
+
+                static constexpr std::size_t size =
+                    traits::vector_pack_size<V>::value;
+
+                for (std::int64_t len_v = std::int64_t(len - (size + 1));
+                     len_v > 0; len_v -= size, len -= size)
+                {
+                    datapar_transform_loop_step_ind::callv(
+                        f, first1, first2, dest);
+                }
+
+                for (/* */; len != 0; --len)
+                {
+                    datapar_transform_loop_step_ind::call1(
+                        f, first1, first2, dest);
+                }
+
+                return hpx::make_tuple(
+                    std::move(first1), std::move(first2), std::move(dest));
+            }
+
+            template <typename InIter1, typename InIter2, typename OutIter,
+                typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                !iterators_datapar_compatible<InIter1, OutIter>::value ||
+                    !iterators_datapar_compatible<InIter2, OutIter>::value ||
+                    !iterator_datapar_compatible<InIter1>::value ||
+                    !iterator_datapar_compatible<InIter2>::value ||
+                    !iterator_datapar_compatible<OutIter>::value,
+                hpx::tuple<InIter1, InIter2, OutIter>>::type
+            call(InIter1 first1, std::size_t count, InIter2 first2,
+                OutIter dest, F&& f)
+            {
+                return util::transform_binary_loop_ind_n<
+                    hpx::execution::sequenced_policy>(
+                    first1, count, first2, dest, std::forward<F>(f));
+            }
+        };
+    }    // namespace detail
+
+    template <typename ExPolicy, typename InIter1, typename InIter2,
+        typename OutIter, typename F>
+    HPX_HOST_DEVICE HPX_FORCEINLINE typename std::enable_if<
+        hpx::is_vectorpack_execution_policy<ExPolicy>::value,
+        hpx::tuple<InIter1, InIter2, OutIter>>::type
+    tag_invoke(hpx::parallel::util::transform_binary_loop_ind_n_t<ExPolicy>,
+        InIter1 first1, std::size_t count, InIter2 first2, OutIter dest, F&& f)
+    {
+        return detail::datapar_transform_binary_loop_ind_n<InIter1,
+            InIter2>::call(first1, count, first2, dest, std::forward<F>(f));
+    }
+
+    namespace detail {
+        ///////////////////////////////////////////////////////////////////////
+        template <typename Iter1, typename Iter2>
+        struct datapar_transform_binary_loop_ind
+        {
+            typedef typename std::decay<Iter1>::type iterator1_type;
+            typedef typename std::decay<Iter2>::type iterator2_type;
+
+            typedef typename std::iterator_traits<iterator1_type>::value_type
+                value1_type;
+            typedef typename std::iterator_traits<iterator2_type>::value_type
+                value2_type;
+
+            typedef typename traits::vector_pack_type<value1_type, 1>::type V11;
+            typedef typename traits::vector_pack_type<value2_type, 1>::type V12;
+
+            typedef typename traits::vector_pack_type<value1_type>::type V1;
+            typedef typename traits::vector_pack_type<value2_type>::type V2;
+
+            template <typename InIter1, typename InIter2, typename OutIter,
+                typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                iterators_datapar_compatible<InIter1, OutIter>::value &&
+                    iterators_datapar_compatible<InIter2, OutIter>::value &&
+                    iterator_datapar_compatible<InIter1>::value &&
+                    iterator_datapar_compatible<InIter2>::value &&
+                    iterator_datapar_compatible<OutIter>::value,
+                util::in_in_out_result<InIter1, InIter2, OutIter>>::type
+            call(InIter1 first1, InIter1 last1, InIter2 first2, OutIter dest,
+                F&& f)
+            {
+                auto ret = util::transform_binary_loop_ind_n<
+                    hpx::execution::par_simd_policy>(first1,
+                    std::distance(first1, last1), first2, dest,
+                    std::forward<F>(f));
+
+                return util::in_in_out_result<InIter1, InIter2, OutIter>{
+                    hpx::get<0>(ret), hpx::get<1>(ret), hpx::get<2>(ret)};
+            }
+
+            template <typename InIter1, typename InIter2, typename OutIter,
+                typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                !iterators_datapar_compatible<InIter1, OutIter>::value ||
+                    !iterators_datapar_compatible<InIter2, OutIter>::value ||
+                    !iterator_datapar_compatible<InIter1>::value ||
+                    !iterator_datapar_compatible<InIter2>::value ||
+                    !iterator_datapar_compatible<OutIter>::value,
+                util::in_in_out_result<InIter1, InIter2, OutIter>>::type
+            call(InIter1 first1, InIter1 last1, InIter2 first2, OutIter dest,
+                F&& f)
+            {
+                return util::transform_binary_loop_ind<
+                    hpx::execution::sequenced_policy>(
+                    first1, last1, first2, dest, std::forward<F>(f));
+            }
+
+            template <typename InIter1, typename InIter2, typename OutIter,
+                typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                iterators_datapar_compatible<InIter1, OutIter>::value &&
+                    iterators_datapar_compatible<InIter2, OutIter>::value &&
+                    iterator_datapar_compatible<InIter1>::value &&
+                    iterator_datapar_compatible<InIter2>::value &&
+                    iterator_datapar_compatible<OutIter>::value,
+                util::in_in_out_result<InIter1, InIter2, OutIter>>::type
+            call(InIter1 first1, InIter1 last1, InIter2 first2, InIter2 last2,
+                OutIter dest, F&& f)
+            {
+                std::size_t count = (std::min)(
+                    std::distance(first1, last1), std::distance(first2, last2));
+
+                auto ret = util::transform_binary_loop_ind_n<
+                    hpx::execution::par_simd_policy>(
+                    first1, count, first2, dest, std::forward<F>(f));
+
+                return util::in_in_out_result<InIter1, InIter2, OutIter>{
+                    hpx::get<0>(ret), hpx::get<1>(ret), hpx::get<2>(ret)};
+            }
+
+            template <typename InIter1, typename InIter2, typename OutIter,
+                typename F>
+            HPX_HOST_DEVICE HPX_FORCEINLINE static typename std::enable_if<
+                !iterators_datapar_compatible<InIter1, OutIter>::value ||
+                    !iterators_datapar_compatible<InIter2, OutIter>::value ||
+                    !iterator_datapar_compatible<InIter1>::value ||
+                    !iterator_datapar_compatible<InIter2>::value ||
+                    !iterator_datapar_compatible<OutIter>::value,
+                util::in_in_out_result<InIter1, InIter2, OutIter>>::type
+            call(InIter1 first1, InIter1 last1, InIter2 first2, InIter2 last2,
+                OutIter dest, F&& f)
+            {
+                return util::transform_binary_loop_ind<
+                    hpx::execution::sequenced_policy>(
+                    first1, last1, first2, last2, dest, std::forward<F>(f));
+            }
+        };
+    }    // namespace detail
+
+    template <typename ExPolicy, typename InIter1, typename InIter2,
+        typename OutIter, typename F>
+    HPX_HOST_DEVICE HPX_FORCEINLINE typename std::enable_if<
+        hpx::is_vectorpack_execution_policy<ExPolicy>::value,
+        util::in_in_out_result<InIter1, InIter2, OutIter>>::type
+    tag_invoke(hpx::parallel::util::transform_binary_loop_ind_t<ExPolicy>,
+        InIter1 first1, InIter1 last1, InIter2 first2, OutIter dest, F&& f)
+    {
+        return detail::datapar_transform_binary_loop_ind<InIter1,
+            InIter2>::call(first1, last1, first2, dest, std::forward<F>(f));
+    }
+
+    template <typename ExPolicy, typename InIter1, typename InIter2,
+        typename OutIter, typename F>
+    HPX_HOST_DEVICE HPX_FORCEINLINE typename std::enable_if<
+        hpx::is_vectorpack_execution_policy<ExPolicy>::value,
+        util::in_in_out_result<InIter1, InIter2, OutIter>>::type
+    tag_invoke(hpx::parallel::util::transform_binary_loop_ind_t<ExPolicy>,
+        InIter1 first1, InIter1 last1, InIter2 first2, InIter2 last2,
+        OutIter dest, F&& f)
+    {
+        return detail::datapar_transform_binary_loop_ind<InIter1,
+            InIter2>::call(first1, last1, first2, last2, dest,
+            std::forward<F>(f));
+    }
 }}}    // namespace hpx::parallel::util
 #endif
