c15e8523fd5edace650002e6d3fa4744bb7ab363
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-11-10 11:41:12 +0530

    Adapt new vector_pack traits
    Adapt all_of, any_of, none_of and find_first vector_pack traits

---
+++ libs/core/execution/include/hpx/execution/traits/detail/simd/vector_pack_all_any_none.hpp
@@ -0,0 +1,42 @@
+//  Copyright (c) 2021 Srinivas Yadav
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#if defined(HPX_HAVE_CXX20_EXPERIMENTAL_SIMD)
+#include <cstddef>
+
+#include <experimental/simd>
+
+namespace hpx { namespace parallel { namespace traits {
+    ///////////////////////////////////////////////////////////////////////
+    template <typename T, typename Abi>
+    HPX_HOST_DEVICE HPX_FORCEINLINE std::size_t all_of(
+        std::experimental::simd_mask<T, Abi> const& msk)
+    {
+        return std::experimental::all_of(msk);
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    template <typename T, typename Abi>
+    HPX_HOST_DEVICE HPX_FORCEINLINE std::size_t any_of(
+        std::experimental::simd_mask<T, Abi> const& msk)
+    {
+        return std::experimental::any_of(msk);
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    template <typename T, typename Abi>
+    HPX_HOST_DEVICE HPX_FORCEINLINE std::size_t none_of(
+        std::experimental::simd_mask<T, Abi> const& msk)
+    {
+        return std::experimental::none_of(msk);
+    }
+}}}    // namespace hpx::parallel::traits
+
+#endif
