82af9ecfd9ccba038b54104cdec1ab1a6f41b8ec
Mikael Simberg <mikael.simberg@iki.fi>
2021-12-03 15:08:10 +0100

    Change default max CPU count to be dynamic

--- tools/perftests_ci/driver.py
+++ tools/perftests_ci/driver.py
@@ -55,7 +55,6 @@ def build(build_type, environment, target, source_dir, build_dir, install_dir,
     env.set_cmake_arg('PYUTILS_HPX_WITH_FETCH_ASIO', 'ON')
     env.set_cmake_arg('PYUTILS_HPX_WITH_MALLOC', 'jemalloc')
     env.set_cmake_arg('PYUTILS_HPX_WITH_TESTS_BENCHMARKS', 'ON')
-    env.set_cmake_arg('PYUTILS_HPX_WITH_MAX_CPU_COUNT', '72')
     env.set_cmake_arg('PYUTILS_CMAKE_BUILD_TYPE', 'Release')
     env.set_cmake_arg('-GNinja', '')
     # For possibly more stable/focused results
