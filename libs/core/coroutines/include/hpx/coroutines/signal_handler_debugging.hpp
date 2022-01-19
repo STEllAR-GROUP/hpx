870ffb5d78e610f9cc68b8e5eb887d76bcf1f788
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2022-01-19 14:38:05 -0600

    Enable the option to attach a debugger on a segmentation fold (linux)
    - flyby: rename HPX_LOCAL_ option to HPX_

---
+++ libs/core/coroutines/include/hpx/coroutines/signal_handler_debugging.hpp
@@ -0,0 +1,20 @@
+//  Copyright (c) 2022 Hartmut Kaiser
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#include <cstddef>
+
+namespace hpx::threads::coroutines {
+
+    HPX_CORE_EXPORT extern bool attach_debugger_on_sigv;
+    HPX_CORE_EXPORT extern bool diagnostics_on_terminate;
+    HPX_CORE_EXPORT extern int exception_verbosity;
+    HPX_CORE_EXPORT extern std::size_t trace_depth;
+}    // namespace hpx::threads::coroutines
---
+++ libs/core/coroutines/src/signal_handler_debugging.cpp
@@ -0,0 +1,23 @@
+//  Copyright (c) 2022 Hartmut Kaiser
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/config.hpp>
+#include <hpx/coroutines/signal_handler_debugging.hpp>
+
+#include <cstddef>
+
+namespace hpx::threads::coroutines {
+
+    bool attach_debugger_on_sigv = false;
+    bool diagnostics_on_terminate = true;
+    int exception_verbosity = 2;
+#if defined(HPX_HAVE_STACKTRACES) && defined(HPX_HAVE_THREAD_BACKTRACE_DEPTH)
+    std::size_t trace_depth = HPX_HAVE_THREAD_BACKTRACE_DEPTH;
+#else
+    std::size_t trace_depth = 0;
+#endif
+}    // namespace hpx::threads::coroutines
