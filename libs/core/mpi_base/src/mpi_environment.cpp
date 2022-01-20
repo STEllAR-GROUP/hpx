662002543954ea254cefa27be426d27fbf5791df
Patrick <patrickdiehl@lsu.edu>
2022-01-20 11:18:19 -0600

    Add copyright

--- libs/core/mpi_base/src/mpi_environment.cpp
+++ libs/core/mpi_base/src/mpi_environment.cpp
@@ -1,5 +1,6 @@
 //  Copyright (c) 2013-2015 Thomas Heller
 //  Copyright (c)      2020 Google
+//  Copyright (c)      2022 Patrick Diehl
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
