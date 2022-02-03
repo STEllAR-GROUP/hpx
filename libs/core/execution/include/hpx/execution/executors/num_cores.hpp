b796bff8a8e2d5bdf2e3df8997557354a28e452c
chuanqiu <chuanqiuhe@gmail.com>
2022-02-03 14:38:50 -0600

    add num_cores facility

---
+++ libs/core/execution/include/hpx/execution/executors/num_cores.hpp
@@ -0,0 +1,55 @@
+//  Copyright (c) 2022 Hartmut Kaiser
+//  Copyright (c) 2022 Chuanqiu He
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file parallel/executors/static_chunk_size.hpp
+
+#pragma once
+
+#include <hpx/execution/executors/execution_parameters_fwd.hpp>
+#include <hpx/execution_base/traits/is_executor_parameters.hpp>
+#include <hpx/executors/execution_policy.hpp>
+#include <cstddef>
+#include <type_traits>
+
+namespace hpx { namespace execution {
+    ///Control number of cores in executors which need a functionality
+    ///for setting the number of cores to be used by an algorithm directly
+    ///
+    struct num_cores
+    {
+        /// Construct a \a num_cores executor parameters object
+        ///
+        /// \note make sure the minimal number of cores is 1
+        ///
+        constexpr explicit num_cores(std::size_t cores = 1) noexcept
+          : num_cores_(cores == 0 ? 1 : cores)
+        {
+        }
+
+        /// \cond NOINTERNAL
+        // discover the number of cores to use for parallelization
+        template <typename Executor>
+        constexpr std::size_t processing_units_count(Executor&&) const noexcept
+        {
+            return num_cores_;
+        }
+        /// \endcond
+
+        /// \cond NOINTERNAL
+        std::size_t num_cores_;
+        /// \endcond
+    };
+}}    // namespace hpx::execution
+
+namespace hpx { namespace parallel { namespace execution {
+    /// \cond NOINTERNAL
+    template <>
+    struct is_executor_parameters<hpx::execution::num_cores> : std::true_type
+    {
+    };
+    /// \endcond
+}}}    // namespace hpx::parallel::execution
