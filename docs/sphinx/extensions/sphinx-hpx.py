9c7c4393263cf450af3f3a21e60442835843fff5
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2022-02-05 10:59:59 -0600

    Adding a sphinx role allowing to link to a file directly in github

--- docs/sphinx/extensions/sphinx-hpx.py
+++ docs/sphinx/extensions/sphinx-hpx.py
@@ -8,6 +8,7 @@ from docutils import nodes
 
 def setup(app):
     app.add_role('hpx-issue', autolink('https://github.com/STEllAR-GROUP/hpx/issues/%s', "Issue #"))
+    app.add_role('hpx-header', autolink_file('http://github.com/STEllAR-GROUP/hpx/blob/%s/%s/%s'))
     app.add_role('hpx-pr', autolink('https://github.com/STEllAR-GROUP/hpx/pull/%s', "PR #"))
     app.add_role('cppreference-header', autolink('http://en.cppreference.com/w/cpp/header/%s'))
     app.add_role('cppreference-algorithm', autolink('http://en.cppreference.com/w/cpp/algorithm/%s'))
@@ -18,5 +19,16 @@ def autolink(pattern, prefix=''):
     def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
         url = pattern % (text,)
         node = nodes.reference(rawtext, prefix + text, refuri=url, **options)
+        return [node], []
+    return role
+
+# The text in the rst file should be:
+# :hpx-header:`base_path,file_name`
+def autolink_file(pattern):
+    def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+        test_parts = text.split(',')
+        commit = inliner.document.settings.env.app.config.html_context['fullcommit']
+        url = pattern % (commit, test_parts[0], test_parts[1])
+        node = nodes.reference(rawtext, test_parts[1], refuri=url, **options)
         return [node], []
     return role
