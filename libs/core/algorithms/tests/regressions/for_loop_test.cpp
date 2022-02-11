715a185ebb023c807163ad47d884cb031d455130
Deepak Suresh <deepusuresh1212@gmail.com>
2022-02-11 23:14:49 +0530

    Update for_loop_test.cpp

--- libs/core/algorithms/tests/regressions/for_loop_test.cpp
+++ libs/core/algorithms/tests/regressions/for_loop_test.cpp
@@ -4,7 +4,6 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 
-
 #include <hpx/local/algorithm.hpp>
 #include <hpx/local/execution.hpp>
 #include <hpx/local/init.hpp>
