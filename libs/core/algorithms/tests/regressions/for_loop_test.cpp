5fd7f8d354d094aa35460de630e060f363501a30
Deepak Suresh <deepusuresh1212@gmail.com>
2022-02-12 12:28:21 +0530

    Update for_loop_test.cpp
    Formatted the file

--- libs/core/algorithms/tests/regressions/for_loop_test.cpp
+++ libs/core/algorithms/tests/regressions/for_loop_test.cpp
@@ -3,7 +3,6 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-
 #include <hpx/local/algorithm.hpp>
 #include <hpx/local/execution.hpp>
 #include <hpx/local/init.hpp>
@@ -12,20 +11,19 @@
 #include "hpx/hpx.hpp"
 #include "hpx/hpx_init.hpp"
 
-int hpx_main(int argc, char *argv[])
+int hpx_main(int argc, char* argv[])
 {
     int start = 7;
     int end = 3;
 
-    hpx::for_loop(hpx::execution::seq, start, end, [&](int){
-        std::cout << "loop running \n";
-    });    
+    hpx::for_loop(hpx::execution::seq, start, end,
+        [&](int) { std::cout << "loop running \n"; });
 
     return hpx::finalize();
 }
 
 int main(int argc, char* argv[])
-{   
+{
     hpx::init_params init_args;
 
     return hpx::init(argc, argv, init_args);
