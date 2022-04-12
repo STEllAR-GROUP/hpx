627d3bae5586c97db0e42d8b757959de4f88e2e5
STE||AR Group <contact@stellar-group.org>
2022-04-11 19:01:10 -0500

    bla

---
+++ examples/1d_stencil/1d_stencil_senders_receivers.cpp
@@ -0,0 +1,8 @@
+//  Copyright (c) 2021 Giannis Gonidelis
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// This example demonstrates the implementational capabilities
+// of senders receivers on a stencil_1d example.
