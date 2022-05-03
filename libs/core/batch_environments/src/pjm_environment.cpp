4ea24f4386a6beff849e61cc628044a5608304a7
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2022-05-03 15:36:27 -0500

    Adding debug output and PJM_NODE_CORE (and PJM_TOTAL_CORE as a fallback)

--- libs/core/batch_environments/src/pjm_environment.cpp
+++ libs/core/batch_environments/src/pjm_environment.cpp
@@ -20,9 +20,13 @@ namespace hpx::util::batch_environments {
     // PJM_MPI_PROC: number of overall localities
     // PJM_NODE: number of physical nodes
     // PJM_PROC_BY_NODE: number of localities per node
+    // PJM_NODE_CORE: number of cores per node
+    // PJM_TOTAL_CORE: total number of cores (PJM_NODE * PJM_NODE_CORE)
+    //
     // Fugaku:
     //      FLIB_AFFINITY_ON_PROCESS: list of indices for allocated
     //                                cores
+    //
     // if launched by mpiexec:
     //      PMIX_RANK: current rank number
     pjm_environment::pjm_environment(
@@ -42,22 +46,34 @@ namespace hpx::util::batch_environments {
             if (have_mpi)
             {
                 // Initialize our node number, if available
-                char* node_num = std::getenv("PMIX_RANK");
-                if (node_num != nullptr)
+                char* var = std::getenv("PMIX_RANK");
+                if (var != nullptr)
                 {
-                    node_num_ = from_string<std::size_t>(node_num);
+                    node_num_ = from_string<std::size_t>(var);
                 }
+            }
+            else if (num_localities_ > 1)
+            {
+                valid_ = false;
+            }
 
-                // Get the number of threads, if available
-                char* affinities = std::getenv("FLIB_AFFINITY_ON_PROCESS");
-                if (affinities != nullptr)
-                {
-                    boost::char_separator<char> sep(",");
-                    boost::tokenizer<boost::char_separator<char>> tok(
-                        std::string(affinities), sep);
-                    num_threads_ = static_cast<std::size_t>(
-                        std::distance(std::begin(tok), std::end(tok)));
-                }
+            // Get the number of threads, if available
+            char* var = std::getenv("PJM_NODE_CORE");
+            if (var != nullptr)
+            {
+                num_threads_ = from_string<std::size_t>(var);
+            }
+            else if ((var = std::getenv("PJM_TOTAL_CORE")) != nullptr)
+            {
+                num_threads_ = from_string<std::size_t>(var) / num_localities_;
+            }
+            else if ((var = std::getenv("FLIB_AFFINITY_ON_PROCESS")) != nullptr)
+            {
+                boost::char_separator<char> sep(",");
+                boost::tokenizer<boost::char_separator<char>> tok(
+                    std::string(var), sep);
+                num_threads_ = static_cast<std::size_t>(
+                    std::distance(std::begin(tok), std::end(tok)));
             }
         }
     }
