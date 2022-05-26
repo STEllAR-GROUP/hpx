224ddee920aea2d9fa17717323eeb8457281d08f
John Sorial <johnzakaria98@gmail.com>
2022-05-26 03:49:34 +0200

    Reformatted to make the IDE happy

--- examples/quickstart/custom_serialization.cpp
+++ examples/quickstart/custom_serialization.cpp
@@ -6,67 +6,78 @@
 
 // This example is meant for inclusion in the documentation.
 
-#include <hpx/include/actions.hpp>
-#include <hpx/serialization.hpp>
 #include <hpx/format.hpp>
-#include <hpx/hpx_main.hpp>
-#include <iostream>
 #include <hpx/future.hpp>
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/actions.hpp>
 #include <hpx/include/runtime.hpp>
+#include <hpx/serialization.hpp>
+#include <iostream>
 
 //[PointMemberSerialization
-struct PointMemberSerialization {
+struct PointMemberSerialization
+{
     int x{0};
     int y{0};
 
     // Required when defining private fields
     friend class hpx::serialization::access;    // Provide member access to HPX
 
-    template<typename Archive>
-    void serialize(Archive &ar, const unsigned int) {
-        ar & x & y;
+    template <typename Archive>
+    void serialize(Archive& ar, const unsigned int)
+    {
+        ar& x& y;
     }
 };
 //]
 
 //[RectangleMemberSerialization
-struct RectangleMemberSerialization {
+struct RectangleMemberSerialization
+{
     PointMemberSerialization top_left;
     PointMemberSerialization lower_right;
 
-    template<typename Archive>
-    void serialize(Archive &ar, const unsigned int) {
-        ar & top_left & lower_right;
+    template <typename Archive>
+    void serialize(Archive& ar, const unsigned int)
+    {
+        ar& top_left& lower_right;
     }
 };
 //]
 
 //[RectangleFREE
-struct RectangleFREE {
+struct RectangleFREE
+{
     PointMemberSerialization top_left;
     PointMemberSerialization lower_right;
 };
 
-template<typename Archive>
-void serialize(Archive &ar, RectangleFREE &pt, const unsigned int) {
-    ar & pt.lower_right & pt.top_left;
+template <typename Archive>
+void serialize(Archive& ar, RectangleFREE& pt, const unsigned int)
+{
+    ar& pt.lower_right& pt.top_left;
 }
 //]
 
 //[PointClass
-class PointClass {
+class PointClass
+{
 public:
     PointClass(int x, int y)
-      : x(x), y(y) {
+      : x(x)
+      , y(y)
+    {
     }
 
     PointClass() = default;
 
-    [[nodiscard]] int getX() const noexcept {
+    [[nodiscard]] int getX() const noexcept
+    {
         return x;
     }
 
-    [[nodiscard]] int getY() const noexcept {
+    [[nodiscard]] int getY() const noexcept
+    {
         return y;
     }
 
@@ -75,15 +86,17 @@ private:
     int y;
 };
 
-template<typename Archive>
-void load(Archive &ar, PointClass &pt, const unsigned int) {
+template <typename Archive>
+void load(Archive& ar, PointClass& pt, const unsigned int)
+{
     int x, y;
     ar >> x >> y;
     pt = PointClass(x, y);
 }
 
-template<typename Archive>
-void save(Archive &ar, PointClass const &pt, const unsigned int) {
+template <typename Archive>
+void save(Archive& ar, PointClass const& pt, const unsigned int)
+{
     ar << pt.getX() << pt.getY();
 }
 // This tells HPX that you have spilt your serialize function into
@@ -92,17 +105,20 @@ HPX_SERIALIZATION_SPLIT_FREE(PointClass)
 //]
 
 //[SendRectangle
-void send_rectangle_struct(RectangleFREE rectangle) {
-    hpx::util::format_to(std::cout, "Rectangle(Point(x={1},y={2}),Point(x={3},y={4}))\n", rectangle.top_left.x, rectangle.top_left.y,
-        rectangle.lower_right.x, rectangle.lower_right.y);
+void send_rectangle_struct(RectangleFREE rectangle)
+{
+    hpx::util::format_to(std::cout,
+        "Rectangle(Point(x={1},y={2}),Point(x={3},y={4}))\n",
+        rectangle.top_left.x, rectangle.top_left.y, rectangle.lower_right.x,
+        rectangle.lower_right.y);
 }
 
 HPX_PLAIN_ACTION(send_rectangle_struct);
 
-int main() {
+int main()
+{
     send_rectangle_struct_action rectangle_action;
-    auto rectangle = RectangleFREE{{0,0},{0,5}};
+    auto rectangle = RectangleFREE{{0, 0}, {0, 5}};
     hpx::async(rectangle_action, hpx::find_here(), rectangle).wait();
-
 }
 //]
