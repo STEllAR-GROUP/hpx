bfc8c75f290f648afc8fdf8e967d866427230b40
kadimitra <kadimitra@ece.auth.gr>
2022-07-11 13:33:44 +0200

    Fix doc includes

--- libs/core/algorithms/include/hpx/parallel/container_algorithms/includes.hpp
+++ libs/core/algorithms/include/hpx/parallel/container_algorithms/includes.hpp
@@ -103,12 +103,91 @@ namespace hpx { namespace ranges {
     ///           sorted range [first2, last2) is found within the sorted range
     ///           [first1, last1). Also returns true if [first2, last2) is empty.
     ///
-    template <typename ExPolicy, typename Iter1, typename Sent1, typename Iter2,
-        typename Sent2, typename Pred = detail::less,
-        typename Proj1 = util::projection_identity,
-        typename Proj2 = util::projection_identity>
-    typename util::detail::algorithm_result<ExPolicy, bool>::type>::type
+    template <typename ExPolicy, typename Iter1, typename Sent1,
+        typename Iter2, typename Sent2,
+        typename Pred = hpx::parallel::v1::detail::less,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    typename hpx::parallel::util::detail::algorithm_result<ExPolicy, bool>::type
     includes(ExPolicy&& policy, Iter1 first1, Sent1 last1, Iter2 first2,
+        Sent2 last2, Pred&& op = Pred(), Proj1&& proj1 = Proj1(),
+        Proj2&& proj2 = Proj2());
+
+    /// Returns true if every element from the sorted range [first2, last2) is
+    /// found within the sorted range [first1, last1). Also returns true if
+    /// [first2, last2) is empty. The version expects both ranges to be sorted
+    /// with the user supplied binary predicate \a f.
+    ///
+    /// \note   At most 2*(N1+N2-1) comparisons, where
+    ///         N1 = std::distance(first1, last1) and
+    ///         N2 = std::distance(first2, last2).
+    ///
+    /// \tparam Iter1       The type of the source iterators used (deduced)
+    ///                     representing the first sequence.
+    ///                     This iterator type must meet the requirements of an
+    ///                     forward iterator.
+    /// \tparam Sent1       The type of the end source iterators used (deduced).
+    ///                     This iterator type must meet the requirements of an
+    ///                     sentinel for Iter1.
+    /// \tparam Iter2       The type of the source iterators used (deduced)
+    ///                     representing the second sequence.
+    ///                     This iterator type must meet the requirements of an
+    ///                     forward iterator.
+    /// \tparam Sent2       The type of the end source iterators used (deduced)
+    ///                     representing the second sequence.
+    ///                     This iterator type must meet the requirements of an
+    ///                     sentinel for Iter2.
+    /// \tparam Pred        The type of an optional function/function object to use.
+    ///                     Unlike its sequential form, the parallel
+    ///                     overload of \a includes requires \a Pred to meet the
+    ///                     requirements of \a CopyConstructible. This defaults
+    ///                     to std::less<>
+    /// \tparam Proj1       The type of an optional projection function applied
+    ///                     to the first sequence. This
+    ///                     defaults to \a util::projection_identity
+    /// \tparam Proj2       The type of an optional projection function applied
+    ///                     to the second sequence. This
+    ///                     defaults to \a util::projection_identity
+    ///
+    /// \param first1       Refers to the beginning of the sequence of elements
+    ///                     of the first range the algorithm will be applied to.
+    /// \param last1        Refers to the end of the sequence of elements of
+    ///                     the first range the algorithm will be applied to.
+    /// \param first2       Refers to the beginning of the sequence of elements
+    ///                     of the second range the algorithm will be applied to.
+    /// \param last2        Refers to the end of the sequence of elements of
+    ///                     the second range the algorithm will be applied to.
+    /// \param op           The binary predicate which returns true if the
+    ///                     elements should be treated as includes. The signature
+    ///                     of the predicate function should be equivalent to
+    ///                     the following:
+    ///                     \code
+    ///                     bool pred(const Type1 &a, const Type2 &b);
+    ///                     \endcode \n
+    ///                     The signature does not need to have const &, but
+    ///                     the function must not modify the objects passed to
+    ///                     it. The types \a Type1 and \a Type2 must be such
+    ///                     that objects of types \a FwdIter1 and \a FwdIter2 can
+    ///                     be dereferenced and then implicitly converted to
+    ///                     \a Type1 and \a Type2 respectively
+    /// \param proj1        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     first sequence as a projection operation before the
+    ///                     actual predicate \a op is invoked.
+    /// \param proj2        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     second sequence as a projection operation before the
+    ///                     actual predicate \a op is invoked.
+    ///
+    /// \returns  The \a includes algorithm returns true every element from the
+    ///           sorted range [first2, last2) is found within the sorted range
+    ///           [first1, last1). Also returns true if [first2, last2) is empty.
+    ///
+    template <typename Iter1, typename Sent1, typename Iter2, typename Sent2,
+            typename Pred = hpx::parallel::v1::detail::less,
+            typename Proj1 = hpx::parallel::util::projection_identity,
+            typename Proj2 = hpx::parallel::util::projection_identity>
+    bool includes(Iter1 first1, Sent1 last1, Iter2 first2,
         Sent2 last2, Pred&& op = Pred(), Proj1&& proj1 = Proj1(),
         Proj2&& proj2 = Proj2());
 
@@ -191,11 +270,75 @@ namespace hpx { namespace ranges {
     ///           [first1, last1). Also returns true if [first2, last2) is empty.
     ///
     template <typename ExPolicy, typename Rng1, typename Rng2,
-        typename Pred = detail::less,
-        typename Proj1 = util::projection_identity,
-        typename Proj2 = util::projection_identity>
-    typename util::detail::algorithm_result<ExPolicy, bool>::type>::type
+        typename Pred = hpx::parallel::v1::detail::less,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    typename hpx::parallel::util::detail::algorithm_result<ExPolicy, bool>::type
     includes(ExPolicy&& policy, Rng1&& rng1, Rng2&& rng2, Pred&& op = Pred(),
+        Proj1&& proj1 = Proj1(), Proj2&& proj2 = Proj2());
+
+    /// Returns true if every element from the sorted range [first2, last2) is
+    /// found within the sorted range [first1, last1). Also returns true if
+    /// [first2, last2) is empty. The version expects both ranges to be sorted
+    /// with the user supplied binary predicate \a f.
+    ///
+    /// \note   At most 2*(N1+N2-1) comparisons, where
+    ///         N1 = std::distance(first1, last1) and
+    ///         N2 = std::distance(first2, last2).
+    ///
+    /// \tparam Rng1        The type of the source range used (deduced).
+    ///                     The iterators extracted from this range type must
+    ///                     meet the requirements of an input iterator.
+    /// \tparam Rng2        The type of the source range used (deduced).
+    ///                     The iterators extracted from this range type must
+    ///                     meet the requirements of an input iterator.
+    /// \tparam Pred        The type of an optional function/function object to use.
+    ///                     Unlike its sequential form, the parallel
+    ///                     overload of \a includes requires \a Pred to meet the
+    ///                     requirements of \a CopyConstructible. This defaults
+    ///                     to std::less<>
+    /// \tparam Proj1       The type of an optional projection function applied
+    ///                     to the first sequence. This
+    ///                     defaults to \a util::projection_identity
+    /// \tparam Proj2       The type of an optional projection function applied
+    ///                     to the second sequence. This
+    ///                     defaults to \a util::projection_identity
+    ///
+    /// \param rng1         Refers to the first sequence of elements the algorithm
+    ///                     will be applied to.
+    /// \param rng2         Refers to the second sequence of elements the algorithm
+    ///                     will be applied to.
+    /// \param op           The binary predicate which returns true if the
+    ///                     elements should be treated as includes. The signature
+    ///                     of the predicate function should be equivalent to
+    ///                     the following:
+    ///                     \code
+    ///                     bool pred(const Type1 &a, const Type2 &b);
+    ///                     \endcode \n
+    ///                     The signature does not need to have const &, but
+    ///                     the function must not modify the objects passed to
+    ///                     it. The types \a Type1 and \a Type2 must be such
+    ///                     that objects of types \a FwdIter1 and \a FwdIter2 can
+    ///                     be dereferenced and then implicitly converted to
+    ///                     \a Type1 and \a Type2 respectively
+    /// \param proj1        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     first sequence as a projection operation before the
+    ///                     actual predicate \a op is invoked.
+    /// \param proj2        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     second sequence as a projection operation before the
+    ///                     actual predicate \a op is invoked.
+    ///
+    /// \returns  The \a includes algorithm returns true every element from the
+    ///           sorted range [first2, last2) is found within the sorted range
+    ///           [first1, last1). Also returns true if [first2, last2) is empty.
+    ///
+    template <typename Rng1, typename Rng2,
+        typename Pred = hpx::parallel::v1::detail::less,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    bool includes(Rng1&& rng1, Rng2&& rng2, Pred&& op = Pred(),
         Proj1&& proj1 = Proj1(), Proj2&& proj2 = Proj2());
 
     // clang-format on
