f8f399ae14ab4bb2af853e4f599fb8088a8269a4
kadimitra <kadimitra@ece.auth.gr>
2022-07-13 11:15:53 +0200

    Fix doc mismatch

--- libs/core/algorithms/include/hpx/parallel/container_algorithms/mismatch.hpp
+++ libs/core/algorithms/include/hpx/parallel/container_algorithms/mismatch.hpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2007-2020 Hartmut Kaiser
+//  Copyright (c) 2022 Dimitra Karatza
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -108,14 +109,14 @@ namespace hpx { namespace ranges {
     ///           If the length of the range [first1, last1) does not mismatch
     ///           the length of the range [first2, last2), it returns false.
     ///
-    template <typename ExPolicy, typename Iter1, typename Sent1, typename Iter2,
-        typename Sent2, typename Pred = ranges::equal_to,
-        typename Proj1 = util::projection_identity,
-        typename Proj2 = util::projection_identity>
-    typename util::detail::algorithm_result<
-        ExPolicy, ranges::mismatch_result<FwdIter1, FwdIter2>>::type
-    mismatch(ExPolicy&& policy, FwdIter1 first1, FwdIter1 last1,
-        FwdIter2 first2, FwdIter2 last2, Pred&& op = Pred(),
+    template <typename ExPolicy, typename Iter1, typename Sent1,
+        typename Iter2, typename Sent2, typename Pred = equal_to,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    typename hpx::parallel::util::detail::algorithm_result<ExPolicy,
+        mismatch_result<Iter1, Iter2>>::type
+    mismatch(ExPolicy&& policy, Iter1 first1,
+        Sent1 last1, Iter2 first2, Sent2 last2, Pred&& op = Pred(),
         Proj1&& proj1 = Proj1(), Proj2&& proj2 = Proj2());
 
     /// Returns std::pair with iterators to the first two non-equivalent
@@ -197,14 +198,165 @@ namespace hpx { namespace ranges {
     ///           and another defined by [first2, last2).
     ///
     template <typename ExPolicy, typename Rng1, typename Rng2,
-        typename Pred = ranges::equal_to,
-        typename Proj1 = util::projection_identity,
-        typename Proj2 = util::projection_identity>
-    typename util::detail::algorithm_result<
-        ExPolicy, ranges::mimatch_result<FwdIter1, FwdIter2>>::type
+        typename Pred = equal_to,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    typename hpx::parallel::util::detail::algorithm_result<ExPolicy,
+        mismatch_result<
+            typename hpx::traits::range_traits<Rng1>::iterator_type,
+            typename hpx::traits::range_traits<Rng2>::iterator_type>>::type
     mismatch(ExPolicy&& policy, Rng1&& rng1, Rng2&& rng2, Pred&& op = Pred(),
         Proj1&& proj1 = Proj1(), Proj2&& proj2 = Proj2());
 
+    /// Returns true if the range [first1, last1) is mismatch to the range
+    /// [first2, last2), and false otherwise.
+    ///
+    /// \note   Complexity: At most min(last1 - first1, last2 - first2)
+    ///         applications of the predicate \a f. If \a FwdIter1
+    ///         and \a FwdIter2 meet the requirements of \a RandomAccessIterator
+    ///         and (last1 - first1) != (last2 - first2) then no applications
+    ///         of the predicate \a f are made.
+    ///
+    /// \tparam Iter1       The type of the source iterators used for the
+    ///                     first range (deduced).
+    ///                     This iterator type must meet the requirements of an
+    ///                     forward iterator.
+    /// \tparam Sent1       The type of the source iterators used for the end of
+    ///                     the first range (deduced).
+    /// \tparam Iter2       The type of the source iterators used for the
+    ///                     second range (deduced).
+    ///                     This iterator type must meet the requirements of an
+    ///                     forward iterator.
+    /// \tparam Sent2       The type of the source iterators used for the end of
+    ///                     the second range (deduced).
+    /// \tparam Pred        The type of an optional function/function object to use.
+    ///                     Unlike its sequential form, the parallel
+    ///                     overload of \a mismatch requires \a Pred to meet the
+    ///                     requirements of \a CopyConstructible. This defaults
+    ///                     to std::equal_to<>
+    /// \tparam Proj1       The type of an optional projection function applied
+    ///                     to the first range. This
+    ///                     defaults to \a util::projection_identity
+    /// \tparam Proj2       The type of an optional projection function applied
+    ///                     to the second range. This
+    ///                     defaults to \a util::projection_identity
+    ///
+    /// \param first1       Refers to the beginning of the sequence of elements
+    ///                     of the first range the algorithm will be applied to.
+    /// \param last1        Refers to the end of the sequence of elements of
+    ///                     the first range the algorithm will be applied to.
+    /// \param first2       Refers to the beginning of the sequence of elements
+    ///                     of the second range the algorithm will be applied to.
+    /// \param last2        Refers to the end of the sequence of elements of
+    ///                     the second range the algorithm will be applied to.
+    /// \param op           The binary predicate which returns true if the
+    ///                     elements should be treated as mismatch. The signature
+    ///                     of the predicate function should be equivalent to
+    ///                     the following:
+    ///                     \code
+    ///                     bool pred(const Type1 &a, const Type2 &b);
+    ///                     \endcode \n
+    ///                     The signature does not need to have const &, but
+    ///                     the function must not modify the objects passed to
+    ///                     it. The types \a Type1 and \a Type2 must be such
+    ///                     that objects of types \a FwdIter1 and \a FwdIter2 can
+    ///                     be dereferenced and then implicitly converted to
+    ///                     \a Type1 and \a Type2 respectively
+    /// \param proj1        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     first range as a
+    ///                     projection operation before the actual predicate
+    ///                     \a is invoked.
+    /// \param proj2        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     second range as a
+    ///                     projection operation before the actual predicate
+    ///                     \a is invoked.
+    ///
+    /// \note     The two ranges are considered mismatch if, for every iterator
+    ///           i in the range [first1,last1), *i mismatchs *(first2 + (i - first1)).
+    ///           This overload of mismatch uses operator== to determine if two
+    ///           elements are mismatch.
+    ///
+    /// \returns  The \a mismatch algorithm returns \a bool.
+    ///           The \a mismatch algorithm returns true if the elements in the
+    ///           two ranges are mismatch, otherwise it returns false.
+    ///           If the length of the range [first1, last1) does not mismatch
+    ///           the length of the range [first2, last2), it returns false.
+    ///
+    template <typename Iter1, typename Sent1,
+        typename Iter2, typename Sent2, typename Pred = equal_to,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    mismatch_result<Iter1, Iter2> mismatch(Iter1 first1, Sent1 last1, 
+        Iter2 first2, Sent2 last2, Pred&& op = Pred(), 
+        Proj1&& proj1 = Proj1(), Proj2&& proj2 = Proj2());
+
+    /// Returns std::pair with iterators to the first two non-equivalent
+    /// elements.
+    ///
+    /// \note   Complexity: At most \a last1 - \a first1 applications of the
+    ///         predicate \a f.
+    ///
+    /// \tparam Rng1        The type of the first source range used (deduced).
+    ///                     The iterators extracted from this range type must
+    ///                     meet the requirements of an forward iterator.
+    /// \tparam Rng2        The type of the second source range used (deduced).
+    ///                     The iterators extracted from this range type must
+    ///                     meet the requirements of an forward iterator.
+    /// \tparam Pred        The type of an optional function/function object to use.
+    ///                     Unlike its sequential form, the parallel
+    ///                     overload of \a mismatch requires \a Pred to meet the
+    ///                     requirements of \a CopyConstructible. This defaults
+    ///                     to std::equal_to<>
+    /// \tparam Proj1       The type of an optional projection function applied
+    ///                     to the first range. This
+    ///                     defaults to \a util::projection_identity
+    /// \tparam Proj2       The type of an optional projection function applied
+    ///                     to the second range. This
+    ///                     defaults to \a util::projection_identity
+    ///
+    /// \param rng1         Refers to the first sequence of elements the
+    ///                     algorithm will be applied to.
+    /// \param rng2         Refers to the second sequence of elements the
+    ///                     algorithm will be applied to.
+    /// \param op           The binary predicate which returns true if the
+    ///                     elements should be treated as mismatch. The signature
+    ///                     of the predicate function should be equivalent to
+    ///                     the following:
+    ///                     \code
+    ///                     bool pred(const Type1 &a, const Type2 &b);
+    ///                     \endcode \n
+    ///                     The signature does not need to have const &, but
+    ///                     the function must not modify the objects passed to
+    ///                     it. The types \a Type1 and \a Type2 must be such
+    ///                     that objects of types \a FwdIter1 and \a FwdIter2 can
+    ///                     be dereferenced and then implicitly converted to
+    ///                     \a Type1 and \a Type2 respectively
+    /// \param proj1        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     first range as a
+    ///                     projection operation before the actual predicate
+    ///                     \a is invoked.
+    /// \param proj2        Specifies the function (or function object) which
+    ///                     will be invoked for each of the elements of the
+    ///                     second range as a
+    ///                     projection operation before the actual predicate
+    ///                     \a is invoked.
+    ///
+    /// \returns  The \a mismatch algorithm returns \a std::pair<FwdIter1, FwdIter2>.
+    ///           The \a mismatch algorithm returns the first mismatching pair
+    ///           of elements from two ranges: one defined by [first1, last1)
+    ///           and another defined by [first2, last2).
+    ///
+    template <typename Rng1, typename Rng2, typename Pred = equal_to,
+        typename Proj1 = hpx::parallel::util::projection_identity,
+        typename Proj2 = hpx::parallel::util::projection_identity>
+    mismatch_result<
+        typename hpx::traits::range_traits<Rng1>::iterator_type,
+        typename hpx::traits::range_traits<Rng2>::iterator_type>
+    mismatch(Rng1&& rng1, Rng2&& rng2, Pred&& op = Pred(),
+        Proj1&& proj1 = Proj1(), Proj2&& proj2 = Proj2());
     // clang-format on
 }}    // namespace hpx::ranges
 
