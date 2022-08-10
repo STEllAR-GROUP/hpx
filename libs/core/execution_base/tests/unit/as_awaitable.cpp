54e5eddba0eae9f189aac24cd29e2d39e5a85ea1
Shreyas Atre <shreyasatre16@gmail.com>
2022-08-10 21:03:09 +0530

    [P2300] as_awaitable : Add tests skeleton
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

---
+++ libs/core/execution_base/tests/unit/as_awaitable.cpp
@@ -0,0 +1,16 @@
+//  Copyright (c) 2022 Shreyas Atre
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/config/coroutines_support.hpp>
+#include <hpx/execution_base/as_awaitable.hpp>
+#include <hpx/modules/testing.hpp>
+
+int main()
+{
+    using namespace hpx::execution::experimental;
+
+    return hpx::util::report_errors();
+}
