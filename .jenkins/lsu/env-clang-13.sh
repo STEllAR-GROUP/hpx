36f6b341c466328e71d2927f769d45576505219a
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2022-08-29 10:56:45 -0500

    Using EVE requires C++20
    - fixing configuration of our clang-v13 builder

--- .jenkins/lsu/env-clang-13.sh
+++ .jenkins/lsu/env-clang-13.sh
@@ -26,8 +26,6 @@ configure_extra_options+=" -DHPX_WITH_PARCELPORT_LCI=ON"
 configure_extra_options+=" -DHPX_WITH_FETCH_LCI=ON"
 configure_extra_options+=" -DHPX_WITH_PARCELPORT_LCI_BACKEND=ibv"
 configure_extra_options+=" -DHPX_WITH_LOGGING=OFF"
-configure_extra_options+=" -DHPX_WITH_DATAPAR_BACKEND=EVE"
-configure_extra_options+=" -DHPX_WITH_FETCH_EVE=ON"
 
 # The pwrapi library still needs to be set up properly on rostam
 # configure_extra_options+=" -DHPX_WITH_POWER_COUNTER=ON"
