a4487cd5293b3ea696cd9b574e0e299a06531a7e
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-10-16 01:28:45 +0200

    Make compatible with hipsycl cmake integration

--- cmake/HPX_SetupSYCL.cmake
+++ cmake/HPX_SetupSYCL.cmake
@@ -1,6 +1,18 @@
 if(HPX_WITH_SYCL)
-  add_compile_options(-fno-sycl)
   hpx_add_config_define(HPX_HAVE_SYCL)
   # TODO do we really have compute? What does that define implicate?
   hpx_add_config_define(HPX_HAVE_COMPUTE)
+
+  if(HPX_WITH_HIPSYCL)
+    hpx_add_config_define(HPX_HAVE_HIPSYCL)
+    find_package(hipSYCL REQUIRED)
+  else()
+    add_compile_options(-fno-sycl)
+  endif()
+else()
+  if(HPX_WITH_HIPSYCL)
+    hpx_error(
+      "HPX_WITH_HIPSYCL=ON requires HPX_WITH_SYCL=ON"
+    )
+  endif()
 endif()
--- libs/core/async_sycl/tests/unit/sycl_vector_add.cpp
+++ libs/core/async_sycl/tests/unit/sycl_vector_add.cpp
@@ -22,14 +22,14 @@
 #warning("Non-Intel compiler have not yet been tested with the HPX Sycl integration...")
 #endif
 #else
-#error("Compiler does not seem to support SYCL! SYCL_LANGUAGE_VERSION is undefined!")
+//#error("Compiler does not seem to support SYCL! SYCL_LANGUAGE_VERSION is undefined!")
 #endif
 
 // Check for separate compiler host and device passes
 #if defined(__SYCL_SINGLE_SOURCE__)
-#error("Sycl single source compiler -- unclear how it works with HPX device guards! Disabled for now")
+#error("Sycl single source compiler not supported! Use one with multiple passes")
 #else
-#pragma message("OKAY: Sycl compiler with two compile passes detected")
+#pragma message("OKAY: Sycl compiler with two or more compile passes detected")
 #endif
 
 
@@ -39,33 +39,89 @@
 #pragma message("Sycl host pass...")
 #endif
 
+#if defined(__HIPSYCL__) 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-copy"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wdouble-promotion"
+#pragma clang diagnostic ignored "-Wunused-local-typedef"
+#pragma clang diagnostic ignored "-Wsign-compare"
+#pragma clang diagnostic ignored "-Wgcc-compat"
+#pragma clang diagnostic ignored "-Wembedded-directive"
+#pragma clang diagnostic ignored "-Wmismatched-tags"
+#pragma clang diagnostic ignored "-Wreorder-ctor"
+#endif
+
+#include <hpx/async_sycl/sycl_event_pool.hpp> 
 #include <CL/sycl.hpp> 
 
 
 
 
+#if defined(__HIPSYCL__)
+using namespace cl::sycl;
+#else
 using namespace sycl;
+#endif
 
-constexpr size_t vector_size = 10000;
+constexpr size_t vector_size = 1000000;
 
 void VectorAdd(queue& q, const std::vector<int>& a_vector,
     const std::vector<int>& b_vector, std::vector<int>& add_parallel)
 {
+    event my_kernel_event;
+    event my_kernel_event2;
+    if (!hpx::sycl::experimental::sycl_event_pool::get_event_pool().pop(my_kernel_event))
+    {
+        HPX_THROW_EXCEPTION(
+            hpx::invalid_status, "add_event_callback", "could not get an event");
+    }
     // input range
     range<1> num_items{a_vector.size()};
-    // buffers from host vectors
-    buffer a_buf(a_vector);
-    buffer b_buf(b_vector);
-    buffer add_buf(add_parallel.data(), num_items);
+    {
+      // buffers from host vectors
+      buffer a_buf(a_vector.data(), num_items);
+      buffer b_buf(b_vector.data(), num_items);
+      buffer add_buf(add_parallel.data(), num_items);
 
-    q.submit([&](handler& h) {
-        // Access buffers
-        accessor a(a_buf, h, read_only);
-        accessor b(b_buf, h, read_only);
-        accessor add(add_buf, h, write_only, no_init);
-        // run Add kernel
-        h.parallel_for(num_items, [=](auto i) { add[i] = a[i] + b[i]; });
-    });
+      my_kernel_event = q.submit([&](handler& h) {
+          // Tell sycl we'd like to access our buffers here
+          accessor a(a_buf, h, read_only);
+          accessor b(b_buf, h, read_only);
+          accessor add(add_buf, h, write_only, no_init);
+          // run Add kernel
+          h.parallel_for(num_items, [=](auto i) { add[i] = a[i] + b[i]; });
+          // Note: destruction of the accessors should not cause a device->host
+          // memcpy (I think...)
+      });
+      // simulate get_future
+      my_kernel_event2 = q.submit([&](handler& h) {
+          h.parallel_for(range<1>{1}, [=](auto i) { });
+          });
+      // should be running
+      const auto event_status =
+          my_kernel_event.get_info<info::event::command_execution_status>();
+      const auto event_status2 =
+          my_kernel_event2.get_info<info::event::command_execution_status>();
+      if (event_status != info::event_command_status::complete &&
+          event_status2 != info::event_command_status::complete)
+          std::cerr << "OKAY: Kernel not yet done" << std::endl;
+      else
+        std::cerr << "ERROR: Kernel already done" << std::endl;
+      // according to the sycl specification (2020) section 3.9.8, the entire
+      // thing will synchronize here, due to the buffers being destroyed
+    }
+    // should be done
+    const auto event_status =
+        my_kernel_event.get_info<info::event::command_execution_status>();
+    const auto event_status2 =
+        my_kernel_event2.get_info<info::event::command_execution_status>();
+    if (event_status != info::event_command_status::complete ||
+        event_status2 != info::event_command_status::complete)
+        std::cerr << "ERROR: Kernel still not done" << std::endl;
+    else
+      std::cerr << "OKAY: Kernel done after end of scope" << std::endl;
 }
 
 int hpx_main(int, char**)
@@ -87,7 +143,8 @@ int hpx_main(int, char**)
     try
     {
         // TODO Insert executor once finished
-        queue q(d_selector);
+        queue q(d_selector, property::queue::in_order{});
+        /* queue q(d_selector); */
         std::cout << "SYCL language version: " << SYCL_LANGUAGE_VERSION << "\n";
         std::cout << "Running on device: "
                   << q.get_device().get_info<info::device::name>() << "\n";
