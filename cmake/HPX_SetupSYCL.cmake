69a8b09d169699f7193b5ac3548b7724d170487e
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-11-23 18:56:54 +0100

    Fixing device guards

--- cmake/HPX_SetupSYCL.cmake
+++ cmake/HPX_SetupSYCL.cmake
@@ -4,10 +4,10 @@ if(HPX_WITH_SYCL)
   hpx_add_config_define(HPX_HAVE_COMPUTE)
 
   if(HPX_WITH_HIPSYCL)
-    hpx_add_config_define(HPX_HAVE_HIPSYCL)
-    find_package(hipSYCL REQUIRED)
+    hpx_add_config_define(HPX_HAVE_HIPSYCL) 
+    find_package(hipSYCL REQUIRED) # for hipsycl: use cmake integration
   else()
-    add_compile_options(-fno-sycl)
+    add_compile_options(-fno-sycl) #by default: no sycl except in files where we allow it
   endif()
 else()
   if(HPX_WITH_HIPSYCL)
