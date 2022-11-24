f8aebd00190a379c831c1382e03d3e6a0d8b6cf0
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-11-24 14:13:33 +0100

    CMake format fixes

--- cmake/HPX_SetupSYCL.cmake
+++ cmake/HPX_SetupSYCL.cmake
@@ -7,12 +7,11 @@ if(HPX_WITH_SYCL)
     hpx_add_config_define(HPX_HAVE_HIPSYCL) 
     find_package(hipSYCL REQUIRED) # for hipsycl: use cmake integration
   else()
-    add_compile_options(-fno-sycl) #by default: no sycl except in files where we allow it
+    add_compile_options(-fno-sycl) # by default: no sycl except in files where
+                                   # we allow it
   endif()
 else()
   if(HPX_WITH_HIPSYCL)
-    hpx_error(
-      "HPX_WITH_HIPSYCL=ON requires HPX_WITH_SYCL=ON"
-    )
+    hpx_error("HPX_WITH_HIPSYCL=ON requires HPX_WITH_SYCL=ON")
   endif()
 endif()
