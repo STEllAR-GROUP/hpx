10617c4a373dd1956a894fc0afe0c52eeda65e0e
dimitraka <kadimitra@ece.auth.gr>
2023-02-22 19:55:51 +0100

    Add example task_group

---
+++ examples/quickstart/task_group_docs.cpp
@@ -0,0 +1,43 @@
+//  Copyright (c) 2023 Dimitra Karatza
+
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// This example is meant for inclusion in the documentation.
+
+//[task_group_docs
+
+#include <hpx/local/init.hpp>
+#include <hpx/task_group.hpp>
+
+void task1()
+{
+    std::cout << "Task 1 executed." << std::endl;
+}
+
+void task2()
+{
+    std::cout << "Task 2 executed." << std::endl;
+}
+
+int hpx_main()
+{
+    hpx::experimental::task_group tg;
+
+    tg.run(task1);
+    tg.run(task2);
+
+    tg.wait();
+
+    std::cout << "All tasks finished!" << std::endl;
+
+    return hpx::local::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    return hpx::local::init(hpx_main, argc, argv);
+}
+
+//]
