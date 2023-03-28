789c64653d10cf68b5b92957d16ff6f024549434
isidorostsa <tsa.isidoros@gmail.com>
2023-03-28 19:02:41 +0300

    change copyright name

--- libs/core/algorithms/include/hpx/algorithms/traits/is_relocatable.hpp
+++ libs/core/algorithms/include/hpx/algorithms/traits/is_relocatable.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2023 Hartmut Kaiser
+//  Copyright (c) 2023 Isidoros Tsaousis-Seiras
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
