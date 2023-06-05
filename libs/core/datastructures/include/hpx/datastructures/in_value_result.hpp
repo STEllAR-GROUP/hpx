f2a7f4a28dd948cccaa974f0a6c62cae28b0051a
Hari Hara Naveen S <johan511@rostam1.rostam.cct.lsu.edu>
2023-06-04 19:47:21 -0500

    fixed licenses, clang-format
    Signed-off-by: Hari Hara Naveen S <johan511@rostam1.rostam.cct.lsu.edu>

--- libs/core/datastructures/include/hpx/datastructures/in_value_result.hpp
+++ libs/core/datastructures/include/hpx/datastructures/in_value_result.hpp
@@ -1,6 +1,12 @@
-#pragma once
-#include <hpx/parallel/algorithms/move.hpp>
+//  Copyright (c) 2020-2022 STE||AR Group
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#pragma once
+
+#include <hpx/parallel/algorithms/move.hpp>
 
 namespace hpx::experimental { namespace ranges {
     template <typename Iter, typename T>
@@ -10,14 +16,12 @@ namespace hpx::experimental { namespace ranges {
         [[no_unique_address]] T value;
 
         template <class I2, class T2>
-        // hpx::convertible?
         constexpr operator in_value_result<I2, T2>() const&
         {
             return {in, value};
         }
 
         template <class I2, class T2>
-        // hpx::convertible?
         constexpr operator in_value_result<I2, T2>() &&
         {
             return {HPX_MOVE(in), HPX_MOVE(value)};
