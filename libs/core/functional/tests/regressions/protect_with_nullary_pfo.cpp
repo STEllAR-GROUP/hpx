146ec080298594ecc51f461c8450fbf6b4e6627e
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2023-06-14 14:21:34 -0500

    Fixing problems reported by clang-15

--- libs/core/functional/tests/regressions/protect_with_nullary_pfo.cpp
+++ libs/core/functional/tests/regressions/protect_with_nullary_pfo.cpp
@@ -54,11 +54,9 @@ int hpx_main()
     iterator_type itr_o = v2.begin();
     (void) itr_o;
 
-    std::size_t i = 0;
     for (std::size_t const& v : my_range)
     {
         hpx::async(hpx::util::protect(print_obj<std::size_t>()), v);
-        ++i;
     }
 
     return hpx::local::finalize();    // Handles HPX shutdown
