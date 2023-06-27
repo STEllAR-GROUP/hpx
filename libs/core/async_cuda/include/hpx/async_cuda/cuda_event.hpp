f435156b652c69a509d7075de335288698ffe3f8
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2023-06-27 11:14:46 -0500

    Fix assert

--- libs/core/async_cuda/include/hpx/async_cuda/cuda_event.hpp
+++ libs/core/async_cuda/include/hpx/async_cuda/cuda_event.hpp
@@ -1,7 +1,6 @@
 //  Copyright (c) 2023 Gregor Daiß
 //  Copyright (c) 2020 John Biddiscombe
 //  Copyright (c) 2020 Teodor Nikolov
-//
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
