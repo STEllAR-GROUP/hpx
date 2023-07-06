9f0d330d69770be4613908130dc4af94a5477779
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2023-07-06 12:51:50 -0500

    Fix inspect

--- libs/core/async_cuda/src/cuda_event.cpp
+++ libs/core/async_cuda/src/cuda_event.cpp
@@ -3,6 +3,8 @@
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// hpxinspect:noascii
 
 #include <hpx/assert.hpp>
 #include <hpx/async_cuda/cuda_event.hpp>
--- libs/core/async_cuda/tests/unit/cuda_multi_device_polling.cpp
+++ libs/core/async_cuda/tests/unit/cuda_multi_device_polling.cpp
@@ -3,8 +3,11 @@
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// hpxinspect:noascii
 
 #include <hpx/algorithm.hpp>
+#include <hpx/assert.hpp>
 #include <hpx/execution.hpp>
 #include <hpx/future.hpp>
 #include <hpx/init.hpp>
