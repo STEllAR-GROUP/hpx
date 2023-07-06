a2c6bc346bcf06fb98ad1338de417b619187f649
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2023-07-06 12:51:50 -0500

    Fix inspect

--- libs/core/async_cuda/include/hpx/async_cuda/cuda_event.hpp
+++ libs/core/async_cuda/include/hpx/async_cuda/cuda_event.hpp
@@ -5,11 +5,15 @@
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// hpxinspect:noascii
 
 #pragma once
 
+#include <cstddef>
 #include <deque>
 
+#include <hpx/assert.hpp>
 #include <hpx/async_cuda/cuda_exception.hpp>
 #include <hpx/async_cuda/custom_gpu_api.hpp>
 #include <hpx/concurrency/stack.hpp>
