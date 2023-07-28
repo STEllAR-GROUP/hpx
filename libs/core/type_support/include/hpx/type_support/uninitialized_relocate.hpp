3afc851abe23633ad68a526899a7a8c9d4dea29b
isidorostsa <tsa.isidoros@gmail.com>
2023-07-28 10:11:11 +0300

    uninitialized_relocate w/ tests and comments fixed

---
+++ libs/core/type_support/include/hpx/type_support/uninitialized_relocate.hpp
@@ -0,0 +1,162 @@
+//  Copyright (c) 2023 Isidoros Tsaousis-Seiras
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/type_support/is_relocatable.hpp>
+#include <hpx/type_support/is_trivially_relocatable.hpp>
+#include <hpx/type_support/relocate_at.hpp>
+
+#include <cstring>
+#include <type_traits>
+
+#if defined(HPX_HAVE_P1144_STD_RELOCATE_AT)
+#include <memory>
+#endif
+
+namespace hpx {
+
+#if defined(HPX_HAVE_P1144_STD_RELOCATE_AT)
+    using std::uninitialized_relocate;
+#else
+
+    namespace detail {
+
+        template <typename InIter, typename FwdIter>
+        struct choose_uninitialized_relocate_helper
+        {
+        //     using in_type = typename std::remove_reference_t<
+        //         decltype(*std::declval<InIter>())>;
+        //     using out_type = typename std::remove_reference_t<
+        //         decltype(*std::declval<FwdIter>())>;
+
+            using in_type = typename std::iterator_traits<InIter>::value_type;
+            using out_type = typename std::iterator_traits<FwdIter>::value_type;
+
+            constexpr static bool valid_relocation =
+                hpx::is_relocatable_from_v<out_type, in_type>;
+
+            constexpr static bool is_buffer_memcpyable =
+                hpx::is_trivially_relocatable_v<in_type> &&
+                //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ The important check
+                std::is_same_v<std::decay_t<in_type>, std::decay_t<out_type>> &&
+                //  can only relocate between same types
+                !std::is_volatile_v<in_type> && !std::is_volatile_v<out_type> &&
+                //  volatile types are not memcpyable
+                std::is_pointer_v<InIter> && std::is_pointer_v<FwdIter>;
+            //  ^^ the best we can do to check for contiguous iterators
+
+            constexpr static bool can_move_construct_nothrow =
+                std::is_nothrow_constructible_v<out_type,
+                    std::add_rvalue_reference_t<in_type>>;
+            // Checks if the move constructor is noexcept to skip
+            // the try-catch block
+
+            using type = std::integral_constant<int,
+                    is_buffer_memcpyable       ? 0 : // memcpy
+                    can_move_construct_nothrow ? 1 : // for loop nothrow
+                                                 2>; // for loop try-catch
+
+            constexpr static int value = type::value;
+        };
+
+        template <typename InIter, typename FwdIter,
+            std::enable_if_t<choose_uninitialized_relocate_helper<InIter,
+                                 FwdIter>::value == 0,
+                int> = 0>
+        FwdIter uninitialized_relocate_helper(
+            InIter first, InIter last, FwdIter dst) noexcept
+        {
+            auto n_objects = std::distance(first, last);
+
+            if (n_objects != 0)
+            {
+                std::byte* first_byte =
+                    reinterpret_cast<std::byte*>(std::addressof(*first));
+                std::byte* last_byte =
+                    reinterpret_cast<std::byte*>(std::addressof(*last));
+
+                auto n_bytes = std::distance(first_byte, last_byte);
+
+                void* dst_void = static_cast<void*>(std::addressof(*dst));
+                void* first_void = static_cast<void*>(std::addressof(*first));
+
+                std::memmove(dst_void, first_void, n_bytes);
+
+                dst += n_objects;
+            }
+
+            return dst;
+        }
+
+        template <typename InIter, typename FwdIter,
+            std::enable_if_t<choose_uninitialized_relocate_helper<InIter,
+                                 FwdIter>::value == 1,
+                int> = 0>
+        FwdIter uninitialized_relocate_helper(
+            InIter first, InIter last, FwdIter dst) noexcept
+        {
+            for (; first != last; ++first, ++dst)
+            {
+                // the move + destroy version will be used
+                hpx::relocate_at(std::addressof(*first), std::addressof(*dst));
+            }
+
+            return dst;
+        }
+
+        template <typename InIter, typename FwdIter,
+            std::enable_if_t<choose_uninitialized_relocate_helper<InIter,
+                                 FwdIter>::value == 2,
+                int> = 0>
+        FwdIter uninitialized_relocate_helper(
+            InIter first, InIter last, FwdIter dst)
+        {
+            FwdIter original_dst = dst;
+
+            for (; first != last; ++first, ++dst)
+            {
+                try
+                {
+                    // the move + destroy version will be used
+                    hpx::relocate_at(
+                        std::addressof(*first), std::addressof(*dst));
+                }
+                catch (...)
+                {
+                    // destroy all objects other that the one
+                    // that caused the exception
+
+                    // destroy all objects constructed so far
+                    std::destroy(original_dst, dst);
+                    // destroy all the objects not relocated yet
+                    std::destroy(++first, last);
+
+                    throw;
+                }
+            }
+
+            return dst;
+        }
+
+    }    // namespace detail
+
+    template <typename InIter, typename FwdIter>
+    FwdIter
+    uninitialized_relocate(InIter first, InIter last, FwdIter dst) noexcept(
+        detail::choose_uninitialized_relocate_helper<InIter, FwdIter>::value !=
+        2)
+    // maybe noexcept(auto)?
+    {
+        static_assert(detail::choose_uninitialized_relocate_helper<InIter,
+                          FwdIter>::valid_relocation,
+            "uninitialized_move(first, last, dst) must be well-formed");
+        return detail::uninitialized_relocate_helper(first, last, dst);
+    }
+
+#endif    // defined(HPX_HAVE_P1144_STD_RELOCATE_AT)
+
+}    // namespace hpx
---
+++ libs/core/type_support/tests/unit/uninitialized_relocate.cpp
@@ -0,0 +1,275 @@
+//  Copyright (c) 2023 Isidoros Tsaousis-Seiras
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/init.hpp>
+#include <hpx/modules/testing.hpp>
+#include <hpx/type_support/uninitialized_relocate.hpp>
+
+#define N 50
+#define K 10
+
+struct trivially_relocatable_struct
+{
+    static int count;
+    static int move_count;
+    static int dtor_count;
+    int data;
+
+    trivially_relocatable_struct(int data)
+      : data(data)
+    {
+        count++;
+    }
+    trivially_relocatable_struct(trivially_relocatable_struct&& other)
+      : data(other.data)
+    {
+        move_count++;
+        count++;
+    }
+    ~trivially_relocatable_struct()
+    {
+        dtor_count++;
+        count--;
+    }
+
+    // making sure the address is never directly accessed
+    friend void operator&(trivially_relocatable_struct) = delete;
+};
+int trivially_relocatable_struct::count = 0;
+int trivially_relocatable_struct::move_count = 0;
+int trivially_relocatable_struct::dtor_count = 0;
+
+HPX_DECLARE_TRIVIALLY_RELOCATABLE(trivially_relocatable_struct);
+static_assert(hpx::is_trivially_relocatable_v<trivially_relocatable_struct>);
+
+struct non_trivially_relocatable_struct
+{
+    static int count;
+    static int move_count;
+    static int dtor_count;
+    int data;
+
+    non_trivially_relocatable_struct(int data)
+      : data(data)
+    {
+        count++;
+    }
+    // mark as noexcept to enter simpler relocation path
+    non_trivially_relocatable_struct(
+        non_trivially_relocatable_struct&& other) noexcept
+      : data(other.data)
+    {
+        move_count++;
+        count++;
+    }
+    ~non_trivially_relocatable_struct()
+    {
+        dtor_count++;
+        count--;
+    }
+
+    // making sure the address is never directly accessed
+    friend void operator&(non_trivially_relocatable_struct) = delete;
+};
+int non_trivially_relocatable_struct::count = 0;
+int non_trivially_relocatable_struct::move_count = 0;
+int non_trivially_relocatable_struct::dtor_count = 0;
+
+static_assert(
+    !hpx::is_trivially_relocatable_v<non_trivially_relocatable_struct>);
+
+struct non_trivially_relocatable_struct_throwing
+{
+    static int count;
+    static int move_count;
+    static int dtor_count;
+
+    int data;
+
+    non_trivially_relocatable_struct_throwing(int data)
+      : data(data)
+    {
+        count++;
+    }
+    // do not mark as noexcept to enter try-catch relocation path
+    non_trivially_relocatable_struct_throwing(
+        non_trivially_relocatable_struct_throwing&& other)
+      : data(other.data)
+    {
+        if (move_count == K)
+        {
+            throw 42;
+        }
+        move_count++;
+
+        count++;
+    }
+    ~non_trivially_relocatable_struct_throwing()
+    {
+        dtor_count++;
+        count--;
+    }
+
+    // making sure the address is never directly accessed
+    friend void operator&(non_trivially_relocatable_struct_throwing) = delete;
+};
+
+int non_trivially_relocatable_struct_throwing::count = 0;
+int non_trivially_relocatable_struct_throwing::move_count = 0;
+int non_trivially_relocatable_struct_throwing::dtor_count = 0;
+
+static_assert(!hpx::is_trivially_relocatable_v<
+              non_trivially_relocatable_struct_throwing>);
+
+int hpx_main()
+{
+    {
+        void* mem1 = std::malloc(N * sizeof(trivially_relocatable_struct));
+        void* mem2 = std::malloc(N * sizeof(trivially_relocatable_struct));
+
+        HPX_TEST(mem1 && mem2);
+
+        trivially_relocatable_struct* ptr1 =
+            static_cast<trivially_relocatable_struct*>(mem1);
+        trivially_relocatable_struct* ptr2 =
+            static_cast<trivially_relocatable_struct*>(mem2);
+
+        HPX_TEST(trivially_relocatable_struct::count == 0);
+        HPX_TEST(trivially_relocatable_struct::move_count == 0);
+        HPX_TEST(trivially_relocatable_struct::dtor_count == 0);
+
+        for (int i = 0; i < N; i++)
+        {
+            hpx::construct_at(ptr1 + i, 1234);
+        }
+
+        // N objects constructed
+        HPX_TEST(trivially_relocatable_struct::count == N);
+
+        // relocate them to ptr2
+        hpx::uninitialized_relocate(ptr1, ptr1 + N, ptr2);
+
+        // All creations - destructions balance out
+        HPX_TEST(trivially_relocatable_struct::count == N);
+
+        // No move constructor or destructor should be called
+        HPX_TEST(trivially_relocatable_struct::move_count == 0);
+        HPX_TEST(trivially_relocatable_struct::dtor_count == 0);
+
+        for (int i = 0; i < N; i++)
+        {
+            HPX_TEST(ptr2[i].data == 1234);
+        }
+
+        std::destroy(ptr2, ptr2 + N);
+
+        std::free(mem1);
+        std::free(mem2);
+    }
+    {
+        void* mem1 = std::malloc(N * sizeof(non_trivially_relocatable_struct));
+        void* mem2 = std::malloc(N * sizeof(non_trivially_relocatable_struct));
+
+        HPX_TEST(mem1 && mem2);
+
+        non_trivially_relocatable_struct* ptr1 =
+            static_cast<non_trivially_relocatable_struct*>(mem1);
+        non_trivially_relocatable_struct* ptr2 =
+            static_cast<non_trivially_relocatable_struct*>(mem2);
+
+        HPX_TEST(non_trivially_relocatable_struct::count == 0);
+        HPX_TEST(non_trivially_relocatable_struct::move_count == 0);
+        HPX_TEST(non_trivially_relocatable_struct::dtor_count == 0);
+
+        for (int i = 0; i < N; i++)
+        {
+            hpx::construct_at(ptr1 + i, 1234);
+        }
+
+        // N objects constructed
+        HPX_TEST(non_trivially_relocatable_struct::count == N);
+
+        // relocate them to ptr2
+        hpx::uninitialized_relocate(ptr1, ptr1 + N, ptr2);
+
+        // All creations - destructions balance out
+        HPX_TEST(non_trivially_relocatable_struct::count == N);
+
+        // Every object was moved from and then destroyed
+        HPX_TEST(non_trivially_relocatable_struct::move_count == N);
+        HPX_TEST(non_trivially_relocatable_struct::dtor_count == N);
+
+        for (int i = 0; i < N; i++)
+        {
+            HPX_TEST(ptr2[i].data == 1234);
+        }
+
+        std::destroy(ptr2, ptr2 + N);
+
+        std::free(mem1);
+        std::free(mem2);
+    }
+    {
+        void* mem1 =
+            std::malloc(N * sizeof(non_trivially_relocatable_struct_throwing));
+        void* mem2 =
+            std::malloc(N * sizeof(non_trivially_relocatable_struct_throwing));
+
+        HPX_TEST(mem1 && mem2);
+
+        non_trivially_relocatable_struct_throwing* ptr1 =
+            static_cast<non_trivially_relocatable_struct_throwing*>(mem1);
+        non_trivially_relocatable_struct_throwing* ptr2 =
+            static_cast<non_trivially_relocatable_struct_throwing*>(mem2);
+
+        HPX_TEST(non_trivially_relocatable_struct_throwing::count == 0);
+        HPX_TEST(non_trivially_relocatable_struct_throwing::move_count == 0);
+        HPX_TEST(non_trivially_relocatable_struct_throwing::dtor_count == 0);
+
+        for (int i = 0; i < N; i++)
+        {
+            hpx::construct_at(ptr1 + i, 1234);
+        }
+
+        // N objects constructed
+        HPX_TEST(non_trivially_relocatable_struct_throwing::count == N);
+
+        // relocate them to ptr2
+        try
+        {
+            hpx::uninitialized_relocate(ptr1, ptr1 + N, ptr2);
+            HPX_TEST(false);    // should never reach this
+        }
+        catch (int forty_two)
+        {
+            HPX_TEST(forty_two == 42);
+        }
+
+        // K move constructors were called
+        HPX_TEST(non_trivially_relocatable_struct_throwing::move_count == K);
+
+        // K - 1 destructors were called to balance out the move constructors
+        // (- 1 because the last move constructor throws)
+        // and then N + 1 destructors were called: K on the old range and
+        // N - (K - 1) = N - K + 1 on the new range
+        HPX_TEST(
+            non_trivially_relocatable_struct_throwing::dtor_count == N + K);
+
+        // It stops at K, so K-1 move-destruct pairs have been executed
+        // after this N - (K - 1) destructs will be done on the old range
+        // and K - 1 on the new range. giving 2*N total destructs
+
+        std::free(mem1);
+        std::free(mem2);
+    }
+    return hpx::local::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    hpx::local::init(hpx_main, argc, argv);
+    return hpx::util::report_errors();
+}
