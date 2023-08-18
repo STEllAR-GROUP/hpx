b7a2faab4b18d2ffd47ec542e12827a04bb2dca1
isidorostsa <tsa.isidoros@gmail.com>
2023-08-18 20:09:56 +0300

    is_relocatable_from

--- libs/core/type_support/include/hpx/type_support/is_relocatable.hpp
+++ libs/core/type_support/include/hpx/type_support/is_relocatable.hpp
@@ -35,6 +35,15 @@ namespace hpx::experimental {
     {
     };
 
+    // ToTp(FromTp&&) must be well-formed
+    template <typename ToTp, typename FromTp>
+    struct is_relocatable_from
+      : std::bool_constant<
+            std::is_constructible_v<std::remove_cv_t<ToTp>, FromTp> &&
+            std::is_same_v<std::decay_t<ToTp>, std::decay_t<FromTp>>>
+    {
+    };
+
     template <typename T>
     inline constexpr bool is_relocatable_v = is_relocatable<T>::value;
 
