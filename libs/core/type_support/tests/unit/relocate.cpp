aa09ce45ee1304b7611dc440ef9294f0f9ad9244
isidorostsa <tsa.isidoros@gmail.com>
2023-08-19 17:13:18 +0300

    relocate test

---
+++ libs/core/type_support/tests/unit/relocate.cpp
@@ -0,0 +1,74 @@
+//  Copyright (c) 2023 Isidoros Tsaousis-Seiras
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/*
+    This test checks that the relocate_at function works correctly
+    for non-trivially relocatable types.
+
+    The trivially relocatable optimization can not be implemented yet
+    so it is not tested separately.
+*/
+
+#include <hpx/init.hpp>
+#include <hpx/modules/testing.hpp>
+#include <hpx/type_support/construct_at.hpp>
+#include <hpx/type_support/relocate_at.hpp>
+
+struct non_trivially_relocatable_struct
+{
+    static int count;
+    int data;
+
+    explicit non_trivially_relocatable_struct(int data)
+      : data(data)
+    {
+        count++;
+    }
+    non_trivially_relocatable_struct(non_trivially_relocatable_struct&& other)
+      : data(other.data)
+    {
+        count++;
+    }
+    ~non_trivially_relocatable_struct()
+    {
+        count--;
+    }
+
+    // making sure the address is never directly accessed
+    friend void operator&(non_trivially_relocatable_struct) = delete;
+};
+int non_trivially_relocatable_struct::count = 0;
+
+int hpx_main()
+{
+    void* mem1 = std::malloc(sizeof(non_trivially_relocatable_struct));
+
+    HPX_TEST(mem1);
+
+    HPX_TEST(non_trivially_relocatable_struct::count == 0);
+
+    non_trivially_relocatable_struct* ptr1 = hpx::construct_at(
+        static_cast<non_trivially_relocatable_struct*>(mem1), 1234);
+
+    // a single object was constructed
+    HPX_TEST(non_trivially_relocatable_struct::count == 1);
+
+    non_trivially_relocatable_struct obj2 = hpx::experimental::relocate(ptr1);
+
+    // count = 1 + 1 (from the move construction) - 1 (from the destruction)
+    HPX_TEST(non_trivially_relocatable_struct::count == 1);
+    HPX_TEST(obj2.data == 1234);
+
+    std::free(mem1);
+
+    return hpx::local::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    hpx::local::init(hpx_main, argc, argv);
+    return hpx::util::report_errors();
+}
