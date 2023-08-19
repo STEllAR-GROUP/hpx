9d496a536b391460ae6f4196217f3e06394fd012
isidorostsa <tsa.isidoros@gmail.com>
2023-08-19 17:14:13 +0300

    clang-format and comments

--- libs/core/type_support/tests/unit/fail_relocate_at.cpp
+++ libs/core/type_support/tests/unit/fail_relocate_at.cpp
@@ -4,6 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// This test should fail to compile
+
 #include <hpx/init.hpp>
 #include <hpx/type_support/relocate_at.hpp>
 #include <vector>
