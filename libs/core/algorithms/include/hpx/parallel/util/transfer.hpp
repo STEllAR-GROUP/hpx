c2885896b7461eb07430e24929063a4f739d0c85
isidorostsa <tsa.isidoros@gmail.com>
2023-08-23 18:52:52 +0300

    clang-format

--- libs/core/algorithms/include/hpx/parallel/util/transfer.hpp
+++ libs/core/algorithms/include/hpx/parallel/util/transfer.hpp
@@ -377,9 +377,10 @@ namespace hpx::parallel::util {
                         ::hpx::parallel::util::loop_with_manual_cleanup_n(
                             HPX_FORWARD(ExPolicy, policy), t, num,
                             [](zip_iterator current) -> void {
-
-                                InIter& current_first = hpx::get<0>(current.get_iterator_tuple());
-                                OutIter& current_dest = hpx::get<1>(current.get_iterator_tuple());
+                                InIter& current_first =
+                                    hpx::get<0>(current.get_iterator_tuple());
+                                OutIter& current_dest =
+                                    hpx::get<1>(current.get_iterator_tuple());
 
                                 hpx::relocate_at(std::addressof(*current_first),
                                     std::addressof(*current_dest));
@@ -390,7 +391,7 @@ namespace hpx::parallel::util {
 
                                 // destroy all objects constructed so far
                                 // the object that caused the exception is
-                                // destroyed by relocate_at 
+                                // destroyed by relocate_at
                                 std::destroy(dest, current_dest);
                                 // destroy all the objects not relocated yet
                                 std::destroy(current_first + 1, last);
