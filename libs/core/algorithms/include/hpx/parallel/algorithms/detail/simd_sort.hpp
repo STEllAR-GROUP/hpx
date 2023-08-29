1f92c0faf94132ad2f78c407ac4fb399df9d617e
Johan511 <harihara.sn@gmail.com>
2023-08-29 17:52:40 +0530

    fixed inspect errors

--- libs/core/algorithms/include/hpx/parallel/algorithms/detail/simd_sort.hpp
+++ libs/core/algorithms/include/hpx/parallel/algorithms/detail/simd_sort.hpp
@@ -1,7 +1,17 @@
+//  Copyright (c) 2023 Hari Hara Naveen S
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <memory>
+#include <type_traits>
+
 #pragma once
 
 namespace hpx::parallel::util {
-    // TODO : add support for _Float16, need compile time test as _Float16 is not always supported
+    // TODO : add support for _Float16
+    // need compile time test as _Float16 is not always supported
     template <class T>
     struct is_simd_sortable
       : std::integral_constant<bool,
