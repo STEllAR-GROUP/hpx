82f290779c5c735ce091a451ded3e63559c58a00
isidorostsa <tsa.isidoros@gmail.com>
2023-09-19 10:22:45 +0300

    detect vec, arr, string w/out causing hard errors

--- libs/core/type_support/tests/unit/is_contiguous_iterator.cpp
+++ libs/core/type_support/tests/unit/is_contiguous_iterator.cpp
@@ -6,15 +6,24 @@
 
 #include <array>
 #include <cassert>
+#include <iterator>
+#include <string>
 #include <vector>
 
 #include <hpx/type_support/is_contiguous_iterator.hpp>
 
-using hpx::traits::is_contiguous_iterator_v;
+using namespace hpx::traits::detail;
+using namespace hpx::traits;
 
 // std::vector<int>::iterator is a contiguous iterator
+static_assert(has_valid_vector_v<int>);
+
 static_assert(is_contiguous_iterator_v<std::vector<int>::iterator>);
+static_assert(is_std_vector_iterator<std::vector<int>::iterator>::value);
+
 static_assert(is_contiguous_iterator_v<std::vector<int>::const_iterator>);
+static_assert(is_std_vector_iterator<std::vector<int>::const_iterator>::value);
+
 // reverse_iterator is not a contiguous iterator
 static_assert(!is_contiguous_iterator_v<std::vector<int>::reverse_iterator>);
 static_assert(
@@ -31,19 +40,35 @@ static_assert(
 // pointers are contiguous iterators
 static_assert(is_contiguous_iterator_v<int*>);
 static_assert(is_contiguous_iterator_v<int const*>);
+
+// std::string::iterator is a contiguous iterator
+static_assert(is_contiguous_iterator_v<std::string::iterator>);
+static_assert(is_contiguous_iterator_v<std::string::const_iterator>);
+
+// Pointers to arrays are still pointers
 static_assert(is_contiguous_iterator_v<int (*)[]>);
 static_assert(is_contiguous_iterator_v<int const (*)[]>);
 static_assert(is_contiguous_iterator_v<int (*)[4]>);
 static_assert(is_contiguous_iterator_v<int const (*)[4]>);
 
-// arrays are not contiguous iterators
+// Pointers to functions are still pointers
+static_assert(is_contiguous_iterator_v<int (*)()>);
+static_assert(is_contiguous_iterator_v<int const (*)()>);
+
+// c-style arrays are not contiguous iterators
 static_assert(!is_contiguous_iterator_v<int[]>);
 static_assert(!is_contiguous_iterator_v<int[4]>);
 static_assert(!is_contiguous_iterator_v<int const[]>);
 static_assert(!is_contiguous_iterator_v<int const[4]>);
 
-// std::string::iterator is a contiguous iterator
-static_assert(is_contiguous_iterator_v<std::string::iterator>);
-static_assert(is_contiguous_iterator_v<std::string::const_iterator>);
+// Unknown type iterators to "weird"
+// types should not cause compile errors
+using function_iterator =
+    std::iterator<std::random_access_iterator_tag, int(int, int)>;
+using empty_array_iterator =
+    std::iterator<std::random_access_iterator_tag, int[]>;
+
+static_assert(!is_contiguous_iterator_v<function_iterator>);
+static_assert(!is_contiguous_iterator_v<empty_array_iterator>);
 
 int main(int, char*[]) {}
