080937d0a36ef3cbfec5bd97f596d2c2ad6dabe0
ct-clmsn <ct.clmsn@gmail.com>
2023-10-10 12:14:23 -0400

    fixes for linking issues in ctest/builder scripts

--- .jenkins/lsu/env-gcc-10.sh
+++ .jenkins/lsu/env-gcc-10.sh
@@ -28,6 +28,7 @@ configure_extra_options+=" -DHPX_WITH_FETCH_LCI=ON"
 configure_extra_options+=" -DHPX_WITH_PARCELPORT_GASNET=ON"
 configure_extra_options+=" -DHPX_WITH_FETCH_GASNET=ON"
 configure_extra_options+=" -DCMAKE_C_COMPILER=gcc"
+configure_extra_options+=" -DCMAKE_C_FLAGS=-fPIC"
 configure_extra_options+=" -DHPX_WITH_PARCELPORT_GASNET_CONDUIT=mpi"
 
 # The pwrapi library still needs to be set up properly on rostam
