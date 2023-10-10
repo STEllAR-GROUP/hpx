c6f6025a077c8ff92d729e5be897d8c3fd4646b0
ct-clmsn <ct.clmsn@gmail.com>
2023-10-10 14:57:19 -0400

    changing builders to test against udp conduit

--- .jenkins/lsu/env-gcc-10.sh
+++ .jenkins/lsu/env-gcc-10.sh
@@ -29,7 +29,7 @@ configure_extra_options+=" -DHPX_WITH_PARCELPORT_GASNET=ON"
 configure_extra_options+=" -DHPX_WITH_FETCH_GASNET=ON"
 configure_extra_options+=" -DCMAKE_C_COMPILER=gcc"
 configure_extra_options+=" -DCMAKE_C_FLAGS=-fPIC"
-configure_extra_options+=" -DHPX_WITH_PARCELPORT_GASNET_CONDUIT=mpi"
+configure_extra_options+=" -DHPX_WITH_PARCELPORT_GASNET_CONDUIT=udp"
 
 # The pwrapi library still needs to be set up properly on rostam
 # configure_extra_options+=" -DHPX_WITH_POWER_COUNTER=ON"
