15b73a33052eb6fcb5d368ab9edefdd7ef9eaf6e
ct-clmsn <ct.clmsn@gmail.com>
2023-11-26 13:10:43 -0500

    added default configuration mode

--- cmake/HPX_SetupGasnet.cmake
+++ cmake/HPX_SetupGasnet.cmake
@@ -286,7 +286,7 @@ macro(hpx_setup_gasnet)
           if(PKGCONFIG_CFLAGS_STATUS)
             message(
               FATAL_ERROR
-              "`pkg-config --cflags ompi` result = ${PKGCONFIG_CFLAGS_STATUS} - see ${PKGCONFIG_CFLAGS_OUTPUT} for more details"
+              "`pkg-config --cflags ompi` result = ${PKGCONFIG_CFLAGS_STATUS} - see ${PKGCONFIG_CFLAGS_ERROR_FILE} for more details"
             )
           endif()
 
@@ -304,25 +304,41 @@ macro(hpx_setup_gasnet)
           if(PKGCONFIG_LIBS_STATUS)
             message(
               FATAL_ERROR
-              "`pkg-config --libs ompi` result = ${PKGCONFIG_LIBS_STATUS} - see ${PKGCONFIG_LIBS_OUTPUT} for more details"
+              "`pkg-config --libs ompi` result = ${PKGCONFIG_LIBS_STATUS} - see ${PKGCONFIG_LIBS_ERROR_FILE} for more details"
             )
           endif()
 
-          message(
-            STATUS
-            "GASNet Build Command\nCC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CCFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CXXFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" ./configure --enable-mpi --with-mpi-cc=${CMAKE_C_COMPILER} --prefix=${GASNET_DIR}/install --with-cflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" --with-cxxflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" && make -j ${GASNET_BUILD_PARALLEL_LEVEL} && make install"
-          )
+          if(PKGCONFIG_LIBS_STATUS OR PKGCONFIG_CFLAGS_STATUS)
+            message(
+              STATUS
+              "GASNet Build Command\nCC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=-fPIC CCFLAGS=-fPIC CXXFLAGS=-fPIC ./configure --enable-mpi --with-mpi-cc=${CMAKE_C_COMPILER} --prefix=${GASNET_DIR}/install --with-cflags=-fPIC --with-cxxflags=-fPIC && make -j ${GASNET_BUILD_PARALLEL_LEVEL} && make install"
+            )
 
-          execute_process(
-            COMMAND
-              bash -c
-              "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CCFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CXXFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" ./configure --enable-mpi --with-mpi-cc=${CMAKE_C_COMPILER} --prefix=${GASNET_DIR}/install --with-cflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" --with-cxxflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" && make -j ${GASNET_BUILD_PARALLEL_LEVEL} && make install"
-            WORKING_DIRECTORY ${GASNET_DIR}
-            RESULT_VARIABLE GASNET_BUILD_STATUS
-            OUTPUT_FILE ${GASNET_BUILD_OUTPUT}
-            ERROR_FILE ${GASNET_ERROR_FILE}
-          )
+            execute_process(
+              COMMAND
+                bash -c
+                "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=-fPIC CCFLAGS=-fPIC CXXFLAGS=-fPIC ./configure --enable-mpi --with-mpi-cc=${CMAKE_C_COMPILER} --prefix=${GASNET_DIR}/install --with-cflags=-fPIC --with-cxxflags=-fPIC && make -j ${GASNET_BUILD_PARALLEL_LEVEL} && make install"
+              WORKING_DIRECTORY ${GASNET_DIR}
+              RESULT_VARIABLE GASNET_BUILD_STATUS
+              OUTPUT_FILE ${GASNET_BUILD_OUTPUT}
+              ERROR_FILE ${GASNET_ERROR_FILE}
+            )
+          else()
+            message(
+              STATUS
+              "GASNet Build Command\nCC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CCFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CXXFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" ./configure --enable-mpi --with-mpi-cc=${CMAKE_C_COMPILER} --prefix=${GASNET_DIR}/install --with-cflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" --with-cxxflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" && make -j ${GASNET_BUILD_PARALLEL_LEVEL} && make install"
+            )
 
+            execute_process(
+              COMMAND
+                bash -c
+                "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CCFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" CXXFLAGS=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" ./configure --enable-mpi --with-mpi-cc=${CMAKE_C_COMPILER} --prefix=${GASNET_DIR}/install --with-cflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" --with-cxxflags=\"-fPIC ${PKGCONFIG_CFLAGS_FLAGS} ${PKGCONFIG_LIBS_FLAGS}\" && make -j ${GASNET_BUILD_PARALLEL_LEVEL} && make install"
+              WORKING_DIRECTORY ${GASNET_DIR}
+              RESULT_VARIABLE GASNET_BUILD_STATUS
+              OUTPUT_FILE ${GASNET_BUILD_OUTPUT}
+              ERROR_FILE ${GASNET_ERROR_FILE}
+            )
+          endif()
         endif()
       endif()
 
