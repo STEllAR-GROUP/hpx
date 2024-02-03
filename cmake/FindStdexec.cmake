22a6d61f8d896245d6847d3a420193caa87a8526
isidorostsa <tsa.isidoros@gmail.com>
2024-02-03 20:30:42 +0200

    Add Stdexec to the build system

---
+++ cmake/FindStdexec.cmake
@@ -0,0 +1,36 @@
+
+if(NOT TARGET STDEXEC::stdexec)
+  if (STDEXEC_ROOT AND NOT Stdexec_ROOT)
+    set(Stdexec_ROOT ${STDEXEC_ROOT} CACHE PATH "stdexec base directory")
+    unset(STDEXEC_ROOT CACHE)
+  endif()
+
+    find_path(
+      Stdexec_INCLUDE_DIR
+      stdexec
+      HINTS ${Stdexec_ROOT}
+    )
+
+    if (Stdexec_INCLUDE_DIR)
+      file(TO_CMAKE_PATH ${Stdexec_INCLUDE_DIR} Stdexec_INCLUDE_DIR)
+    else()
+      message(FATAL_ERROR "stdexec not found")
+    endif()
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(
+        Stdexec
+        REQUIRED_VARS Stdexec_INCLUDE_DIR
+        FOUND_VAR Stdexec_FOUND
+        VERSION_VAR Stdexec_VERSION
+        FAIL_MESSAGE "stdexec not found"
+    )
+
+    add_library(STDEXEC::stdexec INTERFACE IMPORTED)
+    target_include_directories(STDEXEC::stdexec SYSTEM INTERFACE ${Stdexec_INCLUDE_DIR})
+
+
+
+
+    mark_as_advanced(Stdexec_INCLUDE_DIR Stdexec_ROOT)
+endif()
---
+++ libs/core/execution_base/tests/unit/stdexec.cpp
@@ -0,0 +1,24 @@
+//  Copyright (c) 2023 Isidoros Tsaousis-Seiras
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/init.hpp>
+#include <hpx/execution_base/stdexec_forward.hpp>
+
+#include <hpx/modules/testing.hpp>
+
+#include <utility>
+
+using namespace hpx::execution::experimental;
+
+int main() {
+	auto x = just(42);
+
+	auto [a] = sync_wait(std::move(x)).value();
+
+  HPX_TEST(a == 42);
+
+  return hpx::util::report_errors();
+}
