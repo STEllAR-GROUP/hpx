cc03efd608973e0357496076a2faa5283b1f18d5
isidorostsa <tsa.isidoros@gmail.com>
2024-02-17 17:40:07 +0200

    move code to HPX_SetupStdexec

--- cmake/HPX_SetupStdexec.cmake
+++ cmake/HPX_SetupStdexec.cmake
@@ -4,6 +4,30 @@
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+if(Stdexec_ROOT OR HPX_WITH_FETCH_STDEXEC)
+  # explicitly enable HPX_WITH_STDEXEC
+  set(HPX_WITH_STDEXEC ON)
+
+  # prefer Stdexec_ROOT over HPX_WITH_FETCH_STDEXEC by default
+  if(Stdexec_ROOT AND HPX_WITH_FETCH_STDEXEC)
+    set(HPX_WITH_FETCH_STDEXEC OFF)
+    hpx_warn(
+      "Both Stdexec_ROOT and HPX_WITH_FETCH_STDEXEC are provided. HPX_WITH_FETCH_STDEXEC is set to OFF."
+    )
+  endif()
+elseif(HPX_WITH_STDEXEC)
+  hpx_error(
+    "HPX_WITH_STDEXEC is set to ON, but Stdexec_ROOT is not provided and HPX_WITH_FETCH_STDEXEC is not enabled. Please provide Stdexec_ROOT or set HPX_WITH_FETCH_STDEXEC to ON."
+  )
+endif()
+
+# STDEXEC requires C++20
+if(HPX_WITH_STDEXEC AND HPX_WITH_CXX_STANDARD LESS 20)
+  hpx_error(
+  "HPX_WITH_STDEXEC is set to ON, but HPX_WITH_CXX_STANDARD is less than 20. Please set HPX_WITH_CXX_STANDARD to 20 or higher."
+  )
+endif()
+
 if(HPX_WITH_STDEXEC AND NOT TARGET STDEXEC::stdexec)
     hpx_add_config_define(HPX_HAVE_STDEXEC)
 
