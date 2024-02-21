1d9194eeb5b99bc6ec9f4d7cb65a5ef92e6e6894
Vedant <vedantnimjed@gmail.com>
2024-02-21 22:37:45 +0530

    Pyperf integration for testing puporses
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- cmake/HPX_SetupNanobench.cmake
+++ cmake/HPX_SetupNanobench.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     nanobench
     GIT_REPOSITORY https://github.com/martinus/nanobench.git
-    GIT_TAG v4.1.0
+    GIT_TAG v4.3.11
     GIT_SHALLOW TRUE)
 
 FetchContent_MakeAvailable(nanobench)
