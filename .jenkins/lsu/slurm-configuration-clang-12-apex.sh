cd83031f73a63a7a8066f19b2e2fcdd2906f95cf
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2024-03-18 14:59:22 -0500

    Move APEX CI to rostam

--- .jenkins/lsu/slurm-configuration-clang-12-apex.sh
+++ .jenkins/lsu/slurm-configuration-clang-12-apex.sh
@@ -1,7 +1,9 @@
 # Copyright (c) 2020 ETH Zurich
+# Copyright (c) 2023 STE||AR-GROUP
 #
 # SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-configuration_slurm_constraint="mc"
+configuration_slurm_partition="jenkins-compute"
+configuration_slurm_num_nodes="1"
