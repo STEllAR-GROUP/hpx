764add3623740378d293d442638302c11388f395
isidorostsa <tsa.isidoros@gmail.com>
2024-03-27 17:09:41 -0500

    The old calling convention is:                      make_env<tag>(val, old_env) The new calling convention for make_env is:                      make_env(old_env, with(tag, val))

---
+++ libs/core/execution_base/include/hpx/execution_base/exec_forward.hpp
@@ -0,0 +1,15 @@
+//  Copyright (c) 2023 Isidoros Tsaousis-Seiras
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#if defined(HPX_HAVE_STDEXEC)
+#include <exec/execution.hpp>
+
+namespace hpx::execution::experimental {
+    using namespace stdexec;
+}
+#endif
