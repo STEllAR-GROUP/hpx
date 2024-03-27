f7228b9d21ffdedbae2bd43ffc599e1b3b6e5c6f
Vedant <vedantnimjed@gmail.com>
2024-03-27 12:37:56 +0530

    Added benchmark output specialisations for normal case as well
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- tools/perftests_plot.py
+++ tools/perftests_plot.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import json
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 if len(sys.argv) != 2:
     print("No benchmark selected")
@@ -12,24 +13,20 @@ else:
 
     contents = subprocess.run([test_name, "--hpx:verbose_bench"], capture_output=True)
 
-    # print(contents)
     json_obj = json.loads(contents.stdout.decode('utf-8'))
 
     test_names = []
     samples = []
 
+    print(json_obj)
     for test in json_obj["outputs"]:
         test_names.append(test["name"] + "," + test["executor"])
         samples.append(test["series"])
 
     fig = plt.figure()
     ax = fig.add_subplot()
-    bp = ax.boxplot(samples)
+    bp = ax.boxplot(samples, showfliers=False)
 
     ax.set_xticklabels(test_names)
-
     plt.show()
-
-
-    
     
