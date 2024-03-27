4def4cfd7de56d3b44312e2fc6b2587b32339dd8
isidorostsa <tsa.isidoros@gmail.com>
2024-03-27 17:11:48 -0500

    Forwarding implementations

--- libs/core/execution_base/include/hpx/execution_base/sender.hpp
+++ libs/core/execution_base/include/hpx/execution_base/sender.hpp
@@ -14,33 +14,31 @@
 #include <hpx/execution_base/get_env.hpp>
 
 namespace hpx::execution::experimental {
-   template<typename Scheduler>
-   struct is_scheduler : std::bool_constant<scheduler<Scheduler>>
-   {
-   };
+    template<typename Scheduler>
+    inline constexpr bool is_scheduler_v = scheduler<Scheduler>;
 
     template<typename Scheduler>
-    inline constexpr bool is_scheduler_v = is_scheduler<Scheduler>::value;
-
-    template<typename Sender, typename Env = empty_env>
-    struct is_sender : std::bool_constant<sender<Sender>>
+    struct is_scheduler : std::bool_constant<is_scheduler_v<Scheduler>>
     {
     };
 
     template<typename Sender, typename Env = empty_env>
-    inline constexpr bool is_sender_v = is_sender<Sender>::value;
+    inline constexpr bool is_sender_v = sender<Sender>;
 
-    template<typename Sender, typename Receiver>
-    struct is_sender_to : std::bool_constant<sender_to<Sender, Receiver>>
+    template<typename Sender, typename Env = empty_env>
+    struct is_sender : std::bool_constant<is_sender_v<Sender>>
     {
     };
 
     template<typename Sender, typename Receiver>
-    inline constexpr bool is_sender_to_v = is_sender_to<Sender, Receiver>::value;
+    inline constexpr bool is_sender_to_v = sender_to<Sender, Receiver>;
 
+    template<typename Sender, typename Receiver>
+    struct is_sender_to : std::bool_constant<is_sender_to_v<Sender, Receiver>>
+    {
+    };
 
     namespace detail {
-
         // Dummy type used in place of a scheduler if none is given
         struct no_scheduler
         {
