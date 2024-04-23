6d6af074d3bed0277f46ac91e3dd8adf8f422f8b
Christoper Taylor <ct.clmsn@gmail.com>
2024-04-23 10:40:23 -0400

    implement wait loop in case MPI openshmem is installed

--- libs/core/openshmem_base/src/openshmem_environment.cpp
+++ libs/core/openshmem_base/src/openshmem_environment.cpp
@@ -314,7 +314,13 @@ namespace hpx::util {
     void openshmem_environment::wait_until(
         const std::uint8_t value, std::uint8_t * sigaddr)
     {
-        shmem_int_wait_until(reinterpret_cast<volatile unsigned int*>(sigaddr), SHMEM_CMP_EQ, static_cast<unsigned int>(value));
+        union {
+            std::uint8_t * uaddr;
+            volatile int * iaddr;
+        } tmp;
+        tmp.uaddr = sigaddr;
+        volatile int * ptr = tmp.iaddr;
+        shmem_int_wait_until(ptr, SHMEM_CMP_EQ, static_cast<int>(value));
     }
 
     std::size_t wait_until_any(const std::uint8_t value, std::uint8_t * sigaddr, const std::size_t count) {
