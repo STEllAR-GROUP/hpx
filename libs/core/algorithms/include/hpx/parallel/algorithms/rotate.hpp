40285e1fed3069544f2e2ed35eb4c9e49e3f74c7
isidorostsa <tsa.isidoros@gmail.com>
2024-05-23 14:53:25 -0500

    Revert rotate.hpp accidental delete

--- libs/core/algorithms/include/hpx/parallel/algorithms/rotate.hpp
+++ libs/core/algorithms/include/hpx/parallel/algorithms/rotate.hpp
@@ -4,7 +4,8 @@
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
+/// \page hpx::rotate, hpx::rotate_copy
+/// \headerfile hpx/algorithm.hpp
 /// \file hpx/parallel/algorithms/rotate.hpp
 
 #pragma once
