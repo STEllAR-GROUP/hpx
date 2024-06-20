037979998f234752ed6e5ac95deb076f0b661d16
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2024-06-20 10:12:49 -0500

    Adding missing env files, changing DGX to gcc 13

--- .jenkins/lsu/env-gcc-13-cuda-12-dgx.sh
+++ .jenkins/lsu/env-gcc-13-cuda-12-dgx.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 Hartmut Kaiser
+# Copyright (c) 2022-2024 Hartmut Kaiser
 # Copyright (c) 2024 Alireza Kheirkhahan
 #
 # SPDX-License-Identifier: BSL-1.0
@@ -7,7 +7,7 @@
 
 module purge
 module load cmake
-module load gcc/14
+module load gcc/13
 module load cuda/12
 module load boost/1.85.0-${build_type,,}
 module load hwloc
