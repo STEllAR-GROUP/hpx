7844e77f8ed3efe6bddbcdd77c26424568fc1019
dysrhythmic <jaketucker7@gmail.com>
2024-09-01 15:08:33 -0500

    debug constructor issue

--- libs/core/concurrency/include/hpx/concurrency/detail/uint128_atomic.hpp
+++ libs/core/concurrency/include/hpx/concurrency/detail/uint128_atomic.hpp
@@ -10,13 +10,18 @@
 
 #include <atomic>
 
-/* Note: currently only supported on Linux */
+/* Note: currently only supported on Clang */
 #if !defined(HPX_WITH_CXX11_ATOMIC_128BIT_LOCKFREE) && defined(__clang__)
 
 namespace hpx::lockfree {
     struct uint128_atomic : std::atomic<uint128_type>
     {
         using std::atomic<uint128_type>::atomic;
+
+        constexpr uint128_atomic(std::size_t l, std::size_t r) noexcept
+        : std::atomic<uint128_type>(uint128_type{l, r})
+        {
+        }
 
         bool is_lock_free() const noexcept
         {
@@ -59,5 +64,12 @@ namespace hpx::lockfree {
             return result;
         }
     };
+}    // namespace hpx::lockfree
+
+#else
+
+namespace hpx::lockfree {
+    /* Fallback to aliasing std implementation if custom implementation is not supported */
+    using uint128_atomic = std::atomic<uint128_type>;
 }    // namespace hpx::lockfree
 #endif    // !defined(HPX_WITH_CXX11_ATOMIC_128BIT_LOCKFREE) && defined(__linux__)
