aa4bdeb31fcfd0f954e24f5a5baf478be74eb890
isidorostsa <tsa.isidoros@gmail.com>
2024-12-20 10:22:21 +0200

    Editorial

--- libs/core/coroutines/include/hpx/coroutines/detail/get_stack_pointer.hpp
+++ libs/core/coroutines/include/hpx/coroutines/detail/get_stack_pointer.hpp
@@ -1,5 +1,6 @@
 //  Copyright (c) 2013-2016 Thomas Heller
 //  Copyright (c) 2022 Christopher Taylor
+//  Copyright (c) 2024 Isidoros Tsaousis-Seiras
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0.
