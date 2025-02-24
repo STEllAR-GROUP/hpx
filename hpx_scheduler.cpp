5b03133a04bf8afb2f35e9c63ef55b3fcdc6bf5f
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-02-24 21:15:18 +0530

    Create hpx_scheduler.cpp

---
+++ hpx_scheduler.cpp
@@ -0,0 +1,21 @@
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/async.hpp>
+#include <iostream>
+#include <vector>
+
+void execute_task(int task_id) {
+    std::cout << "Executing Task " << task_id 
+              << " on thread: " << hpx::get_worker_thread_num() << std::endl;
+    hpx::this_thread::sleep_for(std::chrono::seconds(2));
+}
+
+int main() {
+    const int num_tasks = 5;
+    std::vector<hpx::future<void>> tasks;
+    for (int i = 0; i < num_tasks; i++) {
+        tasks.push_back(hpx::async(&execute_task, i));
+    }
+    hpx::wait_all(tasks);
+    std::cout << "All tasks completed." << std::endl;
+    return 0;
+}
