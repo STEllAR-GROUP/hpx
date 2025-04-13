ea447abc46a5180dbf858b6b907ce4502e5a3f74
Sai Charan Arvapally <saicharan@Sais-MacBook-Pro.local>
2025-04-13 06:52:02 -0600

    Implement P2079R7  parallel_scheduler in HPX using wrapper
    Signed-off-by: Sai Charan Arvapally <saicharan@Sais-MacBook-Pro.local>

---
+++ libs/core/executors/src/experimental/parallel_scheduler.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Sai Charan Arvapally
+//
+// SPDX-License-Identifier: BSL-1.0
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/config.hpp>
+#include <hpx/executors/experimental/parallel_scheduler.hpp>
+
+namespace hpx::execution::experimental {
+    // Non-inline implementations can be added here in future phases
+}    // namespace hpx::execution::experimental
