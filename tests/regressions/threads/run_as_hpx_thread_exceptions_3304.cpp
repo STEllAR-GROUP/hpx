a28e397a0f81fb4ab2547e150519a2ad73c3fc4f
Agustin Berge <agustinberge@gmail.com>
2025-04-15 10:07:19 -0100

    Adopt manage_runtime in examples and tests

--- tests/regressions/threads/run_as_hpx_thread_exceptions_3304.cpp
+++ tests/regressions/threads/run_as_hpx_thread_exceptions_3304.cpp
@@ -16,30 +16,6 @@
 #include <mutex>
 #include <thread>
 
-std::mutex startup_mtx;
-std::condition_variable startup_cond;
-
-bool running = false;
-bool stop_running = false;
-
-int start_func(hpx::spinlock& mtx, hpx::condition_variable_any& cond)
-{
-    // Signal to constructor that thread has started running.
-    {
-        std::lock_guard<std::mutex> lk(startup_mtx);
-        running = true;
-    }
-
-    {
-        std::unique_lock<hpx::spinlock> lk(mtx);
-        startup_cond.notify_one();
-        while (!stop_running)
-            cond.wait(lk);
-    }
-
-    return hpx::finalize();
-}
-
 void hpx_thread_func()
 {
     HPX_THROW_EXCEPTION(hpx::error::invalid_status, "hpx_thread_func", "test");
@@ -47,20 +23,7 @@ void hpx_thread_func()
 
 int main(int argc, char** argv)
 {
-    hpx::spinlock mtx;
-    hpx::condition_variable_any cond;
-
-    hpx::function<int(int, char**)> start_function =
-        hpx::bind(&start_func, std::ref(mtx), std::ref(cond));
-
-    hpx::start(start_function, argc, argv);
-
-    // wait for the main HPX thread to run
-    {
-        std::unique_lock<std::mutex> lk(startup_mtx);
-        while (!running)
-            startup_cond.wait(lk);
-    }
+    hpx::manage_runtime rt(argc, argv);
 
     bool exception_caught = false;
     try
@@ -74,12 +37,5 @@ int main(int argc, char** argv)
     }
     HPX_TEST(exception_caught);
 
-    {
-        std::lock_guard<hpx::spinlock> lk(mtx);
-        stop_running = true;
-    }
-
-    cond.notify_one();
-
-    return hpx::stop();
+    return 0;
 }
