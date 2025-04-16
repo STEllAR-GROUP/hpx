53d5cfeda5414415a04939457897b2d04c8ebbc3
Agustin Berge <agustinberge@gmail.com>
2025-04-16 10:02:40 +0000

    Add test

--- tests/regressions/threads/run_as_hpx_thread_exceptions_3304.cpp
+++ tests/regressions/threads/run_as_hpx_thread_exceptions_3304.cpp
@@ -4,17 +4,10 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/condition_variable.hpp>
 #include <hpx/exception.hpp>
-#include <hpx/functional.hpp>
 #include <hpx/init.hpp>
 #include <hpx/modules/testing.hpp>
-#include <hpx/mutex.hpp>
 #include <hpx/runtime_local/run_as_hpx_thread.hpp>
-
-#include <functional>
-#include <mutex>
-#include <thread>
 
 void hpx_thread_func()
 {
