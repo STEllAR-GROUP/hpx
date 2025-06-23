e2f216c9fc91563161dcae0b1186c0be5da15092
Xiomara <puma.torres.xiomara@gmail.com>
2025-06-23 13:25:23 -0500

    Implementación de run_on_all prueba - Issue #6651

--- libs/core/algorithms/examples/run_on_all.cpp
+++ libs/core/algorithms/examples/run_on_all.cpp
@@ -1,51 +1,13 @@
-//  Copyright (c) 2025 Hartmut Kaiser
-//
-//  SPDX-License-Identifier: BSL-1.0
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
 #include <hpx/experimental/run_on_all.hpp>
 #include <hpx/hpx_main.hpp>
 #include <hpx/modules/runtime_local.hpp>
-#include <hpx/modules/synchronization.hpp>
-
-#include <cstdlib>
-#include <iostream>
-#include <mutex>
-#include <thread>
-
-int main(int argc, char* argv[])
+int main()
 {
-    int num_threads = 4;
-    if (argc >= 2)
-    {
-        num_threads = std::atoi(argv[1]);
-    }
-
-    constexpr char const* delim =
-        "================================================================\n";
-    std::cout << delim;
-    std::cout << "hpx::run_on_all (using " << num_threads << " threads)\n";
-    std::cout << delim;
-
-    std::cout << "std::hardware_concurrency()   = "
-              << std::thread::hardware_concurrency() << "\n";
-    std::cout << "hpx::get_num_worker_threads() = "
-              << hpx::get_num_worker_threads() << "\n";
-    std::cout << delim;
-
-    hpx::mutex mtx;
-    hpx::experimental::run_on_all(
-        num_threads,    // use num_threads concurrent threads to execute the lambda
-        [&] {
-            std::lock_guard l(mtx);
-            std::cout << "Hello! I am thread " << hpx::get_worker_thread_num()
-                      << " of " << hpx::get_num_worker_threads() << "\n";
-            std::cout << "My C++ std::thread id is "
-                      << std::this_thread::get_id() << "\n";
-        });
-
-    std::cout << delim;
-
+    hpx::experimental::run_on_all([](std::size_t thread_index) {
+        std::cout << "Hola desde tarea " << thread_index
+                  << " ejecutandose en el hilo HPX " << hpx::get_worker_thread_num()
+                  << std::endl;
+    });
+    
     return 0;
 }
--- libs/core/algorithms/include/hpx/parallel/run_on_all.hpp
+++ libs/core/algorithms/include/hpx/parallel/run_on_all.hpp
@@ -1,88 +1,37 @@
-//  Copyright (c) 2025 Hartmut Kaiser
-//
-//  SPDX-License-Identifier: BSL-1.0
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-/// \file run_on_all.hpp
-/// \page hpx::experimental::run_on_all
-/// \headerfile hpx/experimental/run_on_all.hpp
 
 #pragma once
 
-#include <hpx/config.hpp>
-#include <hpx/async_combinators/wait_all.hpp>
-#include <hpx/concepts/concepts.hpp>
-#include <hpx/execution/detail/execution_parameter_callbacks.hpp>
-#include <hpx/execution/execution.hpp>
-#include <hpx/execution_base/execution.hpp>
-#include <hpx/executors/parallel_executor.hpp>
-#include <hpx/functional/experimental/scope_exit.hpp>
-#include <hpx/parallel/algorithms/for_loop_reduction.hpp>
-
-#include <cstddef>
-#include <type_traits>
+#include <hpx/hpx.hpp>
+#include <hpx/include/async.hpp>
+#include <hpx/include/lcos.hpp>
+#include <iostream>
+#include <vector>
 
 namespace hpx::experimental {
 
-    template <typename T, typename Op, typename F, typename... Ts>
-    void run_on_all(std::size_t num_tasks,
-        hpx::parallel::detail::reduction_helper<T, Op>&& r, F&& f, Ts&&... ts)
+    // Ejecuta una función N veces en paralelo usando hpx::async
+    template <typename F>
+    void run_on_all(std::size_t num_tasks, F&& f)
     {
-        // force using index_queue scheduler with given amount of threads
-        hpx::threads::thread_schedule_hint hint;
-        hint.sharing_mode(
-            hpx::threads::thread_sharing_hint::do_not_share_function);
-        auto exec = hpx::execution::experimental::with_processing_units_count(
-            hpx::execution::parallel_executor(
-                hpx::threads::thread_priority::bound,
-                hpx::threads::thread_stacksize::default_, hint),
-            num_tasks);
-        exec.set_hierarchical_threshold(0);
+        std::vector<hpx::future<void>> futures;
 
-        r.init_iteration(0, 0);
-        auto on_exit =
-            hpx::experimental::scope_exit([&] { r.exit_iteration(0); });
+        for (std::size_t i = 0; i < num_tasks; ++i)
+        {
+            futures.push_back(hpx::async([=]() {
+                f(i);    // Pasamos el índice como argumento
+            }));
+        }
 
-        hpx::wait_all(hpx::parallel::execution::bulk_async_execute(
-            exec, [&](auto i) { f(r.iteration_value(i), ts...); }, num_tasks,
-            HPX_FORWARD(Ts, ts)...));
+        hpx::wait_all(futures);    // Esperamos a que terminen todas las tareas
     }
 
-    template <typename T, typename Op, typename F, typename... Ts>
-    void run_on_all(
-        hpx::parallel::detail::reduction_helper<T, Op>&& r, F&& f, Ts&&... ts)
+    // Sobrecarga para usar todos los hilos disponibles automáticamente
+    template <typename F>
+    void run_on_all(F&& f)
     {
-        std::size_t cores =
-            hpx::parallel::execution::detail::get_os_thread_count();
-        run_on_all(
-            cores, HPX_MOVE(r), HPX_FORWARD(F, f), HPX_FORWARD(Ts, ts)...);
+        std::size_t num_threads = hpx::get_num_worker_threads();
+        run_on_all(num_threads, HPX_FORWARD(F, f));
     }
 
-    template <typename F, typename... Ts>
-    void run_on_all(std::size_t num_tasks, F&& f, Ts&&... ts)
-    {
-        // force using index_queue scheduler with given amount of threads
-        hpx::threads::thread_schedule_hint hint;
-        hint.sharing_mode(
-            hpx::threads::thread_sharing_hint::do_not_share_function);
-        auto exec = hpx::execution::experimental::with_processing_units_count(
-            hpx::execution::parallel_executor(
-                hpx::threads::thread_priority::bound,
-                hpx::threads::thread_stacksize::default_, hint),
-            num_tasks);
-        exec.set_hierarchical_threshold(0);
+}
 
-        hpx::wait_all(hpx::parallel::execution::bulk_async_execute(
-            exec, [&](auto) { f(ts...); }, num_tasks, HPX_FORWARD(Ts, ts)...));
-    }
-
-    template <typename F, typename... Ts,
-        HPX_CONCEPT_REQUIRES_(std::is_invocable_v<F&&, Ts&&...>)>
-    void run_on_all(F&& f, Ts&&... ts)
-    {
-        std::size_t cores =
-            hpx::parallel::execution::detail::get_os_thread_count();
-        run_on_all(cores, HPX_FORWARD(F, f), HPX_FORWARD(Ts, ts)...);
-    }
-}    // namespace hpx::experimental
