ef753c16f7601e13d241a6bc881e1e35607ca1ff
Sai Charan Arvapally <saicharan@Sais-MacBook-Pro.local>
2025-07-02 03:37:11 -0600

    fixing compilation errors in thread_pool_scheduler

--- libs/core/executors/include/hpx/executors/thread_pool_scheduler.hpp
+++ libs/core/executors/include/hpx/executors/thread_pool_scheduler.hpp
@@ -288,7 +288,9 @@ namespace hpx::execution::experimental {
         struct sender
         {
             HPX_NO_UNIQUE_ADDRESS std::decay_t<Scheduler> scheduler;
-            using sender_concept = ::hpx::execution::experimental::sender_t;
+            #if defined(HPX_HAVE_STDEXEC)
+            using sender_concept = hpx::execution::experimental::sender_t;
+            #endif
             using completion_signatures =
                 hpx::execution::experimental::completion_signatures<
                     hpx::execution::experimental::set_value_t(),
@@ -334,7 +336,11 @@ namespace hpx::execution::experimental {
                     hpx::execution::experimental::get_stop_token_t,
                     [[maybe_unused]] env const& e) noexcept
                 {
-                    return ::hpx::execution::experimental::inplace_stop_token{};
+                    #if defined(HPX_HAVE_STDEXEC)
+                    return hpx::execution::experimental::inplace_stop_token{};
+                    #else
+                    return hpx::execution::experimental::never_stop_token{};
+                    #endif
                 }
             };
 
@@ -556,7 +562,9 @@ namespace hpx::execution::experimental {
     struct parallel_scheduler_sender
     {
         parallel_scheduler scheduler;
-        using sender_concept = ::hpx::execution::experimental::sender_t;
+        #if defined(HPX_HAVE_STDEXEC)
+        using sender_concept = hpx::execution::experimental::sender_t;
+        #endif
         using completion_signatures =
             hpx::execution::experimental::completion_signatures<
                 hpx::execution::experimental::set_value_t(),
@@ -605,7 +613,11 @@ namespace hpx::execution::experimental {
                 hpx::execution::experimental::get_stop_token_t,
                 [[maybe_unused]] env const& e) noexcept
             {
-                return ::hpx::execution::experimental::inplace_stop_token{};
+                #if defined(HPX_HAVE_STDEXEC)
+                return hpx::execution::experimental::inplace_stop_token{};
+                #else
+                return hpx::execution::experimental::never_stop_token{};
+                #endif
             }
         };
 
