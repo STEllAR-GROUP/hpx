302dfa0fbfeab879c80fe9da1214d2fe73cd353d
Aditya Sapra <adityaework@gmail.com>
2025-08-20 12:46:44 -0500

    Async execution policy and refactor
    Add par_nosync execution policy support which allows async execution , leveraging exiting async_cuda infrastructure and add tag invoke policy branching
    
    Signed-off-by: Aditya Sapra <adityaework@gmail.com>
    Signed-off-by: ArivoliR <arivoli2005@gmail.com>

--- libs/core/async_cuda/include/hpx/async_cuda/thrust/algorithms.hpp
+++ libs/core/async_cuda/include/hpx/async_cuda/thrust/algorithms.hpp
@@ -3,37 +3,49 @@
 
 #pragma once
 
+#include <hpx/async_cuda/thrust/detail/algorithm_map.hpp>
 #include <hpx/async_cuda/thrust/policy.hpp>
+#include <hpx/concepts/concepts.hpp>
+#include <hpx/functional/tag_invoke.hpp>
 #include <hpx/parallel/algorithms/fill.hpp>
 #include <hpx/parallel/algorithms/for_each.hpp>
-#include <hpx/functional/tag_invoke.hpp>
-#include <hpx/async_cuda/thrust/detail/algorithm_map.hpp>
-#include <hpx/concepts/concepts.hpp>
 #include <cuda_runtime.h>
 
 #include <iostream>
 #include <type_traits>
 
-namespace hpx {
-namespace async_cuda {
-namespace thrust {
+namespace hpx { namespace async_cuda { namespace thrust {
 
-template<typename HPXTag, typename ThrustPolicy, typename... Args,
-    HPX_CONCEPT_REQUIRES_(
-        is_thrust_execution_policy_v<std::decay_t<ThrustPolicy>>
-    )>
-auto tag_invoke(HPXTag tag, ThrustPolicy&& policy, Args&&... args) 
-    -> decltype(detail::algorithm_map<HPXTag>::invoke(std::forward<ThrustPolicy>(policy), std::forward<Args>(args)...)) {
-    
-    
-    // Universal dispatch to the mapped Thrust function
-    // This calls detail::algorithm_map<HPXTag>::invoke(policy, args...)
-    // which in turn calls the appropriate ::thrust::algorithm(policy.get(), args...)
-    return detail::algorithm_map<HPXTag>::invoke(
-        std::forward<ThrustPolicy>(policy), 
-        std::forward<Args>(args)...
-    );
-}
-} // namespace thrust
-} // namespace async_cuda
-} // namespace hpx
+    template <typename HPXTag, typename ThrustPolicy, typename... Args,
+        HPX_CONCEPT_REQUIRES_(
+            is_thrust_execution_policy_v<std::decay_t<ThrustPolicy>>),
+        typename = detail::is_algorithm_mapped<HPXTag, ThrustPolicy, Args...>>
+    auto tag_invoke(HPXTag tag, ThrustPolicy&& policy, Args&&... args)
+    {
+        if constexpr (hpx::detail::is_async_execution_policy<
+                          std::decay_t<ThrustPolicy>>::value)
+        {
+            if constexpr (std::is_void_v<decltype(detail::algorithm_map<
+                              HPXTag>::invoke(std::declval<ThrustPolicy>(),
+                              std::declval<Args>()...))>)
+            {
+                detail::algorithm_map<HPXTag>::invoke(
+                    policy, std::forward<Args>(args)...);
+                return policy.get_future();    // future<void>
+            }
+            else
+            {
+                auto result = detail::algorithm_map<HPXTag>::invoke(
+                    policy, std::forward<Args>(args)...);
+                return policy.get_future().then([result](auto&&) mutable {
+                    return result;
+                });    // future<T>
+            }
+        }
+        else
+        {
+            return detail::algorithm_map<HPXTag>::invoke(
+                policy, std::forward<Args>(args)...);
+        }
+    }
+}}}    // namespace hpx::async_cuda::thrust
