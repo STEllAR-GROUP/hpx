9201a1898aba44ef6f9b4f4c951fcff3c5115653
Haokun Wu <kylewhk99@gmail.com>
2025-08-22 14:46:16 -0500

    Extend inspect checks to cover .ixx module interface files
    Signed-off-by: Haokun Wu <kylewhk99@gmail.com>

--- tools/inspect/include_check.cpp
+++ tools/inspect/include_check.cpp
@@ -301,6 +301,7 @@ namespace boost { namespace inspect {
         register_signature(".hxx");
         register_signature(".inc");
         register_signature(".ipp");
+        register_signature(".ixx");
 
         for (names_includes const* names_it = &names[0];
             names_it->name_regex != nullptr; ++names_it)
