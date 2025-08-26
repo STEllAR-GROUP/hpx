fb142c550724530c218598f894bac69e44543e41
David Hua <tkrd1205@gmail.com>
2025-08-26 21:39:27 +1000

    Added copyright
    Signed-off-by: David Hua <tkrd1205@gmail.com>

--- libs/core/algorithms/tests/unit/algorithms/uninitialized_default_constructn_sender.cpp
+++ libs/core/algorithms/tests/unit/algorithms/uninitialized_default_constructn_sender.cpp
@@ -1,3 +1,9 @@
+//  Copyright (c) 2025 Zhengnan Hua
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <hpx/algorithm.hpp>
 #include <hpx/execution_base/stdexec_forward.hpp>
 #include <hpx/init.hpp>
