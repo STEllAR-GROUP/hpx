14d8e1daf03ded6d668c0891a3b2fb0fae740090
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2025-08-28 09:12:05 -0500

    Cleaning up macro names and usage required in the context of C++ modules
    Signed-off-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>

--- libs/core/version/include/hpx/version/api.hpp
+++ libs/core/version/include/hpx/version/api.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2007-2023 Hartmut Kaiser
+//  Copyright (c) 2007-2025 Hartmut Kaiser
 //  Copyright (c)      2011 Bryce Lelbach
 //  Copyright (c)      2013 Adrian Serio
 //
@@ -17,56 +17,56 @@
 namespace hpx {
 
     // Returns the major HPX version.
-    HPX_NODISCARD_CORE std::uint8_t major_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::uint8_t major_version();
 
     // Returns the minor HPX version.
-    HPX_NODISCARD_CORE std::uint8_t minor_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::uint8_t minor_version();
 
     // Returns the sub-minor/patch-level HPX version.
-    HPX_NODISCARD_CORE std::uint8_t subminor_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::uint8_t subminor_version();
 
     // Returns the full HPX version.
-    HPX_NODISCARD_CORE std::uint32_t full_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::uint32_t full_version();
 
     // Returns the full HPX version.
-    HPX_NODISCARD_CORE std::string full_version_as_string();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string full_version_as_string();
 
     // Returns the AGAS subsystem version.
-    HPX_NODISCARD_CORE std::uint8_t agas_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::uint8_t agas_version();
 
     // Returns the tag.
-    HPX_NODISCARD_CORE std::string tag();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string tag();
 
     // Returns the HPX full build information string.
-    HPX_NODISCARD_CORE std::string full_build_string();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string full_build_string();
 
     // Returns the HPX version string.
-    HPX_NODISCARD_CORE std::string build_string();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string build_string();
 
     // Returns the Boost version string.
-    HPX_NODISCARD_CORE std::string boost_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string boost_version();
 
     // Returns the Boost platform string.
-    HPX_NODISCARD_CORE std::string boost_platform();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string boost_platform();
 
     // Returns the Boost compiler string.
-    HPX_NODISCARD_CORE std::string boost_compiler();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string boost_compiler();
 
     // Returns the Boost standard library string.
-    HPX_NODISCARD_CORE std::string boost_stdlib();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string boost_stdlib();
 
     // Returns the copyright string.
-    HPX_NODISCARD_CORE std::string copyright();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string copyright();
 
     // Returns the full version string.
-    HPX_NODISCARD_CORE std::string complete_version();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string complete_version();
 
     // Returns the HPX build type ('Debug', 'Release', etc.)
-    HPX_NODISCARD_CORE std::string build_type();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string build_type();
 
     // Returns the HPX build date and time
-    HPX_NODISCARD_CORE std::string build_date_time();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string build_date_time();
 
     // Return the HPX configuration information
-    HPX_NODISCARD_CORE std::string configuration_string();
+    HPX_CORE_MODULE_EXPORT_NODISCARD std::string configuration_string();
 }    // namespace hpx
