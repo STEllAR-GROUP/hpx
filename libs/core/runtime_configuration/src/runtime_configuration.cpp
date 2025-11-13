9f311b14e4c7961cc2923547c4908ba5b9573c47
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2025-11-13 13:01:00 -0600

    Adapting HPX modules from level 12 to C++ modules
    - flyby: fixing propagation of SCAN_FOR_MODULES to HPX targets
    
    Signed-off-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>

--- libs/core/runtime_configuration/src/runtime_configuration.cpp
+++ libs/core/runtime_configuration/src/runtime_configuration.cpp
@@ -14,7 +14,6 @@
 #include <hpx/modules/preprocessor.hpp>
 #include <hpx/modules/string_util.hpp>
 #include <hpx/modules/util.hpp>
-#include <hpx/prefix/find_prefix.hpp>
 #include <hpx/runtime_configuration/agas_service_mode.hpp>
 #include <hpx/runtime_configuration/component_registry_base.hpp>
 #include <hpx/runtime_configuration/init_ini_data.hpp>
