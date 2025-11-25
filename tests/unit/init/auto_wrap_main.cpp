c7b18a03cd72c59a40e87cf90081978aeb22591f
Bharath <bharath.kollanur@gmail.com>
2025-11-25 15:30:51 -0600

    Removed hpx_auto_wrap.cpp and included HPX_AUTO_WRAP_MAIN_ACTIVATE functionality in the hpx_wrap.cpp which is defined to the target the with the help of target_compile_definitions in the CMakelists.txt file
    Signed-off-by: Bharath <bharath.kollanur@gmail.com>
    Signed-off-by: ArivoliR <arivoli2005@gmail.com>

--- tests/unit/init/auto_wrap_main.cpp
+++ tests/unit/init/auto_wrap_main.cpp
@@ -8,15 +8,15 @@
 #include <hpx/modules/testing.hpp>
 #include <hpx/runtime_local/runtime_local.hpp>
 
-int main(int, char**) {
+int main(int, char**)
+{
     // The wrapper should have initialized HPX runtime before reaching main.
     HPX_TEST(hpx::get_runtime_ptr() != nullptr);
 
-    // Verify that calling thread is an HPX thread by launching another 
+    // Verify that calling thread is an HPX thread by launching another
     // HPX task and waiting for its result.
-    auto f = hpx::async([] { return (hpx::get_runtime_ptr() != nullptr);});
+    auto f = hpx::async([] { return (hpx::get_runtime_ptr() != nullptr); });
     HPX_TEST(f.get());
 
     return hpx::util::report_errors();
-
 }
