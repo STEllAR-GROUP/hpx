453163f24ec2a39aff338ed0decbc2b69dce9ced
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2025-12-01 11:07:58 -0600

    Enable compiling examples and tests using C++ modules
    - flyby: fixing contract tests
    
    Signed-off-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>
    Signed-off-by: ArivoliR <arivoli2005@gmail.com>

--- libs/core/program_options/include/hpx/program_options/detail/value_semantic.hpp
+++ libs/core/program_options/include/hpx/program_options/detail/value_semantic.hpp
@@ -169,7 +169,7 @@ namespace hpx::program_options {
         v = hpx::any_nonser(hpx::optional<T>(hpx::any_cast<T>(a)));
     }
 
-    HPX_CXX_EXPORT template <typename T, typename Char>
+    template <typename T, typename Char>
     void typed_value<T, Char>::xparse(hpx::any_nonser& value_store,
         std::vector<std::basic_string<Char>> const& new_tokens) const
     {
