7d43fe65016eef3b9275c9fab6229cde5453541b
Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>
2026-01-24 05:38:59 +0000

    struct serialization doesnt trigger when reflection is enabled. more issues in AoV an VoA, need to dive deeper into this, something is structurally deficient. Nested templ args testing has no issues. Can move on to merging with latest upstream.
    Signed-off-by: Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>

--- libs/core/serialization/tests/unit/refl/reflection_containers.cpp
+++ libs/core/serialization/tests/unit/refl/reflection_containers.cpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2026 Ujjwal Shekhar
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <hpx/config.hpp>
 #include <hpx/modules/serialization.hpp>
 #include <hpx/modules/testing.hpp>
