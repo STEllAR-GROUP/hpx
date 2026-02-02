26884750ad8de75d76498ed16d4c8c296b6784d7
guptapratykshh <pratykshgupta9999@gmail.com>
2026-02-02 12:39:52 +0530

    update relocation semantics to match p2786r13
    Signed-off-by: guptapratykshh <pratykshgupta9999@gmail.com>

---
+++ libs/core/type_support/include/hpx/type_support/is_replaceable.hpp
@@ -0,0 +1,26 @@
+//  Copyright (c) 2025 Isidoros Tsaousis-Seiras
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#include <type_traits>
+
+namespace hpx::experimental {
+
+    HPX_CXX_EXPORT template <typename T>
+    struct is_replaceable
+      : std::bool_constant<std::is_object_v<T> &&
+            std::is_move_constructible_v<T> && std::is_move_assignable_v<T> &&
+            std::is_destructible_v<T> && !std::is_volatile_v<T>>
+    {
+    };
+
+    HPX_CXX_EXPORT template <typename T>
+    inline constexpr bool is_replaceable_v = is_replaceable<T>::value;
+
+}    // namespace hpx::experimental
