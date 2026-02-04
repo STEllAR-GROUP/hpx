9a8c3bcc54c0bf489a34bca269e28720e8c579a1
Bhoomish <bhoomishguptabits@gmail.com>
2026-02-04 20:09:51 +0000

    Fixed Iterator Category Mismatch
    Signed-off-by: Bhoomish <bhoomishguptabits@gmail.com>

--- libs/core/algorithms/tests/regressions/reduce_6647.cpp
+++ libs/core/algorithms/tests/regressions/reduce_6647.cpp
@@ -1,9 +1,6 @@
 //  Copyright (c) 2026 Bhoomish Gupta
-//
-<<<<<<< HEAD
 //  SPDX - License - Identifier : BSL - 1.0
-=======
->>>>>>> 7cd0cb47ab (Fix bug #6647: Correct type handling in reduce)
+//
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
