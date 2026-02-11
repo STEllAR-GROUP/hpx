0d73ed977b2a7d469ce83b82058037059997d4ab
Bhoomish <bhoomishguptabits@gmail.com>
2026-02-11 00:32:36 +0000

    Fixed namespace error in previous commit
    Signed-off-by: Bhoomish <bhoomishguptabits@gmail.com>

--- libs/core/algorithms/include/hpx/parallel/container_algorithms/reduce.hpp
+++ libs/core/algorithms/include/hpx/parallel/container_algorithms/reduce.hpp
@@ -804,7 +804,7 @@ namespace hpx::ranges {
             using value_type =
                 typename std::iterator_traits<FwdIter>::value_type;
 
-            return hpx::parallel::reduce<value_type>().call(
+            return hpx::parallel::detail::reduce<value_type>().call(
                 HPX_FORWARD(ExPolicy, policy), first, last, value_type{},
                 std::plus<value_type>{});
         }
@@ -829,7 +829,7 @@ namespace hpx::ranges {
             static_assert(hpx::traits::is_forward_iterator_v<iterator_type>,
                 "Requires at least forward iterator.");
 
-            return hpx::parallel::reduce<value_type>().call(
+            return hpx::parallel::detail::reduce<value_type>().call(
                 HPX_FORWARD(ExPolicy, policy), hpx::util::begin(rng),
                 hpx::util::end(rng), value_type{}, std::plus<value_type>{});
         }
@@ -874,8 +874,8 @@ namespace hpx::ranges {
             static_assert(hpx::traits::is_input_iterator_v<FwdIter>,
                 "Requires at least input iterator.");
 
-            return hpx::parallel::detail::reduce<T>().call(hpx::execution::seq, first,
-                last, HPX_MOVE(init), std::plus<T>{});
+            return hpx::parallel::detail::reduce<T>().call(hpx::execution::seq,
+                first, last, HPX_MOVE(init), std::plus<T>{});
         }
 
         template <typename Rng,
@@ -904,8 +904,9 @@ namespace hpx::ranges {
             using value_type =
                 typename std::iterator_traits<FwdIter>::value_type;
 
-            return hpx::parallel::reduce<value_type>().call(hpx::execution::seq,
-                first, last, value_type{}, std::plus<value_type>{});
+            return hpx::parallel::detail::reduce<value_type>().call(
+                hpx::execution::seq, first, last, value_type{},
+                std::plus<value_type>{});
         }
 
         template <typename Rng>
@@ -922,9 +923,9 @@ namespace hpx::ranges {
             static_assert(hpx::traits::is_input_iterator_v<iterator_type>,
                 "Requires at least input iterator.");
 
-            return hpx::parallel::reduce<value_type>().call(hpx::execution::seq,
-                hpx::util::begin(rng), hpx::util::end(rng), value_type{},
-                std::plus<value_type>{});
+            return hpx::parallel::detail::reduce<value_type>().call(
+                hpx::execution::seq, hpx::util::begin(rng), hpx::util::end(rng),
+                value_type{}, std::plus<value_type>{});
         }
     } reduce{};
 }    // namespace hpx::ranges
