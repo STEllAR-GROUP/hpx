4e6a077de6cea1d044b7b2edeb0f43050fbc2630
BhoomishGupta <bhoomishguptabits@gmail.com>
2026-02-24 18:35:53 +0000

    Add rebind_executor_parameters functionality and update related includes
    Signed-off-by: BhoomishGupta <bhoomishguptabits@gmail.com>

--- libs/core/algorithms/include/hpx/parallel/util/detail/chunk_size.hpp
+++ libs/core/algorithms/include/hpx/parallel/util/detail/chunk_size.hpp
@@ -18,6 +18,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -150,12 +151,10 @@ namespace hpx::parallel::util::detail {
             hpx::execution::experimental::get_chunk_size(policy.parameters(),
                 policy.executor(), hpx::chrono::null_duration, cores, count);
 
-        auto [adj_chunk, adj_max] =
+        std::tie(chunk_size, max_chunks) =
             hpx::execution::experimental::adjust_chunk_size_and_max_chunks(
                 policy.parameters(), policy.executor(), count, cores,
                 max_chunks, chunk_size);
-        chunk_size = adj_chunk;
-        max_chunks = adj_max;
 
         auto last = next_or_subrange(it_or_r, count, 0);
         Stride stride = parallel::detail::abs(s);
@@ -236,12 +235,10 @@ namespace hpx::parallel::util::detail {
             hpx::execution::experimental::get_chunk_size(policy.parameters(),
                 policy.executor(), iteration_duration, cores, count);
 
-        auto [adj_chunk, adj_max] =
+        std::tie(chunk_size, max_chunks) =
             hpx::execution::experimental::adjust_chunk_size_and_max_chunks(
                 policy.parameters(), policy.executor(), count, cores,
                 max_chunks, chunk_size);
-        chunk_size = adj_chunk;
-        max_chunks = adj_max;
 
         auto last = next_or_subrange(it_or_r, count, 0);
 
@@ -308,8 +305,10 @@ namespace hpx::parallel::util::detail {
                     hpx::chrono::null_duration, cores, count);
 
             // make sure, chunk size and max_chunks are consistent
-            adjust_chunk_size_and_max_chunks(
-                cores, count, max_chunks, chunk_size, true);
+            std::tie(chunk_size, max_chunks) =
+                hpx::execution::experimental::adjust_chunk_size_and_max_chunks(
+                    policy.parameters(), policy.executor(), count, cores,
+                    max_chunks, chunk_size);
 
             if (stride != 1)
             {
@@ -419,12 +418,10 @@ namespace hpx::parallel::util::detail {
             hpx::execution::experimental::get_chunk_size(policy.parameters(),
                 policy.executor(), hpx::chrono::null_duration, cores, count);
 
-        auto [adj_chunk, adj_max] =
+        std::tie(chunk_size, max_chunks) =
             hpx::execution::experimental::adjust_chunk_size_and_max_chunks(
                 policy.parameters(), policy.executor(), count, cores,
                 max_chunks, chunk_size);
-        chunk_size = adj_chunk;
-        max_chunks = adj_max;
 
         if (stride != 1)
         {
@@ -514,12 +511,10 @@ namespace hpx::parallel::util::detail {
             hpx::execution::experimental::get_chunk_size(policy.parameters(),
                 policy.executor(), iteration_duration, cores, count);
 
-        auto [adj_chunk, adj_max] =
+        std::tie(chunk_size, max_chunks) =
             hpx::execution::experimental::adjust_chunk_size_and_max_chunks(
                 policy.parameters(), policy.executor(), count, cores,
                 max_chunks, chunk_size);
-        chunk_size = adj_chunk;
-        max_chunks = adj_max;
 
         if (stride != 1)
         {
@@ -587,8 +582,10 @@ namespace hpx::parallel::util::detail {
                     hpx::chrono::null_duration, cores, count);
 
             // make sure, chunk size and max_chunks are consistent
-            adjust_chunk_size_and_max_chunks(
-                cores, count, max_chunks, chunk_size, true);
+            std::tie(chunk_size, max_chunks) =
+                hpx::execution::experimental::adjust_chunk_size_and_max_chunks(
+                    policy.parameters(), policy.executor(), count, cores,
+                    max_chunks, chunk_size);
 
             if (stride != 1)
             {
