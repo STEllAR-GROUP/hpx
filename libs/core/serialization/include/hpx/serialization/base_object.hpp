30d3e9ec197e4ebab4bed0fbfcd9f31f2b54e99c
Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>
2026-03-11 13:14:37 +0000

    fixing inspect issues
    Signed-off-by: Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>

--- libs/core/serialization/include/hpx/serialization/base_object.hpp
+++ libs/core/serialization/include/hpx/serialization/base_object.hpp
@@ -1,6 +1,7 @@
 //  Copyright (c) 2014 Thomas Heller
 //  Copyright (c) 2015 Anton Bikineev
 //  Copyright (c) 2022-2025 Hartmut Kaiser
+//  Copyright (c) 2026 Ujjwal Shekhar
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -20,6 +21,7 @@
 #if defined(HPX_SERIALIZATION_HAVE_ALLOW_AUTO_GENERATE)
 #include <cstddef>
 #include <experimental/meta>
+#include <memory>
 #include <optional>
 #endif
 
--- libs/core/serialization/include/hpx/serialization/traits/is_serialization_supported.hpp
+++ libs/core/serialization/include/hpx/serialization/traits/is_serialization_supported.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2023-2025 Hartmut Kaiser
+//  Copyright (c) 2026 Ujjwal Shekhar
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
