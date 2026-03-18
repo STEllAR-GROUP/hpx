f783acbbfd4a4e46a82349e71ee6a998efab9fb2
Sainath Manda <sainathmanda777@gmail.com>
2026-03-18 21:00:59 +0530

    Modernasized HPX algorithm headers
    Signed-off-by: Sainath Manda <sainathmanda777@gmail.com>

--- cmake/HPX_PerformCxxFeatureTests.cmake
+++ cmake/HPX_PerformCxxFeatureTests.cmake
@@ -107,10 +107,7 @@ function(hpx_perform_cxx_feature_tests)
     hpx_check_for_cxx20_std_execution_policies(
       DEFINITIONS HPX_HAVE_CXX20_STD_EXECUTION_POLICES
     )
-
-    hpx_check_for_cxx20_std_ranges_iter_swap(
-      DEFINITIONS HPX_HAVE_CXX20_STD_RANGES_ITER_SWAP
-    )
+    # hpx_check_for_cxx20_std_ranges_iter_swap was deleted
 
     # hpx_check_for_cxx20_trivial_virtual_destructor was deleted
 
