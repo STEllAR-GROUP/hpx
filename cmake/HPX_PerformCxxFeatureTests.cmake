23f62cf454dbc75ac71404f9ff6c2174c5841a14
GitMasterJatin <jatinnsharmaa7@gmail.com>
2026-03-18 15:01:58 +0530

    cmake: remove HPX_WITH_CXX20_STD_RANGES_ITER_SWAP feature test
    Since C++20 is now the minimum required standard for HPX,
    std::ranges::iter_swap is always available. Remove the CMake compile
    test and unconditionally use std::ranges::iter_swap in all affected
    algorithm headers.
    
    Files changed:
    - cmake/tests/cxx20_std_ranges_iter_swap.cpp: deleted
    - cmake/HPX_AddConfigTest.cmake: removed hpx_check_for_cxx20_std_ranges_iter_swap()
    - cmake/HPX_PerformCxxFeatureTests.cmake: removed call site
    
    Headers with all HPX_HAVE_CXX20_STD_RANGES_ITER_SWAP guards removed:
    - algorithms/detail/rotate.hpp (1 guard)
    - algorithms/detail/pivot.hpp (1 guard)
    - algorithms/sort.hpp (2 guards)
    - algorithms/partial_sort.hpp (4 guards)
    - algorithms/partition.hpp (5 guards)
    - algorithms/reverse.hpp (3 guards — incl. conditional #include <ranges>)
    - algorithms/swap_ranges.hpp (2 guards)
    - algorithms/nth_element.hpp (2 guards)
    
    Fixes: https://github.com/STEllAR-GROUP/hpx/issues/7027
    
    Signed-off-by: Jatin Sharma <jatinnsharmaa7@gmail.com>

--- cmake/HPX_PerformCxxFeatureTests.cmake
+++ cmake/HPX_PerformCxxFeatureTests.cmake
@@ -108,9 +108,6 @@ function(hpx_perform_cxx_feature_tests)
       DEFINITIONS HPX_HAVE_CXX20_STD_EXECUTION_POLICES
     )
 
-    hpx_check_for_cxx20_std_ranges_iter_swap(
-      DEFINITIONS HPX_HAVE_CXX20_STD_RANGES_ITER_SWAP
-    )
 
     # hpx_check_for_cxx20_trivial_virtual_destructor was deleted
 
