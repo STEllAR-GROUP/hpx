18161c43a2e82e1c8ad545cb49c9bb5a227e644e
Priyanshi507 <hiiuiuiabi@gmail.com>
2026-03-19 11:48:40 +0000

    cmake: remove HPX_WITH_CXX20_STD_RANGES_ITER_SWAP feature test
    Signed-off-by: Priyanshi507 <hiiuiuiabi@gmail.com>

--- cmake/HPX_PerformCxxFeatureTests.cmake
+++ cmake/HPX_PerformCxxFeatureTests.cmake
@@ -106,9 +106,6 @@ function(hpx_perform_cxx_feature_tests)
 
     hpx_check_for_cxx20_std_execution_policies(
       DEFINITIONS HPX_HAVE_CXX20_STD_EXECUTION_POLICES
-    )
-
-    hpx_check_for_cxx20_std_ranges_iter_swap(
       DEFINITIONS HPX_HAVE_CXX20_STD_RANGES_ITER_SWAP
     )
 
