a129ee4a28e59cc818ae82cf870764ca8c1cddf5
Aneek22112007 <das.aneek007@gmail.com>
2026-03-20 19:51:44 +0530

    Remove unnecessary capture of n in search_n and merge master
    Signed-off-by: Aneek22112007 <das.aneek007@gmail.com>

--- libs/core/algorithms/include/hpx/parallel/algorithms/detail/search.hpp
+++ libs/core/algorithms/include/hpx/parallel/algorithms/detail/search.hpp
@@ -352,7 +352,7 @@ namespace hpx::parallel::detail {
                     tok, HPX_FORWARD(Pred, pred), HPX_FORWARD(Proj, proj));
             };
 
-            auto f2 = [first, last, n, max_start, tok](
+            auto f2 = [first, last, max_start, tok](
                           auto&&... data) mutable -> FwdIter {
                 util::detail::clear_container(data...);
 
