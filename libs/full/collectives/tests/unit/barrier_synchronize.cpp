a8079918359c6942dec04031d215867333bd8fa6
the-ivii <divyanshitalreja12@gmail.com>
2026-03-21 19:48:56 +0530

    Fix barrier_synchronize test to avoid deadlock
    Signed-off-by: the-ivii <divyanshitalreja12@gmail.com>

--- libs/full/collectives/tests/unit/barrier_synchronize.cpp
+++ libs/full/collectives/tests/unit/barrier_synchronize.cpp
@@ -4,29 +4,21 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// Regression test for #6430: verify that hpx::distributed::barrier::synchronize()
+// works correctly when called from hpx_main across multiple localities
+
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_init.hpp>
-#include <hpx/include/actions.hpp>
-#include <hpx/include/lcos.hpp>
 #include <hpx/modules/testing.hpp>
-#include <vector>
 
-void run_test();
-HPX_PLAIN_ACTION(run_test, run_test_action)
-
-void run_test()
+int hpx_main()
 {
+    // Test that barrier::synchronize() works from hpx_main
+    // This was failing with an assertion before the fix
     hpx::distributed::barrier::synchronize();
-}
 
-int hpx_main(hpx::program_options::variables_map&)
-{
-    std::vector<hpx::future<void>> futs;
-    for (auto l : hpx::find_all_localities())
-    {
-        futs.emplace_back(hpx::async<run_test_action>(l));
-    }
-    hpx::wait_all(futs);
+    // Do a second synchronization to ensure it works multiple times
+    hpx::distributed::barrier::synchronize();
 
     return hpx::finalize();
 }
