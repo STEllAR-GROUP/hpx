d6014e48d55173fa7b85359273a86c54f6b55970
Sainath Manda <sainathmanda777@gmail.com>
2026-03-28 15:56:31 +0530

    fix: restore missing HPX_PLUGIN_EXPORT_API macro
    Signed-off-by: Sainath Manda <sainathmanda777@gmail.com>

--- libs/core/plugin/include/hpx/plugin/concrete_factory.hpp
+++ libs/core/plugin/include/hpx/plugin/concrete_factory.hpp
@@ -23,7 +23,7 @@ namespace hpx::util::plugin {
 
         HPX_CXX_CORE_EXPORT template <typename BasePlugin, typename Concrete,
             typename Base, typename... Parameters>
-        struct concrete_factory_item<BasePlugin, Concrete, Base,
+        struct HPX_PLUGIN_EXPORT_API concrete_factory_item<BasePlugin, Concrete, Base,
             hpx::util::pack<Parameters...>> : public Base
         {
             [[nodiscard]] BasePlugin* create(
