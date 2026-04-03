f10d7bfd3f80090ec979ff3c22f0380ff14f5d45
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2026-04-03 18:17:09 -0500

    Limiting the amount of objects that are being cached in memory pools
    - flyby: refactor thread_local_caching_allocator
    
    Signed-off-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>

--- libs/core/executors/include/hpx/executors/parallel_executor.hpp
+++ libs/core/executors/include/hpx/executors/parallel_executor.hpp
@@ -259,7 +259,7 @@ namespace hpx::execution {
 #endif
 
             using allocator_type = hpx::util::thread_local_caching_allocator<
-                hpx::lockfree::variable_size_stack, char,
+                hpx::lockfree::variable_size_stack,
                 hpx::util::internal_allocator<>>;
             hpx::traits::detail::shared_state_ptr_t<result_type> p =
                 lcos::detail::make_continuation_alloc_nounwrap<result_type>(
