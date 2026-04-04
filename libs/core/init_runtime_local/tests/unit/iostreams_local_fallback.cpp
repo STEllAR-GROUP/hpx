dc39fcb4a574a5075173e71f95cfabf5f866967f
Abhishek Bansal <abhibansal593@gmail.com>
2026-04-04 22:13:39 +0530

    Address review comments
    Signed-off-by: Abhishek Bansal <abhibansal593@gmail.com>

--- libs/core/init_runtime_local/tests/unit/iostreams_local_fallback.cpp
+++ libs/core/init_runtime_local/tests/unit/iostreams_local_fallback.cpp
@@ -12,22 +12,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
 
 namespace {
-
-    void test_low_level_iostream_module()
-    {
-        std::vector<char> dest;
-
-        hpx::iostream::filtering_ostream out;
-        out.push(hpx::iostream::back_inserter(dest));
-        out << "low-level iostream module";
-        out.flush();
-
-        std::string const output(dest.begin(), dest.end());
-        HPX_TEST_EQ(output, "low-level iostream module");
-    }
 
     struct stream_redirect
     {
@@ -49,8 +35,8 @@ namespace {
 
     private:
         std::ostream& stream_;
-        std::streambuf* previous_;
         std::ostringstream captured_;
+        std::streambuf* previous_;
     };
 }    // namespace
 
@@ -61,17 +47,15 @@ int hpx_main()
 
     hpx::cout << "local cout smoke test" << hpx::endl;
     hpx::cerr << "local cerr smoke test" << hpx::flush;
-    hpx::consolestream << "local console smoke test" << hpx::async_endl;
+    hpx::consolestream << "local console smoke test" << hpx::endl;
     hpx::util::format_to(hpx::consolestream, "{} {}", "format", 42)
-        << hpx::async_flush;
+        << hpx::flush;
 
     return hpx::local::finalize();
 }
 
 int main(int argc, char** argv)
 {
-    test_low_level_iostream_module();
-
     stream_redirect captured_cout(std::cout);
     stream_redirect captured_cerr(std::cerr);
 
