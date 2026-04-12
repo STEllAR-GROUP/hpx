e2d2924a5a9fcf21c359f3ae7b3154cb9a10231b
LSUDOKO <adoranto737@gmail.com>
2026-04-13 00:26:10 +0530

    add my name

--- libs/core/synchronization/tests/performance/channel_mpmc_comparison.cpp
+++ libs/core/synchronization/tests/performance/channel_mpmc_comparison.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2026 Arpit Kumar
+//  Copyright (c) 2026 Arpit Singh
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
