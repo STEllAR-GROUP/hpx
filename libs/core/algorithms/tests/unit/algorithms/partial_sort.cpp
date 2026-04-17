61c4c4d21fd14285570f34d400d8109858f70362
Aneek22112007 <das.aneek007@gmail.com>
2026-04-18 00:52:17 +0530

    Add return-iterator compliance and edge-case tests for partial_sort

--- libs/core/algorithms/tests/unit/algorithms/partial_sort.cpp
+++ libs/core/algorithms/tests/unit/algorithms/partial_sort.cpp
@@ -1,6 +1,5 @@
 //  Copyright (c) 2020 Francisco Jose Tapia (fjtapia@gmail.com )
 //  Copyright (c) 2021 Akhil J Nair
-//  Copyright (c) 2024 Aneek Barman
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
