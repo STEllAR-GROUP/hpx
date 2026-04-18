9e9e28cc4eeb01b54d3c744513c9cf6ff7a71ca8
Sanchit2662 <sanchit2662@gmail.com>
2026-04-18 22:15:47 +0530

    contracts: add violation handler infrastructure
    Signed-off-by: Sanchit2662 <sanchit2662@gmail.com>

---
+++ libs/core/contracts/include/hpx/contracts/violation_handler.hpp
@@ -0,0 +1,36 @@
+//  Copyright (c) 2026 The STE||AR-Group
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+#include <hpx/assertion/source_location.hpp>
+
+namespace hpx::contracts {
+
+    enum class contract_kind
+    {
+        pre,
+        post,
+        assertion
+    };
+
+    struct violation_info
+    {
+        contract_kind kind;
+        char const* condition;
+        hpx::source_location location;
+    };
+
+    HPX_CXX_CORE_EXPORT using violation_handler_t = void (*)(violation_info const&);
+
+    HPX_CORE_EXPORT void set_violation_handler(violation_handler_t handler) noexcept;
+    HPX_CORE_EXPORT violation_handler_t get_violation_handler() noexcept;
+
+    HPX_CORE_EXPORT void default_violation_handler(violation_info const& info);
+    HPX_CORE_EXPORT void invoke_violation_handler(violation_info const& info);
+
+}    // namespace hpx::contracts
---
+++ libs/core/contracts/src/violation_handler.cpp
@@ -0,0 +1,64 @@
+//  Copyright (c) 2026 The STE||AR-Group
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/contracts/violation_handler.hpp>
+
+#include <cstdlib>
+#include <iostream>
+
+namespace hpx::contracts {
+
+    namespace detail {
+
+        [[nodiscard]] violation_handler_t& get_handler() noexcept
+        {
+            static violation_handler_t handler = nullptr;
+            return handler;
+        }
+
+    }    // namespace detail
+
+    void set_violation_handler(violation_handler_t handler) noexcept
+    {
+        detail::get_handler() = handler;
+    }
+
+    violation_handler_t get_violation_handler() noexcept
+    {
+        return detail::get_handler();
+    }
+
+    void default_violation_handler(violation_info const& info)
+    {
+        char const* kind_str = nullptr;
+        switch (info.kind)
+        {
+        case contract_kind::pre:
+            kind_str = "precondition";
+            break;
+        case contract_kind::post:
+            kind_str = "postcondition";
+            break;
+        case contract_kind::assertion:
+            kind_str = "assertion";
+            break;
+        }
+
+        std::cerr << info.location << ": Contract " << kind_str << " '"
+                  << info.condition << "' violated\n";
+        std::abort();
+    }
+
+    void invoke_violation_handler(violation_info const& info)
+    {
+        violation_handler_t handler = detail::get_handler();
+        if (handler == nullptr)
+            default_violation_handler(info);
+        else
+            handler(info);
+    }
+
+}    // namespace hpx::contracts
