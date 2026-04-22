87d9f62ef609b3d3a207e1ea5bd8d09cc555b504
Aneek22112007 <das.aneek007@gmail.com>
2026-04-22 14:56:10 +0530

    Remove assistant-added copyright header from regression test

--- libs/core/algorithms/tests/unit/algorithms/minmax_element_parallel_projection.cpp
+++ libs/core/algorithms/tests/unit/algorithms/minmax_element_parallel_projection.cpp
@@ -1,8 +1,3 @@
-//  Copyright (c) 2024 The STE||AR-Group
-//
-//  SPDX-License-Identifier: BSL-1.0
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 // Regression test for: incorrect return types in
 // max_element::sequential_minmax_element_ind and
