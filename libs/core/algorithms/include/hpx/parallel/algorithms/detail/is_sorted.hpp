65cebbdbdfa15d8f9f3074c7fbec16d4fc1e1f58
Aneek22112007 <das.aneek007@gmail.com>
2026-04-28 03:44:04 +0530

    Fix clang-format: restore unmodified files to upstream state

--- libs/core/algorithms/include/hpx/parallel/algorithms/detail/is_sorted.hpp
+++ libs/core/algorithms/include/hpx/parallel/algorithms/detail/is_sorted.hpp
@@ -13,7 +13,7 @@
 
 namespace hpx::parallel::detail {
 
-    template <typename Iter, typename Sent,
+    HPX_CXX_CORE_EXPORT template <typename Iter, typename Sent,
         typename Compare, typename Proj = hpx::identity>
     constexpr bool is_sorted_sequential(
         Iter first, Sent last, Compare&& comp, Proj&& proj = Proj())
@@ -32,7 +32,7 @@ namespace hpx::parallel::detail {
         return sorted;
     }
 
-    template <typename Iter, typename Sent,
+    HPX_CXX_CORE_EXPORT template <typename Iter, typename Sent,
         typename Compare, typename Proj = hpx::identity>
     constexpr Iter is_sorted_until_sequential(
         Iter first, Sent last, Compare&& comp, Proj&& proj = Proj())
