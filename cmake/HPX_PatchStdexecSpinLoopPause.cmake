d84b21a560c818f10c00c3f86bc70e9aa2b042fa
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2026-07-30 17:28:51 -0500

    Update HPX_PatchStdexecSpinLoopPause.cmake
    Formatting new file

--- cmake/HPX_PatchStdexecSpinLoopPause.cmake
+++ cmake/HPX_PatchStdexecSpinLoopPause.cmake
@@ -4,8 +4,8 @@
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# This patch file works around a problem seen on ARM gcc 11.4.0, 
-# see: https://github.com/NVIDIA/stdexec/issues/2150
+# This patch file works around a problem seen on ARM gcc 11.4.0, see:
+# https://github.com/NVIDIA/stdexec/issues/2150
 
 if(NOT DEFINED STDEXEC_SPIN_LOOP_PAUSE_FILE)
   message(FATAL_ERROR "STDEXEC_SPIN_LOOP_PAUSE_FILE must be defined")
@@ -18,8 +18,11 @@ endif()
 file(READ "${STDEXEC_SPIN_LOOP_PAUSE_FILE}" _stdexec_spin_loop_pause_contents)
 
 string(
-  REPLACE "STDEXEC_ATTRIBUTE(always_inline) inline" "STDEXEC_ATTRIBUTE(always_inline)"
-          _stdexec_spin_loop_pause_contents "${_stdexec_spin_loop_pause_contents}"
+  REPLACE "STDEXEC_ATTRIBUTE(always_inline) inline"
+          "STDEXEC_ATTRIBUTE(always_inline)" _stdexec_spin_loop_pause_contents
+          "${_stdexec_spin_loop_pause_contents}"
 )
 
-file(WRITE "${STDEXEC_SPIN_LOOP_PAUSE_FILE}" "${_stdexec_spin_loop_pause_contents}")
+file(WRITE "${STDEXEC_SPIN_LOOP_PAUSE_FILE}"
+     "${_stdexec_spin_loop_pause_contents}"
+)
