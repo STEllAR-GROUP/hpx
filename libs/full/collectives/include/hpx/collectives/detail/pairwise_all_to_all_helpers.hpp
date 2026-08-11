43096131ff7c758f762a126058c22ec73dd50a40
iemAnshuman <asquare567@gmail.com>
2026-08-11 16:20:16 +0530

    Move pairwise diagonal insertion outside loops
    Signed-off-by: iemAnshuman <asquare567@gmail.com>

--- libs/full/collectives/include/hpx/collectives/detail/pairwise_all_to_all_helpers.hpp
+++ libs/full/collectives/include/hpx/collectives/detail/pairwise_all_to_all_helpers.hpp
@@ -188,14 +188,20 @@ namespace hpx::collectives::detail {
                     std::vector<T> result;
                     result.reserve(num_sites);
 
-                    for (std::size_t source = 0; source != num_sites; ++source)
+                    for (std::size_t source = 0; source != this_site; ++source)
                     {
-                        if (source == this_site)
-                        {
-                            result.push_back(HPX_MOVE(diagonal));
-                            continue;
-                        }
+                        // received[k - 1] came from the peer k hops behind
+                        // this site, matching the receive order posted above.
+                        std::size_t const k =
+                            (this_site + num_sites - source) % num_sites;
+                        result.push_back(received[k - 1].get());
+                    }
 
+                    result.push_back(HPX_MOVE(diagonal));
+
+                    for (std::size_t source = this_site + 1;
+                        source != num_sites; ++source)
+                    {
                         // received[k - 1] came from the peer k hops behind
                         // this site, matching the receive order posted above.
                         std::size_t const k =
