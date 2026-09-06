97449b48a5c0bb1c81e54ab85f661ee6bb15ece6
v4xsh <vanshdobhal11@gmail.com>
2026-09-06 23:59:48 +0530

    async_colocated: drop whole-file HPX_COMPUTE_DEVICE_CODE guard
    Signed-off-by: v4xsh <vanshdobhal11@gmail.com>

--- libs/full/async_colocated/include/hpx/async_colocated/post_colocated.hpp
+++ libs/full/async_colocated/include/hpx/async_colocated/post_colocated.hpp
@@ -8,7 +8,6 @@
 
 #include <hpx/config.hpp>
 
-#if !defined(HPX_COMPUTE_DEVICE_CODE)
 #include <hpx/modules/actions_base.hpp>
 #include <hpx/modules/agas_base.hpp>
 #include <hpx/modules/async_base.hpp>
@@ -100,4 +99,3 @@ namespace hpx::detail {
     }
 }    // namespace hpx::detail
 
-#endif
