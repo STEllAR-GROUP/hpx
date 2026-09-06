400f809b5c4837cf61016f0a03c3d08268191513
v4xsh <vanshdobhal11@gmail.com>
2026-09-06 23:59:50 +0530

    runtime_configuration: drop HPX_COMPUTE_DEVICE_CODE guard from component_factory_base
    Signed-off-by: v4xsh <vanshdobhal11@gmail.com>

--- libs/core/runtime_configuration/include/hpx/runtime_configuration/component_factory_base.hpp
+++ libs/core/runtime_configuration/include/hpx/runtime_configuration/component_factory_base.hpp
@@ -8,7 +8,6 @@
 
 #include <hpx/config.hpp>
 
-#if !defined(HPX_COMPUTE_DEVICE_CODE)
 #include <hpx/runtime_configuration/component_registry_base.hpp>
 
 namespace hpx::components {
@@ -16,4 +15,3 @@ namespace hpx::components {
     HPX_CXX_CORE_EXPORT struct HPX_CORE_EXPORT component_factory_base;
 }    // namespace hpx::components
 
-#endif
